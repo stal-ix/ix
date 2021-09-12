c53ddd4bd823ecddfa09486eabacf956a16e6881
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-09-13 01:24:36 +0300

    refactor

--- pkgs/lib/xml2/package.sh
+++ pkgs/lib/xml2/package.sh
@@ -1,23 +1,23 @@
+{% extends '//util/autohell.sh' %}
+
+{% block fetch %}
 # url http://xmlsoft.org/sources/libxml2-2.9.12.tar.gz
 # md5 f433a39be087a9f0b197eb2307ad9f75
+{% endblock %}
+
+{% block deps %}
 # bld lib/iconv boot/final/env
+{% endblock %}
 
-build() {
-    $untar $src/libxml* && cd libxml*
+{% block coflags %}
+--with-ftp=off
+--with-http=off
+--with-modules=off
+{% endblock %}
 
-    dash ./configure $COFLAGS \
-        --prefix="$out" \
-        --with-ftp=off \
-        --with-http=off \
-        --with-modules=off
-
-    make -j $make_thrs
-    make install
-
-    cat << EOF > $out/env
+{% block env %}
 export CPPFLAGS="-I$out/include \$CPPFLAGS"
 export LDFLAGS="-L$out/lib -lxml2 \$LDFLAGS"
 export PKG_CONFIG_PATH="$out/lib/pkgconfig:\$PKG_CONFIG_PATH"
 export COFLAGS="--with-libxml2-prefix=$out \$COFLAGS"
-EOF
-}
+{% endblock %}
