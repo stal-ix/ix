226ae4d95e96027b6477d6a0049ad92f49c988c7
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-09-13 02:51:59 +0300

    refactor

--- pkgs/lib/apr-util/package.sh
+++ pkgs/lib/apr-util/package.sh
@@ -1,23 +1,23 @@
+{% extends '//util/autihell.sh' %}
+
+{% block fetch %}
 # url https://archive.apache.org/dist/apr/apr-util-1.6.1.tar.bz2
 # md5 8ff5dc36fa39a2a3db1df196d3ed6086
+{% endblock %}
+
+{% block deps %}
 # lib lib/apr lib/gdbm lib/iconv lib/expat lib/sqlite3 lib/openssl
 # bld dev/build/make dev/build/pkg-config env/std
+{% endblock %}
 
-build() {
-    $untar $src/apr* && cd apr*
+{% block coflags %}
+--with-dbm=gdbm
+--disable-util-dso
+{% endblock %}
 
-    dash ./configure $COFLAGS \
-        --prefix=$out \
-        --with-dbm=gdbm \
-        --disable-util-dso
-
-    make -j $make_thrs
-    make install
-
-    cat << EOF > $out/env
+{% block env %}
 export CPPFLAGS="-I$out/include \$CPPFLAGS"
 export LDFLAGS="-L$out/lib -laprutil-1 \$LDFLAGS"
 export COFLAGS="--with-apr-util=$out --with-libapr-util=$out \$COFLAGS"
 export PKG_CONFIG_PATH="$out/lib/pkgconfig:\$PKG_CONFIG_PATH"
-EOF
-}
+{% endblock %}
--- pkgs/lib/edit/package.sh
+++ pkgs/lib/edit/package.sh
@@ -1,24 +1,22 @@
+{% extends '//util/autohell.sh' %}
+
+{% block fetch %}
 # url http://thrysoee.dk/editline/libedit-20210522-3.1.tar.gz
 # md5 6ad9e9208daf031adf1ebc64441769e0
+{% endblock %}
+
+{% block deps %}
 # lib lib/ncurses
 # bld dev/build/make env/std
+{% endblock %}
 
-build() {
-    $untar $src/libedit* && cd libedit*
+{% block cflags %}
+export CPPFLAGS="-D__STDC_ISO_10646__=1 $CPPFLAGS"
+{% endblock %}
 
-    ln -s $(which dash) sh
-
-    export PATH="$(pwd):$PATH"
-    export CPPFLAGS="-D__STDC_ISO_10646__=1 $CPPFLAGS"
-
-    dash ./configure $COFLAGS --prefix=$out
-    make -j $make_thrs
-    make install
-
-    cat << EOF > $out/env
+{% block env %}
 export COFLAGS="--with-libedit=$out \$COFLAGS"
 export CPPFLAGS="-I$out/include \$CPPFLAGS"
 export LDFLAGS="-L$out/lib -ledit \$LDFLAGS"
 export PKG_CONFIG_PATH="$out/lib/pkgconfig:\$PKG_CONFIG_PATH"
-EOF
-}
+{% endblock %}
