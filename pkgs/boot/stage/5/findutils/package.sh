e7e24e8f5107194cea50eb2ab0172d165ae80646
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-09-12 20:14:47 +0300

    refactor

--- pkgs/boot/stage/5/findutils/package.sh
+++ pkgs/boot/stage/5/findutils/package.sh
@@ -1,12 +1,5 @@
+{% extends '//tool/gnu/findutils/package.sh' %}
+
+{% block deps %}
 # bld boot/stage/4/env
-{% include '//tool/gnu/findutils/version.sh' %}
-
-build() {
-    $untar $src/find* && cd find*
-
-    (cd gl/lib/malloc && echo '#define __nonnull(x)' > tmp && cat dynarray-skeleton.c >> tmp && mv tmp dynarray-skeleton.c)
-
-    dash ./configure $COFLAGS --prefix=$out
-    make -j $make_thrs
-    make install
-}
+{% endblock %}
--- pkgs/tool/gnu/findutils/package.sh
+++ pkgs/tool/gnu/findutils/package.sh
@@ -1,15 +1,16 @@
+{% extends '//util/autohell.sh' %}
+
+{% block fetch %}
 # url https://ftp.gnu.org/gnu/findutils/findutils-4.8.0.tar.xz
 # md5 eeefe2e6380931a77dfa6d9350b43186
+{% endblock %}
+
+{% block deps %}
 # bld env/c lib/iconv boot/final/env
+{% endblock %}
 
-build() {
-    $untar $src/find* && cd find*
-
-    echo '#define __nonnull(x)' > tmp
-    cat gl/lib/malloc/dynarray-skeleton.c >> tmp
-    mv tmp gl/lib/malloc/dynarray-skeleton.c
-
-    dash ./configure $COFLAGS --prefix=$out
-    make -j $make_thrs
-    make install
-}
+{% block patch %}
+echo '#define __nonnull(x)' > tmp
+cat gl/lib/malloc/dynarray-skeleton.c >> tmp
+mv tmp gl/lib/malloc/dynarray-skeleton.c
+{% endblock %}
