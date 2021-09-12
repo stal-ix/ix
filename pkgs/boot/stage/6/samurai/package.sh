1795d333e0f0b8a57ed2c4c3f73850524c8b25f0
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-09-12 20:39:40 +0300

    refactor

--- pkgs/boot/stage/6/samurai/package.sh
+++ pkgs/boot/stage/6/samurai/package.sh
@@ -1,15 +1,5 @@
+{% extends '//dev/build/samurai/package.sh' %}
+
+{% block deps %}
 # bld boot/stage/5/env
-{% include '//dev/build/samurai/version.sh' %}
-
-build() {
-    $unzip $src/*.zip && cd sam*
-
-    export CPPFLAGS="-Dwarn=samu_warn $CPPFLAGS"
-
-    setup_compiler
-
-    make PREFIX=$out CC=clang
-    make PREFIX=$out install
-
-    cd $out/bin && ln -s samu ninja
-}
+{% endblock %}
--- pkgs/dev/build/samurai/package.sh
+++ pkgs/dev/build/samurai/package.sh
@@ -1,15 +1,24 @@
+{% extends '//utils/make.sh' %}
+
+{% block fetch %}
+# url https://github.com/michaelforney/samurai/archive/edeec43d638c826d9e446917f97e95151988e0e0.zip
+# md5 2321662d68a58456cc9ac9a2605beb59
+{% endblock %}
+
+{% block deps %}
 # bld dev/build/make env/std
-{% include 'version.sh' %}
+{% endblock %}
 
-build() {
-    $unzip $src/*.zip && cd sam*
+{% block cflags %}
+export CPPFLAGS="-Dwarn=samu_warn $CPPFLAGS"
+{% endblock %}
 
-    export CPPFLAGS="-Dwarn=samu_warn $CPPFLAGS"
+{% block build %}
+make PREFIX=$out CC=clang
+{% endblock %}
 
-    setup_compiler
+{% block install %}
+make PREFIX=$out install
 
-    make PREFIX=$out CC=clang
-    make PREFIX=$out install
-
-    cd $out/bin && ln -s samu ninja
-}
+cd $out/bin && ln -s samu ninja
+{% endblock %}
