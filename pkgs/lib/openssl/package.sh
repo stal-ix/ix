074b97bf6921ab67f8c0faa458a8870d9ff8ab92
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-09-13 03:29:42 +0300

    refactor

--- pkgs/lib/openssl/package.sh
+++ pkgs/lib/openssl/package.sh
@@ -1,47 +1,46 @@
+{% extends '//util/make.sh' %}
+
+{% block fetch %}
 # url https://www.openssl.org/source/old/1.1.1/openssl-1.1.1j.tar.gz
 # md5 cccaa064ed860a2b4d1303811bf5c682
+{% endblock %}
+
+{% block deps %}
 # bld {{'lib/linux' | linux}}
 # bld dev/build/make dev/lang/perl5 env/std
+{% endblock %}
 
-build() {
-    $untar $src/openssl* && cd openssl*
+{% block cflags %}
+export AR=ar
+export RANLIB=ranlib
+{% endblock %}
 
-    export AR=ar
-    export RANLIB=ranlib
+{% block configure %}
+PLATFORM_darwin_arm64="darwin64-arm64-cc"
+PLATFORM_darwin_x86_64="darwin64-x86_64-cc"
+PLATFORM_linux_x86_64="linux-x86_64-clang"
 
-    setup_compiler
+PLATFORM=$PLATFORM_{{mix.platform.target.os}}_{{mix.platform.target.arch}}
 
-    PLATFORM_darwin_arm64="darwin64-arm64-cc"
-    PLATFORM_darwin_x86_64="darwin64-x86_64-cc"
-    PLATFORM_linux_x86_64="linux-x86_64-clang"
+perl ./Configure \
+    $PLATFORM \
+    no-asm \
+    threads \
+    no-shared \
+    no-dso \
+    no-hw \
+    no-tests \
+    --prefix="$out" \
+    --openssldir="$out"
+{% endblock %}
 
-    PLATFORM=$PLATFORM_{{mix.platform.target.os}}_{{mix.platform.target.arch}}
-
-    perl ./Configure \
-        $PLATFORM \
-        no-asm \
-        threads \
-        no-shared \
-        no-dso \
-        no-hw \
-        no-tests \
-        --prefix="$out" \
-        --openssldir="$out"
-
-    make -j $make_thrs
-    make install
-
-    cat << EOF > $out/env
+{% block env %}
 export COFLAGS="--with-openssl=$out --with-openssldir=$out --with-ssl-dir=$out \$COFLAGS"
 export OPENSSL_INCLUDES="-I$out/include"
 export OPENSSL_LIBS="-L$out/lib -lssl -lcrypto"
 export OPENSSL_DIR="$out"
 export CPPFLAGS="\$OPENSSL_INCLUDES \$CPPFLAGS"
-{% if mix.platform.target.os == 'linux' %}
-export LDFLAGS="-lpthread \$LDFLAGS"
-{% endif %}
 export LDFLAGS="\$OPENSSL_LIBS \$LDFLAGS"
 export PKG_CONFIG_PATH="$out/lib/pkgconfig:\$PKG_CONFIG_PATH"
 export CMFLAGS="-DOPENSSL_ROOT_DIR=$out -DOPENSSL_INCLUDE_DIR=$out/include \$CMFLAGS"
-EOF
-}
+{% endblock %}
