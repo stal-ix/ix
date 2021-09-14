b72ef003606c70ae113a56d9b1522e58afa43f50
PG <anton@samokhvalov.xyz>
2021-09-14 08:59:36 +0300

    refactor

--- pkgs/dev/build/ninja/package.sh
+++ pkgs/dev/build/ninja/package.sh
@@ -1,11 +1,14 @@
+{% extends '//util/cmake.sh' %}
+
+{% block fetch %}
 # url https://github.com/ninja-build/ninja/archive/refs/tags/v1.10.2.tar.gz
 # md5 639f75bc2e3b19ab893eaf2c810d4eb4
-# bld lib/cxx dev/lang/python3 env/std
+{% endblock %}
 
-build() {
-    $untar ${src}/v* && cd ninja*
+{% block deps %}
+# bld lib/cxx dev/lang/python3 dev/build/cmake env/std
+{% endblock %}
 
-    setup_compiler
-    python3 ./configure.py --bootstrap
-    mkdir -p ${out}/bin && cp ninja ${out}/bin/
-}
+{% block cmflags %}
+..
+{% endblock %}
