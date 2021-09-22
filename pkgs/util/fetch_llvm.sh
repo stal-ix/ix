dacd74dca2ead4208674b7773365db6a7dcf00e4
PG <anton@samokhvalov.xyz>
2021-09-22 14:13:40 +0300

    new clang

--- pkgs/util/fetch_llvm.sh
+++ pkgs/util/fetch_llvm.sh
@@ -1,2 +1,2 @@
-# url https://github.com/llvm/llvm-project/releases/download/llvmorg-13.0.0-rc2/llvm-project-13.0.0rc2.src.tar.xz
-# md5 79d7a1e5ca0d788ff3380101f1c9f2b5
+# url https://github.com/llvm/llvm-project/releases/download/llvmorg-13.0.0-rc3/llvm-project-13.0.0rc3.src.tar.xz
+# md5 7b8fc01d1411aa69617062c87c14d010
