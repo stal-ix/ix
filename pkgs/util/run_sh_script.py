2f670a5f533849d707ff7899b29b9e1ee4de4246
PG <anton@samokhvalov.xyz>
2021-09-23 06:03:55 +0300

    better

--- pkgs/util/run_sh_script.py
+++ pkgs/util/run_sh_script.py
@@ -20,4 +20,9 @@ def iter_lines():
     yield DATA
 
 
+# prepare tmp dir
+tmp = os.path.join(os.environ['tmp'], 'tmp')
+os.makedirs(tmp)
+os.environ['TMPDIR'] = tmp
+
 subprocess.run([mix.which('dash')], input=('\n'.join(iter_lines())).encode(), check=True)
