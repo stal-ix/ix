731c91c7935a163b2689c1129554dbcd5bcb2ca3
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-09-26 23:49:13 +0300

    better

--- pkgs/boot/stage/6/clang/package.sh
+++ pkgs/boot/stage/6/clang/package.sh
@@ -1,6 +1,8 @@
 {% extends '//dev/lang/clang/package.sh' %}
 
 {% block extradeps %}
-# bld {{'boot/lib/linux' | linux}} boot/lib/cxx
+# bld {{'boot/lib/linux' | linux}}
+# bld {{'sys/framework/CoreFoundation' | darwin}}
+# bld boot/lib/cxx
 # bld boot/stage/6/cmake boot/stage/6/python boot/stage/5/env
 {% endblock %}
