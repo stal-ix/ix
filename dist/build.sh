7b6297da6ae1cf1854124214cc9bbe9d53e0a134
PG <anton@samokhvalov.xyz>
2021-09-29 22:14:38 +0300

    better

--- dist/build.sh
+++ dist/build.sh
@@ -19,14 +19,17 @@ cd /mix && mkdir bootstrap && cd bootstrap
 python3 -m venv venv
 python3 -m pip install jinja2 beautysh boto3
 
-python3 /bootstrap/mix realm add stable \
-    mix/mix \
-        -Dmix_ver=0f64ec97c3fac42909723c16289aeed99872cf15 \
-        -Dmix_md5=2f6f7a9f678696ed0913d54c4622b3ce \
-    sys/linux/busybox
+python3 /bootstrap/mix realm add stable mix/mix \
+    -Dmix_ver=0f64ec97c3fac42909723c16289aeed99872cf15 \
+    -Dmix_md5=2f6f7a9f678696ed0913d54c4622b3ce
 
 mix realm upgrade
 mix realm upgrade
+
+mix realm add stable  \
+    sys/linux/busybox \
+    tool/curl
+
 mix gc
 EOF
 
