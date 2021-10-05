d208f129ca7fe1545c8aa69f290f033dffbcfa49
PG <anton@samokhvalov.xyz>
2021-10-05 15:57:31 +0300

    better

--- pkgs/pypi/botocore/bin/package.sh
+++
@@ -1,7 +0,0 @@
-{% extends '//pypi/botocore/package.sh' %}
-
-{% block postinstall %}
-find ${out}/lib | grep \\.py | while read l; do
-    sed -e 's|BOTOCORE_ROOT = .*|BOTOCORE_ROOT = ""|' -i ${l}
-done
-{% endblock %}
