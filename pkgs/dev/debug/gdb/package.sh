818e71591b96c1f30c409f5179a6c6ba9cbf817e
PG <anton@samokhvalov.xyz>
2021-10-09 17:51:36 +0300

    better

--- pkgs/dev/debug/gdb/package.sh
+++ pkgs/dev/debug/gdb/package.sh
@@ -1,8 +1,7 @@
 {% extends '//util/autohell.sh' %}
 
 {% block fetch %}
-# url https://ftp.gnu.org/gnu/gdb/gdb-11.1.tar.xz
-# md5 257cb0f67927f79acf636d8c01e19990
+{% include 'version.sh' %}
 {% endblock %}
 
 {% block deps %}
--- pkgs/dev/debug/gdb/py/package.sh
+++ pkgs/dev/debug/gdb/py/package.sh
@@ -1,8 +1,7 @@
 {% extends '//util/python/lib.sh' %}
 
 {% block fetch %}
-# url https://ftp.gnu.org/gnu/gdb/gdb-11.1.tar.xz
-# md5 257cb0f67927f79acf636d8c01e19990
+{% include '//dev/debug/gdb/version.sh' %}
 {% endblock %}
 
 {% block deps %}
