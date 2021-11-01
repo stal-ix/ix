b2ad6eaf199cae546d647c35bb4c2784ccaeb772
PG <anton@samokhvalov.xyz>
2021-11-01 22:46:26 +0300

    better

--- pkgs/lib/curses/netbsd/mix.sh
+++ pkgs/lib/curses/netbsd/mix.sh
@@ -5,9 +5,9 @@
 # md5 8798fe3e6acce82cb08e39b1532b4664
 {% endblock %}
 
-{% block deps %}
-# bld dev/build/make/mix.sh
-# bld env/std/mix.sh
+{% block bld_deps %}
+dev/build/make/mix.sh
+env/std/mix.sh
 {% endblock %}
 
 {% block build %}
