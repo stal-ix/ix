f77d9ce82bee35263ba28ee46220c9168c03c6c2
PG <anton@samokhvalov.xyz>
2021-11-02 16:21:55 +0300

    refactor

--- pkgs/dev/build/cmake/cmake.sh
+++ pkgs/dev/build/cmake/cmake.sh
@@ -2,19 +2,19 @@ build_cmake_prepare() {
     (rm -rf build || true) && mkdir build && cd build
 
     cmake \
-        ${CMFLAGS}                                         \
-        -Wno-dev                                           \
-        -DCMAKE_INSTALL_PREFIX="${out}"                    \
-        -DCMAKE_C_COMPILER="$(command -v clang)"           \
-        -DCMAKE_CXX_COMPILER="$(command -v clang++)"       \
-        -DCMAKE_AR="$(command -v ar)"                      \
-        -DCMAKE_RANLIB="$(command -v ranlib)"              \
-        -DCMAKE_BUILD_TYPE=Release                         \
-        -DBUILD_SHARED_LIBS=OFF                            \
-        -DCMAKE_PREFIX_PATH="${CMPATH}"                    \
-        -DCMAKE_FIND_USE_CMAKE_SYSTEM_PATH=NO              \
-        -DCMAKE_FIND_USE_SYSTEM_ENVIRONMENT_PATH=NO        \
-        -DCMAKE_FIND_USE_SYSTEM_PACKAGE_REGISTRY=NO        \
+        ${CMFLAGS}                                   \
+        -Wno-dev                                     \
+        -DCMAKE_INSTALL_PREFIX="${out}"              \
+        -DCMAKE_C_COMPILER="$(command -v clang)"     \
+        -DCMAKE_CXX_COMPILER="$(command -v clang++)" \
+        -DCMAKE_AR="$(command -v ar)"                \
+        -DCMAKE_RANLIB="$(command -v ranlib)"        \
+        -DCMAKE_BUILD_TYPE=Release                   \
+        -DBUILD_SHARED_LIBS=OFF                      \
+        -DCMAKE_PREFIX_PATH="${CMPATH}"              \
+        -DCMAKE_FIND_USE_CMAKE_SYSTEM_PATH=NO        \
+        -DCMAKE_FIND_USE_SYSTEM_ENVIRONMENT_PATH=NO  \
+        -DCMAKE_FIND_USE_SYSTEM_PACKAGE_REGISTRY=NO  \
         $@
 }
 
