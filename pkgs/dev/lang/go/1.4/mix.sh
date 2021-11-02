ab7e7b578cbc9cc5183c761dc4ac11920bc56815
PG <anton@samokhvalov.xyz>
2021-11-02 18:50:20 +0300

    refactor

--- pkgs/dev/lang/go/1.4/mix.sh
+++ pkgs/dev/lang/go/1.4/mix.sh
@@ -1,21 +1,35 @@
+{% extends '//mix/template/template.sh' %}
+
+{% block fetch %}
 # url https://golang.org/dl/go1.4-bootstrap-20170531.tar.gz
 # md5 d2cc61cb9f829b3510ee39c0c5568014
-# bld shell/cli/bash/minimal/mix.sh
-# bld env/std/mix.sh
+{% endblock %}
 
-build() {
-    cd ${out} && ${untar} ${src}/go* && cd go*
+{% block bld_deps %}
+shell/cli/bash/minimal/mix.sh
+env/std/mix.sh
+{% endblock %}
 
-    setup_compiler
+{% block postunpack %}
+cd src
+{% endblock %}
 
-    (
-        export GOROOT_FINAL="${out}/go"
-        cd src && cat run.bash | grep -v 'time go test' > qw && mv qw run.bash && bash ./all.bash
-    )
+{% block cflags %}
+export GOROOT_FINAL="${out}/go"
+{% endblock %}
 
-    cd ${out} && ln -s go/bin ./
+{% block patch %}
+cat run.bash | grep -v 'time go test' > _ && mv _ run.bash
+{% endblock %}
 
-    cat << EOF > ${out}/env
-export GOROOT_BOOTSTRAP="${out}/go"
-EOF
-}
+{% block build %}
+bash ./all.bash
+{% endblock %}
+
+{% block install %}
+cd ${out} && ln -s go/bin ./
+{% endblock %}
+
+{% block env %}
+export GOROOT_BOOTSTRAP="${GOROOT_FINAL}"
+{% endblock %}
