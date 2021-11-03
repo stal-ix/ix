8246eb0e1d13e146ee7d27f92d5f5402ba0d452c
PG <anton@samokhvalov.xyz>
2021-11-03 18:27:11 +0300

    refactor

--- pkgs/boot/stage/2/env/mix.sh
+++ pkgs/boot/stage/2/env/mix.sh
@@ -1,6 +1,5 @@
-# run {{'boot/stage/2/sbase/mix.sh' | linux}}
+# run boot/stage/2/sbase/mix.sh
 # run {{'boot/stage/2/bsdpatch/mix.sh' | darwin}}
 # run boot/stage/2/heirloom/mix.sh
 # run boot/stage/2/bmake/mix.sh
-# run boot/stage/2/shutil/mix.sh
 # run boot/stage/0/env/mix.sh
