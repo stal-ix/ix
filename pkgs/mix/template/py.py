cf7efd23f6fc5efced737595c054f49124a47d93
PG <anton@samokhvalov.xyz>
2021-11-04 21:46:34 +0300

    unify

--- pkgs/mix/template/py.py
+++ pkgs/mix/template/py.py
@@ -1,11 +1,33 @@
 {% extends 'mix.json' %}
 
 {% block script_body %}
-{% block build %}
-{% include 'run_sh_script.py' %}
-{% endblock %}
-
 import os
+
+{% block build %}
+import subprocess
+
+DATA = r"""
+{% block script %}{% endblock %}
+"""
+
+def iter_lines():
+    yield 'set -e'
+    yield 'set -x'
+
+    for p in reversed(os.environ['PATH'].split(':')):
+        pp = os.path.normpath(os.path.join(p, '..', 'env'))
+
+        if os.path.isfile(pp):
+            yield '. ' + pp
+
+    yield DATA
+
+if DATA.strip():
+    tmp = os.path.join(os.environ['tmp'], 'tmp')
+    os.makedirs(tmp)
+    os.environ['TMPDIR'] = tmp
+    subprocess.run([mix.which('dash')], input=('\n'.join(iter_lines())).encode(), check=True)
+{% endblock %}
 
 env_data = r"""
 {% block env %}
