191445df36f2ef8f5ccb83ecb6d1b0a81d264254
PG <anton@samokhvalov.xyz>
2021-11-04 20:49:21 +0300

    better

--- pkgs/fix.py
+++ pkgs/fix.py
@@ -1,31 +1,8 @@
 import os
 
-T = r'''
-{% extends '//mix/template/hub.sh' %}
-
-{% block deps %}
-{lst}
-{% endblock %}
-'''
 
 def fix(d):
-    if not d.startswith('# run'):
-        return d
-
-    v = []
-
-    for l in d.split('\n'):
-        l = l.strip()
-
-        if not l:
-            continue
-
-        if '# run' not in l:
-            return d
-
-        v.append(l[6:])
-
-    return T.replace('{lst}', '\n'.join(v)).strip() + '\n'
+    return d
 
 
 for a, b, c in os.walk('.'):
