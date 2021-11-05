c2363c049bb6f25bf416f87b2aaf052b21a925f6
PG <anton@samokhvalov.xyz>
2021-11-05 22:28:14 +0300

    better

--- pkgs/lib/curses/netbsd/mix.sh
+++ pkgs/lib/curses/netbsd/mix.sh
@@ -1,5 +1,9 @@
 {% extends '//mix/template/make.sh' %}
 
+{% block sh_script_begin %}
+set -x
+{% endblock %}
+
 {% block fetch %}
 https://github.com/sabotage-linux/netbsd-curses/archive/refs/tags/v0.3.2.tar.gz
 8798fe3e6acce82cb08e39b1532b4664
@@ -18,9 +22,7 @@ STATIC_BINS=yes
 all-static
 {% endblock %}
 
-{% block install_target %}
-install-static
-{% endblock %}
+{% block install_target %}install-static{% endblock %}
 
 {% block env %}
 export COFLAGS="--with-curses=${out} --with-ncurses=${out} \${COFLAGS}"
