0d600fcf8b1a849c012a646b02ce5a7f54e7a326
PG <anton@samokhvalov.xyz>
2021-11-05 16:36:32 +0300

    better

--- pkgs/mix/template/py.py
+++ pkgs/mix/template/py.py
@@ -7,8 +7,9 @@ import os
 import subprocess
 
 DATA = r"""
-{% block script %}{% endblock %}
-"""
+{% block script %}
+{% endblock %}
+""".strip()
 
 def iter_lines():
     yield 'set -e'
@@ -22,11 +23,11 @@ def iter_lines():
 
     yield DATA
 
-if DATA.strip():
+if DATA:
     tmp = os.path.join(os.environ['tmp'], 'tmp')
     os.makedirs(tmp)
     os.environ['TMPDIR'] = tmp
-    subprocess.run([mix.which('dash')], input=('\n'.join(iter_lines())).encode(), check=True)
+    subprocess.run([mix.which('dash')], input=('\n'.join(iter_lines()) + '\n').encode(), check=True)
 {% endblock %}
 
 env_data = r"""
