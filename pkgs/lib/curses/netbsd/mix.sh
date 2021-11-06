df900d3af33c98458ddd6190fbb52d77f85df12a
PG <anton@samokhvalov.xyz>
2021-11-06 16:21:42 +0300

    better

--- pkgs/lib/curses/netbsd/mix.sh
+++ pkgs/lib/curses/netbsd/mix.sh
@@ -1,9 +1,5 @@
 {% extends '//mix/template/make.sh' %}
 
-{% block sh_script_begin %}
-set -x
-{% endblock %}
-
 {% block fetch %}
 https://github.com/sabotage-linux/netbsd-curses/archive/refs/tags/v0.3.2.tar.gz
 8798fe3e6acce82cb08e39b1532b4664
