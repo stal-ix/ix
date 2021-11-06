4200680c90a8f628937141f012b3abfa049815f4
PG <anton@samokhvalov.xyz>
2021-11-07 01:20:51 +0300

    rename

--- pkgs/boot/9/bison/3.4.1_0.sh
+++ pkgs/boot/9/bison/3.4.1_0.sh
@@ -1,11 +1,6 @@
 {% extends '//boot/9/bison/341/template.sh' %}
 
-{% block sh_script_begin %}
-set -x
-{% endblock %}
-
 {% block bison_patch %}
-sleep 1
 cp bb/parse-gram.* src/
 {% endblock %}
 
--- pkgs/boot/9/bison/3.4.1_2.sh
+++ pkgs/boot/9/bison/3.4.1_2.sh
@@ -1,5 +1,5 @@
-{% extends '//boot/9/bison/./3.4.1_1.sh' %}
+{% extends '//boot/9/bison/3.4.1_1.sh' %}
 
 {% block bison %}
-boot/9/bison/./3.4.1_1.sh
+boot/9/bison/3.4.1_1.sh
 {% endblock %}
