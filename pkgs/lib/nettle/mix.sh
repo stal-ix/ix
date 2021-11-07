9a4b659dc727247377fd890a09d2f271c97177a9
PG <anton@samokhvalov.xyz>
2021-11-07 04:46:27 +0300

    add nettle

---
+++ pkgs/lib/nettle/mix.sh
@@ -0,0 +1,21 @@
+{% extends '//mix/template/autohell.sh' %}
+
+{% block fetch %}
+https://ftp.gnu.org/gnu/nettle/nettle-3.7.3.tar.gz
+a60273d0fab9c808646fcf5e9edc2e8f
+{% endblock %}
+
+{% block lib_deps %}
+lib/gmp/mix.sh
+{% endblock %}
+
+{% block bld_deps %}
+dev/doc/texinfo/mix.sh
+dev/build/autoconf/2.69/mix.sh
+dev/build/automake/1.16.3/mix.sh
+env/autohell/mix.sh
+{% endblock %}
+
+{% block autoreconf %}
+autoreconf -i
+{% endblock %}
