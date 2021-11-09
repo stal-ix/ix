e20038a36c74d002f68943e55eb7ef20de62ef1d
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-11-09 19:56:49 +0300

    add protobuf

---
+++ pkgs/dev/tool/protobuf/mix.sh
@@ -0,0 +1,21 @@
+{% extends '//mix/template/autohell.sh' %}
+
+{% block fetch %}
+https://github.com/protocolbuffers/protobuf/archive/refs/tags/v3.19.1.tar.gz
+e5cb6707320a077f0618b669d1856bc4
+{% endblock %}
+
+{% block lib_deps %}
+lib/z/mix.sh
+lib/cxx/mix.sh
+{% endblock %}
+
+{% block bld_deps %}
+dev/build/autoconf/2.69/mix.sh
+dev/build/automake/1.16.3/mix.sh
+env/autohell/mix.sh
+{% endblock %}
+
+{% block autoreconf %}
+dash ./autogen.sh
+{% endblock %}
