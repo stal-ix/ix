1ab458facfa7ecda41c790cfb48c815f3ed8ded0
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-11-10 00:56:42 +0300

    add wget

---
+++ pkgs/net/wget/mix.sh
@@ -0,0 +1,14 @@
+{% extends '//mix/template/autohell.sh' %}
+
+{% block fetch %}
+https://ftp.gnu.org/gnu/wget/wget-1.21.2.tar.lz
+2703828579552a5a6749fadfb1bf305d
+{% endblock %}
+
+{% block bld_deps %}
+lib/intl/mix.sh
+lib/pcre2/mix.sh
+lib/gnutls/mix.sh
+lib/metalink/mix.sh
+env/autohell/mix.sh
+{% endblock %}
