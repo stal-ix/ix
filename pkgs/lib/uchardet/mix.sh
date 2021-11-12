0baac7dc7e47f0cd5d39c1ef2bb47bcb207ed335
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-11-13 00:09:29 +0300

    better

--- pkgs/lib/uchardet/mix.sh
+++ pkgs/lib/uchardet/mix.sh
@@ -5,8 +5,11 @@ https://github.com/freedesktop/uchardet/archive/refs/tags/v0.0.7.tar.gz
 f1ac05da2edfc17b12d1f942695f59b8
 {% endblock %}
 
-{% block bld_deps %}
+{% block lib_deps %}
 lib/c++/mix.sh
+{% endblock %}
+
+{% block bld_deps %}
 env/std/mix.sh
 boot/final/env/tools/mix.sh
 {% endblock %}
