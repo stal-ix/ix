07974454adc91a113d9920e2598f8e691bf0a00c
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-11-14 03:54:19 +0300

    build with dbm

--- pkgs/lib/python/3/11/mix.sh
+++ pkgs/lib/python/3/11/mix.sh
@@ -5,6 +5,11 @@ https://github.com/python/cpython/archive/refs/tags/v3.11.0a2.tar.gz
 f21d7c326ca389e43caf5664a768d745
 {% endblock %}
 
+{% block lib_deps %}
+lib/gdbm/mix.sh
+{{super()}}
+{% endblock %}
+
 {% block bld_deps %}
 dev/build/python/mix.sh
 {{super()}}
@@ -12,7 +17,7 @@ dev/build/python/mix.sh
 
 {% block setup %}
 {{super()}}
-export CPPFLAGS="-DCONFIG_64=1 -DSQLITE_OMIT_LOAD_EXTENSION=1 ${CPPFLAGS}"
+export CPPFLAGS="-DCONFIG_64=1 -DSQLITE_OMIT_LOAD_EXTENSION=1 -DHAVE_NDBM_H=1 ${CPPFLAGS}"
 {% endblock %}
 
 {% block ensure_static_build %}
@@ -28,7 +33,6 @@ cat Modules/Setup \
     | grep -v pyexpat     \
     | grep -v _decimal    \
     | grep -v xxsubtype   \
-    | grep -v _dbmmodule  \
     | grep -v ossaudiodev \
     > _
 
@@ -41,5 +45,5 @@ EOF
 
 {% block test %}
 {{super()}}
-${out}/bin/python3 -c 'import pyexpat'
+${out}/bin/python3 -c 'import pyexpat; import dbm;'
 {% endblock %}
