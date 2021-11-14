01d4e9bc41848caa3b3477edc76c3d4a78473f94
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-11-14 03:24:26 +0300

    fix build

--- pkgs/shell/nnn/mix.sh
+++ pkgs/shell/nnn/mix.sh
@@ -2,5 +2,12 @@
 
 {% block fetch %}
 https://github.com/jarun/nnn/archive/refs/tags/v4.3.tar.gz
-md5
+8263efb03dc28e02f32ddaa0e07ddaf1
+{% endblock %}
+
+{% block bld_deps %}
+lib/readline/mix.sh
+lib/curses/mix.sh
+dev/build/pkg-config/mix.sh
+{{super()}}
 {% endblock %}
