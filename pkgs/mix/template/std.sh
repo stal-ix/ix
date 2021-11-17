66c4101dcc1d4009dab1671caff5e481a2a53fb1
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-11-17 16:36:31 +0300

    refactor

--- pkgs/mix/template/std.sh
+++ pkgs/mix/template/std.sh
@@ -1,4 +1,17 @@
-{% extends 'template.sh' %}
+{% extends 'pkg.sh' %}
+
+{% block preconf %}
+{% include 'blocks/preconf.sh' %}
+{% endblock %}
+
+{% block prepatch %}
+{% block touch_yl %}
+find . | grep '\.[yl]$' | while read l; do
+    echo "TOUCH ${l}"
+    touch ${l}
+done
+{% endblock %}
+{% endblock %}
 
 {% block bld_deps %}
 {% block std_env %}
