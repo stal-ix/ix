a7d4add06dd4bb8e89a4f40898e8381b5aee095b
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-11-17 16:51:43 +0300

    better

--- pkgs/mix/template/std.sh
+++ pkgs/mix/template/std.sh
@@ -16,11 +16,6 @@
 {% include 'blocks/prepatch.sh'%}
 {% endblock %}
 
-{% block bld_deps %}
-{% block std_env %}
 {% block std_box %}
 env/std/mix.sh
-{% endblock %}
-{% endblock %}
-{{super()}}
 {% endblock %}
