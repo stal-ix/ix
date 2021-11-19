8392ff7ae12fcb5f71ecdb9bff541c3dd29c4339
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-11-19 18:21:31 +0300

    add librsvg

---
+++ pkgs/lib/rsvg/mix.sh
@@ -0,0 +1,10 @@
+{% extends '//mix/template/autohell.sh' %}
+
+{% block fetch %}
+https://download.gnome.org/sources/librsvg/2.40/librsvg-2.40.21.tar.xz
+66df60ef1909d6e24df8244042422a98
+{% endblock %}
+
+{% block lib_deps %}
+lib/glib/mix.sh
+{% endblock %}
