f5702309083cb1e7efae5ed0d044b2f8cceca482
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-11-19 16:49:16 +0300

    better

--- pkgs/lib/freetype/mix.sh
+++ pkgs/lib/freetype/mix.sh
@@ -4,3 +4,22 @@
 https://download.savannah.gnu.org/releases/freetype/freetype-2.11.0.tar.xz
 f931582653774e310ed3a7e49b7167a3
 {% endblock %}
+
+{% block lib_deps %}
+lib/z/mix.sh
+lib/bzip2/mix.sh
+lib/png/mix.sh
+lib/brotli/mix.sh
+{% endblock %}
+
+{% block bld_tool %}
+dev/build/autoconf/2.69/mix.sh
+dev/build/automake/1.16.3/mix.sh
+{% endblock %}
+
+{% block autoreconf %}
+cat autogen.sh | grep -v git | sed -e 's|\./configure|\./configure; exit 0|' | dash
+{% endblock %}
+
+{% block coflags %}
+{% endblock %}
