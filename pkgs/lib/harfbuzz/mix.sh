9470f5d10fa0017d70c2194777c4157143ddb56f
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-11-19 15:54:51 +0300

    better

--- pkgs/lib/harfbuzz/mix.sh
+++ pkgs/lib/harfbuzz/mix.sh
@@ -7,8 +7,17 @@ https://github.com/harfbuzz/harfbuzz/archive/refs/tags/3.1.1.tar.gz
 
 {% block lib_deps %}
 lib/c++/mix.sh
+lib/glib/mix.sh
+lib/freetype/mix.sh
+{% if mix.platform.target.os == 'darwin' %}
 lib/darwin/framework/CoreText/mix.sh
 lib/darwin/framework/ApplicationServices/mix.sh
+{% endif %}
+{% endblock %}
+
+{% block bld_tool %}
+dev/build/cmake/mix.sh
+dev/build/pkg-config/mix.sh
 {% endblock %}
 
 {% block setup %}
@@ -18,4 +27,7 @@ export CFLAGS="-w ${CFLAGS}"
 {% block meson_flags %}
 -Ddefault_library=static
 -Dcoretext=enabled
+-Dglib=enabled
+-Dgobject=disabled
+-Dtests=disabled
 {% endblock %}
--- pkgs/term/kitty/mix.sh
+++ pkgs/term/kitty/mix.sh
@@ -10,12 +10,16 @@ lib/python/mix.sh
 lib/lcms2/mix.sh
 lib/png/mix.sh
 lib/harfbuzz/mix.sh
+{% if mix.platform.target.os == 'darwin' %}
 lib/darwin/framework/IOKit/mix.sh
 lib/darwin/framework/Cocoa/mix.sh
 lib/darwin/framework/AppKit/mix.sh
+lib/darwin/framework/Carbon/mix.sh
 lib/darwin/framework/CoreText/mix.sh
+lib/darwin/framework/CoreVideo/mix.sh
 lib/darwin/framework/CoreGraphics/mix.sh
 lib/darwin/framework/UserNotifications/mix.sh
+{% endif %}
 {% endblock %}
 
 {% block bld_tool %}
@@ -26,6 +30,34 @@ dev/build/pkg-config/mix.sh
 export CFLAGS="-w ${CFLAGS}"
 {% endblock %}
 
+{% block patch %}
+cat - glfw/input.c << EOF > _
+#define encode_utf8 xxx_encode_utf8
+EOF
+
+mv _ glfw/input.c
+{% endblock %}
+
 {% block build %}
-python3 ./setup.py build
+python3 ./setup.py build || true
+
+cd build
+
+cat << EOF > qw.cpp
+extern "C" void* PyInit_fast_data_types();
+
+namespace {
+    struct Init {
+        inline Init() {
+            PyInit_fast_data_types();
+        }
+    }INIT;
+}
+EOF
+
+clang++ -c qw.cpp
+rm subseq_matcher-charsets.c.o glfw-cocoa-monotonic.c.o
+clang++ *.o ${lib_python_3_10}/lib/python3.10/config-3.10-darwin/python.o
+
+exit 1
 {% endblock %}
