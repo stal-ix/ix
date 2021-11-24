f9d0c543f7317d4e43d09b7f5805cc3ac562f4d9
Anton Samokhvalov <pg83@yandex.ru>
2021-11-24 20:20:28 +0300

    autoreconf

--- pkgs/lib/lcms2/mix.sh
+++ pkgs/lib/lcms2/mix.sh
@@ -4,3 +4,13 @@
 https://github.com/mm2/Little-CMS/archive/refs/tags/lcms2.12.tar.gz
 0359f600f305143c5457dd075d23bf07
 {% endblock %}
+
+{% block bld_tool %}
+dev/build/autoconf/2/69/mix.sh
+dev/build/automake/1.16.3/mix.sh
+{% endblock %}
+
+{% block autoreconf %}
+libtoolize -c
+autoreconf -i
+{% endblock %}
