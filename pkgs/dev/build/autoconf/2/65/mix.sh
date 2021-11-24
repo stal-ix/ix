32c0ece1b31d87b5ff12ab1fce78871bb1aa101b
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-11-24 17:52:21 +0300

    better

--- pkgs/dev/build/autoconf/2/65/mix.sh
+++
@@ -1,6 +0,0 @@
-{% extends '//dev/build/autoconf/2/71/mix.sh' %}
-
-{% block fetch %}
-https://ftp.gnu.org/gnu/autoconf/autoconf-2.65.tar.xz
-dfb23a46ecb73a0abade39c053a6c5a1
-{% endblock %}
--- pkgs/dev/build/automake/1.7.9/mix.sh
+++
@@ -1,6 +0,0 @@
-{% extends '//dev/build/automake/1.16.3/mix.sh' %}
-
-{% block fetch %}
-https://ftp.gnu.org/gnu/automake/automake-1.7.9.tar.bz2
-571fd0b0598eb2a27dcf68adcfddfacb
-{% endblock %}
