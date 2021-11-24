decddf18b19d92d2b7d7917c6fe58de07e3fc976
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-11-24 17:40:26 +0300

    better

--- pkgs/lib/ucl/mix.sh
+++ pkgs/lib/ucl/mix.sh
@@ -4,3 +4,13 @@
 http://www.oberhumer.com/opensource/ucl/download/ucl-1.03.tar.gz
 852bd691d8abc75b52053465846fba34
 {% endblock %}
+
+{% block bld_tool %}
+dev/build/autoconf/2.69/mix.sh
+dev/build/automake/1.16.3/mix.sh
+{% endblock %}
+
+{% block autoreconf %}
+#libtoolize -c
+#autoreconf -i
+{% endblock %}
