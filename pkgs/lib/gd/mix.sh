82ca0a79438c03d485fc90eb377fd6bd6f565289
Anton Samokhvalov <pg83@yandex.ru>
2021-11-29 22:29:32 +0300

    add gd

---
+++ pkgs/lib/gd/mix.sh
@@ -0,0 +1,26 @@
+{% extends '//mix/template/autohell.sh' %}
+
+{% block fetch %}
+https://github.com/libgd/libgd/archive/refs/tags/gd-2.3.3.tar.gz
+f75f9721b858f25acc5c7c917ab7ccba
+{% endblock %}
+
+{% block lib_deps %}
+lib/webp/mix.sh
+lib/jpeg/mix.sh
+lib/png/mix.sh
+lib/heif/mix.sh
+lib/tiff/mix.sh
+lib/iconv/mix.sh
+lib/freetype/mix.sh
+lib/fontconfig/mix.sh
+{% endblock %}
+
+{% block bld_tool %}
+dev/build/autoconf/2/69/mix.sh
+dev/build/automake/1.16.3/mix.sh
+{% endblock %}
+
+{% block autoreconf %}
+dash bootstrap.sh
+{% endblock %}
---
+++ pkgs/lib/tiff/mix.sh
@@ -0,0 +1,15 @@
+{% extends '//mix/template/cmake.sh' %}
+
+{% block fetch %}
+http://download.osgeo.org/libtiff/tiff-4.3.0.tar.gz
+0a2e4744d1426a8fc8211c0cdbc3a1b3
+{% endblock %}
+
+{% block lib_deps %}
+lib/z/mix.sh
+lib/jpeg/mix.sh
+lib/xz/mix.sh
+lib/zstd/mix.sh
+lib/webp/mix.sh
+lib/c++/mix.sh
+{% endblock %}
