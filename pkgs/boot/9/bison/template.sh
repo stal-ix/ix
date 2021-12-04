71257a6cece83dedfd562666fbcae010d2d09caa
Anton Samokhvalov <pg83@yandex.ru>
2021-12-04 04:49:23 +0300

    better

--- pkgs/boot/9/bison/template.sh
+++ pkgs/boot/9/bison/template.sh
@@ -1,17 +1,18 @@
 {% extends '//dev/lang/bison/template.sh' %}
 
-{% block lib_deps %}
-boot/9/m4/mix.sh
-{% endblock %}
-
 {% block bld_libs %}
 {% endblock %}
 
 {% block bld_deps %}
+boot/9/m4/mix.sh
 boot/9/flex/2.6.4.1.sh
 boot/8/env/std/mix.sh
 {% block bison %}
 {% endblock %}
+{% endblock %}
+
+{% block run_deps %}
+boot/9/m4/mix.sh
 {% endblock %}
 
 {% block toolconf %}
