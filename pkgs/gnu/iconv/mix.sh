fafa19f164dac6698288f232dd3e33d90a47b476
Anton Samokhvalov <pg83@yandex.ru>
2021-12-04 08:05:46 +0300

    better

--- pkgs/gnu/iconv/mix.sh
+++
@@ -1,9 +0,0 @@
-{% extends '//mix/template/proxy.sh' %}
-
-{% block bld_libs %}
-lib/iconv/mix.sh
-{% endblock %}
-
-{% block install %}
-cd ${out} && cp -R $(dirname $(command -v iconv)) ./
-{% endblock %}
--- pkgs/net/curl/mix.sh
+++
@@ -1,9 +0,0 @@
-{% extends '//mix/template/proxy.sh' %}
-
-{% block bld_libs %}
-lib/curl/mix.sh
-{% endblock %}
-
-{% block install %}
-cd ${out} && cp -R $(dirname $(command -v curl)) ./
-{% endblock %}
