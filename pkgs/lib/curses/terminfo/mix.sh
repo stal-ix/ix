c0d0c6adc0d9ec02944811022a9d7fe775b0e709
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-12-08 02:51:02 +0300

    unused now

--- pkgs/lib/curses/terminfo/mix.sh
+++
@@ -1,9 +0,0 @@
-{% extends '//lib/curses/n/mix.sh' %}
-
-{% block env %}
-export TERMINFO="${out}/share/terminfo"
-{% endblock %}
-
-{% block postinstall %}
-cd ${out} && rm -rf lib bin include
-{% endblock %}
