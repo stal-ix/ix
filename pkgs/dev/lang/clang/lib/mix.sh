387f6c1191f35cc594753107f79670c48db0e35a
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-12-15 00:24:08 +0300

    better

--- pkgs/dev/lang/clang/lib/mix.sh
+++ pkgs/dev/lang/clang/lib/mix.sh
@@ -1,48 +1,13 @@
 {% extends '//mix/template/py.py' %}
 
 {% block lib_deps %}
-env/toolchain/ar/llvm/mix.sh
-env/toolchain/ld/lld/mix.sh
+dev/lang/clang/lib/cc/mix.sh
+dev/lang/clang/lib/ar/mix.sh
+dev/lang/clang/lib/ld/mix.sh
 lib/build/mix.sh
 {% endblock %}
 
 {% block env %}
-setup_compiler_env() {
-    export CC=clang
-    export CXX=clang++
-}
-
-setup_compiler() {
-setup_compiler_env
-
-B="--target={{target.arch}}-{{target.vendor}}-{{target.os}} -fcolor-diagnostics -Wno-unused-command-line-argument -nostdinc -nostdinc++ ${OPTFLAGS} ${CPPFLAGS} ${CFLAGS}"
-E="-nostdlib++ ${LDFLAGS} ${OPTFLAGS}"
-
-cat << EOF > clang
-#!$(which dash)
-exec "$(which clang)" ${B} ${CONLYFLAGS} "\$@" ${E}
-EOF
-
-cat << EOF > clang++
-#!$(which dash)
-exec "$(which clang++)" ${B} -Wno-stdlibcxx-not-found ${CXXFLAGS} "\$@" ${E}
-EOF
-
-cat << EOF > cpp
-#!$(which dash)
-exec "$(which clang-cpp)" ${B} "\$@" ${OPTFLAGS}
-EOF
-
-chmod +x clang clang++ cpp
-
-ln -s clang gcc
-ln -s clang c99
-ln -s clang cc
-
-ln -s clang++ g++
-ln -s clang++ c++
-}
-
 setup_toolchain_env() {
     setup_compiler_env
     setup_ar_env
