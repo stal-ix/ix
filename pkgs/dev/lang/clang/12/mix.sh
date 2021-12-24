dac361bf9013aca7be1dcce1cf3cc23692ff5cb4
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-12-25 02:23:38 +0300

    add 12 clang

---
+++ pkgs/dev/lang/clang/12/mix.sh
@@ -0,0 +1,18 @@
+{% extends '//dev/lang/clang/t/mix.sh' %}
+
+{% block fetch %}
+https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-12.0.1.tar.gz
+b23c6287824a30f5a58315f402b1ead3
+{% endblock %}
+
+{% block env %}
+{{super()}}
+export CLANG_VERSION=12.0.1
+{% endblock %}
+
+{% block install %}
+{{super()}}
+mkdir ${out}/share
+mv ${out}/lib/clang/12*/include ${out}/share/
+rm -rf ${out}/libexec
+{% endblock %}
