333a8116577dd30103327e8ccc6b217f7cf32d7e
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-12-28 23:47:51 +0300

    build btop

---
+++ pkgs/lib/range/v3/mix.sh
@@ -0,0 +1,22 @@
+{% extends '//mix/template/cmake.sh' %}
+
+{% block fetch %}
+https://github.com/ericniebler/range-v3/archive/9aa032ccd0eb4bd77f58e5b181168f1038c222c6.zip
+c40b835c3b7dd751f1c4c3bcf5b99c92
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+lib/c++
+{% endblock %}
+
+{% block cmake_flags %}
+RANGE_V3_DOCS=OFF
+RANGE_V3_TESTS=OFF
+RANGE_V3_EXAMPLES=OFF
+RANGE_V3_PERF=OFF
+{% endblock %}
+
+{% block setup %}
+export CPPFLAGS="-w ${CPPFLAGS}"
+{% endblock %}
---
+++ pkgs/sys/btop++/mix.sh
@@ -0,0 +1,46 @@
+{% extends '//mix/template/make.sh' %}
+
+{% block fetch %}
+https://github.com/aristocratos/btop/archive/refs/tags/v1.1.3.tar.gz
+36de5aa034c4d10941dbbce0ccf85cca
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/c++
+lib/range/v3
+{% endblock %}
+
+{% block make_flags %}
+CXX=clang++
+OPTFLAGS=
+{% endblock %}
+
+{% block setup %}
+export CPPFLAGS="-I${PWD} -isystem${PWD} ${CPPFLAGS}"
+{% endblock %}
+
+{% block patch %}
+cat << EOF > ranges.hh
+#include <range/v3/all.hpp>
+
+namespace std::ranges {
+    using namespace ::ranges;
+}
+
+namespace std::views {
+    using namespace ::ranges::views;
+}
+EOF
+
+find . | while read l; do
+    if test -f ${l}; then
+        sed -e 's|<ranges>|<ranges.hh>|g' \
+            -e 's|std::setlocale|setlocale|g' \
+            -i ${l}
+    fi
+done
+
+sed -e 's|.*current_procs.erase.*||' \
+    -i src/linux/btop_collect.cpp
+{% endblock %}
