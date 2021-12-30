4e8abdc3e6ad4208a4a8ff2d8c227a691e408794
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-12-30 21:50:05 +0300

    add less

---
+++ pkgs/tool/less/mix.sh
@@ -0,0 +1,11 @@
+{% extends '//mix/template/autohell.sh' %}
+
+{% block fetch %}
+https://www.greenwoodsoftware.com/less/less-598.tar.gz
+fcd2c9baa3c8f4ece2807c2c8bae5241
+{% endblock %}
+
+{% block bld_libs %}
+lib/c/mix.sh
+lib/curses/mix.sh
+{% endblock %}
