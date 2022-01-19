97f7989cc6606b4434a22ad523873a15379b0e3e
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-01-19 22:31:49 +0300

    more tools

---
+++ pkgs/bin/net/tools/mix.sh
@@ -0,0 +1,24 @@
+{% extends '//mix/template/make.sh' %}
+
+{% block fetch %}
+https://downloads.sourceforge.net/project/net-tools/net-tools-2.10.tar.xz
+sha:b262435a5241e89bfa51c3cabd5133753952f7a7b7b93f32e08cb9d96f580d69
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/linux
+{% endblock %}
+
+{% block bld_tool %}
+bld/bash
+{% endblock %}
+
+{% block configure %}
+yes "" | make config
+{% endblock %}
+
+{% block make_flags %}
+DESTDIR=${out}
+SBINDIR=/bin
+{% endblock %}
---
+++ pkgs/bin/wireless/tools/mix.sh
@@ -0,0 +1,16 @@
+{% extends '//mix/template/make.sh' %}
+
+{% block fetch %}
+https://hewlettpackard.github.io/wireless-tools/wireless_tools.29.tar.gz
+sha:6fb80935fe208538131ce2c4178221bab1078a1656306bce8909c19887e2e5a1
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/linux
+{% endblock %}
+
+{% block make_flags %}
+BUILD_STATIC=y
+INSTALL_DIR=${out}/bin
+{% endblock %}
