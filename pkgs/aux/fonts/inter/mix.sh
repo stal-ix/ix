5af4c1f4ade4f13565d777166162e596802e2fa2
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-02-07 00:51:01 +0000

    better

---
+++ pkgs/aux/fonts/inter/mix.sh
@@ -0,0 +1,6 @@
+{% extends '//mix/template/fonts.sh' %}
+
+{% block fetch %}
+https://github.com/rsms/inter/releases/download/v3.19/Inter-3.19.zip
+sha:150ab6230d1762a57bebf35dfc04d606ff91598a31d785f7f100356ecdcc0032
+{% endblock %}
--- pkgs/mix/template/fonts.sh
+++ pkgs/mix/template/fonts.sh
@@ -4,10 +4,10 @@
 mkdir -p ${out}/share/fonts
 
 find . -type f -name '*.ttf' | while read l; do
-    cp ${l} ${out}/share/fonts/
+    cp "${l}" ${out}/share/fonts/
 done
 
 find . -type f -name '*.otf' | while read l; do
-    cp ${l} ${out}/share/fonts/
+    cp "${l}" ${out}/share/fonts/
 done
 {% endblock %}
