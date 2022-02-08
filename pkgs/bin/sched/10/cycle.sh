33d955643299fdae574be1044b76b60a4bd031cc
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-02-08 04:04:33 +0000

    better

--- pkgs/bin/sched/10/cycle.sh
+++ pkgs/bin/sched/10/cycle.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-sleep 10
+while true; do
+    sleep 10
 
-find /etc/sche.d/10 -type l -name '*.sh'| while read l; do
-    echo "run ${l}"
-    sh "${l}" || true
-    sleep 1
+    find /etc/sche.d/10 -type l -name '*.sh'| while read l; do
+        echo "run ${l}"
+        sh "${l}" || true
+        sleep 1
+    done
 done
