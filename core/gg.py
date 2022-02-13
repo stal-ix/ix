cfa7c308e874697cc7cbd96382b98e1e8959222c
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-02-13 20:28:56 +0000

    better

--- core/gg.py
+++ core/gg.py
@@ -5,7 +5,7 @@ import subprocess
 import core.execute as ce
 
 
-def rec_node_1(n):
+def rec_node(n):
     return itertools.chain([n], n.iter_all_build_depends(), n.iter_all_runtime_depends())
 
 
@@ -18,27 +18,28 @@ def rec_nodes(n):
 
             yield x
 
-            for y in rec_node_1(x):
+            for y in rec_node(x):
                 yield from visit(y)
 
     for x in n:
         yield from visit(x)
 
 
-def build_graph(n):
-    def it():
-        for x in rec_nodes(n):
-            if x.buildable():
-                yield from x.iter_build_commands()
+def build_commands(nodes):
+    for x in rec_nodes(nodes):
+        if x.buildable():
+            yield from x.iter_build_commands()
 
+
+def build_graph(n):
     return {
-        'nodes': list(it()),
+        'nodes': list(build_commands(n)),
         'targets': [(x.out_dir + '/touch') for x in n],
     }
 
 
 def execute_graph(graph):
-    stdin = json.dumps(graph).encode('utf-8')
+    stdin = json.dumps(graph).encode()
     cmd = ['/bin/doas', 'mix', '-T', '/bin/mix', 'execute']
 
     try:
