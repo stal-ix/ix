46f72a78edd284851b42e6c0cdceda24fef31e9e
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-02-13 19:13:07 +0000

    refactor

---
+++ pkgs/mix/realm.py
@@ -0,0 +1,63 @@
+import os
+import json
+import base64
+
+
+meta = json.loads(base64.b64decode("{{meta}}").decode())
+path = os.environ['out']
+
+
+os.makedirs(path)
+os.chdir(path)
+
+
+def iter_dir_1(w):
+    for a, b, c in os.walk(w):
+        for x in c:
+            yield os.path.join(a, x)
+
+        for x in b:
+            dl = os.path.join(a, x)
+
+            if os.path.islink(dl):
+                yield dl
+
+
+def iter_dir(w):
+    for x in iter_dir_1(w):
+        yield x[len(w) + 1:]
+
+
+def install(fr, to):
+    for x in iter_dir(fr):
+        if '/' not in x:
+            continue
+
+        # print(f'{fr} {x} {to}')
+        p = os.path.join(to, x)
+
+        try:
+            os.makedirs(os.path.dirname(p))
+        except Exception:
+            pass
+
+        try:
+            os.unlink(p)
+        except FileNotFoundError:
+            pass
+
+        pf = os.path.join(fr, x)
+        # print(f'symlink {pf} -> {p}')
+        os.symlink(pf, p)
+
+
+for p in reversed(meta['links']):
+    install(p, path)
+
+
+with open('meta.json', 'w') as f:
+    f.write(json.dumps(meta, indent=4, sort_keys=True))
+
+
+with open('touch', 'w') as f:
+    pass
