59ee922eccc7cc1da202a1ba625c6e55dd92b4dc
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-02-14 02:41:29 +0000

    better

--- pkgs/bin/dbus/runit/mix.sh
+++ pkgs/bin/dbus/runit/mix.sh
@@ -5,11 +5,7 @@ bin/dbus/sys
 {% endblock %}
 
 {% block install %}
-{{super()}}
-
-cd ${out}
-
-mkdir -p etc/services/dbus; cd etc/services/dbus
+cd ${out}; mkdir -p etc/services/dbus; cd etc/services/dbus
 
 base64 -d << EOF > system.conf
 {% include 'system.conf/base64' %}
@@ -17,7 +13,7 @@ EOF
 
 cat << EOF > run
 #!/bin/sh
-exec srv dbus dbus-daemon --config-file=/etc/services/dbus/system.conf --nopidfile --syslog --nofork
+exec srv dbus dbus-daemon --config-file=${PWD}/system.conf --nopidfile --syslog --nofork
 EOF
 
 chmod +x run
