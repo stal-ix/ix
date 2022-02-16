52c62c3f5806702d9212f56aaf212c50e371bdc2
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-02-16 10:36:07 +0000

    upver

---
+++ pkgs/bin/kernel/17/mix.sh
@@ -0,0 +1,23 @@
+{% extends '//bin/kernel/t/1/mix.sh' %}
+
+{% block kernel_name %}
+5.17-rc4
+{% endblock %}
+
+{% block fetch %}
+{% include 'ver.sh' %}
+{% endblock %}
+
+{% block host_libs %}
+{{super()}}
+bin/kernel/17/headers
+{% endblock %}
+
+{% block kconfig_flags %}
+{% include 'cfg' %}
+{{super()}}
+{% endblock %}
+
+{% block build %}
+exit 1
+{% endblock %}
---
+++ pkgs/bin/kernel/17/ver.sh
@@ -0,0 +1,2 @@
+https://git.kernel.org/torvalds/t/linux-5.17-rc4.tar.gz
+sha:dbca1579759608c91e5ee2c9634bf523e9d1e344a925d5d861049e0e10b1887d
