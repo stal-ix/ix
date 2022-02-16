c5d33a065593bdbf98124a28681e751404aa46bc
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-02-16 06:08:30 +0000

    moar

---
+++ pkgs/bin/basuctl/mix.sh
@@ -0,0 +1,1 @@
+{% extends '//lib/basu/mix.sh' %}
--- pkgs/lib/icu/mix.sh
+++ pkgs/lib/icu/mix.sh
@@ -1,13 +1,4 @@
-{% extends '//lib/icu/t/mix.sh' %}
-
-{% block run_data %}
-lib/icu/data
-{% endblock %}
-
-{% block cpp_defines %}
-{{super()}}
-ICU_DATA_DIR=\\\"${ICU_DATA}\\\"
-{% endblock %}
+{% extends '//lib/icu/t/lib/mix.sh' %}
 
 {% block install %}
 {{super()}}
