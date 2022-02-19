19f73899ab2f3ea614da2f0377df26ef655a1405
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-02-19 17:33:34 +0000

    add

---
+++ pkgs/lib/curses/pd/mix.sh
@@ -0,0 +1,28 @@
+{% extends '//mix/make.sh' %}
+
+{% block fetch %}
+https://github.com/wmcbrine/PDCurses/archive/2fa0f10dd844da47ee83c05a40a1ec541ceb95e1.zip
+sha:1c68c676b6a684898dd5303291074a8496ecd2039c1b9f20ecc42fdb5a9d189e
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+lib/sdl/2
+{% endblock %}
+
+{% block unpack %}
+{{super()}}
+cd sdl2
+{% endblock %}
+
+{% block install %}
+mkdir -p ${out}/lib ${out}/include/xcurses
+cp pdcurses.a ${out}/lib/libpdcurses.a
+cp ../*.h ${out}/include/xcurses/
+cd ${out}/lib
+ln -s libpdcurses.a libcurses.a
+{% endblock %}
+
+{% block env_lib %}
+export CPPFLAGS="-I${out}/include/xcurses \${CPPFLAGS}"
+{% endblock %}
