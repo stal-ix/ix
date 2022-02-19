3e11e8f9f21997916045cb77d8c9811492cfc9b6
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-02-19 12:29:30 +0000

    fix build

--- pkgs/lib/googletest/mix.sh
+++ pkgs/lib/googletest/mix.sh
@@ -5,6 +5,10 @@ https://github.com/google/googletest/archive/refs/tags/release-1.11.0.tar.gz
 sha:b4870bf121ff7795ba20d20bcdd8627b8e088f2d1dab299a031c1034eddc93d5
 {% endblock %}
 
+{% block build_flags %}
+shut_up
+{% endblock %}
+
 {% block lib_deps %}
 lib/c
 lib/c++
