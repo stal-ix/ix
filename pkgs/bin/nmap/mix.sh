f495f8b7008c1f23c000977e86b5465f8f866d95
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-02-22 21:56:58 +0000

    add nmap

---
+++ pkgs/bin/nmap/mix.sh
@@ -0,0 +1,15 @@
+{% extends '//mix/autohell.sh' %}
+
+{% block fetch %}
+https://nmap.org/dist/nmap-7.92.tar.bz2
+sha:a5479f2f8a6b0b2516767d2f7189c386c1dc858d997167d7ec5cfc798c7571a1
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/z
+lib/pcre
+lib/linux
+lib/ssh/2
+lib/openssl
+{% endblock %}
