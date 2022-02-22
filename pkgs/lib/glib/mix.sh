a55f23dd35a04055f55c8e58dc66b9a1558021d1
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-02-22 19:53:25 +0000

    use magic

--- pkgs/lib/glib/mix.sh
+++ pkgs/lib/glib/mix.sh
@@ -1,7 +1,16 @@
 {% extends '//lib/glib/pure/mix.sh' %}
 
+{% block lib_deps %}
+lib/magic/iface
+{{super()}}
+{% endblock %}
+
 {% block patch %}
 patch -p1 << EOF
 {% include '00.diff' %}
+EOF
+
+patch -p1 << EOF
+{% include '01.diff' %}
 EOF
 {% endblock %}
