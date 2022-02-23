768f76a6fbf958fead90d50d023e7bf2629ff199
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-02-23 14:13:02 +0000

    addrsync

---
+++ pkgs/bin/rsync/mix.sh
@@ -0,0 +1,15 @@
+{% extends '//mix/autorehell.sh' %}
+
+{% block fetch %}
+https://download.samba.org/pub/rsync/src-previews/rsync-3.2.4pre3.tar.gz
+sha:40083afd5514501215a8cd166f5f6adf92af76ff82f4cce01f60a192d7648952
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/z
+lib/lz4
+lib/zstd
+lib/xxhash
+lib/openssl
+{% endblock %}
