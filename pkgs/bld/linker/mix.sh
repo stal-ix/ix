56943031c27062fc6968bff09c2ec8db5d2fca70
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-03-01 15:41:55 +0000

    better

--- pkgs/bld/linker/mix.sh
+++ pkgs/bld/linker/mix.sh
@@ -1,9 +1,7 @@
 {% extends '//mix/hub.sh' %}
 
 {% block run_deps %}
-bin/mold(std_env=boot/8/env/cxx,opt=fast,openssl=md)
-{% endblock %}
-
-{% block ind_deps %}
-bld/linker/lib
+{% if linux %}
+bld/mold
+{% endif %}
 {% endblock %}
---
+++ pkgs/bld/mold/mix.sh
@@ -0,0 +1,9 @@
+{% extends '//mix/hub.sh' %}
+
+{% block run_deps %}
+bin/mold(std_env=boot/8/env/cxx,opt=fast,openssl=md)
+{% endblock %}
+
+{% block ind_deps %}
+bld/mold/lib
+{% endblock %}
