22df37b9321f9da54d674cf14acb40ec28913d3c
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-03-02 01:29:08 +0000

    better

--- pkgs/aux/iso-codes/mix.sh
+++ pkgs/aux/iso-codes/mix.sh
@@ -1,8 +1,10 @@
 {% extends '//mix/autorehell.sh' %}
 
 {% block fetch %}
-https://salsa.debian.org/iso-codes-team/iso-codes/-/archive/v4.9.0/iso-codes-v4.9.0.tar.bz2
-sha:d60541cf1b3acceeef625a4d05f673aff5f22fbe28186e61911357b245c66411
+#https://salsa.debian.org/iso-codes-team/iso-codes/-/archive/v4.9.0/iso-codes-v4.9.0.tar.bz2
+#sha:d60541cf1b3acceeef625a4d05f673aff5f22fbe28186e61911357b245c66411
+http://archive.ubuntu.com/ubuntu/pool/main/i/iso-codes/iso-codes_4.9.0.orig.tar.xz
+sha:d090112a529dbe37a0a1b46b8246cc1cfca92e0281ba27239426a2b693e28967
 {% endblock %}
 
 {% block bld_tool %}
