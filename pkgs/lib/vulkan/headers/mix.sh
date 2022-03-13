4fe9b7f1642d5ce87dbbc32dcd0a6898d980ee20
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-03-13 19:20:17 +0000

    downver

--- pkgs/lib/vulkan/headers/mix.sh
+++ pkgs/lib/vulkan/headers/mix.sh
@@ -1,8 +1,10 @@
 {% extends '//mix/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/KhronosGroup/Vulkan-Headers/archive/refs/tags/v1.3.207.tar.gz
-sha:e1fd80fa34e24767ce06690c8f6ba39545dd0c318b11b931bd4faf3fbcc34dd1
+#https://github.com/KhronosGroup/Vulkan-Headers/archive/refs/tags/v1.3.207.tar.gz
+#sha:e1fd80fa34e24767ce06690c8f6ba39545dd0c318b11b931bd4faf3fbcc34dd1
+https://github.com/KhronosGroup/Vulkan-Headers/archive/refs/tags/v1.2.201.tar.gz
+sha:
 {% endblock %}
 
 {% block lib_deps %}
