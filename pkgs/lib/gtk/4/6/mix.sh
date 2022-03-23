6ab6d3e1a9afaf39d15780626ea8eb6ad18e8da9
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-03-23 19:32:01 +0000

    upver

--- pkgs/lib/gtk/4/6/mix.sh
+++ pkgs/lib/gtk/4/6/mix.sh
@@ -2,7 +2,7 @@
 
 {% block fetch %}
 https://gitlab.gnome.org/GNOME/gtk/-/archive/4.6.2/gtk-4.6.2.tar.bz2
-sha:
+sha:fa2af70198687a71386aa21d29676e8c63453799f4a9c8f8d4c750cf496bca61
 {% endblock %}
 
 {% block bld_tool %}
