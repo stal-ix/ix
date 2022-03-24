9cf97c1aefd583e44e3f79c8b6e235d7ab7447d5
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-03-24 21:51:49 +0000

    better

--- pkgs/lib/webkit/orig/mix.sh
+++ pkgs/lib/webkit/orig/mix.sh
@@ -7,10 +7,6 @@ cd ${out}/lib
 
 rm -rf lib_webkit webkit2gtk*
 
-#find ${tmp}/obj -type f | grep '\.a' | while read l; do
-#    cp ${l} ./
-#done
-
 for x in *.so; do
     cp ${x} $(echo ${x} | sed -e 's|.so|.a|')
 done
