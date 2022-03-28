3b731f9fe0b03e5b62df937624bb79f401d25b71
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-03-28 00:31:19 +0000

    better

--- pkgs/lib/vulkan/loader/data/mix.sh
+++ pkgs/lib/vulkan/loader/data/mix.sh
@@ -5,9 +5,18 @@
 
 cd ${out}; mkdir -p etc/vulkan/icd.d; cd etc/vulkan/icd.d
 
-cat << EOF > radeon_icd.x86_64.json
-{% include 'radeon_icd.x86_64.json' %}
+# all mesa drivers go there, need better solution
+for x in broadcom freedreno intel lvp panfrost radeon virtio; do
+    cat << EOF > ${x}_icd.{{target.arch}}.json
+{
+    "ICD": {
+        "api_version": "1.2.195",
+        "library_path": "libvulkan_${x}.so"
+    },
+    "file_format_version": "1.0.0"
+}
 EOF
+done
 {% endblock %}
 
 {% block env %}
