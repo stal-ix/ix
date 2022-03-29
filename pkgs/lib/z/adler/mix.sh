d24c0ff68b3fe87e58eed23892b3b8ab199e5e93
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-03-29 00:03:56 +0000

    better

--- pkgs/lib/z/adler/mix.sh
+++ pkgs/lib/z/adler/mix.sh
@@ -5,10 +5,6 @@ http://zlib.net/zlib-1.2.12.tar.xz
 sha:7db46b8d7726232a621befaab4a1c870f00a90805511c0e0090441dac57def18
 {% endblock %}
 
-{% block std_box %}
-bld/bootbox
-{% endblock %}
-
 {% block lib_deps %}
 lib/c
 {% endblock %}
