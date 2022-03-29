880ebbc3a1c37c76a1915b0dba9e0c2feeb781f0
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-03-29 05:44:31 +0000

    refac

--- pkgs/bin/quake/2/mix.sh
+++ pkgs/bin/quake/2/mix.sh
@@ -35,26 +35,18 @@ bld/scripts/librarian
 {% block build %}
 {{super()}}
 
-cp release/baseq2/game.so game.a
-patchns game.a game
-
-cp release/ref_gl1.so gl1.a
-patchns gl1.a gl1
-
-cp release/ref_gl3.so gl3.a
-patchns gl3.a gl3
-
-cp release/ref_soft.so soft.a
-patchns soft.a soft
+find . -type f -name '*.so' | while read l; do
+    patchns ${l} $(basename ${l} | sed -e 's|\..*||')_
+done
 
 dl_stubs_3 << EOF >> stubs.cpp
-game     GetGameAPI gameGetGameAPI
-ref_gl1  GetRefAPI  gl1GetRefAPI
-ref_gl3  GetRefAPI  gl3GetRefAPI
-ref_soft GetRefAPI  softGetRefAPI
+game     GetGameAPI game_GetGameAPI
+ref_gl1  GetRefAPI  ref_gl1_GetRefAPI
+ref_gl3  GetRefAPI  ref_gl3_GetRefAPI
+ref_soft GetRefAPI  ref_soft_GetRefAPI
 EOF
 
-cc -o quake2 stubs.cpp $(find build/client -type f -name '*.o') *.a
+cc -o quake2 stubs.cpp $(find build/client -type f -name '*.o') $(find . -type f -name '*.so')
 {% endblock %}
 
 {% block patch %}
