d9f7e8b765cf5ba0bb4c6acd9a3e8b6d97b98d5c
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-03-30 18:24:08 +0000

    better

---
+++ pkgs/bin/star/mix.sh
@@ -0,0 +1,15 @@
+{% extends '//mix/make.sh' %}
+
+{% block fetch %}
+https://sourceforge.net/projects/s-tar/files/star-1.6.tar.bz2
+sha:1b18662ebae6c3509020953d4464e654c7ded4752774de987d8dc307dc43f776
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+{% endblock %}
+
+{% block make_flags %}
+INS_BASE=${out}
+LINKMODE=static
+{% endblock %}
