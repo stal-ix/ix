3ed956537608a1a99f172b1d67b2db2a28d02879
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-03-30 18:42:27 +0000

    better

---
+++ pkgs/bin/m4/quasar/mix.sh
@@ -0,0 +1,10 @@
+{% extends '//mix/make.sh' %}
+
+{% block fetch %}
+http://haddonthethird.net/m4/m4-2.tar.bz2
+sha:e4315fef49b08912b1d1db3774dd98f971397b2751c648512b6c8d852590dc50
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+{% endblock %}
