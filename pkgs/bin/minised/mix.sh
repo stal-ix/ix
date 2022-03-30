aff8a6de866198f6371254249b0a50ec4c8d3955
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-03-30 17:54:40 +0000

    add

---
+++ pkgs/bin/minised/mix.sh
@@ -0,0 +1,10 @@
+{% extends '//mix/make.sh' %}
+
+{% block fetch %}
+http://dl.exactcode.de/oss/minised/minised-1.16.tar.gz
+sha:46e072d5d45c9fd3d5b268523501bbea0ad016232b2d3f366a7aad0b1e7b3f71
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+{% endblock %}
---
+++ pkgs/bin/slibtool/mix.sh
@@ -0,0 +1,18 @@
+{% extends '//mix/autohell.sh' %}
+
+{% block fetch %}
+https://git.midipix.org/slibtool/snapshot/slibtool-0.5.34.tar.bz2
+sha:2b6f9a965b266c0dfbd6bb70915ce869fa8859d9470b65ebdfe847c8c6dd9507
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+{% endblock %}
+
+{% block bld_tool %}
+bin/m4
+{% endblock %}
+
+{% block configure %}
+sh ./configure --prefix=${out}
+{% endblock %}
