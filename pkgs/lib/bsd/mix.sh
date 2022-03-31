01b0eaf40d48d79d1970271873e436d15b7e1299
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-03-31 16:36:48 +0000

    upver

--- pkgs/lib/bsd/mix.sh
+++ pkgs/lib/bsd/mix.sh
@@ -1,8 +1,8 @@
 {% extends '//mix/autorehell.sh' %}
 
 {% block fetch %}
-https://libbsd.freedesktop.org/releases/libbsd-0.11.5.tar.xz
-sha:1a9c952525635c1bb6770cb22e969b938d8e6a9d7912362b98ee8370599b0efd
+https://libbsd.freedesktop.org/releases/libbsd-0.11.6.tar.xz
+sha:19b38f3172eaf693e6e1c68714636190c7e48851e45224d720b3b5bc0499b5df
 {% endblock %}
 
 {% block lib_deps %}
