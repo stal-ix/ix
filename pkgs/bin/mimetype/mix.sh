41a8242cd6977eadac06010ba481791020bc084c
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-03-31 23:18:47 +0000

    refac

--- pkgs/bin/mimetype/mix.sh
+++ pkgs/bin/mimetype/mix.sh
@@ -11,34 +11,9 @@ lib/mimetype
 {% endblock %}
 
 {% block build %}
-cat << EOF > main.cpp
-#include <string>
-#include <fstream>
-#include <sstream>
-#include <iostream>
-
-extern "C" const char* magic_mime_type_ex(const char* path, const void* data, size_t len);
-
-static inline std::string slurp(const char* path) {
-    std::ostringstream buf;
-    std::ifstream input(path);
-    buf << input.rdbuf();
-    return buf.str();
-}
-
-int main(int argc, char** argv) {
-    if (argc < 2) {
-        return 1;
-    }
-
-    auto p = argv[1];
-    auto s = slurp(p);
-
-    std::cout << magic_mime_type_ex(p, s.c_str(), std::min<size_t>(s.length(), 1024)) << std::endl;
-}
+cc -o mimetype -x c++ - << EOF
+{% include 'mt.cpp' %}
 EOF
-
-cc -o mimetype main.cpp
 {% endblock %}
 
 {% block install %}
