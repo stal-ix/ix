a972239911cb4e8e3c4cee66905e7d056fb95e73
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-04-03 01:28:50 +0000

    proper python tool

---
+++ pkgs/bin/python/lite/mix.sh
@@ -0,0 +1,33 @@
+{% extends '//lib/python/3/10/mix.sh' %}
+
+{% block lib_deps %}
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/z
+lib/ffi
+{% if darwin %}
+lib/darwin/framework/SystemConfiguration
+{% endif %}
+{{super()}}
+{% endblock %}
+
+{% block bld_tool %}
+bld/autohell/full
+{{super()}}
+{% endblock %}
+
+{% block extra_modules %}
+_ctypes _ctypes/_ctypes.c _ctypes/callbacks.c _ctypes/callproc.c _ctypes/stgdict.c _ctypes/cfield.c _ctypes/malloc_closure.c
+{% endblock %}
+
+{% block extra_tests %}
+${out}/bin/python3 -c 'import ctypes'
+{% endblock %}
+
+{% block setup %}
+{% endblock %}
+
+{% block env %}
+{% endblock %}
--- pkgs/bld/python/mix.sh
+++ pkgs/bld/python/mix.sh
@@ -1,33 +1,5 @@
-{% extends '//lib/python/3/10/mix.sh' %}
+{% extends '//mix/hub.sh' %}
 
-{% block lib_deps %}
-{% endblock %}
-
-{% block bld_libs %}
-lib/c
-lib/z
-lib/ffi
-{% if darwin %}
-lib/darwin/framework/SystemConfiguration
-{% endif %}
-{{super()}}
-{% endblock %}
-
-{% block std_box %}
-bld/bootbox
-bld/autohell/full
-{% endblock %}
-
-{% block extra_modules %}
-_ctypes _ctypes/_ctypes.c _ctypes/callbacks.c _ctypes/callproc.c _ctypes/stgdict.c _ctypes/cfield.c _ctypes/malloc_closure.c
-{% endblock %}
-
-{% block extra_tests %}
-${out}/bin/python3 -c 'import ctypes'
-{% endblock %}
-
-{% block setup %}
-{% endblock %}
-
-{% block env %}
+{% block run_deps %}
+bin/python/lite(std_box=bld/bootbox,intl_ver=no)
 {% endblock %}
