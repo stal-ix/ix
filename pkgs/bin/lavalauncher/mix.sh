409ff6bfbe9f43da78c9bf5ddeb9cfe0bf41f6db
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-04-04 00:04:17 +0000

    add

---
+++ pkgs/bin/lavalauncher/mix.sh
@@ -0,0 +1,18 @@
+{% extends '//mix/meson.sh' %}
+
+{% block fetch %}
+https://git.sr.ht/~leon_plickat/lavalauncher/archive/v2.1.1.tar.gz
+sha:951edb0e00a118cd57d54349349d4898cfc18c6208a7c4d7f6e892eec1497c3b
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/cairo
+lib/wayland
+lib/xkbcommon
+{% endblock %}
+
+{% block bld_tool %}
+bin/scdoc
+bin/wayland/protocols
+{% endblock %}
