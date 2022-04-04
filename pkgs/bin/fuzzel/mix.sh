fca8128a1c7a84a09b75acc112dd1ff6dbbf45c9
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-04-04 00:31:44 +0000

    add

---
+++ pkgs/bin/fuzzel/mix.sh
@@ -0,0 +1,28 @@
+{% extends '//mix/meson.sh' %}
+
+{% block fetch %}
+https://codeberg.org/dnkl/fuzzel/archive/1.7.0.tar.gz
+sha:f792c9869ec2ab333e99ff49d3e94a81c7ffc45767b74ed98c63404f558a008b
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/png
+lib/cairo
+lib/tllist
+lib/fcft/3
+lib/pixman
+lib/wayland
+lib/xkbcommon
+{% endblock %}
+
+{% block bld_tool %}
+bin/scdoc
+bin/wayland/protocols
+{% endblock %}
+
+{% block meson_flags %}
+enable-cairo=enabled
+png-backend=libpng
+svg-backend=none
+{% endblock %}
