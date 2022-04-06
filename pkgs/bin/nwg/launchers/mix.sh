8e3d62f902aa08dc574d9175227904ff2087e8b5
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-04-06 05:31:15 +0000

    better

---
+++ pkgs/bin/nwg/launchers/mix.sh
@@ -0,0 +1,23 @@
+{% extends '//mix/meson.sh' %}
+
+{% block fetch %}
+https://github.com/nwg-piotr/nwg-launchers/archive/refs/tags/v0.6.3.tar.gz
+sha:8be219ef7e8aead08b46215c9c545b1bf9d108cefee580ffcf297553dee9dfaf
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/c++
+lib/rsvg/dl
+lib/gtk/3/mm
+lib/json/nlohmann
+lib/gtk/layer/shell
+{% endblock %}
+
+{% block patch %}
+cat - common/nwg_classes.h << EOF > _
+#include <array>
+EOF
+
+mv _ common/nwg_classes.h
+{% endblock %}
---
+++ pkgs/lib/json/nlohmann/mix.sh
@@ -0,0 +1,11 @@
+{% extends '//mix/meson.sh' %}
+
+{% block fetch %}
+https://github.com/nlohmann/json/archive/refs/tags/v3.10.5.tar.gz
+sha:5daca6ca216495edf89d167f808d1d03c4a4d929cef7da5e10f135ae1540c7e4
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+lib/c++
+{% endblock %}
