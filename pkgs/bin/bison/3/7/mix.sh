3b839262de55c5c379999789163b8923bb16ced8
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-04-06 23:26:16 +0000

    move

--- pkgs/bin/bison/3/7/mix.sh
+++ pkgs/bin/bison/3/7/mix.sh
@@ -1,4 +1,6 @@
-{% extends '//bin/bison/3/t/mix.sh' %}
+{% extends '//mix/autohell.sh' %}
+
+{% block make_no_thrs %}{% endblock %}
 
 {% block fetch %}
 https://ftp.gnu.org/gnu/bison/bison-3.7.6.tar.xz
@@ -27,4 +29,32 @@ bld/flex
 bld/bison
 {% endblock %}
 {{super()}}
+{% endblock %}
+
+{% block configure_flags %}
+--enable-relocatable
+{% endblock %}
+
+{% block c_rename_symbol %}
+error
+accept
+error_at_line
+error_one_per_line
+error_message_count
+error_print_progname
+{% endblock %}
+
+{% block configure %}
+{{super()}}
+rm tests/bison
+ln -s $(which bison) tests/
+{% endblock %}
+
+{% block patch %}
+rm src/parse-gram.c src/parse-gram.h
+{% endblock %}
+
+{% block env %}
+export YACC="${out}/bin/bison"
+export YFLAGS=-Wnone
 {% endblock %}
