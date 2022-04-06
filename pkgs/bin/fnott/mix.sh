e56a675aaceca407f66d795e23dba9e4120ce139
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-04-06 16:22:38 +0000

    add

---
+++ pkgs/bin/fnott/mix.sh
@@ -0,0 +1,24 @@
+{% extends '//mix/meson.sh' %}
+
+{% block fetch %}
+https://codeberg.org/dnkl/fnott/archive/1.2.1.tar.gz
+sha:a4e72845cde23aff0653f9b218a32cb40ae628b3f690dc02333ea95469faf216
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/png
+lib/dbus
+lib/linux
+lib/tllist
+lib/fcft/3
+lib/pixman
+lib/wayland
+lib/freetype
+lib/fontconfig
+{% endblock %}
+
+{% block bld_tool %}
+bin/scdoc
+bin/wayland/protocols
+{% endblock %}
