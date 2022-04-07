a0bd75b156c7425d8ea6cb45ad2d2ac705c3b22f
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-04-07 15:51:07 +0000

    fix

--- pkgs/mix/dl/fix.sh
+++ pkgs/mix/dl/fix.sh
@@ -1,7 +1,9 @@
 {% extends 'map.sh' %}
 
 {% block export_map %}
+cat << EOF
 {% for sym in mix.parse_list(self.export_symbols()) %}
 {{self.export_lib().strip()}} {{sym}} {{sym}}
 {% endfor %}
+EOF
 {% endblock %}
