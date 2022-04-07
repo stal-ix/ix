e9302b561e138f76da045ad260cd42b06411a042
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-04-07 01:29:08 +0000

    better

--- pkgs/bin/subversion/mix.sh
+++ pkgs/bin/subversion/mix.sh
@@ -1,38 +1,5 @@
-{% extends '//mix/autorehell.sh' %}
-
-{% block fetch %}
-https://archive.apache.org/dist/subversion/subversion-1.14.1.tar.bz2
-2eccc2c7451397e01a13682600af9563
-{% endblock %}
-
-{% block bld_libs %}
-bin/subversion/libs(openssl=1)
-{% endblock %}
-
-{% block bld_tool %}
-bld/python/2
-{% endblock %}
+{% extends '//mix/hub.sh' %}
 
 {% block run_deps %}
-bin/openssh
-{% endblock %}
-
-{% block setup %}
-expat="$(find_pkg expat)"
-
-export COFLAGS=$(echo "${COFLAGS}" | tr ' ' '\n' | grep -v expat | tr '\n' ' ')
-export COFLAGS="${COFLAGS} --with-expat=${expat}/include:${expat}/lib:-lexpat"
-{% endblock %}
-
-{% block patch %}
-rm autogen.sh
-{% endblock %}
-
-{% block setup_tools %}
-ln -s $(which python2) python
-{% endblock %}
-
-{% block install %}
-{{super()}}
-rm -r ${out}/share/pkgconfig
+bin/subversion/unwrap(openssl=1)
 {% endblock %}
---
+++ pkgs/bin/subversion/unwrap/mix.sh
@@ -0,0 +1,48 @@
+{% extends '//mix/autorehell.sh' %}
+
+{% block fetch %}
+https://archive.apache.org/dist/subversion/subversion-1.14.1.tar.bz2
+2eccc2c7451397e01a13682600af9563
+{% endblock %}
+
+{% block bld_libs %}
+lib/z
+lib/lz4
+lib/apr
+lib/intl
+lib/serf
+lib/neon
+lib/expat
+lib/sqlite3
+lib/apr/util
+lib/utf8proc
+{% endblock %}
+
+{% block bld_tool %}
+bld/python/2
+{% endblock %}
+
+{% block run_deps %}
+bin/openssh
+{% endblock %}
+
+{% block setup %}
+expat="$(find_pkg expat)"
+
+export COFLAGS=$(echo "${COFLAGS}" | tr ' ' '\n' | grep -v expat | tr '\n' ' ')
+export COFLAGS="${COFLAGS} --with-expat=${expat}/include:${expat}/lib:-lexpat"
+{% endblock %}
+
+{% block patch %}
+rm autogen.sh
+{% endblock %}
+
+{% block setup_tools %}
+ln -s $(which python2) python
+{% endblock %}
+
+{% block install %}
+{{super()}}
+rm -r ${out}/share/pkgconfig
+{% endblock %}
+{% extends '//mix/hub.sh' %}
