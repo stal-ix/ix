d9d530101abcc70e670f64f4582021e685892ac4
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-04-08 22:54:57 +0000

    better

--- pkgs/bin/mingetty/runit/mix.sh
+++ pkgs/bin/mingetty/runit/mix.sh
@@ -6,7 +6,7 @@ bin/mingetty/runit/slot(slot=2)
 bin/mingetty/runit/slot(slot=3)
 bin/mingetty/runit/slot(slot=4)
 {% if failsafe %}
-bin/mingetty/runit/failsafe(slot=5)
+bin/mingetty/runit/root(slot=5)
 {% else %}
 bin/mingetty/runit/slot(slot=5)
 {% endif %}
