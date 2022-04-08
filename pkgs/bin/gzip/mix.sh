144f9447756956125a0a8ae0fdc4a2717e8157b7
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-04-08 00:03:50 +0000

    upver

--- pkgs/bin/gzip/mix.sh
+++ pkgs/bin/gzip/mix.sh
@@ -1,8 +1,8 @@
 {% extends '//mix/autorehell.sh' %}
 
 {% block fetch %}
-https://ftp.gnu.org/gnu/gzip/gzip-1.10.tar.xz
-691b1221694c3394f1c537df4eee39d3
+https://ftp.gnu.org/gnu/gzip/gzip-1.12.tar.xz
+sha:ce5e03e519f637e1f814011ace35c4f87b33c0bbabeec35baf5fbd3479e91956
 {% endblock %}
 
 {% block bld_libs %}
