c60dae81378796a03a107c15db9c525470416352
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-04-09 20:20:37 +0000

    add

---
+++ pkgs/lib/utf8cpp/mix.sh
@@ -0,0 +1,16 @@
+{% extends '//mix/cmake.sh' %}
+
+{% block fetch %}
+https://github.com/nemtrif/utfcpp/archive/refs/tags/v3.2.1.tar.gz
+sha:8d6aa7d77ad0abb35bb6139cb9a33597ac4c5b33da6a004ae42429b8598c9605
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+lib/c++
+{% endblock %}
+
+{% block cmake_flags %}
+UTF8_TESTS=OFF
+UTF8_SAMPLES=OFF
+{% endblock %}
