0fe9837fa8321a0ca74149a6ac62b909403e143c
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-04-09 00:56:50 +0000

    add

---
+++ pkgs/bin/scummvm/mix.sh
@@ -0,0 +1,41 @@
+{% extends '//mix/autohell.sh' %}
+
+{% block fetch %}
+https://downloads.scummvm.org/frs/scummvm/2.5.1/scummvm-2.5.1.tar.xz
+sha:9fd8db38e4456144bf8c34dacdf7f204e75f18e8e448ec01ce08ce826a035f01
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/z
+lib/c++
+lib/png
+lib/curl
+lib/jpeg
+lib/sdl/2
+lib/giflib
+lib/fribidi
+lib/freetype
+lib/xiph/ogg
+lib/xiph/flac
+lib/xiph/vorbis
+
+lib/drivers/3d
+lib/mesa/gl/dl
+lib/mesa/egl/dl
+{% endblock %}
+
+{% block bld_tool %}
+# llvm-strings?
+bin/elfutils
+{% endblock %}
+
+{% block c_rename_symbol %}
+crc_finalize
+{% endblock %}
+
+{% block configure %}
+sh ./configure \
+    --prefix=${out} \
+    --enable-static
+{% endblock %}
