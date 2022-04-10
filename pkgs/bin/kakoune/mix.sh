4093ffc8deef74097052b05b303c59e02028e20f
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-04-10 02:27:55 +0000

    add

---
+++ pkgs/bin/kakoune/mix.sh
@@ -0,0 +1,19 @@
+{% extends '//mix/make.sh' %}
+
+{% block fetch %}
+https://github.com/mawww/kakoune/archive/refs/tags/v2021.11.08.tar.gz
+sha:0885dc65377057fcfea0e7053d01d95a83668ec1907aced84dface8e00b83dee
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/c++
+{% endblock %}
+
+{% block build_flags %}
+shut_up
+{% endblock %}
+
+{% block bld_tool %}
+bin/gzip
+{% endblock %}
