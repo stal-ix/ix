03f938f162cb299a5b5743ab1c6e5c4b3c0b297f
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-04-13 22:17:25 +0000

    use freeimage

--- pkgs/bin/imv/mix.sh
+++ pkgs/bin/imv/mix.sh
@@ -7,16 +7,14 @@ sha:88d16885017b74b594ddd6a03ef20294a6cc053f04ccc2e1edef69f3515f7999
 
 {% block bld_libs %}
 lib/c
-lib/z
-lib/png
 lib/icu
 lib/mesa
 lib/inih
-lib/tiff
 lib/cairo
 lib/pango
 lib/wayland
 lib/xkbcommon
+lib/freeimage
 lib/jpeg/turbo
 lib/drivers/3d
 {% endblock %}
---
+++ pkgs/lib/freeimage/mix.sh
@@ -0,0 +1,36 @@
+{% extends '//mix/make.sh' %}
+
+{% block fetch %}
+http://downloads.sourceforge.net/freeimage/FreeImage3180.zip
+sha:f41379682f9ada94ea7b34fe86bf9ee00935a3147be41b6569c9605a53e438fd
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+lib/z
+lib/jxl
+lib/c++
+lib/png
+lib/tiff
+lib/webp
+lib/jpeg
+lib/openexr
+lib/jbig2dec
+lib/jpeg/open
+{% endblock %}
+
+{% block build_flags %}
+wrap_cc
+shut_up
+{% endblock %}
+
+{% block make_flags %}
+DESTDIR=${out}
+INCDIR=${out}/include
+INSTALLDIR=${out}/lib
+{% endblock %}
+
+{% block patch %}
+sed -e 's|return NULL;|return 0;|' -i Source/FreeImage/PluginPSD.cpp
+sed -e 's|-o root -g root||' -i Makefile.gnu
+{% endblock %}
