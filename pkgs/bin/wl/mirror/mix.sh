444b841f5a33444b176365ac3931be63c98ca8f4
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-04-13 01:17:51 +0000

    add

---
+++ pkgs/bin/wl/mirror/mix.sh
@@ -0,0 +1,31 @@
+{% extends '//mix/cmake.sh' %}
+
+{% block fetch %}
+https://github.com/Ferdi265/wl-mirror/archive/refs/tags/v0.11.1.tar.gz
+sha:9fb078b384cc7199e24c185d733b8c43b82cea1a2fc64b17cb35c0323a0c755d
+https://gitlab.freedesktop.org/wlroots/wlr-protocols/-/archive/0c7437e2b600382f0be33949c4e244b2a3702fcb/wlr-protocols-0c7437e2b600382f0be33949c4e244b2a3702fcb.tar.bz2
+sha:f71031f8a4450c4eee3577507a43db4a4714ac37bd77dd2ba2c4eca21292c2ea
+{% endblock %}
+
+{% block unpack %}
+{{super()}}
+(cd proto/wlr-protocols; extract 1 ${src}/wlr*)
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/wayland
+lib/mesa/egl
+lib/drivers/3d
+{% endblock %}
+
+{% block bld_tool %}
+bin/wayland/protocols
+{% endblock %}
+
+{% block cmake_flags %}
+WL_PROTOCOL_DIR=${bin_wayland_protocols}/share/wayland-protocols
+FORCE_SYSTEM_WL_PROTOCOLS=ON
+WLR_PROTOCOL_DIR=${PWD}/proto/wlr-protocols
+FORCE_SYSTEM_WLR_PROTOCOLS=ON
+{% endblock %}
