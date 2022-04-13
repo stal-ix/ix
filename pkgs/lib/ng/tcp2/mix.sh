3cb5f8febbaed1bfe8fb1610484cfd87e6866065
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-04-13 21:21:15 +0000

    upver

--- pkgs/lib/ng/tcp2/mix.sh
+++ pkgs/lib/ng/tcp2/mix.sh
@@ -1,8 +1,8 @@
 {% extends '//mix/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/ngtcp2/ngtcp2/archive/refs/tags/v0.2.1.tar.gz
-sha:5f54a39662b31f5fe08132963c8b442397e04f6d47c9597beaba9c086e7fbc9a
+https://github.com/ngtcp2/ngtcp2/archive/refs/tags/v0.3.1.tar.gz
+sha:e0f3271325dad24bbe83d88dc3aef1adcf6ef2224124d454ce2c3e0668aa138b
 {% endblock %}
 
 {% block lib_deps %}
