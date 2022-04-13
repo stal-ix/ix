c5596cbf1d15eddd753dc737016b77e2c46c081a
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-04-13 01:34:33 +0000

    add

---
+++ pkgs/bin/hikari/mix.sh
@@ -0,0 +1,31 @@
+{% extends '//mix/make.sh' %}
+
+{% block fetch %}
+https://hikari.acmelabs.space/releases/hikari-2.3.3.tar.gz
+sha:40736ff326e11a51128c1739051a692ad2c79173fc4b695c8be5ec7a614b4de2
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/pam
+lib/uclx
+lib/pango
+lib/cairo
+lib/input
+lib/wlroots
+lib/wayland
+lib/xkbcommon
+lib/drivers/3d
+{% endblock %}
+
+{% block make_bin %}bmake{% endblock %}
+
+{% block bld_tool %}
+bin/bmake
+bld/pkg/config
+bin/wayland/protocols
+{% endblock %}
+
+{% block make_flags %}
+WITH_POSIX_C_SOURCE=YES
+{% endblock %}
---
+++ pkgs/lib/uclx/mix.sh
@@ -0,0 +1,10 @@
+{% extends '//mix/autorehell.sh' %}
+
+{% block fetch %}
+https://github.com/vstakhov/libucl/archive/refs/tags/0.8.1.tar.gz
+sha:a6397e179672f0e8171a0f9a2cfc37e01432b357fd748b13f4394436689d24ef
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+{% endblock %}
