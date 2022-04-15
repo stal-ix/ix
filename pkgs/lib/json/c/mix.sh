e18c9aace9a1f7a1695c3b10fb1b43187652a086
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-04-15 00:57:01 +0000

    upver

--- pkgs/lib/json/c/mix.sh
+++ pkgs/lib/json/c/mix.sh
@@ -1,8 +1,8 @@
 {% extends '//mix/cmake.sh' %}
 
 {% block fetch %}
-https://s3.amazonaws.com/json-c_releases/releases/json-c-0.15.tar.gz
-55f395745ee1cb3a4a39b41636087501
+https://s3.amazonaws.com/json-c_releases/releases/json-c-0.16.tar.gz
+sha:8e45ac8f96ec7791eaf3bb7ee50e9c2100bbbc87b8d0f1d030c5ba8a0288d96b
 {% endblock %}
 
 {% block lib_deps %}
