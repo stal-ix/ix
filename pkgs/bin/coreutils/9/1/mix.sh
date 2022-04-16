972906031897e9559b72ff09b4dd9d701234e064
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-04-16 03:49:58 +0000

    upver

---
+++ pkgs/bin/coreutils/9/1/mix.sh
@@ -0,0 +1,6 @@
+{% extends '//bin/coreutils/9/0/mix.sh' %}
+
+{% block fetch %}
+https://ftp.gnu.org/gnu/coreutils/coreutils-9.1.tar.gz
+sha:6055df9268603e8239a5c9c1d64cb25b9a992530df66e33b8d78a660edb37b35
+{% endblock %}
