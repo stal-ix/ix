4b96316969c57014af3380d65ca7c75c2a7e438c
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-04-20 01:48:57 +0300

    proper libdecot

--- pkgs/lib/decor/mix.sh
+++ pkgs/lib/decor/mix.sh
@@ -1,18 +1,11 @@
-{% extends '//mix/meson.sh' %}
-
-{% block fetch %}
-https://gitlab.gnome.org/jadahl/libdecor/-/archive/0.1.0/libdecor-0.1.0.tar.bz2
-sha:261029af49ebb6128e00b14f805fb376f5c93b584fce50c5495ee99af426ac78
-{% endblock %}
+{% extends '//lib/decor/t/mix.sh' %}
 
 {% block lib_deps %}
-lib/c
-lib/dbus
-lib/cairo
-lib/pango
-lib/wayland
+lib/decor/cairo
+{{super()}}
 {% endblock %}
 
-{% block bld_tool %}
-bin/wayland/protocols
+{% block patch %}
+>src/libdecor-fallback.c
+{{super()}}
 {% endblock %}
