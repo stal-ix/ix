74057c78a15a0d61362c592c7ac849a8ed39ad77
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-04-21 16:40:30 +0300

    add

---
+++ pkgs/lib/xiph/rnnoise/mix.sh
@@ -0,0 +1,10 @@
+{% extends '//mix/autorehell.sh' %}
+
+{% block fetch %}
+https://gitlab.xiph.org/xiph/rnnoise/-/archive/7f449bf8bd3b933891d12c30112268c4090e4d59/rnnoise-7f449bf8bd3b933891d12c30112268c4090e4d59.tar.bz2
+sha:4af8f97c6b58319e78290f5bdc1b9d304eb9cb571b316cdf0eaab4507973046b
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+{% endblock %}
