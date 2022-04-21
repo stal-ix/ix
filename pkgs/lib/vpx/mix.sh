62132dff8ca7bb094d03a70a15fbb2add3e38241
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-04-21 16:35:19 +0300

    add

---
+++ pkgs/lib/vpx/mix.sh
@@ -0,0 +1,34 @@
+{% extends '//mix/make.sh' %}
+
+{% block fetch %}
+https://chromium.googlesource.com/webm/libvpx/+archive/626ff35955c2c35b806b3e0ecf551a1a8611cdbf.tar.gz
+sem:05ff83b352b7247177f762a307914685
+{% endblock %}
+
+{% block unpack %}
+mkdir src; cd src; extract 0 ${src}/*gz
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+lib/c++
+{% endblock %}
+
+{% block bld_tool %}
+bld/perl
+bin/nasm
+bin/elfutils
+{% endblock %}
+
+{% block configure %}
+sh ./configure \
+    --prefix=${out} \
+    --disable-install-docs \
+    --disable-install-srcs \
+    --enable-postproc \
+    --enable-runtime-cpu-detect \
+    --enable-vp8 \
+    --enable-vp9 \
+    --enable-vp9-highbitdepth \
+    --enable-vp9-temporal-denoising
+{% endblock %}
