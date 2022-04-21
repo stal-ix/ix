138fa70fc39713426651a35ab7bd5e3477b99a7a
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-04-21 03:06:58 +0300

    betting

--- pkgs/bin/qemu/mix.sh
+++ pkgs/bin/qemu/mix.sh
@@ -6,10 +6,32 @@ sha:f6b375c7951f728402798b0baabb2d86478ca53d44cedbefabbe1c46bf46f839
 {% endblock %}
 
 {% block bld_libs %}
-lib/c
-lib/glib
-lib/pixman
 bin/kernel/17/headers
+lib/c
+lib/z
+lib/lzo
+lib/ssh
+lib/usb
+lib/zstd
+lib/glib
+lib/curl
+lib/intl
+lib/udev
+lib/ssh/2
+lib/iconv
+lib/bzip2
+lib/sdl/2
+lib/nettle
+lib/cap/ng
+lib/pixman
+lib/curses
+lib/gcrypt
+lib/gnutls
+lib/fuse/3
+lib/seccomp
+lib/ucontext
+lib/sdl/deps
+lib/sdl/2/image
 {% endblock %}
 
 {% block bld_tool %}
@@ -17,18 +39,28 @@ bld/perl
 bin/bzip2
 bld/ninja
 bld/python
+bld/gettext
 bin/elfutils
 bld/pkg/config
+{% endblock %}
+
+{% block build_flags %}
+wrap_cc
 {% endblock %}
 
 {% block configure %}
 sh ./configure \
     --prefix=${out} \
-    --static \
+    --libexecdir=${out}/bin/{{uniq_id}} \
+    --disable-plugins \
+    --audio-drv-list=sdl \
+    --with-coroutine=ucontext \
     --target-list="x86_64-softmmu"
 {% endblock %}
 
 {% block patch %}
+sed -e 's|SDL_VIDEODRIVER|SDL_VIDEODRIVER_XXX|' -i ui/sdl2.c
+
 find . -type f | while read l; do
     sed -e 's|-lstdc++|-lc|' -i "${l}"
 done
