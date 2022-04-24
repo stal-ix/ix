e7689db7b56a02c8dd5e89666aa30d48812d2da8
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-04-25 01:51:12 +0300

    better

--- pkgs/mix/qt.sh
+++ pkgs/mix/qt.sh
@@ -5,6 +5,11 @@ bld/perl
 {{super()}}
 {% endblock %}
 
+{% block cmake_flags %}
+INSTALL_LIBEXECDIR=bin
+INSTALL_MKSPECSDIR=lib/mkspecs
+{% endblock %}
+
 {% block postinstall %}
 find ${out}/plugins/ -type f -name '*.a' | while read l; do
     cp ${l} ${out}/lib/
