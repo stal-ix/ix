d74bd925020e69ee23e00adc7303ba913134740b
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-04-27 19:03:03 +0300

    better

--- core/build_cmd.py
+++ core/build_cmd.py
@@ -3,7 +3,21 @@ import core.cmd_line as cc
 import core.manager as cm
 
 
+def parse_kind(n):
+    return {
+        'lib': 'lib',
+        'aux': 'aux',
+        'etc': 'aux',
+    }.get(n[:n.index('/')], 'bin')
+
+
 def cli_build(ctx):
     config, pkgs = cc.parse_pkgs(ctx)
+
+    for p in pkgs:
+        f = p['flags']
+
+        if 'kind' not in f:
+            f['kind'] = parse_kind(p['name'])
 
     cg.run(config.ops(), list(cm.Manager(config).load_packages(pkgs)))
