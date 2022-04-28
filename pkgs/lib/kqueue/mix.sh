5b2c8f310ca86439182f43e8041d2f175d4dd6cd
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-04-28 21:21:21 +0300

    better

---
+++ pkgs/lib/kqueue/mix.sh
@@ -0,0 +1,19 @@
+{% extends '//mix/cmake.sh' %}
+
+{% block fetch %}
+https://github.com/mheily/libkqueue/archive/refs/tags/v2.6.1.tar.gz
+sha:666e26b8c2fb597fc686534bd8eb6c3733ce6d0750bf13f0323c95040ca6cb92
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+{% endblock %}
+
+{% block bld_libs %}
+lib/linux
+lib/bsd/overlay
+{% endblock %}
+
+{% block cmake_flags %}
+STATIC_KQUEUE=ON
+{% endblock %}
