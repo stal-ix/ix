1cce2d48f45cde06a0d279e46f588c896f25a291
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-04-28 12:35:18 +0300

    add

---
+++ pkgs/lib/mikmod/mix.sh
@@ -0,0 +1,10 @@
+{% extends '//mix/autorehell.sh' %}
+
+{% block fetch %}
+https://downloads.sourceforge.net/project/mikmod/libmikmod/3.3.11.1/libmikmod-3.3.11.1.tar.gz
+sha:ad9d64dfc8f83684876419ea7cd4ff4a41d8bcd8c23ef37ecb3a200a16b46d19
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+{% endblock %}
--- pkgs/lib/sdl/2/mixer/mix.sh
+++ pkgs/lib/sdl/2/mixer/mix.sh
@@ -9,6 +9,7 @@ sha:b4cf5a382c061cd75081cf246c2aa2f9df8db04bdda8dcdc6b6cca55bede2419
 lib/c
 lib/sdl/2
 lib/mpg123
+lib/mikmod
 lib/xiph/ogg
 lib/xiph/flac
 lib/xiph/vorbis
