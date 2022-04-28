ac6159f1b4a7e0f303c4ba98f9733375e1366c65
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-04-28 03:03:18 +0300

    add

---
+++ pkgs/lib/ebml/mix.sh
@@ -0,0 +1,10 @@
+{% extends '//mix/cmake.sh' %}
+
+{% block fetch %}
+https://github.com/Matroska-Org/libebml/archive/refs/tags/release-1.4.2.tar.gz
+sha:66486742fd4f443553ad1917505208404c8c4240c0ab26cedaf41d9476efc665
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+{% endblock %}
---
+++ pkgs/lib/matroska/mix.sh
@@ -0,0 +1,12 @@
+{% extends '//mix/cmake.sh' %}
+
+{% block fetch %}
+https://github.com/Matroska-Org/libmatroska/archive/refs/tags/release-1.6.3.tar.gz
+sha:0c8c875ae26ac69a722f7fd0f4a4fecb4fdff681f2a165f09c06a40cbf1d0de6
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+lib/c++
+lib/ebml
+{% endblock %}
