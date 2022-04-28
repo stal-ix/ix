1a51fa825d8b4653d2c917ccbfc77c7c5a91721f
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-04-28 03:41:42 +0300

    add

---
+++ pkgs/lib/opus/file/mix.sh
@@ -0,0 +1,13 @@
+{% extends '//mix/autohell.sh' %}
+
+{% block fetch %}
+https://downloads.xiph.org/releases/opus/opusfile-0.12.tar.gz
+sha:118d8601c12dd6a44f52423e68ca9083cc9f2bfe72da7a8c1acb22a80ae3550b
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+lib/opus
+lib/openssl
+lib/xiph/ogg
+{% endblock %}
