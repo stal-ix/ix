2b48d3d490898361c4a2c6e61f7460b66fd06cd4
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-04-29 22:09:38 +0300

    upver

--- pkgs/lib/llvm/14/ver.sh
+++ pkgs/lib/llvm/14/ver.sh
@@ -1,2 +1,2 @@
-https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-14.0.2.tar.gz
-sha:ca52232b3451c8e017f00eb882277707c13e30fac1271ec97015f6d0eeb383d1
+https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-14.0.3.tar.gz
+sha:0e1d049b050127ecf6286107e9a4400b0550f841d5d2288b9d31fd32ed0683d5
