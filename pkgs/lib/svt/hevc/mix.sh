84755a77b3954ca550b06b6fbedf12a55f66e5dd
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-05-01 01:46:03 +0300

    better

--- pkgs/lib/svt/hevc/mix.sh
+++ pkgs/lib/svt/hevc/mix.sh
@@ -1,14 +1,6 @@
-{% extends '//mix/cmake.sh' %}
+{% extends '//lib/svt/hevc/t/mix.sh' %}
 
-{% block fetch %}
-https://github.com/OpenVisualCloud/SVT-HEVC/archive/refs/tags/v1.5.1.tar.gz
-sha:fcadf3a326e012bdc7d7357b55573b45dfd37bf4a4186c93ed1c4b98fe3f1f6d
-{% endblock %}
-
-{% block lib_deps %}
-lib/c
-{% endblock %}
-
-{% block bld_tool %}
-bin/nasm
+{% block cmake_flags %}
+{{super()}}
+BUILD_APP=OFF
 {% endblock %}
