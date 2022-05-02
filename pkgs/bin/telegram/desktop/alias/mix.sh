79421a311d21f657c59312698b89cb0bec123496
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-05-03 01:16:36 +0300

    add alias

---
+++ pkgs/bin/telegram/desktop/alias/mix.sh
@@ -0,0 +1,13 @@
+{% extends '//mix/proxy.sh' %}
+
+{% block install %}
+mkdir ${out}/bin
+cd ${out}/bin
+
+cat << EOF > tg
+#!/usr/bin/env sh
+exec telegram-desktop "\${@}"
+EOF
+
+chmod +x *
+{% endblock %}
--- pkgs/bin/telegram/desktop/mix.sh
+++ pkgs/bin/telegram/desktop/mix.sh
@@ -1,5 +1,6 @@
 {% extends '//mix/hub.sh' %}
 
 {% block run_deps %}
+bin/telegram/desktop/alias
 bin/telegram/desktop/unwrap(allocator=tcmalloc)
 {% endblock %}
