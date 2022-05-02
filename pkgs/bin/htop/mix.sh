9925eddca3ab956130125354eae185e617ecc225
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-05-02 16:32:36 +0300

    upver

--- pkgs/bin/htop/mix.sh
+++ pkgs/bin/htop/mix.sh
@@ -7,5 +7,12 @@ sha:1a1dd174cc828521fe5fd0e052cff8c30aa50809cf80d3ce3a481c37d476ac54
 
 {% block bld_libs %}
 lib/c
+lib/nl
+lib/cap
 lib/curses
+{% endblock %}
+
+{% block configure_flags %}
+--enable-cgroup
+--enable-delayacct
 {% endblock %}
--- pkgs/lib/fuse/3/mix.sh
+++ pkgs/lib/fuse/3/mix.sh
@@ -1,8 +1,8 @@
 {% extends '//mix/meson.sh' %}
 
 {% block fetch %}
-https://github.com/libfuse/libfuse/archive/refs/tags/fuse-3.10.5.tar.gz
-sha:e73f75e58da59a0e333d337c105093c496c0fd7356ef3a5a540f560697c9c4e6
+https://github.com/libfuse/libfuse/archive/refs/tags/fuse-3.11.0.tar.gz
+sha:25a00226d2d449c15b2f08467d6d5ebbb2a428260c4ab773721c32adbc6da072
 {% endblock %}
 
 {% block lib_deps %}
