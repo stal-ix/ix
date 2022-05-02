318fc7f1f4a37b019d244ac69c5951e6e691e0fd
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-05-03 00:48:25 +0300

    add

---
+++ pkgs/lib/adwaita/qt/mix.sh
@@ -0,0 +1,35 @@
+{% extends '//mix/cmake.sh' %}
+
+{% block fetch %}
+https://github.com/FedoraQt/adwaita-qt/archive/refs/tags/1.4.1.tar.gz
+sha:834f2caaf031306b63b31ad6fec489482feda35e9aa260f205cbb976cde97b1d
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+lib/c++
+lib/qt/6/base
+{% endblock %}
+
+{% block bld_tool %}
+bld/qt/6
+{% endblock %}
+
+{% block cmake_flags %}
+USE_QT6=ON
+{% endblock %}
+
+{% block install %}
+{{super()}}
+cd ${out}
+
+cp plugins/styles/adwaita.a lib/libadwaitaplug.a
+
+for x in lib/*.a; do
+    llvm-objcopy --localize-symbol=_Z7ADWAITAv ${x}
+done
+{% endblock %}
+
+{% block cpp_defines %}
+QT_STATICPLUGIN=1
+{% endblock %}
