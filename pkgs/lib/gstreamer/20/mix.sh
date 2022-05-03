42ebe5d47f406ea61c79946022ed58e47127aebe
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-05-03 23:39:44 +0300

    upver

--- pkgs/lib/gstreamer/20/mix.sh
+++
@@ -1,6 +0,0 @@
-{% extends '//lib/gstreamer/19/mix.sh' %}
-
-{% block fetch %}
-https://gitlab.freedesktop.org/gstreamer/gstreamer/-/archive/1.20.2/gstreamer-1.20.2.tar.bz2
-sha:bc82f652d6355c9c89520eca3b38c726e39eac8e611007fa54453807a38948c3
-{% endblock %}
--- pkgs/lib/gstreamer/mix.sh
+++ pkgs/lib/gstreamer/mix.sh
@@ -1,5 +1,63 @@
-{% extends '//mix/hub.sh' %}
+{% extends '//mix/meson.sh' %}
+
+{% block fetch %}
+https://gitlab.freedesktop.org/gstreamer/gstreamer/-/archive/1.20.2/gstreamer-1.20.2.tar.bz2
+sha:bc82f652d6355c9c89520eca3b38c726e39eac8e611007fa54453807a38948c3
+{% endblock %}
 
 {% block lib_deps %}
-lib/gstreamer/{{gstreamer_ver or '20'}}
+lib/c
+lib/z
+lib/orc
+lib/drm
+lib/glib
+lib/udev/g
+lib/opengl
+lib/wayland
+lib/graphene
+lib/json/glib
+{% endblock %}
+
+{% block bld_libs %}
+lib/linux
+{% endblock %}
+
+{% block run_data %}
+aux/iso-codes
+{% endblock %}
+
+{% block bld_tool %}
+bin/orc
+bld/flex
+bld/bison
+bld/gettext
+bld/glib/codegen
+bin/wayland/protocols
+{% endblock %}
+
+{% block patch %}
+sed -e 's|.get_shared_lib()||' -i meson.build
+{% endblock %}
+
+{% block meson_flags %}
+bad=disabled
+ges=disabled
+base=enabled
+good=enabled
+gst-plugins-good:soup=disabled
+ugly=disabled
+devtools=disabled
+rtsp_server=disabled
+{% endblock %}
+
+{% block install %}
+{{super()}}
+cp -R ${out}/lib/gstreamer*/* ${out}/lib/
+cp -R ${out}/lib/include/* ${out}/include/gstreamer*/
+rm -rf ${out}/lib/gstreamer* ${out}/lib/include
+mv ${out}/include/g*/* ${out}/include/
+{% endblock %}
+
+{% block strip_pc %}
+echo 'TODO(pg): fixme'
 {% endblock %}
