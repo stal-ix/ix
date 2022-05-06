ca2ebd82e967d1a74d7b940e023cafdb1d521bbf
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-05-06 05:34:13 +0300

    better

--- pkgs/bin/ananicy/good/mix.sh
+++ pkgs/bin/ananicy/good/mix.sh
@@ -4,15 +4,15 @@
 mkdir -p ${out}/etc/ananicy.d; cd ${out}/etc/ananicy.d
 
 cat << EOF > sway.rules
-{"name": "sway", "rtprio": 99, "sched": "rr"}
+{"name": "sway", "rtprio": 10, "sched": "fifo"}
 EOF
 
 cat << EOF > epiphany.rules
-{"name": "epiphany-bin", "rtprio": 90, "sched": "rr"}
+{"name": "epiphany-bin", "rtprio": 9, "sched": "rr"}
 EOF
 
 cat << EOF > webkit.rules
-{"name": "WebKitWebProcess", "rtprio": 80, "sched": "rr"}
-{"name": "WebKitNetworkProcess", "rtprio": 80, "sched": "rr"}
+{"name": "WebKitWebProcess", "rtprio": 8, "sched": "rr"}
+{"name": "WebKitNetworkProcess", "rtprio": 8, "sched": "rr"}
 EOF
 {% endblock %}
