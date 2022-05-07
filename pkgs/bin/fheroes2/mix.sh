3dffd11e52bc81ed1058415a3b840c1d18b00bd5
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-05-07 18:55:24 +0300

    upver

--- pkgs/bin/fheroes2/mix.sh
+++ pkgs/bin/fheroes2/mix.sh
@@ -1,8 +1,8 @@
 {% extends '//mix/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/ihhub/fheroes2/archive/refs/tags/0.9.14.tar.gz
-sha:5532fa8443b8d8f54fcfc56628f1020470ad215e12285d78c8450e49ea9944be
+https://github.com/ihhub/fheroes2/archive/refs/tags/0.9.15.tar.gz
+sha:5325cbb6fe96ff80a27584d9fd3ea1d572eb6e21f863fbc72d0d245752b5444e
 {% endblock %}
 
 {% block bld_libs %}
