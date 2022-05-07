6af28ca8a3f62948553541c6c7880e31ca39ea2a
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-05-07 23:36:11 +0300

    upver

--- pkgs/lib/k/pwp/mix.sh
+++ pkgs/lib/k/pwp/mix.sh
@@ -1,8 +1,8 @@
 {% extends '//mix/cmake.sh' %}
 
 {% block fetch %}
-https://download.kde.org/stable/plasma-wayland-protocols/plasma-wayland-protocols-1.6.0.tar.xz
-sha:b74ffac969ef067e47180e748a67a3a1816b7157ff4ea620ed4432f59b70f01f
+https://download.kde.org/stable/plasma-wayland-protocols/plasma-wayland-protocols-1.7.0.tar.xz
+sha:084e2685caa61d94c6fe86dce006b22474d7bb4b34c4cb96bd763b97e305fad6
 {% endblock %}
 
 {% block lib_deps %}
