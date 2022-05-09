c85d810b7ac8b4f7fa8e2109176af21d284afd55
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-05-09 17:44:59 +0300

    upver

--- pkgs/bin/kernel/17/t/ver.sh
+++ pkgs/bin/kernel/17/t/ver.sh
@@ -1,2 +1,2 @@
-https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.17.5.tar.xz
-sha:9bbcd185b94436f9c8fe977fa0e862f60d34003562327fcebb27c9fa342fe987
+https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.17.6.tar.xz
+sha:64ccf18380be5b5491322a3fd54904538b544e523e4bf86289dd8df7404cb10c
