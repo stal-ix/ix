cbb55e8f08f3a3646605a3ac776b3384816e635d
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-05-09 20:05:30 +0300

    upver

--- pkgs/bin/kernel/15/ver.sh
+++ pkgs/bin/kernel/15/ver.sh
@@ -1,2 +1,2 @@
-https://www.kernel.org/pub/linux/kernel/v5.x/linux-5.15.33.tar.xz
-sha:c30a17e6090f9ebf2d8ff58cd6c92c7324b1f4a8b3aa6a7f68850310af05a9c4
+https://www.kernel.org/pub/linux/kernel/v5.x/linux-5.15.38.tar.xz
+sha:7e415d420990b88bfec038d56e920b9b28f99d54f31dbbd7aa82e66acca11052
