60d3efa9f2e0b7e1d3f02785b95db10ebc2f0796
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-05-10 16:30:36 +0300

    not need it

--- pkgs/bin/kernel/t/0/mix.sh
+++ pkgs/bin/kernel/t/0/mix.sh
@@ -23,10 +23,6 @@ bin/bison/3/6
 bin/busybox/bc
 {% endblock %}
 
-{% block shell %}
-bin/bash/lite/sh
-{% endblock %}
-
 {% block configure %}
 make HOSTCC="${HOST_CC} -D__always_inline=__inline__ -w" mrproper
 cat << EOF > .config
