4a245ea897ac3a97ecfc4f9c68a094e850228180
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-05-10 16:12:47 +0300

    upver

--- pkgs/lib/snmalloc/t/mix.sh
+++ pkgs/lib/snmalloc/t/mix.sh
@@ -1,12 +1,16 @@
 {% extends '//mix/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/microsoft/snmalloc/archive/refs/tags/0.5.3.tar.gz
-sha:0985e188bac5c69a39d5f5015debab4c19511b633067b577ef167e37d7468661
+https://github.com/microsoft/snmalloc/archive/refs/tags/0.6.0.tar.gz
+sha:de1bfb86407d5aac9fdad88319efdd5593ca2f6c61fc13371c4f34aee0b6664f
 {% endblock %}
 
 {% block lib_deps %}
 lib/c/naked
+{% endblock %}
+
+{% block build_flags %}
+shut_up
 {% endblock %}
 
 {% block bld_libs %}
