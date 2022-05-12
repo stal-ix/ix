2f0486251a5c3dfc36680b71745e94d16014554a
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-05-12 05:14:36 +0300

    clean

--- pkgs/bin/telegram/desktop/unwrap/clang/mix.sh
+++ pkgs/bin/telegram/desktop/unwrap/clang/mix.sh
@@ -6,11 +6,3 @@
 {{super()}}
 export CXXFLAGS="-std=c++20 ${CXXFLAGS}"
 {% endblock %}
-
-{% block patch1 %}
-base64 -d << EOF | patch -p1
-{% include '0.diff/base64' %}
-EOF
-
-{{super()}}
-{% endblock %}
