7b1c4bc3797b84e595f1adf9ea6cec11af74529f
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-05-13 21:17:19 +0300

    better

--- pkgs/bin/rofi/mix.sh
+++ pkgs/bin/rofi/mix.sh
@@ -1,5 +1,7 @@
 {% extends '//mix/meson.sh' %}
 
+{# hard dep on X #}
+
 {% block fetch %}
 https://github.com/lbonn/rofi/releases/download/1.7.3%2Bwayland1/rofi-1.7.3+wayland1.tar.xz
 sha:88c993d792549abc141f95c26ac99126fb91f6eed47bd4f3252e3decfe08dabd
