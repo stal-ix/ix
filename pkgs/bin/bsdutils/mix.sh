f494afad4c98ed8950e60c471b90170f71feccfe
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-05-15 20:52:23 +0300

    refac

--- pkgs/bin/bsdutils/mix.sh
+++ pkgs/bin/bsdutils/mix.sh
@@ -1,22 +1,5 @@
-{% extends '//mix/meson.sh' %}
+{% extends '//mix/hub.sh' %}
 
-{% block fetch %}
-https://github.com/dcantrell/bsdutils/archive/refs/tags/v13.0-beta1.tar.gz
-sha:08970d5511c8dd34b69991ce8d88f5b29708b376bf9bbed42c8bf081c0c3a12e
-{% endblock %}
-
-{% block bld_libs %}
-bin/bsdutils/libs(openssl=1)
-{% endblock %}
-
-{% block bld_tool %}
-bld/flex
-bld/byacc
-{% endblock %}
-
-{% block install %}
-{{super()}}
-cd ${out}
-mv sbin/* bin/
-rm -r sbin
+{% block run_deps %}
+bin/bsdutils/unwrap(openssl=1)
 {% endblock %}
---
+++ pkgs/bin/bsdutils/unwrap/mix.sh
@@ -0,0 +1,33 @@
+{% extends '//mix/meson.sh' %}
+
+{% block fetch %}
+https://github.com/dcantrell/bsdutils/archive/refs/tags/v13.0-beta1.tar.gz
+sha:08970d5511c8dd34b69991ce8d88f5b29708b376bf9bbed42c8bf081c0c3a12e
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/xo
+lib/c++
+lib/edit
+lib/curses
+lib/openssl
+
+{% if linux %}
+lib/fts
+lib/linux
+lib/rpmatch
+{% endif %}
+{% endblock %}
+
+{% block bld_tool %}
+bld/flex
+bld/byacc
+{% endblock %}
+
+{% block install %}
+{{super()}}
+cd ${out}
+mv sbin/* bin/
+rm -r sbin
+{% endblock %}
