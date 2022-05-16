b615697bd4839e9bc6cad2f61a5b7f3d28bb16f7
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-05-16 23:07:17 +0300

    add

---
+++ pkgs/bin/pqiv/mix.sh
@@ -0,0 +1,21 @@
+{% extends '//mix/autohell.sh' %}
+
+{% block fetch %}
+https://github.com/phillipberndt/pqiv/archive/refs/tags/2.12.tar.gz
+sha:1538128c88a70bbad2b83fbde327d83e4df9512a2fb560eaf5eaf1d8df99dbe5
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/glib
+lib/gtk/3
+lib/image/magick
+{% endblock %}
+
+{% block bld_tool %}
+bld/bash
+{% endblock %}
+
+{% block invoke_configure %}
+bash ./configure --prefix=${out}
+{% endblock %}
