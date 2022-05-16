f4de4e17ec5e9c05825dc2fd82dcef8bb5caa643
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-05-16 18:23:06 +0300

    upver

--- pkgs/bin/b2/t/mix.sh
+++ pkgs/bin/b2/t/mix.sh
@@ -1,8 +1,11 @@
 {% extends '//mix/c_std.sh' %}
 
 {% block fetch %}
-https://github.com/boostorg/build/archive/refs/tags/4.4.1.tar.gz
-sha:31a243b1eb26638500977a8386e56d44f86c18db70cf0a5dcdd2d7391afc1a61
+#https://github.com/boostorg/build/archive/refs/tags/4.4.1.tar.gz
+#sha:31a243b1eb26638500977a8386e56d44f86c18db70cf0a5dcdd2d7391afc1a61
+
+https://github.com/bfgroup/b2/archive/refs/tags/4.9.0.tar.gz
+sha:7c614e41f10e004c7539c75c60f7b2df26a61fe35058e9021f8fd5049c97a255
 {% endblock %}
 
 {% block bld_libs %}
--- pkgs/bin/quake/1/vk/mix.sh
+++ pkgs/bin/quake/1/vk/mix.sh
@@ -1,8 +1,8 @@
 {% extends '//mix/make.sh' %}
 
 {% block fetch %}
-https://github.com/Novum/vkQuake/archive/refs/tags/1.13.0.tar.gz
-sha:baeab4fbea17bb41064b56a7e6e745f2856634c88d57ad86de97990399c24b3b
+https://github.com/Novum/vkQuake/archive/refs/tags/1.13.1.tar.gz
+sha:ceb9ee46135309f7caf94267539cae70b47ef8edf104e3901b89d3aee7b0b874
 {% endblock %}
 
 {% block unpack %}
