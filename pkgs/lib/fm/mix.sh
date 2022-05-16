de6a4e3c580d7725312c4599c553b5ddfc90b9ec
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-05-16 18:18:28 +0300

    add

---
+++ pkgs/lib/fm/mix.sh
@@ -0,0 +1,15 @@
+{% extends '//mix/autorehell.sh' %}
+
+{# TODO(pg): X, gtk-doc #}
+
+{% block fetch %}
+https://github.com/lxde/libfm/archive/refs/tags/1.3.2.tar.gz
+sha:e65aa86650879fcd6c06983164b95409b92724482191f06472c567cbd8cf5ddd
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+lib/dbus
+lib/glib
+lib/gtk/3
+{% endblock %}
