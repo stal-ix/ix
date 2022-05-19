c6cf4d41cd6118bd2bf521bc188a2121ca47d9c5
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-05-19 14:36:49 +0300

    upver

--- pkgs/bin/b2/t/mix.sh
+++ pkgs/bin/b2/t/mix.sh
@@ -1,11 +1,8 @@
 {% extends '//mix/c_std.sh' %}
 
 {% block fetch %}
-#https://github.com/boostorg/build/archive/refs/tags/4.4.1.tar.gz
-#sha:31a243b1eb26638500977a8386e56d44f86c18db70cf0a5dcdd2d7391afc1a61
-
-https://github.com/bfgroup/b2/archive/refs/tags/4.9.0.tar.gz
-sha:7c614e41f10e004c7539c75c60f7b2df26a61fe35058e9021f8fd5049c97a255
+https://github.com/bfgroup/b2/archive/refs/tags/4.9.1.tar.gz
+sha:81e49dc85e956c3e708bdd02fcfe0b9f406fca8edca54c75c94ebd6c322ed587
 {% endblock %}
 
 {% block bld_libs %}
