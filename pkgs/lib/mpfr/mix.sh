65121d1757c0136e136afdf568fe7e9f5ee99288
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-05-21 20:18:01 +0300

    proper url

--- pkgs/lib/mpfr/mix.sh
+++ pkgs/lib/mpfr/mix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/autorehell.sh' %}
 
 {% block fetch %}
-https://www.mpfr.org/mpfr-current/mpfr-4.1.0.tar.xz
-bdd3d5efba9c17da8d83a35ec552baef
+https://ftp.gnu.org/gnu/mpfr/mpfr-4.1.0.tar.xz
+sha:0c98a3f1732ff6ca4ea690552079da9c597872d30e96ec28414ee23c95558a7f
 {% endblock %}
 
 {% block lib_deps %}
