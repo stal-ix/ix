1e61358584250b45654d5066e0bda2f4d82aa5ca
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-05-21 22:56:26 +0300

    add

--- pkgs/lib/mesa/t/mix.sh
+++ pkgs/lib/mesa/t/mix.sh
@@ -3,6 +3,9 @@
 {% block fetch %}
 https://archive.mesa3d.org/mesa-22.0.4.tar.xz
 sha:c7971f58fa826e474617cda53250c6600fce60994b9fac5b641522e21f471ed4
+
+#https://archive.mesa3d.org/mesa-22.1.0.tar.xz
+#sha:df6270c1371eaa2aa6eb65b95cbbb2a98b14fa4b7ba0ed45e4ca2fd32df60477
 {% endblock %}
 
 {% block lib_deps %}
