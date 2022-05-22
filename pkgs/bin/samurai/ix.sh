9eb66e5109f64afad061964aafd85ec5b8e73071
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-05-22 23:38:37 +0300

    upver

--- pkgs/bin/samurai/ix.sh
+++ pkgs/bin/samurai/ix.sh
@@ -1,8 +1,10 @@
 {% extends '//die/make.sh' %}
 
 {% block fetch %}
-https://github.com/michaelforney/samurai/archive/edeec43d638c826d9e446917f97e95151988e0e0.zip
-2321662d68a58456cc9ac9a2605beb59
+#https://github.com/michaelforney/samurai/archive/edeec43d638c826d9e446917f97e95151988e0e0.zip
+#2321662d68a58456cc9ac9a2605beb59
+https://git.sr.ht/~mcf/samurai/refs/download/1.2/samurai-1.2.tar.gz
+sha:3b8cf51548dfc49b7efe035e191ff5e1963ebc4fe8f6064a5eefc5343eaf78a5
 {% endblock %}
 
 {% block c_rename_symbol %}
