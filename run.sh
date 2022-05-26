8777415d0d7867ddf4494d66aa53261c59263d26
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-05-26 03:47:45 +0300

    better

--- run.sh
+++
@@ -1,5 +0,0 @@
-git filter-branch --tag-name-filter 'cat' -f --tree-filter '
-find . -type f -name "*.tgz" -o -name "*.zip" -o -name "*.gz" -o -name "*.xz" -o -name "*.bz2" -o -name "*.tar" | while read file; do
-    git rm -r -f --ignore-unmatch $file
-done
-' -- --all
--- run1.sh
+++
@@ -1,3 +0,0 @@
-git for-each-ref --format="%(refname)" refs/original/ | xargs -n 1 git update-ref –d
-git reflog expire --expire=now –all
-git gc --prune=now --aggressive
