8a201026f78c33d4eab17395616f81aed7be6c99
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-05-30 00:05:10 +0300

    upver

--- pkgs/bin/links/ix.sh
+++ pkgs/bin/links/ix.sh
@@ -1,13 +1,14 @@
 {% extends '//die/autohell.sh' %}
 
 {% block fetch %}
-http://links.twibright.com/download/links-2.26.tar.bz2
-sha:f05b3577f25dbe63e491c424f0ecb31f7bfadce9b2bc2f111dfed049c004c9cb
+http://links.twibright.com/download/links-2.27.tar.bz2
+sha:d8ddcbfcede7cdde80abeb0a236358f57fa6beb2bcf92e109624e9b896f9ebb4
 {% endblock %}
 
 {% block bld_libs %}
 lib/c
 lib/z
+lib/xz
 lib/lz4
 lib/zstd
 lib/bzip2
