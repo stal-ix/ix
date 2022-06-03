34a02f2e49e28edbaf06ce2fc13ad72470a5a529
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-06-03 23:06:37 +0300

    add

--- pkgs/lib/drm/t/ix.sh
+++ pkgs/lib/drm/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/meson.sh' %}
 
 {% block fetch %}
-https://dri.freedesktop.org/libdrm/libdrm-2.4.110.tar.xz
-sha:eecee4c4b47ed6d6ce1a9be3d6d92102548ea35e442282216d47d05293cf9737
+https://dri.freedesktop.org/libdrm/libdrm-2.4.111.tar.xz
+sha:1ad7164f77424de6f4ecba7c262bde196a214c6e19a6fbf497f0815f4d7ab2a9
 {% endblock %}
 
 {% block lib_deps %}
