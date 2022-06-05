45da47b1a76c7e63fdff908a7abf247c888210f5
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-06-06 01:03:11 +0300

    build

---
+++ pkgs/bin/qmmp/ix.sh
@@ -0,0 +1,18 @@
+{% extends '//die/cmake.sh' %}
+
+{% block fetch %}
+https://qmmp.ylsoftware.com/files/qmmp/2.1/qmmp-2.1.0.tar.bz2
+sha:ac7b5981e371dc58759d5efd2babb42e377ecc5260ba3e57ac6dbf0897570ddc
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/c++
+lib/qt/6/base
+lib/qt/6/deps
+{% endblock %}
+
+{% block bld_tool %}
+bld/qt/6
+bld/qt/6/tools
+{% endblock %}
