9e658e059e160bd79b6c3470fe02dd1ab36afb60
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-06-05 23:52:24 +0300

    add

---
+++ pkgs/lib/rpmalloc/trim/ix.sh
@@ -0,0 +1,28 @@
+{% extends '//die/premain.sh' %}
+
+{% block lib_deps %}
+lib/c
+lib/c++
+lib/rpmalloc
+{% endblock %}
+
+{% block premain_lang %}c++{% endblock %}
+
+{% block premain_code %}
+#include <thread>
+#include <unistd.h>
+
+extern "C" void rpmalloc_thread_collect();
+
+static void threadFunc() {
+    while (true) {
+        sleep({{delay}});
+        rpmalloc_thread_collect();
+    }
+}
+
+__attribute__((constructor))
+static void initRPM() {
+    std::thread(threadFunc).detach();
+}
+{% endblock %}
