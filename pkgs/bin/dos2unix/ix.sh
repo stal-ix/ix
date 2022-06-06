675566e9c0f84f4a6b77c796b717276f5e4944d6
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-06-06 14:55:02 +0300

    add

---
+++ pkgs/bin/dos2unix/ix.sh
@@ -0,0 +1,15 @@
+{% extends '//die/make.sh' %}
+
+{% block fetch %}
+https://waterlan.home.xs4all.nl/dos2unix/dos2unix-7.4.3.tar.gz
+sha:b68db41956daf933828423aa30510e00c12d29ef5916e715e8d4e694fe66ca72
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+{% endblock %}
+
+{% block bld_tool %}
+bld/gzip
+bld/gettext
+{% endblock %}
