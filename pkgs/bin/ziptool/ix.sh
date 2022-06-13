193a1250589a6eec4a4cf034892e92623db76749
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-06-14 01:59:54 +0300

    better

--- pkgs/bin/ziptool/ix.sh
+++ pkgs/bin/ziptool/ix.sh
@@ -1,1 +1,7 @@
 {% extends '//lib/zip/t/ix.sh' %}
+
+{% block cmake_flags %}
+{{super()}}
+BUILD_TOOLS=ON
+BUILD_DOC=OFF
+{% endblock %}
--- pkgs/lib/zip/ix.sh
+++ pkgs/lib/zip/ix.sh
@@ -4,3 +4,10 @@
 {{super()}}
 sed -e 's|.*bin.*||' -i ${out}/lib/pkgconfig/libzip.pc
 {% endblock %}
+
+{% block cmake_flags %}
+{{super()}}
+BUILD_TOOLS=OFF
+BUILD_REGRESS=OFF
+BUILD_EXAMPLES=OFF
+{% endblock %}
