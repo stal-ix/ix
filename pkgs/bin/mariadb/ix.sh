5b99d745f7e74777d98b2c39cc8d8751aeed24b2
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-06-16 23:29:36 +0300

    add

---
+++ pkgs/bin/mariadb/ix.sh
@@ -0,0 +1,40 @@
+{% extends '//die/cmake.sh' %}
+
+{% block fetch %}
+https://rsync.osuosl.org/pub/mariadb/mariadb-10.9.1/source/mariadb-10.9.1.tar.gz
+sha:c979bd6641e1dffde34cfb969a14411607ad09bfe5a252d9a8b76c313dbf6736
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/z
+lib/xz
+lib/fmt
+lib/lz4
+lib/c++
+lib/zstd
+lib/bzip2
+lib/boost
+lib/curses
+lib/gnutls
+lib/pcre/2/posix
+{% if linux %}
+lib/linux
+{% endif %}
+{% endblock %}
+
+{% block cmake_flags %}
+DISABLE_SHARED=ON
+{% endblock %}
+
+{% block build_flags %}
+wrap_cc
+{% endblock %}
+
+{% block cpp_defines %}
+ssize_t=long
+{% endblock %}
+
+{% block cpp_includes %}
+${PWD}/include/providers
+{% endblock %}
--- pkgs/lib/pcre/2/ix.sh
+++ pkgs/lib/pcre/2/ix.sh
@@ -1,20 +1,4 @@
-{% extends '//die/autorehell.sh' %}
-
-{% block fetch %}
-https://github.com/PhilipHazel/pcre2/archive/refs/tags/pcre2-10.40.tar.gz
-sha:a057381270857d01a1a8b2c2fcf176b1c3c6aea595d586649755c33a5843d52f
-{% endblock %}
-
-{% block lib_deps %}
-lib/c
-{% endblock %}
-
-{% block configure_flags %}
---enable-newline-is-anycrlf
---enable-utf8
---enable-jit
---disable-c++
-{% endblock %}
+{% extends '//lib/pcre/2/posix/ix.sh' %}
 
 {% block install %}
 {{super()}}
