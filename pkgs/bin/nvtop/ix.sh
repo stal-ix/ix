8547c92edc1bdab8682623f1065cad04b1cb6f7b
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-06-17 23:54:27 +0300

    add

---
+++ pkgs/bin/nvtop/ix.sh
@@ -0,0 +1,12 @@
+{% extends '//die/cmake.sh' %}
+
+{% block fetch %}
+https://github.com/Syllo/nvtop/archive/refs/tags/2.0.2.tar.gz
+sha:7f22bb9f59e36551f5d7640d265b63ec6af1a91a421fa220dd913a5a134e9901
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/drm
+lib/curses
+{% endblock %}
