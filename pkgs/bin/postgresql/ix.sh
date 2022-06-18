c1d8dca747235567b6369df80452626c94e7103c
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-06-18 03:14:51 +0300

    add

---
+++ pkgs/bin/postgresql/ix.sh
@@ -0,0 +1,35 @@
+{% extends '//die/autorehell.sh' %}
+
+{% block fetch %}
+https://ftp.postgresql.org/pub/source/v14.4/postgresql-14.4.tar.bz2
+sha:c23b6237c5231c791511bdc79098617d6852e9e3bdf360efd8b5d15a1a3d8f6a
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/z
+lib/icu
+lib/xml2
+lib/xslt
+lib/intl
+lib/linux
+lib/openssl
+lib/readline
+bin/e2fsprogs
+{% endblock %}
+
+{% block configure_flags %}
+--with-openssl
+--with-readline
+--with-uuid=e2fs
+--enable-thread-safety
+{% endblock %}
+
+{% block bld_tool %}
+bld/flex
+bld/bison
+{% endblock %}
+
+{% block build_flags %}
+wrap_cc
+{% endblock %}
