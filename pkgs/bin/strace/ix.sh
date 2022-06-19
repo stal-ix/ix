f8c39cac7d47dcb0f576f3e5c9940967d8c3f9ec
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-06-19 10:41:41 +0300

    upver

--- pkgs/bin/strace/ix.sh
+++ pkgs/bin/strace/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/autorehell.sh' %}
 
 {% block fetch %}
-https://strace.io/files/5.17/strace-5.17.tar.xz
-sha:5fb298dbd1331fd1e1bc94c5c32395860d376101b87c6cd3d1ba9f9aa15c161f
+https://strace.io/files/5.18/strace-5.18.tar.xz
+sha:60293ea79ac9253d600cdc9be077ad2988ca22284a439c9e66be5150db3d1187
 {% endblock %}
 
 {% block bld_libs %}
