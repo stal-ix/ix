7b3a0e86e9ed67b8b5f5117253126a2aa46e08d0
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-06-22 23:07:58 +0300

    add

---
+++ pkgs/bin/bzip3/ix.sh
@@ -0,0 +1,10 @@
+{% extends '//die/autorehell.sh' %}
+
+{% block fetch %}
+https://github.com/kspalaiologos/bzip3/archive/refs/tags/1.1.4.tar.gz
+sha:73d5462f253a5a8dcc201f05602c0ea6284d99d7285665ae0bd0d41d14f927b4
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+{% endblock %}
