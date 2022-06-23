a08f832d3a2038c569312d0563262b08b7524771
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-06-24 02:10:06 +0300

    upver

--- pkgs/lib/googletest/ix.sh
+++ pkgs/lib/googletest/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/google/googletest/archive/refs/tags/release-1.11.0.tar.gz
-sha:b4870bf121ff7795ba20d20bcdd8627b8e088f2d1dab299a031c1034eddc93d5
+https://github.com/google/googletest/archive/refs/tags/release-1.12.0.tar.gz
+sha:2a4f11dce6188b256f3650061525d0fe352069e5c162452818efbbf8d0b5fe1c
 {% endblock %}
 
 {% block build_flags %}
--- pkgs/lib/nss/t/ver.sh
+++ pkgs/lib/nss/t/ver.sh
@@ -1,2 +1,2 @@
-https://ftp.mozilla.org/pub/security/nss/releases/NSS_3_79_RTM/src/nss-3.79.tar.gz
-sha:ebdf2d6a96613b6fe70ad579e9f983e0e94e0110171cfb2999db633d3394a514
+https://ftp.mozilla.org/pub/security/nss/releases/NSS_3_80_RTM/src/nss-3.80.tar.gz
+sha:c0bf1fd2c7e29a6b02b309622baafc443eec90c8934bb155da4bb98988784b6a
