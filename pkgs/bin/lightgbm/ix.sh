cc6d597ebb5fb1ef91c15f35edeee125b9dc43f8
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-06-26 22:13:19 +0300

    add

---
+++ pkgs/bin/lightgbm/ix.sh
@@ -0,0 +1,21 @@
+{% extends '//die/cmake.sh' %}
+
+{% block fetch %}
+https://github.com/microsoft/LightGBM/releases/download/v3.3.2/LightGBM-complete_source_code_tar_gz.tar.gz
+sha:d90242bef13bd5c2f9f6196e57dc2cb0e7bb44bc4b2450ab39f4cce1e0a05dfd
+{% endblock %}
+
+{% block unpack %}
+mkdir src; cd src
+extract 0 ${src}/L*
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/c++
+lib/fmt
+{% endblock %}
+
+{% block cmake_flags %}
+USE_OPENMP=OFF
+{% endblock %}
