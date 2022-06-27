15a2da357cded65862570c0040facccec6c394ae
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-06-28 02:49:04 +0300

    upver

--- pkgs/lib/openal/t/ix.sh
+++ pkgs/lib/openal/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/kcat/openal-soft/archive/refs/tags/1.22.1.tar.gz
-sha:be771f9a8b3c9f283e71d7a9e3dc29a8f8fd3b25263b228d61fbcc638ec8a629
+https://github.com/kcat/openal-soft/archive/refs/tags/1.22.2.tar.gz
+sha:3e58f3d4458f5ee850039b1a6b4dac2343b3a5985a6a2e7ae2d143369c5b8135
 {% endblock %}
 
 {% block lib_deps %}
