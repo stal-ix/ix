14ad5ec92503ac8ba83942bc94d0ac52f82235eb
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-06-28 16:50:04 +0300

    better

--- pkgs/aux/fonts/dejavu/ix.sh
+++ pkgs/aux/fonts/dejavu/ix.sh
@@ -1,6 +1,6 @@
 {% extends '//die/fonts.sh' %}
 
 {% block fetch %}
-http://sourceforge.net/projects/dejavu/files/dejavu/2.37/dejavu-fonts-ttf-2.37.tar.bz2
+https://downloads.sourceforge.net/project/dejavu/dejavu/2.37/dejavu-fonts-ttf-2.37.tar.bz2
 sha:fa9ca4d13871dd122f61258a80d01751d603b4d3ee14095d65453b4e846e17d7
 {% endblock %}
