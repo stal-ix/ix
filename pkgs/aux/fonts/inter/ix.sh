4aed698a4071c4546627c305df7f42450cd79509
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-06-28 16:38:19 +0300

    better

--- pkgs/aux/fonts/inter/ix.sh
+++ pkgs/aux/fonts/inter/ix.sh
@@ -4,3 +4,9 @@
 https://github.com/rsms/inter/releases/download/v3.19/Inter-3.19.zip
 sha:150ab6230d1762a57bebf35dfc04d606ff91598a31d785f7f100356ecdcc0032
 {% endblock %}
+
+{% block unpack %}
+mkdir src; cd src
+extract 0 ${src}/Inter*
+cd "Inter Desktop"
+{% endblock %}
