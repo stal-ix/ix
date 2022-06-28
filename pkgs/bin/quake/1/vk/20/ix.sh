05802d7d92195a67bcdb42616b692bac48ba1037
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-06-28 17:58:44 +0300

    better

---
+++ pkgs/bin/quake/1/vk/20/ix.sh
@@ -0,0 +1,22 @@
+{% extends '//die/meson.sh' %}
+
+{% block fetch %}
+https://github.com/Novum/vkQuake/archive/refs/tags/1.20.0.tar.gz
+sha:2ff682a4b289607c56200d9d0920e3a360fe1b8aeb8cb39d4e0f4b460f4ad744
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/mad
+lib/sdl/2
+lib/xiph/ogg
+lib/xiph/flac
+lib/drivers/3d
+lib/xiph/vorbis
+lib/vulkan/loader
+{% endblock %}
+
+{% block install %}
+mkdir ${out}/bin
+cp $(find ${tmp} -type f -name vkquake) ${out}/bin/
+{% endblock %}
