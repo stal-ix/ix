5d45507e580dc614cb19da304d23e4b137524a22
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-06-28 19:06:33 +0300

    add

---
+++ pkgs/lib/td/ix.sh
@@ -0,0 +1,27 @@
+{% extends '//die/cmake.sh' %}
+
+{% block fetch %}
+https://github.com/tdlib/td/archive/refs/tags/v1.8.0.tar.gz
+sha:30d560205fe82fb811cd57a8fcbc7ac853a5b6195e9cb9e6ff142f5e2d8be217
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+lib/z
+lib/c++
+lib/openssl
+{% endblock %}
+
+{% block bld_libs %}
+lib/linux
+lib/readline
+{% endblock %}
+
+{% block bld_tool %}
+bin/gperf
+bld/scripts/fakegit
+{% endblock %}
+
+{% block patch %}
+>test/CMakeLists.txt
+{% endblock %}
