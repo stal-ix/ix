30a37dd11b1347323401dda93f1f966466b16a9b
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-06-28 16:46:29 +0300

    better

--- pkgs/aux/fonts/awesome/ix.sh
+++ pkgs/aux/fonts/awesome/ix.sh
@@ -4,3 +4,8 @@
 https://github.com/FortAwesome/Font-Awesome/archive/refs/tags/5.15.4.tar.gz
 sha:82c301594a566277ba3cf41e037fc03ae101727d3e5d682d09e322a53937b5ed
 {% endblock %}
+
+{% block unpack %}
+{{super()}}
+cd otfs
+{% endblock %}
