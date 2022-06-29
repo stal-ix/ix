50f1660739741e657b06d730345c90956e908ef8
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-06-29 22:31:21 +0300

    add

---
+++ pkgs/lib/dwarf/ix.sh
@@ -0,0 +1,15 @@
+{% extends '//die/meson.sh' %}
+
+{% block fetch %}
+https://www.prevanders.net/libdwarf-0.4.1.tar.xz
+sha:34277b969d30be3cc4c6fbce6926dd3e6f9ea9a27b01951c6753b479aadfd5ef
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+lib/c++
+{% endblock %}
+
+{% block meson_flags %}
+libelf=false
+{% endblock %}
