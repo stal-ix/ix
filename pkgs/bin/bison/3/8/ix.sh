8d2c86aae08de084a63c9eee906e706fb43d6bb9
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-06-30 20:01:42 +0300

    upver

--- pkgs/bin/bison/3/8/ix.sh
+++ pkgs/bin/bison/3/8/ix.sh
@@ -2,7 +2,7 @@
 
 {% block fetch %}
 https://ftp.gnu.org/gnu/bison/bison-3.8.2.tar.lz
-sha:
+sha:fdf98bfe82abb04a34d4356753f7748dbbd2ef1221b1f202852a2b5ce0f78534
 {% endblock %}
 
 {% block bison_bootstrap %}
