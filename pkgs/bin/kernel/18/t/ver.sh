ab53d1d4d764297a10fdc78033c4a9186651cb45
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-03 00:06:24 +0300

    upver

--- pkgs/bin/kernel/18/t/ver.sh
+++ pkgs/bin/kernel/18/t/ver.sh
@@ -1,2 +1,2 @@
-https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.18.8.tar.xz
-sha:0823eb05dba001cf9e2560a76dfa4d81c854e1dcfbfb25cc73ba64cd7e900a36
+https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.18.9.tar.xz
+sha:3882e26fcedcfe3ccfc158b9be2d95df25f26c3795ecf1ad95708ed532f5c93c
