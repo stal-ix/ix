c7aede09dd11a97a1b851113a339f6ec2060d844
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-02 16:51:25 +0300

    fix

--- pkgs/lib/sbc/ix.sh
+++ pkgs/lib/sbc/ix.sh
@@ -2,7 +2,7 @@
 
 {% block fetch %}
 https://kernel.googlesource.com/pub/scm/bluetooth/sbc/+archive/8dc5d5ba381512ad5b1afa45c63ec6b0a3833244.tar.gz
-sem:
+sem:b873a9da3d89a35363745df3b856641059043d877151c659215ff18e5482d651
 {% endblock %}
 
 {% block lib_deps %}
