639903ac0ddeaa84184cb1f0833209d98696ef6e
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-03 19:29:49 +0300

    add

---
+++ pkgs/bin/ccache/ix.sh
@@ -0,0 +1,16 @@
+{% extends '//die/cmake.sh' %}
+
+{% block fetch %}
+https://github.com/ccache/ccache/releases/download/v4.6.1/ccache-4.6.1.tar.xz
+sha:e5d47bd3cbb504ada864124690e7c0d28ecb1f9aeac22a9976025aed9633f3d1
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/zstd
+lib/hiredis
+{% endblock %}
+
+{% block build_flags %}
+wrap_cc
+{% endblock %}
---
+++ pkgs/lib/hiredis/ix.sh
@@ -0,0 +1,16 @@
+{% extends '//die/cmake.sh' %}
+
+{% block fetch %}
+https://github.com/redis/hiredis/archive/refs/tags/v1.0.2.tar.gz
+sha:e0ab696e2f07deb4252dda45b703d09854e53b9703c7d52182ce5a22616c3819
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+{% endblock %}
+
+{% block install %}
+{{super()}}
+mkdir ${out}/lib/cmake
+mv ${out}/share/hiredis ${out}/lib/cmake/
+{% endblock %}
