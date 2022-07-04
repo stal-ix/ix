c81f102d49a4056232dee57df379892f1331e338
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-04 05:40:45 +0300

    add

---
+++ pkgs/bin/meson/63/ix.sh
@@ -0,0 +1,6 @@
+{% extends '//bin/meson/60/ix.sh' %}
+
+{% block fetch %}
+https://github.com/mesonbuild/meson/archive/refs/tags/0.63.0.tar.gz
+sha:77808d47fa05875c2553d66cb6c6140c2f687b46256dc537eeb8a63889e0bea2
+{% endblock %}
