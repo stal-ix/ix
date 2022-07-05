7c5510d0898e450119d7009f9d6292f38ece0b5a
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-06 00:06:34 +0300

    better

--- pkgs/lib/gtk/4/ix.sh
+++ pkgs/lib/gtk/4/ix.sh
@@ -1,5 +1,35 @@
-{% extends '//die/hub.sh' %}
+{% extends '//lib/gtk/t/ix.sh' %}
+
+{% block fetch %}
+https://gitlab.gnome.org/GNOME/gtk/-/archive/4.6.6/gtk-4.6.6.tar.bz2
+sha:cf18dd932c10b66a797fb2be3b263429e28b4b8ea3ffba6ce888c0c6927baca1
+{% endblock %}
 
 {% block lib_deps %}
-lib/gtk/4/{{gtk4_ver or '6'}}
+{{super()}}
+lib/vulkan/loader
+{% endblock %}
+
+{% block bld_tool %}
+bin/sassc
+{{super()}}
+{% endblock %}
+
+{% block meson_flags %}
+x11-backend=false
+vulkan=enabled
+media-ffmpeg=disabled
+media-gstreamer=disabled
+{% endblock %}
+
+{% block patch %}
+{{super()}}
+patch -p1 << EOF
+{% include '0.diff' %}
+EOF
+{% endblock %}
+
+{% block env %}
+export CMFLAGS="-DUSE_GTK4=ON \${CMFLAGS}"
+export CPPFLAGS="-I${out}/include/gtk-4.0 \${CPPFLAGS}"
 {% endblock %}
