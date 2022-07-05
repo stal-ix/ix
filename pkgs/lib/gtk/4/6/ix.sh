0607a3f52b564bafcf0e60ad12a24b38601f8332
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-05 05:45:41 +0300

    suppoer xcursos size

--- pkgs/lib/gtk/4/6/ix.sh
+++ pkgs/lib/gtk/4/6/ix.sh
@@ -9,3 +9,10 @@ sha:cf18dd932c10b66a797fb2be3b263429e28b4b8ea3ffba6ce888c0c6927baca1
 bin/sassc
 {{super()}}
 {% endblock %}
+
+{% block patch %}
+{{super()}}
+base64 -d << EOF > gdk/wayland/gdkdisplay-wayland.c
+{% include 'gdkdisplay-wayland.c/base64' %}
+EOF
+{% endblock %}
