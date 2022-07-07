7f8191b957eb174ea1fcb90162b701134ff3dff0
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-07 04:34:48 +0300

    buildable

--- pkgs/bin/copyq/ix.sh
+++ pkgs/bin/copyq/ix.sh
@@ -1,5 +1,7 @@
 {% extends '//die/cmake.sh' %}
 
+{# no wayland support currently #}
+
 {% block fetch %}
 https://github.com/hluk/CopyQ/archive/refs/tags/v6.2.0.tar.gz
 sha:d1d46edc7c96e57c44f222b310713a886bc08d2a069756d96f620684d3e71067
@@ -10,14 +12,21 @@ lib/c
 lib/c++
 lib/qt/6/base
 lib/qt/6/deps
+lib/qt/6/declarative
 {% endblock %}
 
 {% block bld_tool %}
 bld/qt/6
 bld/qt/6/tools
+bld/qt/6/tools/qml
 {% endblock %}
 
 {% block cmake_flags %}
+USE_QXT=OFF
 WITH_QT6=ON
 WITH_PLUGINS=OFF
+{% endblock %}
+
+{% block cpp_defines %}
+NO_GLOBAL_SHORTCUTS=1
 {% endblock %}
