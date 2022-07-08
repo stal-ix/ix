17a7666d650a8ea5b19515b593f75cafaa558f61
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-09 01:28:01 +0300

    refac

--- pkgs/bin/sched/ix.sh
+++ pkgs/bin/sched/ix.sh
@@ -1,30 +1,6 @@
-{% extends '//die/proxy.sh' %}
+{% extends '//die/hub.sh' %}
 
 {% block run_deps %}
 bin/runsrv
-{% endblock %}
-
-{% block install %}
-cd ${out}; mkdir -p etc/services/sched{{delay}}; cd etc/services/sched{{delay}}
-
-cat << EOF > cycle
-#!/bin/sh
-
-while true; do
-    sleep {{delay}}
-
-    find /etc/sche.d/{{delay}} -type l -name '*.sh'| while read l; do
-        echo "run \${l}"
-        sh "\${l}" || true
-        sleep 1
-    done
-done
-EOF
-
-cat << EOF > run
-#!/bin/sh
-exec srv sched{{delay}} ${PWD}/cycle
-EOF
-
-chmod +x run cycle
+bin/sched/scripts
 {% endblock %}
