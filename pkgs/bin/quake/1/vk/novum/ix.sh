744cf5439b7f7abce8508bef680fd5f277673420
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-08 20:53:52 +0300

    upver

--- pkgs/bin/quake/1/vk/novum/ix.sh
+++ pkgs/bin/quake/1/vk/novum/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/meson.sh' %}
 
 {% block fetch %}
-https://github.com/Novum/vkQuake/archive/refs/tags/1.20.2.tar.gz
-sha:d2ce37a4991cd1e59915d40055a86af73c934d7c43dfabeb94d3fd568b73131a
+https://github.com/Novum/vkQuake/archive/refs/tags/1.20.3.tar.gz
+sha:7ff480c00513ac7a9c47864c5296d487444bc5b35a4c6c39affc3a5601888339
 {% endblock %}
 
 {% block bld_libs %}
