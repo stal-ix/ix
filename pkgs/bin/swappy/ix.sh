1705f256dc8bc872402eb14a7c10c6d129cb0250
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-09 01:41:14 +0300

    unwrap

--- pkgs/bin/swappy/ix.sh
+++ pkgs/bin/swappy/ix.sh
@@ -1,23 +1,6 @@
-{% extends '//die/meson.sh' %}
-
-{% block fetch %}
-https://github.com/jtheoof/swappy/archive/refs/tags/v1.4.0.tar.gz
-sha:1b1f26fb283d6bb441581b39826e6c254546131e0a2e568de96f4089a1f9ba66
-{% endblock %}
-
-{% block bld_libs %}
-lib/c
-lib/glib
-lib/pango
-lib/cairo
-lib/gtk/3
-{% endblock %}
+{% extends '//die/hub.sh' %}
 
 {% block run_deps %}
 bin/wl/clipboard
-{% endblock %}
-
-{% block bld_tool %}
-bin/scdoc
-bld/glib/codegen
+bin/swappy/unwrap
 {% endblock %}
