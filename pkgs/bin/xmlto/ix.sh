b42906335120e2b18b647823b1a96287e91ad5d6
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-09 01:44:41 +0300

    better

--- pkgs/bin/xmlto/ix.sh
+++ pkgs/bin/xmlto/ix.sh
@@ -1,21 +1,7 @@
-{% extends '//die/autorehell.sh' %}
-
-{% block fetch %}
-https://releases.pagure.org/xmlto/xmlto-0.0.28.tar.bz2
-md5:93bab48d446c826399d130d959fe676f
-{% endblock %}
-
-{% block bld_libs %}
-lib/c
-lib/xslt
-{% endblock %}
-
-{% block bld_tool %}
-bld/flex
-bin/getopt
-{% endblock %}
+{% extends '//die/hub.sh' %}
 
 {% block run_deps %}
 bin/getopt
 bin/xsltproc
+bin/xmlto/unwrap
 {% endblock %}
---
+++ pkgs/bin/xmlto/unwrap/ix.sh
@@ -0,0 +1,16 @@
+{% extends '//die/autorehell.sh' %}
+
+{% block fetch %}
+https://releases.pagure.org/xmlto/xmlto-0.0.28.tar.bz2
+md5:93bab48d446c826399d130d959fe676f
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/xslt
+{% endblock %}
+
+{% block bld_tool %}
+bld/flex
+bin/getopt
+{% endblock %}
