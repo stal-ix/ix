850c530e8bbf2ce5e8465d02b17bf2b0037a2e00
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-11 00:51:33 +0300

    better

--- pkgs/bin/iotop/ix.sh
+++ pkgs/bin/iotop/ix.sh
@@ -2,9 +2,15 @@
 
 {% block fetch %}
 https://github.com/Tomas-M/iotop/archive/refs/tags/v1.22.tar.gz
-sha:
+sha:fd7fc79a3bb96a33dc370ff2982ebed2e96b10580786ed7a529b15c6b64b2912
 {% endblock %}
 
 {% block bld_libs %}
 lib/c
+lib/linux
+lib/curses
+{% endblock %}
+
+{% block bld_tool %}
+bld/pkg/config
 {% endblock %}
--- pkgs/set/debug/ix.sh
+++ pkgs/set/debug/ix.sh
@@ -3,6 +3,7 @@
 {% block run_deps %}
 bin/gdb
 bin/perf
+bin/iotop
 bin/strace
 bin/fswatch
 bin/valgrind
