4b42dda54db99943a0d56dcd047b81d6b1a43fa1
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-11 07:12:16 +0300

    better

--- pkgs/lib/uriparser/ix.sh
+++ pkgs/lib/uriparser/ix.sh
@@ -1,17 +1,6 @@
-{% extends '//die/cmake.sh' %}
-
-{% block fetch %}
-https://github.com/uriparser/uriparser/archive/refs/tags/uriparser-0.9.6.tar.gz
-sha:defaf550bf6fe05e89afb9814dccc6bd643a3b0a8308801a2c04b76682b87383
-{% endblock %}
-
-{% block lib_deps %}
-lib/c
-{% endblock %}
+{% extends '//lib/uriparser/t/ix.sh' %}
 
 {% block cmake_flags %}
-BUILD_SHARED_LIBS=OFF
-URIPARSER_BUILD_TESTS=OFF
+{{super()}}
 URIPARSER_BUILD_TOOLS=OFF
-URIPARSER_BUILD_DOCS=OFF
 {% endblock %}
