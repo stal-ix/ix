519f71a7469fcaa6450d079276474a5914e7395b
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-12 18:07:04 +0300

    add

---
+++ pkgs/lib/ical/ix.sh
@@ -0,0 +1,25 @@
+{% extends '//die/cmake.sh' %}
+
+{% block fetch %}
+https://github.com/libical/libical/archive/refs/tags/v3.0.14.tar.gz
+sha:4284b780356f1dc6a01f16083e7b836e63d3815e27ed0eaaad684712357ccc8f
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+lib/c++
+lib/xml2
+lib/glib
+{% endblock %}
+
+{% block bld_tool %}
+bld/perl
+{% endblock %}
+
+{% block cmake_flags %}
+ENABLE_GTK_DOC=OFF
+{% endblock %}
+
+{% block build_flags %}
+wrap_cc
+{% endblock %}
