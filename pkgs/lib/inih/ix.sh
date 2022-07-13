7ec727aa765c22253a8da315a0e994d0cb51d23e
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-13 13:42:15 +0300

    upver

--- pkgs/lib/inih/ix.sh
+++ pkgs/lib/inih/ix.sh
@@ -1,7 +1,7 @@
 {% extends '//die/meson.sh' %}
 
 {% block fetch %}
-https://github.com/benhoyt/inih/archive/r54/inih-r54.tar.gz
+https://github.com/benhoyt/inih/archive/r54/inih-r56.tar.gz
 sha:b5566af5203f8a49fda27f1b864c0c157987678ffbd183280e16124012869869
 {% endblock %}
 
