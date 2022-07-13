1432b079d10ecb6333a41fa71e8afc28b5b70e08
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-13 13:50:26 +0300

    better

--- pkgs/lib/wx/widgets/ix.sh
+++ pkgs/lib/wx/widgets/ix.sh
@@ -1,8 +1,6 @@
 {% extends '//die/cmake.sh' %}
 
 {% block fetch %}
-#https://github.com/wxWidgets/wxWidgets/archive/refs/tags/v3.2.0.tar.gz
-#sha:43480e3887f32924246eb439520a3a2bc04d7947712de1ea0590c5b58dedadd9
 https://github.com/wxWidgets/wxWidgets/releases/download/v3.2.0/wxWidgets-3.2.0.tar.bz2
 sha:356e9b55f1ae3d58ae1fed61478e9b754d46b820913e3bfbc971c50377c1903a
 {% endblock %}
@@ -13,10 +11,10 @@ lib/c++
 lib/intl
 lib/gtk/3
 lib/pcre/2
+lib/notify
 {% endblock %}
 
 {% block cmake_flags %}
-#wxUSE_INTL=OFF
 wxUSE_XLOCALE=OFF
 {% endblock %}
 
