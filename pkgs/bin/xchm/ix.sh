b5dd632be6cd556385244ee073d00301939b7747
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-13 15:05:52 +0300

    add

---
+++ pkgs/bin/xchm/ix.sh
@@ -0,0 +1,21 @@
+{% extends '//die/autorehell.sh' %}
+
+{% block fetch %}
+https://github.com/rzvncj/xCHM/archive/refs/tags/1.33.tar.gz
+sha:8537563bc790dc2f9b27e0355d8a7e538c01347dcfd8bd392ac78ad3eeb69701
+{% endblock %}
+
+{%block bld_libs %}
+lib/c
+lib/chm
+lib/wx/widgets
+{% endblock %}
+
+{% block bld_tool %}
+bld/gettext
+{% endblock %}
+
+{% block install %}
+{{super()}}
+{{hooks.wrap_xdg_binary('xchm')}}
+{% endblock %}
