954e6c5922792cf93364c8b2fea38b7d1fe98fe0
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-13 14:59:42 +0300

    add

---
+++ pkgs/lib/chm/ix.sh
@@ -0,0 +1,18 @@
+{% extends '//die/autohell.sh' %}
+
+{% block fetch %}
+http://www.jedrea.com/chmlib/chmlib-0.40.tar.bz2
+sha:3449d64b0cf71578b2c7e3ddc048d4af3661f44a83941ea074a7813f3a59ffa3
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+{% endblock %}
+
+{% block shell %}
+bin/bash/lite/sh
+{% endblock %}
+
+{% block cpp_defines %}
+pread64=pread
+{% endblock %}
