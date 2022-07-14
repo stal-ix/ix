69abed312307e57d3098c5e018ee34f2397c6cdb
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-15 01:17:21 +0300

    better

--- pkgs/bin/shepherd/main.c
+++ pkgs/bin/shepherd/main.c
@@ -2,6 +2,7 @@
 #include <spawn.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include <sys/wait.h>
 #include <sys/prctl.h>
@@ -14,12 +15,9 @@ static void onerr(int c, const char* s) {
 }
 
 int main(int argc, char** argv) {
-    int pid;
-    int err;
-
     if (argc < 2) {
         errno = EINVAL;
-        onerr(1, "usage: subreaper [command]");
+        onerr(1, "usage: shepherd [command]");
     }
 
     setpgid(0, 0);
@@ -28,11 +26,13 @@ int main(int argc, char** argv) {
         onerr(2, "prctl");
     }
 
+    int pid;
+
     if (posix_spawnp(&pid, argv[1], 0, 0, &argv[1], environ)) {
         onerr(3, "posix_spawnp");
     }
 
-    err = 0;
+    int err = 0;
 
     while (wait(&err) != pid) {
     }
