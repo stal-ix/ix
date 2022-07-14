ab64cfd5df0ffbc37e13eb00579eac0649b17812
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-14 16:40:55 +0300

    refac

--- pkgs/bin/schedrr/ix.sh
+++ pkgs/bin/schedrr/ix.sh
@@ -1,22 +1,14 @@
-{% extends '//die/c_std.sh' %}
+{% extends '//die/inline/program.sh' %}
 
 {% block bld_libs %}
 lib/c
-lib/c++
 lib/shim/gnu
 {% endblock %}
 
-{% block step_unpack %}
-:
+{% block name %}
+schedrr
 {% endblock %}
 
-{% block build %}
-cc -o schedrr -x c++ - << EOF
-{% include 'rr.cpp' %}
-EOF
-{% endblock %}
-
-{% block install %}
-mkdir ${out}/bin
-cp schedrr ${out}/bin/
+{% block sources %}
+rr.c
 {% endblock %}
--- pkgs/bin/schedrr/rr.c
+++ pkgs/bin/schedrr/rr.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 
 int main(int argc, char** argv) {
-    sched_param param = {};
+    struct sched_param param;
     int sched = SCHED_RR;
-    param.sched_priority = static_cast<int>(99);
+    param.sched_priority = 99;
     sched_setscheduler(atoi(argv[1]), sched, &param);
 }
