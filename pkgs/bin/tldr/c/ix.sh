04a47d82663455d61d6013cab337c73d34227a88
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-15 23:02:29 +0300

    add

---
+++ pkgs/bin/tldr/c/ix.sh
@@ -0,0 +1,17 @@
+{% extends '//die/make.sh' %}
+
+{% block fetch %}
+https://github.com/tldr-pages/tldr-c-client/archive/refs/tags/v1.5.0.tar.gz
+sha:8e3f0c3f471896f8cfadbf9000aa8f2eff61fc3d76e25203ddc7640331c2a2af
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/fts
+lib/zip
+lib/curl
+{% endblock %}
+
+{% block bld_tool %}
+bld/pkg/config
+{% endblock %}
