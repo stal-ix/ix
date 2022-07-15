aae0d56d55145f44e8e4cf2a4d6f8d69f75b9b5e
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-15 20:58:57 +0300

    better

--- pkgs/lib/sigsegv/ix.sh
+++ pkgs/lib/sigsegv/ix.sh
@@ -8,3 +8,7 @@ sha:be78ee4176b05f7c75ff03298d84874db90f4b6c9d5503f0da1226b3a3c48119
 {% block lib_deps %}
 lib/c
 {% endblock %}
+
+{% block env %}
+export COFLAGS="--with-libsigsegv --with-libsigsegv-prefix=${out} \${COFLAGS}"
+{% endblock %}
