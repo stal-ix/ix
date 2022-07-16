de539ed1e155c8317d44e0578be894e4799dd5f3
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-16 15:11:48 +0300

    add

---
+++ pkgs/bin/wf/recorder/ix.sh
@@ -0,0 +1,18 @@
+{% extends '//die/meson.sh' %}
+
+{% block fetch %}
+https://github.com/ammen99/wf-recorder/archive/refs/tags/v0.3.0.tar.gz
+sha:3af1e933872653233d87167bab73d96f984d00e995385190b586be4d89dbbfdf
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/c++
+lib/ffmpeg
+lib/wayland
+{% endblock %}
+
+{% block bld_tool %}
+bin/wayland/scanner
+bin/wayland/protocols
+{% endblock %}
