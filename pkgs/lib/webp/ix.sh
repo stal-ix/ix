d342434b7aa7162af87570850bfb362469a0df92
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-16 21:13:48 +0300

    upver

--- pkgs/lib/webp/ix.sh
+++ pkgs/lib/webp/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/cmake.sh' %}
 
 {% block fetch %}
-https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.2.1.tar.gz
-sha:808b98d2f5b84e9b27fdef6c5372dac769c3bda4502febbfa5031bd3c4d7d018
+https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.2.3.tar.gz
+sha:f5d7ab2390b06b8a934a4fc35784291b3885b557780d099bd32f09241f9d83f9
 {% endblock %}
 
 {% block lib_deps %}
@@ -13,6 +13,5 @@ lib/c
 {{super()}}
 cd ${out}
 mkdir lib/cmake
-# TODO(pg):check this strange cmake shit
 mv share/WebP/cmake lib/cmake/WebP
 {% endblock %}
