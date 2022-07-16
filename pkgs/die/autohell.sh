73981811bdfdada97c0d897adfc1722b024f5e46
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-16 09:52:38 +0300

    refac

--- pkgs/die/autohell.sh
+++ pkgs/die/autohell.sh
@@ -1,4 +1,4 @@
-{% extends 'make.sh' %}
+{% extends 'configure.sh' %}
 
 {% block std_box %}
 bld/pkg/config
@@ -35,23 +35,6 @@ done
 {% endblock %}
 {% endblock %}
 
-{% block configure %}
-{% block check_tools %}
-{% if not boot %}
-which pkg-config
-{% endif %}
-{% endblock %}
-
-{% block invoke_configure %}
-{% set command_args %}
-{% block configure_shell %}
-sh
-{% endblock %}
-
-{% block configure_script %}
-configure
-{% endblock %}
-
 {% block configure_all_flags %}
 ${COFLAGS}
 
@@ -76,19 +59,5 @@ ${COFLAGS}
 {% endblock %}
 --disable-shared
 
---prefix="${out}"
---sbindir="${out}/bin"
---libexecdir="${out}/{{kind}}/{{uniq_id}}"
-
-{% block configure_flags %}
-{% endblock %}
-{% endblock %}
-
-{% if help %}
---help
-{% endif %}
-{% endset %}
-
-{{ix.fix_list(command_args)}}
-{% endblock %}
+{{super()}}
 {% endblock %}
---
+++ pkgs/die/configure.sh
@@ -0,0 +1,35 @@
+{% extends 'make.sh' %}
+
+{% block configure %}
+{% block check_tools %}
+{% if not boot %}
+which pkg-config
+{% endif %}
+{% endblock %}
+
+{% block invoke_configure %}
+{% set command_args %}
+{% block configure_shell %}
+sh
+{% endblock %}
+
+{% block configure_script %}
+configure
+{% endblock %}
+
+{% block configure_all_flags %}
+--prefix="${out}"
+--sbindir="${out}/bin"
+--libexecdir="${out}/{{kind}}/{{uniq_id}}"
+{% block configure_flags %}
+{% endblock %}
+{% endblock %}
+
+{% if help %}
+--help
+{% endif %}
+{% endset %}
+
+{{ix.fix_list(command_args)}}
+{% endblock %}
+{% endblock %}
