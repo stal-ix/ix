c8ccc8d460e8360fb5f15a5887885a18f8c5399f
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-17 19:24:11 +0300

    upver

--- pkgs/lib/notify/ix.sh
+++ pkgs/lib/notify/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/meson.sh' %}
 
 {% block fetch %}
-https://download.gnome.org/sources/libnotify/0.8/libnotify-0.8.0.tar.xz
-sha:46a26f0db4e64cf24016291eb1579ed9f0ba7611fe6cd9e1afec8f42780d3924
+https://download.gnome.org/sources/libnotify/0.8/libnotify-0.8.1.tar.xz
+sha:d033e6d4d6ccbf46a436c31628a4b661b36dca1f5d4174fe0173e274f4e62557
 {% endblock %}
 
 {% block lib_deps %}
