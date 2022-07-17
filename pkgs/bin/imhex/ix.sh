b99428081d93fc402f552516f34689c85eab16b6
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-17 16:37:37 +0300

    it works

--- pkgs/bin/imhex/ix.sh
+++ pkgs/bin/imhex/ix.sh
@@ -24,6 +24,7 @@ lib/json/nlohmann
 
 {% block bld_tool %}
 bld/python
+bld/scripts/dlfcn
 {% endblock %}
 
 {% block patch %}
@@ -59,5 +60,23 @@ wrap_cc
 
 {% block build %}
 {{super()}}
+
 >${tmp}/obj/plugins/builtin.hexplug
+
+dl_stubs << EOF >> stubs.c
+builtin initializePlugin     initializePlugin
+builtin getPluginName        getPluginName
+builtin getPluginAuthor      getPluginAuthor
+builtin getPluginDescription getPluginDescription
+builtin getCompatibleVersion getCompatibleVersion
+builtin setImGuiContext      setImGuiContext
+builtin isBuiltinPlugin      isBuiltinPlugin
+EOF
+
+cc -o imhex stubs.c $(find ${tmp} -type f -name '*.o' | grep -v 'libromfs.*main')
+{% endblock %}
+
+{% block postinstall %}
+cp imhex ${out}/bin/
+cp -R ${out}/share/imhex/plugins ${out}/bin/
 {% endblock %}
