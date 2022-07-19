5ce80feb2c4a8d064a360fd2f4fbd2b4163a01f7
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-19 10:48:36 +0300

    better

--- pkgs/lib/archive/ix.sh
+++ pkgs/lib/archive/ix.sh
@@ -3,6 +3,7 @@
 {% block lib_deps %}
 lib/c
 lib/z
+lib/b2
 lib/xz
 lib/lz4
 lib/lzo
@@ -10,14 +11,4 @@ lib/zstd
 lib/iconv
 lib/expat
 lib/bzip/2
-{% endblock %}
-
-{% block c_rename_symbol %}
-blake2s_init_param
-blake2s_init
-blake2s_init_key
-blake2s_update
-blake2s_final
-blake2s
-{{super()}}
 {% endblock %}
