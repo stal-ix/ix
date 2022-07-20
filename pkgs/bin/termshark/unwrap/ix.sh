1c6173d71309dfcb2345e3c15613601f0826fc79
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-21 01:54:28 +0300

    add

---
+++ pkgs/bin/termshark/unwrap/ix.sh
@@ -0,0 +1,20 @@
+{% extends '//die/cgo.sh' %}
+
+{% block fetch %}
+https://github.com/gcla/termshark/archive/refs/tags/v2.4.0.tar.gz
+sha:949c71866fcd2f9ed71a754ea9e3d1bdc23dee85969dcdc78180f1d18ca8b087
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+{% endblock %}
+
+{% block unpack %}
+{{super()}}
+cd cmd/termshark
+{% endblock %}
+
+{% block install %}
+mkdir ${out}/bin
+cp termshark ${out}/bin/
+{% endblock %}
