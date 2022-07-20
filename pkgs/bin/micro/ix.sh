80d3eecf450b0e6f1218d4e19753dd93e417bd4e
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-21 01:20:18 +0300

    add

---
+++ pkgs/bin/micro/ix.sh
@@ -0,0 +1,20 @@
+{% extends '//die/cgo.sh' %}
+
+{% block fetch %}
+https://github.com/zyedidia/micro/archive/refs/tags/v2.0.10.tar.gz
+sha:d849751fcd70eba48bf3e15c3cc665f818fe77e5ac7819d0afe2123dc258aedc
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+{% endblock %}
+
+{% block unpack %}
+{{super()}}
+cd cmd/micro
+{% endblock %}
+
+{% block install %}
+mkdir ${out}/bin
+cp micro ${out}/bin/
+{% endblock %}
