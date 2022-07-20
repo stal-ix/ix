26ac6dc0bf8538895933e1e60f97977b335d4d24
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-20 17:38:32 +0300

    upver

--- pkgs/bin/fish/ix.sh
+++ pkgs/bin/fish/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/fish-shell/fish-shell/archive/refs/tags/3.5.0.tar.gz
-sha:63263c97a65f87acb6460bb13215c6a8f002e91bde6c26355aa14661f95ada9f
+https://github.com/fish-shell/fish-shell/archive/refs/tags/3.5.1.tar.gz
+sha:22b097975f2f920a97cbe6e2aae92d2d8f215ae0b4243d778ee3cabf8f2fa774
 {% endblock %}
 
 {% block bld_libs %}
