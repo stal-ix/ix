e82618f8ace40ae98036efd7b557580f809f8723
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-21 19:30:17 +0300

    add

---
+++ pkgs/bin/avizo/ix.sh
@@ -0,0 +1,18 @@
+{% extends '//die/meson.sh' %}
+
+{# need vala #}
+
+{% block fetch %}
+https://github.com/misterdanb/avizo/archive/refs/tags/1.2.1.tar.gz
+sha:efd4763db7c802d22e8939dac909d4886f5967aefbfc3fd526735da2a814b91b
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/wayland
+{% endblock %}
+
+{% block bld_tool %}
+bin/wayland/scanner
+bin/wayland/protocols
+{% endblock %}
