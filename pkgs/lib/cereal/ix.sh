7b029adc7bf2b5c806f91088b26e3201016f23a8
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-21 13:51:35 +0300

    add

--- pkgs/lib/cereal/ix.sh
+++ pkgs/lib/cereal/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/USCiLab/cereal/archive/refs/tags/v1.3.1.tar.gz
-sha:65ea6ddda98f4274f5c10fb3e07b2269ccdd1e5cbb227be6a2fd78b8f382c976
+https://github.com/USCiLab/cereal/archive/refs/tags/v1.3.2.tar.gz
+sha:16a7ad9b31ba5880dac55d62b5d6f243c3ebc8d46a3514149e56b5e7ea81f85f
 {% endblock %}
 
 {% block lib_deps %}
@@ -11,7 +11,9 @@ lib/c++
 {% endblock %}
 
 {% block cmake_flags %}
-SKIP_PORTABILITY_TEST=ON
+BUILD_DOC=OFF
+BUILD_SANDBOX=OFF
 SKIP_PERFORMANCE_COMPARISON=ON
 JUST_INSTALL_CEREAL=ON
+WITH_WERROR=OFF
 {% endblock %}
