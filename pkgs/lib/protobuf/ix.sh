e688b51d0f1b4a3346d74f7b28e8b1b76da53f6e
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-21 10:39:31 +0300

    upver

--- pkgs/lib/protobuf/ix.sh
+++ pkgs/lib/protobuf/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/autorehell.sh' %}
 
 {% block fetch %}
-https://github.com/protocolbuffers/protobuf/archive/refs/tags/v21.2.tar.gz
-sha:ca983c9d2c8f8c935513642bcc4b2cbc64e4046e0bb16bf2ff893128577ece8c
+https://github.com/protocolbuffers/protobuf/archive/refs/tags/v21.3.tar.gz
+sha:1e958b62debbb46ebefa16d848741d17c88dc018dd57b729c0cd58965380f3f8
 {% endblock %}
 
 {% block lib_deps %}
