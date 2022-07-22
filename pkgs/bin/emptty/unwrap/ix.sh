08866a405aad43636a1c86ad4b45f9170f8c325a
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-22 03:06:29 +0300

    better

--- pkgs/bin/emptty/unwrap/ix.sh
+++ pkgs/bin/emptty/unwrap/ix.sh
@@ -1,5 +1,7 @@
 {% extends '//die/go/c.sh' %}
 
+{# TODO(pg): get rid of /usr/bin/getent reference #}
+
 {% block fetch %}
 https://github.com/tvrzna/emptty/archive/864c0ec71bdb9309b0458fdaae141654ad04aa93.tar.gz
 sem:17a33795aee47d65721505bf6bb011d3638cfa46d1af313cc79eba8e452a3b97
@@ -14,8 +16,4 @@ noutmp
 {% block install %}
 mkdir ${out}/bin
 cp emptty ${out}/bin/
-{% endblock %}
-
-{% block patch %}
-sed -e 's|/usr/bin/getent|getent|' -i src/session.go
 {% endblock %}
