5893de68dcbbc60485f9e767ff731885780d8829
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-22 03:04:47 +0300

    better

--- pkgs/bin/emptty/ix.sh
+++ pkgs/bin/emptty/ix.sh
@@ -1,21 +1,6 @@
-{% extends '//die/go/c.sh' %}
+{% extends '//die/hub.sh' %}
 
-{% block fetch %}
-https://github.com/tvrzna/emptty/archive/864c0ec71bdb9309b0458fdaae141654ad04aa93.tar.gz
-sem:17a33795aee47d65721505bf6bb011d3638cfa46d1af313cc79eba8e452a3b97
-{% endblock %}
-
-{% block go_tags %}
-nopam
-noxlib
-noutmp
-{% endblock %}
-
-{% block install %}
-mkdir ${out}/bin
-cp emptty ${out}/bin/
-{% endblock %}
-
-{% block patch %}
-sed -e 's|.*setenv.*getShell.*||' -i src/session.go
+{% block run_deps %}
+bin/getent
+bin/emptty/unwrap
 {% endblock %}
---
+++ pkgs/bin/emptty/unwrap/ix.sh
@@ -0,0 +1,21 @@
+{% extends '//die/go/c.sh' %}
+
+{% block fetch %}
+https://github.com/tvrzna/emptty/archive/864c0ec71bdb9309b0458fdaae141654ad04aa93.tar.gz
+sem:17a33795aee47d65721505bf6bb011d3638cfa46d1af313cc79eba8e452a3b97
+{% endblock %}
+
+{% block go_tags %}
+nopam
+noxlib
+noutmp
+{% endblock %}
+
+{% block install %}
+mkdir ${out}/bin
+cp emptty ${out}/bin/
+{% endblock %}
+
+{% block patch %}
+sed -e 's|/usr/bin/getent|getent|' -i src/session.go
+{% endblock %}
