41a9cc5b2cd1832c71a6115f8021097c703b1801
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-22 18:19:57 +0300

    upver

---
+++ pkgs/bin/gopass/ix.sh
@@ -0,0 +1,11 @@
+{% extends '//die/go/c.sh' %}
+
+{% block fetch %}
+https://github.com/gopasspw/gopass/archive/refs/tags/v1.14.3.tar.gz
+sha:7763f14fe8796b73fe08702a39b53849993659a5a0effe8f361cc3218e713606
+{% endblock %}
+
+{% block install %}
+mkdir ${out}/bin
+cp gopass ${out}/bin/
+{% endblock %}
--- pkgs/lib/glfw/ix.sh
+++ pkgs/lib/glfw/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/glfw/glfw/releases/download/3.3.7/glfw-3.3.7.zip
-sha:4ef0c544a8ace9a6cd0e0aef8250090f89fea1bf96e9fc1d9d6f76386c290c9c
+https://github.com/glfw/glfw/releases/download/3.3.8/glfw-3.3.8.zip
+sha:4d025083cc4a3dd1f91ab9b9ba4f5807193823e565a5bcf4be202669d9911ea6
 {% endblock %}
 
 {% block lib_deps %}
