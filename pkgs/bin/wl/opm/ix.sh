f62dadf9149595b6d078b99af49118d6ef933d19
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-22 15:13:20 +0300

    add

---
+++ pkgs/bin/wl/opm/ix.sh
@@ -0,0 +1,16 @@
+{% extends '//die/c/make.sh' %}
+
+{% block fetch %}
+https://git.sr.ht/~leon_plickat/wlopm/archive/v0.1.0.tar.gz
+sha:f9a7ec03a412e602420ab11d0eea872f6d30dfe5cfee93cd3d0289e4fbbb3aa1
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/wayland
+{% endblock %}
+
+{% block bld_tool %}
+bin/wayland/scanner
+bin/wayland/protocols
+{% endblock %}
