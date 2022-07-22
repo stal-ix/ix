8a0ce7db75eacecc435fafd7968738b39e83111e
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-23 00:07:32 +0300

    upver

--- pkgs/bin/haproxy/ix.sh
+++ pkgs/bin/haproxy/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/make.sh' %}
 
 {% block fetch %}
-http://www.haproxy.org/download/2.6/src/haproxy-2.6.1.tar.gz
-sha:915b351e6450d183342c4cdcda7771eac4f0f72bf90582adcd15a01c700d29b1
+http://www.haproxy.org/download/2.6/src/haproxy-2.6.2.tar.gz
+sha:f9b7dc06e02eb13b5d94dc66e0864a714aee2af9dfab10fa353ff9f1f52c8202
 {% endblock %}
 
 {% block bld_libs %}
