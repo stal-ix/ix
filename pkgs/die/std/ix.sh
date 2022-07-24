cfe0f022beaf0d2c2d81bd1a0905a3ec8e4b1824
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-24 23:51:57 +0300

    better

--- pkgs/die/std/ix.sh
+++ pkgs/die/std/ix.sh
@@ -1,13 +1,13 @@
 {% extends 'ix_unwrap.sh' %}
 
 {% block std_box %}
-  {% if 'compress' in build_flags %}
+  {% if linux and 'compress' in build_flags %}
     bld/pack
   {% endif %}
   {{super()}}
 {% endblock %}
 
-{% block step_install %}{{super()}}{% if 'compress' in build_flags %}
+{% block step_install %}{{super()}}{% if linux and 'compress' in build_flags %}
 find ${out}/bin -type f -executable | while read l; do
     packexe ${l}
 done
