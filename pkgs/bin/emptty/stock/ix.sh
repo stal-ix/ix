a984214c3b43b36702da09a648a311530731e223
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-30 04:07:15 +0300

    refac

--- pkgs/bin/emptty/stock/ix.sh
+++ pkgs/bin/emptty/stock/ix.sh
@@ -3,7 +3,7 @@
 {# TODO(pg): get rid of /usr/bin/getent reference #}
 
 {% block bld_tool %}
-bin/emptty/src
+aux/vendor/go(url=https://github.com/tvrzna/emptty/archive/864c0ec71bdb9309b0458fdaae141654ad04aa93.tar.gz,sum=sem:17a33795aee47d65721505bf6bb011d3638cfa46d1af313cc79eba8e452a3b97)
 {% endblock %}
 
 {% block go_tags %}
