95d564fb5149e31690a48b5cfc70a8f2dd5764fb
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-30 23:22:06 +0300

    sudo mode

--- pkgs/bin/light/ix.sh
+++ pkgs/bin/light/ix.sh
@@ -1,18 +1,6 @@
-{% extends '//die/c/autorehell.sh' %}
+{% extends '//die/hub.sh' %}
 
-{% block fetch %}
-https://github.com/haikarainen/light/archive/refs/tags/v1.2.2.tar.gz
-sha:62e889ee9be80fe808a972ef4981acc39e83a20f9a84a66a82cd1f623c868d9c
-{% endblock %}
-
-{% block bld_libs %}
-lib/c
-{% endblock %}
-
-{% block setup %}
-export CFLAGS="-fcommon ${CFLAGS}"
-{% endblock %}
-
-{% block configure_flags %}
---with-udev
+{% block run_deps %}
+bin/light/sudo
+bin/light/unwrap
 {% endblock %}
