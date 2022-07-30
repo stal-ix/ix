2a51a5a03b78cc564e53bb1d7b793a081224c023
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-30 20:42:18 +0300

    upver

--- pkgs/bin/sbcl/ix.sh
+++ pkgs/bin/sbcl/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//bin/sbcl/t/ix.sh' %}
 
 {% block fetch %}
-https://nav.dl.sourceforge.net/project/sbcl/sbcl/2.2.6/sbcl-2.2.6-source.tar.bz2
-sha:3e23048c8fa826fb913220beb2ac3697dbc5c0cdf2e89fed8db39ed1712304a0
+https://nav.dl.sourceforge.net/project/sbcl/sbcl/2.2.7/sbcl-2.2.7-source.tar.bz2
+sha:ec98996fdaa68009d98b4d7db2189271f2ad455ec322ca95a9c6aebf08bead6d
 {% endblock %}
 
 {% block bld_tool %}
