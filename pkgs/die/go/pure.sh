dac150ab4eca4872373456eb151d1e345a2946d9
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-30 23:26:31 +0300

    move

--- pkgs/die/go/pure.sh
+++ pkgs/die/go/pure.sh
@@ -2,7 +2,7 @@
 
 {% block std_box %}
 {{super()}}
-aux/vendor/go(url={{self.go_url().strip()}},sum={{self.go_sum().strip()}})
+die/go/vendor.sh(url={{self.go_url().strip()}},sum={{self.go_sum().strip()}})
 {% endblock %}
 
 {% block go_build_flags %}
