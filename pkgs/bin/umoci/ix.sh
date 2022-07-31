beaa9ce836042299e6b8c3b6230137ccf48cc891
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-31 05:27:38 +0300

    purify

--- pkgs/bin/umoci/ix.sh
+++ pkgs/bin/umoci/ix.sh
@@ -1,8 +1,15 @@
-{% extends '//die/go/c.sh' %}
+{% extends '//die/go/pure.sh' %}
 
-{% block fetch %}
+{% block go_url %}
 https://github.com/opencontainers/umoci/archive/refs/tags/v0.4.7.tar.gz
+{% endblock %}
+
+{% block go_sum %}
 sha:c01b36de6fdc513eb65add57bc882d72f94fc3b4b65a8f9ef59826fb754af93e
+{% endblock %}
+
+{% block go_sha %}
+sha:3d00f649bcd7ffc97c6a16d765e5dfc157bf90361ef18ca309dec0812e075657
 {% endblock %}
 
 {% block unpack %}
