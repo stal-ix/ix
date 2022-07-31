d3e0ce06f1df6f5c667cd60321942b1fb5730e8d
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-07-31 05:45:32 +0300

    upver

--- pkgs/bin/micro/ix.sh
+++ pkgs/bin/micro/ix.sh
@@ -8,6 +8,10 @@ https://github.com/zyedidia/micro/archive/refs/tags/v2.0.10.tar.gz
 sha:d849751fcd70eba48bf3e15c3cc665f818fe77e5ac7819d0afe2123dc258aedc
 {% endblock %}
 
+{% block go_sha %}
+sha:e753ac47242a1c7000f30cf133aee4989ccadfce9565c61c07827a1e8ba019ea
+{% endblock %}
+
 {% block unpack %}
 {{super()}}
 cd cmd/micro
