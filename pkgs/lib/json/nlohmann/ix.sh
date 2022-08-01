1136e6c28ba575754b6036ab6ddf8e0f45134b51
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-08-01 23:07:39 +0300

    downver

--- pkgs/lib/json/nlohmann/ix.sh
+++ pkgs/lib/json/nlohmann/ix.sh
@@ -1,8 +1,10 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/nlohmann/json/archive/refs/tags/v3.11.0.tar.gz
-sha:e0c4fbd03c0bb7e99b40791e0276be61e5f531106e1486e8f0d771a7ed6d754a
+https://github.com/nlohmann/json/archive/refs/tags/v3.10.5.tar.gz
+sha:5daca6ca216495edf89d167f808d1d03c4a4d929cef7da5e10f135ae1540c7e4
+#https://github.com/nlohmann/json/archive/refs/tags/v3.11.0.tar.gz
+#sha:e0c4fbd03c0bb7e99b40791e0276be61e5f531106e1486e8f0d771a7ed6d754a
 {% endblock %}
 
 {% block lib_deps %}
