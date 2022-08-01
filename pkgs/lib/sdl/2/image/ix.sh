145bd2f1d8d413476b667d061b4296c2d76211eb
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-08-02 00:59:06 +0300

    better

--- pkgs/lib/sdl/2/image/ix.sh
+++ pkgs/lib/sdl/2/image/ix.sh
@@ -1,10 +1,8 @@
 {% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
-#https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.5.tar.gz
-#sha:bdd5f6e026682f7d7e1be0b6051b209da2f402a2dd8bd1c4bd9c25ad263108d0
-https://github.com/libsdl-org/SDL_image/archive/refs/tags/release-2.6.0.tar.gz
-sha:fb61d96446cdb1668f837dd7ba1d22d6631755d5dda86580a1cb5aefa3e62278
+https://github.com/libsdl-org/SDL_image/archive/refs/tags/release-2.6.1.tar.gz
+sha:460da7fcee5302fdcc20ee0c26ad1c8d48810d2e368e73d564daf267344780f9
 {% endblock %}
 
 {% block lib_deps %}
