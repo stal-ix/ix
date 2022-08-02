8b6f53bd8be7dea865c87f339216b4ef21a5e63e
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-08-02 19:48:48 +0300

    upver

--- pkgs/bin/fzf/ix.sh
+++ pkgs/bin/fzf/ix.sh
@@ -1,15 +1,15 @@
 {% extends '//die/go/pure.sh' %}
 
 {% block go_url %}
-https://github.com/junegunn/fzf/archive/refs/tags/0.31.0.tar.gz
+https://github.com/junegunn/fzf/archive/refs/tags/0.32.0.tar.gz
 {% endblock %}
 
 {% block go_sum %}
-sha:df4edee32cb214018ed40160ced968d4cc3b63bba5b0571487011ee7099faa76
+sha:3502c15faeb0a6d553c68ab1a7f472af08afed94a1d016427a8ab053ef149a8f
 {% endblock %}
 
 {% block go_sha %}
-sha:66438bdf8de2e0e65098584424dbd84d0c5d1daad2f089e00a943b4a75a0c14d
+sha:ae4f0b0d7155c5db5669dd03da910821d2c2f715c060039a81424758f4db84fa
 {% endblock %}
 
 {% block install %}
