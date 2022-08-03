7669682a4f8a77b4acd688ac42e7bbe23a65f44d
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-08-03 09:14:11 +0300

    better

--- pkgs/bin/elvish/ix.sh
+++ pkgs/bin/elvish/ix.sh
@@ -1,8 +1,15 @@
-{% extends '//die/go/c.sh' %}
+{% extends '//die/go/pure.sh' %}
 
-{% block fetch %}
+{% block go_url %}
 https://github.com/elves/elvish/archive/refs/tags/v0.18.0.tar.gz
+{% endblock %}
+
+{% block go_sum %}
 sha:f4635db90af2241bfd37e17ac1a72567b92d18a396598da2099a908b3d88c590
+{% endblock %}
+
+{% block go_sha %}
+sha:50955a5aede68949f2c69c06086f64139ad34ca4250ed29c4c4841f5460ca577
 {% endblock %}
 
 {% block unpack %}
