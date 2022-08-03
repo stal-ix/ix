4cb1050253c4c434b584817a59af00d659adf8b9
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-08-03 10:00:01 +0300

    better

--- pkgs/bin/termshark/unwrap/ix.sh
+++ pkgs/bin/termshark/unwrap/ix.sh
@@ -1,8 +1,15 @@
-{% extends '//die/go/c.sh' %}
+{% extends '//die/go/pure.sh' %}
 
-{% block fetch %}
+{% block go_url %}
 https://github.com/gcla/termshark/archive/refs/tags/v2.4.0.tar.gz
+{% endblock %}
+
+{% block go_sum %}
 sha:949c71866fcd2f9ed71a754ea9e3d1bdc23dee85969dcdc78180f1d18ca8b087
+{% endblock %}
+
+{% block go_sha %}
+sha:75f9a30d44c27bbc8b2dc6895ac270c76c21517e5464f1b47ac62177ed9301e7
 {% endblock %}
 
 {% block unpack %}
