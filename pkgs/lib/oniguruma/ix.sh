4e083d85364fb4a693655381979b3fe7d72a7a0a
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-08-04 15:52:30 +0300

    refac

--- pkgs/lib/oniguruma/ix.sh
+++ pkgs/lib/oniguruma/ix.sh
@@ -5,10 +5,14 @@ https://github.com/kkos/oniguruma/archive/refs/tags/v6.9.8.tar.gz
 sha:26372720bab6733c869a9eb69377f73971138dc5e3eff4a81f5ba8aea5435fb2
 {% endblock %}
 
-{% block env_lib %}
+{% block env %}
 export COFLAGS="--with-onig=${out} \${COFLAGS}"
 {% endblock %}
 
 {% block lib_deps %}
 lib/c
+{% endblock %}
+
+{% block cmake_flags %}
+INSTALL_DOCUMENTATION=OFF
 {% endblock %}
