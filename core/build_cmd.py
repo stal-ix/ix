23dd1953efcd3973cb9bd4d85216b51b0b0939e7
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-08-04 13:30:47 +0300

    subtree build

--- core/build_cmd.py
+++ core/build_cmd.py
@@ -1,4 +1,5 @@
 import core.gg as cg
+import core.utils as cu
 import core.realm as cr
 import core.config as cf
 import core.manager as cm
@@ -13,6 +14,20 @@ def parse_kind(n):
     }.get(n[:n.index('/')], 'bin')
 
 
+def iter_buildable(p):
+    if p.buildable():
+        yield p
+    else:
+        for x in p.iter_all_runtime_depends():
+            if x.buildable():
+                yield x
+
+
+def iter_buildables(pkgs):
+    for p in pkgs:
+        yield from iter_buildable(p)
+
+
 def cli_build(ctx):
     config = cf.config_from(ctx)
     pkgs = cr.struct(cc.lex(ctx['args']))
@@ -23,4 +38,4 @@ def cli_build(ctx):
         if 'kind' not in f:
             f['kind'] = parse_kind(p['name'])
 
-    cg.run(config.ops, list(cm.Manager(config).load_packages(pkgs)))
+    cg.run(config.ops, list(cu.uniq_p(iter_buildables(cm.Manager(config).load_packages(pkgs)))))
