7b30fb2c984c78215f42ea2768c20832c8f7f49b
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-08-05 15:12:54 +0300

    use nettle

--- pkgs/bin/bsdtar/ix.sh
+++ pkgs/bin/bsdtar/ix.sh
@@ -1,13 +1,12 @@
 {% extends '//lib/archive/ix.sh' %}
 
 {% block bld_libs %}
-lib/md
 lib/intl
+lib/nettle
 {{super()}}
 {% endblock %}
 
 {% block cmake_flags %}
 {{super()}}
-ENABLE_MD=ON
-ENABLE_LIBMD=ON
+ENABLE_NETTLE=ON
 {% endblock %}
