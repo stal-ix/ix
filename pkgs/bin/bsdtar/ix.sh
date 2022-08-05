e8222239107e6dfb11b362023204bae5d43601d2
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-08-05 12:00:25 +0300

    refac

--- pkgs/bin/bsdtar/ix.sh
+++ pkgs/bin/bsdtar/ix.sh
@@ -1,6 +1,13 @@
 {% extends '//lib/archive/ix.sh' %}
 
 {% block bld_libs %}
+lib/md
 lib/intl
 {{super()}}
+{% endblock %}
+
+{% block cmake_flags %}
+{{super()}}
+ENABLE_MD=ON
+ENABLE_LIBMD=ON
 {% endblock %}
