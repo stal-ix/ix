3644a0fe08b7d3d0c60772344338ef9894187652
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-08-12 20:29:55 +0300

    upver

--- pkgs/lib/xz/t/ix.sh
+++ pkgs/lib/xz/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
-https://tukaani.org/xz/xz-5.2.5.tar.xz
-sha:3e1e518ffc912f86608a8cb35e4bd41ad1aec210df2a47aaa1f95e7f5576ef56
+https://tukaani.org/xz/xz-5.2.6.tar.xz
+sha:e076ba3439cb7cfc45b908c869f51a8c89f3c9ee9ee982fde28849c015e723a7
 {% endblock %}
 
 {% block lib_deps %}
