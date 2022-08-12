c5dc4157cf17203c875d27825728b222c00ff812
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-08-12 11:09:45 +0300

    upver

--- pkgs/bin/nvme/cli/ix.sh
+++ pkgs/bin/nvme/cli/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://github.com/linux-nvme/nvme-cli/archive/refs/tags/v2.1.1.tar.gz
-sha:cc8bd36b9aa71243e2bace238aa5505244fef3fef5418b0c61f75d1988458272
+https://github.com/linux-nvme/nvme-cli/archive/refs/tags/v2.1.2.tar.gz
+sha:b39811fd2ab8005d515f2f539984cc08f9ab1fe34e5359da10091fdf153df231
 {% endblock %}
 
 {% block bld_libs %}
