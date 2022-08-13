44adc2e89e151d216590781aa8ea8db154b2c23b
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-08-13 22:41:01 +0300

    upver

--- pkgs/lib/shaderc/ix.sh
+++ pkgs/lib/shaderc/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/google/shaderc/archive/refs/tags/v2022.1.tar.gz
-sha:592c0ba07706a940f95a24073d7f3d050daa0839f24019434a7b2776e5620ace
+https://github.com/google/shaderc/archive/refs/tags/v2022.2.tar.gz
+sha:517d36937c406858164673db696dc1d9c7be7ef0960fbf2965bfef768f46b8c0
 {% endblock %}
 
 {% block lib_deps %}
