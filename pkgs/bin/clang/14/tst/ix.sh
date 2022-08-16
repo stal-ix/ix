b39d1cad4fd374c9d787b78e57cf693bdacd9099
Anton Samokhvalov <pg@yandex-team.ru>
2022-08-16 20:16:09 +0300

    better

--- pkgs/bin/clang/14/tst/ix.sh
+++ pkgs/bin/clang/14/tst/ix.sh
@@ -4,10 +4,3 @@
 bin/gcc(gcc_ver=7,for_target={{host.gnu.three}})
 bin/gcc(gcc_ver=7,for_target={{target.gnu.three}})
 {% endblock %}
-
-{% block patch %}
-{{super()}}
-sed -e 's|const APInt \&|const auto\&|g' -i llvm/utils/TableGen/CodeGenSchedule.cpp
-sed -e 's|(CHIArg \&A)|(auto \&A)|g' -i llvm/lib/Transforms/Scalar/GVNHoist.cpp
-sed -e 's|OverriderInfo\& Overrider =|auto\& Overrider =|' -i clang/lib/AST/VTableBuilder.cpp
-{% endblock %}
