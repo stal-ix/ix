758570262527051e92984c989dff87a7974d8fe5
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-08-16 11:29:18 +0300

    upver

--- pkgs/lib/ldns/ix.sh
+++ pkgs/lib/ldns/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://github.com/NLnetLabs/ldns/archive/refs/tags/1.8.2.tar.gz
-sha:20e90be1704d35ac28da597595b32af33e8b708af9a8f1ca409d7b418ff2f9e0
+https://github.com/NLnetLabs/ldns/archive/refs/tags/1.8.3.tar.gz
+sha:33fb1a77f2de2fca9e749d17256334a3222a9e9d11b31c6d998bd920f3bd6776
 {% endblock %}
 
 {% block lib_deps %}
