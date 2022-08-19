ad7cc8ecea3585f0d445f7b59ee230dccb6c7947
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-08-19 14:35:31 +0300

    upver

--- pkgs/bin/gcc/12/ix.sh
+++ pkgs/bin/gcc/12/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//bin/gcc/11/ix.sh' %}
 
 {% block fetch %}
-http://mirror.koddos.net/gcc/releases/gcc-12.1.0/gcc-12.1.0.tar.xz
-sha:62fd634889f31c02b64af2c468f064b47ad1ca78411c45abe6ac4b5f8dd19c7b
+http://mirror.koddos.net/gcc/releases/gcc-12.2.0/gcc-12.2.0.tar.xz
+sha:e549cf9cf3594a00e27b6589d4322d70e0720cdd213f39beb4181e06926230ff
 {% endblock %}
 
 {% block patch %}
