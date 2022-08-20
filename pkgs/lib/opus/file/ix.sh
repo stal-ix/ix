8db7569c889b3c8c22fb95e82ccf1f433107aae4
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-08-20 16:40:34 +0300

    upver

--- pkgs/lib/opus/file/ix.sh
+++ pkgs/lib/opus/file/ix.sh
@@ -1,7 +1,7 @@
 {% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
-https://downloads.xiph.org/releases/opus/opusfile-0.12.tar.gz
+http://downloads.xiph.org/releases/opus/opusfile-0.12.tar.gz
 sha:118d8601c12dd6a44f52423e68ca9083cc9f2bfe72da7a8c1acb22a80ae3550b
 {% endblock %}
 
--- pkgs/lib/sdl/2/mixer/ix.sh
+++ pkgs/lib/sdl/2/mixer/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
-https://github.com/libsdl-org/SDL_mixer/archive/refs/tags/release-2.6.1.tar.gz
-sha:8c7193d3b6ae8d44f4436e53b26735b02f6cceb70fb1a3ae3f44ebd0c965e0a6
+https://github.com/libsdl-org/SDL_mixer/archive/refs/tags/release-2.6.2.tar.gz
+sha:30c64e23506fcb1699981a297833407bf5bf9d8ab77390e26b26117b4a58bad5
 {% endblock %}
 
 {% block lib_deps %}
