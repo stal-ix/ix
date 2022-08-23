b12f4f4deb6d087967340246942202ccf69285fc
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-08-24 01:32:23 +0300

    upver

--- pkgs/lib/ng/tcp2/ix.sh
+++ pkgs/lib/ng/tcp2/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/ngtcp2/ngtcp2/archive/refs/tags/v0.7.0.tar.gz
-sha:b2015c58e6d249bb71c7f7726a1e5ec5ccc78be7d50d82938358fac14ec98ca9
+https://github.com/ngtcp2/ngtcp2/archive/refs/tags/v0.8.0.tar.gz
+sha:d4a7444a125af8d92249646951268fd4a1693a3e219fdcdbed7e6a94bce0c47e
 {% endblock %}
 
 {% block lib_deps %}
@@ -16,4 +16,9 @@ lib/ng/http/3
 ENABLE_OPENSSL=ON
 ENABLE_LIB_ONLY=OFF
 ENABLE_STATIC_LIB=OFF
+{% endblock %}
+
+{% block setup %}
+# for example
+export CXXFLAGS="-std=c++17 ${CXXFLAGS}"
 {% endblock %}
