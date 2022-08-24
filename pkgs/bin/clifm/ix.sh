1f4786f0f0688748f902bbaa646354a9658414d4
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-08-24 22:35:00 +0300

    upver

--- pkgs/bin/clifm/ix.sh
+++ pkgs/bin/clifm/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/leo-arch/clifm/archive/refs/tags/v1.6.tar.gz
-sha:303f0ee2c6b19d3612de623c3ae0ee3ac110dcacfc348ec0f1d80015fc5f26e1
+https://github.com/leo-arch/clifm/archive/refs/tags/v1.7.tar.gz
+sha:6408fe75c80646f33cd1ffab5b7e81ca098e70bd01cbd7b20d3ecbe870dc47c9
 {% endblock %}
 
 {% block bld_libs %}
