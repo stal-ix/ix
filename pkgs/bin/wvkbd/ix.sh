71b25c79e9530c52a7829436ad4b97060f1791d9
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-08-26 03:46:22 +0300

    upver

--- pkgs/bin/wvkbd/ix.sh
+++ pkgs/bin/wvkbd/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/make.sh' %}
 
 {% block fetch %}
-https://github.com/jjsullivan5196/wvkbd/archive/refs/tags/v0.9.tar.gz
-sha:bb9de4e2861418ebbc8bb34014d3fcf3ba02adfb4d86d5b87b3ebe45da490b15
+https://github.com/jjsullivan5196/wvkbd/archive/refs/tags/v0.10.tar.gz
+sha:28b3269ca9954f26816269df98b93986495691f23f0fb2a2d3a4e4bce0e8f1ea
 {% endblock %}
 
 {% block bld_libs %}
