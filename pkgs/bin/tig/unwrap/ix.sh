4688d790d7435e6cfb60aa864857951344298516
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-08-26 15:40:49 +0300

    upver

--- pkgs/bin/tig/unwrap/ix.sh
+++ pkgs/bin/tig/unwrap/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://github.com/jonas/tig/archive/refs/tags/tig-2.5.6.tar.gz
-sha:4805b73b29338ba225bc7b03301b4ed8803433b0a593dcf118bd750ac79a5786
+https://github.com/jonas/tig/archive/refs/tags/tig-2.5.7.tar.gz
+sha:60a1acfda3238df28dfbe309adc1892a8c9b35138c738f1567c6c3c65c81fb72
 {% endblock %}
 
 {% block bld_libs %}
