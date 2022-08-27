e602564cdb831e7499f785ec20d89fb2e28c05ac
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-08-27 13:16:29 +0300

    upver

--- pkgs/bin/memcached/ix.sh
+++ pkgs/bin/memcached/ix.sh
@@ -1,11 +1,16 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://memcached.org/files/memcached-1.6.15.tar.gz
-sha:8d7abe3d649378edbba16f42ef1d66ca3f2ac075f2eb97145ce164388e6ed515
+https://memcached.org/files/memcached-1.6.17.tar.gz
+sha:2055e373613d8fc21529aff9f0adce3e23b9ce01ba0478d30e7941d9f2bd1224
 {% endblock %}
 
 {% block bld_libs %}
 lib/c
 lib/event
+{% endblock %}
+
+{% block patch %}
+# incoherent NDEBUG usage
+echo 'int cache_error = 0;' >> cache.c
 {% endblock %}
