bafb32698116fb81547fdb8208b009af94464923
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-08-31 00:38:36 +0300

    upver

--- pkgs/bin/git/unwrap/ix.sh
+++ pkgs/bin/git/unwrap/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/make.sh' %}
 
 {% block fetch %}
-https://github.com/git/git/archive/refs/tags/v2.37.2.tar.gz
-sha:433f5378d688a0368b3cc65db2d11418f877cbd373ee8538ce38a5795f366ba9
+https://github.com/git/git/archive/refs/tags/v2.37.3.tar.gz
+sha:730ea150a9af30e6301d3ff4169567d5a5c52950e122c1a10d21998f7a7f70d7
 {% endblock %}
 
 {% block bld_libs %}
