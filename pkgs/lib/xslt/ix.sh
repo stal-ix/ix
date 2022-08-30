b425b56a8cf16a2aa84067f12f5cbd5689088b18
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-08-30 15:49:07 +0300

    upver

--- pkgs/lib/xslt/ix.sh
+++ pkgs/lib/xslt/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://gitlab.gnome.org/GNOME/libxslt/-/archive/v1.1.36/libxslt-v1.1.36.tar.bz2
-sha:9798e44a5fb0720cbda8a2323bb0e1db219659e9cd14fa4d73ee251ec281b3d4
+https://gitlab.gnome.org/GNOME/libxslt/-/archive/v1.1.37/libxslt-v1.1.37.tar.bz2
+sha:6dbeb21aa8c938e6a39010901c0e84122bb87225b4af31f76feb4e3a5b138a5c
 {% endblock %}
 
 {% block lib_deps %}
