9a055a4140d76d73c9d008d5952a0285eceee94d
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-08-31 01:40:18 +0300

    upver

--- pkgs/bin/emptty/stock/ix.sh
+++ pkgs/bin/emptty/stock/ix.sh
@@ -11,7 +11,7 @@ sha:530da58d898c499516b5f5b189e1c86801df949fb794c2242de652ef3ee65d15
 {% endblock %}
 
 {% block go_sha %}
-sha:
+sha:02ecb90f020507e09705de71dc4fb2f8fe6f0d70352754bddfe17dbfeb4d8f7e
 {% endblock %}
 
 {% block go_tags %}
