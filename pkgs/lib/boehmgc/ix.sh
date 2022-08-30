ff70ee0db2119c1cbeb3f0daf6dce8a02f33c96c
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-08-31 00:15:08 +0300

    downver

--- pkgs/lib/boehmgc/ix.sh
+++ pkgs/lib/boehmgc/ix.sh
@@ -1,8 +1,10 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/ivmai/bdwgc/releases/download/v8.2.2/gc-8.2.2.tar.gz
-sha:f30107bcb062e0920a790ffffa56d9512348546859364c23a14be264b38836a0
+#https://github.com/ivmai/bdwgc/releases/download/v8.2.2/gc-8.2.2.tar.gz
+#sha:f30107bcb062e0920a790ffffa56d9512348546859364c23a14be264b38836a0
+https://github.com/ivmai/bdwgc/archive/742febb2f662a45b69bc0c5faef2111d576694fd.zip
+sha:b6065d4c6c80a04655894441321f182cb7b1d7fdfb3247f21f866a568468642c
 {% endblock %}
 
 {% block lib_deps %}
@@ -13,7 +15,7 @@ lib/darwin/framework/CoreFoundation
 {% endif %}
 {% endblock %}
 
-{% block env_lib %}
+{% block env %}
 export COFLAGS="--with-libgc-prefix=${out} \${COFLAGS}"
 {% endblock %}
 
