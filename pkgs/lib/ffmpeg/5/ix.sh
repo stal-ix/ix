506d1e767a6ddd6b53dc35cb6317b28151fbd952
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-09-01 12:14:07 +0300

    upver

--- pkgs/lib/ffmpeg/5/ix.sh
+++ pkgs/lib/ffmpeg/5/ix.sh
@@ -1,6 +1,6 @@
 {% extends '//lib/ffmpeg/4/ix.sh' %}
 
 {% block fetch %}
-https://ffmpeg.org/releases/ffmpeg-5.1.tar.xz
-sha:55eb6aab5ee235550fa54a33eaf8bf1b4ec66c01453182b12f6a993d75698b03
+https://ffmpeg.org/releases/ffmpeg-5.1.1.tar.xz
+sha:95bf3ff8c496511e71e958fb249e663c8c9c3de583c5bebc0f5a9745abbc0435
 {% endblock %}
