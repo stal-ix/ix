a4694bffe9b5e84b6c34c5f7647980c7f0353857
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-09-04 13:06:54 +0300

    upver

--- pkgs/bin/meson/63/ix.sh
+++ pkgs/bin/meson/63/ix.sh
@@ -1,6 +1,12 @@
 {% extends '//bin/meson/60/ix.sh' %}
 
 {% block fetch %}
-https://github.com/mesonbuild/meson/archive/refs/tags/0.63.0.tar.gz
-sha:77808d47fa05875c2553d66cb6c6140c2f687b46256dc537eeb8a63889e0bea2
+https://github.com/mesonbuild/meson/archive/refs/tags/0.63.2.tar.gz
+sha:023a3f7c74e68991154c3205a6975705861eedbf8130e013d15faa1df1af216e
+{% endblock %}
+
+{% block patch %}
+patch -p2 << EOF
+{% include '0.diff' %}
+EOF
 {% endblock %}
