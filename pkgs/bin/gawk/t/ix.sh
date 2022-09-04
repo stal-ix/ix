c6b0b4619aa38c7c85fbe6f812c4dd00e5f9adcd
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-09-04 22:42:05 +0300

    upver

--- pkgs/bin/gawk/t/ix.sh
+++ pkgs/bin/gawk/t/ix.sh
@@ -1,14 +1,19 @@
 {% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
-https://ftp.gnu.org/gnu/gawk/gawk-5.1.1.tar.xz
-sha:d87629386e894bbea11a5e00515fc909dc9b7249529dad9e6a3a2c77085f7ea2
+https://ftp.gnu.org/gnu/gawk/gawk-5.2.0.tar.xz
+sha:e4ddbad1c2ef10e8e815ca80208d0162d4c983e6cca16f925e8418632d639018
 {% endblock %}
 
 {% block bld_libs %}
 lib/c
 lib/intl
 lib/sigsegv
+{% endblock %}
+
+{% block std_box %}
+bld/bison
+{{super()}}
 {% endblock %}
 
 {% block c_rename_symbol %}
--- pkgs/bld/boot/5/gawk/ix.sh
+++ pkgs/bld/boot/5/gawk/ix.sh
@@ -3,6 +3,11 @@
 {% block bld_libs %}
 {% endblock %}
 
+{% block fetch %}
+https://ftp.gnu.org/gnu/gawk/gawk-5.1.1.tar.xz
+sha:d87629386e894bbea11a5e00515fc909dc9b7249529dad9e6a3a2c77085f7ea2
+{% endblock %}
+
 {% block bld_deps %}
 bld/boot/4/byacc
 bld/boot/4/env
