8088be04657445b1543d7062f99d1caedf0b5d9b
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-09-05 18:08:44 +0300

    upver

--- pkgs/lib/neon/ix.sh
+++ pkgs/lib/neon/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://notroj.github.io/neon/neon-0.32.2.tar.gz
-sha:986566468c6295fc5d0fb141a5981e31c9f82ee38e938374abed8471ef2fb286
+https://notroj.github.io/neon/neon-0.32.3.tar.gz
+sha:94cb875dc6dbfcdee58f039b763c674b0232886cdfd7a5de91be5cdfa2b7596a
 {% endblock %}
 
 {% block lib_deps %}
