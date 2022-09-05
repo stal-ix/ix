c628ab8599f36fd6cf63f44b7ffda8e09f30bb82
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-09-05 18:11:28 +0300

    upver

--- pkgs/lib/sqlite/3/ix.sh
+++ pkgs/lib/sqlite/3/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://www.sqlite.org/2022/sqlite-autoconf-3390200.tar.gz
-sha:852be8a6183a17ba47cee0bbff7400b7aa5affd283bf3beefc34fcd088a239de
+https://www.sqlite.org/2022/sqlite-autoconf-3390300.tar.gz
+sha:7868fb3082be3f2cf4491c6fba6de2bddcbc293a35fefb0624ee3c13f01422b9
 {% endblock %}
 
 {% block lib_deps %}
