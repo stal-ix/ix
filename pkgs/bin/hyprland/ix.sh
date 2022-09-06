9cd357d99c68bed1ab4f95fe4b1c99e1a4acdfc0
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-09-07 02:11:11 +0300

    upver

--- pkgs/bin/hyprland/ix.sh
+++ pkgs/bin/hyprland/ix.sh
@@ -1,8 +1,8 @@
-{% extends '//die/c/cmake.sh' %}
+{% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://github.com/hyprwm/Hyprland/archive/refs/tags/v0.9.0beta.tar.gz
-sha:3027593887bb4247196b0cb5a6e803e6dbbbc3f725e0fdbecc904596a05c02ac
+https://github.com/hyprwm/Hyprland/releases/download/v0.12.1beta/source-v0.12.1beta.tar.gz
+sha:be9220878e65e1fbf1ff7e9b4d6c3f4c3386198b458f78cc874576fdb8f65e7a
 {% endblock %}
 
 {% block bld_libs %}
@@ -20,20 +20,5 @@ lib/mesa/glesv2/dl
 {% endblock %}
 
 {% block bld_tool %}
-bld/make
 bld/wayland
-{% endblock %}
-
-{% block patch %}
-sed -e 's|.*X11.*||' -i src/includes.hpp
-sed -e 's| xcb||' -i CMakeLists.txt
-{% endblock %}
-
-{% block cmake_flags %}
-NO_XWAYLAND=ON
-{% endblock %}
-
-{% block build %}
-make ext-workspace-unstable-v1-protocol.o
-{{super()}}
 {% endblock %}
