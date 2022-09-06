ebaa2f11e6b48972dbc368b3b9e1226515c10e13
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-09-07 02:19:30 +0300

    upver

--- pkgs/lib/gtk/4/ix.sh
+++ pkgs/lib/gtk/4/ix.sh
@@ -1,8 +1,10 @@
 {% extends '//lib/gtk/t/ix.sh' %}
 
 {% block fetch %}
-https://gitlab.gnome.org/GNOME/gtk/-/archive/4.6.7/gtk-4.6.7.tar.bz2
-sha:3dc66b6844fad3f1a19ab1898ab7103e00487e26f94758f385fe80a117062fa3
+#https://gitlab.gnome.org/GNOME/gtk/-/archive/4.6.7/gtk-4.6.7.tar.bz2
+#sha:3dc66b6844fad3f1a19ab1898ab7103e00487e26f94758f385fe80a117062fa3
+https://gitlab.gnome.org/GNOME/gtk/-/archive/4.8.0/gtk-4.8.0.tar.bz2
+sha:6d9a79bd1c3f907bef1dd1169d71066e41e6ca911b327f46ab94229f7a50788a
 {% endblock %}
 
 {% block lib_deps %}
