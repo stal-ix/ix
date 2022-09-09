6b158297e08a2349630768f1e7d9476104513b3c
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-09-09 17:44:10 +0300

    upver

--- pkgs/bin/fdupes/ix.sh
+++ pkgs/bin/fdupes/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://github.com/adrianlopezroche/fdupes/archive/refs/tags/v2.2.0.tar.gz
-sha:8f02e6d855c56f38afdac9ff5ae2becd79cf7b87c31aa7bf9e3620d1bac00bab
+https://github.com/adrianlopezroche/fdupes/archive/refs/tags/v2.2.1.tar.gz
+sha:7d12e9d28c6a5dc46ff3a37fafd65b4b84f5c19dddf630beec8d67bc158fa265
 {% endblock %}
 
 {% block bld_libs %}
