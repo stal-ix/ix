5516147ebc1a334d77ef0be54f59ee2079b6da52
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-09-10 21:49:04 +0000

    upver

--- pkgs/lib/mad/ix.sh
+++ pkgs/lib/mad/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/tenacityteam/libmad/archive/refs/tags/0.16.2.tar.gz
-sha:6c14c2f4dcf7fd32e447eef99d6c53faded52e22f95e223e6f58844030851b99
+https://github.com/tenacityteam/libmad/archive/refs/tags/0.16.3.tar.gz
+sha:83ca48db60f480ae22234bae08e6ad651adec2667a68ad2df6fd61e6a50a32c7
 {% endblock %}
 
 {% block lib_deps %}
