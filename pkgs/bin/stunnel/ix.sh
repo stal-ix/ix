fa2079f30735b481c68a4535fd2865a25328be9c
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-09-12 20:46:50 +0000

    upver

--- pkgs/bin/stunnel/ix.sh
+++ pkgs/bin/stunnel/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
-https://www.stunnel.org/downloads/stunnel-5.65.tar.gz
-sha:60c500063bd1feff2877f5726e38278c086f96c178f03f09d264a2012d6bf7fc
+https://www.stunnel.org/downloads/stunnel-5.66.tar.gz
+sha:558178704d1aa5f6883aac6cc5d6bbf2a5714c8a0d2e91da0392468cee9f579c
 {% endblock %}
 
 {% block bld_libs %}
