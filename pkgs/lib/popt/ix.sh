2e22b0c977b48d99bc4ea0085666e24d21d70032
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-09-16 15:17:41 +0300

    upver

--- pkgs/lib/popt/ix.sh
+++ pkgs/lib/popt/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-http://ftp.rpm.org/popt/releases/popt-1.x/popt-1.18.tar.gz
-sha:5159bc03a20b28ce363aa96765f37df99ea4d8850b1ece17d1e6ad5c24fdc5d1
+http://ftp.rpm.org/popt/releases/popt-1.x/popt-1.19.tar.gz
+sha:c25a4838fc8e4c1c8aacb8bd620edb3084a3d63bf8987fdad3ca2758c63240f9
 {% endblock %}
 
 {% block lib_deps %}
