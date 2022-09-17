3a53f1a5e42927ae2bd1f415bb1c8b3ac9341e9e
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-09-17 23:24:14 +0300

    upver

--- pkgs/bin/acpid/2/ix.sh
+++ pkgs/bin/acpid/2/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://downloads.sourceforge.net/project/acpid2/acpid-2.0.33.tar.xz
-sha:0856f71b3eb34a1b663d0a8e6363dfcbc519e63d847330498898658e2972dbe8
+https://downloads.sourceforge.net/project/acpid2/acpid-2.0.34.tar.xz
+sha:2d095c8cfcbc847caec746d62cdc8d0bff1ec1bc72ef7c674c721e04da6ab333
 {% endblock %}
 
 {% block bld_libs %}
