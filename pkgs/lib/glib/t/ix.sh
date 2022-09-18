cb2e63a02e986bd954f96e865dd4e511a92c040b
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-09-18 05:24:54 +0300

    upver

--- pkgs/lib/glib/t/ix.sh
+++ pkgs/lib/glib/t/ix.sh
@@ -1,10 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-#https://gitlab.gnome.org/GNOME/glib/-/archive/2.73.3/glib-2.73.3.tar.bz2
-#sha:36bc72280e27f84e454499aa1e6c14d1f1b85bbab2c1803c2dd46fde1877c22f
-https://download.gnome.org/sources/glib/2.73/glib-2.73.3.tar.xz
-sha:df1a2b841667d6b48b2ef6969ebda4328243829f6e45866726f806f90f64eead
+https://download.gnome.org/sources/glib/2.74/glib-2.74.0.tar.xz
+sha:3652c7f072d7b031a6b5edd623f77ebc5dcd2ae698598abcc89ff39ca75add30
 {% endblock %}
 
 {% block lib_deps %}
@@ -36,7 +34,9 @@ ${PWD}/inc
 {% endblock %}
 
 {% block patch %}
-sed -e 's|.*static_assert.*||' -i gio/gio-launch-desktop.c
+sed -e 's|.*static_assert.*||'   \
+    -e 's|.*G_STATIC_ASSERT.*||' \
+    -i gio/gio-launch-desktop.c
 {% endblock %}
 
 {% block meson_flags %}
