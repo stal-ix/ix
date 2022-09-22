42a49f3ca375e1a70af33050a192286b66c872ec
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-09-22 21:20:57 +0300

    upver

--- pkgs/lib/vulkan/loader/t/ix.sh
+++ pkgs/lib/vulkan/loader/t/ix.sh
@@ -1,8 +1,10 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/KhronosGroup/Vulkan-Loader/archive/refs/tags/sdk-1.3.211.0.tar.gz
-sha:bb42a8e0dda103f98bddb9ae2311ae8e8e988588b600a1114b74a1ba3a0843f4
+#https://github.com/KhronosGroup/Vulkan-Loader/archive/refs/tags/sdk-1.3.211.0.tar.gz
+#sha:bb42a8e0dda103f98bddb9ae2311ae8e8e988588b600a1114b74a1ba3a0843f4
+https://github.com/KhronosGroup/Vulkan-Loader/archive/refs/tags/sdk-1.3.224.1.tar.gz
+sha:4d54b1489faa42d309e5d1e34d6655a9587ad988e99bb2a2ce0e357844f2cb2d
 {% endblock %}
 
 {% block lib_deps %}
@@ -19,6 +21,11 @@ bld/pkg/config
 BUILD_WSI_XCB_SUPPORT=NO
 BUILD_WSI_XLIB_SUPPORT=NO
 BUILD_STATIC_LOADER=YES
+{% endblock %}
+
+{% block build_flags %}
+# werror
+shut_up
 {% endblock %}
 
 {% block patch %}
