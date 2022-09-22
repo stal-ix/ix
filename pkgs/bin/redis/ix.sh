cd003ce9ff940cc253ed6e90ba4289556223a031
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-09-22 07:06:37 +0000

    upver

--- pkgs/bin/redis/ix.sh
+++ pkgs/bin/redis/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/make.sh' %}
 
 {% block fetch %}
-https://github.com/redis/redis/archive/refs/tags/7.0.4.tar.gz
-sha:1eeacd656e6b6e45aee3c4037dd098932979d3853220bbeb84cb35ca7ef6d2ca
+https://github.com/redis/redis/archive/refs/tags/7.0.5.tar.gz
+sha:40827fcaf188456ad9b3be8e27a4f403c43672b6bb6201192dc15756af6f1eae
 {% endblock %}
 
 {% block bld_libs %}
