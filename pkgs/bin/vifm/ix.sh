fa685f815256f3b8f766841a19b8f817950e6ab1
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-09-22 07:18:46 +0000

    upver

--- pkgs/bin/vifm/ix.sh
+++ pkgs/bin/vifm/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
-https://github.com/vifm/vifm/archive/refs/tags/v0.12.tar.gz
-sha:f145896c1a4237d6aa160e8616f4c600f276729a17958b6a1cf5186276815395
+https://github.com/vifm/vifm/archive/refs/tags/v0.12.1.tar.gz
+sha:410b2f183e9fdefb145cf6811c106e9297c9d8f7ddfe38f8840869c63e089f95
 {% endblock %}
 
 {% block bld_libs %}
