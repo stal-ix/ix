4c51321122a8806071ca7dd455d4ff712d36bb11
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-09-26 23:34:39 +0300

    upver

--- pkgs/bin/bash/5/t/ix.sh
+++ pkgs/bin/bash/5/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//bin/bash/t/ix.sh' %}
 
 {% block fetch %}
-https://ftp.gnu.org/gnu/bash/bash-5.1.16.tar.gz
-sha:5bac17218d3911834520dad13cd1f85ab944e1c09ae1aba55906be1f8192f558
+https://ftp.gnu.org/gnu/bash/bash-5.2.tar.gz
+sha:a139c166df7ff4471c5e0733051642ee5556c1cc8a4a78f145583c5c81ab32fb
 {% endblock %}
 
 {% block patch %}
