8001f673c08701a82117cca5c5410a3eff43c5bf
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-09-27 04:21:05 +0300

    fix build

--- pkgs/bin/gdb/ix.sh
+++ pkgs/bin/gdb/ix.sh
@@ -1,1 +1,6 @@
 {% extends '//bin/gdb/t/ix.sh' %}
+
+{% block patch %}
+{{super()}}
+sed -e 's|return rl_completer_word_break_characters|return (char*)rl_completer_word_break_characters|' -i gdb/completer.c
+{% endblock %}
