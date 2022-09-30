7dfeae9a13f77fff30e3adb8f7624bcfbf1ec137
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-09-30 20:21:02 +0300

    upver

--- pkgs/lib/ebml/ix.sh
+++ pkgs/lib/ebml/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/Matroska-Org/libebml/archive/refs/tags/release-1.4.2.tar.gz
-sha:66486742fd4f443553ad1917505208404c8c4240c0ab26cedaf41d9476efc665
+https://github.com/Matroska-Org/libebml/archive/refs/tags/release-1.4.3.tar.gz
+sha:bcd22b03a197918eb994b310b45b5eed2644763010444159bc8738f9432354ca
 {% endblock %}
 
 {% block lib_deps %}
