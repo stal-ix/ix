d1c051315a055cf6ed2cf01713f624072cf4bf93
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-10-03 00:25:29 +0300

    fix

--- pkgs/bld/qt/6/tools/qml/ix.sh
+++ pkgs/bld/qt/6/tools/qml/ix.sh
@@ -1,11 +1,8 @@
 {% extends 't/ix.sh' %}
 
 {% block cmake_flags %}
+{{super()}}
 QT_FORCE_FIND_TOOLS=ON
-QT_FORCE_BUILD_TOOLS=ON
-QT_WILL_BUILD_TOOLS=ON
-QT_BUILD_TOOLS_WHEN_CROSSCOMPILING=ON
-QT_FEATURE_cross_compile=OFF
 {% endblock %}
 
 {% block bld_tool %}
