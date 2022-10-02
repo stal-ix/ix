73019b4dfa6e5a3b1b2232b31db3c0b9ac54449b
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-10-03 01:12:08 +0300

    upver

--- pkgs/bin/nvtop/ix.sh
+++ pkgs/bin/nvtop/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/Syllo/nvtop/archive/refs/tags/2.0.3.tar.gz
-sha:ecf18c55e513fca672426cc9a3ea9ac888b4061be2cca455f484b681a1435518
+https://github.com/Syllo/nvtop/archive/refs/tags/2.0.4.tar.gz
+sha:14687353e0cc561c87200b9409aa62d9cb26a4c75fbdbdd0925cf4c1297a279f
 {% endblock %}
 
 {% block bld_libs %}
