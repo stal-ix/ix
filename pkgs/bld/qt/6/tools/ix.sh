96ee8bb5277b0411ccecd38dc6c3c0180e4a0774
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-10-02 05:46:44 +0300

    upver

--- pkgs/bld/qt/6/tools/ix.sh
+++ pkgs/bld/qt/6/tools/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/qt.sh' %}
 
 {% block fetch %}
-https://github.com/qt/qttools/archive/refs/tags/v6.3.1.tar.gz
-sha:36cda60947c37f1abd7ef9755207fa5c5b979176d97bc3b91171937a7870b937
+https://github.com/qt/qttools/archive/refs/tags/v6.4.0.tar.gz
+sha:b408ed50b0523ee9cc8fab94db2cd73e7ac693605764b12ec251c99563877ff2
 {% endblock %}
 
 {% block bld_libs %}
--- pkgs/bld/qt/6/tools/qml/ix.sh
+++ pkgs/bld/qt/6/tools/qml/ix.sh
@@ -1,5 +1,24 @@
 {% extends '//lib/qt/6/declarative/t/ix.sh' %}
 
+{% block cmake_flags %}
+{{super()}}
+QT_FEATURE_qml_animation=OFF
+QT_FEATURE_qml_debug=OFF
+QT_FEATURE_qml_delegate_model=OFF
+QT_FEATURE_qml_devtools=OFF
+QT_FEATURE_qml_itemmodel=OFF
+QT_FEATURE_qml_jit=OFF
+QT_FEATURE_qml_list_model=OFF
+QT_FEATURE_qml_locale=OFF
+QT_FEATURE_qml_network=OFF
+QT_FEATURE_qml_object_model=OFF
+QT_FEATURE_qml_profiler=OFF
+QT_FEATURE_qml_table_model=OFF
+QT_FEATURE_qml_worker_script=ON
+QT_FEATURE_qml_xml_http_request=OFF
+QT_FEATURE_qml_xmllistmodel=OFF
+{% endblock %}
+
 {% block env %}
 export Qt6QmlTools_DIR=${out}/lib/cmake/Qt6QmlTools
 {% endblock %}
