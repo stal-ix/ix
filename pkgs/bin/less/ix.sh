09283e1186260698235cdd7e822f48e0ef9b538a
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-10-03 19:58:03 +0300

    upver

--- pkgs/bin/less/ix.sh
+++ pkgs/bin/less/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://www.greenwoodsoftware.com/less/less-598.tar.gz
-md5:fcd2c9baa3c8f4ece2807c2c8bae5241
+https://www.greenwoodsoftware.com/less/less-608.tar.gz
+sha:a69abe2e0a126777e021d3b73aa3222e1b261f10e64624d41ec079685a6ac209
 {% endblock %}
 
 {% block bld_libs %}
