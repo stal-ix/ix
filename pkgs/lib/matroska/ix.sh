2432e718b4f5f9089bfa0a0eb8205c9f5ebce028
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-10-09 01:20:16 +0300

    upver

--- pkgs/lib/matroska/ix.sh
+++ pkgs/lib/matroska/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/Matroska-Org/libmatroska/archive/refs/tags/release-1.7.0.tar.gz
-sha:47f6ec7fbb32af8b2f8b640c49faac755fd376d732b3fdb68ad802fe5420ee88
+https://github.com/Matroska-Org/libmatroska/archive/refs/tags/release-1.7.1.tar.gz
+sha:64763443947833e6c17f1f555f4bb0df6c9f91881810d9d5e0f0bad3622d308b
 {% endblock %}
 
 {% block lib_deps %}
