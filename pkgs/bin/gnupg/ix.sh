2f58a8c7800d76df7ff6b2aa5cc9570d4ba7b192
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-10-13 23:46:53 +0300

    upver

--- pkgs/bin/gnupg/ix.sh
+++ pkgs/bin/gnupg/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
-https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-2.3.7.tar.bz2
-sha:ee163a5fb9ec99ffc1b18e65faef8d086800c5713d15a672ab57d3799da83669
+https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-2.3.8.tar.bz2
+sha:540b7a40e57da261fb10ef521a282e0021532a80fd023e75fb71757e8a4969ed
 {% endblock %}
 
 {% block bld_libs %}
