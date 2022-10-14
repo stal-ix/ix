72568445ab7d6c5965a05c920a5259b1908450be
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-10-14 15:58:33 +0300

    upver

--- pkgs/lib/wacom/t/ix.sh
+++ pkgs/lib/wacom/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://github.com/linuxwacom/libwacom/releases/download/libwacom-2.4.0/libwacom-2.4.0.tar.xz
-sha:d0d022761e3f9ab23e329583b7d2bd470b0450dfb077caeb22c5a0d66c2bd414
+https://github.com/linuxwacom/libwacom/releases/download/libwacom-2.5.0/libwacom-2.5.0.tar.xz
+sha:40b69a4c38bbcbc80b1231bc115551107ebbc0ba14d2ad1c3e54355dcd876816
 {% endblock %}
 
 {% block lib_deps %}
