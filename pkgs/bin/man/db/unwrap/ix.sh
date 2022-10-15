6726c8d959bf10e6c635878036d2c6267bd6e9c0
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-10-16 00:00:15 +0300

    buildable

--- pkgs/bin/man/db/unwrap/ix.sh
+++ pkgs/bin/man/db/unwrap/ix.sh
@@ -1,14 +1,26 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://gitlab.com/cjwatson/man-db/-/archive/2.11.0/man-db-2.11.0.tar.bz2
-sha:3a12924927e4cbe03d04f204b89fa870866efbe7484decb05e46ed2601c8a366
+http://download.savannah.nongnu.org/releases/man-db/man-db-2.10.2.tar.xz
+sha:ee97954d492a13731903c9d0727b9b01e5089edbd695f0cdb58d405a5af5514d
 {% endblock %}
 
 {% block bld_libs %}
 lib/c
 lib/z
 lib/gdbm
+lib/linux
 lib/seccomp
 lib/pipeline
+{% endblock %}
+
+{% block bld_tool %}
+bin/groff
+bld/gettext
+{% endblock %}
+
+{% block configure_flags %}
+--disable-setuid
+--with-systemdtmpfilesdir=${out}/tmp
+--with-systemdsystemunitdir=${out}/share
 {% endblock %}
