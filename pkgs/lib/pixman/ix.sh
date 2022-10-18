0a0f9a996f303e7e016a778a5dab0a2b460e0815
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-10-18 15:18:35 +0300

    upver

--- pkgs/lib/pixman/ix.sh
+++ pkgs/lib/pixman/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://www.cairographics.org/releases/pixman-0.40.0.tar.gz
-sha:6d200dec3740d9ec4ec8d1180e25779c00bc749f94278c8b9021f5534db223fc
+https://www.cairographics.org/releases/pixman-0.42.0.tar.gz
+sha:07f74c8d95e4a43eb2b08578b37f40b7937e6c5b48597b3a0bb2c13a53f46c13
 {% endblock %}
 
 {% block lib_deps %}
