a3bb0537c9c648410ec4501b41cf915af5e47624
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-10-18 15:16:06 +0300

    upver

--- pkgs/lib/pciaccess/ix.sh
+++ pkgs/lib/pciaccess/ix.sh
@@ -1,10 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-#https://gitlab.freedesktop.org/xorg/lib/libpciaccess/-/archive/libpciaccess-0.16/libpciaccess-libpciaccess-0.16.tar.bz2
-#sha:8a86df9cfbdc5527e95f789eee0212928324a69bc6ca69d24d0a5cf8744cb467
-https://github.com/freedesktop/xorg-libpciaccess/archive/refs/tags/libpciaccess-0.16.tar.gz
-sha:eed3edf8080d325bf9ac046969cd085db9e85f6f06c3ad255f615e06ff47b6de
+https://github.com/freedesktop/xorg-libpciaccess/archive/refs/tags/libpciaccess-0.17.tar.gz
+sha:16086781a9e7dd88de51ecbcee5f7bb904e037aa46982a5ec42b52f4cf3dc4b6
 {% endblock %}
 
 {% block lib_deps %}
