98b0a7a0266fe558b675e3fc436728518e2bbcd8
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-10-18 20:34:05 +0300

    upver

--- pkgs/lib/de265/t/ix.sh
+++ pkgs/lib/de265/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://github.com/strukturag/libde265/archive/refs/tags/v1.0.8.tar.gz
-md5:5abe0f3d4377af32e17e94cf15dde306
+https://github.com/strukturag/libde265/archive/refs/tags/v1.0.9.tar.gz
+sha:153554f407718a75f1e0ae197d35b43147ce282118a54f894554dbe27c32163d
 {% endblock %}
 
 {% block lib_deps %}
