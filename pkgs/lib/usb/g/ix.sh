02824bdfe3e53cf7ee3de4dd0a5a1bf471932a7a
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-10-19 20:28:53 +0300

    upver

--- pkgs/lib/usb/g/ix.sh
+++ pkgs/lib/usb/g/ix.sh
@@ -1,14 +1,15 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://github.com/hughsie/libgusb/archive/refs/tags/0.3.10.tar.gz
-sha:e699b06f4809f9d166b13dc9f5812b0dd026591d3cae90ec5483aa2687f3b80b
+https://github.com/hughsie/libgusb/archive/refs/tags/0.4.2.tar.gz
+sha:01a99aec431cc5e80979f8aad11e0e8f1aac3fd8504d3c2f2f2c0956bc35837b
 {% endblock %}
 
 {% block lib_deps %}
 lib/c
 lib/usb
 lib/glib
+lib/json/glib
 {% endblock %}
 
 {% block meson_flags %}
