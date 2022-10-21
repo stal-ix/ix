9ff50472f975a9949a52becd6d346ddb1bc816df
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-10-21 17:48:38 +0300

    upver

--- pkgs/bin/rsync/ix.sh
+++ pkgs/bin/rsync/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://download.samba.org/pub/rsync/src/rsync-3.2.6.tar.gz
-sha:fb3365bab27837d41feaf42e967c57bd3a47bc8f10765a3671efd6a3835454d3
+https://download.samba.org/pub/rsync/src/rsync-3.2.7.tar.gz
+sha:4e7d9d3f6ed10878c58c5fb724a67dacf4b6aac7340b13e488fb2dc41346f2bb
 {% endblock %}
 
 {% block bld_libs %}
