9604e3ff2c77ab01f4208ccc3debd890340ba38f
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-10-22 20:08:44 +0300

    upver

--- pkgs/bin/loksh/ix.sh
+++ pkgs/bin/loksh/ix.sh
@@ -1,12 +1,13 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://github.com/dimkr/loksh/releases/download/7.0/loksh-7.0.tar.xz
-sha:413275c9a259629bbdb4ce97c610c45bc14a589f73cd51571ed82eec58e45840
+https://github.com/dimkr/loksh/releases/download/7.2/loksh-7.2.tar.xz
+sha:0083df24b3916c2f34ab63f8b4de6720ba9799e0e9d139c4b2d4ae41ad6aa53d
 {% endblock %}
 
 {% block lib_deps %}
 lib/c
+lib/curses
 {% endblock %}
 
 {% block c_rename_symbol %}
