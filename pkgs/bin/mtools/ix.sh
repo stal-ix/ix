341795f1afc5b9308747524787e1abb10a6fcc7b
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-10-22 22:27:42 +0300

    upver

--- pkgs/bin/mtools/ix.sh
+++ pkgs/bin/mtools/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-http://ftp.gnu.org/gnu/mtools/mtools-4.0.41.tar.lz
-sha:e0425e206c1d7faebf9c151d1b55c66385e05af752266525602ce3cd2779c9f9
+http://ftp.gnu.org/gnu/mtools/mtools-4.0.42.tar.lz
+sha:40a98fd9bcc865997866015013c3c64a304d98c2892fe4ab892ffd6ac3dd9bae
 {% endblock %}
 
 {% block bld_libs %}
