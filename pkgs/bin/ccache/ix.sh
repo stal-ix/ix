7ca39e7de06595116e9c0416c981d53309cd2d2a
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-10-23 12:49:30 +0300

    upver

--- pkgs/bin/ccache/ix.sh
+++ pkgs/bin/ccache/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/ccache/ccache/releases/download/v4.7/ccache-4.7.tar.xz
-sha:d9283bbe22085268f52d309b91adb81a77a7abe10867eb6852057910ffac736a
+https://github.com/ccache/ccache/releases/download/v4.7.1/ccache-4.7.1.tar.xz
+sha:04ccc746524cb7fb914ccdfc66e7c54504204afee0e270ac75e8f66196521b6d
 {% endblock %}
 
 {% block bld_libs %}
@@ -13,4 +13,9 @@ lib/hiredis
 
 {% block build_flags %}
 wrap_cc
+{% endblock %}
+
+{% block cmake_flags %}
+ENABLE_DOCUMENTATION=OFF
+ENABLE_TESTING=OFF
 {% endblock %}
