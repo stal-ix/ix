c772003fd27479df751b73b29d3c2c48a03c3bc3
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-10-23 12:44:32 +0300

    upver

--- pkgs/bin/graphviz/5/ix.sh
+++
@@ -1,6 +0,0 @@
-{% extends '//bin/graphviz/4/ix.sh' %}
-
-{% block fetch %}
-https://gitlab.com/graphviz/graphviz/-/archive/5.0.1/graphviz-5.0.1.tar.bz2
-sha:c01a174a8f468f05ac5ff17a1923e51275e332a95b7f549552593c46091b27a3
-{% endblock %}
--- pkgs/bin/graphviz/ix.sh
+++ pkgs/bin/graphviz/ix.sh
@@ -1,5 +1,6 @@
-{% extends '//die/hub.sh' %}
+{% extends '//bin/graphviz/4/ix.sh' %}
 
-{% block run_deps %}
-bin/graphviz/6
+{% block fetch %}
+https://gitlab.com/graphviz/graphviz/-/archive/7.0.0/graphviz-7.0.0.tar.bz2
+sha:380c084c1fac55f442ba0ae1253b24c33b3855cf519ac490975b8572b9893153
 {% endblock %}
