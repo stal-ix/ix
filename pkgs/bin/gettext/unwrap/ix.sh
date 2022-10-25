fb280c4494d7099f9e5ddd386f17cbb400e7a7e2
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-10-26 00:51:22 +0300

    upver

--- pkgs/bin/gettext/unwrap/ix.sh
+++ pkgs/bin/gettext/unwrap/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://ftp.gnu.org/pub/gnu/gettext/gettext-0.21.tar.gz
-sha:c77d0da3102aec9c07f43671e60611ebff89a996ef159497ce8e59d075786b12
+https://ftp.gnu.org/pub/gnu/gettext/gettext-0.21.1.tar.gz
+sha:e8c3650e1d8cee875c4f355642382c1df83058bd5a11ee8555c0cf276d646d45
 {% endblock %}
 
 {% block purge_autohell %}configure{% endblock %}
