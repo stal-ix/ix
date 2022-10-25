e8c80b8f14fe6b349491eb9fa165d7a36f6ad613
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-10-26 01:03:34 +0300

    upver

--- pkgs/bin/swig/4/ix.sh
+++ pkgs/bin/swig/4/ix.sh
@@ -1,14 +1,14 @@
 {% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
-http://prdownloads.sourceforge.net/swig/swig-4.0.2.tar.gz
-md5:7c3e46cb5af2b469722cafa0d91e127b
+http://prdownloads.sourceforge.net/swig/swig-4.1.0.tar.gz
+sha:d6a9a8094e78f7cfb6f80a73cc271e1fe388c8638ed22668622c2c646df5bb3d
 {% endblock %}
 
 {% block bld_libs %}
 lib/c
 lib/c++
-lib/pcre
+lib/pcre/2
 {% endblock %}
 
 {% block bld_tool %}
--- pkgs/lib/gdk/pixbuf/ix.sh
+++ pkgs/lib/gdk/pixbuf/ix.sh
@@ -1,7 +1,7 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-# TODO(pg): 2.42.9 broke gtk3 build
+# TODO(pg): 2.42.9-10 broke gtk3 build
 https://download.gnome.org/sources/gdk-pixbuf/2.42/gdk-pixbuf-2.42.6.tar.xz
 sha:c4a6b75b7ed8f58ca48da830b9fa00ed96d668d3ab4b1f723dcf902f78bde77f
 {% endblock %}
