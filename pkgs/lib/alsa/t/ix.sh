a2499a4975f1038489dfaf6ea146363380722483
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-10-26 01:16:32 +0300

    upver

--- pkgs/lib/alsa/t/ix.sh
+++ pkgs/lib/alsa/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://www.alsa-project.org/files/pub/lib/alsa-lib-1.2.7.2.tar.bz2
-sha:8a35b7218e50f2a2c79342d0de98ded81439ce19e12809385ec9be9596de7c2f
+https://www.alsa-project.org/files/pub/lib/alsa-lib-1.2.8.tar.bz2
+sha:1ab01b74e33425ca99c2e36c0844fd6888273193bd898240fe8f93accbcbf347
 {% endblock %}
 
 {% block lib_deps %}
