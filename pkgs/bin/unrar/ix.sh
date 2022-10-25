7f12f2958acab8eea76b7da447b43cb9a6cecb52
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-10-26 01:04:39 +0300

    upver

--- pkgs/bin/unrar/ix.sh
+++ pkgs/bin/unrar/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/make.sh' %}
 
 {% block fetch %}
-https://www.rarlab.com/rar/unrarsrc-6.1.7.tar.gz
-sha:de75b6136958173fdfc530d38a0145b72342cf0d3842bf7bb120d336602d88ed
+https://www.rarlab.com/rar/unrarsrc-6.2.1.tar.gz
+sha:5cc8f7ded262d27c29d01e7a119d2fd23edda427711820454f2eb667044a8900
 {% endblock %}
 
 {% block bld_libs %}
