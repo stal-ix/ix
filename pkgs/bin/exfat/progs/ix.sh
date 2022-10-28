300f677e0dfa4714fa507c51dc6a6fdc625bb4f8
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-10-28 22:02:41 +0300

    upver

--- pkgs/bin/exfat/progs/ix.sh
+++ pkgs/bin/exfat/progs/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://github.com/exfatprogs/exfatprogs/releases/download/1.1.3/exfatprogs-1.1.3.tar.xz
-sha:3dbd74b60eec18d2c68d57be4a872d7baa4f7d848e669e31159452ef5245b809
+https://github.com/exfatprogs/exfatprogs/releases/download/1.2.0/exfatprogs-1.2.0.tar.xz
+sha:56d9a49465deafc367d428afc71c8098705a30ee19a3cdf3c5320650b8880742
 {% endblock %}
 
 {% block bld_libs %}
