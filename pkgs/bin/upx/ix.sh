9f88560b7177e0bfea654206ff69c7413d8383b5
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-10-29 19:39:27 +0300

    upver

--- pkgs/bin/upx/ix.sh
+++ pkgs/bin/upx/ix.sh
@@ -1,35 +1,11 @@
-{% extends '//die/c/make.sh' %}
+{% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/upx/upx/releases/download/v3.96/upx-3.96-src.tar.xz
-sha:47774df5c958f2868ef550fb258b97c73272cb1f44fe776b798e393465993714
+https://github.com/upx/upx/releases/download/v4.0.0/upx-4.0.0-src.tar.xz
+sha:eaf938f8824e65f06852142fda97f8349ad660612979fa311618109c819f36ea
 {% endblock %}
 
 {% block bld_libs  %}
-lib/z
 lib/c
 lib/c++
-lib/ucl/codec
-{% endblock %}
-
-{% block bld_tool %}
-bld/bash
-bld/perl
-{% endblock %}
-
-{% block unpack %}
-{{super()}}
-cd src
-{% endblock %}
-
-{% block make_flags %}
-CHECK_WHITESPACE=true
-UPX_LZMA_VERSION=0x465
-UPX_LZMADIR=${PWD}
-CXXFLAGS_WERROR=
-{% endblock %}
-
-{% block install %}
-mkdir ${out}/bin
-cp upx.out ${out}/bin/upx
 {% endblock %}
