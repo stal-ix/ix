0a90a4c18cc2d67cd06acf43afde1ae0b927043d
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-10-29 19:25:23 +0300

    upver

--- pkgs/lib/msh3/ix.sh
+++ pkgs/lib/msh3/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/nibanks/msh3/archive/refs/tags/v0.2.0.tar.gz
-sha:9b46bad7a577b958d0da54d3da9fa266550d44ed5d3a34fb7e39b1f505e5f955
+https://github.com/nibanks/msh3/archive/refs/tags/v0.4.0.tar.gz
+sha:07eaa26a027e60cbf16576741706b81fcbd7e68cfb5e2cc517211450b5b036c9
 {% endblock %}
 
 {% block lib_deps %}
