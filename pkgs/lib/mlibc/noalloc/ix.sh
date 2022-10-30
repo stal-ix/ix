8bccdc6dab3fd1bdee0662cad6106e8c66b7efe4
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-10-30 21:36:02 +0300

    better

--- pkgs/lib/mlibc/noalloc/ix.sh
+++ pkgs/lib/mlibc/noalloc/ix.sh
@@ -1,5 +1,10 @@
 {% extends '//lib/mlibc/pure/ix.sh' %}
 
+{% block bld_tool %}
+{{super()}}
+bld/python
+{% endblock %}
+
 {% block patch %}
 {{super()}}
 
@@ -10,4 +15,20 @@ EOF
 cat << EOF > options/internal/generic/allocator.cpp
 {% include 'allocator.cpp' %}
 EOF
+
+cat << EOF > run.py
+import sys
+
+skip = False
+
+for l in sys.stdin.read().split('\n'):
+    if 'void free' in l:
+        skip = True
+
+    if not skip:
+        print(l)
+EOF
+
+cat options/ansi/generic/stdlib-stubs.cpp | python3 ./run.py > _
+mv _ options/ansi/generic/stdlib-stubs.cpp
 {% endblock %}
