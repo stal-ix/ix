04917ad7ef1843de987f1ef27cb26d1f0260898f
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-10-31 11:51:53 +0300

    upver

--- pkgs/lib/utf8/proc/ix.sh
+++ pkgs/lib/utf8/proc/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/JuliaStrings/utf8proc/archive/refs/tags/v2.7.0.tar.gz
-md5:11e307064ed630f6a99d825608a248e4
+https://github.com/JuliaStrings/utf8proc/archive/refs/tags/v2.8.0.tar.gz
+sha:a0a60a79fe6f6d54e7d411facbfcc867a6e198608f2cd992490e46f04b1bcecc
 {% endblock %}
 
 {% block lib_deps %}
