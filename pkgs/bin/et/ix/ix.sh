ee28e4e7ec1fd5a519d9f74a90d3fbb187f04eb1
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-01 12:53:37 +0300

    fix

--- pkgs/bin/et/ix/ix.sh
+++ pkgs/bin/et/ix/ix.sh
@@ -1,8 +1,29 @@
 {% extends '//bin/et/stock/ix.sh' %}
 
+{% block bld_libs %}
+{{super()}}
+lib/shim/ix
+{% endblock %}
+
 {% block cmake_flags %}
 {{super()}}
 DISABLE_SENTRY=ON
 DISABLE_TELEMETRY=ON
 DISABLE_CRASH_LOG=ON
+{% endblock %}
+
+{% block patch %}
+{{super()}}
+
+sed -e 's|GetTempDirectory|GetTempDirectory_xxx|' -i src/base/Headers.hpp
+
+cat << EOF >> src/base/Headers.hpp
+#pragma once
+
+#include <ix.h>
+
+inline string GetTempDirectory() {
+    return  ix_temp_dir();
+}
+EOF
 {% endblock %}
