6928e35bea2983f05534b6b0cd79fca6cc1fd9d0
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-02 20:38:45 +0300

    better

--- pkgs/lib/uring/ix.sh
+++ pkgs/lib/uring/ix.sh
@@ -1,5 +1,7 @@
 {% extends '//die/c/make.sh' %}
 
+{# 2.3 broken - conflict headers with linux #}
+
 {% block fetch %}
 https://github.com/axboe/liburing/archive/refs/tags/liburing-2.2.tar.gz
 sha:e092624af6aa244ade2d52181cc07751ac5caba2f3d63e9240790db9ed130bbc
@@ -7,9 +9,6 @@ sha:e092624af6aa244ade2d52181cc07751ac5caba2f3d63e9240790db9ed130bbc
 
 {% block lib_deps %}
 lib/c
-{% endblock %}
-
-{% block bld_libs %}
 lib/linux
 {% endblock %}
 
