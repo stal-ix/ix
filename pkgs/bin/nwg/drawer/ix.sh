89f0347a713999723cfb8de9cd47ef2f809d64ff
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-04 12:16:03 +0300

    fix

---
+++ pkgs/bin/nwg/drawer/ix.sh
@@ -0,0 +1,22 @@
+{% extends '//die/go/pure.sh' %}
+
+{% block go_url %}
+https://github.com/nwg-piotr/nwg-drawer/archive/refs/tags/v0.3.3.tar.gz
+{% endblock %}
+
+{% block go_sum %}
+sha:16485bd215e8e5967bb8ae68ff8b5ccfb752d9ccb8b1889a8dac24e7e6de48f9
+{% endblock %}
+
+{% block go_sha %}
+sha:eb99e5a3b99efb127969fe02ff684d2ff9eebefdefdec4b506377968af332677
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/gtk/3
+{% endblock %}
+
+{% block go_tags %}
+no_x11
+{% endblock %}
--- pkgs/lib/brotli/ix.sh
+++ pkgs/lib/brotli/ix.sh
@@ -1,1 +1,9 @@
 {% extends '//lib/brotli/t/ix.sh' %}
+
+{% block install %}
+{{super()}}
+cd ${out}/lib/pkgconfig
+for x in *.pc; do
+    sed -e 's|-R.* ||' -i ${x}
+done
+{% endblock %}
