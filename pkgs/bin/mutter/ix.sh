cc5b973d7880bc66ab8b36b79d5ded4290c0ec8e
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-04 21:13:27 +0300

    add

--- pkgs/bin/mutter/ix.sh
+++ pkgs/bin/mutter/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://gitlab.gnome.org/GNOME/mutter/-/archive/1f03a20939e05500d9d49c3078bb2839c2507fc7/mutter-1f03a20939e05500d9d49c3078bb2839c2507fc7.tar.bz2
-sha:0401241f6400046e40966a7775db2a11d5fdc1a8f147898cd290e3a2b1ae9132
+https://gitlab.gnome.org/GNOME/mutter/-/archive/43.1/mutter-43.1.tar.bz2
+sha:65e3fd39c94bdfb8334c7e7df17e1ce6f4119f91cbed774a637764e8a6b92e75
 {% endblock %}
 
 {# hard X11 dep #}
@@ -17,13 +17,16 @@ lib/input
 lib/wacom
 lib/cairo
 lib/pango
+lib/lcms/2
 lib/udev/g
 lib/opengl
+lib/colord
 lib/wayland
 lib/shim/x11
 lib/canberra
 lib/graphene
 lib/xkbcommon
+lib/json/glib
 lib/drivers/3d
 lib/mesa/gl/dl
 lib/vulkan/loader
@@ -34,8 +37,8 @@ lib/gsettings/desktop/schemas
 
 {% block bld_tool %}
 bld/gettext
-bld/glib/codegen
 bld/wayland
+bld/glib/codegen
 {% endblock %}
 
 {% block meson_flags %}
@@ -62,10 +65,9 @@ udev=false
 {% endblock %}
 
 {% block patch %}
-sed -e 's|have_x11 = true|have_x11 = false|' \
+sed -e 's|ice_dep = .*||' \
+    -e 's|have_x11 = .*|have_x11 = false|' \
     -e 's|have_xwayland = .*|have_xwayland = false|' \
-    -e 's|ice_dep = .*||' \
+    -e 's|have_x11_client = .*|have_x11_client = false|' \
     -i meson.build
-
-sed -e 's|.*include.*X11.*||' -i src/meta/common.h
 {% endblock %}
