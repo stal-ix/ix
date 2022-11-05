3449cb72b369ec5fcc86046281ceaf4bab898449
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-06 01:59:58 +0300

    unused

--- pkgs/bin/telegram/desktop/unwrap/clang/ix.sh
+++ pkgs/bin/telegram/desktop/unwrap/clang/ix.sh
@@ -4,23 +4,3 @@
 {{super()}}
 export CXXFLAGS="-std=c++20 ${CXXFLAGS}"
 {% endblock %}
-
-{% block patch1 %}
-{{super()}}
-
-base64 -d << EOF > Telegram/codegen/codegen/style/structure_types.cpp
-{% include 'structure_types.cpp/base64' %}
-EOF
-
-base64 -d << EOF > Telegram/codegen/codegen/style/structure_types.h
-{% include 'structure_types.h/base64' %}
-EOF
-
-base64 -d << EOF > Telegram/SourceFiles/passport/passport_panel_edit_scans.h
-{% include 'passport_panel_edit_scans.h/base64' %}
-EOF
-
-base64 -d << EOF > Telegram/SourceFiles/passport/passport_panel_edit_scans.cpp
-{% include 'passport_panel_edit_scans.cpp/base64' %}
-EOF
-{% endblock %}
