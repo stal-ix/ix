b54af634e38f6d9cad30b8cc5922465ba614d0ae
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-06 15:20:26 +0300

    add

---
+++ pkgs/bin/tea/ix.sh
@@ -0,0 +1,14 @@
+{% extends '//die/c/cmake.sh' %}
+
+{% block fetch %}
+https://github.com/psemiletov/tea-qt/archive/refs/tags/61.2.0.tar.gz
+sha:d93f93934d4f97cbd19e5312374168e4b673e11d753d58a5fcfa510ee863d2f6
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/c++
+lib/qt/6/base
+lib/qt/6/deps
+lib/qt/6/compat
+{% endblock %}
