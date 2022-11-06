2987419e7e868222e84855dfdd402aadf5d72e4c
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-06 20:38:13 +0300

    upver

---
+++ pkgs/bin/meson/64/ix.sh
@@ -0,0 +1,6 @@
+{% extends '//bin/meson/63/ix.sh' %}
+
+{% block fetch %}
+https://github.com/mesonbuild/meson/archive/refs/tags/0.64.0.tar.gz
+sha:6477993d781b6efea93091616a6d6a0766c0e026076dbeb11249bf1c9b49a347
+{% endblock %}
--- pkgs/bin/meson/better/ix.sh
+++ pkgs/bin/meson/better/ix.sh
@@ -1,4 +1,4 @@
-{% extends '//bin/meson/63/ix.sh' %}
+{% extends '//bin/meson/64/ix.sh' %}
 
 {% block patch %}
 sed -e 's|self._search_libs.*|[], []|' \
