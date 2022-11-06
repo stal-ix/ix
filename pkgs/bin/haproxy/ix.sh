0034ba149a7af2c90802be51612710ab898324e5
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-06 19:33:09 +0300

    better

--- pkgs/bin/haproxy/ix.sh
+++ pkgs/bin/haproxy/ix.sh
@@ -9,9 +9,17 @@ sha:ce9e19ebfcdd43e51af8a6090f1df8d512d972ddf742fa648a643bbb19056605
 lib/c
 lib/linux
 lib/pcre/2
+lib/xcrypt
+lib/openssl
+lib/execinfo
+lib/pcre/2/posix
 {% endblock %}
 
 {% block make_flags %}
 TARGET=linux-musl
 SBINDIR=${out}/bin
+USE_CRYPT=yes
+USE_PCRE2=yes
+USE_OPENSSL=yes
+USE_BACKTRACE=yes
 {% endblock %}
