3403ab753dfba94f644b212018fce35d5ab78aa8
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-06 16:25:12 +0300

    upver

--- pkgs/bin/rlwrap/ix.sh
+++ pkgs/bin/rlwrap/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://github.com/hanslub42/rlwrap/archive/refs/tags/v0.46.tar.gz
-sha:4bc53e96b49405e4a8ca579465bc1129adc719a96840b71b3ae99ec88e4d8c29
+https://github.com/hanslub42/rlwrap/archive/refs/tags/0.46.1.tar.gz
+sha:c537e8a53df36f1f996601d776203478ad56fab1d67b3c1a63057badb0851cec
 {% endblock %}
 
 {% block bld_libs %}
