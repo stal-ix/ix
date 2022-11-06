46a41820666770529a99d0f9cb68a8c96421f70f
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-06 23:47:54 +0300

    better

--- pkgs/bin/tea/ix.sh
+++ pkgs/bin/tea/ix.sh
@@ -8,7 +8,14 @@ sha:d93f93934d4f97cbd19e5312374168e4b673e11d753d58a5fcfa510ee863d2f6
 {% block bld_libs %}
 lib/c
 lib/c++
+lib/poppler
+lib/djvulibre
 lib/qt/6/base
 lib/qt/6/deps
 lib/qt/6/compat
+{% endblock %}
+
+{% block cmake_flags %}
+USE_DJVU=ON
+USE_PDF=ON
 {% endblock %}
