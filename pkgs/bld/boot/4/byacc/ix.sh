442d4069bbcc406cc119097d0efb76e7d7a8cf56
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-07 13:50:51 +0300

    better

--- pkgs/bld/boot/4/byacc/ix.sh
+++ pkgs/bld/boot/4/byacc/ix.sh
@@ -1,5 +1,10 @@
 {% extends '//bin/byacc/yacc/ix.sh' %}
 
+{% block fetch %}
+https://invisible-mirror.net/archives/byacc/byacc-20211224.tgz
+sha:7bc42867a095df2189618b64497016298818e88e513fca792cb5adc9a68ebfb8
+{% endblock %}
+
 {% block bld_libs %}
 {% endblock %}
 
