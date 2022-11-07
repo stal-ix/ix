52fe2d2cbaa9b635fbe265aabdb363559428f1e2
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-07 13:41:44 +0300

    upver

--- pkgs/bin/sed/ix.sh
+++ pkgs/bin/sed/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
-https://ftp.gnu.org/gnu/sed/sed-4.8.tar.xz
-sha:f79b0cfea71b37a8eeec8490db6c5f7ae7719c35587f21edb0617f370eeff633
+https://ftp.gnu.org/gnu/sed/sed-4.9.tar.xz
+sha:6e226b732e1cd739464ad6862bd1a1aba42d7982922da7a53519631d24975181
 {% endblock %}
 
 {% block bld_libs %}
--- pkgs/bld/boot/5/sed/ix.sh
+++ pkgs/bld/boot/5/sed/ix.sh
@@ -1,5 +1,10 @@
 {% extends '//bin/sed/ix.sh' %}
 
+{% block fetch %}
+https://ftp.gnu.org/gnu/sed/sed-4.8.tar.xz
+sha:f79b0cfea71b37a8eeec8490db6c5f7ae7719c35587f21edb0617f370eeff633
+{% endblock %}
+
 {% block bld_libs %}
 {% endblock %}
 
