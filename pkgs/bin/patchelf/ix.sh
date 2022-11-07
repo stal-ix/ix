1d3d6a96035ec37555d8c540875537f77c790c5b
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-07 20:29:44 +0300

    upver

--- pkgs/bin/patchelf/ix.sh
+++ pkgs/bin/patchelf/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://github.com/NixOS/patchelf/archive/refs/tags/0.16.1.tar.gz
-sha:50e07dd71e2bb88fff043aa6b126b413b0951da848498fe0f2ad38af6650d405
+https://github.com/NixOS/patchelf/archive/refs/tags/0.17.0.tar.gz
+sha:cfdd0591bfe17f50775695fbddf94d49ef03d5a603888667747ad105f2e9853a
 {% endblock %}
 
 {% block bld_libs %}
