bd456b9cd699db073ec371197a847b0884dbe722
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-10 23:20:30 +0300

    add

---
+++ pkgs/bin/efsw/ix.sh
@@ -0,0 +1,27 @@
+{% extends '//die/c/premake.sh' %}
+
+{% block fetch %}
+https://github.com/SpartanJ/efsw/archive/refs/tags/1.3.0.tar.gz
+sha:e190b72f23d92c42e1a0dab9bb6354a56f75c7535fd1f3e3a10b1c317e05f8f6
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+{% endblock %}
+
+{% block make_flags %}
+-C make/linux
+{% endblock %}
+
+{% block build_flags %}
+wrap_cc
+{% endblock %}
+
+{% block setup %}
+export CPPFLAGS="-Du_int32_t=uint32_t -include sys/select.h ${CPPFLAGS}"
+{% endblock %}
+
+{% block install %}
+mkdir ${out}/bin
+cp bin/efsw* ${out}/bin/efsw
+{% endblock %}
