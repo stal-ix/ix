83956facebeef1b242eba551f355fcfb6eb41515
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-13 01:58:40 +0300

    better

--- pkgs/bin/mrustc/rustc/ix.sh
+++ pkgs/bin/mrustc/rustc/ix.sh
@@ -1,5 +1,26 @@
-{% extends '//die/hub.sh' %}
+{% extends '//bin/mrustc/t/ix.sh' %}
 
-{% block run_deps %}
-bin/mrustc/rustc/unwrap(openssl=1)
+{% block bld_libs %}
+lib/c
+lib/z
+lib/c++
+bin/mrustc/std
+lib/shim/atomic
+{% endblock %}
+
+{% block patch %}
+{{super()}}
+for x in curl-sys libgit2-sys libnghttp2-sys libssh2-sys libz-sys lzma-sys; do
+    echo 'fn main() {}' > vendor/${x}/build.rs
+done
+{% endblock %}
+
+{% block build %}
+{{super()}}
+cargo ${RUSTC_SRC}/compiler/rustc
+{% endblock %}
+
+{% block install %}
+mkdir ${out}/bin
+cp ${OUTPUT_DIR}/rustc ${out}/bin/
 {% endblock %}
