60f63991157b0325778dc794f72b23a92180cd46
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-12 23:35:51 +0300

    better

--- pkgs/bin/yasm/ix.sh
+++ pkgs/bin/yasm/ix.sh
@@ -2,7 +2,6 @@
 
 {% block fetch %}
 #http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
-#sha:3dce6601b495f5b3d45b59f7d2492a340ee7e84b5beca17e48f862502bd5603f
 https://github.com/yasm/yasm/releases/download/v1.3.0/yasm-1.3.0.tar.gz
 sha:3dce6601b495f5b3d45b59f7d2492a340ee7e84b5beca17e48f862502bd5603f
 {% endblock %}
