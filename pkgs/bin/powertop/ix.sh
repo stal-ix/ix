6e71124e1d151426d1af066c015e398191d969d8
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-12 13:03:44 +0300

    add

---
+++ pkgs/bin/powertop/ix.sh
@@ -0,0 +1,19 @@
+{% extends '//die/c/autorehell.sh' %}
+
+{% block fetch %}
+https://github.com/fenrus75/powertop/archive/refs/tags/v2.15.tar.gz
+sha:e58ab3fd7b8ff5f4dd0d17f11848817e7d83c0a6918145ac81de03b5dccf8f49
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/nl
+lib/linux
+lib/curses
+lib/pciutils
+{% endblock %}
+
+{% block bld_tool %}
+bld/gettext
+bin/auto/conf/archive
+{% endblock %}
