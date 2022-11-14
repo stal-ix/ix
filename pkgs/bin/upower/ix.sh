b937f5b84313ece8e05b068a7dce16fdac598250
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-14 21:02:35 +0300

    better

---
+++ pkgs/bin/upower/ix.sh
@@ -0,0 +1,15 @@
+{% extends '//lib/upower/t/ix.sh' %}
+
+{% block bld_libs %}
+lib/linux
+lib/udev/g
+{{super()}}
+{% endblock %}
+
+{% block meson_flags %}
+{{super()}}
+os_backend=linux
+udevrulesdir=${out}/share/udev
+udevhwdbdir=${out}/share/udev
+systemdsystemunitdir=${out}/share/systemd
+{% endblock %}
