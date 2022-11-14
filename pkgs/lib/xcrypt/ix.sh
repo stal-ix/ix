a10aac93b2a884a9fb46eda234073c5ee16c0e59
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-14 11:57:28 +0300

    upver

--- pkgs/lib/xcrypt/ix.sh
+++ pkgs/lib/xcrypt/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh'%}
 
 {% block fetch %}
-https://github.com/besser82/libxcrypt/archive/refs/tags/v4.4.30.tar.gz
-sha:5556309d2dcd200da0190aa9bcc1a5bf71059f04423fc7c25dee59bb4355d731
+https://github.com/besser82/libxcrypt/archive/refs/tags/v4.4.31.tar.gz
+sha:17034be118f9b19589fe400ee4070dbd0ea36223be8c9b21db77e353ff888451
 {% endblock %}
 
 {% block lib_deps %}
