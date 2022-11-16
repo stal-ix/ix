7c4c2e72675fdec3e5caa91a94846e5c886ae8b1
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-16 22:50:52 +0300

    upver

--- pkgs/bin/wayland/protocols/ix.sh
+++ pkgs/bin/wayland/protocols/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://github.com/wayland-project/wayland-protocols/archive/refs/tags/1.28.tar.gz
-sha:914dfa09e7e866e913b27d2d9bda0e20e728c7b1c831fd3db71538d9f99a4869
+https://github.com/wayland-project/wayland-protocols/archive/refs/tags/1.29.tar.gz
+sha:4a85786ae69cd6d53bbe9278572f3c3d6ea342875ea444960edb6089237c3a18
 {% endblock %}
 
 {% block bld_tool %}
