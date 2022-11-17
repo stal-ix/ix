bea1f1310478cdaf414f24484b0026264bd70fe4
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-17 11:37:36 +0300

    add

--- pkgs/bin/lisgd/ix.sh
+++ pkgs/bin/lisgd/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/make.sh' %}
 
 {% block fetch %}
-https://git.sr.ht/~mil/lisgd/archive/0.3.5.tar.gz
-sha:e795f7d00142eee6b8d8f5f2cd19a5282b8c911b1c829d88353ccbb5bc877e46
+https://git.sr.ht/~mil/lisgd/archive/0.3.6.tar.gz
+sha:e19d366a46beda586c8d30dd17c7d44625b929b732493a3e7799aa31173239a0
 {% endblock %}
 
 {% block bld_libs %}
