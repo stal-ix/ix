9d30f545d8db52ee3e2e4739625476f41ac49aa2
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-17 18:40:10 +0300

    better

--- pkgs/bin/webkitproc/ix.sh
+++ pkgs/bin/webkitproc/ix.sh
@@ -1,5 +1,5 @@
 {% extends '//die/hub.sh' %}
 
 {% block run_deps %}
-bin/webkitproc/unwrap(allocator=tcmalloc,gtk_icons=aux/hicolor-icon-theme)
+bin/webkitproc/unwrap(allocator=tcmalloc,gtk_ver=4,gtk_icons=aux/hicolor-icon-theme)
 {% endblock %}
