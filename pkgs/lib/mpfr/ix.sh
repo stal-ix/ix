606fbb5d4a004216731f84cb2538a9ff9962ddc0
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-18 12:08:21 +0300

    upver

--- pkgs/lib/mpfr/ix.sh
+++ pkgs/lib/mpfr/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://ftp.gnu.org/gnu/mpfr/mpfr-4.1.0.tar.xz
-sha:0c98a3f1732ff6ca4ea690552079da9c597872d30e96ec28414ee23c95558a7f
+https://ftp.gnu.org/gnu/mpfr/mpfr-4.1.1.tar.xz
+sha:ffd195bd567dbaffc3b98b23fd00aad0537680c9896171e44fe3ff79e28ac33d
 {% endblock %}
 
 {% block lib_deps %}
@@ -15,6 +15,15 @@ bld/texinfo
 bld/auto/archive
 {% endblock %}
 
-{% block env_lib %}
+{% block configure_flags %}
+--enable-thread-safe
+--enable-formally-proven-code
+{% endblock %}
+
+{% block setup %}
+export CPPFLAGS="-include stdint.h ${CPPFLAGS}"
+{% endblock %}
+
+{% block env %}
 export COFLAGS="--with-mpfr=${out} --with-libmpfr-prefix=${out} \${COFLAGS}"
 {% endblock %}
