74ed45c51e1f7ccdb650a996c3c049c6e8cf03fb
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-18 13:02:31 +0300

    better

--- pkgs/lib/xcrypt/ix.sh
+++ pkgs/lib/xcrypt/ix.sh
@@ -1,10 +1,18 @@
 {% extends '//die/c/autorehell.sh'%}
 
 {% block fetch %}
-https://github.com/besser82/libxcrypt/archive/refs/tags/v4.4.31.tar.gz
-sha:17034be118f9b19589fe400ee4070dbd0ea36223be8c9b21db77e353ff888451
+https://github.com/besser82/libxcrypt/archive/refs/tags/v4.4.32.tar.gz
+sha:7f3bc8099b0b8459179995ab6e4f0b2ee72b70bd97489567c07713f8be5a9590
 {% endblock %}
 
 {% block lib_deps %}
 lib/c
+{% endblock %}
+
+{% block bld_libs %}
+lib/kernel
+{% endblock %}
+
+{% block configure_flags %}
+--disable-symvers
 {% endblock %}
