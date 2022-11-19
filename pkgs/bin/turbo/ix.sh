9197aed9191b426aa6fa613aa24e862214466afe
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-19 16:17:20 +0300

    upver

--- pkgs/bin/turbo/ix.sh
+++ pkgs/bin/turbo/ix.sh
@@ -1,8 +1,10 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/magiblot/turbo/archive/80d2b67b9bce7eaa49a77ad05c367f5c99ae6afb.zip
-sha:0ea380517f303cc5d4f4d2db9ed6c77ad48467e2af82dc46026999442c367009
+#https://github.com/magiblot/turbo/archive/80d2b67b9bce7eaa49a77ad05c367f5c99ae6afb.zip
+#sha:0ea380517f303cc5d4f4d2db9ed6c77ad48467e2af82dc46026999442c367009
+https://github.com/magiblot/turbo/archive/51ab6566b01a4edf927519c4d96eb55b302aa391.zip
+sha:636ed0021334a744e1786772b6c697943400d3b5f87e7ddbc2f189b04e9758ad
 {% endblock %}
 
 {% block bld_libs %}
