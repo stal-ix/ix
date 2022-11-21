77cf0413555b9fd473e947bae0edf005846512aa
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-21 19:26:10 +0300

    refac

--- pkgs/bin/xchm/ix.sh
+++ pkgs/bin/xchm/ix.sh
@@ -1,21 +1,6 @@
-{% extends '//die/c/autorehell.sh' %}
+{% extends '//die/hub.sh' %}
 
-{% block fetch %}
-https://github.com/rzvncj/xCHM/archive/refs/tags/1.33.tar.gz
-sha:8537563bc790dc2f9b27e0355d8a7e538c01347dcfd8bd392ac78ad3eeb69701
-{% endblock %}
-
-{%block bld_libs %}
-lib/c
-lib/chm
-lib/wx/widgets
-{% endblock %}
-
-{% block bld_tool %}
-bld/gettext
-{% endblock %}
-
-{% block install %}
-{{super()}}
-{{hooks.wrap_xdg_binary('xchm')}}
+{% block run_deps %}
+bin/xchm/unwrap
+bin/xdg/wrapper(name=xchm)
 {% endblock %}
