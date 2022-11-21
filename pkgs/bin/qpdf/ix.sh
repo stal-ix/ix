9f3950467b4b14631c4755c2de3e50ebc45703ea
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-21 13:02:00 +0300

    upver

--- pkgs/bin/qpdf/ix.sh
+++ pkgs/bin/qpdf/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/qpdf/qpdf/archive/refs/tags/v11.1.1.tar.gz
-sha:785edab622a1bc7e25e1537ad2c325005d48c5c7957f7abedff405deb80fa59a
+https://github.com/qpdf/qpdf/archive/refs/tags/v11.2.0.tar.gz
+sha:d5fde4881948db41300f5806ea54f07f9fc37bc414a6a78efa2649d212b56707
 {% endblock %}
 
 {% block bld_libs %}
