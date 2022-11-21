a1e50c5df13ff306a1a6a513f9fc30f3f5c866f9
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-22 01:04:06 +0300

    better

--- pkgs/bin/sudo/wrapper/ix.sh
+++ pkgs/bin/sudo/wrapper/ix.sh
@@ -1,18 +1,5 @@
-{% extends '//die/proxy.sh' %}
+{% extends '//die/wrapper.sh' %}
 
-{% block install %}
-mkdir -p ${out}/fix; cd ${out}/fix
-
-cat << EOF > wrap-sudo-{{wrap}}.sh
-#!/usr/bin/env sh
-mv bin/{{wrap}} bin/{{wrap}}-bin
-mv fix/{{wrap}}.tmp bin/{{wrap}}
-EOF
-
-cat << EOF > {{wrap}}.tmp
-#!/usr/bin/env sh
-exec sudo \$(which {{wrap}}-bin) "\${@}"
-EOF
-
-chmod +x wrap-sudo-{{wrap}}.sh {{wrap}}.tmp
+{% block wrapper %}
+sudo
 {% endblock %}
