26c525ecfae2348eb1dd54948c6596ad6eedd5aa
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-21 15:38:36 +0300

    better

--- pkgs/bin/pciutils/ix.sh
+++ pkgs/bin/pciutils/ix.sh
@@ -1,5 +1,15 @@
 {% extends '//lib/pciutils/ix.sh' %}
 
+{% block bld_libs %}
+lib/kmod
+{{super()}}
+{% endblock %}
+
+{% block bld_tool %}
+bld/pkg/config
+{{super()}}
+{% endblock %}
+
 {% block make_install_target %}
 install
 {% endblock %}
