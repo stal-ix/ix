354f997a1f2e1b8eb717b5ba1246179325a5f5bb
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-21 19:29:46 +0300

    better

--- pkgs/bin/uget/ix.sh
+++ pkgs/bin/uget/ix.sh
@@ -1,39 +1,6 @@
-{% extends '//die/c/autorehell.sh' %}
+{% extends '//die/hub.sh' %}
 
-{% block fetch %}
-https://downloads.sourceforge.net/urlget/uget-2.2.3-1.tar.gz
-sha:11356e4242151b9014fa6209c1f0360b699b72ef8ab47dbeb81cc23be7db9049
-{% endblock %}
-
-{% block bld_libs %}
-lib/c
-lib/curl
-lib/gtk/3
-lib/notify
-lib/openssl
-lib/gtk/deps
-{% endblock %}
-
-{% block bld_tool %}
-bld/gettext
-bld/intltool
-{% endblock %}
-
-{% block shell %}
-bin/bash/lite/sh
-{% endblock %}
-
-{% block configure_flags %}
---disable-rss-notify
---disable-gstreamer
---with-openssl=yes
-{% endblock %}
-
-{% block setup %}
-export CPPFLAGS="-fcommon ${CPPFLAGS}"
-{% endblock %}
-
-{% block install %}
-{{super()}}
-{{hooks.wrap_xdg_binary('uget-gtk')}}
+{% block run_deps %}
+bin/uget/unwrap
+bin/xdg/wrapper(name=uget-gtk)
 {% endblock %}
