4b7f8bb3f100802642c952c30e7bc57a73b5fd3d
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-21 19:12:06 +0300

    refac

---
+++ pkgs/bin/inkscape/unwrap/ix.sh
@@ -0,0 +1,55 @@
+{% extends '//die/c/cmake.sh' %}
+
+{% block fetch %}
+https://inkscape.org/gallery/item/34673/inkscape-1.2.1.tar.xz
+sha:46ce7da0eba7ca4badc1db70e9cbb67e0adf9bb342687dc6e08b5ca21b8d4c1b
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/c++
+lib/cdr
+# check it
+lib/gsl/gnu
+lib/dbus
+lib/intl
+lib/jpeg
+lib/xslt
+lib/pango
+lib/cairo
+lib/boost
+lib/visio
+lib/lcms/2
+lib/gspell
+lib/soup/2
+#lib/poppler
+lib/boehmgc
+lib/potrace
+lib/rsvg/dl
+lib/gtk/3/mm
+lib/readline
+lib/harfbuzz
+lib/freetype
+lib/dbus/glib
+lib/googletest
+lib/fontconfig
+lib/image/magick
+lib/double/conversion
+{% endblock %}
+
+{% block bld_tool %}
+bld/python
+bld/gettext
+bin/ragel/6
+bld/glib/codegen
+{% endblock %}
+
+{% block cmake_flags %}
+WITH_X11=OFF
+WITH_LIBWPG=OFF
+ENABLE_POPPLER=OFF
+{% endblock %}
+
+{% block setup %}
+export CXXFLAGS="-Wno-register ${CXXFLAGS}"
+{% endblock %}
--- pkgs/bld/inkscape/ix.sh
+++ pkgs/bld/inkscape/ix.sh
@@ -1,5 +1,5 @@
 {% extends '//die/hub.sh' %}
 
 {% block run_deps %}
-bin/inkscape(gtk_icons=aux/hicolor-icon-theme)
+bin/inkscape/unwrap(gtk_icons=aux/hicolor-icon-theme)
 {% endblock %}
