5b1e95dbdaae1451f36b65ae06dc4bc6de04a2a4
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-21 19:42:21 +0300

    better

--- pkgs/bin/xdg/wrapper/ix.sh
+++ pkgs/bin/xdg/wrapper/ix.sh
@@ -9,11 +9,14 @@ export XDG_DATA_DIRS="__realm__/share:\${XDG_DATA_DIRS}"
 exec {{prefix}} "__realm__/bin/bin_{{name}}/{{name}}" "\$@"
 EOF
 
-cat << EOF > "wrap-xdg-{{name}}.sh"
+cat << EOF > wrap-xdg-{{name}}.sh
 mkdir -p bin/bin_{{name}}
 mv bin/{{name}} bin/bin_{{name}}/
 cat fix/{{name}} | sed -e "s|__realm__|\${PWD}|" > bin/{{name}}
 chmod +x bin/{{name}}
+EOF
+
+cat << EOF > glib-compile-schemas.sh
 if test -d share/glib-2.0/schemas; then
     glib-compile-schemas share/glib-2.0/schemas
 fi
