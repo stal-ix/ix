943d8a8d8ed324033e0d533913122f7451f70900
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-22 15:11:35 +0300

    refac

--- pkgs/die/wrapper.sh
+++ pkgs/die/wrapper.sh
@@ -7,12 +7,12 @@ cat << EOF > {{wrap}}
 #!/usr/bin/env sh
 {% block wrapper_env %}
 {% endblock %}
-exec {{self.wrapper().strip()}} "__realm__/bin/bin_{{wrap}}/{{wrap}}" "\$@"
+exec {{self.wrapper().strip()}} "__realm__/bin/bin_{{uniq_id}}/{{wrap}}" "\$@"
 EOF
 
 cat << EOF > {{uniq_id}}-{{wrap}}.sh
-mkdir -p bin/bin_{{wrap}}
-mv bin/{{wrap}} bin/bin_{{wrap}}/
+mkdir -p bin/bin_{{uniq_id}}
+mv bin/{{wrap}} bin/bin_{{uniq_id}}/
 cat fix/{{wrap}} | sed -e "s|__realm__|\${PWD}|" > bin/{{wrap}}
 chmod +x bin/{{wrap}}
 EOF
