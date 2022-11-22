0a4f8fb66d39f177ad2d38300bad45abaf9e12e3
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-22 15:15:43 +0300

    better

--- pkgs/bin/xdg/er/ix.sh
+++ pkgs/bin/xdg/er/ix.sh
@@ -5,12 +5,13 @@
 {% endblock %}
 
 {% block wrapper_env %}
+export PATH="__realm__/bin:\${PATH}"
 export XDG_DATA_DIRS="__realm__/share:\${XDG_DATA_DIRS}"
 {% endblock %}
 
 {% block install %}
 {{super()}}
-cat << EOF > ${out}/fix/glib-compile-schemas.sh
+cat << EOF > ${out}/fix/glib_compile_schemas.sh
 if test -d share/glib-2.0/schemas; then
     glib-compile-schemas share/glib-2.0/schemas
 fi
