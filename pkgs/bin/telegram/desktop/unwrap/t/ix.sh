8cf4d8fdc8e04b6caa46bdad5f062030c98f0302
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-24 16:39:49 +0300

    better

--- pkgs/bin/telegram/desktop/unwrap/t/ix.sh
+++ pkgs/bin/telegram/desktop/unwrap/t/ix.sh
@@ -4,6 +4,14 @@
 
 {% block fetch %}
 {% include 'ver.sh' %}
+#https://github.com/telegramdesktop/tdesktop/archive/1e6937a075a9620d48b72556b8e9eefb8d44ef36.zip
+#sha:45b910379f72882e7a543ceb017729e82775f173ac1f4f08c716e4e946af8834
+{% endblock %}
+
+{% block unpack %}
+mkdir src; cd src
+extract 1 ${src}/td*
+#extract 1 ${src}/1e*
 {% endblock %}
 
 {% block bld_libs %}
