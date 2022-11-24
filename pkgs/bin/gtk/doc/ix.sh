07be7c7a269cdf8b17bd754895edb55003d7e5b8
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-24 17:32:28 +0300

    better

--- pkgs/bin/gtk/doc/ix.sh
+++ pkgs/bin/gtk/doc/ix.sh
@@ -14,6 +14,26 @@ pip/pygments
 bin/xsltproc
 {% endblock %}
 
+{#
+TODO(pg): fix bin refs
+
+pg-> cat config.py
+version = "1.32"
+
+# tools
+dblatex = ''
+fop = ''
+pkg_config = '/ix/store/r4ASLogzC0nAm1u1-bin-pkg-config/bin/pkg-config'
+xsltproc = '/ix/store/QhqWsRCXUZSUbpPI-bin-xsltproc/bin/xsltproc'
+
+# configured directories
+prefix = '/ix/store/eJ231NzIF7V5qO5z-bin-gtk-doc'
+datarootdir = "${prefix}/share".replace('${prefix}', prefix)
+datadir = "${datarootdir}".replace('${datarootdir}', datarootdir)
+
+exeext = ''
+#}
+
 {% block meson_flags %}
 yelp_manual=false
 tests=false
