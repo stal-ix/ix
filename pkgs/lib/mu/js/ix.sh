58f4ec1a93aecb2577e9501c9d747e2d68a7af82
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-25 11:05:58 +0300

    upver

--- pkgs/lib/mu/js/ix.sh
+++ pkgs/lib/mu/js/ix.sh
@@ -1,7 +1,9 @@
 {% extends '//die/c/make.sh' %}
 
+{% block version %}1.3.2{% endblock %}
+
 {% block fetch %}
-https://github.com/ccxvii/mujs/archive/refs/tags/1.3.2.tar.gz
+https://github.com/ccxvii/mujs/archive/refs/tags/{{self.version()}}.tar.gz
 sha:59763cc32776714b4f00a0efede83f4f56b381c9dbaced4756d665c214249dca
 {% endblock %}
 
@@ -23,5 +25,5 @@ install-static
 
 {% block install %}
 {{super()}}
-sed -e 's|Version:.*|Version: 1.2.0|' -i ${out}/lib/pkgconfig/mujs.pc
+sed -e 's|Version:.*|Version: {{self.version()}}|' -i ${out}/lib/pkgconfig/mujs.pc
 {% endblock %}
