fb4be48f7b0e2b20bd210df3c0826a320f19e173
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-27 02:46:35 +0300

    refac

--- pkgs/lib/openssl/wolf/ix.sh
+++ pkgs/lib/openssl/wolf/ix.sh
@@ -1,11 +1,11 @@
-{% extends '//lib/openssl/fake/ix.sh' %}
+{% extends '//die/proxy.sh' %}
 
 {% block lib_deps %}
 lib/c
 lib/wolfssl
+lib/openssl/fake
 {% endblock %}
 
 {% block install %}
-{{super()}}
 cp -R ${lib_wolfssl}/include/wolfssl ${out}/include
 {% endblock %}
