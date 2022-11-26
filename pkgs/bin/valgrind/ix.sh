ae4b5d7baee3119fb590d5b110399be1d805912a
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-27 02:01:10 +0300

    refac

--- pkgs/bin/valgrind/ix.sh
+++ pkgs/bin/valgrind/ix.sh
@@ -5,8 +5,9 @@ https://sourceware.org/pub/valgrind/valgrind-3.19.0.tar.bz2
 sha:dd5e34486f1a483ff7be7300cc16b4d6b24690987877c3278d797534d6738f02
 {% endblock %}
 
-{% block lib_deps %}
+{% block bld_libs %}
 lib/c
+lib/shim/fake(lib_name=gcc)
 {% endblock %}
 
 {% block bld_tool %}
@@ -19,8 +20,4 @@ wrap_cc
 
 {% block patch %}
 sed -e 's|.*If this is glibc then figure out.*|export GLIBC_VERSION=musl|' -i configure
-
-find . -type f | while read l; do
-    sed -e 's|-lgcc||g' -i ${l}
-done
 {% endblock %}
