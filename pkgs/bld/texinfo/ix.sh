1d3e4ff5d3f48d19f887e12fdefde4a207778ed2
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-27 22:23:46 +0300

    refac

--- pkgs/bld/texinfo/ix.sh
+++ pkgs/bld/texinfo/ix.sh
@@ -1,5 +1,5 @@
 {% extends '//die/hub.sh' %}
 
 {% block run_deps %}
-bin/texinfo(std_box=bld/boot/box,intl_ver=no,curses=no)
+bin/tex/info(std_box=bld/boot/box,intl_ver=no,curses=no)
 {% endblock %}
