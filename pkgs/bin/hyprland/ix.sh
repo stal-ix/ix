cb0c3b8dc765a6ea76a00b56b0d4fae8b0f4c4cb
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-28 23:47:35 +0300

    upver

--- pkgs/bin/hyprland/ix.sh
+++ pkgs/bin/hyprland/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://github.com/hyprwm/Hyprland/releases/download/v0.12.1beta/source-v0.12.1beta.tar.gz
-sha:be9220878e65e1fbf1ff7e9b4d6c3f4c3386198b458f78cc874576fdb8f65e7a
+https://github.com/hyprwm/Hyprland/archive/refs/tags/v0.18.0beta.tar.gz
+sha:6b2c07263fbd874faa498d211f801191a947ea41b370cbd0f1fa0a58b7fe5a54
 {% endblock %}
 
 {% block bld_libs %}
@@ -13,13 +13,59 @@ lib/cairo
 lib/input
 lib/opengl
 lib/wayland
+lib/shim/x11
+lib/range/v3
 lib/xkbcommon
-lib/wlroots/15
+lib/drivers/3d
+lib/wlroots/16
 lib/mesa/gl/dl
 lib/mesa/egl/dl
 lib/mesa/glesv2/dl
 {% endblock %}
 
 {% block bld_tool %}
+bld/python
+bld/fakegit
 bld/wayland
+{% endblock %}
+
+{% block patch %}
+rm -r subprojects
+
+sed -e "s|subproject.*wlroots.*|dependency('wlroots')|" \
+    -e 's|have_xwlr = .*|have_xwlr = false|' \
+    -i meson.build
+
+sed -e 's|.get_variable.*wlroots.*|,|' \
+    -i src/meson.build
+
+cat << EOF > patch.py
+import sys
+
+def it():
+    for x in sys.stdin.read().split('\n'):
+        if 'bool operator==(' in x:
+            x = x.replace(') {', ') const {')
+        yield x
+
+print('\n'.join(it()).strip() + '\n')
+EOF
+
+find . -type f -name '*.hpp' | while read l; do
+    cat ${l} | python3 patch.py > _
+    mv _ ${l}
+done
+
+cat - src/helpers/MiscFunctions.cpp << EOF > _
+#include <sstream>
+EOF
+
+mv _ src/helpers/MiscFunctions.cpp
+
+sed -e 's|NULL, XCB_STACK_MODE_ABOVE|0, XCB_STACK_MODE_ABOVE|' \
+    -i src/managers/XWaylandManager.cpp
+{% endblock %}
+
+{% block build_flags %}
+shut_up
 {% endblock %}
