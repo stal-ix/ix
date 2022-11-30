e63bcfc0085416785e668993258dfc54e20cb4ef
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-11-30 16:13:59 +0300

    better

--- pkgs/lib/idn/2/ix.sh
+++ pkgs/lib/idn/2/ix.sh
@@ -10,3 +10,18 @@
 {% block env %}
 export COFLAGS="--with-libidn2=${out} \${COFLAGS}"
 {% endblock %}
+
+{% block install %}
+{{super()}}
+llvm-objcopy \
+    --strip-symbol=_idn2_punycode_encode@IDN2_0.0.0 \
+    --strip-symbol=_idn2_punycode_decode@IDN2_0.0.0 \
+    ${out}/lib/libidn2.a
+{% endblock %}
+
+{% block test %}
+if (llvm-nm ${out}/lib/libidn2.a | grep @); then
+    echo 'have versioned symbols'
+    exit 1
+fi
+{% endblock %}
