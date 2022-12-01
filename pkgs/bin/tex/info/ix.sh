7ce87e0d14e6a09d3f6b3a6139eff1a570ccb49e
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-12-01 12:55:28 +0300

    upver

--- pkgs/bin/tex/info/ix.sh
+++ pkgs/bin/tex/info/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
-https://ftp.gnu.org/gnu/texinfo/texinfo-7.0.tar.xz
-sha:20744b82531ce7a04d8cee34b07143ad59777612c3695d5855f29fba40fbe3e0
+https://ftp.gnu.org/gnu/texinfo/texinfo-7.0.1.tar.xz
+sha:bcd221fdb2d807a8a09938a0f8d5e010ebd2b58fca16075483d6fcb78db2c6b2
 {% endblock %}
 
 {% block bld_libs %}
