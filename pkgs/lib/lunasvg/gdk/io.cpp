d45c3ed72439e10a9211d7a233dccdbce188f5e7
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-12-01 17:34:28 +0300

    better

--- pkgs/lib/lunasvg/gdk/io.cpp
+++ pkgs/lib/lunasvg/gdk/io.cpp
@@ -1,56 +1,10 @@
+#include <glib.h>
 #include <string>
 #include <iostream>
-
 #include <lunasvg.h>
-
 #include <gdk-pixbuf/gdk-pixbuf.h>
 
 using namespace lunasvg;
-
-static std::string Decode(const std::string& input, std::string& out) {
-    static constexpr unsigned char kDecodingTable[] = {
-      64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-      64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-      64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 62, 64, 64, 64, 63,
-      52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 64, 64, 64, 64, 64, 64,
-      64,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
-      15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 64, 64, 64, 64, 64,
-      64, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-      41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 64, 64, 64, 64, 64,
-      64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-      64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-      64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-      64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-      64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-      64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-      64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-      64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64
-    };
-
-    size_t in_len = input.size();
-    if (in_len % 4 != 0) return "Input data size is not a multiple of 4";
-
-    size_t out_len = in_len / 4 * 3;
-    if (input[in_len - 1] == '=') out_len--;
-    if (input[in_len - 2] == '=') out_len--;
-
-    out.resize(out_len);
-
-    for (size_t i = 0, j = 0; i < in_len;) {
-      uint32_t a = input[i] == '=' ? 0 & i++ : kDecodingTable[static_cast<int>(input[i++])];
-      uint32_t b = input[i] == '=' ? 0 & i++ : kDecodingTable[static_cast<int>(input[i++])];
-      uint32_t c = input[i] == '=' ? 0 & i++ : kDecodingTable[static_cast<int>(input[i++])];
-      uint32_t d = input[i] == '=' ? 0 & i++ : kDecodingTable[static_cast<int>(input[i++])];
-
-      uint32_t triple = (a << 3 * 6) + (b << 2 * 6) + (c << 1 * 6) + (d << 0 * 6);
-
-      if (j < out_len) out[j++] = (triple >> 2 * 8) & 0xFF;
-      if (j < out_len) out[j++] = (triple >> 1 * 8) & 0xFF;
-      if (j < out_len) out[j++] = (triple >> 0 * 8) & 0xFF;
-    }
-
-    return "";
-}
 
 namespace {
     // TODO(pg): check memory leak
@@ -96,14 +50,13 @@ namespace {
         );
     }
 
-    static auto b64decode(const std::string& s) {
-        std::string ret;
+    static auto b64decode(std::string s) {
+        gsize len = 0;
 
-        if (auto err = Decode(s, ret); err.size()) {
-            throw std::runtime_error("base64 decode error: " + err);
-        }
+        g_base64_decode_inplace(s.data(), &len);
+        s.resize(len);
 
-        return ret;
+        return s;
     }
 
     struct Loader {
--- pkgs/lib/lunasvg/gdk/ix.sh
+++ pkgs/lib/lunasvg/gdk/ix.sh
@@ -3,6 +3,7 @@
 {% block lib_deps %}
 lib/c
 lib/c++
+lib/glib
 lib/lunasvg
 lib/gdk/pixbuf
 {% endblock %}
