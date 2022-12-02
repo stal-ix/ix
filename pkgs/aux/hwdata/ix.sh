464ccc6eb62e4ea7bdfb2b6a11c85571477925e4
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-12-02 17:44:32 +0300

    upver

--- pkgs/aux/hwdata/ix.sh
+++ pkgs/aux/hwdata/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/make.sh' %}
 
 {% block fetch %}
-https://github.com/vcrhonek/hwdata/archive/refs/tags/v0.364.tar.gz
-sha:e79ee7e0251c94273ac8ffa7f68892152d3cfc243a471bc61771d8ab53da3331
+https://github.com/vcrhonek/hwdata/archive/refs/tags/v0.365.tar.gz
+sha:d6ae2436a69c6f5e04d926f5d753a99d90deb864065a2e2f8a8eda87b70dcdd5
 {% endblock %}
 
 {% block configure %}
