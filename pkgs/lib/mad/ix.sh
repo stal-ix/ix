a5aa6907a0b20600923459d4553c8c33e8c56f6f
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-12-03 15:51:46 +0300

    fix build

--- pkgs/lib/mad/ix.sh
+++ pkgs/lib/mad/ix.sh
@@ -8,3 +8,14 @@ sha:83ca48db60f480ae22234bae08e6ad651adec2667a68ad2df6fd61e6a50a32c7
 {% block lib_deps %}
 lib/c
 {% endblock %}
+
+{% block install %}
+{{super()}}
+cd ${out}/lib/pkgconfig
+ln -s libmad.pc mad.pc
+{% endblock %}
+
+{% block test %}
+# for vkquake
+test -f ${out}/lib/pkgconfig/mad.pc
+{% endblock %}
