c46d62895b316b320c3a2230074d0ee05d57f215
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-12-03 23:16:13 +0300

    add

---
+++ pkgs/bin/codelite/ix.sh
@@ -0,0 +1,39 @@
+{% extends '//die/c/cmake.sh' %}
+
+{% block fetch %}
+https://github.com/eranif/codelite/archive/refs/tags/16.0.0.tar.gz
+sha:fb707b98b2f56ae0791c7cb341c6a0240ff26a785dae31f7dcd1038956737957
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/c++
+lib/ssh
+lib/glib
+lib/pcre/2
+lib/sqlite/3
+lib/hunspell
+lib/wx/widgets
+{% endblock %}
+
+{% block bld_tool %}
+bld/flex
+bld/bison
+{% endblock %}
+
+{% block patch %}
+sed -e 's|libssh.so|libssh.a|' -i CMakeLists.txt
+sed -e 's|.*splitterLeft.*SetSplitterLeft.*||' -i wxcrafter/myxh_propgrid.cpp
+sed -e 's|thread_local |static thread_local |' -i Plugin/globals.cpp
+{% endblock %}
+
+{% block cpp_missing %}
+unistd.h
+sys/types.h
+sys/stat.h
+fcntl.h
+{% endblock %}
+
+{% block cmake_flags %}
+CL_PREFIX=${out}
+{% endblock %}
