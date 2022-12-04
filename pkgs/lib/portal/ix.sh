f1fdefe12bebbd62df64e413ea32bf84bd283743
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-12-05 02:26:04 +0300

    better

--- pkgs/lib/portal/ix.sh
+++ pkgs/lib/portal/ix.sh
@@ -1,15 +1,13 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-#https://github.com/flatpak/libportal/archive/refs/tags/0.6.tar.gz
-#sha:8ad326c4f53b7433645dc86d994fef0292bee8adda0fe67db9288ace19250a9c
 https://github.com/flatpak/libportal/releases/download/0.6/libportal-0.6.tar.xz
 sha:88a12c3ba71bc31acff7238c280de697d609cebc50830c3766776ec35abc6566
 {% endblock %}
 
 {% block lib_deps %}
 lib/c
-lib/gtk
+lib/gtk/{{gtk_ver}}
 {% endblock %}
 
 {% block bld_tool %}
@@ -22,5 +20,5 @@ vapi=false
 docs=false
 tests=false
 introspection=false
-backends=gtk{{gtk_ver or '3'}}
+backends=gtk{{gtk_ver}}
 {% endblock %}
