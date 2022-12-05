2636107fcc819f12c37327c7f857e7da771ae2cd
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-12-05 19:17:07 +0300

    upver

--- pkgs/lib/deflate/ix.sh
+++ pkgs/lib/deflate/ix.sh
@@ -1,14 +1,14 @@
-{% extends '//die/c/make.sh' %}
+{% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/ebiggers/libdeflate/archive/refs/tags/v1.14.tar.gz
-sha:89e7df898c37c3427b0f39aadcf733731321a278771d20fc553f92da8d4808ac
-{% endblock %}
-
-{% block build_flags %}
-wrap_cc
+https://github.com/ebiggers/libdeflate/archive/refs/tags/v1.15.tar.gz
+sha:58b95040df7383dc0413defb700d9893c194732474283cc4c8f144b00a68154b
 {% endblock %}
 
 {% block lib_deps %}
 lib/c
+{% endblock %}
+
+{% block cmake_flags %}
+LIBDEFLATE_BUILD_SHARED_LIB=OFF
 {% endblock %}
