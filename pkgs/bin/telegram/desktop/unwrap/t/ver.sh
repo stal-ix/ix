a1f06cef51464c5c7f5139b9f9381948b32184d1
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-12-08 01:03:24 +0300

    upver

--- pkgs/bin/telegram/desktop/unwrap/t/ver.sh
+++ pkgs/bin/telegram/desktop/unwrap/t/ver.sh
@@ -1,2 +1,2 @@
-https://github.com/telegramdesktop/tdesktop/releases/download/v4.3.4/tdesktop-4.3.4-full.tar.gz
-sha:aa11c52f7812c6e9a5ccf2e3a31d526a1db5562c1bbcb571aa70d681ca909b93
+https://github.com/telegramdesktop/tdesktop/releases/download/v4.4.1/tdesktop-4.4.1-full.tar.gz
+sha:76981e32541c42b3212427e9abb4db2a6f55a14eaba31b3d7ad7d3d5ed8008b1
