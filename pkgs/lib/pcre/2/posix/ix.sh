fa06e03beecb52bb4f4ae571a429e04c3f9c466b
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-12-12 22:50:57 +0300

    upver

--- pkgs/lib/pcre/2/posix/ix.sh
+++ pkgs/lib/pcre/2/posix/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/PhilipHazel/pcre2/archive/refs/tags/pcre2-10.41.tar.gz
-sha:a8a1de4b88333991ae11e22cfdde3e8efe47a71247ece801677869a27d20ac84
+https://github.com/PhilipHazel/pcre2/archive/refs/tags/pcre2-10.42.tar.gz
+sha:a8e52a9bd1bca8f51c5c24823adc2a99acb12288e289a6507090c1a4a4815010
 {% endblock %}
 
 {% block lib_deps %}
