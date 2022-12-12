6a7ca86d400713595385443b0c544301b24f8c1f
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-12-12 22:24:48 +0300

    fix

--- pkgs/lib/tllist/ix.sh
+++ pkgs/lib/tllist/ix.sh
@@ -2,7 +2,7 @@
 
 {% block fetch %}
 https://codeberg.org/dnkl/tllist/archive/1.1.0.zip
-sha:9eb339e668d50e44ac1ffef825e8d17948e199d05840fd0e77775324ce3ffb4d
+sha:e75061ceac622a24e118bd78c6c129545a0ce060f71b6cd30494ff76a3d2b6d4
 {% endblock %}
 
 {% block lib_deps %}
