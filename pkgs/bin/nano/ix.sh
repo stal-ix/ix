84b9f930e3a44beb61120615f1ecac14e473d58e
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-12-14 16:06:21 +0300

    upver

--- pkgs/bin/nano/ix.sh
+++ pkgs/bin/nano/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://www.nano-editor.org/dist/v7/nano-7.0.tar.xz
-sha:8dd6eac38b2b8786d82681f0e1afd84f6b75210d17391b6443c437e451552149
+https://www.nano-editor.org/dist/v7/nano-7.1.tar.xz
+sha:57ba751e9b7519f0f6ddee505202e387c75dde440c1f7aa1b9310cc381406836
 {% endblock %}
 
 {% block bld_libs %}
