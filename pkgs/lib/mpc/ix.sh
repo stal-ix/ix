0d35dc50c6d2e77255426bb3bd0f27017ad6f489
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-12-14 22:08:34 +0300

    upver

--- pkgs/lib/mpc/ix.sh
+++ pkgs/lib/mpc/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://ftp.gnu.org/gnu/mpc/mpc-1.2.1.tar.gz
-md5:9f16c976c25bb0f76b50be749cd7a3a8
+https://ftp.gnu.org/gnu/mpc/mpc-1.3.0.tar.gz
+sha:0e3b12181d37207230f5a7a7ddcfc22abfc5fc9c05825e1a65401a489a432a2a
 {% endblock %}
 
 {% block lib_deps %}
@@ -11,6 +11,6 @@ lib/gmp
 lib/mpfr
 {% endblock %}
 
-{% block env_lib %}
+{% block env %}
 export COFLAGS="--with-mpc=${out} \${COFLAGS}"
 {% endblock %}
