5a810125daaff8a4ddbb33ded11bd86d96d96c2d
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-12-15 12:44:02 +0300

    upver

--- pkgs/lib/mbedtls/3/t/ix.sh
+++ pkgs/lib/mbedtls/3/t/ix.sh
@@ -1,10 +1,21 @@
 {% extends '//lib/mbedtls/t/ix.sh' %}
 
 {% block fetch %}
-https://github.com/ARMmbed/mbedtls/archive/refs/tags/v3.2.1.tar.gz
-sha:d0e77a020f69ad558efc660d3106481b75bd3056d6301c31564e04a0faae88cc
+https://github.com/ARMmbed/mbedtls/archive/refs/tags/v3.3.0.tar.gz
+sha:113fa84bc3cf862d56e7be0a656806a5d02448215d1e22c98176b1c372345d33
 {% endblock %}
 
 {% block lib_deps %}
 lib/c
+{% endblock %}
+
+{% block bld_libs %}
+{{super()}}
+pip/jinja2
+pip/jsonschema
+{% endblock %}
+
+{% block bld_tool %}
+bld/perl
+{{super()}}
 {% endblock %}
