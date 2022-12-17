6fe582cd2cd5af69b7b6d9c73958ca40ad25d8a7
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-12-17 14:22:51 +0300

    better

---
+++ pkgs/bin/clang/env/ix.sh
@@ -0,0 +1,29 @@
+{% extends '//die/proxy.sh' %}
+
+{% block install %}
+mkdir ${out}/fix
+cd ${out}/fix
+
+cat << EOF > c++
+#!/usr/bin/env sh
+set -e
+. __realm__/env
+exec clang++ -nostdlib -nostdlib++ -nostdinc -nostdinc++ \${CPPFLAGS} \${CFLAGS} \${CXXFLAGS} \${LDFLAGS} \${OPTFLAGS} -fuse-ld=lld "\${@}"
+EOF
+
+cat << EOF > cc
+#!/usr/bin/env sh
+set -e
+. __realm__/env
+exec clang -nostdlib -nostdlib++ -nostdinc -nostdinc++ \${CPPFLAGS} \${CFLAGS} \${LDFLAGS} \${OPTFLAGS} -fuse-ld=lld "\${@}"
+EOF
+
+cat << EOF > prepare-compiler.sh
+mv fix/c++ bin/
+sed -e "s|__realm__|\${PWD}|" -i bin/c++
+mv fix/cc bin/
+sed -e "s|__realm__|\${PWD}|" -i bin/cc
+EOF
+
+chmod +x *
+{% endblock %}
