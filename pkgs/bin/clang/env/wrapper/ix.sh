bf4f2207dadfc63f804eaac638d45f366fa2c951
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-12-18 18:18:15 +0300

    better

--- pkgs/bin/clang/env/wrapper/ix.sh
+++ pkgs/bin/clang/env/wrapper/ix.sh
@@ -8,31 +8,31 @@ cat << EOF > c++
 #!/usr/bin/env sh
 set -e
 . __realm__/env
-exec clang++ \${CPPFLAGS} \${CFLAGS} \${CXXFLAGS} \${LDFLAGS} \${OPTFLAGS} "\${@}"
+exec __realm__/bin/clang++ \${CPPFLAGS} \${CFLAGS} \${CXXFLAGS} \${LDFLAGS} \${OPTFLAGS} "\${@}"
 EOF
 
 cat << EOF > cc
 #!/usr/bin/env sh
 set -e
 . __realm__/env
-exec clang \${CPPFLAGS} \${CFLAGS} \${LDFLAGS} \${OPTFLAGS} "\${@}"
+exec __realm__/bin/clang \${CPPFLAGS} \${CFLAGS} \${LDFLAGS} \${OPTFLAGS} "\${@}"
 EOF
 
 cat << EOF > cpp
 #!/usr/bin/env sh
 set -e
 . __realm__/env
-exec clang-cpp \${CPPFLAGS} "\${@}"
+exec __realm__/bin/clang-cpp \${CPPFLAGS} "\${@}"
 EOF
 
 cat << EOF > prepare-compiler.sh
 mkdir -p bin
 mv fix/c++ bin/
-sed -e "s|__realm__|\${PWD}|" -i bin/c++
+sed -e "s|__realm__|\${PWD}|g" -i bin/c++
 mv fix/cc bin/
-sed -e "s|__realm__|\${PWD}|" -i bin/cc
+sed -e "s|__realm__|\${PWD}|g" -i bin/cc
 mv fix/cpp bin/
-sed -e "s|__realm__|\${PWD}|" -i bin/cpp
+sed -e "s|__realm__|\${PWD}|g" -i bin/cpp
 EOF
 
 chmod +x *
