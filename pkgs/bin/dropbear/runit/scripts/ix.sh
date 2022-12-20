2cdc580d69704b5ca08c9cdd4bfe662a9c43fd99
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-12-20 18:58:47 +0300

    add local server

---
+++ pkgs/bin/dropbear/runit/scripts/ix.sh
@@ -0,0 +1,34 @@
+{% extends '//die/proxy.sh' %}
+
+{% block install %}
+mkdir -p ${out}/etc/services/dropbear
+cd ${out}/etc/services/dropbear
+
+cat << EOF > run
+#!/bin/sh
+exec srv dropbear ${PWD}/script
+EOF
+
+cat << EOF > script
+#!/bin/sh
+
+set -xue
+
+mkdir -p /var/run/dropbear
+cd /var/run/dropbear
+
+/bin/dropbearkey -t rsa -f dropbear_rsa_host_key
+/bin/dropbearkey -t dss -f dropbear_dss_host_key
+/bin/dropbearkey -t ecdsa -f dropbear_ecdsa_host_key
+/bin/dropbearkey -t ed25519 -f dropbear_ed25519_host_key
+
+exec /bin/dropbear -E -F \
+    -r dropbear_dss_host_key     \
+    -r dropbear_rsa_host_key     \
+    -r dropbear_ecdsa_host_key   \
+    -r dropbear_ed25519_host_key \
+    -P /var/run/dropbear/pid
+EOF
+
+chmod +x run script
+{% endblock %}
