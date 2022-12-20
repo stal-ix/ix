2a819411c04a022ba2c4f75e3016117b7acc4c98
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-12-20 19:41:33 +0300

    better

--- pkgs/bin/dropbear/runit/scripts/ix.sh
+++ pkgs/bin/dropbear/runit/scripts/ix.sh
@@ -17,17 +17,16 @@ set -xue
 mkdir -p /var/run/dropbear
 cd /var/run/dropbear
 
-/bin/dropbearkey -t rsa -f dropbear_rsa_host_key
-/bin/dropbearkey -t dss -f dropbear_dss_host_key
-/bin/dropbearkey -t ecdsa -f dropbear_ecdsa_host_key
-/bin/dropbearkey -t ed25519 -f dropbear_ed25519_host_key
+/bin/dropbearkey -t rsa -f dropbear_rsa_host_key || true
+/bin/dropbearkey -t dss -f dropbear_dss_host_key || true
+/bin/dropbearkey -t ecdsa -f dropbear_ecdsa_host_key || true
+/bin/dropbearkey -t ed25519 -f dropbear_ed25519_host_key || true
 
-exec /bin/dropbear -E -F \
-    -r dropbear_dss_host_key     \
-    -r dropbear_rsa_host_key     \
-    -r dropbear_ecdsa_host_key   \
-    -r dropbear_ed25519_host_key \
-    -P /var/run/dropbear/pid
+exec /bin/dropbear -E -F -P pid \
+    -r dropbear_dss_host_key    \
+    -r dropbear_rsa_host_key    \
+    -r dropbear_ecdsa_host_key  \
+    -r dropbear_ed25519_host_key
 EOF
 
 chmod +x run script
