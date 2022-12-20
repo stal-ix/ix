3594a7ee7b2f937fa6da2823318da8f8e59af92e
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-12-21 00:43:27 +0300

    better

--- pkgs/aux/entropy/ix.sh
+++ pkgs/aux/entropy/ix.sh
@@ -7,10 +7,10 @@ bin/openssl
 {% block install %}
 mkdir -p ${out}/share
 openssl enc -aes-256-ctr \
-    -pass pass:{{entropy_seed}} \
-    -nosalt                     \
-    < /dev/zero                 \
-    | head -c {{entropy_size}}  \
+    -pass pass:"{{entropy_seed}}" \
+    -nosalt                       \
+    < /dev/zero                   \
+    | head -c {{entropy_size}}    \
     > ${out}/share/entropy
 {% endblock %}
 
