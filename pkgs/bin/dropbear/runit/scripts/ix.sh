ffa6002aa7803edda4dd87e2442e7d9979f2cb40
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-12-20 23:22:12 +0300

    fix

--- pkgs/bin/dropbear/runit/scripts/ix.sh
+++ pkgs/bin/dropbear/runit/scripts/ix.sh
@@ -17,11 +17,11 @@ set -xue
 mkdir -p /var/run/dropbear
 cd /var/run/dropbear
 
-exec /bin/dropbear -E -F -P pid      \
-    -r /bin/dropbear/dss_host_key    \
-    -r /bin/dropbear/rsa_host_key    \
-    -r /bin/dropbear/ecdsa_host_key  \
-    -r /bin/dropbear/ed25519_host_key
+exec /bin/dropbear -E -F -P pid \
+    -r /etc/keys/dss_host_key   \
+    -r /etc/keys/rsa_host_key   \
+    -r /etc/keys/ecdsa_host_key \
+    -r /etc/keys/ed25519_host_key
 EOF
 
 chmod +x run script
