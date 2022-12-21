062b3e226afb9c8bb09888ca0cc371ac6c9431ba
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-12-21 16:52:29 +0300

    upver

--- pkgs/bin/nasm/ix.sh
+++ pkgs/bin/nasm/ix.sh
@@ -1,12 +1,16 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://github.com/netwide-assembler/nasm/archive/refs/tags/nasm-2.15.05.tar.gz
-md5:2e154a96a13bf937d5247467d986bbde
+https://github.com/netwide-assembler/nasm/archive/refs/tags/nasm-2.16.tar.gz
+sha:b59da6ef9bfeaf35572398f065848991485ea5e9583b9e88365c3e941e9e5214
 {% endblock %}
 
 {% block bld_tool %}
 bld/perl
+{% endblock %}
+
+{% block conf_ver %}
+2/71
 {% endblock %}
 
 {% block autoreconf %}
