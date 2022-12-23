b344dbba69429f36e68e8b73a76783999bbcd7a3
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-12-23 15:41:49 +0300

    upver

--- pkgs/lib/device/mapper/ix.sh
+++ pkgs/lib/device/mapper/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-http://mirrors.kernel.org/sourceware/lvm2/LVM2.2.03.17.tgz
-sha:7286cfa9651828c589389509546333b8da965dfa84a1a4c8ab3e681a47fabae7
+http://mirrors.kernel.org/sourceware/lvm2/LVM2.2.03.18.tgz
+sha:9f683e2980d95c0dcebbd25c7c177032c5615d7267bfc885eabfce59280f4769
 {% endblock %}
 
 {% block lib_deps %}
