2fa4cb10e2e3df3c09b399fa7a760c991172528e
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-12-24 16:48:54 +0300

    upver

--- pkgs/lib/sigc++/3/ix.sh
+++ pkgs/lib/sigc++/3/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://download.gnome.org/sources/libsigc++/3.0/libsigc%2B%2B-3.0.7.tar.xz
-sha:bfbe91c0d094ea6bbc6cbd3909b7d98c6561eea8b6d9c0c25add906a6e83d733
+https://download.gnome.org/sources/libsigc++/3.4/libsigc%2B%2B-3.4.0.tar.xz
+sha:02e2630ffb5ce93cd52c38423521dfe7063328863a6e96d41d765a6116b8707e
 {% endblock %}
 
 {% block lib_deps %}
