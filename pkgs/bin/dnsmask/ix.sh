e767b88e0bf6377c9bca5b9713abedbfc90b34a7
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-12-26 01:59:12 +0300

    better

--- pkgs/bin/dnsmask/ix.sh
+++ pkgs/bin/dnsmask/ix.sh
@@ -5,6 +5,10 @@ https://thekelleys.org.uk/dnsmasq/dnsmasq-2.88.tar.xz
 sha:23544deda10340c053bea6f15a93fed6ea7f5aaa85316bfc671ffa6d22fbc1b3
 {% endblock %}
 
+{% block bld_tool %}
+bld/pkg/config
+{% endblock %}
+
 {% block bld_libs %}
 lib/c
 lib/gmp
@@ -15,6 +19,11 @@ lib/kernel
 lib/lua/puc/5/2
 {% endblock %}
 
+{% block setup %}
+export COPTS="-DHAVE_DBUS -DHAVE_NETTLEHASH -DHAVE_LIBIDN2 -DHAVE_LUASCRIPT"
+{% endblock %}
+
 {% block make_flags %}
+COPTS="${COPTS}"
 BINDIR=${out}/bin
 {% endblock %}
