5577bf8d171ceeadc3286fd887a8f49746a83094
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-12-25 17:46:40 +0300

    fixes

--- pkgs/bin/auto/make/1/16/1/ix.sh
+++ pkgs/bin/auto/make/1/16/1/ix.sh
@@ -2,5 +2,5 @@
 
 {% block fetch %}
 https://ftp.gnu.org/gnu/automake/automake-1.16.1.tar.xz
-sha:
+sha:5d05bb38a23fd3312b10aea93840feec685bdf4a41146e78882848165d3ae921
 {% endblock %}
--- pkgs/bin/keyd/ix.sh
+++ pkgs/bin/keyd/ix.sh
@@ -2,7 +2,7 @@
 
 {% block fetch %}
 https://github.com/rvaiya/keyd/archive/refs/tags/v2.4.2.tar.gz
-sha:77c4bf6fad2f9eef93d1ebbca2d2520f5296cec32b1a64d62cb5521ecf3eb6e1
+sha:877e1a39efaa062c996856d632d6aeedd409422be6c5bdb2064a9b707293c280
 {% endblock %}
 
 {% block bld_libs %}
