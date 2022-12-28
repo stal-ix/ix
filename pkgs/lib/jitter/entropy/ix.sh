a5c1e007331f313447a79a64a05b312f08cabafa
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-12-28 14:57:25 +0300

    add

---
+++ pkgs/lib/jitter/entropy/ix.sh
@@ -0,0 +1,28 @@
+{% extends '//die/c/make.sh' %}
+
+{% block fetch %}
+https://github.com/smuellerDD/jitterentropy-library/archive/refs/tags/v3.4.1.tar.gz
+sha:33352369092b5a0d1463c720f5a8a3e870dbd63502ea57692e7da84a65c0c5c3
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+{% endblock %}
+
+{% block setup %}
+export OPTFLAGS="${OPTFLAGS} -O0"
+{% endblock %}
+
+{% block build_flags %}
+wrap_cc
+{% endblock %}
+
+{% block bld_tool %}
+bin/gzip
+{% endblock %}
+
+{% block install %}
+{{super()}}
+cd ${out}/lib
+cp libjitterentropy.so.3.4.1 libjitterentropy.a
+{% endblock %}
