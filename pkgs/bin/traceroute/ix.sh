d3fe173660238808c76ddb5c5c5b20b19ff4f431
Anton Samokhvalov <anton@samokhvalov.xyz>
2022-12-28 14:10:46 +0300

    upver

--- pkgs/bin/traceroute/ix.sh
+++ pkgs/bin/traceroute/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/make.sh' %}
 
 {% block fetch %}
-https://downloads.sourceforge.net/project/traceroute/traceroute/traceroute-2.1.0/traceroute-2.1.0.tar.gz
-sha:3669d22a34d3f38ed50caba18cd525ba55c5c00d5465f2d20d7472e5d81603b6
+https://downloads.sourceforge.net/project/traceroute/traceroute/traceroute-2.1.1/traceroute-2.1.1.tar.gz
+sha:8fc8d5046e855d7588607bb319f5b82e3ba13e91d5d4636863262e6386bbaf76
 {% endblock %}
 
 {% block bld_libs %}
