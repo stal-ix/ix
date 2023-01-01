ef8724251c8bef1c8cf61318314e759b32e7d6ff
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-01-01 16:17:14 +0300

    upver

--- pkgs/lib/pcap/ix.sh
+++ pkgs/lib/pcap/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://www.tcpdump.org/release/libpcap-1.10.1.tar.gz
-sha:ed285f4accaf05344f90975757b3dbfe772ba41d1c401c2648b7fa45b711bdd4
+https://www.tcpdump.org/release/libpcap-1.10.2.tar.gz
+sha:db6d79d4ad03b8b15fb16c42447d093ad3520c0ec0ae3d331104dcfb1ce77560
 {% endblock %}
 
 {% block lib_deps %}
