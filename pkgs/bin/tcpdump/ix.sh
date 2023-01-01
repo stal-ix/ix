9be986f71d57c8575b3639caa48b3337f2492383
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-01-01 16:18:18 +0300

    upver

--- pkgs/bin/tcpdump/ix.sh
+++ pkgs/bin/tcpdump/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://www.tcpdump.org/release/tcpdump-4.99.1.tar.gz
-sha:79b36985fb2703146618d87c4acde3e068b91c553fb93f021a337f175fd10ebe
+https://www.tcpdump.org/release/tcpdump-4.99.2.tar.gz
+sha:f4304357d34b79d46f4e17e654f1f91f9ce4e3d5608a1badbd53295a26fb44d5
 {% endblock %}
 
 {% block lib_deps %}
