ca1d5e6acb41bb6089981a8b654a8f5f29e4ec2a
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-01-01 21:25:42 +0300

    upver

--- pkgs/bin/bc/gavin/ix.sh
+++ pkgs/bin/bc/gavin/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/make.sh' %}
 
 {% block fetch %}
-https://github.com/gavinhoward/bc/archive/refs/tags/6.2.1.tar.gz
-sha:38cbcb070dced4d96ba191be57ac5edf97d34e983379408da4ed2e318f7a6d7f
+https://github.com/gavinhoward/bc/archive/refs/tags/6.2.2.tar.gz
+sha:baf037784ed7a2f610930544327f680e76d653cd721459c891ae84eeca4276b9
 {% endblock %}
 
 {% block bld_libs %}
