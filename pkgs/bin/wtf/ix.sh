383076f7214b39842b94f071e1ecc33c7c64fce9
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-01-02 17:17:26 +0300

    fix build

--- pkgs/bin/wtf/ix.sh
+++ pkgs/bin/wtf/ix.sh
@@ -1,8 +1,15 @@
-{% extends '//die/go/c.sh' %}
+{% extends '//die/go/pure.sh' %}
 
-{% block fetch %}
+{% block go_url %}
 https://github.com/wtfutil/wtf/archive/refs/tags/v0.41.0.tar.gz
+{% endblock %}
+
+{% block go_sum %}
 sha:e01f45aaa7a1e31ab555071763da184d611f87b1800265dc782502da9c985eaf
+{% endblock %}
+
+{% block go_sha %}
+sha:6bfa1141fec6be6f6b58249bb0595f9aa7cd3978f9205a8be0f12922990be8b1
 {% endblock %}
 
 {% block go_build_flags %}
