eaf19feb1b95fcc55bf144aaf226aea443fa36a3
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-01-05 11:34:53 +0300

    better

--- pkgs/bin/bsdutils/ix.sh
+++ pkgs/bin/bsdutils/ix.sh
@@ -1,5 +1,5 @@
 {% extends '//die/hub.sh' %}
 
 {% block run_deps %}
-bin/bsdutils/lite(curses=netbsd,purec=musl/unwrap,libc=musl/pure,opt=O2;ICF/all)
+bin/bsdutils/lite(curses=netbsd,purec=musl/unwrap,libc=musl/pure)
 {% endblock %}
