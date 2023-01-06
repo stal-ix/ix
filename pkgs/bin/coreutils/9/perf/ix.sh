00c0b146160cb23c2dd95d7df7e1ee9929a3811b
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-01-06 18:54:52 +0300

    perf test

---
+++ pkgs/bin/coreutils/9/perf/ix.sh
@@ -0,0 +1,17 @@
+{% extends '//bin/coreutils/9/1/ix.sh' %}
+
+{% block box %}
+bin/sed
+bin/grep
+bin/file
+bin/patch
+bin/gawk
+bin/bsdutils/lite
+{% endblock %}
+
+{% block configure %}
+date
+{{super()}}
+date
+exit 1
+{% endblock %}
