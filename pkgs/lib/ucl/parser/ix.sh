0ae1afdc890248ba4b4f84684dd433978a9ec597
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-01-08 18:35:19 +0300

    upver

--- pkgs/lib/ucl/parser/ix.sh
+++ pkgs/lib/ucl/parser/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://github.com/vstakhov/libucl/archive/refs/tags/0.8.1.tar.gz
-sha:a6397e179672f0e8171a0f9a2cfc37e01432b357fd748b13f4394436689d24ef
+https://github.com/vstakhov/libucl/archive/refs/tags/0.8.2.tar.gz
+sha:d95a0e2151cc167a0f3e51864fea4e8977a0f4c473faa805269a347f7fb4e165
 {% endblock %}
 
 {% block lib_deps %}
