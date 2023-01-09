0fb525404c756ca40cc3aeb785fcf1eb24b00f67
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-01-09 20:54:29 +0300

    better

--- pkgs/bin/busybox/ntpd/ix.sh
+++ pkgs/bin/busybox/ntpd/ix.sh
@@ -1,31 +1,7 @@
-{% extends '//die/proxy.sh' %}
+{% extends '//die/hub.sh' %}
 
 {% block run_deps %}
-# TODO(pg): move to upper level
+bin/sched
 bin/busybox
-bin/sched(delay={{delay}})
-{% endblock %}
-
-{% block install %}
-cd ${out}
-
-mkdir etc; cd etc
-
-cat << EOF > ntp.conf
-server 0.asia.pool.ntp.org
-server 0.oceania.pool.ntp.org
-server 0.europe.pool.ntp.org
-server 0.north-america.pool.ntp.org
-server 2.south-america.pool.ntp.org
-driftfile /var/run/ntpd/ntp.drift
-pidfile   /var/run/ntpd/ntp.pid
-leapfile  /var/run/ntpd/ntp.leapseconds
-EOF
-
-mkdir -p sche.d/{{delay}}; cd sche.d/{{delay}}
-
-cat << EOF > ntpd.sh
-ntpd -dd -n -q
-hwclock -u -w
-EOF
+bin/busybox/ntpd/scripts
 {% endblock %}
---
+++ pkgs/bin/busybox/ntpd/scripts/ix.sh
@@ -0,0 +1,25 @@
+{% extends '//die/proxy.sh' %}
+
+{% block install %}
+cd ${out}
+
+mkdir etc; cd etc
+
+cat << EOF > ntp.conf
+server 0.asia.pool.ntp.org
+server 0.oceania.pool.ntp.org
+server 0.europe.pool.ntp.org
+server 0.north-america.pool.ntp.org
+server 2.south-america.pool.ntp.org
+driftfile /var/run/ntpd/ntp.drift
+pidfile   /var/run/ntpd/ntp.pid
+leapfile  /var/run/ntpd/ntp.leapseconds
+EOF
+
+mkdir -p sche.d/{{delay}}; cd sche.d/{{delay}}
+
+cat << EOF > ntpd.sh
+ntpd -dd -n -q
+hwclock -u -w
+EOF
+{% endblock %}
