bfdb9d7dd5794a5875d4d05b6ed14612271d7741
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-01-10 22:01:04 +0300

    upver

--- pkgs/lib/cerf/ix.sh
+++ pkgs/lib/cerf/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://jugit.fz-juelich.de/mlz/libcerf/-/archive/v2.2/libcerf-v2.2.tar.bz2
-sha:32919dff5d2e260f5319e6df30577caa43d004d6b0d455db88ee844d08329c36
+https://jugit.fz-juelich.de/mlz/libcerf/-/archive/v2.3/libcerf-v2.3.tar.bz2
+sha:5fce0a90dc30d0ac7ac90d6df752080d2d346cd49154706e4397e2a9a3b826f7
 {% endblock %}
 
 {% block lib_deps %}
