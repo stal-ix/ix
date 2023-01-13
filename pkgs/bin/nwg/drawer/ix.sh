d09e17ca1044581a6879a55c9f05c004d1a9f39b
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-01-13 18:34:37 +0300

    upver

--- pkgs/bin/nwg/drawer/ix.sh
+++ pkgs/bin/nwg/drawer/ix.sh
@@ -1,15 +1,15 @@
 {% extends '//bin/nwg/t/ix.sh' %}
 
 {% block go_url %}
-https://github.com/nwg-piotr/nwg-drawer/archive/refs/tags/v0.3.6.tar.gz
+https://github.com/nwg-piotr/nwg-drawer/archive/refs/tags/v0.3.7.tar.gz
 {% endblock %}
 
 {% block go_sum %}
-sha:a2c1fe63b905dbe91afd231c92fe78086d3f916363663751b646e6e9faefba49
+sha:083df513265cab03095461e8e0e8582efb917690ddad572959dd00972ca0abd1
 {% endblock %}
 
 {% block go_sha %}
-sha:271d33914a8cc258979529cff9c496003f7179d016e8c21237a27561e9759e60
+sha:1cd17354a04aab0f78900d13294a707494c7f9c7dc284deb5410c379f4d1d183
 {% endblock %}
 
 {% block install %}
