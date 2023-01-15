ef92578c91ca546ab84a051fe58519f5430551af
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-01-15 15:23:58 +0300

    upver

--- pkgs/lib/mpg123/t/ix.sh
+++ pkgs/lib/mpg123/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://www.mpg123.org/download/mpg123-1.31.1.tar.bz2
-sha:5dcb0936efd44cb583498b6585845206f002a7b19d5066a2683be361954d955a
+https://www.mpg123.org/download/mpg123-1.31.2.tar.bz2
+sha:b17f22905e31f43b6b401dfdf6a71ed11bb7d056f68db449d70b9f9ae839c7de
 {% endblock %}
 
 {% block lib_deps %}
