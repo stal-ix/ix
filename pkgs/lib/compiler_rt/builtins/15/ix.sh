63aee5beed06af6db1d52202110eda5a896b55b0
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-01-15 21:26:29 +0300

    fix build

--- pkgs/lib/compiler_rt/builtins/15/ix.sh
+++ pkgs/lib/compiler_rt/builtins/15/ix.sh
@@ -3,3 +3,10 @@
 {% block fetch %}
 {% include '//lib/llvm/15/ver.sh' %}
 {% endblock %}
+
+{% block cpp_defines %}
+{{super()}}
+{% if target.arch == 'riscv64' %}
+__NR_riscv_flush_icache=0
+{% endif %}
+{% endblock %}
