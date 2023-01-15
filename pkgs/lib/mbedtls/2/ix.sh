7472a93b59d81127c1cc074c3cab7216bdbace1e
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-01-15 22:17:02 +0300

    upver

--- pkgs/lib/mbedtls/2/ix.sh
+++ pkgs/lib/mbedtls/2/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//lib/mbedtls/t/ix.sh' %}
 
 {% block fetch %}
-https://github.com/ARMmbed/mbedtls/archive/refs/tags/v2.27.0.tar.gz
-md5:f0ca42d59198e4f0695229f193b434c7
+https://github.com/ARMmbed/mbedtls/archive/refs/tags/v2.28.2.tar.gz
+sha:bc55232bf71fd66045122ba9050a29ea7cb2e8f99b064a9e6334a82f715881a0
 {% endblock %}
 
 {% block lib_deps %}
