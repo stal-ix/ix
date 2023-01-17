3d4ad198a5ffce4393de7a6fb62ead9bb738c6a1
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-01-17 23:02:26 +0300

    upver

--- pkgs/bin/porto/ix.sh
+++ pkgs/bin/porto/ix.sh
@@ -1,30 +1,36 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/yandex/porto/archive/refs/tags/v4.20.8.tar.gz
-sha:0fa0887ade6a98c3d4f591c9d6c35816d251b054b60e8955ab31636611fca10b
-{% endblock %}
-
-{% block setup_tools %}
-cat << EOF > pandoc
-#!/bin/sh
-EOF
-
-chmod +x pandoc
+https://github.com/yandex/porto/archive/refs/tags/v5.3.9.tar.gz
+sha:3c742d7ee08f1d97d6855517d9250f0a88b2b50d25192728004777896e97e187
 {% endblock %}
 
 {% block lib_deps %}
 lib/c
 lib/nl
 lib/c++
+lib/fts
 lib/curses
+lib/kernel
 lib/protobuf
 {% endblock %}
 
 {% block bld_tool %}
 bin/protoc
+bld/fake(tool_name=pandoc)
 {% endblock %}
 
 {% block build_flags %}
 shut_up
+{% endblock %}
+
+{% block cpp_missing %}
+sys/time.h
+unistd.h
+{% endblock %}
+
+{% block cmake_flags %}
+ENABLE_GO=OFF
+ENABLE_PYTHON=OFF
+ENABLE_PORTODSHIM=OFF
 {% endblock %}
