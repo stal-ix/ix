cac537e3c11d0cb918cbbb7d75e80d8dc242f090
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-01-17 11:09:28 +0300

    upver

--- pkgs/bin/redis/ix.sh
+++ pkgs/bin/redis/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/make.sh' %}
 
 {% block fetch %}
-https://github.com/redis/redis/archive/refs/tags/7.0.7.tar.gz
-sha:7a46c27a143cab7eed1ba3965ab69cd2bfc9984b94ddb916c56548c310342e98
+https://github.com/redis/redis/archive/refs/tags/7.0.8.tar.gz
+sha:0e439cbc19f6db5a4c63d355519ab73bf6ac2ecd47df806c14b19564b3d0c593
 {% endblock %}
 
 {% block bld_libs %}
