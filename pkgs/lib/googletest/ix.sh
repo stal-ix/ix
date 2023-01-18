ed221b2cbb9b137e8c11e337a0bab2f633bb2b83
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-01-18 21:01:16 +0300

    upver

--- pkgs/lib/googletest/ix.sh
+++ pkgs/lib/googletest/ix.sh
@@ -1,12 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/google/googletest/archive/refs/tags/release-1.12.1.tar.gz
-sha:81964fe578e9bd7c94dfdb09c8e4d6e6759e19967e397dbea48d1c10e45d0df2
-{% endblock %}
-
-{% block build_flags %}
-shut_up
+https://github.com/google/googletest/archive/refs/tags/v1.13.0.tar.gz
+sha:ad7fdba11ea011c1d925b3289cf4af2c66a352e18d4c7264392fead75e919363
 {% endblock %}
 
 {% block lib_deps %}
