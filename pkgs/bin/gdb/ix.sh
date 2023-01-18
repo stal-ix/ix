ef77390b3bf5e49ca3c64e9388579bbc73b91cf9
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-01-18 20:52:28 +0300

    fix build

--- pkgs/bin/gdb/ix.sh
+++ pkgs/bin/gdb/ix.sh
@@ -4,17 +4,7 @@
 lib/c
 {% endblock %}
 
-{% block bld_libs %}
-bin/gdb/shim
-{{super()}}
-{% endblock %}
-
 {% block patch %}
 {{super()}}
 sed -e 's|return rl_completer_word_break_characters|return (char*)rl_completer_word_break_characters|' -i gdb/completer.c
-#sed -e 's|SVE_SIG_ZREGS_SIZE|XXXYYY|' -i gdb/nat/aarch64-sve-linux-ptrace.h
-cat << EOF > gdbserver/Makefile.in
-all:
-install:
-EOF
 {% endblock %}
