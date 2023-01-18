8aaa67afbc3a15216c13b070f1d544a40e80ae4f
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-01-18 15:14:17 +0300

    cross

--- pkgs/lib/curses/netbsd/ix.sh
+++ pkgs/lib/curses/netbsd/ix.sh
@@ -7,6 +7,7 @@ sha:9d3ebd651e5f70b87b1327b01cbd7e0c01a0f036b4c1371f653b7704b11daf23
 
 {% block cpp_defines %}
 __pure=
+TERMINFO_COMPAT=1
 {% endblock %}
 
 {% block patch %}
@@ -20,6 +21,7 @@ sed -e 's|fpurge|curses_fpurge|' -i libcurses/tty.c
 
 {% block make_flags %}
 STATIC_BINS=yes
+HOSTCC=${HOST_CC}
 {% endblock %}
 
 {% block make_target %}
@@ -35,5 +37,9 @@ export COFLAGS="--with-curses=${out} --with-ncurses=${out} \${COFLAGS}"
 {% endblock %}
 
 {% block lib_deps %}
+lib/c
+{% endblock %}
+
+{% block host_libs %}
 lib/c
 {% endblock %}
