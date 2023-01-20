cf92627a137b65ef95d2b13765bcd838f71bd8fb
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-01-20 20:15:40 +0300

    upver

--- pkgs/bin/mold/ix.sh
+++ pkgs/bin/mold/ix.sh
@@ -1,37 +1,6 @@
-{% extends '//die/c/cmake.sh' %}
+{% extends '//die/hub.sh' %}
 
-{% block fetch %}
-https://github.com/rui314/mold/archive/refs/tags/v1.7.1.tar.gz
-sha:fa2558664db79a1e20f09162578632fa856b3cde966fbcb23084c352b827dfa9
-{% endblock %}
-
-{% block bld_libs %}
-lib/z
-lib/c
-lib/c++
-lib/zstd
-lib/xxhash
-lib/openssl
-lib/intel/tbb
-{% endblock %}
-
-{% block cmake_flags %}
-MOLD_LTO=OFF
-MOLD_USE_MOLD=OFF
-MOLD_USE_MIMALLOC=OFF
-MOLD_MOSTLY_STATIC=ON
-MOLD_USE_SYSTEM_TBB=ON
-{% endblock %}
-
-{% block patch %}
-rm -r third-party/tbb
-rm -r third-party/xxhash
-rm -r third-party/mimalloc
-
-find . -type f | while read l; do
-    sed -e 's|mold-wrapper SHARED|mold-wrapper STATIC|' \
-        -e 's|xxhash/xxhash.h|xxhash.h|' \
-        -e 's|third-party/xxhash.h|xxhash.h|' \
-        -i ${l}
-done
+{% block run_deps %}
+# actually, for_target
+bin/mold/{{host.os}}
 {% endblock %}
---
+++ pkgs/bin/mold/linux/ix.sh
@@ -0,0 +1,6 @@
+{% extends '//bin/mold/darwin/ix.sh' %}
+
+{% block fetch %}
+https://github.com/rui314/mold/archive/refs/tags/v1.10.0.tar.gz
+sha:bcf59e21cebf2b7e7346ccfd1803865e411b8a35b5a2cb581936c4ace3d2bac8
+{% endblock %}
