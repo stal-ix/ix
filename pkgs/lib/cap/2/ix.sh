7c4f512c57c9c0c84016ea214552faa990d84a45
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-01-21 22:55:22 +0300

    cross

--- pkgs/lib/cap/2/ix.sh
+++ pkgs/lib/cap/2/ix.sh
@@ -5,7 +5,17 @@ https://www.kernel.org/pub/linux/libs/security/linux-privs/libcap2/libcap-2.65.t
 sha:73e350020cc31fe15360879d19384ffa3395a825f065fcf6bda3a5cdf965bebd
 {% endblock %}
 
+{% block host_libs %}
+{{super()}}
+lib/c
+{% endblock %}
+
 {% block unpack %}
 {{super()}}
 cd libcap
+{% endblock %}
+
+{% block make_flags %}
+{{super()}}
+BUILD_CC=${HOST_CC}
 {% endblock %}
