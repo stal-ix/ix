a3c94806ec344a3f87e147bac8edf9fe1e613be0
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-01-21 15:15:56 +0300

    fix build

--- pkgs/lib/raw/ix.sh
+++ pkgs/lib/raw/ix.sh
@@ -10,3 +10,17 @@ lib/c
 lib/jasper
 lib/lcms/2
 {% endblock %}
+
+{% block install %}
+{{super()}}
+for x in ${out}/lib/pkgconfig/*.pc; do
+    sed -e 's|-lstdc++||g' -i ${x}
+done
+{% endblock %}
+
+{% block test %}
+cat ${out}/lib/pkgconfig/* | grep 'stdc' | while read l; do
+    echo 'FIX libstdc++'
+    exit 1
+done
+{% endblock %}
