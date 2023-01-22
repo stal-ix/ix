91ff270b032cf8ec16493c34bddf2fa3190b43d2
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-01-22 10:02:44 +0300

    better

--- pkgs/bin/mold/linux/ix.sh
+++ pkgs/bin/mold/linux/ix.sh
@@ -1,6 +1,6 @@
 {% extends '//bin/mold/darwin/ix.sh' %}
 
 {% block fetch %}
-https://github.com/rui314/mold/archive/refs/tags/v1.10.0.tar.gz
-sha:bcf59e21cebf2b7e7346ccfd1803865e411b8a35b5a2cb581936c4ace3d2bac8
+https://github.com/rui314/mold/archive/refs/tags/v1.10.1.tar.gz
+sha:19e4aa16b249b7e6d2e0897aa1843a048a0780f5c76d8d7e643ab3a4be1e4787
 {% endblock %}
