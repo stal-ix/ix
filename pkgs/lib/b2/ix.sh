76e576a1f4641c39728db8ce849723f542bbf6a2
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-01-22 19:03:34 +0300

    cross

--- pkgs/lib/b2/ix.sh
+++ pkgs/lib/b2/ix.sh
@@ -11,5 +11,17 @@ lib/c
 
 {% block configure_flags %}
 --disable-native
+{% if x86_64 %}
 --enable-fat
+{% else %}
+--disable-fat
+{% endif %}
+{% endblock %}
+
+{% block configure %}
+{% if aarch64 %}
+# https://github.com/BLAKE2/libb2/blob/master/configure.ac#L87
+export ax_cv_have_ssse3_ext=no
+{% endif %}
+{{super()}}
 {% endblock %}
