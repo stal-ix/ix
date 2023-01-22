a990d525c4bddbb551d3d81e878fade0a2afc447
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-01-22 18:36:45 +0300

    ci

--- pkgs/set/ci/unwrap/linux/aarch64/ix.sh
+++ pkgs/set/ci/unwrap/linux/aarch64/ix.sh
@@ -1,5 +1,17 @@
 {% extends '//die/hub.sh' %}
 
 {% block run_deps %}
+bin/iwd
 bin/gdb
+bin/git
+bin/dash
+bin/bash
+bin/curl
+bin/wget
+bin/foot
+bin/make
+bin/ninja
+bin/bsdtar
+set/box/gnu
+bin/coreutils
 {% endblock %}
