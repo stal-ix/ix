f72e0fde13c7ea94b2ac7e18fca65b4e005ce551
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-01-22 19:26:43 +0300

    cross

--- pkgs/bin/openssh/client/ix.sh
+++ pkgs/bin/openssh/client/ix.sh
@@ -1,5 +1,11 @@
 {% extends '//bin/openssh/t/ix.sh' %}
 
+{% block patch %}
+{{super()}}
+sed -e 's|host-key:|host-key-xxx:|' -i Makefile.in
+echo 'host-key:' >> Makefile.in
+{% endblock %}
+
 {% block install %}
 {{super()}}
 rm -r ${out}/etc ${out}/share
