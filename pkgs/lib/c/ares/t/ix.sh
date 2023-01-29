29f9663865e747434186430e9b257ad47337e45a
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-01-29 12:26:04 +0300

    upver

--- pkgs/lib/c/ares/t/ix.sh
+++ pkgs/lib/c/ares/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/c-ares/c-ares/archive/refs/tags/cares-1_18_1.tar.gz
-sha:414872549eec4e221b576693fdc9c9bce44ff794d0f1f06f2515b56a7f6ec9c9
+https://github.com/c-ares/c-ares/archive/refs/tags/cares-1_19_0.tar.gz
+sha:948016368481b6c5063b849b6dec2a7fd659eee2174b7f3db22ff1b22055ed2a
 {% endblock %}
 
 {% block lib_deps %}
