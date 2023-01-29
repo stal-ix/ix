991b216f683ff49b594247d2571f99dc6af385b7
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-01-29 12:33:15 +0300

    better

--- pkgs/lib/image/magick/t/ix.sh
+++ pkgs/lib/image/magick/t/ix.sh
@@ -1,7 +1,7 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://imagemagick.org/archive/ImageMagick-7.1.0-59.tar.xz
+https://imagemagick.org/archive/releases/ImageMagick-7.1.0-59.tar.xz
 sha:ee2247f2ad8798e674de7fba37e7f3986f5b6be9abec042ab00b0befa8ae5b70
 {% endblock %}
 
