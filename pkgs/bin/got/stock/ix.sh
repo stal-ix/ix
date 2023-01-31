0fb9640c3287db578813918689e177a150f6f974
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-01-31 23:53:48 +0300

    upver

--- pkgs/bin/got/stock/ix.sh
+++ pkgs/bin/got/stock/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
-https://gameoftrees.org/releases/portable/got-portable-0.82.tar.gz
-sha:2dbd16678813b86fc60409c487d7dffcad1c8b08c35f772612122d33227f9662
+https://gameoftrees.org/releases/portable/got-portable-0.83.tar.gz
+sha:90d854e8e47d21434f2fbd83f749e1ab65f9be6556ed8526a67abf10e52f1bff
 {% endblock %}
 
 {% block bld_libs %}
