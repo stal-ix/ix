3f65c6b18c4d95e30525963fed8f5b8ffe5036e2
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-01 22:35:51 +0300

    doc

--- pkgs/lib/shim/utmp/ix.sh
+++ pkgs/lib/shim/utmp/ix.sh
@@ -8,6 +8,12 @@ lib/c
 login.c
 {% endblock %}
 
+{#
+logsys.c:177:9: error: call to undeclared function 'login';
+ISO C99 and later do not support implicit function declarations [-Wimplicit-function-declaration
+login(&ut);
+#}
+
 {% block env %}
 export CFLAGS="-Wno-implicit-function-declaration \${CFLAGS}"
 {% endblock %}
