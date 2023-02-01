9a68ab6fa2b9e11c95e72b8fc8ff6652cd8f0c86
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-01 16:36:55 +0300

    temp fix

--- pkgs/bin/telegram/desktop/ix.sh
+++ pkgs/bin/telegram/desktop/ix.sh
@@ -3,5 +3,5 @@
 {% block run_deps %}
 bin/xdg/open
 bin/telegram/desktop/extra
-bin/telegram/desktop/unwrap(allocator=tcmalloc)
+bin/telegram/desktop/unwrap(allocator=tcmalloc,clang_ver=15)
 {% endblock %}
--- pkgs/bld/compiler/unwrap/ix.sh
+++ pkgs/bld/compiler/unwrap/ix.sh
@@ -1,5 +1,9 @@
 {% extends '//die/hub.sh' %}
 
 {% block run_deps %}
-bin/clang/{{clang_ver or '16'}}(std_env=bld/boot/8/env/cxx)
+{% if clang_ver %}
+bin/clang/{{clang_ver}}(clang_ver=)
+{% else %}
+bin/clang/16(std_env=bld/boot/8/env/cxx)
+{% endif %}
 {% endblock %}
