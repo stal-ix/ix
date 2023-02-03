ca89da744f4ea97161c5d46be72935f1a4960b7f
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-03 14:48:44 +0300

    refac

--- pkgs/bin/go/20/t/ix.sh
+++ pkgs/bin/go/20/t/ix.sh
@@ -1,53 +1,6 @@
-{% extends '//bin/go/4/ix.sh' %}
+{% extends '//bin/go/19/t/ix.sh' %}
 
 {% block fetch %}
 https://github.com/golang/go/archive/refs/tags/go1.20.tar.gz
 sha:3094dd1d21843f0ddf0b14e45a6099cb9eb2c97c874d304e70289f1c7525671f
-{% endblock %}
-
-{% block bld_libs %}
-{% endblock %}
-
-{% block bld_tool %}
-bld/tar
-bld/perl
-bld/python
-{{super()}}
-{% endblock %}
-
-{% block use_data %}
-aux/ca-bundle
-{% endblock %}
-
-{% block setup_tools %}
-cat << EOF > xcrun
-#!$(which python3)
-import sys
-import subprocess
-
-subprocess.check_call(sys.argv[1:])
-EOF
-
-chmod +x xcrun
-{% endblock %}
-
-{% block patch %}
-{{super()}}
-find . -type f -name '*.go' | while read l; do
-    sed -e "s|/etc/.*/ca-bundle.crt|${CA_BUNDLE}|" -i ${l}
-done
-{% endblock %}
-
-{% block build %}
-cp -R ${GOROOT_BOOTSTRAP} ${tmp}/boot
-
-find ${tmp}/boot | while read l; do
-    chmod +rwx ${l}
-done
-
-export GOCACHE=${tmp}
-export GOROOT=${tmp}/boot
-export GOROOT_BOOTSTRAP=${tmp}/boot
-
-{{super()}}
 {% endblock %}
