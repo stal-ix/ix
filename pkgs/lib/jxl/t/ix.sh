143897fd1575050c93c48a25065c63fdc8399d26
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-04 11:28:58 +0300

    upver

--- pkgs/lib/jxl/t/ix.sh
+++ pkgs/lib/jxl/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/libjxl/libjxl/archive/refs/tags/v0.8.0.tar.gz
-sha:6b4c140c1738acbed6b7d22858e0526373f0e9938e3f6c0a6b8943189195aad1
+https://github.com/libjxl/libjxl/archive/refs/tags/v0.8.1.tar.gz
+sha:60f43921ad3209c9e180563025eda0c0f9b1afac51a2927b9ff59fff3950dc56
 {% endblock %}
 
 {% block lib_deps %}
@@ -15,15 +15,20 @@ lib/highway
 {% endblock %}
 
 {% block cmake_flags %}
+JPEGXL_STATIC=ON
+JPEGXL_EMSCRIPTEN=OFF
+
 JPEGXL_ENABLE_BENCHMARK=OFF
 JPEGXL_ENABLE_EXAMPLES=OFF
 JPEGXL_ENABLE_PLUGINS=OFF
-JPEGXL_STATIC=ON
-JPEGXL_FORCE_SYSTEM_BROTLI=ON
-JPEGXL_FORCE_SYSTEM_LCMS2=ON
 JPEGXL_ENABLE_SKCMS=OFF
 JPEGXL_ENABLE_SJPEG=OFF
 JPEGXL_ENABLE_VIEWERS=OFF
-JPEGXL_EMSCRIPTEN=OFF
+
 JPEGXL_BUNDLE_LIBPNG=OFF
+
+JPEGXL_FORCE_SYSTEM_BROTLI=ON
+JPEGXL_FORCE_SYSTEM_LCMS2=ON
+JPEGXL_FORCE_SYSTEM_GTEST=ON
+JPEGXL_FORCE_SYSTEM_HWY=ON
 {% endblock %}
