1d64ea965a5ae496a561d0843f93d467026a8e40
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-04 20:10:16 +0300

    upver

--- pkgs/bin/chroma/ix.sh
+++ pkgs/bin/chroma/ix.sh
@@ -1,15 +1,15 @@
 {% extends '//die/go/pure.sh' %}
 
 {% block go_url %}
-https://github.com/alecthomas/chroma/archive/refs/tags/v2.2.0.tar.gz
+https://github.com/alecthomas/chroma/archive/refs/tags/v2.5.0.tar.gz
 {% endblock %}
 
 {% block go_sum %}
-sha:6870cb8c47897f2a75abaf3e6cbb160989b4e915b86fdcd9322fb17e073ccd81
+sha:ad0bce3d6ba4397a866e5c8728c146fb370a2b0adb1d35fccdf7d515a9c7f553
 {% endblock %}
 
 {% block go_sha %}
-sha:b45ddb93e968fb7bd865710ce1c6fe8c33625c504c940c463a9a0e962db5c345
+sha:9dddebc23f50cbc1c101e99448692e84f0e037bf136e9c19f578450e671d6f5e
 {% endblock %}
 
 {% block unpack %}
