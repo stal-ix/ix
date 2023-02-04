96168bd0f85a967c553b7adfe2f08e240276da83
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-04 19:39:17 +0300

    upver

--- pkgs/lib/openal/t/ix.sh
+++ pkgs/lib/openal/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/kcat/openal-soft/archive/refs/tags/1.22.2.tar.gz
-sha:3e58f3d4458f5ee850039b1a6b4dac2343b3a5985a6a2e7ae2d143369c5b8135
+https://github.com/kcat/openal-soft/archive/refs/tags/1.23.0.tar.gz
+sha:af2abf9cb539c9d66296a83ba63a75eb5868658d0f0b28b27c556f45e70c5231
 {% endblock %}
 
 {% block lib_deps %}
@@ -16,6 +16,17 @@ lib/kernel
 {% endblock %}
 
 {% block cmake_flags %}
+{% if x86_64 %}
+ALSOFT_REQUIRE_SSE=ON
+ALSOFT_REQUIRE_SSE2=ON
+ALSOFT_REQUIRE_SSE3=ON
+ALSOFT_REQUIRE_SSE4_1=ON
+{% endif %}
+
+ALSOFT_BACKEND_OSS=OFF
+ALSOFT_BACKEND_SNDIO=ON
+ALSOFT_BACKEND_WAVE=ON
+
 ALSOFT_DLOPEN=OFF
 ALSOFT_EXAMPLES=OFF
 {% endblock %}
