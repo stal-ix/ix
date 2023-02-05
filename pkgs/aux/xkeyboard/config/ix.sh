d0a3e16d1c3e63a8dc75b07a35c3e8bddf34d1d5
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-05 15:24:28 +0300

    upver

--- pkgs/aux/xkeyboard/config/ix.sh
+++ pkgs/aux/xkeyboard/config/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://github.com/freedesktop/xkeyboard-config/archive/refs/tags/xkeyboard-config-2.37.tar.gz
-sha:3f578ae674d9a01a6d0bba23f3ff55b9e07c335e7bb81e9eb192eb8efde0176b
+https://gitlab.freedesktop.org/xkeyboard-config/xkeyboard-config/-/archive/xkeyboard-config-2.38/xkeyboard-config-xkeyboard-config-2.38.tar.bz2
+sha:5ab92ce8099eee66622808954e08d0e8dea3d254df100c7f636ed6cd68e8d9cd
 {% endblock %}
 
 {% block bld_tool %}
