d76d5128bbaa5ec6b597cab3d9d4b273497b4368
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-06 14:19:47 +0300

    upver

--- pkgs/bin/kernel/6/1/t/ix.sh
+++ pkgs/bin/kernel/6/1/t/ix.sh
@@ -1,6 +1,6 @@
 {% extends '//bin/kernel/t/2/ix.sh' %}
 
-{% block kernel_version %}6-1-9{% endblock %}
+{% block kernel_version %}6-1-10{% endblock %}
 
 {% block fetch %}
 {% include 'ver.sh' %}
--- pkgs/bin/kernel/6/1/t/ver.sh
+++ pkgs/bin/kernel/6/1/t/ver.sh
@@ -1,2 +1,2 @@
-https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.1.9.tar.xz
-sha:d60cf185693c386e7acd9f3eb3a94ae30ffbfee0a9447a20e83711e0bdf5922b
+https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.1.10.tar.xz
+sha:0be2919ba91cf5873a4cb4d429de78aad0469120d624e333a43b4b011d74d19d
