f770252845477af55db88cbb58a72fcf362d72e6
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-08 13:24:54 +0300

    better

--- pkgs/bin/transmission/t/ix.sh
+++ pkgs/bin/transmission/t/ix.sh
@@ -1,7 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-{% include 'ver.sh' %}
+https://github.com/transmission/transmission/archive/refs/tags/4.0.0.tar.gz
+sha:f6419cbf12178d08dce3be6d2f1a391d297544d03c8020532badea4f31bf5101
 {% endblock %}
 
 {% block bld_libs %}
