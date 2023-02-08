f73a131ca5c4d7a24e31e7a91306eb390f92dcf0
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-08 14:30:53 +0300

    better

--- pkgs/bin/showfont/ix.sh
+++ pkgs/bin/showfont/ix.sh
@@ -1,4 +1,4 @@
-{% extends '//lib/sdl/2/ttf/ix.sh' %}
+{% extends '//lib/sdl/2/ttf/t/ix.sh' %}
 
 {% block bld_libs %}
 lib/glu
@@ -6,4 +6,10 @@ lib/shim/fake(lib_name=OpenGL)
 lib/shim/fake(lib_name=GLX)
 lib/sdl/deps
 {{super()}}
+{% endblock %}
+
+{% block install %}
+mkdir ${out}/bin
+cp ${tmp}/obj/glfont ${out}/bin/
+cp ${tmp}/obj/showfont ${out}/bin/
 {% endblock %}
---
+++ pkgs/lib/sdl/2/ttf/t/ix.sh
@@ -0,0 +1,17 @@
+{% extends '//die/c/cmake.sh' %}
+
+{% block fetch %}
+https://github.com/libsdl-org/SDL_ttf/archive/refs/tags/release-2.20.2.tar.gz
+sha:0fe9d587cdc4e6754b647536d0803bea8ca6ac77146c4209e0bed22391cf8241
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+lib/sdl/2
+lib/freetype
+lib/harfbuzz
+{% endblock %}
+
+{% block cmake_flags %}
+SDL2TTF_HARFBUZZ=ON
+{% endblock %}
