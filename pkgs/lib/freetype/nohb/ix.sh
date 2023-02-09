7c8c5982fd1c55fbd0347d84c821ed0c5a3d6fa7
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-09 14:44:18 +0300

    upver

--- pkgs/lib/freetype/nohb/ix.sh
+++ pkgs/lib/freetype/nohb/ix.sh
@@ -1,14 +1,14 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://download.savannah.gnu.org/releases/freetype/freetype-2.12.1.tar.xz
-sha:4766f20157cc4cf0cd292f80bf917f92d1c439b243ac3018debf6b9140c41a7f
+https://github.com/freetype/freetype/archive/refs/tags/VER-2-13-0.tar.gz
+sha:a683f1091aee95d2deaca9292d976f87415610b8ae1ea186abeebcb08e83ab12
 {% endblock %}
 
 {% block lib_deps %}
 lib/c
 lib/z
 lib/png
-lib/bzip/2
 lib/brotli
+lib/bzip/2
 {% endblock %}
