62db78b2e3d96f732876bcf4d7557dc3155b42c2
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-09 17:07:24 +0300

    build it

--- pkgs/bin/ft2demos/ix.sh
+++ pkgs/bin/ft2demos/ix.sh
@@ -1,13 +1,23 @@
 {% extends '//die/c/meson.sh' %}
 
-{# broken, need internal freetype headers #}
-
 {% block fetch %}
-https://gitlab.freedesktop.org/freetype/freetype-demos/-/archive/VER-2-12-0/freetype-demos-VER-2-12-0.tar.bz2
-sha:47a64eba92f0fe39a0e9a940dbe2a49b9fb20a6c89d2cb0681d63897f37c6db0
+https://gitlab.freedesktop.org/freetype/freetype-demos/-/archive/VER-2-13-0/freetype-demos-VER-2-13-0.tar.bz2
+sha:afcc45109122cc4c805b20883033423e7740baf6d54b180bf9568f34790e9e3b
 {% endblock %}
 
 {% block bld_libs %}
 lib/c
 lib/freetype
+{% endblock %}
+
+{% block meson_flags %}
+librsvg=disabled
+{% endblock %}
+
+{% block patch %}
+sed -e 's|libfreetype2 = subproject|libfreetype2 = dependency|' -i meson.build
+sed -e 's|.*get_variable.*freetype_dep.*|libfreetype2_dep = libfreetype2|' -i meson.build
+sed -e 's|.*freetype2.*truetype.*.*||' -i meson.build
+echo 'int main() {}' > src/ftvalid.c
+echo 'int main() {}' > src/ttdebug.c
 {% endblock %}
