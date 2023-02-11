79eac7b81e57eba6a14e7c2364c0cb9d1cf27c45
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-11 13:09:58 +0300

    upver

--- pkgs/lib/wlroots/16/ix.sh
+++ pkgs/lib/wlroots/16/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//lib/wlroots/15/ix.sh' %}
 
 {% block fetch %}
-https://gitlab.freedesktop.org/wlroots/wlroots/-/archive/0.16.1/wlroots-0.16.1.tar.bz2
-sha:1ea948d044d7c475f91c89719986482fe427eb48223cfe2ddf9426ff60b94674
+https://gitlab.freedesktop.org/wlroots/wlroots/-/archive/0.16.2/wlroots-0.16.2.tar.bz2
+sha:afea2cc740344c4e86749cf4908e07692e183cb14a3db854d24dec454c664b88
 {% endblock %}
 
 {% block bld_data %}
