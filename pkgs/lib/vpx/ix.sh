05d67864ec19e40bd6dd72b52fa8c3e82bbd2d3e
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-11 13:08:30 +0300

    upver

--- pkgs/lib/vpx/ix.sh
+++ pkgs/lib/vpx/ix.sh
@@ -1,11 +1,9 @@
 {% extends '//die/c/make.sh' %}
 
 {% block fetch %}
-https://chromium.googlesource.com/webm/libvpx/+archive/03265cd42b3783532de72f2ded5436652e6f5ce3.tar.gz
-sem:5cb5e79c7ff32eda78123ed8132005ee25c7b5a00d276d86b220d07050010547
+https://github.com/webmproject/libvpx/archive/refs/tags/v1.13.0.tar.gz
+sha:cb2a393c9c1fae7aba76b950bb0ad393ba105409fe1a147ccd61b0aaa1501066
 {% endblock %}
-
-{% block skip_dirs %}0{% endblock %}
 
 {% block lib_deps %}
 lib/c
