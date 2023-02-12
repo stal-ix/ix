dc991afaecfb8365441836ca566beb7e94ffa94e
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-13 01:14:18 +0300

    better

--- pkgs/bin/coreutils/8/31/ix.sh
+++ pkgs/bin/coreutils/8/31/ix.sh
@@ -5,14 +5,15 @@ https://ftp.gnu.org/gnu/coreutils/coreutils-8.31.tar.xz
 sha:ff7a9c918edce6b4f4b2725e3f9b37b0c4d193531cac49a48b56c4d0d3a9e9fd
 {% endblock %}
 
+{% block bld_libs %}
+{{super()}}
+lib/obstack
+{% endblock %}
+
 {% block bld_tool %}
+bld/perl
 {% block yacc %}
 bld/byacc
 {% endblock %}
-{{super()}}
-{% endblock %}
-
-{% block setup %}
-export CFLAGS="-Wno-incompatible-function-pointer-types ${CFLAGS}"
 {{super()}}
 {% endblock %}
