6750dd8a7ab699abe476e8046a6cc65b5ad8914a
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-13 00:56:16 +0300

    explain

--- pkgs/bin/bash/4/ix.sh
+++ pkgs/bin/bash/4/ix.sh
@@ -5,6 +5,8 @@ https://ftp.gnu.org/gnu/bash/bash-4.4.18.tar.gz
 sha:604d9eec5e4ed5fd2180ee44dd756ddca92e0b6aa4217bbab2b6227380317f23
 {% endblock %}
 
+{#parse.y:5741:21: error: call to undeclared function 'count_all_jobs'; ISO C99 and later do not support implicit function declarations [-Wimplicit-function-declaration]#}
+
 {% block setup %}
 export CFLAGS="-Wno-implicit-function-declaration ${CFLAGS}"
 {{super()}}
