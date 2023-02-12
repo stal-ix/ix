e8d9cddd58bf22b1481beff4423db32509facf86
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-12 12:58:37 +0300

    better

--- pkgs/lib/harfbuzz/icu/ix.sh
+++ pkgs/lib/harfbuzz/icu/ix.sh
@@ -10,3 +10,8 @@ lib/icu
 cd ${out}
 find . -type f | grep -v icu | xargs rm
 {% endblock %}
+
+{% block env %}
+{# actually, part of include/harfbuzz/hb-features.h #}
+export CPPFLAGS="-DHB_HAS_ICU=1 \${CPPFLAGS}"
+{% endblock %}
