794a0a21d65535929ab464d211deed5e47378497
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-13 01:01:45 +0300

    better

--- pkgs/bin/sud/client/ix.sh
+++ pkgs/bin/sud/client/ix.sh
@@ -13,7 +13,7 @@ mv old/ssh bin/sud_client
 rm -r old
 {% endblock %}
 
-{% block setup %}
-export CPPFLAGS="-D_GNU_SOURCE=1 -Wno-implicit-function-declaration ${CPPFLAGS}"
+{% block cpp_defines %}
 {{super()}}
+_GNU_SOURCE=1
 {% endblock %}
