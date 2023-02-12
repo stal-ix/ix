982808232688ff8bd406031d1ea87eaadef32102
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-13 01:16:41 +0300

    better

--- pkgs/bin/bison/3/6/ix.sh
+++ pkgs/bin/bison/3/6/ix.sh
@@ -5,7 +5,7 @@ https://ftp.gnu.org/gnu/bison/bison-3.6.4.tar.xz
 sha:8b13473b31ca7fcf65e5e8a74224368ffd5df19275602a9c9567ba393f18577d
 {% endblock %}
 
-{% block setup %}
+{% block bld_libs %}
+lib/obstack
 {{super()}}
-export CFLAGS="-Wno-incompatible-function-pointer-types ${CFLAGS}"
 {% endblock %}
