c64da0b65135f0363916e668cda828d2e1284d2f
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-12 13:06:13 +0300

    upver

--- pkgs/lib/adwaita/unwrap/ix.sh
+++ pkgs/lib/adwaita/unwrap/ix.sh
@@ -1,10 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-#https://gitlab.gnome.org/GNOME/libadwaita/-/archive/1.2.1/libadwaita-1.2.1.tar.bz2
-#sha:ee7fa58d0b8dffe36bb477bf72a84d065cb530b070c8197ea9004740552fe655
-https://github.com/GNOME/libadwaita/archive/93602c06d0919e75052535e87afea41edb79260f.tar.gz
-sem:daee22ae2c8e818304d8a64fedea9a9000f01d9fc5128acdad2bea953aaebedc
+https://github.com/GNOME/libadwaita/archive/refs/tags/1.3.beta.tar.gz
+sha:a77379fee96377f4264d8350793bfbd55343574d16d5d1fc1de4016dba53f755
 {% endblock %}
 
 {% block lib_deps %}
