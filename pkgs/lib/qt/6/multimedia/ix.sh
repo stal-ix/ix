a0c755ae5d6cc63f7c34c468f55926d14f6be721
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-13 21:24:51 +0300

    add

---
+++ pkgs/lib/qt/6/multimedia/ix.sh
@@ -0,0 +1,24 @@
+{% extends '//die/c/qt.sh' %}
+
+{% block fetch %}
+https://github.com/qt/qtmultimedia/archive/refs/tags/v6.4.2.tar.gz
+sha:bb893098c9526291ca1dadbc7ca1e3b0595c5b4813526daf8d0f917fba324cfc
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+lib/c++
+lib/qt/6/base
+lib/qt/6/shadertools
+{% endblock %}
+
+{% block bld_tool %}
+{{super()}}
+bld/qt/6
+bld/qt/6/tools/shader
+{% endblock %}
+
+{% block env %}
+{{super()}}
+export QT_PATH="${out}:\${QT_PATH}"
+{% endblock %}
