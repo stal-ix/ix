9663da8c043542798840986513c178b3c425716e
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-14 11:44:41 +0300

    better

--- pkgs/lib/bsd/ix.sh
+++ pkgs/lib/bsd/ix.sh
@@ -9,6 +9,20 @@ lib/md
 lib/kernel
 {% endblock %}
 
+{% block patch %}
+{{super()}}
+cat << EOF >> include/bsd/sys/cdefs.h
+#pragma once
+#ifndef __scanflike
+# if LIBBSD_GCC_VERSION >= 0x0300 || __has_attribute(__format__)
+#  define __scanflike(x, y) __attribute((__format__(__scanf__, (x), (y))))
+# else
+#  define __scanflike(x, y)
+# endif
+#endif
+EOF
+{% endblock %}
+
 {% block env %}
 {{super()}}
 export BSD_HEADERS=${out}/include/bsd
