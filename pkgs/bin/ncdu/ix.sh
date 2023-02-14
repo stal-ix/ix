19bf41e3de85277ba5ab86d79cb71eafcf99f1d6
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-14 11:24:58 +0300

    upver

--- pkgs/bin/ncdu/ix.sh
+++ pkgs/bin/ncdu/ix.sh
@@ -1,12 +1,16 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://dev.yorhel.nl/download/ncdu-1.17.tar.gz
-sha:810745a8ed1ab3788c87d3aea4cc1a14edf6ee226f764bcc383e024ba56adbf1
+https://dev.yorhel.nl/download/ncdu-1.18.1.tar.gz
+sha:7c0fa1eb29d85aaed4ba174164bdbb8f011b5c390d017c57d668fc7231332405
 {% endblock %}
 
 {% block bld_libs %}
 lib/c
 lib/kernel
 lib/curses
+{% endblock %}
+
+{% block configure_flags %}
+--with-shell=/bin/sh
 {% endblock %}
