6c4e2a9cd03148518a085b0ce6b80e53e737271e
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-14 11:44:41 +0300

    better

--- pkgs/bin/got/stock/ix.sh
+++ pkgs/bin/got/stock/ix.sh
@@ -24,33 +24,10 @@ bld/byacc
 {% block patch %}
 >compat/reallocarray.c
 >compat/getprogname.c
-
-cat << EOF > xxx.h
-#pragma once
-
-#include <stddef.h>
-#include <stdint.h>
-#include <limits.h>
-#include <stdio.h>
-
-#define __scanflike(X, Y)
-
-int fpurge(FILE* stream);
-void arc4random_buf(void*, size_t);
-uint32_t arc4random();
-void arc4random_stir();
-void arc4random_addrandom(unsigned char*, int);
-void setproctitle(const char*, ...);
-const char* getprogname();
-void closefrom(int);
-void* reallocarray(void*, size_t, size_t);
-void* recallocarray(void* ptr, size_t, size_t, size_t);
-void freezero(void*, size_t);
-long long strtonum(const char*, long long, long long, const char**);
-int mergesort(void*, size_t, size_t, int (*)(const void*, const void*));
-EOF
 {% endblock %}
 
 {% block cpp_missing %}
-${PWD}/xxx.h
+bsd/stdlib.h
+bsd/unistd.h
+bsd/stdio.h
 {% endblock %}
