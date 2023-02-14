947ccd7c80d1b0115779dd2433fda7831a8b0dc3
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-15 00:30:19 +0300

    better bootstrap

--- pkgs/bin/go/19/ix.sh
+++ pkgs/bin/go/19/ix.sh
@@ -1,6 +1,23 @@
-{% extends 't/ix.sh' %}
+{% extends '//bin/go/19/t/ix.sh' %}
+
+{% block bld_libs %}
+lib/c
+{% endblock %}
 
 {% block bld_tool %}
 bin/go/4
+{{super()}}
+{% endblock %}
+
+{% block cgo %}
+{% endblock %}
+
+{% block setup %}
+{{super()}}
+export CPPFLAGS="${CPPFLAGS} -fno-color-diagnostics"
+{% endblock %}
+
+{% block build %}
+export GO_EXTLINK_ENABLED=1
 {{super()}}
 {% endblock %}
