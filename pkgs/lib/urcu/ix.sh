8a9bc12cf9a6da6969825cdff7f346bb772cbcfb
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-14 20:55:28 +0300

    upver

--- pkgs/lib/urcu/ix.sh
+++ pkgs/lib/urcu/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://lttng.org/files/urcu/userspace-rcu-0.13.2.tar.bz2
-sha:1213fd9f1b0b74da7de2bb74335b76098db9738fec5d3cdc07c0c524f34fc032
+https://lttng.org/files/urcu/userspace-rcu-0.14.0.tar.bz2
+sha:ca43bf261d4d392cff20dfae440836603bf009fce24fdc9b2697d837a2239d4f
 {% endblock %}
 
 {% block lib_deps %}
