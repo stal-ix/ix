d5e33f824fd1bd465f398384e39e14d40d0792dd
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-15 00:25:05 +0300

    better

--- pkgs/bin/go/20/t/ix.sh
+++
@@ -1,6 +0,0 @@
-{% extends '//bin/go/19/t/ix.sh' %}
-
-{% block fetch %}
-https://github.com/golang/go/archive/refs/tags/go1.20.tar.gz
-sha:3094dd1d21843f0ddf0b14e45a6099cb9eb2c97c874d304e70289f1c7525671f
-{% endblock %}
