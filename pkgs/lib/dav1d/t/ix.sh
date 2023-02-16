f5d85d03a814d6826c89bb793e7a76dc32b695bf
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-16 18:21:06 +0300

    fix build

---
+++ pkgs/lib/dav1d/t/ix.sh
@@ -0,0 +1,19 @@
+{% extends '//die/c/meson.sh' %}
+
+{% block fetch %}
+https://github.com/videolan/dav1d/archive/refs/tags/1.1.0.tar.gz
+sha:b163791a587c083803a3db2cd18b4fbaf7fb865b47d038c4869ffef7722b6b16
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+{% endblock %}
+
+{% block bld_tool %}
+bin/nasm
+{% endblock %}
+
+{% block meson_flags %}
+enable_examples=false
+enable_tests=false
+{% endblock %}
