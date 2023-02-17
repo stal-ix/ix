c3d5aedcb4cc4e60b082b70df020f1769940309a
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-17 23:32:56 +0300

    better

--- pkgs/bin/sndio/runit/scripts/ix.sh
+++
@@ -1,5 +0,0 @@
-{% extends '//die/hub.sh' %}
-
-{% block run_deps %}
-etc/services/runit(srv_slot=system,srv_name=sndiod,srv_command=chrt -f 10 sndiod -dd -f rsnd/{{alsa_device or 'default'}})
-{% endblock %}
