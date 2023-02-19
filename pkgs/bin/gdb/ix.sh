f55c3d342a2221ead7e7547ecccfb2daaf16b174
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-20 01:15:58 +0300

    better

--- pkgs/bin/gdb/ix.sh
+++ pkgs/bin/gdb/ix.sh
@@ -6,5 +6,6 @@ lib/c
 
 {% block patch %}
 {{super()}}
-sed -e 's| = { 0 }||' -i gdb/aarch64-linux-nat.c
+sed -e 's| = { 0 }|; memset(tpidrs, 0, sizeof(tpidrs))|' \
+    -i gdb/aarch64-linux-nat.c
 {% endblock %}
