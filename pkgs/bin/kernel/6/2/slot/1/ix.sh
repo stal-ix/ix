bf113c2ea81e1e878b0ed6e926d0545716616c32
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-20 11:45:43 +0300

    add

---
+++ pkgs/bin/kernel/6/2/slot/1/ix.sh
@@ -0,0 +1,7 @@
+{% extends '//bin/kernel/6/2/t/ix.sh' %}
+
+{% block slot %}1{% endblock %}
+
+{% block kernel_flags %}
+{% include 'cfg' %}
+{% endblock %}
---
+++ pkgs/bin/kernel/6/2/t/ix.sh
@@ -0,0 +1,11 @@
+{% extends '//bin/kernel/t/2/ix.sh' %}
+
+{% block kernel_version %}6-1-12{% endblock %}
+
+{% block fetch %}
+{% include 'ver.sh' %}
+{% endblock %}
+
+{% block kernel_headers %}
+bin/kernel/6/2/headers
+{% endblock %}
