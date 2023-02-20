473fad8db134a137a1e0c90a6deed7bd070fc3c7
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-20 19:04:24 +0300

    upver

--- pkgs/lib/pango/t/ix.sh
+++ pkgs/lib/pango/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://download.gnome.org/sources/pango/1.50/pango-1.50.12.tar.xz
-sha:caef96d27bbe792a6be92727c73468d832b13da57c8071ef79b9df69ee058fe3
+https://download.gnome.org/sources/pango/1.50/pango-1.50.13.tar.xz
+sha:5cdcf6d761d26a3eb9412b6cb069b32bd1d9b07abf116321167d94c2189299fd
 {% endblock %}
 
 {% block lib_deps %}
