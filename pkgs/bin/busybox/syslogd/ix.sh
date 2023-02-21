f4bdf1957b5e1c115485bcd6f9487f63f860a2f1
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-22 00:00:03 +0300

    better

--- pkgs/bin/busybox/syslogd/ix.sh
+++ pkgs/bin/busybox/syslogd/ix.sh
@@ -1,18 +1,6 @@
-{% extends '//die/proxy.sh' %}
+{% extends '//die/hub.sh' %}
 
 {% block run_deps %}
-# TODO(pg): move to upper level
-bin/runsrv
 bin/busybox
-{% endblock %}
-
-{% block install %}
-cd ${out}; mkdir -p etc/services/syslogd; cd etc/services/syslogd
-
-cat << EOF > run
-#!/bin/sh
-exec srv syslogd syslogd -n -O /var/log/syslog
-EOF
-
-chmod +x run
+etc/services/runit(srv_dir=syslogd,srv_command=syslogd -n -O /var/log/syslog)
 {% endblock %}
