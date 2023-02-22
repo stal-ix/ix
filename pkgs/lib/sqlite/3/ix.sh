4c2ae7c7d2c66c05b912ff9d29c13718e2f6e1b4
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-22 12:05:59 +0300

    upver

--- pkgs/lib/sqlite/3/ix.sh
+++ pkgs/lib/sqlite/3/ix.sh
@@ -1,23 +1,6 @@
-{% extends '//die/c/autorehell.sh' %}
+{% extends 't/ix.sh' %}
 
-{% block fetch %}
-https://www.sqlite.org/2022/sqlite-autoconf-3400100.tar.gz
-sha:2c5dea207fa508d765af1ef620b637dcb06572afa6f01f0815bd5bbf864b33d9
-{% endblock %}
-
-{% block lib_deps %}
-lib/c
-{% endblock %}
-
-{% block configure_flags %}
---enable-dynamic-extensions=no
-{% endblock %}
-
-{% block cpp_defines %}
-SQLITE_OMIT_LOAD_EXTENSION=1
-SQLITE_ENABLE_COLUMN_METADATA=1
-{% endblock %}
-
-{% block env_lib %}
+{% block env %}
+{{super()}}
 export COFLAGS="--with-sqlite3=${out} \${COFLAGS}"
 {% endblock %}
