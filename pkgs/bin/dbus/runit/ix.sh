ea92cd760856bcea3092cc6be3e1a08d0ac8c40d
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-22 21:17:28 +0300

    refac

--- pkgs/bin/dbus/runit/ix.sh
+++ pkgs/bin/dbus/runit/ix.sh
@@ -1,7 +1,7 @@
 {% extends '//die/hub.sh' %}
 
 {% block run_deps %}
-bin/runsrv
 bin/dbus/sys
 bin/dbus/runit/scripts
+etc/services/runit(srv_dir=dbus,srv_command=chrt -f 10 dbus-daemon --config-file=/etc/dbus.conf --nopidfile --syslog --nofork)
 {% endblock %}
