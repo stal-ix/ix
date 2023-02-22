c931a15d5a4bc4803dabc92ab3f61d1efc500ef2
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-22 21:17:28 +0300

    refac

--- pkgs/bin/dbus/runit/scripts/ix.sh
+++ pkgs/bin/dbus/runit/scripts/ix.sh
@@ -1,16 +1,8 @@
 {% extends '//die/proxy.sh' %}
 
 {% block install %}
-cd ${out}; mkdir -p etc/services/dbus; cd etc/services/dbus
-
-base64 -d << EOF > system.conf
+mkdir -p ${out}/etc
+base64 -d << EOF > ${out}/etc/dbus.conf
 {% include 'system.conf/base64' %}
 EOF
-
-cat << EOF > run
-#!/bin/sh
-exec srv dbus chrt -f 10 dbus-daemon --config-file=${PWD}/system.conf --nopidfile --syslog --nofork
-EOF
-
-chmod +x run
 {% endblock %}
