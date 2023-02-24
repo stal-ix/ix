b2bdf14a3d80ef5c324ecc9b376bbe738f669529
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-24 20:05:24 +0300

    upver

--- pkgs/bin/cabextract/ix.sh
+++ pkgs/bin/cabextract/ix.sh
@@ -1,10 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-#https://www.cabextract.org.uk/cabextract-1.10.tar.gz
-#sha:edfc785ef252460cab7fdfa6fb2599058a6f5618f7f48a4ad22da816da8cb117
-https://www.cabextract.org.uk/cabextract-1.9.tar.gz
-sha:1bbc793d83c73288acd7e28ce33ec04955a76c73bf6471424ff835d725fcc4c1
+https://www.cabextract.org.uk/cabextract-1.11.tar.gz
+sha:b5546db1155e4c718ff3d4b278573604f30dd64c3c5bfd4657cd089b823a3ac6
 {% endblock %}
 
 {% block bld_libs %}
