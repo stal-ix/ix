cf34172d5ad62d883cbf3cf36ddaf71b466f7855
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-24 12:18:52 +0300

    upver

--- pkgs/bin/meson/100/ix.sh
+++ pkgs/bin/meson/100/ix.sh
@@ -6,8 +6,8 @@ mkdir src; cd src; extract0 ${src}/1*; cd meson*
 {% endblock %}
 
 {% block fetch %}
-https://github.com/mesonbuild/meson/archive/refs/tags/1.0.0.tar.gz
-sha:a2ada84d43c7e57400daee80a880a1f5003d062b2cb6c9be1747b0db38f2eb8d
+https://github.com/mesonbuild/meson/archive/refs/tags/1.0.1.tar.gz
+sha:4ab3a5c0060dc22bdefb04507efc6c38acb910e91bcd467a38e1fa211e5a6cfe
 {% endblock %}
 
 {% block test %}
