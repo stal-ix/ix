7956d6c326ddffdcd3c71f23593646c20c74d57b
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-27 01:04:22 +0300

    refac

--- pkgs/aux/git/ix.sh
+++ pkgs/aux/git/ix.sh
@@ -3,7 +3,7 @@
 {% block use_network %}true{% endblock %}
 
 {% block predict_outputs %}
-[{"path": "share/{{parent_id}}.tar.lz4", "sum": "74ef5fd2d9d4431f0dfe94ccbf14c0a16a9a61efee98b534a5c2f7998bad320d"}]
+[{"path": "share/{{parent_id}}.tar.lz4", "sum": "{{sha}}"}]
 {% endblock %}
 
 {% block bld_tool %}
@@ -13,17 +13,18 @@ bin/lz4
 {% endblock %}
 
 {% block build %}
-git clone --recurse-submodules --shallow-submodules --depth 1 --branch mariadb-10.11.2 https://github.com/MariaDB/server.git
+mkdir git
+cd git
+git clone --recurse-submodules --shallow-submodules --depth 1 --branch {{branch}} {{repo}}
 find . -type d -name '.git' | while read l; do
     rm -rf "${l}"
 done
-tar --sort=name --owner=root:0 --group=root:0 --mtime='UTC 1970-01-01'  -c -f {{parent_id}}.tar server
+tar --sort=name --owner=root:0 --group=root:0 --mtime='UTC 1970-01-01'  -c -f ../{{parent_id}}.tar *
 {% endblock %}
 
 {% block install %}
 mkdir ${out}/share
 lz4 {{parent_id}}.tar ${out}/share/{{parent_id}}.tar.lz4
-ls -la ${out}/share/
 {% endblock %}
 
 {% block postinstall %}
