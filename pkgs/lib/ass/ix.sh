8500181130edd95e8b3f397194fb27f3c9284f93
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-02-28 00:48:04 +0300

    better

--- pkgs/lib/ass/ix.sh
+++ pkgs/lib/ass/ix.sh
@@ -1,14 +1,15 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://github.com/libass/libass/archive/refs/tags/0.17.0.tar.gz
-sha:90ca569bcd47eea1e170d8c5dfd1e6208f1c1b70f8cf67d4219916af02e34c09
+https://github.com/libass/libass/archive/refs/tags/0.17.1.tar.gz
+sha:5ba42655d7e8c5e87bba3ffc8a2b1bc19c29904240126bb0d4b924f39429219f
 {% endblock %}
 
 {% block lib_deps %}
 lib/c
 lib/glib
 lib/fribidi
+lib/unibreak
 lib/freetype
 lib/harfbuzz
 lib/fontconfig
