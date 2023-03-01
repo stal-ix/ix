bbba08135df34bab99edae571a415ca24854e3ab
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-03-01 23:15:53 +0300

    upver

--- pkgs/lib/re2/ix.sh
+++ pkgs/lib/re2/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/google/re2/archive/refs/tags/2022-12-01.tar.gz
-sha:665b65b6668156db2b46dddd33405cd422bd611352c5052ab3dae6a5fbac5506
+https://github.com/google/re2/archive/refs/tags/2023-03-01.tar.gz
+sha:7a9a4824958586980926a300b4717202485c4b4115ac031822e29aa4ef207e48
 {% endblock %}
 
 {% block lib_deps %}
@@ -14,6 +14,6 @@ lib/c++
 RE2_BUILD_TESTING=OFF
 {% endblock %}
 
-{% block env_lib %}
+{% block env %}
 export CMFLAGS="-Dre2_DIR=${out}/lib/cmake/re2 \${CMFLAGS}"
 {% endblock %}
