0d93726ed27f159b262e102a771d85e145ede2ba
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-03-02 16:03:55 +0300

    better source

--- pkgs/lib/natpmp/ix.sh
+++ pkgs/lib/natpmp/ix.sh
@@ -1,7 +1,8 @@
 {% extends '//die/c/make.sh' %}
 
 {% block fetch %}
-http://miniupnp.free.fr/files/download.php?file=libnatpmp-20150609.tar.gz
+#http://miniupnp.free.fr/files/download.php?file=libnatpmp-20150609.tar.gz
+https://launchpad.net/ubuntu/+archive/primary/+sourcefiles/libnatpmp/20150609-7.1build2/libnatpmp_20150609.orig.tar.gz
 sha:e1aa9c4c4219bc06943d6b2130f664daee213fb262fcb94dd355815b8f4536b0
 {% endblock %}
 
