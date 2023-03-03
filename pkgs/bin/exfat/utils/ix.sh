29747577f6d5bf8e89e5ab009121d18b366e7fe0
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-03-03 19:50:57 +0300

    upver

--- pkgs/bin/exfat/utils/ix.sh
+++ pkgs/bin/exfat/utils/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://github.com/relan/exfat/releases/download/v1.3.0/exfat-utils-1.3.0.tar.gz
-sha:dfebd07a7b907e2d603d3a9626e6440bd43ec6c4e8c07ccfc57ce9502b724835
+https://github.com/relan/exfat/releases/download/v1.4.0/exfat-utils-1.4.0.tar.gz
+sha:241575fa93104406a47e79e53e4d907bae69886f11621f70a45276c62b75bf69
 {% endblock %}
 
 {% block bld_libs %}
