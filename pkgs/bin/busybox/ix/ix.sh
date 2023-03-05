d271662e41f79fdadb47cbe7421576295125dae4
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-03-05 23:11:07 +0300

    disable broken ops

--- pkgs/bin/busybox/ix/ix.sh
+++ pkgs/bin/busybox/ix/ix.sh
@@ -1,5 +1,11 @@
 {% extends '//bin/busybox/t/ix.sh' %}
 
+{% block kconfig_flags %}
+{{super()}}
+CONFIG_SHA1_HWACCEL=n
+CONFIG_SHA256_HWACCEL=n
+{% endblock %}
+
 {% block patch %}
 {{super()}}
 sed -e 's|"syslogd"|"syslogd/syslogd"|' -i sysklogd/syslogd.c
