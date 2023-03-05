4ada0fa61df36b689b441ba65a2c86cbf73ccd82
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-03-05 14:55:04 +0300

    upver

--- pkgs/bin/elvish/ix.sh
+++ pkgs/bin/elvish/ix.sh
@@ -1,15 +1,15 @@
 {% extends '//die/go/pure.sh' %}
 
 {% block go_url %}
-https://github.com/elves/elvish/archive/refs/tags/v0.19.0.tar.gz
+https://github.com/elves/elvish/archive/refs/tags/v0.19.1.tar.gz
 {% endblock %}
 
 {% block go_sum %}
-sha:f7a5bd9bcbc42fb894e94e90bd3a3a964a9dc488d39c4fe668d06688651f60e4
+sha:4ae99be96a717156b40fa79a0571f320c9b25ecc2daf41c0f79c63dcde709d1d
 {% endblock %}
 
 {% block go_sha %}
-sha:192e41dd75d2effb9a532caf664ca627b1eff033551240ee0a37811fab3fdacd
+sha:e87f4dc999e0d1c47cb2d6c4a5f1b37d3c209c49b61aeb55bfaf65b45514a4af
 {% endblock %}
 
 {% block unpack %}
