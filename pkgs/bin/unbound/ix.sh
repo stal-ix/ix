fd527bb02cc11f58f928ccf6c174762b586d34bf
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-03-07 00:02:11 +0300

    fix build

--- pkgs/bin/unbound/ix.sh
+++ pkgs/bin/unbound/ix.sh
@@ -1,7 +1,13 @@
 {% extends 't/ix.sh' %}
 
 {% block bld_libs %}
+{{super()}}
 lib/hiredis
 lib/ng/http/2
+{% endblock %}
+
+{% block setup %}
+export ac_cv_func_ev_loop=no
+export ac_cv_func_ev_default_loop=no
 {{super()}}
 {% endblock %}
