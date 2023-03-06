610822a8fdab713465e7bb5bfccfda448c883956
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-03-06 14:14:11 +0300

    support user

--- pkgs/etc/services/runit/script/ix.sh
+++ pkgs/etc/services/runit/script/ix.sh
@@ -5,9 +5,13 @@
 {% block install %}
 cd ${out}; mkdir -p etc/services/{{sd}}; cd etc/services/{{sd}}
 
+cat << EOF > cmd
+exec {{srv_command}}
+EOF
+
 cat << EOF > run
 #!/bin/sh
-exec srv {{sd}} {{srv_command}}
+exec srv {{sd}} su {{srv_user or 'root'}} ${PWD}/cmd
 EOF
 
 chmod +x run
