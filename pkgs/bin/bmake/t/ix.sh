b497c8b38c0e5e0277c1363a18d13e3bc16cb276
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-03-06 09:34:46 +0300

    upver

--- pkgs/bin/bmake/t/ix.sh
+++ pkgs/bin/bmake/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
-https://www.crufty.net/ftp/pub/sjg/bmake-20230208.tar.gz
-sha:654c5328fe732691c9fa2e99144431f86dbbaff1376c73ecc40c245b7906b29f
+https://www.crufty.net/ftp/pub/sjg/bmake-20230303.tar.gz
+sha:e8698724ac68c63f8e6682a93c3154c1d93dc6a9072f13c8cef07ece4ccd0ed6
 {% endblock %}
 
 {% block bld_libs %}
