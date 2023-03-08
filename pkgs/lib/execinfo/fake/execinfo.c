174a041718a55563da0e9e06931502ad67ba7a86
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-03-08 18:40:36 +0300

    fix gcc build

--- pkgs/lib/execinfo/fake/execinfo.c
+++ pkgs/lib/execinfo/fake/execinfo.c
@@ -1,12 +1,12 @@
 #include "execinfo.h"
 
-int backtrace(void**, int) {
+int backtrace(void** x, int y) {
     return 0;
 }
 
-char** backtrace_symbols(void*const*, int) {
+char** backtrace_symbols(void*const* x, int y) {
     return 0;
 }
 
-void backtrace_symbols_fd(void*const*, int, int) {
+void backtrace_symbols_fd(void*const* x, int y, int z) {
 }
