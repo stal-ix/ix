31fcfb55d44de31f3dd93837fcaf1465a3b2c1f0
Anton Samokhvalov <pg@yandex-team.ru>
2023-03-08 21:22:48 +0300

    fix native build

--- pkgs/lib/obstack/ix.sh
+++ pkgs/lib/obstack/ix.sh
@@ -5,8 +5,8 @@ https://github.com/void-linux/musl-obstack/archive/refs/tags/v1.2.2.tar.gz
 md5:edee8cb45ca351de5759b85f2aca0f3b
 {% endblock %}
 
-{% block autoreconf %}
-sh bootstrap.sh
+{% block patch %}
+rm bootstrap.sh
 {% endblock %}
 
 {% block lib_deps %}
