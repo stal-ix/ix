43315960d5db016417655d14a33cd8b0171f7dc5
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-03-11 13:50:49 +0300

    upver

--- pkgs/lib/protobuf/t/ix.sh
+++ pkgs/lib/protobuf/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/protocolbuffers/protobuf/archive/refs/tags/v22.1.tar.gz
-sha:0b6494b6e1a8d197f6626ca0c5aa9ab35fc1e5aa3f724787133ce4fa4aa78499
+https://github.com/protocolbuffers/protobuf/archive/refs/tags/v22.2.tar.gz
+sha:9d39f0b36761068ed4cfb80ac446bb413d74dc60426bb942c53645484afb1407
 {% endblock %}
 
 {% block lib_deps %}
