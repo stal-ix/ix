aae7a35bdf2bf7b82f915b713e5ca12f2cbab608
Anton Samokhvalov <pg@yandex-team.ru>
2023-03-12 18:08:07 +0300

    pretty printers

--- pkgs/bin/gdb/reloc/ya/ix.sh
+++ pkgs/bin/gdb/reloc/ya/ix.sh
@@ -1,7 +1,13 @@
 {% extends '//bin/gdb/reloc/ix.sh' %}
 
+{% block gdb_args %}--eval-command="source \${p2}/share/gdb/python/arc/__init__.py"{% endblock %}
+
 {% block install %}
 {{super()}}
+cd ${out}/share/gdb/python
+mkdir arc
+cd arc
+bsdtar -x -f {{arcadia_pretty_printers}} --strip-components 1
 mkdir ${out}/fix
 cat << EOF > ${out}/fix/mksymlink.sh
 mkdir gdb
