a21d2163028511512ed29d781d384c2e26a6433d
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-03-13 22:05:41 +0300

    upver

--- pkgs/bin/ccache/ix.sh
+++ pkgs/bin/ccache/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/ccache/ccache/releases/download/v4.7.4/ccache-4.7.4.tar.xz
-sha:df0c64d15d3efaf0b4f6837dd6b1467e40eeaaa807db25ce79c3a08a46a84e36
+https://github.com/ccache/ccache/releases/download/v4.8/ccache-4.8.tar.xz
+sha:b963ee3bf88d7266b8a0565e4ba685d5666357f0a7e364ed98adb0dc1191fcbb
 {% endblock %}
 
 {% block bld_libs %}
