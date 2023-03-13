c7924abd14b8acb71fc6405f865d7da5ef758a3e
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-03-13 23:21:39 +0300

    fix build

--- pkgs/lib/placebo/5/ix.sh
+++ pkgs/lib/placebo/5/ix.sh
@@ -9,3 +9,8 @@ sha:780e1c5d304a2c407b6709ac66e811bd1be03a769601f1dab41cf77c8519c668
 pip/glad2
 {{super()}}
 {% endblock %}
+
+{% block patch %}
+{{super()}}
+sed -e 's|.*PYTHONPATH.*||' -i meson.build
+{% endblock %}
