2fc255bc7f8d398d4976f72d5ef037fe44526fb2
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-03-13 23:44:48 +0300

    upver

--- pkgs/lib/glib/mm/ix.sh
+++ pkgs/lib/glib/mm/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://download.gnome.org/sources/glibmm/2.66/glibmm-2.66.2.tar.xz
-sha:b2a4cd7b9ae987794cbb5a1becc10cecb65182b9bb841868625d6bbb123edb1d
+https://download.gnome.org/sources/glibmm/2.66/glibmm-2.66.6.tar.xz
+sha:5358742598181e5351d7bf8da072bf93e6dd5f178d27640d4e462bc8f14e152f
 {% endblock %}
 
 {% block lib_deps %}
