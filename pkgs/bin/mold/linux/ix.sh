7b62a550c7dd09bc593c53ec44f4376dc56c82b6
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-03-16 18:30:57 +0300

    upver

---
+++ pkgs/bin/mold/linux/ix.sh
@@ -0,0 +1,6 @@
+{% extends '//bin/mold/darwin/ix.sh' %}
+
+{% block fetch %}
+https://github.com/rui314/mold/archive/refs/tags/v1.11.0.tar.gz
+sha:99318eced81b09a77e4c657011076cc8ec3d4b6867bd324b8677974545bc4d6f
+{% endblock %}
