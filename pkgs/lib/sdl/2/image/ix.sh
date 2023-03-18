41bf1bec070097ec1297dc034f5c651fe3739060
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-03-18 20:47:22 +0300

    better

--- pkgs/lib/sdl/2/image/ix.sh
+++ pkgs/lib/sdl/2/image/ix.sh
@@ -1,4 +1,4 @@
-{% extends '//die/c/autohell.sh' %}
+{% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
 https://github.com/libsdl-org/SDL_image/archive/refs/tags/release-2.6.2.tar.gz
@@ -9,8 +9,34 @@ sha:5d91ea72b449a161821ef51464d0767efb6fedf7a773f923c43e483dc137e362
 lib/c
 lib/jxl
 lib/png
+lib/webp
 lib/jpeg
 lib/tiff
 lib/avif
 lib/sdl/2
+{% endblock %}
+
+{% block build_flags %}
+wrap_cc
+{% endblock %}
+
+{% block cmake_flags %}
+BUILD_SHARED_LIBS=ON
+CMAKE_INSTALL_LIBDIR=lib
+CMAKE_INSTALL_INCLUDEDIR=include
+SDL2IMAGE_BUILD_SHARED_LIBS=ON
+SDL2IMAGE_DEPS_SHARED=OFF
+SDL2IMAGE_SAMPLES=OFF
+SDL2IMAGE_WEBP=ON
+SDL2IMAGE_AVIF=ON
+{% endblock %}
+
+{% block install %}
+{{super()}}
+cd ${out}/lib
+cp libSDL2_image.so libSDL2_image.a
+{% endblock %}
+
+{% block test %}
+test -f ${out}/lib/pkgconfig/SDL2_image.pc
 {% endblock %}
