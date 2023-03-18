0b4d2b7d19b4fee8b1d75650f35442018d17edc8
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-03-18 20:47:22 +0300

    better

--- pkgs/lib/avif/ix.sh
+++ pkgs/lib/avif/ix.sh
@@ -3,6 +3,7 @@
 {% block install %}
 {{super()}}
 sed -e 's|.*exec_prefix.*||' -i ${out}/lib/pkgconfig/libavif.pc
+sed -e 's|LINK_ONLY:Threads::Threads|LINK_ONLY:c|' -i ${out}/lib/cmake/libavif/libavif-config.cmake
 {% endblock %}
 
 {% block cmake_flags %}
