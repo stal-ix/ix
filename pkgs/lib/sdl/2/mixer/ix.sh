c455b483b997ee7d31bf45e5725ef747ac4da1d6
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-03-18 19:29:56 +0300

    better

--- pkgs/lib/sdl/2/mixer/ix.sh
+++ pkgs/lib/sdl/2/mixer/ix.sh
@@ -1,4 +1,4 @@
-{% extends '//die/c/autohell.sh' %}
+{% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
 https://github.com/libsdl-org/SDL_mixer/archive/refs/tags/release-2.6.3.tar.gz
@@ -17,13 +17,32 @@ lib/opus/file
 lib/xiph/vorbis
 {% endblock %}
 
-{% block configure_flags %}
---disable-music-mod-modplug-shared
---disable-music-mod-xmp-shared
---disable-music-midi-fluidsynth-shared
---disable-music-ogg-vorbis-shared
---disable-music-ogg-tremor-shared
---disable-music-flac-libflac-shared
---disable-music-mp3-mpg123-shared
---disable-music-opus-shared
+{% block build_flags %}
+wrap_cc
+{% endblock %}
+
+{% block cmake_flags %}
+SDL2MIXER_CMD=OFF
+BUILD_SHARED_LIBS=ON
+CMAKE_INSTALL_LIBDIR=lib
+CMAKE_INSTALL_INCLUDEDIR=include
+SDL2MIXER_BUILD_SHARED_LIBS=ON
+SDL2MIXER_BUILD_STATIC_LIBS=OFf
+SDL2MIXER_DEPS_SHARED=OFF
+SDL2MIXER_MIDI_FLUIDSYNTH=OFF
+SDL2MIXER_MP3_MPG123=ON
+SDL2MIXER_OPUS_SHARED=OFF
+SDL2MIXER_SAMPLES=OFF
+SDL2MIXER_VORBIS=VORBISFILE
+SDL2MIXER_VORBIS_VORBISFILE_SHARED=OFF
+{% endblock %}
+
+{% block install %}
+{{super()}}
+cd ${out}/lib
+cp libSDL2_mixer.so libSDL2_mixer.a
+{% endblock %}
+
+{% block test %}
+test -f ${out}/lib/pkgconfig/SDL2_mixer.pc
 {% endblock %}
