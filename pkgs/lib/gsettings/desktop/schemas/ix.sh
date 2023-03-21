f3b69ad1462d0726f50acef71f6642006ae51feb
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-03-21 22:09:31 +0300

    upver

--- pkgs/lib/gsettings/desktop/schemas/ix.sh
+++ pkgs/lib/gsettings/desktop/schemas/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://gitlab.gnome.org/GNOME/gsettings-desktop-schemas/-/archive/43.0/gsettings-desktop-schemas-43.0.tar.bz2
-sha:321ac4209394acd8293e980b5a0b921285621bbbbee1fcfff9402bff19d66c13
+https://gitlab.gnome.org/GNOME/gsettings-desktop-schemas/-/archive/44.0/gsettings-desktop-schemas-44.0.tar.bz2
+sha:b1bff22c6131c0bf638505811f2c72a77a6577aee8a4a851e16f79c317dbc50d
 {% endblock %}
 
 {% block bld_tool %}
