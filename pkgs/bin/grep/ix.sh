912f2fa37d1a018b14655d015f3975f752bace39
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-03-23 13:37:51 +0300

    upver

--- pkgs/bin/grep/ix.sh
+++ pkgs/bin/grep/ix.sh
@@ -1,12 +1,6 @@
-{% extends '//die/c/autohell.sh' %}
-
-{% block fetch %}
-https://ftp.gnu.org/gnu/grep/grep-3.8.tar.xz
-sha:498d7cc1b4fb081904d87343febb73475cf771e424fb7e6141aff66013abc382
-{% endblock %}
+{% extends 't/ix.sh' %}
 
 {% block bld_libs %}
-lib/c
-lib/intl
-lib/sigsegv
+{{super()}}
+lib/bsd/overlay
 {% endblock %}
