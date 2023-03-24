cf1d0fd25fc53875510d4e5660f1daf6b97df8c2
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-03-24 15:23:02 +0300

    upver

--- pkgs/bin/q/jackctl/ix.sh
+++ pkgs/bin/q/jackctl/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://download.sourceforge.net/qjackctl/qjackctl-0.9.7.tar.gz
-sha:524843618152070c90a40a18d0e9a16e784424ce54231aff5c0ced12f2769080
+https://download.sourceforge.net/qjackctl/qjackctl-0.9.10.tar.gz
+sha:9c07a8a98744b42c53a93864e47842d818dfaaea85078356851c6592997f1eee
 {% endblock %}
 
 {% block bld_libs %}
