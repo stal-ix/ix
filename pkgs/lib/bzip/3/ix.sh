4afe3346b26cea3f538cf90f4a0f4667e5bac615
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-03-24 23:24:57 +0300

    upver

--- pkgs/lib/bzip/3/ix.sh
+++ pkgs/lib/bzip/3/ix.sh
@@ -1,14 +1,6 @@
-{% extends '//die/c/autorehell.sh' %}
+{% extends 't/ix.sh' %}
 
-{% block fetch %}
-https://github.com/kspalaiologos/bzip3/archive/refs/tags/1.2.2.tar.gz
-sha:e7792f3c83f1d9efd0d7b18da2eb6a1f119ffcdeb5515cf441145c2e9b72652e
-{% endblock %}
-
-{% block lib_deps %}
-lib/c
-{% endblock %}
-
-{% block bld_tool %}
-bld/fakegit
+{% block install %}
+{{super()}}
+sed -e 's|.*/bin.*||' -i ${out}/lib/pkgconfig/bzip3.pc
 {% endblock %}
