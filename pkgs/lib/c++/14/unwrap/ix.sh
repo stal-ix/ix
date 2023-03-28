c82a4c2fedfb987805ef246686ba6357c74c4137
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-03-29 02:41:43 +0300

    better

--- pkgs/lib/c++/14/unwrap/ix.sh
+++ pkgs/lib/c++/14/unwrap/ix.sh
@@ -2,7 +2,30 @@
 
 {% block install %}
 {{super()}}
+
 sed -e 's|__wrap_iter<pointer>|pointer|' \
     -e 's|__wrap_iter<const_pointer>|const_pointer|' \
     -i ${out}/include/vector
+
+cat << EOF > fix.py
+import sys
+
+def it():
+    skip = False
+
+    for l in sys.stdin.read().split('\n'):
+        if 'vector<bool>' in l:
+            skip = True
+
+        if skip and '_Tp' in l:
+            skip = False
+
+        if not skip:
+            yield l
+
+sys.stdout.write('\n'.join(it()).strip() + '\n')
+EOF
+
+cat ${out}/include/vector | python3 fix.py > _
+mv _ ${out}/include/vector
 {% endblock %}
