80d7faae8072b7a877b463f3874ca3d3d560f73b
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-03-30 00:30:54 +0300

    add

---
+++ pkgs/bin/webp/gui/ix.sh
@@ -0,0 +1,21 @@
+{% extends '//bin/webp/ix.sh' %}
+
+{% block bld_libs %}
+lib/glu
+lib/sdl/1
+lib/sdl/deps
+lib/freeglut
+lib/shim/fake(lib_name=OpenGL)
+lib/shim/fake(lib_name=GLX)
+{{super()}}
+{% endblock %}
+
+{% block patch %}
+{{super()}}
+sed -e 's|WEBP_BUILD_VWEBP OFF|WEBP_BUILD_VWEBP ON|' -i CMakeLists.txt
+{% endblock %}
+
+{% block install %}
+{{super()}}
+cp ${tmp}/obj/vwebp_sdl ${out}/bin/
+{% endblock %}
