f18ceaa7deec1ee1120ad65c20aaa13e96302e9b
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-03-30 23:28:25 +0300

    upver

--- pkgs/lib/ng/tcp2/ix.sh
+++ pkgs/lib/ng/tcp2/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/ngtcp2/ngtcp2/archive/refs/tags/v0.14.0.tar.gz
-sha:3f32a6cf10e565ae435b18bbede14066941c5040029522f2dc71f760e5fb165b
+https://github.com/ngtcp2/ngtcp2/archive/refs/tags/v0.14.1.tar.gz
+sha:5871f8eb78b91ba8b8c5b98ec10ac689684cb711cf990d5aab8777bd95af63b4
 {% endblock %}
 
 {% block lib_deps %}
