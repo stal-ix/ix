f67fabab9f7ff95fd56c093f922e9230cefd5ad4
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-03-30 16:06:45 +0300

    upver

--- pkgs/bin/kernel/6/2/t/ver.sh
+++ pkgs/bin/kernel/6/2/t/ver.sh
@@ -1,2 +1,2 @@
-https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.2.8.tar.xz
-sha:fed0ad87d42f83a70ce019ff2800bc30a855e672e72bf6d54a014d98d344f665
+https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.2.9.tar.xz
+sha:903449c164c03f0e742aacc920e18563585e07a28c6cb79e0fd6c36695fd43f5
