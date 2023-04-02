ccedf3dfd7e0cdddb4605b77051aedf1a20650c7
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-04-02 23:39:59 +0300

    upver

--- pkgs/bin/copyq/ix.sh
+++ pkgs/bin/copyq/ix.sh
@@ -3,8 +3,8 @@
 {# no wayland support currently #}
 
 {% block fetch %}
-https://github.com/hluk/CopyQ/archive/refs/tags/v6.4.0.tar.gz
-sha:ac6757a10ed2d629de9acbb646cdbba5cf51197e9dfe39d3db2ff34069fc4831
+https://github.com/hluk/CopyQ/archive/refs/tags/v7.0.0.tar.gz
+sha:5898ba2025f8e9f61a4b4732e85adf3b1653fad20b5c1f1b0c879a7b72cd4d48
 {% endblock %}
 
 {% block bld_libs %}
@@ -16,6 +16,7 @@ lib/qt/6/declarative
 {% endblock %}
 
 {% block bld_tool %}
+bld/icu
 bld/qt/6
 bld/qt/6/tools
 bld/qt/6/tools/qml
