d764d9cd0e90b835dd6a9dce0cd9b3e7715d6e0b
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-04-02 22:51:41 +0300

    upver

--- pkgs/bin/ugrep/ix.sh
+++ pkgs/bin/ugrep/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://github.com/Genivia/ugrep/archive/refs/tags/v3.11.0.tar.gz
-sha:35ed29b251f71a7165b059b99b17cffe6947abebb8db025383aa9b897408e0be
+https://github.com/Genivia/ugrep/archive/refs/tags/v3.11.1.tar.gz
+sha:0a36d7bb62c49262b433eb301d8de6b90233a23446a71484738cd35650cc5c91
 {% endblock %}
 
 {% block bld_libs %}
