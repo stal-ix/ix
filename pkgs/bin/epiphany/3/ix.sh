5aa72dbfd38cc3ecb6229de513b7feecafe3a866
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-04-03 18:23:39 +0300

    refac

--- pkgs/bin/epiphany/3/ix.sh
+++ pkgs/bin/epiphany/3/ix.sh
@@ -1,10 +1,5 @@
 {% extends '//bin/epiphany/t/ix.sh' %}
 
-{% block fetch %}
-https://gitlab.gnome.org/GNOME/epiphany/-/archive/43.0/epiphany-43.0.tar.bz2
-sha:867117c09a532c06e2db0f283c153f310bfec0e28c4ef06955dfa1f3ec6167c7
-{% endblock %}
-
 {% block bld_libs %}
 lib/handy
 lib/dazzle
