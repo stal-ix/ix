bd86b23f50030cf8f425491e4d27ffdce31faf83
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-04-06 00:11:01 +0300

    upver

--- pkgs/bin/vifm/ix.sh
+++ pkgs/bin/vifm/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
-https://github.com/vifm/vifm/archive/refs/tags/v0.12.1.tar.gz
-sha:410b2f183e9fdefb145cf6811c106e9297c9d8f7ddfe38f8840869c63e089f95
+https://github.com/vifm/vifm/archive/refs/tags/v0.13.tar.gz
+sha:8379397b2824cc74a91f5cfa00b5496f5d08cdcec18e3d13b64c480151225ca8
 {% endblock %}
 
 {% block bld_libs %}
@@ -11,5 +11,6 @@ lib/curses
 {% endblock %}
 
 {% block bld_tool %}
+bld/perl
 bin/mandoc
 {% endblock %}
