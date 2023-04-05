2f9ba68731364c23879123137b853a8eded687f7
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-04-05 16:03:38 +0300

    upver

--- pkgs/lib/llvm/16/ver.sh
+++ pkgs/lib/llvm/16/ver.sh
@@ -1,2 +1,2 @@
-https://github.com/llvm/llvm-project/releases/download/llvmorg-16.0.0/llvm-project-16.0.0.src.tar.xz
-sha:9a56d906a2c81f16f06efc493a646d497c53c2f4f28f0cb1f3c8da7f74350254
+https://github.com/llvm/llvm-project/releases/download/llvmorg-16.0.1/llvm-project-16.0.1.src.tar.xz
+sha:ab7e3b95adb88fd5b669ca8c1d3c1e8d2a601c4478290d3ae31d8d70e96f2064
