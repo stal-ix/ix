4fb832a7bfbebb50a73b19b94014f3cdcb4d4faf
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-04-06 00:22:19 +0300

    fix build

--- pkgs/bin/aria/2/ix.sh
+++ pkgs/bin/aria/2/ix.sh
@@ -1,23 +1,6 @@
-{% extends '//die/c/autorehell.sh' %}
+{% extends '//die/hub.sh' %}
 
-{% block fetch %}
-https://github.com/aria2/aria2/archive/refs/tags/release-1.36.0.tar.gz
-sha:782b5dad48cb333e0cbef8795f868f7963455efecfd2a818feb8dab1cfa973f5
-{% endblock %}
-
-{% block bld_libs %}
-lib/c
-lib/z
-lib/uv
-lib/c++
-lib/gmp
-lib/expat
-lib/ssh/2
-lib/c/ares
-lib/openssl
-lib/sqlite/3
-{% endblock %}
-
-{% block bld_tool %}
-bld/gettext
+{% block run_deps %}
+# missing std::binary_function
+bin/aria/2/unwrap(cplpl_std=15)
 {% endblock %}
