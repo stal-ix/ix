24938add95fbc84c65aee582eda5928c4702830d
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-04-06 17:08:47 +0300

    upver

--- pkgs/bld/inkscape/ix.sh
+++ pkgs/bld/inkscape/ix.sh
@@ -1,5 +1,5 @@
 {% extends '//die/hub.sh' %}
 
 {% block run_deps %}
-bin/inkscape/unwrap(gtk_icons=aux/hicolor-icon-theme)
+bin/inkscape/unwrap(gtk_icons=aux/hicolor-icon-theme,cplpl_std=15)
 {% endblock %}
