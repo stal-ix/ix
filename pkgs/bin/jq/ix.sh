a66e472e5ac75157aa6b4ad8f6b306b6835702ff
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-04-09 01:47:35 +0300

    better

--- pkgs/bin/jq/ix.sh
+++ pkgs/bin/jq/ix.sh
@@ -1,7 +1,9 @@
 {% extends '//die/c/autorehell.sh' %}
 
+{% block version %}1.6{% endblock %}
+
 {% block fetch %}
-https://github.com/stedolan/jq/archive/refs/tags/jq-1.6.tar.gz
+https://github.com/stedolan/jq/archive/refs/tags/jq-{{self.version()}}.tar.gz
 sha:158182b85f3be9e23ab1dc50cfcc24e415aade2a0b8a5d9f709e0b587666d61b
 {% endblock %}
 
@@ -13,9 +15,13 @@ lib/oniguruma
 {% block bld_tool %}
 bld/flex
 bld/bison
+bld/fakegit
 {% endblock %}
 
-{% block patch1 %}
-# TODO(pg): proper bootstrap
+{% block cpp_defines %}
+JQ_VERSION=\\\"{{self.version()}}\\\"
+{% endblock %}
+
+{% block patch %}
 rm src/parser.c src/parser.h src/lexer.c src/lexer.h
 {% endblock %}
