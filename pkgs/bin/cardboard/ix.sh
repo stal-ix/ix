bd310618ecb4b354b2817359cf458ec3b93532d8
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-04-08 16:05:18 +0300

    fix

--- pkgs/bin/cardboard/ix.sh
+++ pkgs/bin/cardboard/ix.sh
@@ -1,5 +1,5 @@
 {% extends '//die/hub.sh' %}
 
 {% block run_deps %}
-bin/cardboard/unwrap(cplpl_std=16)
+bin/cardboard/unwrap
 {% endblock %}
--- pkgs/bin/ugrep/ix.sh
+++ pkgs/bin/ugrep/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://github.com/Genivia/ugrep/archive/refs/tags/v3.11.1.tar.gz
-sha:0a36d7bb62c49262b433eb301d8de6b90233a23446a71484738cd35650cc5c91
+https://github.com/Genivia/ugrep/archive/refs/tags/v3.11.2.tar.gz
+sha:a314cc6fe149eef9bc0f0d69c6b331d9d4491a100677c1b3fbb2159806cca2dd
 {% endblock %}
 
 {% block bld_libs %}
