24c1f99d72e30358abce980b597203b35e087b52
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-04-08 19:39:10 +0300

    upver

--- pkgs/bin/micro/ix.sh
+++ pkgs/bin/micro/ix.sh
@@ -9,7 +9,7 @@ sha:1bb499edeaaadf1fe1791a49f96ab672c4e1add31ee125882ccd85a0fc8a4abe
 {% endblock %}
 
 {% block go_sha %}
-sha:5694b1299c63d9d0699ff660138e7e5fec29457f04392393ba9160fa59965427
+sha:59843c7ab8b723062598c6a146ede0ace3c732347990d9284ddc807d17571dcd
 {% endblock %}
 
 {% block unpack %}
--- pkgs/bin/ted/unwrap/ix.sh
+++ pkgs/bin/ted/unwrap/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/python/bin.sh' %}
 
 {% block fetch %}
-https://github.com/pg83/ted/archive/refs/tags/v5.tar.gz
-sha:ab77978a182a737ccb2001cb819407538687a9ef503fe18449b7d4e3608600ce
+https://github.com/pg83/ted/archive/refs/tags/v6.tar.gz
+sha:8e220e3167ef8d447f966483d36988880b810b20a351b4dd38b791fb2a5116e3
 {% endblock %}
 
 {% block bld_libs %}
