21bb2708a352f3ded797fc2d3d361a883d0470a9
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-04-08 09:17:34 +0300

    upver

--- pkgs/lib/mesa/t/ix.sh
+++ pkgs/lib/mesa/t/ix.sh
@@ -1,10 +1,10 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-#https://gitlab.freedesktop.org/mesa/mesa/-/archive/mesa-23.0.0/mesa-mesa-23.0.0.tar.bz2
-#sha:49d59bea3ebcb562034e7d0a62bdc12069932d322182d5c0ec30f0e216892c44
-https://gitlab.freedesktop.org/mesa/mesa/-/archive/mesa-22.3.7/mesa-mesa-22.3.7.tar.bz2
-sha:687ee41d6725fefcdd78599756b290de6c52f76df5d84cae363c9d943641b1d4
+https://gitlab.freedesktop.org/mesa/mesa/-/archive/mesa-23.0.2/mesa-mesa-23.0.2.tar.bz2
+sha:20617a24eafd5e8ee7c8070a23d93bd66d378cee4715e0e219b0217c1d28c428
+#https://gitlab.freedesktop.org/mesa/mesa/-/archive/mesa-22.3.7/mesa-mesa-22.3.7.tar.bz2
+#sha:687ee41d6725fefcdd78599756b290de6c52f76df5d84cae363c9d943641b1d4
 {% endblock %}
 
 {% block lib_deps %}
@@ -54,7 +54,7 @@ EOF
 #find . -type f -name '*defaults.conf'
 #exit 1
 
-python3 ./merge.py src/util/00-mesa-defaults.conf src/amd/vulkan/00-radv-defaults.conf > _
+python3 ./merge.py src/util/00-mesa-defaults.conf src/util/00-radv-defaults.conf > _
 mv _ src/util/00-mesa-defaults.conf
 
 cat << EOF >> src/util/xmlconfig.h
