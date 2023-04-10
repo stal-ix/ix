1ee8f786c1c8f4bc9f1b950b7cc9d22f29470500
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-04-10 22:05:23 +0300

    upver

--- pkgs/bin/hypr/picker/ix.sh
+++ pkgs/bin/hypr/picker/ix.sh
@@ -1,13 +1,8 @@
 {% extends '//bin/hypr/t/ix.sh' %}
 
 {% block fetch %}
-https://github.com/hyprwm/hyprpicker/archive/6dc7f3321e04dca1c588b930856cae1fcc90f40a.zip
-sha:4b5230d303e040868dc45a92713a32a7063805c752df7b3ab57470bc48db8310
-{% endblock %}
-
-{% block patch %}
-{{super()}}
-sed -e 's|pLS->screenBuffer.data|(char*)pLS->screenBuffer.data|' -i src/hyprpicker.cpp
+https://github.com/hyprwm/hyprpicker/archive/refs/tags/v0.1.0.tar.gz
+sha:a347e8884f08853f16765c281f37dae7ad9518e3dd998816b5ecf0a29941a010
 {% endblock %}
 
 {% block install %}
