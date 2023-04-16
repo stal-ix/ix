d0ee79074f76f6a72e353f27c64ff83f0c6b1089
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-04-16 18:36:15 +0300

    better

--- pkgs/bin/dnsmasq/runit/ix.sh
+++ pkgs/bin/dnsmasq/runit/ix.sh
@@ -1,8 +1,26 @@
 {% extends '//die/hub.sh' %}
 
+{% set opts %}
+-2=lo
+--no-hosts
+--log-debug
+--log-queries
+--no-negcache
+--log-queries
+--filter-AAAA
+--user=dnsmasq
+--interface=lo
+--cache-size=1000
+--clear-on-reload
+--keep-in-foreground
+--pid-file=/var/run/dnsmasq/pid
+--conf-file=/var/run/resolvconf/dnsmasq_conf.conf
+--resolv-file=/var/run/resolvconf/dnsmasq_resolv.conf
+--log-facility=/var/run/dnsmasq/log
+{% endset %}
+
 {% block run_deps %}
 bin/dnsmasq
-etc/user/nobody
-bin/dnsmasq/runit/config
-etc/services/runit(srv_dir=dnsmasq,srv_command=exec /bin/dnsmasq --log-debug -d -C/etc/dnsmasq.conf -i lo --filter-AAAA)
+etc/user/dnsmasq
+etc/services/runit(srv_dir=dnsmasq,srv_command=exec /bin/dnsmasq {{ix.fix_list(opts)}})
 {% endblock %}
