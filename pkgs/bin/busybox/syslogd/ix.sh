3b6b3d2c9ffce9b38083bc20f5822fb3f1830a42
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-04-16 18:57:27 +0300

    better

--- pkgs/bin/busybox/syslogd/ix.sh
+++ pkgs/bin/busybox/syslogd/ix.sh
@@ -2,5 +2,5 @@
 
 {% block run_deps %}
 bin/busybox
-etc/services/runit(srv_dir=syslogd,srv_command=exec syslogd -n -O /var/log/syslog)
+etc/services/runit(srv_dir=syslogd,srv_command=exec syslogd -n -O /var/run/syslogd/log)
 {% endblock %}
