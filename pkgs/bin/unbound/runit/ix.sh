c3eea9bd7f32fb25b18cfdcad19639eeed7fc626
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-04-17 13:22:15 +0300

    better

--- pkgs/bin/unbound/runit/ix.sh
+++ pkgs/bin/unbound/runit/ix.sh
@@ -1,10 +1,8 @@
 {% extends '//die/hub.sh' %}
 
 {% block run_deps %}
-bin/subreaper
 bin/unbound/lite
 etc/user/unbound
 bin/unbound/runit/conf
-# TODO(pg83): proper hangup fix
-etc/services/runit(srv_dir=unbound,srv_command=exec /bin/subreaper /bin/timeout 300 unbound -d -p -v -c /etc/unbound.conf)
+etc/services/runit(srv_dir=unbound,srv_command=exec /bin/unbound -d -p -v -c /etc/unbound.conf)
 {% endblock %}
