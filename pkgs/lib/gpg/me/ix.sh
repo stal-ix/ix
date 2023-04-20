91d486a0823908034ac7b643d38e410f4412dfb8
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-04-20 21:04:48 +0300

    upver

--- pkgs/lib/gpg/me/ix.sh
+++ pkgs/lib/gpg/me/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
-https://www.gnupg.org/ftp/gcrypt/gpgme/gpgme-1.19.0.tar.bz2
-sha:cb58494dc415fba9eeb12b826550ad3190dc92e265c5bb2ae1a21c92841cfd38
+https://www.gnupg.org/ftp/gcrypt/gpgme/gpgme-1.20.0.tar.bz2
+sha:25a5785a5da356689001440926b94e967d02e13c49eb7743e35ef0cf22e42750
 {% endblock %}
 
 {% block lib_deps %}
