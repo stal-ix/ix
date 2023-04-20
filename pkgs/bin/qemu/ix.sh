08cf83cc006ccc2c74fdf3a915b8fcb90b3530c6
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-04-20 05:51:36 +0300

    upver

--- pkgs/bin/qemu/ix.sh
+++ pkgs/bin/qemu/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/configure.sh' %}
 
 {% block fetch %}
-https://download.qemu.org/qemu-7.1.0.tar.xz
-sha:a0634e536bded57cf38ec8a751adb124b89c776fe0846f21ab6c6728f1cbbbe6
+https://download.qemu.org/qemu-8.0.0.tar.xz
+sha:bb60f0341531181d6cc3969dd19a013d0427a87f918193970d9adb91131e56d0
 {% endblock %}
 
 {% block bld_libs %}
