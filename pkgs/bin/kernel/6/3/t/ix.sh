e50d6fc04c8247763ca4c21050dda51717a80436
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-04-25 01:32:25 +0300

    add

---
+++ pkgs/bin/kernel/6/3/t/ix.sh
@@ -0,0 +1,11 @@
+{% extends '//bin/kernel/t/2/ix.sh' %}
+
+{% block kernel_version %}6-3{% endblock %}
+
+{% block fetch %}
+{% include 'ver.sh' %}
+{% endblock %}
+
+{% block kernel_headers %}
+bin/kernel/6/3/headers
+{% endblock %}
---
+++ pkgs/bin/kernel/6/3/t/ver.sh
@@ -0,0 +1,2 @@
+https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.3.tar.xz
+sha:ba3491f5ed6bd270a370c440434e3d69085fcdd528922fa01e73d7657db73b1e
