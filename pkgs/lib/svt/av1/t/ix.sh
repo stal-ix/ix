812409859edbd1276ed4a332ef449ec59e817187
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-04-27 17:08:29 +0300

    upver

--- pkgs/lib/svt/av1/t/ix.sh
+++ pkgs/lib/svt/av1/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://gitlab.com/AOMediaCodec/SVT-AV1/-/archive/v1.4.1/SVT-AV1-v1.4.1.tar.bz2
-sha:0e988582f315fe76c909accf5e7f81b975c5bd2b850ee760d8e9fac297f70b5d
+https://gitlab.com/AOMediaCodec/SVT-AV1/-/archive/v1.5.0/SVT-AV1-v1.5.0.tar.bz2
+sha:a649b071906fb840df19fb0e2ec97c04fde82c8ed64dfb8662f625cb8bc6245e
 {% endblock %}
 
 {% block lib_deps %}
