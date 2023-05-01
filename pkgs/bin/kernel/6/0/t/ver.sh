bf7635f7aa7ebe977340bc2afd01a8e227e48b5f
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-05-01 18:12:17 +0300

    rm

--- pkgs/bin/kernel/6/0/t/ver.sh
+++
@@ -1,2 +0,0 @@
-https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.0.19.tar.xz
-sha:abe37eb0e2e331bdc7c4110115664e180e7d43b7336de6b4cd2bd1b123d30207
--- pkgs/bin/kernel/6/1/t/ver.sh
+++
@@ -1,2 +0,0 @@
-https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.1.12.tar.xz
-sha:d47aa675170904dcc93eeaa7c96db54d476a11c5d3e8cf3d3b96e364e2a0edea
