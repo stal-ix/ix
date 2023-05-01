7928e8fa5cb41b24841d86849759f4a7f488a025
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-05-01 18:28:16 +0300

    better

--- pkgs/bin/kernel/gengrub/fix.sh
+++ pkgs/bin/kernel/gengrub/fix.sh
@@ -4,7 +4,7 @@ cat << EOF > etc/grub.cfg
 probe --part-uuid \$root --set=rootuuid
 EOF
 
-ls bin/kernel-* | while read l; do
+ls bin/kernel-* | sort -r | while read l; do
     cat << EOF >> etc/grub.cfg
 menuentry "${l}" {
     linux /${l} ro root=PARTUUID=\$rootuuid
