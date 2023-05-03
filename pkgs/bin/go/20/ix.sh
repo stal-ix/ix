56baefdd7735a83ffb875350389e931f4437a7af
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-05-03 23:54:15 +0300

    upver

--- pkgs/bin/go/20/ix.sh
+++ pkgs/bin/go/20/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//bin/go/19/t/ix.sh' %}
 
 {% block fetch %}
-https://github.com/golang/go/archive/refs/tags/go1.20.3.tar.gz
-sha:991a67cecebb7b9b1237fdbca76c4754a9f5e1669d5d49b58a9931813047e905
+https://github.com/golang/go/archive/refs/tags/go1.20.4.tar.gz
+sha:3bcdd27c4b5a6fad1fa3ce1f6a7e522cd6f939d9151ca1e89f40b81ef7caeefe
 {% endblock %}
 
 {% block bld_libs %}
