709d37a3cf458d75e740ec40d7327b5ab2553490
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-05-11 21:38:24 +0300

    revert, broke spdlog

--- pkgs/lib/fmt/ix.sh
+++ pkgs/lib/fmt/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/fmtlib/fmt/archive/refs/tags/10.0.0.tar.gz
-sha:ede1b6b42188163a3f2e0f25ad5c0637eca564bd8df74d02e31a311dd6b37ad8
+https://github.com/fmtlib/fmt/archive/refs/tags/9.1.0.tar.gz
+sha:5dea48d1fcddc3ec571ce2058e13910a0d4a6bab4cc09a809d8b1dd1c88ae6f2
 {% endblock %}
 
 {% block lib_deps %}
