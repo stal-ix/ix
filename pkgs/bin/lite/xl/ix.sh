5e31599ad6e7b53bc8a96e6e08d919e077db9a08
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-05-14 19:12:17 +0300

    refac

--- pkgs/bin/lite/xl/ix.sh
+++ pkgs/bin/lite/xl/ix.sh
@@ -1,19 +1,5 @@
-{% extends '//die/c/meson.sh' %}
+{% extends '//die/hub.sh' %}
 
-{% block fetch %}
-https://github.com/lite-xl/lite-xl/archive/refs/tags/v2.1.1.tar.gz
-sha:75c0cbda2d34a88e86d3aa48abe9f0366093df3019796c03067a7594cc55abe6
-{% endblock %}
-
-{% block bld_libs %}
-lib/c
-lib/sdl/2
-lib/pcre/2
-lib/sdl/deps
-lib/freetype
-lib/lua/puc/5/4
-{% endblock %}
-
-{% block meson_flags %}
-renderer=true
+{% block run_deps %}
+bin/lite/xl/unwrap(lua_ver=puc/5/4)
 {% endblock %}
---
+++ pkgs/bin/lite/xl/unwrap/ix.sh
@@ -0,0 +1,19 @@
+{% extends '//die/c/meson.sh' %}
+
+{% block fetch %}
+https://github.com/lite-xl/lite-xl/archive/refs/tags/v2.1.1.tar.gz
+sha:75c0cbda2d34a88e86d3aa48abe9f0366093df3019796c03067a7594cc55abe6
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/lua
+lib/sdl/2
+lib/pcre/2
+lib/sdl/deps
+lib/freetype
+{% endblock %}
+
+{% block meson_flags %}
+renderer=true
+{% endblock %}
