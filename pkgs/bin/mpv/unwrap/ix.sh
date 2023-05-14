fa526e80ee0d3224a4edf5388f055197ef502298
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-05-14 18:57:03 +0300

    upver

---
+++ pkgs/bin/mpv/unwrap/ix.sh
@@ -0,0 +1,24 @@
+{% extends '//lib/mpv/t/ix.sh' %}
+
+{% block bld_libs %}
+lib/lua
+lib/mu/js
+lib/opengl
+lib/wayland
+lib/blueray
+lib/xkbcommon
+lib/drivers/3d
+lib/rubber/band
+lib/vulkan/headers
+{{super()}}
+{% endblock %}
+
+{% block bld_tool %}
+{{super()}}
+bld/wayland
+{% endblock %}
+
+{% block waf_flags %}
+{{super()}}
+--enable-javascript
+{% endblock %}
