11e3e39de982b7f96f742bc75e67acb8c69be058
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-05-15 22:21:07 +0300

    upver

--- pkgs/lib/cap/2/ix.sh
+++ pkgs/lib/cap/2/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//lib/cap/t/ix.sh' %}
 
 {% block fetch %}
-https://www.kernel.org/pub/linux/libs/security/linux-privs/libcap2/libcap-2.68.tar.gz
-sha:046e55716e0643b565efcd1dab1d26c5625709fcd0b5c271290c7ea1524cf906
+https://www.kernel.org/pub/linux/libs/security/linux-privs/libcap2/libcap-2.69.tar.gz
+sha:3a99ec26452e328e0ea408efd67096ef914f4ee4788fa8e8e21f214e2bd670b9
 {% endblock %}
 
 {% block host_libs %}
