10e259feddcde5d73116e91724e573b27127a089
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-06-10 21:05:56 +0300

    upver

--- pkgs/lib/grpc/ix.sh
+++ pkgs/lib/grpc/ix.sh
@@ -5,6 +5,12 @@ lib/protobuf
 {{super()}}
 {% endblock %}
 
+{% block bld_tool %}
+{{super()}}
+bin/protoc
+bin/grpc/codegen
+{% endblock %}
+
 {% block cmake_flags %}
 {{super()}}
 gRPC_BUILD_CODEGEN=OFF
