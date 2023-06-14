48adbc97ecadb0abc0b0de1df26086d317039615
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-06-14 16:40:44 +0300

    upver

--- pkgs/lib/md/ix.sh
+++ pkgs/lib/md/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
-https://libbsd.freedesktop.org/releases/libmd-1.0.4.tar.xz
-sha:f51c921042e34beddeded4b75557656559cf5b1f2448033b4c1eec11c07e530f
+https://libbsd.freedesktop.org/releases/libmd-1.1.0.tar.xz
+sha:1bd6aa42275313af3141c7cf2e5b964e8b1fd488025caf2f971f43b00776b332
 {% endblock %}
 
 {% block lib_deps %}
