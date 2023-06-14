b5b896da916c61991392fc076fdabd160c6f37f4
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-06-15 01:40:47 +0300

    description

--- pkgs/aux/xkeyboard/config/ix.sh
+++ pkgs/aux/xkeyboard/config/ix.sh
@@ -14,6 +14,18 @@ bld/gettext
 export XKB_CONFIG="${out}/share/X11/xkb"
 {% endblock %}
 
+{#
+ix/build/8QI51vcj4Yc20ltl/src/build/qemu-keymap -f pc-bios/keymaps/ar -l ar
+xkbcommon: ERROR: Couldn't find file "symbols/ar" in include paths
+xkbcommon: ERROR: 1 include paths searched:
+xkbcommon: ERROR:   /ix/store/tYxqJQfbGp45fjJD-aux-xkeyboard-config/share/X11/xkb
+xkbcommon: ERROR: 1 include paths could not be added:
+xkbcommon: ERROR:   /ix/store/uqszlytMmakBrOId-lib-xkbcommon/etc/xkb
+xkbcommon: ERROR: Abandoning symbols file "(unnamed)"
+xkbcommon: ERROR: Failed to compile xkb_symbols
+xkbcommon: ERROR: Failed to compile keymap
+#}
+
 {% block install %}
 {{super()}}
 cd ${out}/share/X11/xkb/symbols
