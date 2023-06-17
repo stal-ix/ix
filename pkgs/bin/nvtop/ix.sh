add77e516171d232fc0e9b5d527b4193b9ce365a
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-06-17 17:29:03 +0300

    upver

--- pkgs/bin/nvtop/ix.sh
+++ pkgs/bin/nvtop/ix.sh
@@ -2,11 +2,12 @@
 
 {% block fetch %}
 https://github.com/Syllo/nvtop/archive/refs/tags/3.0.2.tar.gz
-sha:
+sha:63fa45a2d675fe5320704850c216da6a6bb2edaf09821c26b3800fa7744bae00
 {% endblock %}
 
 {% block bld_libs %}
 lib/c
 lib/drm
+lib/udev
 lib/curses
 {% endblock %}
