854d19ad97ed0a4381bce48b44849c126e5f25b4
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-06-23 20:07:49 +0300

    better

---
+++ pkgs/bin/fizzy/ix.sh
@@ -0,0 +1,15 @@
+{% extends '//die/c/cmake.sh' %}
+
+{% block fetch %}
+https://github.com/wasmx/fizzy/archive/refs/tags/v0.8.0.tar.gz
+sha:ccccae0043749dc0246af59ef4ef5439a49d33b5a73499a855546bb32f6c5364
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/c++
+{% endblock %}
+
+{% block cmake_flags %}
+FIZZY_WASI=ON
+{% endblock %}
