4ae609355b42a6090f6b9f7f90f028f26f6c5580
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-06-24 01:17:11 +0300

    upver

--- pkgs/lib/zip/t/ix.sh
+++ pkgs/lib/zip/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://libzip.org/download/libzip-1.9.1.tar.xz
-sha:425f94fe9d23fefdc2261618a62afaea6b25803d47da4f9babf85f6f2f6b49af
+https://libzip.org/download/libzip-1.10.0.tar.xz
+sha:cd2a7ac9f1fb5bfa6218272d9929955dc7237515bba6e14b5ad0e1d1e2212b43
 {% endblock %}
 
 {% block lib_deps %}
