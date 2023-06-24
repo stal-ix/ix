3c6a9e47c3b6b909ec7c944e147759bbbe6e810e
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-06-24 14:33:51 +0300

    cleanups

--- pkgs/bin/wasm/edge/ix.sh
+++ pkgs/bin/wasm/edge/ix.sh
@@ -14,10 +14,8 @@ lib/llvm/15
 {% endblock %}
 
 {% block cmake_flags %}
-WASMEDGE_BUILD_AOT_RUNTIME=OFF
-#WASMEDGE_BUILD_SHARED_LIB=OFF
-#WASMEDGE_BUILD_STATIC_LIB=ON
 WASMEDGE_BUILD_PLUGINS=OFF
+WASMEDGE_BUILD_AOT_RUNTIME=OFF
 {% endblock %}
 
 {% block cpp_defines %}
