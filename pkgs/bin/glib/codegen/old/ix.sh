16fa1109d430ae1b8cf02a9526530379c1db5d67
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-06-26 18:32:14 +0300

    fix

---
+++ pkgs/bin/glib/codegen/old/ix.sh
@@ -0,0 +1,6 @@
+{% extends '//bin/glib/codegen/ix.sh' %}
+
+{% block fetch %}
+https://download.gnome.org/sources/glib/2.74/glib-2.74.5.tar.xz
+sha:ceba83a5999ceb31a4c4fc9921207cb9ffffd2ab1d6ec03c162d3f608a5c14c8
+{% endblock %}
