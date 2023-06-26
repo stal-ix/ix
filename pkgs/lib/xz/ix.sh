f11e7ce03a7b5f85f93d05deafb4199bba3feec7
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-06-27 01:07:39 +0300

    upver

--- pkgs/lib/xz/ix.sh
+++ pkgs/lib/xz/ix.sh
@@ -1,5 +1,24 @@
 {% extends 't/ix.sh' %}
 
+{% block configure_flags %}
+{{super()}}
+--disable-xz
+--disable-xzdec
+--disable-scripts
+--disable-lzmadec
+--disable-lzmainfo
+--disable-lzma-links
+{% if wasi %}
+--enable-threads=no
+{% endif %}
+{% endblock %}
+
+{% block cpp_defines %}
+{% if wasi %}
+__wasilibc_unmodified_upstream=1
+{% endif %}
+{% endblock %}
+
 {% block env %}
 export COFLAGS="--with-lzma=${out} --with-liblzma-prefix=${out} \${COFLAGS}"
 {% endblock %}
