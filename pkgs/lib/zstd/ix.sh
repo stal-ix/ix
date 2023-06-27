0b6ac3d8d13e5a001e46fe39852366e40fe91587
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-06-28 02:49:48 +0300

    better

--- pkgs/lib/zstd/ix.sh
+++ pkgs/lib/zstd/ix.sh
@@ -1,4 +1,9 @@
-{% extends '//lib/zstd/t/ix.sh' %}
+{% extends 't/ix.sh' %}
+
+{% block cmake_flags %}
+{{super()}}
+ZSTD_BUILD_PROGRAMS=OFF
+{% endblock %}
 
 {% block env %}
 export COFLAGS="--with-zstd=${out} \${COFLAGS}"
