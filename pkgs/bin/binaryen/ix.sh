31d20413aaf9f62ecf0bdbdf120cc6ddbbaa59f7
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-06-29 22:58:44 +0300

    upver

--- pkgs/bin/binaryen/ix.sh
+++ pkgs/bin/binaryen/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/WebAssembly/binaryen/archive/refs/tags/version_113.tar.gz
-sha:d2c32e85587ae95001a78a8765e12556be0fa8965e2f1fa6fc622aa2e0c4a33d
+https://github.com/WebAssembly/binaryen/archive/refs/tags/version_114.tar.gz
+sha:54f794a843d96cc841bf8045a9dfeaad8161341f0b50ac5b197518c2d39482ce
 {% endblock %}
 
 {% block bld_libs %}
