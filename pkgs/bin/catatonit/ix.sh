6e36d88b47f53e8e1db4c69bf4423908b676c2e0
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-06-29 16:23:43 +0300

    better

--- pkgs/bin/catatonit/ix.sh
+++ pkgs/bin/catatonit/ix.sh
@@ -1,8 +1,19 @@
 {% extends '//die/c/autorehell.sh' %}
 
-{% block fetch %}
-https://github.com/openSUSE/catatonit/archive/d8d72fea155c144ed3bf298a35a1aba5625a5656.tar.gz
-sem:041d742ff44484c78593ba8ce37e7dfd4fe67b1feb4ac5ea24bdd3968fdc9d55
+{% block git_repo %}
+https://github.com/openSUSE/catatonit
+{% endblock %}
+
+{% block git_branch %}
+main
+{% endblock %}
+
+{% block git_commit %}
+d8d72fea155c144ed3bf298a35a1aba5625a5656
+{% endblock %}
+
+{% block git_sha %}
+dc3d323a3dbd89897f0d03a20c4a055ace1b9ef6e6e2b5815a6be4455e488a90
 {% endblock %}
 
 {% block bld_libs %}
