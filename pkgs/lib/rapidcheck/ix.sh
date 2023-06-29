bf2a34d15af3da1b39c5c11ef16770885d739dee
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-06-29 04:36:32 +0300

    better

--- pkgs/lib/rapidcheck/ix.sh
+++ pkgs/lib/rapidcheck/ix.sh
@@ -1,8 +1,19 @@
 {% extends '//die/c/cmake.sh' %}
 
-{% block fetch %}
-https://github.com/emil-e/rapidcheck/archive/1505cbbce733bde3b78042cf2e9309c0b7f227a2.tar.gz
-sem:f637700386b3f63de6c9b246200cf7d339a51269e541e5238532bbec01ca1a2b
+{% block git_repo %}
+https://github.com/emil-e/rapidcheck
+{% endblock %}
+
+{% block git_commit %}
+a5724ea5b0b00147109b0605c377f1e54c353ba2
+{% endblock %}
+
+{% block git_branch %}
+master
+{% endblock %}
+
+{% block git_sha %}
+e567eba9ca32921a84c844f5132b23ac1e3b9c5cb5ba043b1c258d59e653f24b
 {% endblock %}
 
 {% block lib_deps %}
