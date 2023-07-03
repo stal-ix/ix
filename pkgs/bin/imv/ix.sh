b79d2e899640e1f26a91726e607bc6bfc167a194
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-07-04 02:48:18 +0300

    better

--- pkgs/bin/imv/ix.sh
+++ pkgs/bin/imv/ix.sh
@@ -1,30 +1,5 @@
-{% extends '//die/c/meson.sh' %}
+{% extends '//die/hub.sh' %}
 
-{% block fetch %}
-https://git.sr.ht/~exec64/imv/archive/v4.4.0.tar.gz
-sha:55431c8c27fbe82d05955f61bde38ba15febf8a7fd8f0f741e504bae0b823bcf
-{% endblock %}
-
-{% block bld_libs %}
-lib/c
-lib/icu
-lib/heif
-lib/inih
-lib/cairo
-lib/pango
-lib/opengl
-lib/ns/gif
-lib/wayland
-lib/xkbcommon
-lib/freeimage
-lib/jpeg/turbo
-lib/drivers/3d
-{% endblock %}
-
-{% block bld_tool %}
-bld/wayland
-{% endblock %}
-
-{% block meson_flags %}
-windows=wayland
+{% block run_deps %}
+bin/imv/unwrap(libjpeg_ver=turbo)
 {% endblock %}
--- pkgs/lib/jpeg/turbo/ix.sh
+++ pkgs/lib/jpeg/turbo/ix.sh
@@ -1,8 +1,10 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/libjpeg-turbo/libjpeg-turbo/archive/refs/tags/2.1.5.1.tar.gz
-sha:61846251941e5791005fb7face196eec24541fce04f12570c308557529e92c75
+#https://github.com/libjpeg-turbo/libjpeg-turbo/archive/refs/tags/2.1.5.1.tar.gz
+#sha:61846251941e5791005fb7face196eec24541fce04f12570c308557529e92c75
+https://github.com/libjpeg-turbo/libjpeg-turbo/archive/refs/tags/3.0.0.tar.gz
+sha:171dae5d73560bc94006a7c0c3281bd9bfde6a34f7e41e66f930a1a9162bd7df
 {% endblock %}
 
 {% block bld_tool %}
