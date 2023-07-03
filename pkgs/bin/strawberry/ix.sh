90a22942654a053acd34d120ee161ee3592c3769
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-07-04 02:23:57 +0300

    fix build

--- pkgs/bin/strawberry/ix.sh
+++ pkgs/bin/strawberry/ix.sh
@@ -16,8 +16,8 @@ lib/glib
 lib/alsa
 lib/boost
 lib/gnutls
+lib/protoc
 lib/sqlite/3
-lib/protobuf
 lib/gstreamer
 lib/qt/6/base
 lib/qt/6/deps
@@ -32,7 +32,12 @@ bld/qt/6/tools
 {% endblock %}
 
 {% block patch %}
+sed -e 's|.*FATAL.*Protobuf.*||' -i CMakeLists.txt
 find . -type f -name '*.cpp' | while read l; do
     sed -e 's|STRAWBERRY_VERSION_DISPLAY|"{{self.version()}}"|g' -i ${l}
 done
+{% endblock %}
+
+{% block cmake_flags %}
+protobuf_PROTOC_EXE="${PROTOC_EXE}"
 {% endblock %}
--- pkgs/lib/protobuf/ix.sh
+++ pkgs/lib/protobuf/ix.sh
@@ -4,3 +4,8 @@
 protobuf_BUILD_PROTOC_BINARIES=OFF
 {{super()}}
 {% endblock %}
+
+{% block install %}
+{{super()}}
+sed -e 's|protobuf::protoc|protoc|' -i ${out}/lib/cmake/protobuf/protobuf-generate.cmake
+{% endblock %}
