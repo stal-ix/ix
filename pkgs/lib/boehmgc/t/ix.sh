323b458cf60a8c447f62f39f1541a05d64a7e27b
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-07-05 02:45:11 +0300

    upver

--- pkgs/lib/boehmgc/t/ix.sh
+++ pkgs/lib/boehmgc/t/ix.sh
@@ -1,8 +1,10 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/ivmai/bdwgc/archive/742febb2f662a45b69bc0c5faef2111d576694fd.zip
-sha:b6065d4c6c80a04655894441321f182cb7b1d7fdfb3247f21f866a568468642c
+#https://github.com/ivmai/bdwgc/archive/742febb2f662a45b69bc0c5faef2111d576694fd.zip
+#sha:b6065d4c6c80a04655894441321f182cb7b1d7fdfb3247f21f866a568468642c
+https://github.com/ivmai/bdwgc/archive/refs/tags/v8.2.4.tar.gz
+sha:18e63ab1428bd52e691da107a6a56651c161210b11fbe22e2aa3c31f7fa00ca5
 {% endblock %}
 
 {% block lib_deps %}
