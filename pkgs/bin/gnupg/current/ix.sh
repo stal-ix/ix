ecf3a597c765d456d2fc7a38e682fd67b251e0ec
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-07-05 00:31:05 +0300

    upver

--- pkgs/bin/gnupg/current/ix.sh
+++ pkgs/bin/gnupg/current/ix.sh
@@ -1,6 +1,6 @@
 {% extends '//bin/gnupg/lts/ix.sh' %}
 
 {% block fetch %}
-https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-2.4.2.tar.bz2
-sha:97eb47df8ae5a3ff744f868005a090da5ab45cb48ee9836dbf5ee739a4e5cf49
+https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-2.4.3.tar.bz2
+sha:a271ae6d732f6f4d80c258ad9ee88dd9c94c8fdc33c3e45328c4d7c126bd219d
 {% endblock %}
