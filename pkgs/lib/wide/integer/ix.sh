0e1c3797aba485b03518146531f5785af422cb51
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-07-05 02:38:32 +0300

    better

--- pkgs/lib/wide/integer/ix.sh
+++ pkgs/lib/wide/integer/ix.sh
@@ -1,8 +1,15 @@
 {% extends '//die/c/cmake.sh' %}
 
-{% block fetch %}
-https://github.com/ckormanyos/wide-integer/archive/8f65d9deaf580ccb3b8fa76ae261ee3bbbc5aa4d.zip
-sha:9efb69fb0ad8b2695c935294b3f1fbd52cf0eb9602757b5e41c60cf040683856
+{% block git_repo %}
+https://github.com/ckormanyos/wide-integer
+{% endblock %}
+
+{% block git_commit %}
+d4ab8f42402d26bade2901fcc83c58a2bb9c5037
+{% endblock %}
+
+{% block git_sha %}
+4aabb2cb352993b688930466b31409dd6cdc093443f056b2c8be306a02215253
 {% endblock %}
 
 {% block lib_deps %}
