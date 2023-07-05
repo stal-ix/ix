b6131af0193052e956e5abf58c37fab64e855cdc
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-07-05 03:12:23 +0300

    better

--- pkgs/lib/tg/rlottie/ix.sh
+++ pkgs/lib/tg/rlottie/ix.sh
@@ -1,8 +1,15 @@
 {% extends '//die/c/cmake.sh' %}
 
-{% block fetch %}
-https://github.com/TelegramMessenger/rlottie/archive/67f103bc8b625f2a4a9e94f1d8c7bd84c5a08d1d.zip
-sha:7211ee04fc8bc36f07430423a0a9397d8793ff9c0a8feb9d802c50fb252fa9e1
+{% block git_repo %}
+https://github.com/TelegramMessenger/rlottie
+{% endblock %}
+
+{% block git_commit %}
+67f103bc8b625f2a4a9e94f1d8c7bd84c5a08d1d
+{% endblock %}
+
+{% block git_sha %}
+b866ccd7fc444b926571ef80a594cbba609b09e8a599adb26a45241d8eb33ac8
 {% endblock %}
 
 {% block lib_deps %}
