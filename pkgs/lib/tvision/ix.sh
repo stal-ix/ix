acfe71a0845bfb647bcbf0f090d5b15511465945
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-07-05 03:08:06 +0300

    better

--- pkgs/lib/tvision/ix.sh
+++ pkgs/lib/tvision/ix.sh
@@ -1,8 +1,15 @@
 {% extends '//die/c/cmake.sh' %}
 
-{% block fetch %}
-https://github.com/magiblot/tvision/archive/0decd5baf3700df39fecef5d3c450c92dda3b216.zip
-sha:1985df0062cbd9f68108bfec9bb6d06a862f333caf35bf0fa7ac5e711cb957ef
+{% block git_repo %}
+https://github.com/magiblot/tvision
+{% endblock %}
+
+{% block git_commit %}
+0decd5baf3700df39fecef5d3c450c92dda3b216
+{% endblock %}
+
+{% block git_sha %}
+02700d4b2327d73e9be4a9222a5580f57ff9ab7e7a57195ef94db0c767124795
 {% endblock %}
 
 {% block lib_deps %}
