2c6aee32ab0b71dd981ef7cfa7a0a8c5a1ea9fb3
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-07-07 23:09:29 +0300

    upver

--- pkgs/lib/intel/pt/ix.sh
+++ pkgs/lib/intel/pt/ix.sh
@@ -1,11 +1,11 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/intel/libipt/archive/refs/tags/v2.0.4.tar.gz
-md5:cf2c97292dc61ea898dd84b460921f79
+https://github.com/intel/libipt/archive/refs/tags/v2.0.6.tar.gz
+sha:631f4ebc81e12c0493288af3cc307b0de5b65f018d3e3839c87014b0d26acd75
 {% endblock %}
 
-{% block env_lib %}
+{% block env %}
 export COFLAGS="--with-libipt-prefix=${out} \${COFLAGS}"
 {% endblock %}
 
