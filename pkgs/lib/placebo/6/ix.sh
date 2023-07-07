d9e8976cb20eef2aef6cf2ce6a67c1405593b93a
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-07-07 15:13:05 +0300

    add

---
+++ pkgs/lib/placebo/6/ix.sh
@@ -0,0 +1,6 @@
+{% extends '//lib/placebo/5/ix.sh' %}
+
+{% block fetch %}
+https://github.com/haasn/libplacebo/archive/refs/tags/v6.292.0.tar.gz
+sha:cd9ecbea9b2cd61c4ea5816d262f620a3daff194399d2090e5a485c9adb294bb
+{% endblock %}
