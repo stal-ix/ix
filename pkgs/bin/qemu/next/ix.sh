0da64915d03ae8d4bdbe034882ebd35e45d58ce0
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-07-09 20:49:40 +0300

    add

---
+++ pkgs/bin/qemu/next/ix.sh
@@ -0,0 +1,13 @@
+{% extends '//bin/qemu/t/ix.sh' %}
+
+{% block git_repo %}
+https://github.com/qemu/qemu
+{% endblock %}
+
+{% block git_branch %}
+v8.0.3
+{% endblock %}
+
+{% block git_sha %}
+a44c14c068cd941514a42168d47bf505d4263d0748937a5564617ecc45ca5558
+{% endblock %}
