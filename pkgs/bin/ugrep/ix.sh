35b8d41c93a4cb559a9c62b55e4bfbacf9ad68f9
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-07-10 03:51:27 +0300

    upver

--- pkgs/bin/ugrep/ix.sh
+++ pkgs/bin/ugrep/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://github.com/Genivia/ugrep/archive/refs/tags/v3.12.0.tar.gz
-sha:f69330b74a2d2e46c878c19da3453e97d92ae960d0e1a92a853481cb889fca3e
+https://github.com/Genivia/ugrep/archive/refs/tags/v3.12.2.tar.gz
+sha:37c207f5363fad2aea2d9793b9131fba5c2ba5d4360be575a6956c1aeba629c5
 {% endblock %}
 
 {% block bld_libs %}
@@ -13,12 +13,4 @@ lib/lz4
 lib/zstd
 lib/bzip/2
 lib/pcre/2
-{% endblock %}
-
-{% block bld_tool %}
-bld/bash
-{% endblock %}
-
-{% block patch %}
-sed -e 's|/bin/bash|/usr/bin/env bash|' -i add-filters.sh
 {% endblock %}
