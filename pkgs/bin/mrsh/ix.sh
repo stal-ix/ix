e236ae71d508a09b798c26e139e584ec98b8a88f
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-07-10 13:45:05 +0300

    better

--- pkgs/bin/mrsh/ix.sh
+++ pkgs/bin/mrsh/ix.sh
@@ -1,8 +1,15 @@
 {% extends '//die/c/meson.sh' %}
 
-{% block fetch %}
-https://github.com/emersion/mrsh/archive/cd3c3a48055ab4085d83f149ff4b4feba40b40cb.zip
-md5:23e571a3436846a1722ddbdfc22e3377
+{% block git_repo %}
+https://github.com/emersion/mrsh
+{% endblock %}
+
+{% block git_commit %}
+cd3c3a48055ab4085d83f149ff4b4feba40b40cb
+{% endblock %}
+
+{% block git_sha %}
+ad4a9985d1f93ed25397a3814d7bea582241377e8cbfe44293b4855680b012b2
 {% endblock %}
 
 {% block bld_libs %}
