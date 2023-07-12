afb48ee73dff521c42ae35035820541dde5e736d
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-07-12 16:07:39 +0300

    proper deps

--- pkgs/bin/nghttp2/ix.sh
+++ pkgs/bin/nghttp2/ix.sh
@@ -1,14 +1,22 @@
 {% extends '//lib/ng/http/2/t/ix.sh' %}
 
 {% block bld_libs %}
+lib/z
+lib/ev
 lib/xml/2
 lib/boost
+lib/c/ares
+lib/ng/tcp2
 lib/jansson
+lib/openssl
+lib/ng/http/3
 {{super()}}
 {% endblock %}
 
 {% block configure_flags %}
 {{super()}}
 --enable-app
+#--with-libngtcp2
+#--with-libnghttp3
 --enable-hpack-tools
 {% endblock %}
