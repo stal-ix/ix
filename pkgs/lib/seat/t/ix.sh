3f52ef926c20ab2809839ddd57a7f4b5efec734d
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-07-19 23:25:17 +0300

    upver

--- pkgs/lib/seat/t/ix.sh
+++ pkgs/lib/seat/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://git.sr.ht/~kennylevinsen/seatd/archive/0.7.0.tar.gz
-sha:210ddf8efa1149cde4dd35908bef8e9e63c2edaa0cdb5435f2e6db277fafff3c
+https://git.sr.ht/~kennylevinsen/seatd/archive/0.8.0.tar.gz
+sha:a562a44ee33ccb20954a1c1ec9a90ecb2db7a07ad6b18d0ac904328efbcf65a0
 {% endblock %}
 
 {% block lib_deps %}
