df0c6fcbd744311154b0baa594cbe6110b51a96f
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-07-20 16:06:33 +0300

    fix path

--- pkgs/lib/gmp/ix.sh
+++ pkgs/lib/gmp/ix.sh
@@ -1,8 +1,10 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://gmplib.org/download/gmp/gmp-6.2.1.tar.lz
-md5:03a31d8cbaf29d136252f8f38875ed82
+#https://gmplib.org/download/gmp/gmp-6.2.1.tar.lz
+#md5:03a31d8cbaf29d136252f8f38875ed82
+https://github.com/stal-ix/sources/raw/main/gmp-6.2.1.tar.xz
+sha:fd4829912cddd12f84181c3451cc752be224643e87fac497b69edddadc49b4f2
 {% endblock %}
 
 {% block lib_deps %}
