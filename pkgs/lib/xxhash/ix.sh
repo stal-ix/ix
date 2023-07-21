be97c7299f3bf298a777d909aaea417d7b5c6c44
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-07-22 00:38:32 +0300

    better

--- pkgs/lib/xxhash/ix.sh
+++ pkgs/lib/xxhash/ix.sh
@@ -1,5 +1,10 @@
 {% extends 't/ix.sh' %}
 
+{% block std_env %}
+{{super()}}
+bld/wrapcc
+{% endblock %}
+
 {% block env %}
 {{super()}}
 export COFLAGS="--with-xxhash=${out} --with-libxxhash-prefix=${out} \${COFLAGS}"
