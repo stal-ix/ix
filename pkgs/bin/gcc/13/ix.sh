f4234fbbacc4c3982393b507b77a64c59976186a
Anton Samokhvalov <pg@yandex-team.ru>
2023-07-23 23:00:16 +0300

    upver

--- pkgs/bin/gcc/13/ix.sh
+++ pkgs/bin/gcc/13/ix.sh
@@ -4,11 +4,3 @@
 http://mirror.koddos.net/gcc/releases/gcc-13.1.0/gcc-13.1.0.tar.xz
 sha:61d684f0aa5e76ac6585ad8898a2427aade8979ed5e7f85492286c4dfc13ee86
 {% endblock %}
-
-{% block install %}
-# clock skew
-find . -type f -name '*.d' -delete
-find . -type f -name '*.Po' -delete
-{{super()}}
-{% endblock %}
-
--- pkgs/bin/kernel/6/4/t/ver.sh
+++ pkgs/bin/kernel/6/4/t/ver.sh
@@ -1,2 +1,2 @@
 https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.4.5.tar.xz
-sha:
+sha:374e2c07463c51dfd71204b7fac3b73c7f973550ae019b74e9f2b815b28de9b7
