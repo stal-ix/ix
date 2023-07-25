c136a2e792edf2c107fe32306d9ae0644aa53dba
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-07-26 00:29:16 +0300

    better

--- pkgs/bin/sniffer/ix.sh
+++ pkgs/bin/sniffer/ix.sh
@@ -5,7 +5,7 @@ https://github.com/chenjiandongx/sniffer/archive/refs/tags/v0.6.1.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-80c644f80306633a6b3e33c1cad5a7f810544d6ec1bc2630721615b737d4608e
+28ae63bbf927aa869a88ab7c0bd968ee6653e6ac913c19099fd58003b58bdb95
 {% endblock %}
 
 {% block bld_libs %}
