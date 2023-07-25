a0c5ac92fbe7f94153159d5549ab1494780d0f57
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-07-25 20:12:08 +0300

    fix

--- pkgs/bin/gopass/ix.sh
+++ pkgs/bin/gopass/ix.sh
@@ -5,7 +5,7 @@ https://github.com/gopasspw/gopass/archive/refs/tags/v1.14.3.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-sha:c0423c287ce3e989076a625b0ca9cbe0bf0469fd1a6609e3df562cd9b961543d
+c0423c287ce3e989076a625b0ca9cbe0bf0469fd1a6609e3df562cd9b961543d
 {% endblock %}
 
 {% block install %}
