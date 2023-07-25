496bf3777aa877e6fdaba0b0ed20b24b85f23908
Anton Samokhvalov <pg@yandex-team.ru>
2023-07-25 04:47:36 +0300

    fix build

--- pkgs/bin/goawk/ix.sh
+++ pkgs/bin/goawk/ix.sh
@@ -5,7 +5,7 @@ https://github.com/benhoyt/goawk/archive/refs/tags/v1.24.0.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-sha:7005be5287fb18932d1349464bf7297bbc8eec8c78e072e496d4470d33460c79
+160c318c1dbca10fcfdb460dd97a362f8f5ac14668fa733da31f54ce7698d9a0
 {% endblock %}
 
 {% block install %}
