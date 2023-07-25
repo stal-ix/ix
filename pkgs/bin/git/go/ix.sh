8e36aae5d00d2ae35e2d758e923104a8d82c885b
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-07-25 20:14:47 +0300

    fix

--- pkgs/bin/git/go/ix.sh
+++ pkgs/bin/git/go/ix.sh
@@ -5,7 +5,7 @@ https://github.com/go-git/go-git/archive/refs/tags/v5.4.2.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-sha:e09ed69954f655e8abdd78cd375531ac53a54afdb33574db65a55f80294b827a
+e09ed69954f655e8abdd78cd375531ac53a54afdb33574db65a55f80294b827a
 {% endblock %}
 
 {% block unpack %}
