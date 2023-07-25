d1f5a10dd51ca0955831d6d77b57632f74dd021d
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-07-25 20:15:41 +0300

    fix

--- pkgs/bin/emptty/stock/ix.sh
+++ pkgs/bin/emptty/stock/ix.sh
@@ -7,7 +7,7 @@ https://github.com/tvrzna/emptty/archive/refs/tags/v0.10.0.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-sha:081cc826ec8e8d9239ca7fafe71c2b8074b63c1948efceb3d635fc20dcf99621
+081cc826ec8e8d9239ca7fafe71c2b8074b63c1948efceb3d635fc20dcf99621
 {% endblock %}
 
 {% block go_tags %}
