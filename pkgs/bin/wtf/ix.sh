f7dc7901a9d187625ff118a07d065e56db44bce1
Anton Samokhvalov <pg@yandex-team.ru>
2023-07-25 17:32:27 +0300

    fix

--- pkgs/bin/wtf/ix.sh
+++ pkgs/bin/wtf/ix.sh
@@ -5,7 +5,7 @@ https://github.com/wtfutil/wtf/archive/refs/tags/v0.41.0.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-sha:6bfa1141fec6be6f6b58249bb0595f9aa7cd3978f9205a8be0f12922990be8b1
+0bb9bdc69cc7e7305f6dcc2167738ce30fe9f14e974811e702f58c6c1ceeda5a
 {% endblock %}
 
 {% block go_build_flags %}
