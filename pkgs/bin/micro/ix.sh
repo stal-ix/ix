43e343d4e018eec02c8b9450eb399aaa89b936a3
Anton Samokhvalov <pg@yandex-team.ru>
2023-07-25 05:28:09 +0300

    fix

--- pkgs/bin/micro/ix.sh
+++ pkgs/bin/micro/ix.sh
@@ -5,7 +5,7 @@ https://github.com/zyedidia/micro/archive/refs/tags/v2.0.11.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-sha:59843c7ab8b723062598c6a146ede0ace3c732347990d9284ddc807d17571dcd
+f62313bdef160f44357d12414a50922e574b3d14bc25df84e7a7eb5011f6c369
 {% endblock %}
 
 {% block unpack %}
