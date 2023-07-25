4817449158f5d2393152f51758a88b2cba1a4291
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-07-25 19:10:59 +0300

    fix

--- pkgs/bin/nwg/drawer/ix.sh
+++ pkgs/bin/nwg/drawer/ix.sh
@@ -5,7 +5,7 @@ https://github.com/nwg-piotr/nwg-drawer/archive/refs/tags/v0.3.7.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-sha:1cd17354a04aab0f78900d13294a707494c7f9c7dc284deb5410c379f4d1d183
+1cd17354a04aab0f78900d13294a707494c7f9c7dc284deb5410c379f4d1d183
 {% endblock %}
 
 {% block install %}
