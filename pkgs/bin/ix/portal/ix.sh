92898f1b7f266417f1a0c1b51bddc946f6ccfed2
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-07-25 20:05:47 +0300

    fix

--- pkgs/bin/ix/portal/ix.sh
+++ pkgs/bin/ix/portal/ix.sh
@@ -5,7 +5,7 @@ https://github.com/pg83/portal/archive/refs/tags/1.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-sha:182aca4153e1476644c541b34b0e1849b072602678753aed17bc149853c16e43
+182aca4153e1476644c541b34b0e1849b072602678753aed17bc149853c16e43
 {% endblock %}
 
 {% block install %}
