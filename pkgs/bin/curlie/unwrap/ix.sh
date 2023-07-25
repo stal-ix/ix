8d520591b48f991316c30b82efe632ddcd7d54de
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-07-26 00:36:48 +0300

    upver

--- pkgs/bin/curlie/unwrap/ix.sh
+++ pkgs/bin/curlie/unwrap/ix.sh
@@ -5,7 +5,7 @@ https://github.com/rs/curlie/archive/refs/tags/v1.6.9.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-18adf1faf09781224b3d9d7d7986fda86de2e8ebaf600d29e2281decd2d89e2e
+11e17bf6f1d0c637cd0defd66850ea10611b044e0c20b74c45deb14170c90a32
 {% endblock %}
 
 {% block patch %}
