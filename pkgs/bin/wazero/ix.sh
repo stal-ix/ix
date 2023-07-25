9459ffb078acfa0d6876460f1ca9c3c9f161f676
Anton Samokhvalov <pg@yandex-team.ru>
2023-07-25 17:48:32 +0300

    fix

--- pkgs/bin/wazero/ix.sh
+++ pkgs/bin/wazero/ix.sh
@@ -5,7 +5,7 @@ https://github.com/tetratelabs/wazero/archive/refs/tags/v1.3.1.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-sha:af74e96af6e0c6ab34ad3e37e52251b59b1e24c59edf6667ca03fdb97f8a8079
+af74e96af6e0c6ab34ad3e37e52251b59b1e24c59edf6667ca03fdb97f8a8079
 {% endblock %}
 
 {% block unpack %}
