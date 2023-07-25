5f3b712aee2d14ff65c857ca46538c37b549a7cf
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-07-25 19:19:37 +0300

    fix

--- pkgs/bin/nwg/dock/ix.sh
+++ pkgs/bin/nwg/dock/ix.sh
@@ -5,7 +5,7 @@ https://github.com/nwg-piotr/nwg-dock/archive/refs/tags/v0.3.1.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-sha:5efdf211fc4cbe8d8008595231908709de3fc93a2eb6227d90e25e167e56e585
+5efdf211fc4cbe8d8008595231908709de3fc93a2eb6227d90e25e167e56e585
 {% endblock %}
 
 {% block install %}
