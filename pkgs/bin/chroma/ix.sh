70c65caf3db8dfdddd9d3b1495d36bf94527d0fe
Anton Samokhvalov <pg@yandex-team.ru>
2023-07-25 17:45:46 +0300

    fix

--- pkgs/bin/chroma/ix.sh
+++ pkgs/bin/chroma/ix.sh
@@ -5,7 +5,7 @@ https://github.com/alecthomas/chroma/archive/refs/tags/v2.8.0.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-sha:38eea3b350354ffa0c2b446364da12cb12a8156568bb2c1f7cfc2d69fe82e322
+38eea3b350354ffa0c2b446364da12cb12a8156568bb2c1f7cfc2d69fe82e322
 {% endblock %}
 
 {% block unpack %}
