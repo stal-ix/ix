f813066963b502a8ba04186bf18f2ec325d13743
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-07-25 19:10:04 +0300

    fix

--- pkgs/bin/nwg/menu/ix.sh
+++ pkgs/bin/nwg/menu/ix.sh
@@ -5,7 +5,7 @@ https://github.com/nwg-piotr/nwg-menu/archive/refs/tags/v0.1.1.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-sha:4bab6977ed8028583a053c8af27ec08033933a84b3ea7e5efb0fe9348598777c
+4bab6977ed8028583a053c8af27ec08033933a84b3ea7e5efb0fe9348598777c
 {% endblock %}
 
 {% block install %}
