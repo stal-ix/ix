19bf36795e4a899a07902b99ebd61f841f4a88e8
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-07-26 00:40:56 +0300

    fix

--- pkgs/bin/shadowsocks/go/2/ix.sh
+++ pkgs/bin/shadowsocks/go/2/ix.sh
@@ -5,7 +5,7 @@ https://github.com/shadowsocks/go-shadowsocks2/archive/refs/tags/v0.1.5.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-f5444c003e1e75476131d03cf3fe9b3a9bec9dbd8934fc687a2daccc44dfd37c
+6748ce1af793e3f961251410c278a00d119da44918cbbcd28f1ed402c6691c6d
 {% endblock %}
 
 {% block install %}
