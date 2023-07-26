e31b6d25cbdfe819233928158225c645eeed7f1d
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-07-26 04:40:41 +0300

    fix

--- pkgs/bin/etcd/ix.sh
+++ pkgs/bin/etcd/ix.sh
@@ -5,7 +5,7 @@ https://github.com/etcd-io/etcd/archive/refs/tags/v3.4.22.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-c8b3cc5444eac371cd31893edce688c2be17b05359e76e21b57c6d92415674bf
+f41af4fe4507b1f36a3d898f4f687f91d129e32f9412c876b87ff04c77a611cc
 {% endblock %}
 
 {% block bld_libs %}
