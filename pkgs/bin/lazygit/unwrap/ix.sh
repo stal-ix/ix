152843285a96e96efbcaa9dada40f66bd22ee52c
Anton Samokhvalov <pg@yandex-team.ru>
2023-07-26 22:15:17 +0300

    fix

--- pkgs/bin/lazygit/unwrap/ix.sh
+++ pkgs/bin/lazygit/unwrap/ix.sh
@@ -5,7 +5,7 @@ https://github.com/jesseduffield/lazygit/archive/refs/tags/v0.39.4.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-5d2b311b2a07a8a98c9bd22cceaf4a4ebe94a754969bc5ab09060a0f8774ad98
+83431a770c6fc604b2ad7c57c1b8c5c1bdc71047cb07c713dc357166284a999c
 {% endblock %}
 
 {% block install %}
