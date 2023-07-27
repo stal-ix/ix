5702d485a68a5948fdec5ca5bdb774a5b20a5a26
Anton Samokhvalov <pg@yandex-team.ru>
2023-07-27 22:54:42 +0300

    fix

--- pkgs/lib/wide/integer/ix.sh
+++ pkgs/lib/wide/integer/ix.sh
@@ -9,7 +9,7 @@ d4ab8f42402d26bade2901fcc83c58a2bb9c5037
 {% endblock %}
 
 {% block git_sha %}
-4aabb2cb352993b688930466b31409dd6cdc093443f056b2c8be306a02215253
+39a2613ed428fd22dc011c39cb50105552b5635dc309d844022e607840e104af
 {% endblock %}
 
 {% block lib_deps %}
