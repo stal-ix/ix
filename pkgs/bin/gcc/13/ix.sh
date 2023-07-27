dd6819c0925d095b0d73097e4ed8da6aba7c1851
Anton Samokhvalov <pg@yandex-team.ru>
2023-07-27 23:35:10 +0300

    upver

--- pkgs/bin/gcc/13/ix.sh
+++ pkgs/bin/gcc/13/ix.sh
@@ -1,6 +1,6 @@
 {% extends '//bin/gcc/12/ix.sh' %}
 
 {% block fetch %}
-http://mirror.koddos.net/gcc/releases/gcc-13.1.0/gcc-13.1.0.tar.xz
-sha:61d684f0aa5e76ac6585ad8898a2427aade8979ed5e7f85492286c4dfc13ee86
+http://mirror.koddos.net/gcc/releases/gcc-13.2.0/gcc-13.2.0.tar.xz
+sha:e275e76442a6067341a27f04c5c6b83d8613144004c0413528863dc6b5c743da
 {% endblock %}
