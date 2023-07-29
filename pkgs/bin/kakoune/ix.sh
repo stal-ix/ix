a921ce2df29cde1a24f17f343b9d2f05dd0241c7
Anton Samokhvalov <pg@yandex-team.ru>
2023-07-29 22:05:58 +0300

    upver

--- pkgs/bin/kakoune/ix.sh
+++ pkgs/bin/kakoune/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/make.sh' %}
 
 {% block fetch %}
-https://github.com/mawww/kakoune/archive/refs/tags/v2022.10.31.tar.gz
-sha:f3eab1e663763c62881d553ad6263526940fa3ca357e0183767885415fc80b03
+https://github.com/mawww/kakoune/archive/refs/tags/v2023.07.29.tar.gz
+sha:0fbefc81783903b7301359cbc2e0754ba008af6f2f6ef83651a32eec7375497b
 {% endblock %}
 
 {% block bld_libs %}
