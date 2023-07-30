5e111f2916ffc6d6253e8bed4fa52312916fe6a3
Anton Samokhvalov <pg@yandex-team.ru>
2023-07-30 20:37:18 +0300

    upver

--- pkgs/bin/binutils/t/ver.sh
+++ pkgs/bin/binutils/t/ver.sh
@@ -1,2 +1,2 @@
-https://ftp.gnu.org/gnu/binutils/binutils-2.40.tar.bz2
-sha:f8298eb153a4b37d112e945aa5cb2850040bcf26a3ea65b5a715c83afe05e48a
+https://ftp.gnu.org/gnu/binutils/binutils-2.41.tar.bz2
+sha:a4c4bec052f7b8370024e60389e194377f3f48b56618418ea51067f67aaab30b
