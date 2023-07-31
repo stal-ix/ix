89e0acaf1757b04a29696675f5991de7b77d277c
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-07-31 21:16:02 +0300

    upver

--- pkgs/bin/squashfs/fuse/ix.sh
+++ pkgs/bin/squashfs/fuse/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://github.com/vasi/squashfuse/archive/refs/tags/0.3.0.tar.gz
-sha:cc59722e1c7fafb142146f394eacec20f020330ada848113c2e1fc83ab679c05
+https://github.com/vasi/squashfuse/archive/refs/tags/0.4.0.tar.gz
+sha:bddbfeed1b50e795a4a466b2977617e48904108e261d64563234e98ea2cd2f19
 {% endblock %}
 
 {% block bld_libs %}
