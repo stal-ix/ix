7c9042d3d416998c36aca09cf0e47ddfbb574381
Anton Samokhvalov <pg@yandex-team.ru>
2023-08-01 18:08:15 +0300

    upver

--- pkgs/lib/re2/ix.sh
+++ pkgs/lib/re2/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/google/re2/archive/refs/tags/2023-07-01.tar.gz
-sha:18cf85922e27fad3ed9c96a27733037da445f35eb1a2744c306a37c6d11e95c4
+https://github.com/google/re2/archive/refs/tags/2023-08-01.tar.gz
+sha:d82d0efe2389949244445e7a6ac9a10fccc3d6a3d267ec4652991a51291647b0
 {% endblock %}
 
 {% block lib_deps %}
