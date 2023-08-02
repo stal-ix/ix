9951535f94ee50c5c5c31c3610a75ee8d97344d9
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-08-03 02:52:44 +0300

    upver

--- pkgs/lib/harfbuzz/t/ix.sh
+++ pkgs/lib/harfbuzz/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://github.com/harfbuzz/harfbuzz/archive/refs/tags/8.1.0.tar.gz
-sha:8d544f1b74797b7b4d88f586e3b9202528b3e8c17968d28b7cdde02041bff5a0
+https://github.com/harfbuzz/harfbuzz/archive/refs/tags/8.1.1.tar.gz
+sha:b16e6bc0fc7e6a218583f40c7d201771f2e3072f85ef6e9217b36c1dc6b2aa25
 {% endblock %}
 
 {% block lib_deps %}
