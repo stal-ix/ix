3e8757e461975f53452d461b24b5cf6a5a43d345
Anton Samokhvalov <pg@yandex-team.ru>
2023-08-02 03:04:55 +0300

    fix

--- pkgs/lib/mnl/ix.sh
+++ pkgs/lib/mnl/ix.sh
@@ -1,7 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://www.netfilter.org/pub/libmnl/libmnl-1.0.5.tar.bz2
+#https://www.netfilter.org/pub/libmnl/libmnl-1.0.5.tar.bz2
+https://github.com/stal-ix/sources/raw/main/libmnl-1.0.5.tar.bz2
 sha:274b9b919ef3152bfb3da3a13c950dd60d6e2bcd54230ffeca298d03b40d0525
 {% endblock %}
 
