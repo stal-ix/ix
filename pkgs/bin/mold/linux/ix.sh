0c05f79dc42c2a59eae26a8d4e5323e3c826a6dc
Anton Samokhvalov <pg@yandex-team.ru>
2023-08-03 03:46:37 +0300

    upver

--- pkgs/bin/mold/linux/ix.sh
+++ pkgs/bin/mold/linux/ix.sh
@@ -1,6 +1,6 @@
 {% extends '//bin/mold/darwin/ix.sh' %}
 
 {% block fetch %}
-https://github.com/rui314/mold/archive/refs/tags/v1.11.0.tar.gz
-sha:99318eced81b09a77e4c657011076cc8ec3d4b6867bd324b8677974545bc4d6f
+https://github.com/rui314/mold/archive/refs/tags/v2.0.0.tar.gz
+sha:2ae8a22db09cbff626df74c945079fa29c1e5f60bbe02502dcf69191cf43527b
 {% endblock %}
