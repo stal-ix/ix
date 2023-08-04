cbf00883e8868253d731eb12cd26fe2015fc7214
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-08-04 16:41:29 +0300

    upver

--- pkgs/lib/openexr/ix.sh
+++ pkgs/lib/openexr/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/AcademySoftwareFoundation/openexr/archive/refs/tags/v3.1.9.tar.gz
-sha:103e902d3902800ab07b5f3a298be7afd2755312737b2cdbfa01326ff99dac07
+https://github.com/AcademySoftwareFoundation/openexr/archive/refs/tags/v3.1.10.tar.gz
+sha:6e0fd3f0eb1cb907bd3593830ea2b3431b85f22a6f18f99f8cfa099fec70067d
 {% endblock %}
 
 {% block lib_deps %}
