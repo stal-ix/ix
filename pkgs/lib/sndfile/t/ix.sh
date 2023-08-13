685bbb2f27b53a4417b9141e00a839a954c33c4d
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-08-13 18:49:49 +0300

    upver

--- pkgs/lib/sndfile/t/ix.sh
+++ pkgs/lib/sndfile/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/libsndfile/libsndfile/releases/download/1.2.0/libsndfile-1.2.0.tar.xz
-sha:0e30e7072f83dc84863e2e55f299175c7e04a5902ae79cfb99d4249ee8f6d60a
+https://github.com/libsndfile/libsndfile/releases/download/1.2.2/libsndfile-1.2.2.tar.xz
+sha:3799ca9924d3125038880367bf1468e53a1b7e3686a934f098b7e1d286cdb80e
 {% endblock %}
 
 {% block lib_deps %}
