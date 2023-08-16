8103ff8b239fd83d4ae6e8e6706c37e18d6d07ce
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-08-17 02:15:33 +0300

    upver

--- pkgs/bin/cmake/t/ix.sh
+++ pkgs/bin/cmake/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/Kitware/CMake/archive/refs/tags/v3.27.1.tar.gz
-sha:185993d71e548467a295e049066c4b5f3d76c5556bbea4b65c58d83340f83f39
+https://github.com/Kitware/CMake/archive/refs/tags/v3.27.3.tar.gz
+sha:0ec8faba825e3385e0b278184eaacdf45ae600275189447fcc11d98c4d30a68c
 {% endblock %}
 
 {% block bld_libs %}
