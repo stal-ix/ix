eee7300a6f217adae86254324eb16cb2eadbaf75
Anton Samokhvalov <pg@yandex-team.ru>
2023-08-22 17:41:45 +0300

    upver

--- pkgs/bin/dinit/ix.sh
+++ pkgs/bin/dinit/ix.sh
@@ -4,12 +4,12 @@
 https://github.com/davmac314/dinit
 {% endblock %}
 
-{% block git_commit %}
-306a2f37b9f153422ee2668b9abb1f5f8b4a961f
+{% block git_branch %}
+v0.17.0
 {% endblock %}
 
 {% block git_sha %}
-243a9161ee7b985b14773721796a822cddd845811b3982ce0e8141c645be765a
+bfe46a426d376965e14dd78189b24e1594880d7ba1c48fd008ba4985467f90a1
 {% endblock %}
 
 {% block bld_libs %}
