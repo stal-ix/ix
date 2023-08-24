9a83a584e5d5fca17bbfe9bbd667a6cbbc73db16
Anton Samokhvalov <pg@yandex-team.ru>
2023-08-24 19:53:19 +0300

    upver

--- pkgs/bin/wazero/ix.sh
+++ pkgs/bin/wazero/ix.sh
@@ -1,11 +1,11 @@
 {% extends '//die/go/pure.sh' %}
 
 {% block go_url %}
-https://github.com/tetratelabs/wazero/archive/refs/tags/v1.4.0.tar.gz
+https://github.com/tetratelabs/wazero/archive/refs/tags/v1.5.0.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-f8d94c72bd5facb8df625c2594301158492f8124fdfa9dc0d6309019a6aea32a
+0baec9dbd2046f2ec147be10c6a571fb905721f1f9c64c7d5bd6f7a58ede2760
 {% endblock %}
 
 {% block unpack %}
