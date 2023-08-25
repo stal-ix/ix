210e6396cbebb3750aa549c511b63205e1e1a971
Anton Samokhvalov <pg@yandex-team.ru>
2023-08-26 00:55:54 +0300

    refac

---
+++ pkgs/lib/tcmalloc/12/ix.sh
@@ -0,0 +1,6 @@
+{% extends '//lib/tcmalloc/11/ix.sh' %}
+
+{% block fetch %}
+https://github.com/gperftools/gperftools/archive/refs/tags/gperftools-2.12.tar.gz
+sha:1cc42af8c0ec117695ecfa49ef518d9eaf7b215a2657b51f655daa2dc07101ce
+{% endblock %}
