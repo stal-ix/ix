e396935c29dd16463d3594751d63d9934f2b9774
Anton Samokhvalov <pg@yandex-team.ru>
2023-08-26 15:58:51 +0300

    upver

--- pkgs/lib/freetype/t/ix.sh
+++ pkgs/lib/freetype/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://gitlab.freedesktop.org/freetype/freetype/-/archive/VER-2-13-1/freetype-VER-2-13-1.tar.bz2
-sha:f3e648ca3948824c7e89a60c8ed6945135821d43dea2ef06681675bd14203358
+https://gitlab.freedesktop.org/freetype/freetype/-/archive/VER-2-13-2/freetype-VER-2-13-2.tar.bz2
+sha:175f39fd796410d136451ce9637f26d359b44258c8a2005682bacb326db80478
 {% endblock %}
 
 {% block lib_deps %}
