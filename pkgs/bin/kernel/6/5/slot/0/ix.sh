2e2b76eae393b46b2e87830bd99a6af2cfb685fb
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-08-29 01:37:35 +0300

    add

---
+++ pkgs/bin/kernel/6/5/slot/0/ix.sh
@@ -0,0 +1,3 @@
+{% extends '//bin/kernel/6/5/slot/1/ix.sh' %}
+
+{% block slot %}0{% endblock %}
---
+++ pkgs/bin/kernel/6/5/t/ver.sh
@@ -0,0 +1,2 @@
+https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.4.12.tar.xz
+sha:cca91be956fe081f8f6da72034cded96fe35a50be4bfb7e103e354aa2159a674
