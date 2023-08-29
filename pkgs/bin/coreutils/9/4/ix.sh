5f7bcb9a7a378e5713431d96fdf127e7c73e39d1
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-08-29 21:21:24 +0300

    upver

---
+++ pkgs/bin/coreutils/9/4/ix.sh
@@ -0,0 +1,10 @@
+{% extends '//bin/coreutils/9/1/ix.sh' %}
+
+{% block openssl_ver %}
+lib/openssl/3
+{% endblock %}
+
+{% block fetch %}
+https://ftp.gnu.org/gnu/coreutils/coreutils-9.4.tar.gz
+sha:5f600d9093973b0afe25393d9bc18c44f2232657f4ca0d95ea31c702eb66b739
+{% endblock %}
