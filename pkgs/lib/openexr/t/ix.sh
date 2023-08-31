f06f69f698a796bc20c92d5130ea17d12723504c
Anton Samokhvalov <pg@yandex-team.ru>
2023-08-31 21:00:54 +0300

    upver

---
+++ pkgs/lib/openexr/t/ix.sh
@@ -0,0 +1,17 @@
+{% extends '//die/c/cmake.sh' %}
+
+{% block fetch %}
+https://github.com/AcademySoftwareFoundation/openexr/archive/refs/tags/v3.2.0.tar.gz
+sha:b1b200606640547fceff0d3ebe01ac05c4a7ae2a131be7e9b3e5b9f491ef35b3
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+lib/z
+lib/c++
+lib/imath
+{% endblock %}
+
+{% block cmake_flags %}
+OPENEXR_INSTALL_EXAMPLES=OFF
+{% endblock %}
