4689c4db1b7eed2fd31c1a1eaa0aa0685d439305
Anton Samokhvalov <pg@yandex-team.ru>
2023-09-01 03:23:59 +0300

    upver

--- pkgs/aux/ca-bundle/ix.sh
+++ pkgs/aux/ca-bundle/ix.sh
@@ -1,5 +1,5 @@
 {% extends 'nopredict/ix.sh' %}
 
 {% block predict_outputs %}
-[{"path": "share/ssl/cert.pem", "sum": "00e4920ab22c10d00674b521ef543910815d6f1d256a855283669b890dfa373f"}]
+[{"path": "share/ssl/cert.pem", "sum": "fa59b36b3ae08c2c2bde39631559d947a912caeb57c7ac89fc26264b1034c431"}]
 {% endblock %}
