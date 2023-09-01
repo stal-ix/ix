7a476bc0e25ffd570e75ee41c3c8f59f3d0c78ea
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-09-01 15:04:22 +0300

    fix lib/k/imageformats build

--- pkgs/lib/openexr/ix.sh
+++ pkgs/lib/openexr/ix.sh
@@ -4,3 +4,8 @@
 {{super()}}
 OPENEXR_INSTALL_TOOLS=OFF
 {% endblock %}
+
+{% block install %}
+{{super()}}
+sed -e 's|;PkgConfig::deflate||' -i ${out}/lib/cmake/OpenEXR/OpenEXRTargets.cmake
+{% endblock %}
