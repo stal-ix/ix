5729e52fe43ff13adf96016245140924c8fb27cb
Anton Samokhvalov <pg@yandex-team.ru>
2023-09-05 01:30:48 +0300

    upver

--- pkgs/lib/tom/math/ix.sh
+++ pkgs/lib/tom/math/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/make.sh' %}
 
 {% block fetch %}
-https://github.com/libtom/libtommath/releases/download/v1.2.0/ltm-1.2.0.tar.xz
-sha:b7c75eecf680219484055fcedd686064409254ae44bc31a96c5032843c0e18b1
+https://github.com/libtom/libtommath/releases/download/v1.2.1/ltm-1.2.1.tar.xz
+sha:986025d7b374276fee2e30e99f3649e4ac0db8a02257a37ee10eae72abed0d1f
 {% endblock %}
 
 {% block lib_deps %}
