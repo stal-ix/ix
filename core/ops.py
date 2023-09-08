9df16b8b24856b3799638a11dd5f70d558a6bfb6
Anton Samokhvalov <pg@yandex-team.ru>
2023-09-09 00:45:51 +0300

    ability to dump graph

--- core/ops.py
+++ core/ops.py
@@ -1,8 +1,9 @@
 import os
+import json
 import getpass
 
 
-def construct(cfg, kind=None):
+def construct_impl(cfg, kind):
     if not kind:
         if getpass.getuser() == 'ix':
             kind = 'local'
@@ -25,3 +26,23 @@ def construct(cfg, kind=None):
     import core.ops_sys as o
 
     return o.Ops(cfg)
+
+
+class Dump:
+    def __init__(self, slave):
+        self.slave = slave
+
+    def execute_graph(self, graph):
+        print(json.dumps(graph, indent=4, sort_keys=True))
+
+    def __getattr__(self, name):
+        return getattr(self.slave, name)
+
+
+def construct(cfg, kind=None):
+    res = construct_impl(cfg, kind)
+
+    if os.environ.get('IX_DUMP_GRAPH', None):
+        res = Dump(res)
+
+    return res
