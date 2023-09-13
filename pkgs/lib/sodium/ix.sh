9b9d145e0ed7d2b57a4c306010913ae37b1bc54d
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-09-13 16:04:48 +0300

    upver

--- pkgs/lib/sodium/ix.sh
+++ pkgs/lib/sodium/ix.sh
@@ -1,10 +1,18 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://github.com/jedisct1/libsodium/archive/refs/tags/1.0.18.tar.gz
-sha:d59323c6b712a1519a5daf710b68f5e7fde57040845ffec53850911f10a5d4f4
+https://github.com/jedisct1/libsodium/archive/refs/tags/1.0.19.tar.gz
+sha:ac627795b45972fdfafd0ac6214896ced6b3135ebe5dea8b3cc04ceb44da3d4c
 {% endblock %}
 
 {% block lib_deps %}
 lib/c
+{% endblock %}
+
+{% block bld_tool %}
+bin/getopt
+{% endblock %}
+
+{% block patch %}
+rm autogen.sh
 {% endblock %}
