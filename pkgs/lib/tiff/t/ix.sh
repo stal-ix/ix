1ef0447d9e5516f46e218da7f41ad1e6d4c08487
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-09-13 22:55:00 +0300

    upver

--- pkgs/lib/tiff/t/ix.sh
+++ pkgs/lib/tiff/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-http://download.osgeo.org/libtiff/tiff-4.5.1.tar.gz
-sha:d7f38b6788e4a8f5da7940c5ac9424f494d8a79eba53d555f4a507167dca5e2b
+http://download.osgeo.org/libtiff/tiff-4.6.0.tar.gz
+sha:88b3979e6d5c7e32b50d7ec72fb15af724f6ab2cbf7e10880c360a77e4b5d99a
 {% endblock %}
 
 {% block lib_deps %}
