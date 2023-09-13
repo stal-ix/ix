4902364eff2ba86fb54c86a4ca67694cce0e48b8
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-09-13 20:21:49 +0300

    fix build

--- pkgs/lib/sodium/ix.sh
+++ pkgs/lib/sodium/ix.sh
@@ -16,3 +16,21 @@ bin/getopt
 {% block patch %}
 rm autogen.sh
 {% endblock %}
+
+{% block c_rename_symbol %}
+blake2b
+blake2b_compress_avx2
+blake2b_compress_ref
+blake2b_compress_sse41
+blake2b_compress_ssse3
+blake2b_final
+blake2b_init
+blake2b_init_key
+blake2b_init_key_salt_personal
+blake2b_init_param
+blake2b_init_salt_personal
+blake2b_long
+blake2b_pick_best_implementation
+blake2b_salt_personal
+blake2b_update
+{% endblock %}
