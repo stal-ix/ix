8b843d2b5ed61a041e0dc87cb7adadf3bef193fd
Anton Samokhvalov <pg@yandex-team.ru>
2023-09-15 01:40:15 +0300

    upver

--- pkgs/lib/webp/t/ix.sh
+++ pkgs/lib/webp/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.3.1.tar.gz
-sha:b3779627c2dfd31e3d8c4485962c2efe17785ef975e2be5c8c0c9e6cd3c4ef66
+https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.3.2.tar.gz
+sha:2a499607df669e40258e53d0ade8035ba4ec0175244869d1025d460562aa09b4
 {% endblock %}
 
 {% block lib_deps %}
