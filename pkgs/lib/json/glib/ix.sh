0e37b12f3de52430957fc0bc8f9a042459d35959
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-09-16 19:17:41 +0300

    upver

--- pkgs/lib/json/glib/ix.sh
+++ pkgs/lib/json/glib/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://gitlab.gnome.org/GNOME/json-glib/-/archive/1.6.6/json-glib-1.6.6.tar.bz2
-md5:cd56648d96dd79197c6d6af153d0e8ae
+https://gitlab.gnome.org/GNOME/json-glib/-/archive/1.8.0/json-glib-1.8.0.tar.bz2
+sha:01122bf2429461273add059d683341bf447857c7794439b2ee08ce37e0e6af9a
 {% endblock %}
 
 {% block lib_deps %}
