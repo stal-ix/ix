a0f2000309ca7387ef8c51fdb25805e7b19af6b4
Anton Samokhvalov <pg@yandex-team.ru>
2023-09-23 18:13:59 +0300

    fix

--- pkgs/bin/joe/ix.sh
+++ pkgs/bin/joe/ix.sh
@@ -1,7 +1,7 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://sourceforge.net/projects/joe-editor/files/JOE%20sources/joe-4.6/joe-4.6.tar.gz/download
+https://sourceforge.net/projects/joe-editor/files/JOE%20sources/joe-4.6/joe-4.6.tar.gz
 sha:495a0a61f26404070fe8a719d80406dc7f337623788e445b92a9f6de512ab9de
 {% endblock %}
 
