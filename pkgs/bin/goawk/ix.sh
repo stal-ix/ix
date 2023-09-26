a0c55e660523e1cc00c1d4f7d6747a8dda53ff97
Anton Samokhvalov <pg@yandex-team.ru>
2023-09-26 22:25:14 +0300

    upver

--- pkgs/bin/goawk/ix.sh
+++ pkgs/bin/goawk/ix.sh
@@ -1,11 +1,11 @@
 {% extends '//die/go/pure.sh' %}
 
 {% block go_url %}
-https://github.com/benhoyt/goawk/archive/refs/tags/v1.24.0.tar.gz
+https://github.com/benhoyt/goawk/archive/refs/tags/v1.25.0.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-a7475fbe074ae40d807e253419d182859f875513dea7726996f2707aecf9aa8c
+a2831b0f1114325eb37b7f1a4572c1c27dc3aad3c1964742cdf29afbe90c7b2f
 {% endblock %}
 
 {% block install %}
