12333b3713982842007d43cc1e4e005bf93466f4
Anton Samokhvalov <pg@yandex-team.ru>
2023-09-26 20:56:49 +0300

    upver

--- pkgs/lib/gumbo/ix.sh
+++ pkgs/lib/gumbo/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://codeberg.org/grisha/gumbo-parser/archive/0.12.0.tar.gz
-sha:f4820b8028b07406ba24ac74ef77057f5d9871f1b17c5486ad78e3da6e541e51
+https://codeberg.org/grisha/gumbo-parser/archive/0.12.1.tar.gz
+sha:c0bb5354e46539680724d638dbea07296b797229a7e965b13305c930ddc10d82
 {% endblock %}
 
 {% block lib_deps %}
