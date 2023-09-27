9dca0980b7c31c31ad4125334f48b4f653f50428
Anton Samokhvalov <pg@yandex-team.ru>
2023-09-27 04:36:57 +0300

    better

--- pkgs/bld/gir/ix.sh
+++ pkgs/bld/gir/ix.sh
@@ -1,29 +1,6 @@
-{% extends '//lib/gi/repository/t/ix.sh' %}
+{% extends '//die/hub.sh' %}
 
-{% block bld_libs %}
-{{super()}}
-lib/glib/dl
-lib/gi/repository/bootstrap/dl
-lib/gi/repository/bootstrap/shim
-{% endblock %}
-
-{% block bld_tool %}
-{{super()}}
+{% block run_deps %}
+bld/gir/unwrap
 bld/gir/scanner
-{% endblock %}
-
-{% block meson_flags %}
-{{super()}}
-gi_cross_use_prebuilt_gi=true
-{% endblock %}
-
-{% block setup %}
-{{super()}}
-export OPTFLAGS="${OPTFLAGS} -O0"
-export GI_SCANNER_DISABLE_CACHE=1
-{% endblock %}
-
-{% block postinstall %}
-rm -rf ${out}/include
-rm ${out}/bin/g-ir-scanner
 {% endblock %}
