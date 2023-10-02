395421df06ac75c08720fef45cf47eeca28fa288
Anton Samokhvalov <pg@yandex-team.ru>
2023-10-02 23:47:54 +0300

    upver

--- pkgs/bin/ttyd/ix.sh
+++ pkgs/bin/ttyd/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/tsl0922/ttyd/archive/refs/tags/1.7.3.tar.gz
-sha:c9cf5eece52d27c5d728000f11315d36cb400c6948d1964a34a7eae74b454099
+https://github.com/tsl0922/ttyd/archive/refs/tags/1.7.4.tar.gz
+sha:300d8cef4b0b32b0ec30d7bf4d3721a5d180e22607f9467a95ab7b6d9652ca9b
 {% endblock %}
 
 {% block bld_libs %}
