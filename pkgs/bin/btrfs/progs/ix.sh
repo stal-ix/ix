6a66bcf24a0db6a1edf6df4bf73782b1307d736c
Anton Samokhvalov <pg@yandex-team.ru>
2023-10-04 14:17:38 +0300

    upver

--- pkgs/bin/btrfs/progs/ix.sh
+++ pkgs/bin/btrfs/progs/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://www.kernel.org/pub/linux/kernel/people/kdave/btrfs-progs/btrfs-progs-v6.5.1.tar.xz
-sha:dacbb28136e82586af802205263a428c3d1941778bc3fdc9b1b386ea12eb904e
+https://www.kernel.org/pub/linux/kernel/people/kdave/btrfs-progs/btrfs-progs-v6.5.2.tar.xz
+sha:c558b2ddd43f5747a2f5cb62aed3e5c5099703886485a480310fed4698d3610c
 {% endblock %}
 
 {% block bld_libs %}
