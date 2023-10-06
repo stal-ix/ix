7a6eca47186e0233436ac46064e2e9b4e4a8621a
Anton Samokhvalov <pg@yandex-team.ru>
2023-10-06 22:21:51 +0300

    upver

--- pkgs/lib/mbedtls/3/t/ix.sh
+++ pkgs/lib/mbedtls/3/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//lib/mbedtls/t/ix.sh' %}
 
 {% block fetch %}
-https://github.com/ARMmbed/mbedtls/archive/refs/tags/v3.4.1.tar.gz
-sha:a420fcf7103e54e775c383e3751729b8fb2dcd087f6165befd13f28315f754f5
+https://github.com/ARMmbed/mbedtls/archive/refs/tags/v3.5.0.tar.gz
+sha:bdee0e3e45bbf360541306cac0cc27e00402c7a46b9bdf2d24787d5107f008f2
 {% endblock %}
 
 {% block lib_deps %}
