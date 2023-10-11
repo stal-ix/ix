07e006ea6b39e1c1691fdcdef59b0eb3a3de39ed
Anton Samokhvalov <pg@yandex-team.ru>
2023-10-12 02:35:53 +0300

    fix

--- pkgs/lib/cpuid/ix.sh
+++ pkgs/lib/cpuid/ix.sh
@@ -2,7 +2,7 @@
 
 {% block fetch %}
 https://github.com/anrieff/libcpuid/archive/refs/tags/v0.6.4.tar.gz
-sha:1cbb1a79bfe6c37884a538b56504fa0975e78e492aee7c265a42f654c6056cb3
+sha:5f0114ad4bf01dc3ef4c1fea6fe1839546f592e6517d65ea8be9a8c2a925fd14
 {% endblock %}
 
 {% block lib_deps %}
