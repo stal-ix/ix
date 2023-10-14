080ace9ddea3a7de7b0516197f73c4e5b8432cda
Anton Samokhvalov <pg@yandex-team.ru>
2023-10-15 00:44:44 +0300

    upver

--- pkgs/bin/fzf/ix.sh
+++ pkgs/bin/fzf/ix.sh
@@ -1,11 +1,11 @@
 {% extends '//die/go/pure.sh' %}
 
 {% block go_url %}
-https://github.com/junegunn/fzf/archive/refs/tags/0.42.0.tar.gz
+https://github.com/junegunn/fzf/archive/refs/tags/0.43.0.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-a8975e3e46cbbff37d9306cb374c5a7593d2348bc74ad031f382ad557c6756c6
+417014bf5740c6bb4a2ad6b461d1ddaaafcbcd7943ce5d64e4a567d4840405c0
 {% endblock %}
 
 {% block install %}
