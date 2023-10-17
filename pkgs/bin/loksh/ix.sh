500a237eb992c4be60901574fb9579b211bf8666
Anton Samokhvalov <pg@yandex-team.ru>
2023-10-17 03:13:18 +0300

    upver

--- pkgs/bin/loksh/ix.sh
+++ pkgs/bin/loksh/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://github.com/dimkr/loksh/releases/download/7.3/loksh-7.3.tar.xz
-sha:e60bee39836432024569f44b499581ef8ebedbd8fe5b6172245e9a1af4304e9b
+https://github.com/dimkr/loksh/releases/download/7.4/loksh-7.4.tar.xz
+sha:14636f794e939817f79653e00b2af7e73a199e585d52055455c15b9203caf299
 {% endblock %}
 
 {% block lib_deps %}
