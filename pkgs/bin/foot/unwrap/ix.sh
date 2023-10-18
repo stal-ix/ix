93d3759db1bcc66a639e385a2ee7c2c53fbc30e9
Anton Samokhvalov <pg@yandex-team.ru>
2023-10-18 03:53:35 +0300

    upver

--- pkgs/bin/foot/unwrap/ix.sh
+++ pkgs/bin/foot/unwrap/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://codeberg.org/dnkl/foot/archive/1.16.1.tar.gz
-sha:43df31d457ee1f30909d12875a18845e66277def32a25d9754c96995c58d179a
+https://codeberg.org/dnkl/foot/archive/1.16.2.tar.gz
+sha:0e02af376e5f4a96eeb90470b7ad2e79a1d660db2a7d1aa772be43c7db00e475
 {% endblock %}
 
 {% block bld_libs %}
