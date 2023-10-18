4cb8ccb8ea718d08c95699466134189498068fe9
Anton Samokhvalov <pg@yandex-team.ru>
2023-10-18 22:56:47 +0300

    upver

--- pkgs/bin/ryzen/adj/ix.sh
+++ pkgs/bin/ryzen/adj/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/FlyGoat/RyzenAdj/archive/refs/tags/v0.13.0.tar.gz
-sha:083d929031521c40308d62a0167b6d8e5035699233be2452ae694ac85d66cdcf
+https://github.com/FlyGoat/RyzenAdj/archive/refs/tags/v0.14.0.tar.gz
+sha:adb9f040b10fe0a7c1a4317b4c9b404f6db66631754300fc364003779312a0b1
 {% endblock %}
 
 {% block bld_libs %}
