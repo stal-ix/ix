40cad06937f1fdfa89855135c63f455d118b48f5
Anton Samokhvalov <pg@yandex-team.ru>
2023-10-21 00:43:44 +0300

    upver

--- pkgs/bin/meson/120/ix.sh
+++ pkgs/bin/meson/120/ix.sh
@@ -1,6 +1,6 @@
 {% extends '//bin/meson/100/ix.sh' %}
 
 {% block fetch %}
-https://github.com/mesonbuild/meson/archive/refs/tags/1.2.2.tar.gz
-sha:1caa0ef6082e311bdca9836e7907f548b8c3f041a42ed41f0ff916b83ac7dddd
+https://github.com/mesonbuild/meson/archive/refs/tags/1.2.3.tar.gz
+sha:1340bbc017eead2ddf706fb36a6dcc46796deec6b1513005de67e862c1b737cf
 {% endblock %}
