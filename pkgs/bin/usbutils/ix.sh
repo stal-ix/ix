d29f2bebfd8b149482bb60a2fdb66c6b2c156a25
Anton Samokhvalov <pg@yandex-team.ru>
2023-10-23 03:30:33 +0300

    upver

--- pkgs/bin/usbutils/ix.sh
+++ pkgs/bin/usbutils/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://www.kernel.org/pub/linux/utils/usb/usbutils/usbutils-014.tar.xz
-sha:3a079cfad60560227b67192482d7813bf96326fcbb66c04254839715f276fc69
+https://www.kernel.org/pub/linux/utils/usb/usbutils/usbutils-016.tar.xz
+sha:75050b3a113a3d07b9e822fb367b8c7ef3524027d33aea7c9febfab038511e43
 {% endblock %}
 
 {% block bld_libs %}
