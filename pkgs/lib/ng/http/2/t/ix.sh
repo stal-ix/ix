e7169b2e239a487930dce6a3491f330a1ee1afcc
Anton Samokhvalov <pg@yandex-team.ru>
2023-10-27 18:55:58 +0300

    upver

--- pkgs/lib/ng/http/2/t/ix.sh
+++ pkgs/lib/ng/http/2/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://github.com/nghttp2/nghttp2/archive/refs/tags/v1.57.0.tar.gz
-sha:3c0b4e023dddf2afa087aa4409f7dbe03c099b4c63655e7545a607035085848a
+https://github.com/nghttp2/nghttp2/archive/refs/tags/v1.58.0.tar.gz
+sha:7da19947b33a07ddcf97b9791331bfee8a8545e6b394275a9971f43cae9d636b
 {% endblock %}
 
 {% block lib_deps %}
