feb7587a9d88cbb5e0e594fd090b7b3c5f582f20
Anton Samokhvalov <pg@yandex-team.ru>
2023-10-27 19:03:14 +0300

    upver

--- pkgs/bin/openvi/stock/ix.sh
+++ pkgs/bin/openvi/stock/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/make.sh' %}
 
 {% block fetch %}
-https://github.com/johnsonjh/OpenVi/archive/refs/tags/7.4.24.tar.gz
-sha:c79c87021c059fbd234578741f623f28aead5b3355edf0e677995d76b10b741b
+https://github.com/johnsonjh/OpenVi/archive/refs/tags/7.4.25.tar.gz
+sha:8a5a870cf6c581dfee519c3631475589620ed7b7cca2c6e015ae1a78094a8292
 {% endblock %}
 
 {% block bld_libs %}
