0e0f31a2f75611d288517e8924cb1e7b0615b212
Anton Samokhvalov <pg@yandex-team.ru>
2023-10-27 17:35:52 +0300

    upver

--- pkgs/bin/etcd/ix.sh
+++ pkgs/bin/etcd/ix.sh
@@ -1,18 +1,23 @@
 {% extends '//die/go/pure.sh' %}
 
 {% block go_url %}
-https://github.com/etcd-io/etcd/archive/refs/tags/v3.4.22.tar.gz
+https://github.com/etcd-io/etcd/archive/refs/tags/v3.5.10.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-f41af4fe4507b1f36a3d898f4f687f91d129e32f9412c876b87ff04c77a611cc
+1cb0b0b969101e127d1f6dd8be66a6a14640da16a4b0f532bd9bf902a89bd20b
 {% endblock %}
 
 {% block bld_libs %}
 lib/c
 {% endblock %}
 
+{% block unpack %}
+{{super()}}
+cd server
+{% endblock %}
+
 {% block install %}
 mkdir ${out}/bin
-cp etcd ${out}/bin/
+cp server ${out}/bin/etcd
 {% endblock %}
