fe799c55bb92de48f9febac7cd35f8f0e63ec1a7
Anton Samokhvalov <pg@yandex-team.ru>
2023-10-30 22:06:41 +0300

    better

--- pkgs/bin/nwg/look/ix.sh
+++ pkgs/bin/nwg/look/ix.sh
@@ -8,7 +8,19 @@ https://github.com/nwg-piotr/nwg-look/archive/refs/tags/v0.2.4.tar.gz
 2285db7f26968b712b26a2a8daf6ee07c81cf99f75c830509dd9808f3458f6cb
 {% endblock %}
 
+{% block bld_tool %}
+{{super()}}
+bld/make
+{% endblock %}
+
+{% block patch %}
+{{super()}}
+sed -e 's|/usr/|/|' -i Makefile
+sed -e 's|"/usr/share|"/'${out}'/share|' -i tools.go
+{% endblock %}
+
 {% block install %}
-mkdir ${out}/bin
-cp nwg-look ${out}/bin/
+mkdir bin
+cp nwg-look bin/
+make DESTDIR=${out} install
 {% endblock %}
