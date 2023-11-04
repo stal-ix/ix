f43128280d052a6637870fd190a0cd944ae887f0
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-11-05 02:26:38 +0300

    better

--- pkgs/lib/cffi/ix.sh
+++ pkgs/lib/cffi/ix.sh
@@ -1,42 +1,6 @@
-{% extends '//die/c/ix.sh' %}
-
-{% block fetch %}
-https://github.com/python-cffi/cffi/archive/refs/tags/v1.16.0.tar.gz
-sha:83414dab92270767261176d155908f18da846a4234dc195620f85a431d19e9d8
-{% endblock %}
+{% extends '//die/hub.sh' %}
 
 {% block lib_deps %}
-lib/c
-lib/ffi
-lib/python
-{% endblock %}
-
-{% block bld_libs %}
-pip/setuptools
-{% endblock %}
-
-{% block bld_tool %}
-bld/python/{{python_ver}}
-{% endblock %}
-
-{% block build %}
-${NATIVE_PYTHON} setup.py build
-{% endblock %}
-
-{% block install %}
-${NATIVE_PYTHON} setup.py install \
-    --prefix=${out} \
-    --install-lib=${out}
-cd ${out}
-mv cffi* lib
-cd lib
-mv _cffi_backend.*.so lib_cffi_backend.a
-{% endblock %}
-
-{% block patch %}
-sed -e 's|install_requires.*pycparser.*|pass|' -i setup.py
-{% endblock %}
-
-{% block build_flags %}
-wrap_cc
+lib/cffi/bare
+pip/pycparser
 {% endblock %}
