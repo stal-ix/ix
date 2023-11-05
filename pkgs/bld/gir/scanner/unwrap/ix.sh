78151065a1885463bf4566ff84ff8a92d05a915d
Anton Samokhvalov <pg@yandex-team.ru>
2023-11-06 01:15:13 +0300

    better

--- pkgs/bld/gir/scanner/unwrap/ix.sh
+++ pkgs/bld/gir/scanner/unwrap/ix.sh
@@ -1,9 +1,5 @@
 {% extends '//die/python/bin.sh' %}
 
-{% block step_unpack %}
-:
-{% endblock %}
-
 {% block bld_libs %}
 lib/gi/repository/py/register
 {{super()}}
@@ -11,12 +7,12 @@ lib/gi/repository/py/register
 
 {% block entry_point %}g-ir-scanner-bin{% endblock %}
 
-{% block build %}
+{% block step_unpack %}
+export PY_ENTRY_FILE=$(source_env "${IX_T_DIR}"; echo ${PY_ENTRY_FILE})
 cat - ${PY_ENTRY_FILE} << EOF > g-ir-scanner-bin
 __file__ = "${out}/bin/g-ir-scanner-bin"
 import sys
 import _giscanner
 sys.modules['giscanner._giscanner'] = _giscanner
 EOF
-{{super()}}
 {% endblock %}
