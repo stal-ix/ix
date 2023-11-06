6ee445df223ad551681f112b932ae5b969f3661b
Anton Samokhvalov <pg@yandex-team.ru>
2023-11-06 23:02:53 +0300

    add

--- pkgs/lib/py/wlroots/py/ix.sh
+++ pkgs/lib/py/wlroots/py/ix.sh
@@ -11,7 +11,10 @@ cd ${out}
 mv pywl* lib
 cd lib
 rm wlroots/_ffi.py
->wlroots/xwayland.py
+cat << EOF > wlroots/xwayland.py
+class Surface:
+    pass
+EOF
 cp -R EGG-INFO pywlroots-0.16.6-py3.12.egg-info
 py_exports > exports
 cat exports
