f617fd38cf70f5a42bcafd7c08b98da17096ab48
Anton Samokhvalov <pg@yandex-team.ru>
2023-11-06 04:07:42 +0300

    better

--- pkgs/bld/python/frozen/ix.sh
+++ pkgs/bld/python/frozen/ix.sh
@@ -6,10 +6,22 @@ lib/py/extra
 {{super()}}
 {% endblock %}
 
-{% block entry_point %}xxx{% endblock %}
+{% block entry_point %}python3{% endblock %}
 
 {% block step_unpack %}
-cat << EOF > xxx
+cat << EOF > python3
 import sys
+import code
+import runpy
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        for x in sys.argv[1:]:
+            runpy.run_path(x)
+    else:
+        if sys.stdin.isatty():
+            code.interact()
+        else:
+            exec(sys.stdin.read())
 EOF
 {% endblock %}
