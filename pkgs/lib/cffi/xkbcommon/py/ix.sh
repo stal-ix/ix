27871eb65baefed03dc5c86181676b8041a20ec9
Anton Samokhvalov <pg@yandex-team.ru>
2023-11-06 19:36:42 +0300

    better

--- pkgs/lib/cffi/xkbcommon/py/ix.sh
+++ pkgs/lib/cffi/xkbcommon/py/ix.sh
@@ -1,35 +1,22 @@
 {% extends '//lib/cffi/xkbcommon/t/ix.sh' %}
 
-{% block bld_libs %}
-lib/cffi/py
-pip/setuptools
-{% endblock %}
-
 {% block bld_tool %}
 bld/pip
 bin/unzip
-bld/python/{{python_ver}}
-{% endblock %}
-
-{% block build %}
-${NATIVE_PYTHON} setup.py build
+{{super()}}
 {% endblock %}
 
 {% block install %}
-${NATIVE_PYTHON} setup.py install \
-    --prefix=${out} \
-    --install-lib=${out}
+{{super()}}
 cd ${out}
 mkdir lib
 mv *.egg lib/
 cd lib
 unzip *.egg
+rm xkbcommon/_ffi.py
+rm *.egg
 py_exports > exports
 cat exports
-{% endblock %}
-
-{% block patch1 %}
-sed -e 's|setup_requires|setup_requires_xxx|' -e 's|install_requires|install_requires_xxx|' -i setup.cfg
 {% endblock %}
 
 {% block env %}
