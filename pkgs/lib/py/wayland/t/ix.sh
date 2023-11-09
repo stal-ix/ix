e191849d24172ef4bb4eeb9ded66e6cc5c57f03d
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-11-10 01:23:13 +0300

    better

--- pkgs/lib/py/wayland/t/ix.sh
+++ pkgs/lib/py/wayland/t/ix.sh
@@ -7,11 +7,11 @@ sha:e04befb267069ae317cd1a07f1d85b4bfc059090255e61b3efe49691a506aebd
 
 {% block lib_deps %}
 lib/c
-lib/cffi
 lib/wayland
 {% endblock %}
 
 {% block bld_libs %}
+lib/cffi
 pip/setuptools
 {% endblock %}
 
