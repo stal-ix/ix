ed62726dd8ad02c9f4db53e0fba0bfcd9a435767
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-11-12 19:29:50 +0300

    upver

--- pkgs/bin/hypr/land/stock/ver.sh
+++ pkgs/bin/hypr/land/stock/ver.sh
@@ -1,2 +1,2 @@
-https://github.com/hyprwm/Hyprland/releases/download/v0.32.0/source-v0.32.0.tar.gz
-sha:7a63d8d275eb9112fd35f545aa2f2c7251ada97e9e4d4252632a0dd8ea5b7318
+https://github.com/hyprwm/Hyprland/releases/download/v0.32.3/source-v0.32.3.tar.gz
+sha:285e8ebbd003978a2124e984db7677d044c21ca3cb72948eb71742cd8396df35
