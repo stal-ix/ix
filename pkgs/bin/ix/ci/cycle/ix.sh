2c6b8d15aa79461c8a8dcf1848dfe884ec206e7a
Anton Samokhvalov <pg@yandex-team.ru>
2023-11-13 18:25:16 +0300

    better

--- pkgs/bin/ix/ci/cycle/ix.sh
+++ pkgs/bin/ix/ci/cycle/ix.sh
@@ -6,6 +6,7 @@ cd ${out}; mkdir bin; cd bin
 cat << EOF > ci_cycle
 #!/usr/bin/env sh
 set -xue
+sleep 200
 mkdir -p {{wd}}
 cd {{wd}}
 (cd ix; git pull) || (rm -rf ix; git clone https://github.com/pg83/ix)
@@ -13,8 +14,7 @@ cd ix
 export IX_ROOT={{wd}}/ix_root
 export IX_EXEC_KIND=local
 mv ${IX_ROOT}/build/* ${IX_ROOT}/trash/ || true
-./ix build set/ci bld/all || true
-sleep 200
+./ix build set/ci bld/all
 EOF
 
 chmod +x *
