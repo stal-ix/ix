123f62e0e464e32af3efc284df3b3f831219c3ed
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-11-16 14:17:42 +0300

    upver

--- pkgs/lib/mimalloc/1/ix.sh
+++ pkgs/lib/mimalloc/1/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//lib/mimalloc/2/ix.sh' %}
 
 {% block fetch %}
-https://github.com/microsoft/mimalloc/archive/refs/tags/v1.7.6.tar.gz
-sha:d74f86ada2329016068bc5a243268f1f555edd620b6a7d6ce89295e7d6cf18da
+https://github.com/microsoft/mimalloc/archive/refs/tags/v1.8.2.tar.gz
+sha:4058d53d6ceb75862f32c30a6ee686c3cbb5e965b2c324b828ca454f7fe064f9
 {% endblock %}
 
 {% block bld_libs %}
