eaf5e3fd1ca2232043ce8018e920565c7a4a133d
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-11-19 02:19:40 +0300

    better

--- pkgs/bin/kernel/gengrub/fix.sh
+++ pkgs/bin/kernel/gengrub/fix.sh
@@ -1,14 +1,15 @@
 mkdir -p etc
 
 cat << EOF > etc/grub.cfg
+insmod all_video
+timeout=5
 probe --part-uuid \$root --set=rootuuid
 EOF
 
 ls bin/kernel-* | sort -r | while read l; do
     cat << EOF >> etc/grub.cfg
-insmod all_video
 menuentry "${l}" {
-    linux /${l} ro root=PARTUUID=\$rootuuid
+    linux /${l} ro root=PARTUUID=\$rootuuid quiet=1
     boot
 }
 EOF
