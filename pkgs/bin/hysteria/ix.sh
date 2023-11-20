6926b780d45300c6a529ab6095be7d45a988be48
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-11-20 20:08:52 +0300

    upver

--- pkgs/bin/hysteria/ix.sh
+++ pkgs/bin/hysteria/ix.sh
@@ -1,11 +1,11 @@
 {% extends '//die/go/pure.sh' %}
 
 {% block go_url %}
-https://github.com/apernet/hysteria/archive/refs/tags/app/v2.2.0.tar.gz
+https://github.com/apernet/hysteria/archive/refs/tags/app/v2.2.1.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-3294657b92e8c601255aeabd881ecbd58560e35a3b858a088fa8a37ddf78189e
+0c44205900e40eaa073bd8355765dd2abe1351d8aa08ff2cfa74b656490e1a02
 {% endblock %}
 
 {% block unpack %}
