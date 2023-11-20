6afbd42ed9b129d5e6d0799c407997333cbd947b
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-11-20 23:11:41 +0300

    fix build

--- pkgs/lib/gnutls/unwrap/ix.sh
+++ pkgs/lib/gnutls/unwrap/ix.sh
@@ -11,6 +11,20 @@
 export COFLAGS="--with-gnutls=${out} \${COFLAGS}"
 {% endblock %}
 
+{% block c_rename_symbol %}
+{{super()}}
+glthread_once_singlethreaded
+glthread_recursive_lock_destroy_multithreaded
+glthread_recursive_lock_init_multithreaded
+glthread_recursive_lock_lock_multithreaded
+glthread_recursive_lock_unlock_multithreaded
+glthread_rwlock_destroy_multithreaded
+glthread_rwlock_init_multithreaded
+glthread_rwlock_rdlock_multithreaded
+glthread_rwlock_unlock_multithreaded
+glthread_rwlock_wrlock_multithreaded
+{% endblock %}
+
 {% block patch %}
 {{super()}}
 sed -e 's|+= src/gl|+= |' -i Makefile.am
