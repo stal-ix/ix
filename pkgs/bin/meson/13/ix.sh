7faefcd3499ba76b779773e955b3d2dea216d5fe
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-11-20 20:12:33 +0300

    upver

---
+++ pkgs/bin/meson/13/ix.sh
@@ -0,0 +1,6 @@
+{% extends '//bin/meson/100/ix.sh' %}
+
+{% block fetch %}
+https://github.com/mesonbuild/meson/archive/refs/tags/1.3.0.tar.gz
+sha:6c4b4f35b51a260b362535fd32fee6d244871be38a2880f4de3ea9c6e69a0808
+{% endblock %}
