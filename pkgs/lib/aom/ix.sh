dcb0a5c07b85d73d410a1223af0a8181f937544e
Anton Samokhvalov <pg@yandex-team.ru>
2023-11-21 15:24:20 +0300

    upver

--- pkgs/lib/aom/ix.sh
+++ pkgs/lib/aom/ix.sh
@@ -1,57 +1,8 @@
-{% extends '//die/c/cmake.sh' %}
-
-{% block fetch %}
-https://storage.googleapis.com/aom-releases/libaom-3.7.0.tar.gz
-sha:d1773bb6f050b430af47741306ac37a6aa2fec7364cb0dc27e3b1454a138934f
-{% endblock %}
-
-{% block lib_deps %}
-lib/c
-{% endblock %}
-
-{% block bld_libs %}
-lib/c++
-{% endblock %}
-
-{% block bld_tool %}
-bld/perl
-{% if x86_64 %}
-bin/nasm
-{% endif %}
-{% if darwin %}
-bld/cctools
-{% endif %}
-{% endblock %}
+{% extends 't/ix.sh' %}
 
 {% block cmake_flags %}
-AOM_TARGET_CPU={{target.arch}}
-ENABLE_EXAMPLES=OFF
-ENABLE_WERROR=OFF
-ENABLE_TESTS=OFF
 ENABLE_TOOLS=OFF
-ENABLE_DOCS=OFF
-{% if x86_64 %}
-ENABLE_MMX=ON
-ENABLE_SSE=ON
-ENABLE_NASM=ON
-ENABLE_SSE2=ON
-ENABLE_SSE3=ON
-ENABLE_SSSE3=ON
-ENABLE_SSE4_1=ON
-ENABLE_SSE4_2=ON
-ARCH_X86_64=ON
-HAVE_MMX=1
-HAVE_SSE=1
-HAVE_SSE2=1
-HAVE_SSE3=1
-HAVE_SSE4_1=1
-HAVE_SSE4_2=1
-HAVE_SSSE3=1
-{% endif %}
-{% if aarch64 %}
-ENABLE_NEON=ON
-ARCH_ARM=ON
-{% endif %}
+{{super()}}
 {% endblock %}
 
 {% block install %}
---
+++ pkgs/lib/aom/t/ix.sh
@@ -0,0 +1,54 @@
+{% extends '//die/c/cmake.sh' %}
+
+{% block fetch %}
+https://storage.googleapis.com/aom-releases/libaom-3.7.1.tar.gz
+sha:97e0e4b52f9ef64c3e397257f95fd171a952cb72f1ae030750a162f7dea8f6a5
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+{% endblock %}
+
+{% block bld_libs %}
+lib/c++
+{% endblock %}
+
+{% block bld_tool %}
+bld/perl
+{% if x86_64 %}
+bin/nasm
+{% endif %}
+{% if darwin %}
+bld/cctools
+{% endif %}
+{% endblock %}
+
+{% block cmake_flags %}
+AOM_TARGET_CPU={{target.arch}}
+ENABLE_EXAMPLES=OFF
+ENABLE_WERROR=OFF
+ENABLE_TESTS=OFF
+ENABLE_DOCS=OFF
+{% if x86_64 %}
+ENABLE_MMX=ON
+ENABLE_SSE=ON
+ENABLE_NASM=ON
+ENABLE_SSE2=ON
+ENABLE_SSE3=ON
+ENABLE_SSSE3=ON
+ENABLE_SSE4_1=ON
+ENABLE_SSE4_2=ON
+ARCH_X86_64=ON
+HAVE_MMX=1
+HAVE_SSE=1
+HAVE_SSE2=1
+HAVE_SSE3=1
+HAVE_SSE4_1=1
+HAVE_SSE4_2=1
+HAVE_SSSE3=1
+{% endif %}
+{% if aarch64 %}
+ENABLE_NEON=ON
+ARCH_ARM=ON
+{% endif %}
+{% endblock %}
