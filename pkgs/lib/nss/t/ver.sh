57e7538a39b0f0fde0c1e9d0198d643de2624ae4
Anton Samokhvalov <pg@yandex-team.ru>
2023-11-21 17:34:42 +0300

    upver

--- pkgs/lib/nss/t/ver.sh
+++ pkgs/lib/nss/t/ver.sh
@@ -1,2 +1,2 @@
-https://ftp.mozilla.org/pub/security/nss/releases/NSS_3_93_RTM/src/nss-3.93.tar.gz
-sha:15f54bb72048eb105f8c0e936a04b899e74c3db9a19bbc1e00acee2af9476a8a
+https://ftp.mozilla.org/pub/security/nss/releases/NSS_3_94_RTM/src/nss-3.94.tar.gz
+sha:463ae180ee9e5ee9e3ad4f629326657e236780cc865572a930a16520abad9dd8
