249be70d5cb38a2439416c0a4be3e0c5e6d24ef4
Anton Samokhvalov <pg@yandex-team.ru>
2023-11-21 03:35:44 +0300

    upver

--- pkgs/lib/c/ares/t/ix.sh
+++ pkgs/lib/c/ares/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/c-ares/c-ares/archive/refs/tags/cares-1_22_0.tar.gz
-sha:fc2f388bad8d96870af775f0e80c9940abb0764fd69917830c52bd470037f052
+https://github.com/c-ares/c-ares/archive/refs/tags/cares-1_22_1.tar.gz
+sha:ffa650eedfe4ad52a3e8f162605489c68c133e5242b535832e8d6fa6a73f4a36
 {% endblock %}
 
 {% block lib_deps %}
