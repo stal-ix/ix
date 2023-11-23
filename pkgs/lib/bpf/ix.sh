ff5d889148e234a6a4756a1b30e33ad2bba7ba01
Anton Samokhvalov <pg@yandex-team.ru>
2023-11-23 15:12:40 +0300

    upver

--- pkgs/lib/bpf/ix.sh
+++ pkgs/lib/bpf/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/make.sh' %}
 
 {% block fetch %}
-https://github.com/libbpf/libbpf/archive/refs/tags/v1.2.2.tar.gz
-sha:32b0c41eabfbbe8e0c8aea784d7495387ff9171b5a338480a8fbaceb9da8d5e5
+https://github.com/libbpf/libbpf/archive/refs/tags/v1.3.0.tar.gz
+sha:11db86acd627e468bc48b7258c1130aba41a12c4d364f78e184fd2f5a913d861
 {% endblock %}
 
 {% block lib_deps %}
