ad021efa80fb10cf83fafb4cf76e4064dc44d175
Anton Samokhvalov <anton@samokhvalov.xyz>
2023-11-25 04:17:53 +0300

    revert

--- pkgs/lib/xml/2/t/ix.sh
+++ pkgs/lib/xml/2/t/ix.sh
@@ -1,8 +1,10 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://gitlab.gnome.org/GNOME/libxml2/-/archive/v2.12.1/libxml2-v2.12.1.tar.bz2
-sha:8c7e368b1830028f6f4f0e3d128d317f8c49a32e66f4b87ce59a017bdf6514af
+https://gitlab.gnome.org/GNOME/libxml2/-/archive/v2.11.4/libxml2-v2.11.4.tar.bz2
+sha:a9493ae091f58037dd5e73fc6035a4907eae58e2cc4756abc4e6253ee6036166
+#https://gitlab.gnome.org/GNOME/libxml2/-/archive/v2.12.1/libxml2-v2.12.1.tar.bz2
+#sha:8c7e368b1830028f6f4f0e3d128d317f8c49a32e66f4b87ce59a017bdf6514af
 {% endblock %}
 
 {% block lib_deps %}
