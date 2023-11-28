64b3fed58a832e558387258841f6540239937f23
Anton Samokhvalov <pg@yandex-team.ru>
2023-11-29 01:28:30 +0300

    upver

--- pkgs/bin/kernel/6/6/t/ver.sh
+++ pkgs/bin/kernel/6/6/t/ver.sh
@@ -1,2 +1,2 @@
-https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.6.2.tar.xz
-sha:73d4f6ad8dd6ac2a41ed52c2928898b7c3f2519ed5dbdb11920209a36999b77e
+https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.6.3.tar.xz
+sha:28edfc3d4f90cd738f2a20f5a2d68510268176d6111f6278d8f495edfd9495a7
