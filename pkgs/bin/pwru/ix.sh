669862d39b03926ac6f13f19ee372917d089d55d
Anton Samokhvalov <pg@yandex-team.ru>
2023-11-30 20:24:45 +0300

    better

--- pkgs/bin/pwru/ix.sh
+++ pkgs/bin/pwru/ix.sh
@@ -14,4 +14,23 @@ lib/pcap
 lib/llvm/17
 {% endblock %}
 
+{% block bld_tool %}
+bin/ld
+{% endblock %}
 
+{% block build %}
+set -xue
+export XDG_CONFIG_HOME=${tmp}
+export HOME=${tmp}
+export CC=clang-17
+export LD=ld
+export CGO_CPPFLAGS="${CPPFLAGS}"
+export CGO_CFLAGS="${CFLAGS}"
+export CGO_CXXFLAGS="${CXXFLAGS}"
+export CGO_LDFLAGS="${LDFLAGS}"
+export GOFLAGS="-buildmode=pie -mod=vendor -modcacherw"
+export TARGET_GOARCH=amd64
+go env -w "CC=clang-17"
+go generate
+{{super()}}
+{% endblock %}
