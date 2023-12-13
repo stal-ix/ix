55fd4469931aae198541ca5a72339bfcaa251155
Anton Samokhvalov <pg@yandex-team.ru>
2023-12-14 00:12:56 +0300

    better

--- pkgs/bin/kernel/6/6/t/ix.sh
+++ pkgs/bin/kernel/6/6/t/ix.sh
@@ -1,10 +1,6 @@
 {% extends '//bin/kernel/t/2/ix.sh' %}
 
-{% block kernel_version %}6-6-7{% endblock %}
-
-{% block fetch %}
 {% include 'ver.sh' %}
-{% endblock %}
 
 {% block kernel_headers %}
 bin/kernel/6/6/headers
--- pkgs/bin/kernel/6/6/t/ver.sh
+++ pkgs/bin/kernel/6/6/t/ver.sh
@@ -1,2 +1,6 @@
-https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.6.7.tar.xz
+{% block kernel_version %}6-6-7{% endblock %}
+
+{% block fetch %}
+https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-{{self.kernel_version().replace('-', '.')}}.tar.xz
 sha:0ce68ec6019019140043263520955ecd04839e55a1baab2fa9155b42bb6fd841
+{% endblock %}
