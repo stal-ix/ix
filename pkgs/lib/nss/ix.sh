71b730d8491d9473c49cf26a324f4fc54acc0820
Anton Samokhvalov <pg@yandex-team.ru>
2023-12-16 20:20:07 +0300

    refac

--- pkgs/lib/nss/ix.sh
+++ pkgs/lib/nss/ix.sh
@@ -1,23 +1,6 @@
-{% extends 't/ix.sh' %}
+{% extends '//die/hub.sh' %}
 
-{% block bld_tool %}
-{{super()}}
-bld/genpc
-{% endblock %}
-
-{% block install %}
-{{super()}}
-mkdir ${out}/include
-cp -R ../dist/public/* ${out}/include/
-cd ${out}/lib
-rm *.TOC *_static.a
-for x in *.so; do
-    mv ${x} $(echo ${x} | sed -e 's|3.so|.a|' | sed -e 's|.so|.a|')
-done
-genpc "nss" "3.66"
-{% endblock %}
-
-{% block env %}
-{{super()}}
-export NSS_HEADERS=${out}/include/nss
+{% block lib_deps %}
+lib/nss/unwrap
+lib/shim/fake/pkg(pkg_name=nss,pkg_ver=3.96)
 {% endblock %}
