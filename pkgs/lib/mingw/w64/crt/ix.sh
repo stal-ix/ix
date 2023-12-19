fc0fc2457e4f24caae432758c6ea3653d8e6380e
Anton Samokhvalov <pg@yandex-team.ru>
2023-12-19 18:56:21 +0300

    better

--- pkgs/lib/mingw/w64/crt/ix.sh
+++ pkgs/lib/mingw/w64/crt/ix.sh
@@ -30,5 +30,5 @@ for x in atexit strtold; do
     llvm-objcopy --redefine-sym ${x}=${x}_2 libmsvcr120_app.a
     llvm-objcopy --redefine-sym ${x}=${x}_3 libmsvcr120d.a
 done
-rm libmsvcr120d.a libvcruntime140_app.a
+rm libmsvcr120d.a libmsvcr90d.a libvcruntime140_app.a
 {% endblock %}
