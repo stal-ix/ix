4604cd61f80442b97f80e8bb81bdc1e138a04e12
Anton Samokhvalov <pg@yandex-team.ru>
2023-12-19 21:16:45 +0300

    fix build

--- pkgs/bin/sed/ix.sh
+++ pkgs/bin/sed/ix.sh
@@ -8,4 +8,7 @@ sha:6e226b732e1cd739464ad6862bd1a1aba42d7982922da7a53519631d24975181
 {% block bld_libs %}
 lib/c
 lib/intl
+{% if mingw32 %}
+lib/shim/dll(dll_name=bcrypt)
+{% endif %}
 {% endblock %}
