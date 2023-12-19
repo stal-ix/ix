45f9009b8bf761030caa480925d8562a0cfcb01f
Anton Samokhvalov <pg@yandex-team.ru>
2023-12-19 12:57:12 +0300

    upver

--- pkgs/bin/dhcpcd/ix.sh
+++ pkgs/bin/dhcpcd/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
-https://github.com/NetworkConfiguration/dhcpcd/archive/refs/tags/v10.0.5.tar.gz
-sha:046b060d72b158f813ea61acc1eff773dea4d9ad035a674ed87ecd95bd35cff7
+https://github.com/NetworkConfiguration/dhcpcd/archive/refs/tags/v10.0.6.tar.gz
+sha:f997a869437cc77304d5817064ecea4f488f5a0893fa642703807b5007727204
 {% endblock %}
 
 {% block bld_libs %}
