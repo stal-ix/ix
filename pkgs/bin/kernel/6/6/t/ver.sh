814c8e29bcf7d40fa596f8c0968199c624b019bf
Anton Samokhvalov <pg@yandex-team.ru>
2023-12-20 22:22:13 +0300

    upver

--- pkgs/bin/kernel/6/6/t/ver.sh
+++ pkgs/bin/kernel/6/6/t/ver.sh
@@ -1,6 +1,6 @@
-{% block kernel_version %}6-6-7{% endblock %}
+{% block kernel_version %}6-6-8{% endblock %}
 
 {% block fetch %}
 https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-{{self.kernel_version().replace('-', '.')}}.tar.xz
-sha:0ce68ec6019019140043263520955ecd04839e55a1baab2fa9155b42bb6fd841
+sha:5036c434e11e4b36d8da3f489851f7f829cf785fa7f7887468537a9ea4572416
 {% endblock %}
