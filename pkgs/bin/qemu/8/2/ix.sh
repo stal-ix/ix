6d5b7784a28653b27ec7c25e7bda16ffffc8e2e4
Anton Samokhvalov <pg@yandex-team.ru>
2023-12-20 23:09:39 +0300

    add

---
+++ pkgs/bin/qemu/8/2/ix.sh
@@ -0,0 +1,9 @@
+{% extends '//bin/qemu/8/1/ix.sh' %}
+
+{% block git_branch %}
+v8.2.0
+{% endblock %}
+
+{% block git_sha %}
+a1405565ef14f132bd85347e4a4e374650bdfd1014b32bd96bc0ce3cb1d12afc
+{% endblock %}
