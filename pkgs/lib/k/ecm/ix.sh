ad8648034f8b00fb138456cae5fe758ea381df53
Anton Samokhvalov <pg@yandex-team.ru>
2023-12-20 23:43:10 +0300

    better

--- pkgs/lib/k/ecm/ix.sh
+++ pkgs/lib/k/ecm/ix.sh
@@ -1,8 +1,10 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://download.kde.org/stable/frameworks/5.112/extra-cmake-modules-5.112.0.tar.xz
-sha:ac1084772280d57e5f31e2e2816ecbec4884169413f24f063660eb6f15d4c2e2
+https://github.com/KDE/extra-cmake-modules/archive/refs/tags/v5.247.0.tar.gz
+sha:998b4400cb4d55bfd7ffd8d09864645f469f67f476f48989f94aea604b2ddb7f
+#https://download.kde.org/stable/frameworks/5.113/extra-cmake-modules-5.113.0.tar.xz
+#sha:265e5440eebeca07351a469e617a4bf35748927bd907b00ace9c018392bb3bc4
 {% endblock %}
 
 {% block postinstall %}
