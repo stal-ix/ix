f8111af4798610b30fdc35ea1b11a5e120b220cd
Anton Samokhvalov <pg@yandex-team.ru>
2023-12-22 02:43:36 +0300

    upver

--- pkgs/lib/wlroots/17/ix.sh
+++ pkgs/lib/wlroots/17/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//lib/wlroots/16/ix.sh' %}
 
 {% block fetch %}
-https://gitlab.freedesktop.org/wlroots/wlroots/-/archive/0.17.0/wlroots-0.17.0.tar.bz2
-sha:0f0bb100b2277e6a0622bfe2594e4139b8462cb4c49ae883f042e8c4b1bdbc3e
+https://gitlab.freedesktop.org/wlroots/wlroots/-/archive/0.17.1/wlroots-0.17.1.tar.bz2
+sha:80fa4163ed585bce6fa0e9a35401f51da64231a3a5511118f1e4c1ac97d458f7
 {% endblock %}
 
 {% block lib_deps %}
