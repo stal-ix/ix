20464b29749fa9e8209b8a6a8216a56d525c6aad
Anton Samokhvalov <pg@yandex-team.ru>
2023-12-21 20:45:15 +0300

    upver

--- pkgs/lib/drm/t/ix.sh
+++ pkgs/lib/drm/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://dri.freedesktop.org/libdrm/libdrm-2.4.118.tar.xz
-sha:a777bd85f2b5fc9c57f886c82058300578317cafdbc77d0a769d7e9a9567ab88
+https://dri.freedesktop.org/libdrm/libdrm-2.4.119.tar.xz
+sha:0a49f12f09b5b6e68eaaaff3f02ca7cff9aa926939b212d343161d3e8ac56291
 {% endblock %}
 
 {% block lib_deps %}
