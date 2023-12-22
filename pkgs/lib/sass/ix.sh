e983e1286f245875151d494abbfb14d3e858cc21
Anton Samokhvalov <pg@yandex-team.ru>
2023-12-22 22:09:28 +0300

    upver

--- pkgs/lib/sass/ix.sh
+++ pkgs/lib/sass/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://github.com/sass/libsass/archive/refs/tags/3.6.5.tar.gz
-md5:c49765b9b3824dcd4a7423225ca28bad
+https://github.com/sass/libsass/archive/refs/tags/3.6.6.tar.gz
+sha:11f0bb3709a4f20285507419d7618f3877a425c0131ea8df40fe6196129df15d
 {% endblock %}
 
 {% block lib_deps %}
