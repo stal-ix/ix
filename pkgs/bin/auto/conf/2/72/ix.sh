e3006e6b865252c3547f59cbcaaf44f34cbe8c0e
Anton Samokhvalov <pg@yandex-team.ru>
2023-12-23 16:49:45 +0300

    upver

---
+++ pkgs/bin/auto/conf/2/72/ix.sh
@@ -0,0 +1,6 @@
+{% extends '//bin/auto/conf/2/71/ix.sh' %}
+
+{% block fetch %}
+https://ftp.gnu.org/gnu/autoconf/autoconf-2.72.tar.xz
+sha:ba885c1319578d6c94d46e9b0dceb4014caafe2490e437a0dbca3f270a223f5a
+{% endblock %}
