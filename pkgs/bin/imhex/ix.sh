ce5ffa12fafb5dbf79598d6b11938ce563fd5025
Anton Samokhvalov <pg@yandex-team.ru>
2023-12-29 19:45:30 +0300

    upver

--- pkgs/bin/imhex/ix.sh
+++ pkgs/bin/imhex/ix.sh
@@ -1,25 +1,74 @@
-{% extends 'stock/ix.sh' %}
+{% extends '//die/c/cmake.sh' %}
 
-{# TODO(pg): need portal #}
+{% block git_repo %}
+https://github.com/WerWolv/ImHex
+{% endblock %}
+
+{% block git_branch %}
+v1.32.1
+{% endblock %}
+
+{% block git_sha %}
+15dccbe93875394c79e286f87ef3d77f4cd2912cdd98902b45e85a73e43e8bdc
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/c++
+lib/fmt
+lib/dbus
+lib/curl
+lib/yara
+lib/glfw
+lib/opengl
+lib/archive
+lib/freetype
+lib/mbedtls/3
+lib/cap/stone
+lib/glfw/deps
+lib/python/3/10
+lib/json/nlohmann
+lib/shim/fake(lib_name=glfw)
+{% endblock %}
+
+{% block bld_tool %}
+bld/python
+bld/dlfcn
+{% endblock %}
+
+{% block patch %}
+find . -type f -name CMakeLists.txt | while read l; do
+    sed -e 's|find_package.*OpenGL.*||' \
+        -e 's|OpenGL::GL||g' \
+        -i ${l}
+done
+
+sed -e 's|.*downloadImHexPatternsFiles(.*||' -i cmake/build_helpers.cmake
+
+find . -type f | while read l; do
+    sed -e 's|std::abs(index)|(index > 0 ? index : -index)|g' \
+        -e 's|boyer_moore_horspool_searcher|default_searcher|g' \
+        -e 's|tellg() + a_length|tellg() + (std::streamoff)a_length|g' \
+        -i "${l}"
+done
+{% endblock %}
+
+{% block cmake_flags %}
+IMHEX_STATIC_LINK_PLUGINS=ON
+IMHEX_OFFLINE_BUILD=ON
+USE_SYSTEM_FMT=ON
+USE_SYSTEM_CURL=ON
+USE_SYSTEM_YARA=ON
+USE_SYSTEM_CAPSTONE=ON
+USE_SYSTEM_NLOHMANN_JSON=ON
+{% endblock %}
+
+{% block build_flags %}
+shut_up
+wrap_cc
+{% endblock %}
 
 {% block build %}
 {{super()}}
-
-dl_stubs << EOF >> stubs.c
-builtin initializePlugin     initializePlugin
-builtin getPluginName        getPluginName
-builtin getPluginAuthor      getPluginAuthor
-builtin getPluginDescription getPluginDescription
-builtin getCompatibleVersion getCompatibleVersion
-builtin setImGuiContext      setImGuiContext
-builtin isBuiltinPlugin      isBuiltinPlugin
-EOF
-
-cc -o imhex stubs.c $(find ${tmp} -type f -name '*.o' | grep -v 'libromfs.*main')
-{% endblock %}
-
-{% block install %}
-{{super()}}
-cp imhex ${out}/bin/
-mv ${out}/share/imhex/plugins ${out}/bin/
+>${tmp}/obj/plugins/builtin.hexplug
 {% endblock %}
