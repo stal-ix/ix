ab021102abed385a5d916ce1f9503cf88b5f71d4
Anton Samokhvalov <pg@yandex-team.ru>
2023-12-30 20:08:57 +0300

    upver

--- pkgs/bin/neovim/unwrap/ix.sh
+++ pkgs/bin/neovim/unwrap/ix.sh
@@ -1,10 +1,45 @@
-{% extends '//die/hub.sh' %}
+{% extends '//die/c/cmake.sh' %}
 
-{% block run_deps %}
-{% if linux %}
-# strange memory alloc failures with tcmalloc
-bin/neovim/unwrap/unwrap(purec=musl/unwrap,libc=musl/pure)
-{% else %}
-bin/neovim/unwrap/unwrap
-{% endif %}
+{% block fetch %}
+https://github.com/neovim/neovim/archive/refs/tags/v0.9.5.tar.gz
+sha:fe74369fc30a32ec7a086b1013acd0eacd674e7570eb1acc520a66180c9e9719
+{% endblock %}
+
+{% block bld_libs %}
+lib/uv
+lib/lua
+lib/uv/l
+lib/intl
+lib/vterm
+lib/termkey
+lib/unibilium
+lib/msgpack/c
+lib/tree/sitter
+{% endblock %}
+
+{% block bld_tool %}
+bin/gperf
+bld/gettext
+bin/neovim/puc(lua_ver=puc/5/2)
+{% endblock %}
+
+{% block script_init_env %}
+export LUA_PATH=
+{{super()}}
+{% endblock %}
+
+{% block setup_tools %}
+L=$(which lua)
+
+cat << EOF > luajit
+#!$(which sh)
+export LUA_PATH="${LUA_PATH}:\${LUA_PATH}"
+exec "${L}" "\$@"
+EOF
+
+chmod +x luajit
+{% endblock %}
+
+{% block build_flags %}
+shut_up
 {% endblock %}
