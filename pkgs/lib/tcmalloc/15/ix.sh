e4ee3b6d6bef022d7a1bb355fe18e80a24f248df
Anton Samokhvalov <pg@yandex-team.ru>
2024-01-06 22:45:36 +0300

    add

---
+++ pkgs/lib/tcmalloc/15/ix.sh
@@ -0,0 +1,6 @@
+{% extends '//lib/tcmalloc/11/ix.sh' %}
+
+{% block fetch %}
+https://github.com/gperftools/gperftools/archive/refs/tags/gperftools-2.15.tar.gz
+sha:3918ff2e21bb3dbb5a801e1daf55fb20421906f7c42fbb482bede7bdc15dfd2e
+{% endblock %}
