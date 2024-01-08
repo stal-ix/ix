c80ac6b685f001487105ad184c67b0ce29731d18
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-01-08 06:12:36 +0300

    better

--- pkgs/bin/kernel/6/7/ver.sh
+++
@@ -1,6 +0,0 @@
-{% block kernel_version %}6-7{% endblock %}
-
-{% block fetch %}
-https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-{{self.kernel_version().replace('-', '.')}}.tar.xz
-sha:
-{% endblock %}
