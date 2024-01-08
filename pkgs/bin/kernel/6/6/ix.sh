daaf0b9420f8618967647d57eb94dc0f0beed3ee
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-01-08 06:23:12 +0300

    refac

--- pkgs/bin/kernel/6/6/ix.sh
+++
@@ -1,5 +0,0 @@
-{% extends '//die/hub.sh' %}
-
-{% block run_deps %}
-bin/kernel/6/6/slot/0
-{% endblock %}
--- pkgs/bin/kernel/6/6/t/ver.sh
+++
@@ -1,6 +0,0 @@
-{% block kernel_version %}6-6-10{% endblock %}
-
-{% block fetch %}
-https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-{{self.kernel_version().replace('-', '.')}}.tar.xz
-sha:9ee627e4c109aec7fca3eda5898e81d201af2c7eb2f7d9d7d94c1f0e1205546c
-{% endblock %}
