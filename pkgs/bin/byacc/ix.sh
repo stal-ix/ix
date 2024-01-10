2c936c511f037ca09f0da25234575c38bd4e82db
Anton Samokhvalov <pg@yandex-team.ru>
2024-01-10 18:35:29 +0300

    upver

--- pkgs/bin/byacc/ix.sh
+++ pkgs/bin/byacc/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
-https://invisible-mirror.net/archives/byacc/byacc-20230521.tgz
-sha:5ad915a7d5833aa38a5e31bd077505666029c35e365dff8569fe4598eaa9fef2
+https://invisible-mirror.net/archives/byacc/byacc-20240109.tgz
+sha:f2897779017189f1a94757705ef6f6e15dc9208ef079eea7f28abec577e08446
 {% endblock %}
 
 {% block bld_libs %}
