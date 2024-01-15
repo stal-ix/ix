411bae098c2cc6de3091b75cefd8229053ad4053
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-01-15 16:35:59 +0300

    better

--- pkgs/bin/epiphany/ix.sh
+++ pkgs/bin/epiphany/ix.sh
@@ -1,7 +1,7 @@
 {% extends '//die/hub.sh' %}
 
 {% block run_deps %}
-bin/epiphany/unwrap(gtk_ver=4)
 org/freedesktop/secrets
 org/freedesktop/portal/desktop
+bin/epiphany/unwrap(gtk_ver=4)
 {% endblock %}
