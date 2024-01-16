54e6c539be94cc2571502d23edc215607243190d
Anton Samokhvalov <pg@yandex-team.ru>
2024-01-16 08:53:08 +0300

    upver

--- pkgs/bin/shadow/ix.sh
+++ pkgs/bin/shadow/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://github.com/shadow-maint/shadow/archive/refs/tags/4.14.2.tar.gz
-sha:ad7e266120013930129de67de6cd41a6e4fd53a406ccf55cdc4106eead0ae7a2
+https://github.com/shadow-maint/shadow/archive/refs/tags/4.14.3.tar.gz
+sha:d47b1d227257bef30111e5ea47785154bc826ef9aa91b1d93d09f4ca81e9db80
 {% endblock %}
 
 {% block bld_libs %}
