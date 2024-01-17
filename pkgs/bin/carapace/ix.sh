937a0ce131cf81be0371becb2cbbd5ec3466241b
Anton Samokhvalov <pg@yandex-team.ru>
2024-01-17 08:59:26 +0300

    upver

--- pkgs/bin/carapace/ix.sh
+++ pkgs/bin/carapace/ix.sh
@@ -1,11 +1,11 @@
 {% extends '//die/go/pure.sh' %}
 
 {% block go_url %}
-https://github.com/rsteube/carapace-bin/archive/refs/tags/v0.29.0.tar.gz
+https://github.com/rsteube/carapace-bin/archive/refs/tags/v0.29.1.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-e833b7fd0ce232c04bd5c9080c962adc136aecfa95107082230bfac42c9f8371
+c4cd874e447b15e9aa3418ff44b35743237afd7ff0818a57fbfcd3fef06e7784
 {% endblock %}
 
 {% block unpack %}
