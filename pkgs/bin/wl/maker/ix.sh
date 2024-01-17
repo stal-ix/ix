08cee0137726de5357c524e1bd98c2ed54f52d40
Anton Samokhvalov <pg@yandex-team.ru>
2024-01-17 12:10:53 +0300

    wip

---
+++ pkgs/bin/wl/maker/ix.sh
@@ -0,0 +1,38 @@
+{% extends '//die/c/cmake.sh' %}
+
+{% block git_repo %}
+https://github.com/phkaeser/wlmaker
+{% endblock %}
+
+{% block git_commit %}
+ed7a302fa78717ac496aeb2f2bd3256618f07d99
+{% endblock %}
+
+{% block git_sha %}
+5627a0582b5d7e44849170ef94f77b1657caf5422ca78f44c570419f37e42237
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/drm
+lib/cairo
+lib/curses
+lib/wayland
+lib/wlroots/17
+lib/xkb/common
+{% endblock %}
+
+{% block bld_tool %}
+bld/wayland
+{% endblock %}
+
+{% block patch %}
+sed -e 's|1.22.90|1.22.0|g' -i CMakeLists.txt
+cat << EOF >> third_party/protocols/CMakeLists.txt
+set_target_properties(protocol_headers PROPERTIES LINKER_LANGUAGE CXX)
+EOF
+{% endblock %}
+
+{% block cmake_flags %}
+CURSES_CURSES_LIBRARY=/
+{% endblock %}
