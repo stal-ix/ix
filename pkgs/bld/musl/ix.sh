a8a80e92a7b0286da45dbdad0a5e6a440a362136
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-01-18 08:22:12 +0300

    better

--- pkgs/bld/musl/ix.sh
+++ pkgs/bld/musl/ix.sh
@@ -19,5 +19,58 @@ sh ./configure \
 {% endblock %}
 
 {% block postinstall %}
-:
+cat << EOF > gcc.c
+void _Unwind_Backtrace() {
+}
+
+void _Unwind_DeleteException() {
+}
+
+void _Unwind_FindEnclosingFunction() {
+}
+
+void _Unwind_GetCFA() {
+}
+
+void _Unwind_GetDataRelBase() {
+}
+
+void _Unwind_GetIP() {
+}
+
+void _Unwind_GetIPInfo() {
+}
+
+void _Unwind_GetLanguageSpecificData() {
+}
+
+void _Unwind_GetRegionStart() {
+}
+
+void _Unwind_GetTextRelBase() {
+}
+
+void _Unwind_RaiseException() {
+}
+
+void _Unwind_Resume() {
+}
+
+void _Unwind_Resume_or_Rethrow() {
+}
+
+void _Unwind_SetGR() {
+}
+
+void _Unwind_SetIP() {
+}
+EOF
+
+cc -o ${out}/lib/libgcc_s.so.1 -shared gcc.c
+{% endblock %}
+
+{% block env %}
+export LDSO=${out}/bin/ld-musl-x86_64.so.1
+export LD_LIBRARY_PATH=${out}/lib
+export LDFLAGS="-L${out}/lib"
 {% endblock %}
