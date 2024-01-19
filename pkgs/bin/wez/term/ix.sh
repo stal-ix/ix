497a38ef8aff0f459d87718ddb723cdedb7ea51c
Anton Samokhvalov <pg@yandex-team.ru>
2024-01-19 21:53:00 +0300

    WIP

---
+++ pkgs/bin/wez/term/ix.sh
@@ -0,0 +1,36 @@
+{% extends '//die/rust/cargo.sh' %}
+
+{% block cargo_url %}
+https://github.com/wez/wezterm/archive/refs/tags/20230712-072601-f4abf8fd.tar.gz
+{% endblock %}
+
+{% block cargo_sha %}
+69e9
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/png
+lib/glib
+lib/iconv
+lib/expat
+lib/brotli
+lib/pcre/2
+lib/wayland
+lib/freetype
+lib/harfbuzz
+lib/wayland/dl
+lib/mesa/gl/dl
+lib/drivers/3d
+lib/mesa/egl/dl
+lib/xkb/common/dl
+{% endblock %}
+
+{% block bld_tool %}
+bld/pkg/config
+{% endblock %}
+
+{% block install %}
+mkdir ${out}/bin
+cp target/release/wezterm ${out}/bin/
+{% endblock %}
