95f466358199244439ec81a8d372a82670a12a10
Anton Samokhvalov <pg@yandex-team.ru>
2024-01-20 11:39:32 +0300

    better

--- pkgs/lib/asm/ix.sh
+++ pkgs/lib/asm/ix.sh
@@ -5,7 +5,8 @@ https://www.agner.org/optimize/asmlib.zip
 sha:481d6245051a2b82c06e393b782ba1dd1fe9724209ed9ba5ad5692728ef8da7a
 {% endblock %}
 
-{% block bld_tool %}
+{% block std_box %}
+{{super()}}
 bld/nasm
 {% endblock %}
 
