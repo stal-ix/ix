7da97a91cfc3dc959e1d5c36746180a08dce1d3c
Anton Samokhvalov <pg@yandex-team.ru>
2024-01-21 09:55:45 +0300

    upver

--- pkgs/bin/moe/ix.sh
+++ pkgs/bin/moe/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
-https://ftp.gnu.org/gnu/moe/moe-1.13.tar.lz
-sha:43a557bc512f89d6c718e5f41029cfe3a055682620eb8dbece6302f34a26146b
+https://ftp.gnu.org/gnu/moe/moe-1.14.tar.lz
+sha:f4babd6ce0ae19516f983454fb20d32dff71ad316337ac6bf93a42a5ff209c9d
 {% endblock %}
 
 {% block bld_libs %}
