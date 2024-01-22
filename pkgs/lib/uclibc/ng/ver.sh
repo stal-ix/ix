e822b5575a1d541fcca7c53bbd55c6acf1c1646c
Anton Samokhvalov <pg@yandex-team.ru>
2024-01-22 12:15:39 +0300

    upver

--- pkgs/lib/uclibc/ng/ver.sh
+++ pkgs/lib/uclibc/ng/ver.sh
@@ -1,2 +1,2 @@
-http://downloads.uclibc-ng.org/releases/1.0.41/uClibc-ng-1.0.41.tar.xz
-sha:b32a92a0218d95922d6976464e6ef51e2ebacfbcdb605820458d9dbb8a61e025
+http://downloads.uclibc-ng.org/releases/1.0.45/uClibc-ng-1.0.45.tar.xz
+sha:c2f4c6b6e19d7c9c226992a3746efd7ab932040463c15ee0bc8f4132b5777ac4
