28fadc318d437dcb108b5b04d91d47717fe69f65
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-01-23 12:07:31 +0300

    better

--- pkgs/bin/niri/ix.sh
+++ pkgs/bin/niri/ix.sh
@@ -1,33 +1,7 @@
-{% extends '//die/rust/cargo.sh' %}
+{% extends '//die/hub.sh' %}
 
-{% block cargo_url %}
-https://github.com/YaLTeR/niri/archive/refs/tags/v0.1.0-beta.1.tar.gz
-{% endblock %}
-
-{% block cargo_sha %}
-94b4b96bfd1cd5c1380358d3d7c8fbc1b20ebd4e8dea33cab122014fbb7fca69
-{% endblock %}
-
-{% block lib_deps %}
-lib/seat
-lib/udev
-lib/glib
-lib/pango
-lib/cairo
-lib/input
-lib/opengl
-lib/drivers/3d
-lib/wayland/dl
-lib/mesa/egl/dl
-lib/xkb/common/dl
-lib/wayland/dl/server
-{% endblock %}
-
-{% block cargo_features %}
-dbus
-{% endblock %}
-
-{% block install %}
-mkdir ${out}/bin
-cp ${tmp}/release/niri ${out}/bin/
+{% block run_deps %}
+bin/niri/unwrap
+bin/dbus/session
+bin/dbus/session/runner(prog=niri)
 {% endblock %}
