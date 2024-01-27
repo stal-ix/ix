89a0290d547c92d1062c672e2d9ec24410066bf1
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-01-27 11:50:46 +0300

    add

---
+++ pkgs/bin/cage/break/ix.sh
@@ -0,0 +1,27 @@
+{% extends '//die/c/meson.sh' %}
+
+{% block fetch %}
+https://github.com/project-repo/cagebreak/archive/refs/tags/2.3.0.tar.gz
+sha:fd18e9d3cfffb8369dd850d329765687fbbda4dcb1bc3e846b683b2a6ac4d739
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/pango
+lib/cairo
+lib/wayland
+lib/wlroots/17
+lib/xkb/common
+{% endblock %}
+
+{% block bld_tool %}
+bld/wayland
+{% endblock %}
+
+{% block build_flags %}
+shut_up
+{% endblock %}
+
+{% block patch %}
+sed -e "s|/usr/share|${out}/share|" -i meson.build
+{% endblock %}
