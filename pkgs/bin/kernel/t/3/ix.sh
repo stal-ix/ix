cc2c41a89b861cc4a4bf02b058219e517e09aeb1
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-01-27 22:15:10 +0300

    upver

--- pkgs/bin/kernel/t/3/ix.sh
+++ pkgs/bin/kernel/t/3/ix.sh
@@ -3,7 +3,3 @@
 {% block kernel_headers %}
 lib/linux/headers/custom(kernel_url={{self.kernel_url().strip()}},kernel_sha={{self.kernel_sha().strip()}})
 {% endblock %}
-
-{% block slot %}
-0
-{% endblock %}
