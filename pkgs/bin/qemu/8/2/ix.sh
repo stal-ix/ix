463c0a215f4a705449250f22d823519763ee8515
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-01-30 01:22:54 +0300

    upver

--- pkgs/bin/qemu/8/2/ix.sh
+++ pkgs/bin/qemu/8/2/ix.sh
@@ -1,9 +1,9 @@
 {% extends '//bin/qemu/8/1/ix.sh' %}
 
 {% block git_branch %}
-v8.2.0
+v8.2.1
 {% endblock %}
 
 {% block git_sha %}
-a1405565ef14f132bd85347e4a4e374650bdfd1014b32bd96bc0ce3cb1d12afc
+cb9debfa74cdb9127bddcd2c7e03806f5a6a46688826cb23df128adf2b204330
 {% endblock %}
