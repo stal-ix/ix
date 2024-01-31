c0ea5f684dc5b6ab8af3aa0d88ef50cc5b3c1de2
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-01-31 22:48:46 +0300

    fix

--- pkgs/bin/clang/16/yandex/ix.sh
+++ pkgs/bin/clang/16/yandex/ix.sh
@@ -22,9 +22,12 @@ asan_static.patch
 D92001.patch
 D21113-case-insesitive-include-paths.patch
 D142421.patch
-{#D149723-optimize-renamer-clang-tidy-check.patch#}
 fix-build.patch
 dwarf-emit-namespaces.patch
+{% endblock %}
+
+{% block common_patches %}
+D149723-optimize-renamer-clang-tidy-check.patch
 {% endblock %}
 
 {% block llvm_patches %}
@@ -33,6 +36,11 @@ vfs-case-insensitive.patch
 
 {% block patch %}
 {{super()}}
+{% for p in self.common_patches().strip().split() %}
+(base64 -d | patch -p1) << EOF
+{{ix.load_file('patches/clang/' + p) | b64e}}
+EOF
+{% endfor %}
 cd llvm
 {% for p in self.llvm_patches().strip().split() %}
 (base64 -d | patch -p1) << EOF
