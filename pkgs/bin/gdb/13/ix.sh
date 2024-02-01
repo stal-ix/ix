c67934af181ca0584d33d07ffcef9f8f0920c21f
Anton Samokhvalov <pg@yandex-team.ru>
2024-02-01 15:29:02 +0300

    add

---
+++ pkgs/bin/gdb/13/ix.sh
@@ -0,0 +1,6 @@
+{% extends '//bin/gdb/14/ix.sh' %}
+
+{% block fetch %}
+https://ftp.gnu.org/gnu/gdb/gdb-13.2.tar.xz
+sha:fd5bebb7be1833abdb6e023c2f498a354498281df9d05523d8915babeb893f0a
+{% endblock %}
