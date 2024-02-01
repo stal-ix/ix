3bfe0c5d0dbe2f1bcbc4ba937aa3951b3576508f
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-02-01 12:38:38 +0300

    upver

--- pkgs/bin/kernel/6/7/ver.sh
+++ pkgs/bin/kernel/6/7/ver.sh
@@ -1,13 +1,9 @@
 {% block kernel_version %}
-6.7.2
-{% endblock %}
-
-{% block kernel_url %}
-https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.7.2.tar.xz
+6.7.3
 {% endblock %}
 
 {% block kernel_sha %}
-c34de41baa29c475c0834e88a3171e255ff86cd32d83c6bffc2b797e60bfa671
+b7f08c652747574a3aa26e317d7a8f23ffab3fb645e1b1533b215dcfd5742b44
 {% endblock %}
 
 {% include '//bin/kernel/t/ver.sh' %}
