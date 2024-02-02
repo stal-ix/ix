fcf6697d4d94a25caa7abdf1c7b39fe847745e54
Anton Samokhvalov <pg@yandex-team.ru>
2024-02-02 22:32:58 +0300

    upver

--- pkgs/lib/iwasm/t/ix.sh
+++ pkgs/lib/iwasm/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/bytecodealliance/wasm-micro-runtime/archive/refs/tags/WAMR-1.2.3.tar.gz
-sha:85057f788630dc1b8c371f5443cc192627175003a8ea63c491beaff29a338346
+https://github.com/bytecodealliance/wasm-micro-runtime/archive/refs/tags/WAMR-1.3.2.tar.gz
+sha:58961ba387ed66ace2dd903597f1670a42b8154a409757ae6f06f43fe867a98c
 {% endblock %}
 
 {% block bld_libs %}
