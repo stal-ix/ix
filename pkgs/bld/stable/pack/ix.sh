1804c60cc1a49311ea0b6be5406b291c106687be
Anton Samokhvalov <pg@yandex-team.ru>
2024-02-02 19:35:24 +0300

    upver

--- pkgs/bld/stable/pack/ix.sh
+++ pkgs/bld/stable/pack/ix.sh
@@ -8,8 +8,18 @@ bld/r/o
 
 {% block install %}
 mkdir ${out}/bin
+
 base64 -d << EOF > ${out}/bin/stable_pack
 {% include 'stable_pack/base64' %}
 EOF
+
+base64 -d << EOF > ${out}/bin/store_links
+{% include 'store_links/base64' %}
+EOF
+
+base64 -d << EOF > ${out}/bin/stable_pack_2
+{% include 'stable_pack_2/base64' %}
+EOF
+
 chmod +x ${out}/bin/*
 {% endblock %}
