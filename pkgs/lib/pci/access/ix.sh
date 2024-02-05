f841c132db13575ff63b55fd7964de5bfd4e2d94
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-02-05 13:59:27 +0300

    upver

--- pkgs/lib/pci/access/ix.sh
+++ pkgs/lib/pci/access/ix.sh
@@ -1,16 +1,14 @@
-{% extends '//die/c/autorehell.sh' %}
+{% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-#https://gitlab.freedesktop.org/xorg/lib/libpciaccess/-/archive/libpciaccess-0.17/libpciaccess-libpciaccess-0.17.tar.bz2
-#sha:6e6d69d3cf5ee294dcb38ccfb360f1895a263df5dd8058563cdee62d9b9c17fd
-http://distfiles.gentoo.org/distfiles/71/libpciaccess-0.17.tar.xz
-sha:74283ba3c974913029e7a547496a29145b07ec51732bbb5b5c58d5025ad95b73
+https://gitlab.freedesktop.org/xorg/lib/libpciaccess/-/archive/libpciaccess-0.18/libpciaccess-libpciaccess-0.18.tar.bz2
+sha:c087f68008d91ebe0b5e5e77ca8568cf2c1ab35379f55cc098a5e87958808d90
 {% endblock %}
 
 {% block lib_deps %}
 lib/c
 {% endblock %}
 
-{% block bld_tool %}
-bin/auto/conf/xorg
+{% block bld_libs %}
+lib/kernel
 {% endblock %}
