8a7ad7e7cf62d0dc9f944e33119d983dbbb9c6a4
Anton Samokhvalov <pg@yandex-team.ru>
2024-02-06 20:42:56 +0300

    better

--- pkgs/bld/boot/0/mix/darwin/arm64/ix.sh
+++
@@ -1,9 +0,0 @@
-{% extends '//bld/boot/0/ix/template.sh' %}
-
-{% block md5 %}
-md5:9dc885670475ad994afdccdad56d4637
-{% endblock %}
-
-{% block link %}
-cache/6495d8eb5fce7b9e7f72dc271ff37fb5
-{% endblock %}
--- pkgs/bld/boot/0/mix/ix.sh
+++
@@ -1,5 +0,0 @@
-{% extends '//die/hub.sh' %}
-
-{% block run_deps %}
-bld/boot/0/ix/{{host.os}}/{{host.arch}}
-{% endblock %}
