a5165037aa70da2632c0064eaa3b4a57006286f2
Anton Samokhvalov <pg@yandex-team.ru>
2024-02-07 13:41:12 +0300

    upver

--- pkgs/bin/axel/ix.sh
+++ pkgs/bin/axel/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://github.com/axel-download-accelerator/axel/archive/refs/tags/v2.17.12.tar.gz
-sha:0ba5fa5ffd919faa51b0e55717a59f9475c2913095bc14c27c2e24ab2dad3ce9
+https://github.com/axel-download-accelerator/axel/archive/refs/tags/v2.17.13.tar.gz
+sha:b10cb89b8a6b31fab2e5183c7a461a95691c5a5344d466f4ef47aa62e5c456a2
 {% endblock %}
 
 {% block bld_libs %}
