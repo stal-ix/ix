8a9e5623515b4317004b52b7d414a06383c0a8d2
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-02-07 10:37:53 +0300

    upver

--- pkgs/lib/git/2/ix.sh
+++ pkgs/lib/git/2/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/libgit2/libgit2/archive/refs/tags/v1.7.0.tar.gz
-sha:d9d0f84a86bf98b73e68997f5c1543cc5067d0ca9c7a5acaba3e8d117ecefef3
+https://github.com/libgit2/libgit2/archive/refs/tags/v1.7.2.tar.gz
+sha:de384e29d7efc9330c6cdb126ebf88342b5025d920dcb7c645defad85195ea7f
 {% endblock %}
 
 {% block lib_deps %}
