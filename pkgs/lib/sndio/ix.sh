4b13767d0fa696a055ded4e8720bd1892340b576
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-02-08 16:26:26 +0300

    better

--- pkgs/lib/sndio/ix.sh
+++ pkgs/lib/sndio/ix.sh
@@ -4,14 +4,9 @@
 
 {% block build_flags %}
 wrap_cc
+rename_dynlib
 {% endblock %}
 
 {% block make_flags %}
 -C libsndio
-{% endblock %}
-
-{% block install %}
-{{super()}}
-cd ${out}/lib
-cp libsndio.so libsndio.a
 {% endblock %}
