cbbf31fbda4cf324879ca256babf33a61bd13875
Anton Samokhvalov <pg@yandex-team.ru>
2024-02-10 20:40:10 +0300

    fix build

--- pkgs/bin/coreutils/9/4/ix.sh
+++ pkgs/bin/coreutils/9/4/ix.sh
@@ -8,3 +8,8 @@ lib/openssl/3
 https://ftp.gnu.org/gnu/coreutils/coreutils-9.4.tar.gz
 sha:5f600d9093973b0afe25393d9bc18c44f2232657f4ca0d95ea31c702eb66b739
 {% endblock %}
+
+{% block cpp_includes %}
+${PWD}/lib
+{{super()}}
+{% endblock %}
