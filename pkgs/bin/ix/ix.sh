b77d06843cb09572a63dc2324087aedd4702ae9e
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-02-11 18:57:51 +0300

    better

--- pkgs/bin/ix/ix.sh
+++ pkgs/bin/ix/ix.sh
@@ -3,6 +3,7 @@
 {% block run_deps %}
 bin/aria/2
 bin/ix/curl
+bin/su/exec
 bin/ix/bsdtar
 bin/ix/python
 bin/ix/respawn
--- pkgs/etc/services/runit/ix.sh
+++ pkgs/etc/services/runit/ix.sh
@@ -2,5 +2,6 @@
 
 {% block run_deps %}
 bin/runsrv
+bin/su/exec
 etc/services/runit/script
 {% endblock %}
