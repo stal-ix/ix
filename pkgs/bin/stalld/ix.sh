879cc6fb1e46818061184036bebb0d37410af36b
Anton Samokhvalov <pg@yandex-team.ru>
2024-02-11 17:17:05 +0300

    upver

--- pkgs/bin/stalld/ix.sh
+++ pkgs/bin/stalld/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/make.sh' %}
 
 {% block fetch %}
-https://git.kernel.org/pub/scm/utils/stalld/stalld.git/snapshot/stalld-1.17.1.tar.gz
-sha:e7cccf63eb721bf50f7d978178d66b2eceebc24d0d8aabff7b118b768c027be7
+https://git.kernel.org/pub/scm/utils/stalld/stalld.git/snapshot/stalld-1.19.1.tar.gz
+sha:44f90b935080835b017a6cca6fa8ba8336b66fe2f799495b64a4a92f24f61d59
 {% endblock %}
 
 {% block bld_libs %}
@@ -20,5 +20,19 @@ sed -e 's|/usr/|/|' -i Makefile
 {% endblock %}
 
 {% block make_flags %}
+USE_BPF=0
 DESTDIR=${out}
+{% endblock %}
+
+{% block cpp_missing %}
+pthread.h
+unistd.h
+sys/types.h
+sys/stat.h
+fcntl.h
+sched.h
+{% endblock %}
+
+{% block cpp_defines %}
+_GNU_SOURCE
 {% endblock %}
