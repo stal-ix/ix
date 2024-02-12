8d649540197cf02d582e4e749c68d69d629deaec
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-02-13 01:00:16 +0300

    upver

--- pkgs/lib/gcr/4/ix.sh
+++ pkgs/lib/gcr/4/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//lib/gcr/t/ix.sh' %}
 
 {% block fetch %}
-https://gitlab.gnome.org/GNOME/gcr/-/archive/4.1.0/gcr-4.1.0.tar.bz2
-sha:3fa4bceaee1f1a9ee1f81157969f1262df19dae975c8a148d5f4c425f866573a
+https://gitlab.gnome.org/GNOME/gcr/-/archive/4.2.0/gcr-4.2.0.tar.bz2
+sha:3596dfda370bac19ba722e4ee2abcc70408f4a0c2688747d95a244586e9a1805
 {% endblock %}
 
 {% block lib_deps %}
