ec44ede39b5ff5664536fc4f2c05c22abef90433
Anton Samokhvalov <pg@yandex-team.ru>
2024-02-13 18:58:30 +0300

    better

---
+++ pkgs/bin/secret/tool/ix.sh
@@ -0,0 +1,1 @@
+{% extends '//lib/secret/t/ix.sh' %}
--- pkgs/lib/secret/ix.sh
+++ pkgs/lib/secret/ix.sh
@@ -1,27 +1,6 @@
-{% extends '//die/c/meson.sh' %}
-
-{% block fetch %}
-https://gitlab.gnome.org/GNOME/libsecret/-/archive/0.21.3/libsecret-0.21.3.tar.bz2
-sha:2e6963924a4e17de6d11820d2383fb2d8657f132805af33f0e590df099880720
-{% endblock %}
-
-{% block lib_deps %}
-lib/c
-lib/glib
-lib/gcrypt
-{% endblock %}
-
-{% block bld_tool %}
-bld/glib
-{% endblock %}
-
-{% block meson_flags %}
-manpage=false
-gtk_doc=false
-debugging=false
-introspection=false
-{% endblock %}
+{% extends 't/ix.sh' %}
 
 {% block c_rename_symbol %}
+{{super()}}
 SECMEM_pool_data_v1_0
 {% endblock %}
