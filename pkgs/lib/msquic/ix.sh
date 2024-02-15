85a0126932410be375c344550dce16312cf49516
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-02-15 23:57:07 +0300

    better

--- pkgs/lib/msquic/ix.sh
+++ pkgs/lib/msquic/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/microsoft/msquic/archive/refs/tags/v2.3.2.tar.gz
-sha:5bfc5398e1818cc596a7f43d3f2abf9f6e4877510bd44a726a9cb2c836d559b3
+https://github.com/microsoft/msquic/archive/refs/tags/v2.3.4.tar.gz
+sha:5fea92f4644462a5529f50930e2b391214d610586e07acb093ca1d0e1db3eb0e
 {% endblock %}
 
 {% block lib_deps %}
@@ -34,4 +34,6 @@ sed -e 's|QUIC_TLS STREQUAL "openssl"|0|' -i CMakeLists.txt
 {{super()}}
 cd src/inc
 cp msquic.hpp *.h ${out}/include/
+mkdir ${out}/lib/cmake
+mv ${out}/share/msquic ${out}/lib/cmake/
 {% endblock %}
