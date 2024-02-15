614d0397d4260380c2ce03663c589d02e6a9d0e9
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-02-16 02:04:56 +0300

    lto-aware

--- pkgs/lib/musl/naked/unwrap/ix.sh
+++ pkgs/lib/musl/naked/unwrap/ix.sh
@@ -1,21 +1,8 @@
 {% extends '//lib/musl/noalloc/ix.sh' %}
 
-{% block bad_sym %}
-stub_invalid_handle
-stub_dladdr
-stub_dlopen
-stub_dlsym
-dlclose
-dlerror
-dlinfo
-dlopen
-dladdr
-dlsym
-{% endblock %}
-
-{% block install %}
+{% block patch %}
 {{super()}}
-{% for x in ix.parse_list(self.bad_sym()) %}
-llvm-objcopy --strip-symbol={{x}} ${out}/lib/libc.a
-{% endfor %}
+find src/ldso -type f | grep -v dl_iterate_phdr | while read l; do
+    rm ${l}
+done
 {% endblock %}
