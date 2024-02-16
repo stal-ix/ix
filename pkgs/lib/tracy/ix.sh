7e97f3c82b20ca34fe6cdceb39187c86cc427b86
Anton Samokhvalov <pg@yandex-team.ru>
2024-02-16 15:25:58 +0300

    add

---
+++ pkgs/lib/tracy/ix.sh
@@ -0,0 +1,15 @@
+{% extends '//die/c/cmake.sh' %}
+
+{% block fetch %}
+https://github.com/wolfpld/tracy/archive/refs/tags/v0.10.tar.gz
+sha:a76017d928f3f2727540fb950edd3b736caa97b12dbb4e5edce66542cbea6600
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+lib/c++
+{% endblock %}
+
+{% block bld_libs %}
+lib/kernel
+{% endblock %}
