bb8810b4ee30ddc5de7be128743342541f6c32a6
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-02-16 21:42:15 +0300

    better

--- pkgs/bin/sndfile/ix.sh
+++ pkgs/bin/sndfile/ix.sh
@@ -1,7 +1,14 @@
 {% extends '//lib/sndfile/t/ix.sh' %}
 
 {% block bld_libs %}
+lib/opus
+lib/lame
 lib/sndio
 lib/kernel
+lib/mpg123
+lib/xiph/ogg
+lib/xiph/flac
+lib/xiph/speex
+lib/xiph/vorbis
 {{super()}}
 {% endblock %}
