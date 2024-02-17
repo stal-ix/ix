38ab5b8a0a61723658d3c425a793cf2cfd5b62f2
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-02-17 18:50:07 +0300

    upver

--- pkgs/bin/scdoc/ix.sh
+++ pkgs/bin/scdoc/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/make.sh' %}
 
 {% block fetch %}
-https://git.sr.ht/~sircmpwn/scdoc/archive/1.11.2.tar.gz
-sha:e9ff9981b5854301789a6778ee64ef1f6d1e5f4829a9dd3e58a9a63eacc2e6f0
+https://git.sr.ht/~sircmpwn/scdoc/archive/1.11.3.tar.gz
+sha:4c5c6136540384e5455b250f768e7ca11b03fdba1a8efc2341ee0f1111e57612
 {% endblock %}
 
 {% block build_flags %}
