c256eb8d28ce113addd7ffaa84ae6b1ebf1e8a83
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-02-19 16:16:28 +0300

    add

---
+++ pkgs/bin/i3/status/rust/ix.sh
@@ -0,0 +1,13 @@
+{% extends '//die/rust/cargo.sh' %}
+
+{% block cargo_url %}
+https://github.com/greshake/i3status-rust/archive/refs/tags/v0.33.0.tar.gz
+{% endblock %}
+
+{% block cargo_sha %}
+73232f55d13d9160b3706ad659c39a417f5688aaa112a3d4a3214397c6c6b16b
+{% endblock %}
+
+{% block bld_libs %}
+lib/openssl
+{% endblock %}
