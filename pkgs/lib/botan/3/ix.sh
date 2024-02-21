77650b9de68e6be03eec3735d4ff3cdeecd37258
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-02-22 00:25:54 +0300

    upver

---
+++ pkgs/lib/botan/3/ix.sh
@@ -0,0 +1,6 @@
+{% extends '//lib/botan/ix.sh' %}
+
+{% block fetch %}
+https://github.com/randombit/botan/archive/refs/tags/3.3.0.tar.gz
+sha:57fefda7b9ab6f8409329620cdaf26d2d7e962b6a10eb321d331e9ecb796f804
+{% endblock %}
