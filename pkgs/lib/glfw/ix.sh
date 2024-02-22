9988966ba554c2b5876d6a540a73bceee02c0bf6
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-02-22 09:48:56 +0300

    upver

--- pkgs/lib/glfw/ix.sh
+++ pkgs/lib/glfw/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/glfw/glfw/archive/refs/tags/3.3.9.tar.gz
-sha:a7e7faef424fcb5f83d8faecf9d697a338da7f7a906fc1afbc0e1879ef31bd53
+https://github.com/glfw/glfw/archive/refs/tags/3.3.10.tar.gz
+sha:4ff18a3377da465386374d8127e7b7349b685288cb8e17122f7e1179f73769d5
 {% endblock %}
 
 {% block lib_deps %}
@@ -25,11 +25,4 @@ GLFW_BUILD_TESTS=OFF
 GLFW_BUILD_EXAMPLES=OFF
 GLFW_VULKAN_STATIC=ON
 GLFW_USE_WAYLAND=ON
-{% endblock %}
-
-{% block patch %}
-# imhex segfaults on glfwTerminate
-find . -name '*.c' | while read l; do
-    sed -e 's|regfree.*|{}|' -i ${l}
-done
 {% endblock %}
