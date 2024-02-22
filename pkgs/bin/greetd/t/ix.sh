3d35abab8afb7ce120378191c7e5d037f4814a96
Anton Samokhvalov <pg@yandex-team.ru>
2024-02-22 22:19:06 +0300

    better

--- pkgs/bin/greetd/t/ix.sh
+++ pkgs/bin/greetd/t/ix.sh
@@ -1,9 +1,9 @@
-{% extends '//die/rust/cargo.sh' %}
+{% extends '//die/rust/cargo_v2.sh' %}
 
 {% block cargo_url %}
 https://git.sr.ht/~kennylevinsen/greetd/archive/0.9.0.tar.gz
 {% endblock %}
 
 {% block cargo_sha %}
-1bc314b525f56d8ebc62d9e9d25c95876adf3f65390db33b551e5b2482c464a7
+d29ad6351a1af22c3079d8c8190f2ae4143e66205c5c9d4cd60939597f989502
 {% endblock %}
