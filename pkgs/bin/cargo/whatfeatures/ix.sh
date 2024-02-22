043ed3f9a6447cdfb2cd9ddffad335bfcc962a4f
Anton Samokhvalov <pg@yandex-team.ru>
2024-02-22 23:28:43 +0300

    better

--- pkgs/bin/cargo/whatfeatures/ix.sh
+++ pkgs/bin/cargo/whatfeatures/ix.sh
@@ -1,11 +1,11 @@
-{% extends '//die/rust/cargo.sh' %}
+{% extends '//die/rust/cargo_v2.sh' %}
 
 {% block cargo_url %}
 https://github.com/museun/cargo-whatfeatures/archive/refs/tags/v0.9.9.tar.gz
 {% endblock %}
 
 {% block cargo_sha %}
-df5e98f33b2ee309138dcdffeaeba3086f706c465a5a0d676bf9b4ede11a52d2
+55cbcf6ab338dce9d1db947956d5c502b65bb12d815b17af35500957850a2976
 {% endblock %}
 
 {% block bld_libs %}
