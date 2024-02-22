c604d7f52fa4d9ec167532e80d7877eff713bd5f
Anton Samokhvalov <pg@yandex-team.ru>
2024-02-23 01:15:50 +0300

    upver

--- pkgs/lib/unwind/ix.sh
+++ pkgs/lib/unwind/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://github.com/libunwind/libunwind/archive/refs/tags/v1.8.0.tar.gz
-sha:5bddae0096d5e66bf6299e46d31f7c727bdf9fccbdbd8e1b6ffd9b1f6142cd2e
+https://github.com/libunwind/libunwind/archive/refs/tags/v1.8.1.tar.gz
+sha:38833b7b1582db7d76485a62a213706c9252b3dab7380069fea5824e823d8e41
 {% endblock %}
 
 {# can not use full lib/c here, cause libc++-16 contains incompatible libunwind.h #}
