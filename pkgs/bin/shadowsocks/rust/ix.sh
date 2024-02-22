e7ae209164e4e58205d18311a2b54e7e8092367a
Anton Samokhvalov <pg@yandex-team.ru>
2024-02-22 22:17:48 +0300

    better

--- pkgs/bin/shadowsocks/rust/ix.sh
+++ pkgs/bin/shadowsocks/rust/ix.sh
@@ -1,11 +1,11 @@
-{% extends '//die/rust/cargo.sh' %}
+{% extends '//die/rust/cargo_v2.sh' %}
 
 {% block cargo_url %}
 https://github.com/shadowsocks/shadowsocks-rust/archive/refs/tags/v1.18.1.tar.gz
 {% endblock %}
 
 {% block cargo_sha %}
-1eada146744301a024d2b8a974eed28527e10610e6e8f1e2503f4de464e360a2
+38f304ef9005af50263154676df1ef32c4272406538ad4ab3c95856de569e9e8
 {% endblock %}
 
 {% block bld_tool %}
