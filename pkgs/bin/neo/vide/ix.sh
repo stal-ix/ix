85acd016b7256e9e41bdc1e8001d7b5b3200be9f
Anton Samokhvalov <pg@yandex-team.ru>
2024-02-22 21:59:48 +0300

    better

--- pkgs/bin/neo/vide/ix.sh
+++ pkgs/bin/neo/vide/ix.sh
@@ -1,11 +1,11 @@
-{% extends '//die/rust/cargo.sh' %}
+{% extends '//die/rust/cargo_v2.sh' %}
 
 {% block cargo_url %}
 https://github.com/neovide/neovide/archive/refs/tags/0.12.2.tar.gz
 {% endblock %}
 
 {% block cargo_sha %}
-cee800370d887c928d55d2883093a0ae1f3989307249b56e45ca3c0a5b42a959
+46ceea9523529e4517e39f7e24150456f885aa0ae07b390c94896001a3f94e32
 {% endblock %}
 
 {% block bld_libs %}
