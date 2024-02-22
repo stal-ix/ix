37a01841d8987817eae4d70c88a8d28fb146b353
Anton Samokhvalov <pg@yandex-team.ru>
2024-02-22 23:30:37 +0300

    better

--- pkgs/bin/xplr/ix.sh
+++ pkgs/bin/xplr/ix.sh
@@ -1,4 +1,4 @@
-{% extends '//die/rust/cargo.sh' %}
+{% extends '//die/rust/cargo_v2.sh' %}
 
 {# hard luajit vendor #}
 
@@ -7,7 +7,7 @@ https://github.com/sayanarijit/xplr/archive/refs/tags/v0.21.5.tar.gz
 {% endblock %}
 
 {% block cargo_sha %}
-60398192f13dcdc5de328bb7ea740ae274617e6688bcd2a6ba812f4c99fe285e
+ffae36ded8b34597ae4b10a736963a51afbd03a4be8a19045fa713f7e58d2d91
 {% endblock %}
 
 {% block bld_tool %}
