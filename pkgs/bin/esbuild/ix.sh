c1d5fde78fdaee05dac31e5bb14bcf7821a12b4e
Anton Samokhvalov <pg@yandex-team.ru>
2024-02-23 01:32:59 +0300

    better

--- pkgs/bin/esbuild/ix.sh
+++ pkgs/bin/esbuild/ix.sh
@@ -1,15 +1,11 @@
-{% extends '//die/go/build.sh' %}
+{% extends '//die/go/build_v2.sh' %}
 
 {% block go_url %}
 https://github.com/evanw/esbuild/archive/refs/tags/v0.19.6.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-cb6b5c550b59ea517e2eae140c9f843c130e3e99dd3864426e79945c982747af
-{% endblock %}
-
-{% block go_refine %}
-rm scripts/browser/esbuild.wasm.bagel
+ee0501044ff46151a8ee67202b1be45f0f2f55bb6ae59035d41d7be734f4fa9d
 {% endblock %}
 
 {% block unpack %}
