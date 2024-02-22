e209280b2989b9c051109b1e686a517a25c19e9e
Anton Samokhvalov <pg@yandex-team.ru>
2024-02-23 00:06:45 +0300

    refac

---
+++ pkgs/aux/go/v2/ix.sh
@@ -0,0 +1,25 @@
+{% extends '//aux/fetch/ix.sh' %}
+
+{% set fname %}go_{{parent_id}}.tar.lz4{% endset %}
+
+{% block bld_tool %}
+bin/go
+{{super()}}
+{% endblock %}
+
+{% block build %}
+export GOSUMDB=off
+export GOCACHE=${tmp}/cgo
+export GOMODCACHE=${tmp}/gmc
+export GOPROXY="https://proxy.golang.org,direct"
+
+{% if go_refine %}
+{{go_refine | b64d}}
+{% endif %}
+
+find . -type f -name go.mod | while read l; do (
+    cd $(dirname ${l})
+    go mod tidy
+    go mod vendor
+) done
+{% endblock %}
