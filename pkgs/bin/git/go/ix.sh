2b4883844d97d08c41155fda703ad1f57f94fb7f
Anton Samokhvalov <pg@yandex-team.ru>
2024-02-23 17:42:02 +0300

    better

--- pkgs/bin/git/go/ix.sh
+++ pkgs/bin/git/go/ix.sh
@@ -1,11 +1,11 @@
-{% extends '//die/go/build.sh' %}
+{% extends '//die/go/build_v2.sh' %}
 
 {% block go_url %}
 https://github.com/go-git/go-git/archive/refs/tags/v5.4.2.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-e23a96436d12d169a45c4b0338f4a29dfc6e22e005d617af74dd2bea92d492d4
+f02fb68b225614a7b492dc2ad8dcba0ca1d5bb952e966806978f9b26d8fa910e
 {% endblock %}
 
 {% block unpack %}
