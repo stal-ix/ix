4875f7c91812f3bda9c7a6de5b8216b797ea7b16
Anton Samokhvalov <pg@yandex-team.ru>
2024-02-23 17:23:07 +0300

    better

--- pkgs/bin/duf/ix.sh
+++ pkgs/bin/duf/ix.sh
@@ -1,11 +1,11 @@
-{% extends '//die/go/build.sh' %}
+{% extends '//die/go/build_v2.sh' %}
 
 {% block go_url %}
 https://github.com/muesli/duf/archive/refs/tags/v0.8.1.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-60b9cded0d9be9c4de65cebb2b424db97e18cd1f2a738e6dd9f2941685e932a6
+4ffb366c2ec0e9cb49e8637239c602b4e2a40cf9f2c2656f6c5067e206381110
 {% endblock %}
 
 {% block install %}
