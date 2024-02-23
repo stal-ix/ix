8156392f62b05108979d3e1067c5bcaf0ad5f329
Anton Samokhvalov <pg@yandex-team.ru>
2024-02-23 14:09:39 +0300

    better

--- pkgs/bin/clx/ix.sh
+++ pkgs/bin/clx/ix.sh
@@ -1,11 +1,11 @@
-{% extends '//die/go/build.sh' %}
+{% extends '//die/go/build_v2.sh' %}
 
 {% block go_url %}
 https://github.com/bensadeh/circumflex/archive/refs/tags/3.5.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-4a69b5d7caf96a27367eb9867eba0d8562f5628a68130d8b7ddd401868acdb9a
+82663cb2b1f634dcb0a18394d81975099f3a8d864d27ff5645f1f3e45ad69a22
 {% endblock %}
 
 {% block install %}
