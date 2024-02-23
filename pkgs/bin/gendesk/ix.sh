a79137f4df5646a8101457c110b32090d4c9c69c
Anton Samokhvalov <pg@yandex-team.ru>
2024-02-23 17:40:11 +0300

    better

--- pkgs/bin/gendesk/ix.sh
+++ pkgs/bin/gendesk/ix.sh
@@ -1,15 +1,11 @@
-{% extends '//die/go/build.sh' %}
+{% extends '//die/go/build_v2.sh' %}
 
 {% block go_url %}
 https://github.com/xyproto/gendesk/archive/refs/tags/1.0.9.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-6175e5350985c864956dff6de260c8e1b4566717c6769c79b47d9e6a070d442e
-{% endblock %}
-
-{% block go_refine %}
-find . -type l -delete
+81cf78b86132c609d1926bff1e0f7f0fceb4f4d2070e6f486e79935ae27b42be
 {% endblock %}
 
 {% block install %}
