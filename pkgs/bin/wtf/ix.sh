0140021d5e1fe28c180ca6ef1edb9a3c8137951f
Anton Samokhvalov <pg@yandex-team.ru>
2024-02-23 14:16:43 +0300

    better

--- pkgs/bin/wtf/ix.sh
+++ pkgs/bin/wtf/ix.sh
@@ -1,11 +1,11 @@
-{% extends '//die/go/build.sh' %}
+{% extends '//die/go/build_v2.sh' %}
 
 {% block go_url %}
 https://github.com/wtfutil/wtf/archive/refs/tags/v0.41.0.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-340691f26026ce146b6ce8e9468f57d0cd664e82d8969b81b030b315337454bf
+e94b4b22229591462fb54d26a599a7045d6b322c10f30c43aabd6501c9f53af2
 {% endblock %}
 
 {% block go_build_flags %}
