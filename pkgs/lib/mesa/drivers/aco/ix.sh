36a8f64df48459594ed61af549b4cc69f2f8f44e
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-02-24 10:46:29 +0300

    add

--- pkgs/lib/mesa/drivers/aco/ix.sh
+++ pkgs/lib/mesa/drivers/aco/ix.sh
@@ -1,5 +1,7 @@
 {% extends '//die/hub.sh' %}
 
+{# AMD radv + radeonsi + aco shaders #}
+
 {% block lib_deps %}
 lib/mesa/drivers/valve(vulkan=amd,opengl=radeonsi)
 lib/mesa/drivers/opengl(driver=radeonsi)
--- pkgs/lib/mesa/drivers/radeonsi/ix.sh
+++ pkgs/lib/mesa/drivers/radeonsi/ix.sh
@@ -1,5 +1,7 @@
 {% extends '//die/hub.sh' %}
 
+{# classic AMD radv + radeonsi + llvm shaders #}
+
 {% block lib_deps %}
 lib/mesa/drivers/llvm(vulkan=amd,opengl=radeonsi)
 lib/mesa/drivers/opengl(driver=radeonsi)
