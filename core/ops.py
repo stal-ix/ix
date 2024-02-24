8f1880b986dce3c54cdca8beeef12f36ecdd56cb
Anton Samokhvalov <pg@yandex-team.ru>
2024-02-24 23:06:54 +0300

    better

--- core/ops.py
+++ core/ops.py
@@ -5,11 +5,11 @@ import getpass
 
 def construct_impl(cfg, kind):
     if not kind:
-        if getpass.getuser() == 'ix':
-            kind = 'local'
+        kind = os.environ.get('IX_EXEC_KIND', None)
 
     if not kind:
-        kind = os.environ.get('IX_EXEC_KIND', None)
+        if getpass.getuser() == 'ix':
+            kind = 'local'
 
     if not kind:
         if os.path.isfile('/bin/bin_ix/assemble'):
