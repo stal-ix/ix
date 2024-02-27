6d06e04bbc2d4a2710b02d8822486b7b85b98fe0
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-02-27 12:16:37 +0300

    fix

--- pkgs/lib/jpeg/open/ix.sh
+++ pkgs/lib/jpeg/open/ix.sh
@@ -20,5 +20,9 @@ done
 
 {% block env %}
 {{super()}}
-export CPPFLAGS="-I${out}/include/openjpeg-{{self.version()}} \${CPPFLAGS}"
+{#
+./base/sjpx_openjpeg.c:751:5: ?[0m?[0;1;35mwarning: ?[0m?[1m'OPJ_VERSION_MAJOR' is not defined, evaluates to 0 [-Wundef]?[0m
+  751 | #if OPJ_VERSION_MAJOR >= 2 && OPJ_VERSION_MINOR >= 1
+#}
+export CPPFLAGS="-DOPJ_VERSION_MAJOR=2 -DOPJ_VERSION_MINOR=5 -I${out}/include/openjpeg-{{self.version()}} \${CPPFLAGS}"
 {% endblock %}
