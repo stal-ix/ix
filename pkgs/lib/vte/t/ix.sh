60236d79acb67701ea61f27c7bc41d8e0aea4481
Anton Samokhvalov <pg@yandex-team.ru>
2024-02-27 15:10:49 +0300

    better

--- pkgs/lib/vte/t/ix.sh
+++ pkgs/lib/vte/t/ix.sh
@@ -1,10 +1,5 @@
 {% extends '//die/c/meson.sh' %}
 
-{% block fetch %}
-https://gitlab.gnome.org/GNOME/vte/-/archive/0.74.2/vte-0.74.2.tar.bz2
-sha:2a1162738c9bccfac1bb801125c1889d3980d857499909439803cf1def4c25d1
-{% endblock %}
-
 {% block lib_deps %}
 lib/c
 lib/c++
