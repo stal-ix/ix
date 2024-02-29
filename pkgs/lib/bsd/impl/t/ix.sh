86b8212d1d117286ffe0976759811685f72b3b9b
Anton Samokhvalov <pg@yandex-team.ru>
2024-02-29 12:41:38 +0300

    upver

--- pkgs/lib/bsd/impl/t/ix.sh
+++ pkgs/lib/bsd/impl/t/ix.sh
@@ -1,11 +1,11 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://libbsd.freedesktop.org/releases/libbsd-0.11.8.tar.xz
-sha:55fdfa2696fb4d55a592fa9ad14a9df897c7b0008ddb3b30c419914841f85f33
+#https://libbsd.freedesktop.org/releases/libbsd-0.11.8.tar.xz
+#sha:55fdfa2696fb4d55a592fa9ad14a9df897c7b0008ddb3b30c419914841f85f33
 #broke lib/sndio
-#https://libbsd.freedesktop.org/releases/libbsd-0.12.0.tar.xz
-#sha:f741a3bc75162ba19f2f6666076a7961cd75dc93c234e9be4594da1e6f848cfb
+https://libbsd.freedesktop.org/releases/libbsd-0.12.1.tar.xz
+sha:d7747f8ec1baa6ff5c096a9dd587c061233dec90da0f1aedd66d830f6db6996a
 {% endblock %}
 
 {% block bld_tool %}
