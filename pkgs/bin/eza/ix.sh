3a8100a7c2c47a4d7cc8aa4af5804ace42660d49
Anton Samokhvalov <pg@yandex-team.ru>
2024-02-29 12:06:41 +0300

    upver

--- pkgs/bin/eza/ix.sh
+++ pkgs/bin/eza/ix.sh
@@ -1,11 +1,11 @@
 {% extends '//die/rust/cargo.sh' %}
 
 {% block cargo_url %}
-https://github.com/eza-community/eza/archive/refs/tags/v0.18.4.tar.gz
+https://github.com/eza-community/eza/archive/refs/tags/v0.18.5.tar.gz
 {% endblock %}
 
 {% block cargo_sha %}
-b7491b40b5634e608b34edc84abc61beccd1704e65b51e3f00868c1aa73d53e6
+abbc45a8b0fd3f47616a0cb940b82553ee187623f7b91256bcff097c4cb00948
 {% endblock %}
 
 {% block bld_libs %}
