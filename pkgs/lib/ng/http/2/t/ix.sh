a4048b05e4be6dd7e4bc41589340bf1cc903764d
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-01 17:33:57 +0300

    upver

--- pkgs/lib/ng/http/2/t/ix.sh
+++ pkgs/lib/ng/http/2/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://github.com/nghttp2/nghttp2/archive/refs/tags/v1.59.0.tar.gz
-sha:0dd5c980f1262ff5f03676fd99f46f250b66c842f7d864fa1ca9f8453e5f8868
+https://github.com/nghttp2/nghttp2/archive/refs/tags/v1.60.0.tar.gz
+sha:3cc9403c64e0a133868f62132ff0884cd5dc567eee5bd7b2d03ac81293695d6b
 {% endblock %}
 
 {% block lib_deps %}
