ddd0c24f31401ffcb07e297f1869e3d90ef0374d
Anton Samokhvalov <pg@yandex-team.ru>
2024-03-02 00:59:11 +0300

    upver

--- pkgs/bin/lzip/ix.sh
+++ pkgs/bin/lzip/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
-https://download.savannah.gnu.org/releases/lzip/lzip-1.24.tar.gz
-sha:d42659229b10e066eeb6e81eb673cdd893b672e512d26719c2d95975556ca56c
+https://download.savannah.gnu.org/releases/lzip/lzip-1.24.1.tar.gz
+sha:30c9cb6a0605f479c496c376eb629a48b0a1696d167e3c1e090c5defa481b162
 {% endblock %}
 
 {% block bld_libs %}
