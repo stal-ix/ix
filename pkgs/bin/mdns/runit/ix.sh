5b54cba39bd09d73dff01a61765739d6b15f7737
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-02 16:18:06 +0300

    better

--- pkgs/bin/mdns/runit/ix.sh
+++ pkgs/bin/mdns/runit/ix.sh
@@ -3,6 +3,6 @@
 {% block run_deps %}
 etc/user/nobody
 bin/mdns/responder/d
-etc/services/runit(srv_dir=mdnsd,srv_command=chown nobody /var/run/mdnsd; exec mdnsd -debug)
+etc/services/runit(srv_dir=mdnsd,srv_command=chown nobody /var/run/mdnsd; hostname -F /etc/hostname; exec mdnsd -debug)
 etc/services/runit(srv_dir=mdnsd_exporter,srv_user=nobody,srv_command=exec dns-sd -R hostname _tcp local 0)
 {% endblock %}
