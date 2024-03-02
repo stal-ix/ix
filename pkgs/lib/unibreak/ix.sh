c420b60d379031a4a6fd46746a5e970fa6b97570
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-02 16:14:38 +0300

    upver

--- pkgs/lib/unibreak/ix.sh
+++ pkgs/lib/unibreak/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://github.com/adah1972/libunibreak/archive/refs/tags/libunibreak_6_0.tar.gz
-sha:09e9d2f41e1179f9cb9aec60abe768bec66290037a6d35450d2dc002d4d85dff
+https://github.com/adah1972/libunibreak/archive/refs/tags/libunibreak_6_1.tar.gz
+sha:890674996168ef5ba143d80d49ab8b61594a4eb70198dcac76caf6e1bd264a41
 {% endblock %}
 
 {% block lib_deps %}
