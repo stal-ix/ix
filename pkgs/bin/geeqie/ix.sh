e68af025d4d1a5737fdb89e03bb9fcbab32a18e6
Anton Samokhvalov <pg@yandex-team.ru>
2024-03-08 22:27:11 +0300

    upver

--- pkgs/bin/geeqie/ix.sh
+++ pkgs/bin/geeqie/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-http://geeqie.org/cgi-bin/gitweb.cgi?p=geeqie.git;a=snapshot;h=fa7667fb6a2d59f4facb11c083c4d74d0d80f459;sf=tbz2
-sha:261d4b90baa401f01b95b9d55564f542e215cc5c68e0cdee52ee29e62bc41b3a
+http://geeqie.org/cgi-bin/gitweb.cgi?p=geeqie.git;a=snapshot;h=f8c3ab333e95d45d4e68007256362dbdbdb3de3a;sf=tbz2
+sha:41bb7d3177982aeeec0e92f1d969e601042b2e6855db25c6bdd83dd68d503994
 {% endblock %}
 
 {% block bld_libs %}
@@ -18,4 +18,8 @@ lib/archive
 {% block bld_tool %}
 bld/glib
 bld/gettext
+{% endblock %}
+
+{% block cpp_missing %}
+locale.h
 {% endblock %}
