a5ee7899330bf3f6218cd5652606476753b084c5
Anton Samokhvalov <pg@yandex-team.ru>
2024-03-09 21:30:39 +0300

    upver

--- pkgs/bin/niri/unwrap/ix.sh
+++ pkgs/bin/niri/unwrap/ix.sh
@@ -1,11 +1,11 @@
 {% extends '//die/rust/cargo.sh' %}
 
 {% block cargo_url %}
-https://github.com/YaLTeR/niri/archive/refs/tags/v0.1.2.tar.gz
+https://github.com/YaLTeR/niri/archive/1971a41fdd68f0a33bd5db22f776821682c23206.zip
 {% endblock %}
 
 {% block cargo_sha %}
-f4dbde12c6c6208d092d0049093086f69403c620628d92d324efe0bfd9a27f2c
+42c0d89b0c5189637de369641d8425add02785b5bf4b049cbd9816e1481fc355
 {% endblock %}
 
 {% block lib_deps %}
