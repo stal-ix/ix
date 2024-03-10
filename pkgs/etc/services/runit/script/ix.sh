52ec9ecb1fda145dee859962976397831c58c1bc
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-10 21:53:33 +0300

    better

--- pkgs/etc/services/runit/script/ix.sh
+++ pkgs/etc/services/runit/script/ix.sh
@@ -3,21 +3,28 @@
 {% set sd %}{% if srv_dir %}{{srv_dir}}{% else %}user_{{srv_slot}}_{{srv_name}}{% endif %}{% endset %}
 
 {% block install %}
-cd ${out}; mkdir -p etc/services/{{sd}}; cd etc/services/{{sd}}
+cd ${out}
 
-cat << EOF > cmd
+mkdir -p etc/services/{{sd}}
+cd etc/services/{{sd}}
+
+cat << EOF > cmd_1
+{% if srv_user %}
+mkdir -p /var/run/{{sd}}
+chown {{srv_user}} /var/run/{{sd}}
+exec /bin/su -s /bin/sh {{srv_user}} ${PWD}/cmd_2
+{% else %}
+exec /bin/sh ${PWD}/cmd_2
+{% endif %}
+EOF
+
+cat << EOF > cmd_2
 {{srv_command}}
 EOF
 
 cat << EOF > run
 #!/bin/sh
-{% if srv_user %}
-mkdir -p /var/run/{{sd}}
-chown {{srv_user}} /var/run/{{sd}}
-exec srv {{sd}} /bin/su -s /bin/sh {{srv_user}} ${PWD}/cmd
-{% else %}
-exec srv {{sd}} /bin/sh ${PWD}/cmd
-{% endif %}
+exec srv {{sd}} /bin/sh ${PWD}/cmd_1
 EOF
 
 chmod +x run
