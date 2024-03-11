30bdb18acca910208fb97237550f85573b039f6b
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-11 21:22:56 +0300

    add

---
+++ pkgs/bin/kernel/6/8/ver.sh
@@ -0,0 +1,9 @@
+{% block kernel_version %}
+6.8
+{% endblock %}
+
+{% block kernel_sha %}
+c969dea4e8bb6be991bbf7c010ba0e0a5643a3a8d8fb0a2aaa053406f1e965f3
+{% endblock %}
+
+{% include '//bin/kernel/t/ver.sh' %}
