1e0702f2c2fc86de35ceba255b000009c9711ccb
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-12 13:25:00 +0300

    refac

--- pkgs/bld/fetch/cached/ix.sh
+++
@@ -1,6 +0,0 @@
-{% extends '//die/hub.sh' %}
-
-{% block run_deps %}
-bin/aria/2
-bld/fetch/cached/scripts
-{% endblock %}
--- pkgs/lib/zip/t/ix.sh
+++ pkgs/lib/zip/t/ix.sh
@@ -1,15 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
-{% block http_url %}
+{% block fetch %}
 https://libzip.org/download/libzip-1.10.1.tar.xz
-{% endblock %}
-
-{% block http_sha %}
-dc3c8d5b4c8bbd09626864f6bcf93de701540f761d76b85d7c7d710f4bd90318
-{% endblock %}
-
-{% block bld_data %}
-aux/http(http_url={{self.http_url().strip()}},fname={{self.http_url().strip() | basename}},sha={{self.http_sha().strip()}})
+sha:dc3c8d5b4c8bbd09626864f6bcf93de701540f761d76b85d7c7d710f4bd90318
 {% endblock %}
 
 {% block lib_deps %}
