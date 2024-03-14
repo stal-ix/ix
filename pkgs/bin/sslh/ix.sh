04c1f3f6bcfc3faad161210e91a526ddaca07ded
Anton Samokhvalov <pg@yandex-team.ru>
2024-03-14 19:41:50 +0300

    fix

--- pkgs/bin/sslh/ix.sh
+++ pkgs/bin/sslh/ix.sh
@@ -1,8 +1,8 @@
-{% extends '//die/c/make.sh' %}
+{% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
-https://github.com/yrutschle/sslh/archive/refs/tags/v2.0.1.tar.gz
-sha:0e92a53432bb62a45095f5478546fecb65b9013b972c3ee9b405044daf7a19ac
+https://github.com/yrutschle/sslh/archive/refs/tags/v2.1.0.tar.gz
+sha:5c9cdc4f35a0f875b69932ef0fb09abd04c538735ceaf8bb8c97f3efa6e1d02e
 {% endblock %}
 
 {% block bld_libs %}
