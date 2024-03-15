04f2d4aac86a8f22cad295055e9a3149f39f36dc
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-15 17:00:23 +0300

    upver

--- pkgs/lib/dav1d/t/ix.sh
+++ pkgs/lib/dav1d/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://github.com/videolan/dav1d/archive/refs/tags/1.4.0.tar.gz
-sha:0b6d484fc3d20541a21403812499530b72875dee0ec88f682cb1d73943dfc156
+https://github.com/videolan/dav1d/archive/refs/tags/1.4.1.tar.gz
+sha:04da7689748a3501d00b6796b6de0596177019709ecc6095c667b7964895dd8c
 {% endblock %}
 
 {% block lib_deps %}
