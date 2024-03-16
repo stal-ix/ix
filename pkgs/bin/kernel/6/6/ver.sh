ec7baf3ad1c4c2bd98b37bf2f37e6892d7fa1f18
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-16 13:47:42 +0300

    upver

--- pkgs/bin/kernel/6/6/ver.sh
+++ pkgs/bin/kernel/6/6/ver.sh
@@ -1,9 +1,9 @@
 {% block kernel_version %}
-6.6.20
+6.6.22
 {% endblock %}
 
 {% block kernel_sha %}
-e2f6c7f39b304248193370f8c5755553ab73ad5672e92dae994a344084d8dd22
+23e3e7b56407250f5411bdab95763d0bc4e3a19dfa431d951df7eacabd61a2f4
 {% endblock %}
 
 {% include '//bin/kernel/t/ver.sh' %}
