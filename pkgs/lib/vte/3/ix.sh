4ddad2beb635e1fd4b15f473080dc1688b54265b
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-18 20:54:07 +0300

    upver

--- pkgs/lib/vte/3/ix.sh
+++ pkgs/lib/vte/3/ix.sh
@@ -1,12 +1,13 @@
 {% extends '//lib/vte/t/ix.sh' %}
 
 {% block fetch %}
-https://gitlab.gnome.org/GNOME/vte/-/archive/0.74.2/vte-0.74.2.tar.bz2
-sha:2a1162738c9bccfac1bb801125c1889d3980d857499909439803cf1def4c25d1
+https://gitlab.gnome.org/GNOME/vte/-/archive/0.76.0/vte-0.76.0.tar.bz2
+sha:7d235b3699107a308515e2a333d5b3ca55a7568de756b04c28f333e6b5cb735b
 {% endblock %}
 
 {% block lib_deps %}
 {{super()}}
+lib/lz4
 lib/gtk/3
 {% endblock %}
 
