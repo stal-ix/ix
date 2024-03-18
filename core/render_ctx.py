12edfd0582ffd615a9860d194fe84f02264f862c
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-18 23:07:42 +0300

    not now

--- core/render_ctx.py
+++ core/render_ctx.py
@@ -91,7 +91,6 @@ class RenderContext:
             kind: True,
             tp['os']: True,
             tp['arch']: True,
-            'uid': pkg.uid,
         }, pkg.flags)
 
         if args['boot']:
