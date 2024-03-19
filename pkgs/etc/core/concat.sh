7e2325aa739e9d715279cc50c8e5a0b422cbe89f
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-19 15:21:31 +0300

    fix

--- pkgs/etc/core/concat.sh
+++ pkgs/etc/core/concat.sh
@@ -2,7 +2,7 @@ set -xue
 
 cd etc
 
-ls | grep '.*\.d$' | grep -v ananicy | grep -v bash_completion | grep -v grub | grep -v init | while read l; do
+ls | grep '.*\.d$' | grep -v ananicy | grep -v bash_completion | grep -v grub | grep -v init | grep -v xinetd | grep -v pam | grep -v profile | while read l; do
     cat ${l}/* > ${l%.d}
     rm -r ${l}
 done
