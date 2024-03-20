cc8570e4f42d61e3f0158843b08f518f0c0dd1bb
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-21 00:14:01 +0300

    fix

--- pkgs/lib/tiff/ix.sh
+++ pkgs/lib/tiff/ix.sh
@@ -5,3 +5,8 @@
 tiff-opengl=OFF
 tiff-tools=OFF
 {% endblock %}
+
+{% block install %}
+{{super()}}
+sed -e 's|.*INTERFACE_LINK_LIBRARIES.*||' -i ${out}/lib/cmake/tiff/TiffTargets.cmake
+{% endblock %}
