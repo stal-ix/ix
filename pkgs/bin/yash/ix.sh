19474a5f0fb0e72a97b3a4ba9b11ba01b3a1787d
Anton Samokhvalov <pg@yandex-team.ru>
2024-03-21 14:12:12 +0300

    upver

--- pkgs/bin/yash/ix.sh
+++ pkgs/bin/yash/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
-https://github.com/magicant/yash/releases/download/2.55/yash-2.55.tar.xz
-sha:97cd809d5e216b3c4afae42379f1bd4f5082b7c16d51e282d60a5014fbc9e1f6
+https://github.com/magicant/yash/releases/download/2.56.1/yash-2.56.1.tar.xz
+sha:f7f5a1ffd246692568e4823a59b20357317d92663573bd1099254c0c89de71f5
 {% endblock %}
 
 {% block bld_libs %}
