c5d05dce4007eb40e11d3a779c719a7ba6f4bb2e
Anton Samokhvalov <pg@yandex-team.ru>
2024-03-21 14:23:29 +0300

    add

--- pkgs/bin/yash/ix.sh
+++ pkgs/bin/yash/ix.sh
@@ -1,20 +1,9 @@
-{% extends '//die/c/autohell.sh' %}
-
-{% block fetch %}
-https://github.com/magicant/yash/releases/download/2.56.1/yash-2.56.1.tar.xz
-sha:f7f5a1ffd246692568e4823a59b20357317d92663573bd1099254c0c89de71f5
-{% endblock %}
+{% extends 't/ix.sh' %}
 
 {% block bld_libs %}
 lib/c
 lib/edit
 lib/curses
-{% endblock %}
-
-{% block c_rename_symbol %}
-vfree
-wordfree
-add_history
 {% endblock %}
 
 {% block configure %}
