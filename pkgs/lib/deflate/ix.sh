a0c54819927dcc3e658488b385ccdeea58d7855c
Anton Samokhvalov <pg@yandex-team.ru>
2024-03-23 17:49:07 +0300

    upver

--- pkgs/lib/deflate/ix.sh
+++ pkgs/lib/deflate/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/ebiggers/libdeflate/archive/refs/tags/v1.19.tar.gz
-sha:27bf62d71cd64728ff43a9feb92f2ac2f2bf748986d856133cc1e51992428c25
+https://github.com/ebiggers/libdeflate/archive/refs/tags/v1.20.tar.gz
+sha:ed1454166ced78913ff3809870a4005b7170a6fd30767dc478a09b96847b9c2a
 {% endblock %}
 
 {% block lib_deps %}
