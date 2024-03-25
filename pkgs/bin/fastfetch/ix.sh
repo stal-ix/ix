72e98ad8480e4d3efcecb13dbcd66d682637e9c7
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-25 22:09:03 +0300

    upver

--- pkgs/bin/fastfetch/ix.sh
+++ pkgs/bin/fastfetch/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/fastfetch-cli/fastfetch/archive/refs/tags/2.8.9.tar.gz
-sha:36211738354869a274c8b9c145258b86a9156573586e2fba0c5e8410d5a8c469
+https://github.com/fastfetch-cli/fastfetch/archive/refs/tags/2.8.10.tar.gz
+sha:e3ea8388d79cc02ed1a6e45b8e5fec4068507ca03923386bdc9a7b3ff02d2a52
 {% endblock %}
 
 {% block bld_libs %}
