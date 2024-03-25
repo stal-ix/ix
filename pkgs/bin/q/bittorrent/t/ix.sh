8b90d893cf291b097b8a7c45cf4904bf6f6162e0
Anton Samokhvalov <pg@yandex-team.ru>
2024-03-25 23:09:16 +0300

    upver

--- pkgs/bin/q/bittorrent/t/ix.sh
+++ pkgs/bin/q/bittorrent/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/qbittorrent/qBittorrent/archive/refs/tags/release-4.6.3.tar.gz
-sha:9a44d974a5b2cf945ccdecdc20f452d6b8f4800487492945729a493ff6d11502
+https://github.com/qbittorrent/qBittorrent/archive/refs/tags/release-4.6.4.tar.gz
+sha:5842eb2cc1745b72b090df562d35a7ec0a6469d898d411544808731578447e1d
 {% endblock %}
 
 {% block bld_libs %}
