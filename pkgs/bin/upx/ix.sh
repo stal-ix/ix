65fb5f18e562429e74af521e10bc328e82499c4d
Anton Samokhvalov <pg@yandex-team.ru>
2024-03-28 00:52:46 +0300

    upver

--- pkgs/bin/upx/ix.sh
+++ pkgs/bin/upx/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/upx/upx/releases/download/v4.2.2/upx-4.2.2-src.tar.xz
-sha:42ee0455eea610ef7ee732aa1f657b34a351ebcfa64a24c1e2a7aaec74c1e038
+https://github.com/upx/upx/releases/download/v4.2.3/upx-4.2.3-src.tar.xz
+sha:d6357eec6ed4c1b51f40af2316b0958ff1b7fa6f53ef3de12da1d5c96d30e412
 {% endblock %}
 
 {% block bld_libs  %}
