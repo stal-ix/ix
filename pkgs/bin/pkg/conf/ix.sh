6717e2e40e77c238275703eded7dba1bd0fc7767
Anton Samokhvalov <pg@yandex-team.ru>
2024-03-28 21:29:30 +0300

    upver

--- pkgs/bin/pkg/conf/ix.sh
+++ pkgs/bin/pkg/conf/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://github.com/pkgconf/pkgconf/archive/refs/tags/pkgconf-2.1.1.tar.gz
-sha:6a181e0bf1195e95b7cd535a1854827aedb383b26b1fc24ca13586cb5e8e55af
+https://github.com/pkgconf/pkgconf/archive/refs/tags/pkgconf-2.2.0.tar.gz
+sha:2c72cbf37b2d42a9fbf7ed8f0e5432a0b0925481f67995a21ecf77962a6000bc
 {% endblock %}
 
 {% block bld_libs %}
