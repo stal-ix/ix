de142876b2ceb35abe20800a86f776d20b35c07a
Anton Samokhvalov <pg@yandex-team.ru>
2024-03-29 15:11:26 +0300

    upver

--- pkgs/bin/prometheus/t/ix.sh
+++ pkgs/bin/prometheus/t/ix.sh
@@ -1,9 +1,9 @@
 {% extends '//die/go/build.sh' %}
 
 {% block go_url %}
-https://github.com/prometheus/prometheus/archive/refs/tags/v2.51.0.tar.gz
+https://github.com/prometheus/prometheus/archive/refs/tags/v2.51.1.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-023cedbe9e7621edd6f3b16a1a60068782b3e6cf17e3939a92890118af06b0bc
+d3076178a17160dd312cc838872d51dc0afd9bde5466a3fbf137cc194fac87c5
 {% endblock %}
