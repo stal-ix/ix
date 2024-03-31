8b74faeef1873cd48fa82ff8dc476a8374f1008a
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-31 20:07:34 +0300

    upgrade

--- pkgs/bin/nwg/menu/ix.sh
+++ pkgs/bin/nwg/menu/ix.sh
@@ -5,10 +5,14 @@ https://github.com/nwg-piotr/nwg-menu/archive/refs/tags/v0.1.1.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-a03a4c7a12cdaca6d2d97400d8dbc44df8eebd21f5a3a8bb66f0b763a87c2941
+db511009ace82f4bc945541566d71f1cf50298da2b20fcfd2b3cde64370e9ca8
 {% endblock %}
 
 {% block install %}
 mkdir ${out}/bin
 cp nwg-menu ${out}/bin/
+{% endblock %}
+
+{% block go_version %}
+v3
 {% endblock %}
