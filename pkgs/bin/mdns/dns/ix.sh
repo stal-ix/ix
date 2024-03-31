8bd8e1bce686ce23ff1569fe92882e71d1b667ac
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-31 20:12:44 +0300

    upgrade

--- pkgs/bin/mdns/dns/ix.sh
+++ pkgs/bin/mdns/dns/ix.sh
@@ -5,10 +5,14 @@ https://github.com/pg83/mdns2dns/archive/refs/tags/v1.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-d215064bfa78522a7b2618db976a1b396ea32014cfedf079951c203fae317c74
+3c1007d1f94deb6703914b23690b9ac5f6ee45e30306c7c6b392a3cefa46be28
 {% endblock %}
 
 {% block install %}
 mkdir ${out}/bin
 cp mdns2dns ${out}/bin/
+{% endblock %}
+
+{% block go_version %}
+v3
 {% endblock %}
