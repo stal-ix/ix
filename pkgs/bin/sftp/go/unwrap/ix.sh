19d2512bb6f632611862404683760cb4557109f9
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-31 20:13:39 +0300

    upgrade

--- pkgs/bin/sftp/go/unwrap/ix.sh
+++ pkgs/bin/sftp/go/unwrap/ix.sh
@@ -5,10 +5,14 @@ https://github.com/drakkan/sftpgo/archive/refs/tags/v2.5.6.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-34cffe0f78dcf5181fa16461969700b5c024545c4cd27b6ff6212d54c6c5f67a
+1a988fc8195017ee13178c1e3eff222bd2a64163ab1cac55397c02744d0c2a33
 {% endblock %}
 
 {% block install %}
 mkdir ${out}/bin
 cp sftpgo ${out}/bin
+{% endblock %}
+
+{% block go_version %}
+v3
 {% endblock %}
