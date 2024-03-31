5d10b2e71002a949cce70a498a6a2921a9f0d990
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-31 20:10:55 +0300

    upgrade

--- pkgs/bin/bluetuith/ix.sh
+++ pkgs/bin/bluetuith/ix.sh
@@ -5,10 +5,14 @@ https://github.com/darkhz/bluetuith/archive/refs/tags/v0.1.7.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-6856a3b9d4e4bcc944d28dbbd6ed9747400e2a7752b4df5a65be792d9c0058fc
+e9226fc57118d73d2fc7552bd87bf85eb15442c5b277c32159f195dd89b1a25e
 {% endblock %}
 
 {% block install %}
 mkdir ${out}/bin
 cp bluetuith ${out}/bin/
+{% endblock %}
+
+{% block go_version %}
+v3
 {% endblock %}
