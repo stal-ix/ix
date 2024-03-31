b924b1c7b45e4246a990e200fa43769c3543785f
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-31 20:04:06 +0300

    upgrade

--- pkgs/bin/chroma/ix.sh
+++ pkgs/bin/chroma/ix.sh
@@ -5,7 +5,7 @@ https://github.com/alecthomas/chroma/archive/refs/tags/v2.13.0.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-fcd83f231fda82a70db6dba8e88b02ffa752a903c7d843aec395f5bd893649b4
+429e7e69cc44a0aa28fcdbe6c321100614c7c74d3eef053c4cfdb09f7e7ec942
 {% endblock %}
 
 {% block unpack %}
@@ -16,4 +16,8 @@ cd cmd/chroma
 {% block install %}
 mkdir ${out}/bin
 cp chroma ${out}/bin/
+{% endblock %}
+
+{% block go_version %}
+v3
 {% endblock %}
