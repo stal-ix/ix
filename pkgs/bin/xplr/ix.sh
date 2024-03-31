14ca8bf361c641709060d69f64972d4b7318ba51
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-31 19:44:01 +0300

    upgrade

--- pkgs/bin/xplr/ix.sh
+++ pkgs/bin/xplr/ix.sh
@@ -7,7 +7,7 @@ https://github.com/sayanarijit/xplr/archive/refs/tags/v0.21.5.tar.gz
 {% endblock %}
 
 {% block cargo_sha %}
-ffae36ded8b34597ae4b10a736963a51afbd03a4be8a19045fa713f7e58d2d91
+aaef65b91d0fdd616fd373d370164fac68a17f259b5489958586e1e72756b888
 {% endblock %}
 
 {% block bld_tool %}
@@ -18,4 +18,8 @@ bld/make
 {{super()}}
 cat .cargo/config
 rm .cargo/config
+{% endblock %}
+
+{% block cargo_ver %}
+v3
 {% endblock %}
