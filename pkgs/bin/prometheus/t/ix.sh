1239cdb7f5db9670b5a245ba0988586751db096b
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-31 22:50:05 +0300

    fix

--- pkgs/bin/prometheus/t/ix.sh
+++ pkgs/bin/prometheus/t/ix.sh
@@ -5,5 +5,5 @@ https://github.com/prometheus/prometheus/archive/refs/tags/v2.51.1.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-d3076178a17160dd312cc838872d51dc0afd9bde5466a3fbf137cc194fac87c5
+dd8cbc02d6c9040c0a8bee3abd54e99d7260b5c9b6d99e3dab6c5200d2edeae4
 {% endblock %}
