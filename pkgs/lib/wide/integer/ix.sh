c83618a2a21fae2b172046befaf2238f35294aa8
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-31 19:24:33 +0300

    upgrade

--- pkgs/lib/wide/integer/ix.sh
+++ pkgs/lib/wide/integer/ix.sh
@@ -9,10 +9,14 @@ d4ab8f42402d26bade2901fcc83c58a2bb9c5037
 {% endblock %}
 
 {% block git_sha %}
-39a2613ed428fd22dc011c39cb50105552b5635dc309d844022e607840e104af
+02a6a716809968f404092afea30a44c84319bf7b563b304eb3dad156d327124c
 {% endblock %}
 
 {% block lib_deps %}
 lib/c
 lib/c++
+{% endblock %}
+
+{% block git_version %}
+v3
 {% endblock %}
