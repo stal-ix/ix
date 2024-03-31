4aac8f99e1ba1148ee96842d816e23c1dc55092a
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-31 20:14:03 +0300

    upgrade

--- pkgs/bin/ip/get/ix.sh
+++ pkgs/bin/ip/get/ix.sh
@@ -5,7 +5,7 @@ https://github.com/ipfs/ipget/archive/refs/tags/v0.10.0.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-e5832cc3c1485fa0b14bbb89fd2e40d2c356fd5f3f8a30865d91195a23892be2
+1ce26f4c4532e1b7b98d99f0f9f18074c86b6f5ac0aefe928f5dc98fcb3ec22f
 {% endblock %}
 
 {% block go_compiler %}
@@ -15,4 +15,8 @@ bin/go/lang/20
 {% block install %}
 mkdir ${out}/bin
 cp ipget ${out}/bin/
+{% endblock %}
+
+{% block go_version %}
+v3
 {% endblock %}
