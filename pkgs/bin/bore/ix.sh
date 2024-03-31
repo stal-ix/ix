cb8a965aa04aca78f3d28197f5f908a4ed868cb9
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-31 19:45:39 +0300

    upgrade

--- pkgs/bin/bore/ix.sh
+++ pkgs/bin/bore/ix.sh
@@ -5,10 +5,14 @@ https://github.com/ekzhang/bore/archive/refs/tags/v0.5.0.tar.gz
 {% endblock %}
 
 {% block cargo_sha %}
-10a59a3d4b5f83b7a137ac98b395be40c9b05d006a4b0553e362517f1da1f3a4
+5afaad495162d5f82b095ca225ec845abb383248764c82976d74584119d96f99
 {% endblock %}
 
 {% block install %}
 mkdir ${out}/bin
 cp ${tmp}/release/bore ${out}/bin/
+{% endblock %}
+
+{% block cargo_ver %}
+v3
 {% endblock %}
