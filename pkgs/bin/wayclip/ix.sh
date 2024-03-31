fee391e5d0432dd742d430e26c1b6ac52cd38472
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-31 18:12:43 +0300

    upver

--- pkgs/bin/wayclip/ix.sh
+++ pkgs/bin/wayclip/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/make.sh' %}
 
 {% block fetch %}
-https://github.com/noocsharp/wayclip/archive/refs/tags/0.4.1.tar.gz
-sha:c4fa53618b0869595957e146cb1cfb32309dcbec3ad354ff8e3b075f4219cba8
+https://github.com/noocsharp/wayclip/archive/refs/tags/0.4.2.tar.gz
+sha:2a557e5bc713fdc64da5d957001f05330afbc88b2c59bef056623cfd2809816d
 {% endblock %}
 
 {% block bld_libs %}
