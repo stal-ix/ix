b00f921ef6635cef54acac5488db7d391d63424c
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-31 20:12:31 +0300

    upgrade

--- pkgs/bin/pspy/ix.sh
+++ pkgs/bin/pspy/ix.sh
@@ -5,10 +5,14 @@ https://github.com/DominicBreuker/pspy/archive/refs/tags/v1.2.1.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-34977e0284afd516704bfea5881f83ec7eb07d31a7304d7ce819a008b30735a2
+ef501aa19e17c125c95f84e442f235e29e78671615aa559fa7de8903511f9b38
 {% endblock %}
 
 {% block install %}
 mkdir ${out}/bin
 cp pspy ${out}/bin/
+{% endblock %}
+
+{% block go_version %}
+v3
 {% endblock %}
