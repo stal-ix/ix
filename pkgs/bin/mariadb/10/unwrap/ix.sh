36c2e8a3b3702c1ecd93037deeb63786c4a12056
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-31 23:04:04 +0300

    fix

--- pkgs/bin/mariadb/10/unwrap/ix.sh
+++ pkgs/bin/mariadb/10/unwrap/ix.sh
@@ -1,7 +1,7 @@
 {% extends '//bin/mariadb/11/unwrap/ix.sh' %}
 
 {% block git_sha %}
-6cfe1a9525cc8b38d85a842d40567ccee9c7075ea67a4a63eea08386955e150d
+44941ec1ca2b4e6b5b95ef7c57602e20ea694df7409256571105f6a8fe413c3e
 {% endblock %}
 
 {% block git_branch %}
