e22d933a4c6d7542e74c445645a9386395f89f77
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-31 19:52:48 +0300

    upgrade

--- pkgs/bin/cosmic/term/ix.sh
+++ pkgs/bin/cosmic/term/ix.sh
@@ -5,5 +5,9 @@ https://github.com/pop-os/cosmic-term/archive/90179d5a5f837b60406bcd20315bb8a81c
 {% endblock %}
 
 {% block cargo_sha %}
-9fa8d4b1e1982e4c9589a0e8222bb8b9a93b5e424979044b7cdf3b165a9c971b
+d68aa5d12eadee32bac71e8fb52fed47ded5602b182cfd147e2d28fa39740000
+{% endblock %}
+
+{% block cargo_ver %}
+v3
 {% endblock %}
