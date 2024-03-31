8bdf5bab7378fcd9ff0ebd32e52c50c66a9b4e0d
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-31 20:06:46 +0300

    upgrade

--- pkgs/bin/lf/ix.sh
+++ pkgs/bin/lf/ix.sh
@@ -5,10 +5,14 @@ https://github.com/gokcehan/lf/archive/refs/tags/r31.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-3e3af15bdcb80eb6f26515dc37f4ed7605e99c2bc5171cf14c1bff28267b38bf
+52b34f2cab6fd8451186944c7e65f0e5023d5dd2061bb73df33e2ac191ec44c5
 {% endblock %}
 
 {% block install %}
 mkdir ${out}/bin
 cp lf ${out}/bin/
+{% endblock %}
+
+{% block go_version %}
+v3
 {% endblock %}
