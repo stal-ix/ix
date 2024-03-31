835702a49badb386115f17fb2e3dc7a8b1a57299
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-31 19:22:36 +0300

    upgrade

--- pkgs/lib/ini/parser/ix.sh
+++ pkgs/lib/ini/parser/ix.sh
@@ -9,7 +9,7 @@ deb85ad4936d4ca32cc2260ce43323d47936410d
 {% endblock %}
 
 {% block git_sha %}
-82e3e13cfe3b3e41c01dc1cf03965c95a0e1ab2217a10348e6d3bf9e06a3306f
+a5876c2b204a43f34b5f3d664067003ddb063138b9bc5d55001865c97159dddb
 {% endblock %}
 
 {% block lib_deps %}
@@ -26,4 +26,8 @@ cp src/*.h ${out}/include/
 cp *.a ${out}/lib/
 cp *.pc ${out}/lib/pkgconfig/
 sed -e "s|/usr|${out}|" -i ${out}/lib/pkgconfig/iniparser.pc
+{% endblock %}
+
+{% block git_version %}
+v3
 {% endblock %}
