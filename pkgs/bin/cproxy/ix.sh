4eafaf87809c756b44871cd1d6a6da7784559963
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-31 19:45:49 +0300

    upgrade

--- pkgs/bin/cproxy/ix.sh
+++ pkgs/bin/cproxy/ix.sh
@@ -5,10 +5,14 @@ https://github.com/NOBLES5E/cproxy/archive/refs/tags/v4.1.6.post1.tar.gz
 {% endblock %}
 
 {% block cargo_sha %}
-08927476e8e1930f9cd2383b7770dc47bbb709a28e327ab7da9a8706eab9cfbb
+37e95b716448e30944030fb4e5d7c679ae039dd97394d9fe43a0d49166ad8a7a
 {% endblock %}
 
 {% block install %}
 mkdir ${out}/bin
 cp ${tmp}/release/cproxy ${out}/bin/
+{% endblock %}
+
+{% block cargo_ver %}
+v3
 {% endblock %}
