c5f2507a854ba52b6af3bf201179c2b788c7cbbc
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-31 20:29:14 +0300

    better

--- pkgs/bld/r/w/ix.sh
+++
@@ -1,9 +0,0 @@
-{% extends '//die/proxy.sh' %}
-
-{% block install %}
-mkdir ${out}/bin
-base64 -d << EOF > ${out}/bin/chrw
-{% include 'chrw/base64' %}
-EOF
-chmod +x ${out}/bin/*
-{% endblock %}
--- pkgs/bld/realm/materialize/ix.sh
+++ pkgs/bld/realm/materialize/ix.sh
@@ -3,7 +3,6 @@
 {% block run_deps %}
 bld/sh
 bld/box
-bld/r/w
 bin/rsync
 bld/python
 bld/glib/old
@@ -14,6 +13,10 @@ cd ${out}; mkdir bin; cd bin
 
 base64 -d << EOF > prepare_realm
 {% include 'realm.py/base64' %}
+EOF
+
+base64 -d << EOF > chrw
+{% include 'chrw/base64' %}
 EOF
 
 chmod +x *
