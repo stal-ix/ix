57bb731cbd702b26a85d7aa386e8de77721e0b2d
Anton Samokhvalov <pg@yandex-team.ru>
2024-04-02 02:15:41 +0300

    upver

--- pkgs/bin/lf/ix.sh
+++ pkgs/bin/lf/ix.sh
@@ -1,11 +1,11 @@
 {% extends '//die/go/build.sh' %}
 
 {% block go_url %}
-https://github.com/gokcehan/lf/archive/refs/tags/r31.tar.gz
+https://github.com/gokcehan/lf/archive/refs/tags/r32.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-52b34f2cab6fd8451186944c7e65f0e5023d5dd2061bb73df33e2ac191ec44c5
+aac62aafaaa338517379e4177b6bec4f562c7e5f79254c6ebf1194505c78db4e
 {% endblock %}
 
 {% block install %}
