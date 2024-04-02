a83d0ec7ee7baeb39df8d2335e45a6722e523a04
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-03 00:32:35 +0300

    upver

--- pkgs/lib/msgpack/c/ix.sh
+++ pkgs/lib/msgpack/c/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/msgpack/msgpack-c/archive/refs/tags/c-6.0.0.tar.gz
-sha:af6f3cf25edb220aa2140b09bb5bdd73ddf00938194bd94ebe5c92090cccb466
+https://github.com/msgpack/msgpack-c/archive/refs/tags/c-6.0.1.tar.gz
+sha:58d5fe49d0ee2b374d60a61aabf8028b2c92004e6f11bff04e74b639fc8ad541
 {% endblock %}
 
 {% block lib_deps %}
