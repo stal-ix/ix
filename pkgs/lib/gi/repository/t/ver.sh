08a39e9e100c7f90d4cb7f2f9aadf899326eeb81
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-02 03:24:22 +0300

    revert

--- pkgs/lib/gi/repository/t/ver.sh
+++ pkgs/lib/gi/repository/t/ver.sh
@@ -1,2 +1,5 @@
-https://gitlab.gnome.org/GNOME/gobject-introspection/-/archive/1.80.1/gobject-introspection-1.80.1.tar.bz2
-sha:6d5eca17cac69b2ed88c1ab59405e2ca38b330fcf6dfcad88c30969af69682a5
+https://gitlab.gnome.org/GNOME/gobject-introspection/-/archive/1.78.1/gobject-introspection-1.78.1.tar.bz2
+sha:a18862a00bb7eed6a3f04e2952737ec814699acc85372ae476534990302c5e0c
+#broke telegram build
+#https://gitlab.gnome.org/GNOME/gobject-introspection/-/archive/1.80.1/gobject-introspection-1.80.1.tar.bz2
+#sha:6d5eca17cac69b2ed88c1ab59405e2ca38b330fcf6dfcad88c30969af69682a5
