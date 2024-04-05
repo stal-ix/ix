643ef0b75f25c150d369da9b51718ac1ea791ce5
Anton Samokhvalov <pg@yandex-team.ru>
2024-04-05 12:39:23 +0300

    upver

--- pkgs/bin/nasm/ix.sh
+++ pkgs/bin/nasm/ix.sh
@@ -1,8 +1,13 @@
 {% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
+{% if std_box or std_env %}
 https://www.nasm.us/pub/nasm/releasebuilds/2.16.01/nasm-2.16.01.tar.gz
 sha:d833bf0f5716e89dbcd345b7f545f25fe348c6e2ef16dbc293e1027bcd22d881
+{% else %}
+https://www.nasm.us/pub/nasm/releasebuilds/2.16.02/nasm-2.16.02.tar.gz
+sha:3ab7ca964f741fc05efc50689382f6730fce96bc73e4e74e5fcce66cc973abce
+{% endif %}
 {% endblock %}
 
 {% block bld_libs %}
