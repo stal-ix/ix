624c71d68eb0786d3fded7bffee41929d82a7f4b
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-05 22:13:19 +0300

    add

---
+++ pkgs/lib/firmware/new/ix.sh
@@ -0,0 +1,6 @@
+{% extends '//lib/firmware/ix.sh' %}
+
+{% block fetch %}
+https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/snapshot/linux-firmware-20240312.tar.gz
+sha:89cbac35d1bd21ebf64936d764ccd01d4e0b6cde973e3b940f8ad2bac9086ec8
+{% endblock %}
