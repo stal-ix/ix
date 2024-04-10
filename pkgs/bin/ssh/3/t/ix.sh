b91569711725148fa8a99deaeaf74c6730ee3d0c
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-10 22:53:43 +0300

    upver

--- pkgs/bin/ssh/3/t/ix.sh
+++ pkgs/bin/ssh/3/t/ix.sh
@@ -1,9 +1,9 @@
 {% extends '//die/go/build.sh' %}
 
 {% block go_url %}
-https://github.com/francoismichel/ssh3/archive/refs/tags/v0.1.6.tar.gz
+https://github.com/francoismichel/ssh3/archive/refs/tags/v0.1.7.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-52f1182e5294e171531bf9f90eb161d3d23a72be9866100fc243ca9c4f1d214c
+dbc610014112ee1eca99b7ef04b9d272b765dd5cb1383c773ad2cebf4679bff8
 {% endblock %}
