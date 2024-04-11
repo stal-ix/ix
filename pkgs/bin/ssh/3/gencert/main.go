a888f3e9f0a681a43915e03ead6d19e6bd55fee6
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-11 11:50:01 +0300

    useful tool

---
+++ pkgs/bin/ssh/3/gencert/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"crypto"
+	"crypto/ed25519"
+	"crypto/rand"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"math/big"
+	"os"
+	"time"
+)
+
+func GenerateCert(priv crypto.PrivateKey) (*x509.Certificate) {
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		panic(err)
+	}
+
+	cert := x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			Organization: []string{"SSH3Organization"},
+		},
+		NotBefore: time.Now(),
+		NotAfter:  time.Now().Add(time.Hour * 24 * 365 * 10),
+
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+		DNSNames:              []string{"*", "selfsigned.ssh3"},
+		IsCA:                  true,
+	}
+
+	return &cert
+}
+
+func DumpCertAndKeyToFiles(cert *x509.Certificate, pubkey crypto.PublicKey, privkey crypto.PrivateKey, certPath, keyPath string) {
+	certBytes, err := x509.CreateCertificate(rand.Reader, cert, cert, pubkey, privkey)
+	if err != nil {
+		panic(err)
+	}
+	certFile, err := os.Create(certPath)
+	if err != nil {
+		panic(err)
+	}
+	defer certFile.Close()
+	err = pem.Encode(certFile, &pem.Block{Type: "CERTIFICATE", Bytes: certBytes})
+	if err != nil {
+		panic(err)
+	}
+	keyFile, err := os.OpenFile(keyPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		panic(err)
+	}
+	defer keyFile.Close()
+
+	keyBytes, err := x509.MarshalPKCS8PrivateKey(privkey)
+	if err != nil {
+		panic(err)
+	}
+	err = pem.Encode(keyFile, &pem.Block{Type: "PRIVATE KEY", Bytes: keyBytes})
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	pubkey, privkey, err := ed25519.GenerateKey(rand.Reader)
+	if err != nil {
+	    panic(err)
+	}
+	cert := GenerateCert(privkey)
+	DumpCertAndKeyToFiles(cert, pubkey, privkey, "cert", "key")
+}
