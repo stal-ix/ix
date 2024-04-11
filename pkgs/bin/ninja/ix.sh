21aab011bce399ee4a5b9a6d6a0d8c0380f5e2cb
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-12 01:58:33 +0300

    upver

--- pkgs/bin/ninja/ix.sh
+++ pkgs/bin/ninja/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/ninja-build/ninja/archive/refs/tags/v1.11.1.tar.gz
-sha:31747ae633213f1eda3842686f83c2aa1412e0f5691d1c14dbbcc67fe7400cea
+https://github.com/ninja-build/ninja/archive/refs/tags/v1.12.0.tar.gz
+sha:8b2c86cd483dc7fcb7975c5ec7329135d210099a89bc7db0590a07b0bbfe49a5
 {% endblock %}
 
 {% block bld_libs %}
