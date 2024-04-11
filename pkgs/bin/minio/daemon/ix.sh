faf477303a7b9c12c8b7c3bb4152cb5be87a8180
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-12 01:56:45 +0300

    add

---
+++ pkgs/bin/minio/daemon/ix.sh
@@ -0,0 +1,14 @@
+{% extends '//die/go/build.sh' %}
+
+{% block go_url %}
+https://github.com/minio/minio/archive/refs/tags/RELEASE.2024-04-06T05-26-02Z.tar.gz
+{% endblock %}
+
+{% block go_sha %}
+ebbff8def580f28e6435a585bc2d24b3cbb58c5610f39a5b8492663f2d9eb218
+{% endblock %}
+
+{% block install %}
+mkdir ${out}/bin
+cp minio ${out}/bin/
+{% endblock %}
--- pkgs/bin/minio/ix.sh
+++ pkgs/bin/minio/ix.sh
@@ -1,14 +1,7 @@
-{% extends '//die/go/build.sh' %}
+{% extends '//die/hub.sh' %}
 
-{% block go_url %}
-https://github.com/minio/minio/archive/refs/tags/RELEASE.2024-04-06T05-26-02Z.tar.gz
+{% block run_deps %}
+bin/minio/client
+bin/minio/daemon
 {% endblock %}
 
-{% block go_sha %}
-ebbff8def580f28e6435a585bc2d24b3cbb58c5610f39a5b8492663f2d9eb218
-{% endblock %}
-
-{% block install %}
-mkdir ${out}/bin
-cp minio ${out}/bin/
-{% endblock %}
