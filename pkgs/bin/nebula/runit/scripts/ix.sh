1f1790a1a38edce1fad0b2e21420549e49e8f45a
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-16 02:15:26 +0300

    better

--- pkgs/bin/nebula/runit/scripts/ix.sh
+++ pkgs/bin/nebula/runit/scripts/ix.sh
@@ -4,15 +4,15 @@
 mkdir -p ${out}/etc/nebula
 
 base64 -d << EOF > ${out}/etc/nebula/ca
-{{nebula_ca}}
+{{nebula_ca | defined('nebula_ca')}}
 EOF
 
 base64 -d << EOF > ${out}/etc/nebula/crt
-{{nebula_crt}}
+{{nebula_crt | defined('nebula_crt')}}
 EOF
 
 base64 -d << EOF > ${out}/etc/nebula/key
-{{nebula_key}}
+{{nebula_key | defined('nebula_key')}}
 EOF
 
 cat << EOF > ${out}/etc/nebula/config.yaml
@@ -23,6 +23,6 @@ pki:
 EOF
 
 base64 -d << EOF >> ${out}/etc/nebula/config.yaml
-{{nebula_config}}
+{{nebula_config | defined('nebula_config')}}
 EOF
 {% endblock %}
