c8a04186594bef7f23c923338720702eaa7f170c
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-17 01:08:58 +0300

    better

--- pkgs/bin/sud/ix.sh
+++ pkgs/bin/sud/ix.sh
@@ -5,5 +5,5 @@ bin/setcwd
 bin/sud/client
 bin/sud/scripts
 bin/sud/server(purec=,libc=,allocator=scudo)
-etc/services/runit(srv_dir=sud,srv_command=exec sud_server -R -F -E -s -g -j -k -m -p 127.0.0.1:1 -P dropbear.pid)
+etc/services/runit(srv_dir=sud,srv_command=exec sud_server -R -F -E -s -g -j -k -m -p 127.0.0.1:1 -P /proc/self/fd/1)
 {% endblock %}
