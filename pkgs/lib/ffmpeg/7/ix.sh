fd7f8243901828416c763fdeb411fd6315a51611
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-18 00:05:07 +0300

    add

---
+++ pkgs/lib/ffmpeg/7/ix.sh
@@ -0,0 +1,6 @@
+{% extends '//lib/ffmpeg/6/ix.sh' %}
+
+{% block fetch %}
+https://ffmpeg.org/releases/ffmpeg-7.0.tar.xz
+sha:4426a94dd2c814945456600c8adfc402bee65ec14a70e8c531ec9a2cd651da7b
+{% endblock %}
