989d558dc9367b4754e5a5b5db4dec24b3341cd6
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-17 23:09:17 +0300

    upver

--- pkgs/bin/yambar/ix.sh
+++ pkgs/bin/yambar/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://codeberg.org/dnkl/yambar/archive/1.10.0.tar.gz
-sha:ebf08a17b77053f16038c1538fe9d410d06289b8ea28ff5a5022aca8e9b84807
+https://codeberg.org/dnkl/yambar/archive/1.11.0.tar.gz
+sha:3649878ae7282203b4ff2aa9f45afad32a6fc8c921d94b6abdac2a1d21045aaf
 {% endblock %}
 
 {% block bld_libs %}
@@ -28,11 +28,6 @@ backend-x11=disabled
 backend-wayland=enabled
 {% endblock %}
 
-{% block patch %}
-(base64 -d | patch -p1) << EOF
-{% include '0.diff/base64' %}
-EOF
-
-sed -e 's|.*export_dyn.*||' -i meson.build
-sed -e 's|#error|#warning|' -i char32.c
+{% block build_flags %}
+wrap_cc
 {% endblock %}
