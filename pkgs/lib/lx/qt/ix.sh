559d64614d4f4c785cf1695c4099ba0d94f6178a
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-20 16:28:50 +0300

    add

---
+++ pkgs/lib/lx/qt/ix.sh
@@ -0,0 +1,25 @@
+{% extends '//die/c/cmake.sh' %}
+
+{# hard X11 dep #}
+
+{% block fetch %}
+https://github.com/lxqt/liblxqt/archive/refs/tags/2.0.0.tar.gz
+sha:464090c3e3eba5204c59500b2585c4210b44f9438438864fcceb3d09669611a5
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+lib/c++
+lib/qt/xdg
+lib/qt/6/base
+lib/k/window/system
+{% endblock %}
+
+{% block bld_tool %}
+bld/lxqt
+bld/qt/6/tools
+{% endblock %}
+
+{% block cmake_flags %}
+BUILD_BACKLIGHT_LINUX_BACKEND=OFF
+{% endblock %}
