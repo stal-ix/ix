ba61581869e3acc9b3b51f64c3c52e525e4f713a
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-20 15:37:11 +0300

    better

--- pkgs/bin/e2fsprogs/ix.sh
+++ pkgs/bin/e2fsprogs/ix.sh
@@ -1,1 +1,8 @@
 {% extends '//lib/e2fsprogs/ix.sh' %}
+
+{% block configure_flags %}
+{{super()}}
+--sysconfdir=${out}/share/e2fs
+--enable-symlink-install
+--enable-relative-symlinks
+{% endblock %}
