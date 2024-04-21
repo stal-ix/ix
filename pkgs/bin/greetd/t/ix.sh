e7e48074bf04f762c73d22102df43662dc3e9827
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-21 16:40:31 +0300

    fix

--- pkgs/bin/greetd/t/ix.sh
+++ pkgs/bin/greetd/t/ix.sh
@@ -5,5 +5,5 @@ https://git.sr.ht/~kennylevinsen/greetd/archive/0.9.0.tar.gz
 {% endblock %}
 
 {% block cargo_sha %}
-d29ad6351a1af22c3079d8c8190f2ae4143e66205c5c9d4cd60939597f989502
+b85455ba2a558795c7e9b171122218520bcb0589268ea9bdb59fd27d1ed6d641
 {% endblock %}
