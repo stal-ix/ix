18fa6b48613dec8e84c5ebe2cc3c495d3adabb34
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-23 12:20:45 +0300

    upver

--- pkgs/bin/nmap/ix.sh
+++ pkgs/bin/nmap/ix.sh
@@ -1,16 +1,17 @@
 {% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
-https://nmap.org/dist/nmap-7.93.tar.bz2
-sha:55bcfe4793e25acc96ba4274d8c4228db550b8e8efd72004b38ec55a2dd16651
+https://nmap.org/dist/nmap-7.95.tar.bz2
+sha:e14ab530e47b5afd88f1c8a2bac7f89cd8fe6b478e22d255c5b9bddb7a1c5778
 {% endblock %}
 
 {% block bld_libs %}
 lib/c
 lib/z
-lib/pcre
-lib/kernel
 lib/ssh/2
+lib/kernel
+lib/pcre/2
 lib/linear
 lib/openssl
 {% endblock %}
+
