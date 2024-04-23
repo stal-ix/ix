dea4885202980a2305ec484c0dd99c10f9c84c9f
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-23 13:35:15 +0300

    upver

--- pkgs/lib/xml/b/t/ix.sh
+++ pkgs/lib/xml/b/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://github.com/hughsie/libxmlb/archive/refs/tags/0.3.18.tar.gz
-sha:3798be088653a939b8516900eb5bd9491eda8975c5b9886434c670fa79cc56aa
+https://github.com/hughsie/libxmlb/archive/refs/tags/0.3.19.tar.gz
+sha:7050a3e43c4e1f53020cfbdfcb6fd5dc664c6c240faceb5ae3c8830423918262
 {% endblock %}
 
 {% block lib_deps %}
