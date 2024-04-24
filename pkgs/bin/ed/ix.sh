4f6ef745a8fd2a391d7b998ef61a47668b914a66
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-24 21:45:54 +0300

    upver

--- pkgs/bin/ed/ix.sh
+++ pkgs/bin/ed/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
-https://ftp.gnu.org/gnu/ed/ed-1.20.1.tar.lz
-sha:b1a463b297a141f9876c4b1fcd01477f645cded92168090e9a35db2af4babbca
+https://ftp.gnu.org/gnu/ed/ed-1.20.2.tar.lz
+sha:65fec7318f48c2ca17f334ac0f4703defe62037bb13cc23920de077b5fa24523
 {% endblock %}
 
 {% block bld_libs %}
