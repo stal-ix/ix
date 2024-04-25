8da1e4383bf39868303b061cc36f17a7d30b1b94
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-25 23:00:00 +0300

    upver

--- pkgs/lib/sd/bus/c++/t/ix.sh
+++ pkgs/lib/sd/bus/c++/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/Kistler-Group/sdbus-cpp/archive/refs/tags/v1.5.0.tar.gz
-sha:577986929f911320fb9ef6a3e2badd464dc38411ebc25d2966f5cb85c39f0897
+https://github.com/Kistler-Group/sdbus-cpp/archive/refs/tags/v1.6.0.tar.gz
+sha:7ec8a2565bfc8f975c7ee528cb292021063ed793d6864c1c8733ca10ff906164
 {% endblock %}
 
 {% block lib_deps %}
