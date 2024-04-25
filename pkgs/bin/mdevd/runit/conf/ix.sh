c6a5016a911bf2c36370f1662153d89732f68fc5
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-25 12:21:04 +0300

    better

--- pkgs/bin/mdevd/runit/conf/ix.sh
+++ pkgs/bin/mdevd/runit/conf/ix.sh
@@ -11,22 +11,16 @@ echo >> mdev.conf
 
 mkdir -p runit/1.d; cd runit/1.d
 
-cat << EOF > 01-mount-ro.sh
-# mount ro
-mount -t sysfs sysfs /sys
-mount -t debugfs none /sys/kernel/debug
-mount -t proc proc /proc
-mount -t cgroup2 none /sys/fs/cgroup
+cat << EOF > 01-01-mount-ro.sh
 # mount dev
 mount -t tmpfs tmpfs /dev
 mkdir /dev/pts /dev/shm
 mount -t devpts devpts /dev/pts
 mount -t tmpfs shmfs /dev/shm
-# populate some nodes
-ln -s /proc/self/fd/0 /dev/stdin
-ln -s /proc/self/fd/1 /dev/stdout
-ln -s /proc/self/fd/2 /dev/stderr
+EOF
+
+cat << EOF > 01-03-mount-ro.sh
 # setup /dev
-mdev -s
+mdevd-coldplug
 EOF
 {% endblock %}
