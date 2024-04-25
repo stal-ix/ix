c188c3c45e302afe66507f4039779feb7ed35d74
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-25 18:25:05 +0300

    upver

--- pkgs/lib/fribidi/t/ix.sh
+++ pkgs/lib/fribidi/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson_cross.sh' %}
 
 {% block fetch %}
-https://github.com/fribidi/fribidi/archive/refs/tags/v1.0.13.tar.gz
-sha:f24e8e381bcf76533ae56bd776196f3a0369ec28e9c0fdb6edd163277e008314
+https://github.com/fribidi/fribidi/archive/refs/tags/v1.0.14.tar.gz
+sha:7b1b3837f6583432068c71ca333a8988c803e9b94259edbd37c85367bbc51446
 {% endblock %}
 
 {% block lib_deps %}
