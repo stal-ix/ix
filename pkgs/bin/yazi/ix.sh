3d908fc08c9faf96ad8e0da86fba47f58fc8e79e
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-26 14:57:17 +0300

    add

--- pkgs/bin/yazi/ix.sh
+++ pkgs/bin/yazi/ix.sh
@@ -1,18 +1,5 @@
-{% extends '//die/rust/cargo.sh' %}
+{% extends '//die/hub.sh' %}
 
-{% block cargo_url %}
-https://github.com/sxyazi/yazi/archive/refs/tags/v0.2.5.tar.gz
-{% endblock %}
-
-{% block cargo_sha %}
-5eff3a8625641587967ddf771dfd13e8e2c24c5b158fa0721ddb9af69b062a0a
-{% endblock %}
-
-{% block bld_libs %}
-lib/oniguruma
-{% endblock %}
-
-{% block unpack %}
-{{super()}}
-cd yazi-fm
+{% block run_deps %}
+bin/yazi/unwrap(allocator=jemalloc)
 {% endblock %}
--- pkgs/set/ci/unwrap/linux/x86_64/ix.sh
+++ pkgs/set/ci/unwrap/linux/x86_64/ix.sh
@@ -37,6 +37,7 @@
 #}
 
 {% block run_deps %}
+bin/yazi
 bin/util/wipefs
 bin/bcache/tools
 bin/lxqt/askpass
