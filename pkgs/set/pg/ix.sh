9e52df229835d33b1f31cf117344b22b1af6500a
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-26 23:12:20 +0300

    better

--- pkgs/set/pg/ix.sh
+++ pkgs/set/pg/ix.sh
@@ -5,7 +5,11 @@
 
 {% block run_deps %}
 bin/mc
+bin/git
+bin/mpv
 bin/ted
+bin/mako
+bin/grim
 bin/wget
 bin/curl
 bin/htop
@@ -13,11 +17,21 @@ bin/niri
 bin/bash
 bin/less
 set/debug
-bin/ssh/3
+bin/fuzzel
+bin/python
 bin/waybar
+bin/evince
 bin/iwd/ctl
+bin/swayimg
+bin/epiphany
+bin/telegram
+bin/xdg/open
 bin/alacritty
-set/stalix/gui
-aux/fonts/inter
+bin/fontconfig
 bin/brightnessctl
+{% endblock %}
+
+{% block run_data %}
+aux/fonts/inter
+set/fonts/default
 {% endblock %}
