09c4b292764e66300ea5a853d65dfa8a0251a55e
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-28 12:57:12 +0300

    upver

--- pkgs/lib/jasper/t/ix.sh
+++ pkgs/lib/jasper/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/jasper-software/jasper/archive/refs/tags/version-4.2.3.tar.gz
-sha:1263d70c663f1b5a4ed2a428371486091d282af96b7270e8b1cbfcbf44ad95ff
+https://github.com/jasper-software/jasper/archive/refs/tags/version-4.2.4.tar.gz
+sha:23a3d58cdeacf3abdf9fa1d81dcefee58da6ab330940790c0f27019703bfd2cd
 {% endblock %}
 
 {% block lib_deps %}
