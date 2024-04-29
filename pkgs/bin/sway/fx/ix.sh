85c14afa97a302eba2a10cb024d760641a3d170f
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-30 00:13:48 +0300

    fix

--- pkgs/bin/sway/fx/ix.sh
+++ pkgs/bin/sway/fx/ix.sh
@@ -1,11 +1,5 @@
-{% extends '//bin/sway/compositor/ix.sh' %}
+{% extends '//die/hub.sh' %}
 
-{% block fetch %}
-https://github.com/WillPower3309/swayfx/archive/refs/tags/0.4.tar.gz
-sha:fa164734a7b32fd82f31e54c407b147e92247ef275de9df4a87b6198a36f20e2
-{% endblock %}
-
-{% block bld_libs %}
-lib/scene/fx
-{{super()}}
+{% block run_deps %}
+bin/sway/fx/unwrap(allocator=tcmalloc)
 {% endblock %}
