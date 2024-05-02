a17af650f13de286231bc9952a8695c7f146baef
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-05-02 22:31:35 +0300

    upver

--- pkgs/lib/exiv2/t/ix.sh
+++ pkgs/lib/exiv2/t/ix.sh
@@ -3,8 +3,8 @@
 {% block fetch %}
 #https://github.com/Exiv2/exiv2/archive/refs/tags/v0.28.0.tar.gz
 #sha:04c0675caf4338bb96cd09982f1246d588bcbfe8648c0f5a30b56c7c496f1a0b
-https://github.com/Exiv2/exiv2/archive/refs/tags/v0.27.6.tar.gz
-sha:f16ee5ff08b6994c66106109417857f13e711fca100ac43c6a403d4f02b59602
+https://github.com/Exiv2/exiv2/archive/refs/tags/v0.28.2.tar.gz
+sha:543bead934135f20f438e0b6d8858c55c5fcb7ff80f5d1d55489965f1aad58b9
 {% endblock %}
 
 {% block lib_deps %}
