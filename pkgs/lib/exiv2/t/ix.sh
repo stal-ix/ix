1a5eeecd9d3ae7f54991d35f92b62e77ed6b7624
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-05-02 22:44:11 +0300

    revert

--- pkgs/lib/exiv2/t/ix.sh
+++ pkgs/lib/exiv2/t/ix.sh
@@ -1,10 +1,11 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-#https://github.com/Exiv2/exiv2/archive/refs/tags/v0.28.0.tar.gz
-#sha:04c0675caf4338bb96cd09982f1246d588bcbfe8648c0f5a30b56c7c496f1a0b
-https://github.com/Exiv2/exiv2/archive/refs/tags/v0.28.2.tar.gz
-sha:543bead934135f20f438e0b6d8858c55c5fcb7ff80f5d1d55489965f1aad58b9
+https://github.com/Exiv2/exiv2/archive/refs/tags/v0.28.0.tar.gz
+sha:04c0675caf4338bb96cd09982f1246d588bcbfe8648c0f5a30b56c7c496f1a0b
+# broken darktable
+#https://github.com/Exiv2/exiv2/archive/refs/tags/v0.28.2.tar.gz
+#sha:543bead934135f20f438e0b6d8858c55c5fcb7ff80f5d1d55489965f1aad58b9
 {% endblock %}
 
 {% block lib_deps %}
