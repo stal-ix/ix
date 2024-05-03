e8e17ec5af82dc92b1052fd77ef06665b3b3c713
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-05-03 12:14:29 +0300

    better

--- pkgs/bin/ix/assemble/env/ix.sh
+++ pkgs/bin/ix/assemble/env/ix.sh
@@ -2,6 +2,11 @@
 
 {% block install %}
 mkdir -p ${out}/etc/env.d
+
+cat << EOF > ${out}/etc/env.d/ix_root.sh
+export IX_ROOT=/ix
+EOF
+
 cat << EOF > ${out}/etc/env.d/ix_exec_kind.sh
 export IX_EXEC_KIND=system
 EOF
