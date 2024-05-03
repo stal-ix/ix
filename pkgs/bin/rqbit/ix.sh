fe87370c2843e5ade62e1109876fb37583923b76
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-05-03 20:17:35 +0300

    add

---
+++ pkgs/bin/rqbit/ix.sh
@@ -0,0 +1,18 @@
+{% extends '//die/rust/cargo.sh' %}
+
+{% block cargo_url %}
+https://github.com/ikatson/rqbit/archive/refs/tags/v5.6.4.tar.gz
+{% endblock %}
+
+{% block cargo_sha %}
+5597584d7db116b918f5a13c1d2a15b5fb1514dcc4262ac5caf841104784fbbc
+{% endblock %}
+
+{% block install %}
+mkdir ${out}/bin
+cp ${tmp}/release/rqbit ${out}/bin/
+{% endblock %}
+
+{% block bld_libs %}
+lib/openssl
+{% endblock %}
