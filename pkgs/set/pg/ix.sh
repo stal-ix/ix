a2e546e9f6c5e0e0634c9985e04bc684b1601a67
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-05-04 22:19:26 +0300

    realm

--- pkgs/set/pg/ix.sh
+++ pkgs/set/pg/ix.sh
@@ -1,5 +1,9 @@
 {% extends '//die/hub.sh' %}
 
 {% block run_deps %}
+{% if system_realm %}
+set/pg/system
+{% else %}
 set/pg/user(mesa_driver=aco)
+{% endif %}
 {% endblock %}
---
+++ pkgs/set/pg/system/ix.sh
@@ -0,0 +1,7 @@
+{% extends '//die/hub.sh' %}
+
+{% block run_deps %}
+set/stalix
+bin/kernel/6/8
+bin/kernel/gengrub
+{% endblock %}
