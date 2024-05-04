8a71d441006db3af2a66bac0b5a168a22cde4086
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-05-04 18:59:11 +0300

    better

--- pkgs/etc/tmpfs/ix.sh
+++ pkgs/etc/tmpfs/ix.sh
@@ -7,12 +7,10 @@ cat << EOF > ${out}/etc/runit/1.d/00-01-immutable.sh
 # prepare tmpfs root
 mount -t tmpfs tmpfs /sys
 cd /sys
-mkdir -p var/mnt/root
+mkdir -p var/mnt/root ix home
 ln -s ix/realm/system/bin bin
 ln -s ix/realm/system/etc etc
-mkdir ix
 mount /ix ix
-mkdir home
 mount /home home
 ln -s / usr
 pivot_root /sys /sys/var/mnt/root
@@ -26,9 +24,5 @@ cat << EOF > ${out}/etc/runit/1.d/10-mount-rw.sh
 mount -o remount,rw /var/mnt/root
 mount -o remount,rw /ix
 mount -o remount,rw /home
-EOF
-
-cat << EOF > ${out}/etc/env.d/ix_root.sh
-export IX_ROOT=/ix
 EOF
 {% endblock %}
