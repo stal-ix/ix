33a7c3d663990e084661813c837ddb34ec5f25e1
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-05-04 22:23:31 +0300

    better

--- pkgs/set/pg/ix.sh
+++ pkgs/set/pg/ix.sh
@@ -2,7 +2,7 @@
 
 {% block run_deps %}
 {% if system_realm %}
-set/pg/system
+set/pg/system(alsa_device=hw:1,dev_mngr=fs,failsafe=1,kernel_boot_flags=amd_pstate=active)
 {% else %}
 set/pg/user(mesa_driver=aco)
 {% endif %}
