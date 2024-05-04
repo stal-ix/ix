cbbf391b771f649196a4f662c0df608b28d71384
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-05-04 14:13:33 +0300

    better

--- pkgs/etc/immutable/ix.sh
+++ pkgs/etc/immutable/ix.sh
@@ -11,21 +11,24 @@ mkdir -p var/mnt/root
 ln -s ix/realm/system/bin bin
 ln -s ix/realm/system/etc etc
 mkdir ix
+ln -s /var/mnt/root/ix/realm ix/realm
+ln -s /var/mnt/root/ix/store ix/store
 mkdir home
 ln -s / usr
 pivot_root /sys /sys/var/mnt/root
 cd /
 /var/mnt/root/bin/mount /var/mnt/root/ix /ix
+cd /
 mount /var/mnt/root/home /home
-mount -o remount,rw /ix
-mount -o remount,rw /home
 mkdir -p dev sys proc var/run var/tmp var/log
 chmod 01777 var/tmp
 EOF
 
 cat << EOF > ${out}/etc/runit/1.d/10-mount-rw.sh
 # mount rw
-mount -o remount,rw none /var/mnt/root
+mount -o remount,rw /var/mnt/root
+mount -o remount,rw /ix
+mount -o remount,rw /home
 EOF
 
 cat << EOF > ${out}/etc/env.d/ix_root.sh
