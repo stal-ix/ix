b42cbb2b4207b7e2faf2c7c0725f12e7c2580e22
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-05-04 20:19:15 +0300

    better

---
+++ pkgs/set/pg/unwrap/ix.sh
@@ -0,0 +1,34 @@
+{% extends '//die/hub.sh' %}
+
+{# this is my home config, for demonstration purposes  #}
+{# one can make an overlay in private git repo instead #}
+
+{% block run_deps %}
+bin/mc
+bin/git
+bin/mpv
+bin/ted
+bin/mako
+bin/grim
+bin/wget
+bin/curl
+bin/htop
+bin/niri
+bin/bash
+bin/less
+set/debug
+bin/fuzzel
+bin/python
+bin/waybar
+bin/evince
+bin/sway/bg
+bin/iwd/ctl
+bin/swayimg
+bin/epiphany
+bin/telegram
+bin/xdg/open
+bin/alacritty
+bin/fontconfig
+bin/brightnessctl
+bin/coreutils/uutils
+{% endblock %}
