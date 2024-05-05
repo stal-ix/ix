d2a767182b2d365947602ed35b20a0ab8fedbdb5
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-05-05 20:28:26 +0300

    doc

--- pkgs/set/pg/ix.sh
+++ pkgs/set/pg/ix.sh
@@ -2,8 +2,10 @@
 
 {% block run_deps %}
 {% if system_realm %}
+# ix mut system -set/stalix set/pg --system_realm=1
 set/pg/system(alsa_device=hw:1,dev_mngr=fs,failsafe=1,kernel_boot_flags=amd_pstate=active)
 {% else %}
+# ix mut set/pg
 set/pg/user(mesa_driver=aco)
 {% endif %}
 {% endblock %}
