b6e249c8dce254e3e0d0b53e96caa67bd065cc7a
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-05-05 19:53:38 +0300

    upver

--- pkgs/bin/hypr/paper/ix.sh
+++ pkgs/bin/hypr/paper/ix.sh
@@ -1,8 +1,14 @@
 {% extends '//bin/hypr/t/ix.sh' %}
 
 {% block fetch %}
-https://github.com/hyprwm/hyprpaper/archive/refs/tags/v0.2.0.tar.gz
-sha:20c71db247c54480f5187b77b2b24cb11ec4c53ee599bd7d777f1fccdb344b5f
+https://github.com/hyprwm/hyprpaper/archive/refs/tags/v0.7.0.tar.gz
+sha:4840425013b95b49120aeb87d4d4e7d83f8956e847a537e1e6dd7f58a4b643b4
+{% endblock %}
+
+{% block bld_libs %}
+{{super()}}
+lib/webp
+lib/hypr/lang
 {% endblock %}
 
 {% block install %}
