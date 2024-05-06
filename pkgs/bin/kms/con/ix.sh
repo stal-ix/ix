7042bcdedfbf6e38238ef7e36c29ba2edfe959d9
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-05-06 14:16:55 +0300

    better

--- pkgs/bin/kms/con/ix.sh
+++ pkgs/bin/kms/con/ix.sh
@@ -5,6 +5,30 @@
 bld/dlfcn
 {% endblock %}
 
+{% block patch %}
+{{super()}}
+sed -e 's|void kmscon_load_modules|void kmscon_load_modules_xxx|' \
+    -i src/kmscon_module.c
+cat << EOF >> src/kmscon_module.c
+static void load1(const char* name) {
+    struct kmscon_module* mod;
+    mod = 0;
+    kmscon_module_open(&mod, name);
+    if (mod && kmscon_module_load(mod) == 0) {
+        shl_dlist_link(&module_list, &mod->list);
+    }
+}
+
+void kmscon_load_modules(void) {
+    load1("mod-bbulk");
+    load1("mod-gltex");
+    load1("mod-pango");
+    load1("mod-pixman");
+    load1("mod-unifont");
+}
+EOF
+{% endblock %}
+
 {% block postinstall %}
 cd ${tmp}/obj/src
 ls mod*.a | while read l; do
@@ -14,5 +38,7 @@ ls mod*.a | while read l; do
 done | dl_stubs > stubs.c
 cat stubs.c
 cc -o kmscon1 stubs.c $(find kmscon.p -type f -name '*.o') $(find -type f -name '*.a')
-cp kmscon1 ${out}/bin/
+rm -rf ${out}/bin
+mkdir ${out}/bin
+cp kmscon1 ${out}/bin/kmscon
 {% endblock %}
