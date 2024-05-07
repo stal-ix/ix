1b941734219428aab4a06b54ef59399128ca8b07
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-05-07 19:24:43 +0300

    better

--- pkgs/bin/gcc/14/ix.sh
+++ pkgs/bin/gcc/14/ix.sh
@@ -1,5 +1,6 @@
-{% extends '//die/hub.sh' %}
+{% extends '//bin/gcc/13/unwrap/ix.sh' %}
 
-{% block run_deps %}
-bin/gcc/14/unwrap(libcplpl_ver=17)
+{% block fetch %}
+http://ftp.gnu.org/gnu/gcc/gcc-14.1.0/gcc-14.1.0.tar.xz
+sha:e283c654987afe3de9d8080bc0bd79534b5ca0d681a73a11ff2b5d3767426840
 {% endblock %}
