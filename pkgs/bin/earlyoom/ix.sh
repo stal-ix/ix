cf327ae38b6d11054fb8f9e447e55a729799ab3a
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-05-08 19:44:41 +0300

    upver

--- pkgs/bin/earlyoom/ix.sh
+++ pkgs/bin/earlyoom/ix.sh
@@ -1,8 +1,12 @@
 {% extends '//die/c/make.sh' %}
 
+{% block version %}
+1.8.2
+{% endblock %}
+
 {% block fetch %}
-https://github.com/rfjakob/earlyoom/archive/refs/tags/v1.8.1.tar.gz
-sha:d7d180353216c064a99619e20e3071ac838e6642221d197fd685e6a0cc8bb032
+https://github.com/rfjakob/earlyoom/archive/refs/tags/v{{self.version().strip()}}.tar.gz
+sha:13869413dc0d973e454854077be7a2049d350f77a97f6911b654e88dc4b20346
 {% endblock %}
 
 {% block bld_libs %}
@@ -17,5 +21,6 @@ bld/fake(tool_name=systemctl)
 
 {% block make_flags %}
 BINDIR=/bin
+VERSION={{self.version().strip()}}
 SYSCONFDIR=${out}/etc
 {% endblock %}
