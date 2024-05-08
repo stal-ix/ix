e4f0edb0437a838f54ca6725ac887cd2ea6e30ea
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-05-08 19:58:38 +0300

    upver

--- pkgs/lib/nss/t/ver.sh
+++ pkgs/lib/nss/t/ver.sh
@@ -1,2 +1,2 @@
-https://ftp.mozilla.org/pub/security/nss/releases/NSS_3_99_RTM/src/nss-3.99.tar.gz
-sha:5cd5c2c8406a376686e6fa4b9c2de38aa280bea07bf927c0d521ba07c88b09bd
+https://ftp.mozilla.org/pub/security/nss/releases/NSS_3_100_RTM/src/nss-3.100.tar.gz
+sha:1e35373ce9cb5b776f678bb341b0625c437520d09ebd91d1abd622e072e38d88
