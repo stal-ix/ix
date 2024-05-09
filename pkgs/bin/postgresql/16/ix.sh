6412b1b09f50d116defb9fe1665c5e3ca03bc697
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-05-09 18:52:49 +0300

    upver

--- pkgs/bin/postgresql/16/ix.sh
+++ pkgs/bin/postgresql/16/ix.sh
@@ -1,6 +1,6 @@
 {% extends '//bin/postgresql/t/ix.sh' %}
 
 {% block fetch %}
-https://ftp.postgresql.org/pub/source/v16.1/postgresql-16.1.tar.bz2
-sha:ce3c4d85d19b0121fe0d3f8ef1fa601f71989e86f8a66f7dc3ad546dd5564fec
+https://ftp.postgresql.org/pub/source/v16.3/postgresql-16.3.tar.bz2
+sha:331963d5d3dc4caf4216a049fa40b66d6bcb8c730615859411b9518764e60585
 {% endblock %}
