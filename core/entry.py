9705d531f8e35614a8d195be011ceec70e848965
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-05-12 15:54:28 +0300

    refac

---
+++ core/entry.py
@@ -0,0 +1,34 @@
+import sys
+sys.dont_write_bytecode = True
+
+import traceback
+sys.excepthook = traceback.print_exception
+
+import os
+os.environ['SSL_CERT_FILE'] = 'nowhere'
+
+try:
+    import ssl
+    ssl._create_default_https_context = ssl._create_unverified_context
+except ImportError:
+    pass
+
+try:
+    # we always download with checksums
+    # and can not assume proper cert availability in bootstrap context
+    # so we safely disable cert validation
+    import urllib3
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+except ImportError:
+    pass
+
+import fcntl
+fcntl.fcntl(1, fcntl.F_SETFL, 0)
+
+import time
+
+import core.main as cm
+
+def entry(me):
+    seed = os.environ.get('IX_SEED', str(time.time()))
+    sys.exit(cm.main(sys.argv, me, seed))
