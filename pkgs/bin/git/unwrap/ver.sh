978fcc185a40a95357950ee82b6a16c01c103b9c
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-05-14 22:29:30 +0300

    upver

--- pkgs/bin/git/unwrap/ver.sh
+++ pkgs/bin/git/unwrap/ver.sh
@@ -1,2 +1,2 @@
-https://github.com/git/git/archive/refs/tags/v2.45.0.tar.gz
-sha:bc67d69af994e344091b61139b60501dbbf7e7aac6a8a50e0ac95a22503b5d1c
+https://github.com/git/git/archive/refs/tags/v2.45.1.tar.gz
+sha:d98c8f70d58f49f7546d59b25e25f2deae6999eb036a33b0fe6f5d07c33f67c6
