626cbd69d30de74ee1fed61f964d5dc567ae3acb
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-05-15 14:10:20 +0300

    fix

--- pkgs/bin/bash/3/ix.sh
+++ pkgs/bin/bash/3/ix.sh
@@ -2,7 +2,7 @@
 
 {% block fetch %}
 https://ftp.gnu.org/gnu/bash/bash-3.2.57.tar.gz
-md5:237a8767c990b43ae2c89895c2dbc062
+sha:3fa9daf85ebf35068f090ce51283ddeeb3c75eb5bc70b1a4a7cb05868bfe06a4
 {% endblock %}
 
 {% block bld_libs %}
