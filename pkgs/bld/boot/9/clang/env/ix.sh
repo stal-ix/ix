aacdd5510ef16ef1769ec46df16167284a8fe8b3
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-05-18 12:11:32 +0300

    refac

--- pkgs/bld/boot/9/clang/env/ix.sh
+++ pkgs/bld/boot/9/clang/env/ix.sh
@@ -1,11 +1,7 @@
 {% extends '//die/hub.sh' %}
 
 {% block run_deps %}
-{% if linux %}
-bld/boot/9/clang(libc=musl/pure,purec=musl/unwrap)
-{% else %}
-bld/boot/9/clang
-{% endif %}
+bld/boot/9/clang(libc_lite=1)
 bld/boot/9/nasm
 bld/boot/8/env/box
 {% endblock %}
