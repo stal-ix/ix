55e3c5e70b000885a39d3ae42f764626aca724de
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-05-21 18:00:43 +0300

    upver

--- pkgs/bin/openvi/stock/ix.sh
+++ pkgs/bin/openvi/stock/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/make.sh' %}
 
 {% block fetch %}
-https://github.com/johnsonjh/OpenVi/archive/refs/tags/7.5.28.tar.gz
-sha:478392a48fabbfab70b53b1aa2f5d4fda329872c1436044ee786874ff7f495d2
+https://github.com/johnsonjh/OpenVi/archive/refs/tags/7.5.29.tar.gz
+sha:3be3eff39561ea5b2edd5e2883a8d9c32e2ba8d999bfdb9fb77c85c8cbd65d4c
 {% endblock %}
 
 {% block bld_libs %}
