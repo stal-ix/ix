a74baafa6ebe7cd418002c9e2b032315776a68ef
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-05-21 17:50:14 +0300

    upver

--- pkgs/lib/ini/parser/ix.sh
+++ pkgs/lib/ini/parser/ix.sh
@@ -1,22 +1,15 @@
-{% extends '//die/c/make.sh' %}
+{% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/ndevilla/iniparser/archive/refs/tags/v4.2.1.tar.gz
-sha:9120fd13260be1dbec74b8aaf47777c434976626f3b3288c0d17b70e21cce2d2
+https://github.com/ndevilla/iniparser/archive/refs/tags/v4.2.2.tar.gz
+sha:82983d9712e321494ee67e1d90963545b6788c198611dab094804a7b7414b233
 {% endblock %}
 
 {% block lib_deps %}
 lib/c
 {% endblock %}
 
-{% block make_target %}
-libiniparser.a
-{% endblock %}
-
-{% block install %}
-mkdir -p ${out}/include ${out}/lib/pkgconfig
-cp src/*.h ${out}/include/
-cp *.a ${out}/lib/
-cp *.pc ${out}/lib/pkgconfig/
-sed -e "s|/usr|${out}|" -i ${out}/lib/pkgconfig/iniparser.pc
+{% block cmake_flags %}
+BUILD_EXAMPLES=OFF
+BUILD_DOCS=OFF
 {% endblock %}
