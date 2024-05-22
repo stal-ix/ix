fa57d283c405d7dbbcec4923f95bb9ec19db5811
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-05-22 19:50:19 +0300

    fix build

---
+++ pkgs/bin/libtool/prev/ix.sh
@@ -0,0 +1,6 @@
+{% extends '//bin/libtool/ix.sh' %}
+
+{% block fetch %}
+https://ftpmirror.gnu.org/libtool/libtool-2.4.6.tar.gz
+sha:e3bd4d5d3d025a36c21dd6af7ea818a2afcd4dfc1ea5a17b39d7854bcd0c06e3
+{% endblock %}
