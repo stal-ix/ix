de97beb7a38ba731fb79b30d339f99581d4e22e2
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-05-26 17:57:29 +0300

    upver

--- pkgs/lib/gsl/gnu/ix.sh
+++ pkgs/lib/gsl/gnu/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://ftp.gnu.org/gnu/gsl/gsl-2.7.1.tar.gz
-sha:dcb0fbd43048832b757ff9942691a8dd70026d5da0ff85601e52687f6deeb34b
+https://ftp.gnu.org/gnu/gsl/gsl-2.8.tar.gz
+sha:6a99eeed15632c6354895b1dd542ed5a855c0f15d9ad1326c6fe2b2c9e423190
 {% endblock %}
 
 {% block lib_deps %}
