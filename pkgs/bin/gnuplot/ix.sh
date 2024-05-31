a07bf729c97c9307f0d8edd76ba70d23c9dec962
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-05-31 10:58:44 +0300

    upver

--- pkgs/bin/gnuplot/ix.sh
+++ pkgs/bin/gnuplot/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://downloads.sourceforge.net/project/gnuplot/gnuplot/6.0.0/gnuplot-6.0.0.tar.gz
-sha:635a28f0993f6ab0d1179e072ad39b8139d07f51237f841d93c6c2ff4b1758ec
+https://downloads.sourceforge.net/project/gnuplot/gnuplot/6.0.1/gnuplot-6.0.1.tar.gz
+sha:e85a660c1a2a1808ff24f7e69981ffcbac66a45c9dcf711b65610b26ea71379a
 {% endblock %}
 
 {% block bld_libs %}
