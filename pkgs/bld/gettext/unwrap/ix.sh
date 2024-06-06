7363381a29d52649085981bb5cc48b274b14abe6
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-06-06 18:08:54 +0300

    darwin port

--- pkgs/bld/gettext/unwrap/ix.sh
+++ pkgs/bld/gettext/unwrap/ix.sh
@@ -6,6 +6,9 @@
 
 {% block bld_libs %}
 lib/c
+{% if darwin %}
+lib/iconv
+{% endif %}
 lib/shim/gnu
 {% endblock %}
 
@@ -18,6 +21,16 @@ bld/byacc
 {% block configure_flags %}
 --disable-c++
 --disable-nls
+--disable-java
+--disable-csharp
+--without-git
+--without-cvs
+--without-xz
 --disable-curses
 --with-included-gettext
+{% endblock %}
+
+{% block setup_target_flags %}
+{{super()}}
+export CFLAGS="-Wno-incompatible-function-pointer-types ${CFLAGS}"
 {% endblock %}
