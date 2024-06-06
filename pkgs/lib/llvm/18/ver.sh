655678c158ec056b33ce5ab6365e1903bb0963ab
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-06-06 09:02:32 +0300

    upver

--- pkgs/lib/llvm/18/ver.sh
+++ pkgs/lib/llvm/18/ver.sh
@@ -1,2 +1,2 @@
-https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-18.1.6.tar.gz
-sha:01390edfae5b809e982b530ff9088e674c62b13aa92cb9dc1e067fa2cf501083
+https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-18.1.7.tar.gz
+sha:b60df7cbe02cef2523f7357120fb0d46cbb443791cde3a5fb36b82c335c0afc9
