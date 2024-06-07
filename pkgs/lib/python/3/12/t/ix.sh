c3f0960723498a7eed942bff437ee25721f8d530
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-06-07 08:43:08 +0300

    upver

--- pkgs/lib/python/3/12/t/ix.sh
+++ pkgs/lib/python/3/12/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//lib/python/3/11/ix.sh' %}
 
 {% block fetch %}
-https://github.com/python/cpython/archive/refs/tags/v3.12.3.tar.gz
-sha:5c91f5e64a3c85c582cd0454edafb820d4f6226a8eab232cd05185de60350c8d
+https://github.com/python/cpython/archive/refs/tags/v3.12.4.tar.gz
+sha:9fe6e3672a17a4d9c9ff95aed286115cf196c53eade7d7d864c05bdce0a04f2d
 {% endblock %}
 
 {% block conf_ver %}
