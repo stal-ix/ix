7a3109b91a3ec17c69bcf870ffebcade030347e3
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-06-11 18:48:40 +0300

    upver

--- pkgs/bin/pdpmake/ix.sh
+++ pkgs/bin/pdpmake/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/make.sh' %}
 
 {% block fetch %}
-https://github.com/rmyorston/pdpmake/archive/refs/tags/1.4.0.tar.gz
-sha:d17600f2699938980cc8734ed9f1cd993b11264e22b6fe2c18cdb8ed6ab5045d
+https://github.com/rmyorston/pdpmake/archive/refs/tags/1.4.3.tar.gz
+sha:e078ab376683de2a3044826c301f0cdef1147065f5244b52af33727797489819
 {% endblock %}
 
 {% block bld_libs %}
