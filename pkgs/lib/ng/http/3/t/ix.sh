4730d9300b7688cbb9313858d1d10ff824e0d072
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-06-13 21:08:14 +0300

    upver

--- pkgs/lib/ng/http/3/t/ix.sh
+++ pkgs/lib/ng/http/3/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/ngtcp2/nghttp3/releases/download/v1.3.0/nghttp3-1.3.0.tar.xz
-sha:450525981d302f23832b18edd1a62cf58019392ca6402408d0eb1a7f3fd92ecf
+https://github.com/ngtcp2/nghttp3/releases/download/v1.4.0/nghttp3-1.4.0.tar.xz
+sha:c87b27ef33982a3b831dd349f4a75c55bd4c22a8ec0890095b84b54009df9d6a
 {% endblock %}
 
 {% block lib_deps %}
