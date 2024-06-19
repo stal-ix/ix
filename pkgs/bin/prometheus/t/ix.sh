ecd7941cda6162fa0e6db36e09588cab1a58d474
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-06-19 21:17:29 +0300

    upver

--- pkgs/bin/prometheus/t/ix.sh
+++ pkgs/bin/prometheus/t/ix.sh
@@ -1,9 +1,9 @@
 {% extends '//die/go/build.sh' %}
 
 {% block go_url %}
-https://github.com/prometheus/prometheus/archive/refs/tags/v2.52.1.tar.gz
+https://github.com/prometheus/prometheus/archive/refs/tags/v2.53.0.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-3bbdd0945e0cb829cc950126dd789b9bf5f115dcbdff7095f9d0e83ff7690286
+f58b034079e2949a945aa16c700bf71ff019294bca033da7325604229e2fdd8c
 {% endblock %}
