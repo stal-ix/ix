8aa5b2f44e73e9b051cc1eda7649df38b5429477
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-06-21 08:40:25 +0300

    upver

--- pkgs/lib/display/info/ix.sh
+++ pkgs/lib/display/info/ix.sh
@@ -1,10 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-#https://gitlab.freedesktop.org/emersion/libdisplay-info/-/archive/0.1.1/libdisplay-info-0.1.1.tar.bz2
-#sha:51cdb0362882ca2af62532ab4d95e60d81e9890b339264719fd55f8e3945d695
-http://distfiles.gentoo.org/distfiles/a4/libdisplay-info-0.1.1.tar.xz
-sha:0d8731588e9f82a9cac96324a3d7c82e2ba5b1b5e006143fefe692c74069fb60
+https://gitlab.freedesktop.org/emersion/libdisplay-info/-/archive/0.2.0/libdisplay-info-0.2.0.tar.bz2
+sha:f6cf2ddbba3753ae38de5113d1fcb8fab977dfaf5fb07b38cd68d8482765e208
 {% endblock %}
 
 {% block lib_deps %}
