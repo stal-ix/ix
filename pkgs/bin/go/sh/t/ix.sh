9a6b283ff2a4122da22dd6efbc4c0c94b4ad2adc
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-06-24 21:26:27 +0300

    upver

---
+++ pkgs/bin/go/sh/t/ix.sh
@@ -0,0 +1,9 @@
+{% extends '//die/go/build.sh' %}
+
+{% block go_url %}
+https://github.com/mvdan/sh/archive/refs/tags/v3.8.0.tar.gz
+{% endblock %}
+
+{% block go_sha %}
+3525ae04bb813eb5a202c5518032297ec6af22f89538f5bda94dbe625609b648
+{% endblock %}
