c4b86d0864b33648a45347b17787ea79c4668112
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-06-30 15:25:46 +0300

    upver

--- pkgs/bin/carapace/ix.sh
+++ pkgs/bin/carapace/ix.sh
@@ -1,7 +1,7 @@
 {% extends '//die/go/build.sh' %}
 
 {% block go_url %}
-https://github.com/rsteube/carapace-bin/archive/refs/tags/v0.29.1.tar.gz
+https://github.com/carapace-sh/carapace-bin/archive/refs/tags/v1.0.4.tar.gz
 {% endblock %}
 
 {% block go_sha %}
@@ -10,10 +10,10 @@ cd81c4f0581559a8ab9ff767789dd6935ab47b0fe6fe9ba0ac308775beb443e3
 
 {% block unpack %}
 {{super()}}
-cd cmd/carapace
+cd cmd/carapace-generate
 {% endblock %}
 
 {% block install %}
 mkdir ${out}/bin
-cp carapace ${out}/bin/
+cp carapace-generate ${out}/bin/
 {% endblock %}
