08ca85b5f9f90a14b8de41407d5ed87be2aa5fa6
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-07-02 23:51:46 +0300

    better

--- pkgs/lib/icu/test/main.cpp
+++ pkgs/lib/icu/test/main.cpp
@@ -1,8 +1,12 @@
 #include <unicode/ubrk.h>
-#include <iostream>
 
 int main() {
     UErrorCode status = U_ZERO_ERROR;
     auto iter = ubrk_open(UBRK_LINE, "", nullptr, 0, &status);
-    std::cerr << status << " " << (size_t)(iter) << std::endl;
+    if (status) {
+        abort();
+    }
+    if (!iter) {
+        abort();
+    }
 }
