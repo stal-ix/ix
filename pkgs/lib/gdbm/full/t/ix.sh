b91bebc097a62e44c3891d13c79ecdfd008e763a
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-07-02 22:38:20 +0300

    upver

--- pkgs/lib/gdbm/full/t/ix.sh
+++ pkgs/lib/gdbm/full/t/ix.sh
@@ -1,12 +1,16 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://ftp.gnu.org/gnu/gdbm/gdbm-1.19.tar.gz
-sha:37ed12214122b972e18a0d94995039e57748191939ef74115b1d41d8811364bc
+https://ftp.gnu.org/gnu/gdbm/gdbm-1.24.tar.gz
+sha:695e9827fdf763513f133910bc7e6cfdb9187943a4fec943e57449723d2b8dbf
 {% endblock %}
 
 {% block lib_deps %}
 lib/c
+{% endblock %}
+
+{% block conf_ver %}
+2/71
 {% endblock %}
 
 {% block bld_tool %}
