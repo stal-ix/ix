9a4a1f8a713fc4b9de83496423d134f30a2c135c
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-07-03 09:03:55 +0300

    upver

--- pkgs/lib/ass/ix.sh
+++ pkgs/lib/ass/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://github.com/libass/libass/archive/refs/tags/0.17.2.tar.gz
-sha:db30b6122a9c6fb5a29e0ed8393b37d3dc5b0ef0025043fff631f863b28ea477
+https://github.com/libass/libass/archive/refs/tags/0.17.3.tar.gz
+sha:26fbfb7a7bd3e6d5c713f8a65a12b36084d1dde6efaed8a9996489054c4aeca0
 {% endblock %}
 
 {% block lib_deps %}
