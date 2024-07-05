653e2fe1e0c08ab7f3539a72d9ac5282bcc95caa
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-07-05 08:35:01 +0300

    upver

--- pkgs/lib/dwarf/ix.sh
+++ pkgs/lib/dwarf/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://www.prevanders.net/libdwarf-0.10.0.tar.xz
-sha:17b7143c4b3e5949d1578c43e8f1e2abd9f1a47e725e6600fe7ac4833a93bb77
+https://www.prevanders.net/libdwarf-0.10.1.tar.xz
+sha:b511a2dc78b98786064889deaa2c1bc48a0c70115c187900dd838474ded1cc19
 {% endblock %}
 
 {% block lib_deps %}
