45374e85da8dfd240f2db81e7bc96d12772abc36
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-07-05 12:01:58 +0300

    better

--- pkgs/bin/skia/svg/ss.cpp
+++ pkgs/bin/skia/svg/ss.cpp
@@ -1,11 +1,7 @@
 #include "include/core/SkCanvas.h"
 #include "include/core/SkStream.h"
-#include "modules/skresources/include/SkResources.h"
-#include "modules/skshaper/utils/FactoryHelpers.h"
 #include "modules/svg/include/SkSVGDOM.h"
 #include "modules/svg/include/SkSVGNode.h"
-#include "src/utils/SkOSPath.h"
-#include "tools/fonts/FontToolUtils.h"
 
 int main(int argc, char** argv) {
     auto stream = SkStream::MakeFromFile(argv[1]);
@@ -14,11 +10,7 @@ int main(int argc, char** argv) {
         abort();
     }
 
-    auto predecode = skresources::ImageDecodeStrategy::kPreDecode;
-
-    auto dom = SkSVGDOM::Builder()
-        .setTextShapingFactory(SkShapers::BestAvailable())
-        .make(*stream);
+    auto dom = SkSVGDOM::Builder().make(*stream);
 
     if (!dom) {
         abort();
