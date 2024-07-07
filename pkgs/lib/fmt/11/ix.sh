ee67e73a9ea51080c2236ac7b1fe28571160de75
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-07-07 06:29:04 +0300

    upver

---
+++ pkgs/lib/fmt/11/ix.sh
@@ -0,0 +1,6 @@
+{% extends '//lib/fmt/t/ix.sh' %}
+
+{% block fetch %}
+https://github.com/fmtlib/fmt/archive/refs/tags/11.0.1.tar.gz
+sha:7d009f7f89ac84c0a83f79ed602463d092fbf66763766a907c97fd02b100f5e9
+{% endblock %}
