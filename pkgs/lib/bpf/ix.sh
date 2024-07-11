93153d64f274c721eb3182c499091873a5646f32
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-07-11 11:58:19 +0300

    upver

--- pkgs/lib/bpf/ix.sh
+++ pkgs/lib/bpf/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/make.sh' %}
 
 {% block fetch %}
-https://github.com/libbpf/libbpf/archive/refs/tags/v1.4.3.tar.gz
-sha:d8be49641dd4c5caa27986a8291907176e3b6fd6fe650e4fee5b45f8093fc935
+https://github.com/libbpf/libbpf/archive/refs/tags/v1.4.4.tar.gz
+sha:b4fe864ed96cf276e3370a4706bafbafde1eccd5894418c4d8eeb941f371eb8a
 {% endblock %}
 
 {% block lib_deps %}
