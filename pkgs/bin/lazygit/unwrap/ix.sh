cb9ac14d5709a8c95bbcf804dfe137480eca0de5
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-07-13 21:24:03 +0300

    upver

--- pkgs/bin/lazygit/unwrap/ix.sh
+++ pkgs/bin/lazygit/unwrap/ix.sh
@@ -1,11 +1,11 @@
 {% extends '//die/go/build.sh' %}
 
 {% block go_url %}
-https://github.com/jesseduffield/lazygit/archive/refs/tags/v0.42.0.tar.gz
+https://github.com/jesseduffield/lazygit/archive/refs/tags/v0.43.1.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-4a609972214dddc9be5895b1996889ab8c9963e901886886a5b92126931ee824
+aaf033373bae37fb5bac261fec6d8fb7643e44d9b33f953eb188f82c2ffc7712
 {% endblock %}
 
 {% block go_tool %}
