23ed39535be3442769c4c6d43718e680eda4ba06
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-07-13 21:27:59 +0300

    upver

--- pkgs/bin/fheroes2/ix.sh
+++ pkgs/bin/fheroes2/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/ihhub/fheroes2/archive/refs/tags/1.1.0.tar.gz
-sha:df46c5fe702e71022974db8247aba49fb64693e4b967b68bae16872fba875542
+https://github.com/ihhub/fheroes2/archive/refs/tags/1.1.1.tar.gz
+sha:3b5df5aa26d11615fcc15d626d77832f730e24fbab7d419539c4236df5a94c5e
 {% endblock %}
 
 {% block bld_libs %}
