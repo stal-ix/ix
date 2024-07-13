42c3f523013d222c415222269372bbb042ab0329
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-07-13 14:17:37 +0300

    add

---
+++ pkgs/lib/wlroots/18/ix.sh
@@ -0,0 +1,6 @@
+{% extends '//lib/wlroots/17/ix.sh' %}
+
+{% block fetch %}
+https://gitlab.freedesktop.org/wlroots/wlroots/-/archive/0.18.0/wlroots-0.18.0.tar.bz2
+sha:5d46f6a4734ca659ad144c2bcab5da781987091dcbb0744c8aa05940eb73d372
+{% endblock %}
