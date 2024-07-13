76cd33c4c93ac1de84dd1292a20e2bf5c2ec3199
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-07-13 22:22:12 +0300

    upver

---
+++ pkgs/bin/bpf/tool/ix.sh
@@ -0,0 +1,43 @@
+{% extends '//die/c/make.sh' %}
+
+{% block git_repo %}
+https://github.com/libbpf/bpftool
+{% endblock %}
+
+{% block git_branch %}
+v7.4.0
+{% endblock %}
+
+{% block git_sha %}
+30df20f4000f6a50fdc7316fca608cc92a9333986dbadeea5b76c019eac6d66a
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/z
+lib/cap
+lib/bpf
+lib/bfd
+lib/iberty
+lib/llvm/17
+lib/opcodes
+{% endblock %}
+
+{% block step_unpack %}
+{{super()}}
+cd src
+{% endblock %}
+
+{% block make_flags %}
+LLVM=1
+V=1
+{% endblock %}
+
+{% block cpp_defines %}
+PACKAGE_VERSION=1
+{% endblock %}
+
+{% block install %}
+{{super()}}
+mv ${out}/sbin ${out}/bin
+{% endblock %}
