ae23d1c4cf4b8ec8823b23c4b10b05d51a230e3d
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-07-22 00:59:14 +0300

    better

--- pkgs/bin/clapper/ix.sh
+++ pkgs/bin/clapper/ix.sh
@@ -1,28 +1,6 @@
-{% extends '//die/c/meson.sh' %}
+{% extends '//die/hub.sh' %}
 
-{% block fetch %}
-https://github.com/Rafostar/clapper/archive/refs/tags/0.6.1.tar.gz
-sha:d244ec6108ebff5ccc817a5888f3f73f52cac129fe480d480cb3f6b9db19cfbe
+{% block run_deps %}
+bin/xdg/er(wrap=clapper)
+bin/clapper/unwrap(gtk_ver=4)
 {% endblock %}
-
-{% block bld_libs %}
-lib/c
-lib/glib
-lib/gtk/4
-lib/soup/3
-lib/adwaita
-lib/micro/dns
-lib/gstreamer
-{% endblock %}
-
-{% block bld_tool %}
-bld/glib
-bld/gnome
-bld/gettext
-bld/fake(tool_name=update-mime-database)
-{% endblock %}
-
-{% block meson_flags %}
-clapper-app=enabled
-{% endblock %}
-
