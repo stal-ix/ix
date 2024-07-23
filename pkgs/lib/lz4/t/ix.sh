e759c786d768a43ad45a3653ec9fc06f02aedbeb
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-07-23 08:07:35 +0300

    upver

--- pkgs/lib/lz4/t/ix.sh
+++ pkgs/lib/lz4/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/make.sh' %}
 
 {% block fetch %}
-https://github.com/lz4/lz4/archive/refs/tags/v1.9.4.tar.gz
-sha:0b0e3aa07c8c063ddf40b082bdf7e37a1562bda40a0ff5272957f3e987e0e54b
+https://github.com/lz4/lz4/archive/refs/tags/v1.10.0.tar.gz
+sha:537512904744b35e232912055ccf8ec66d768639ff3abe5788d90d792ec5f48b
 {% endblock %}
 
 {% block lib_deps %}
