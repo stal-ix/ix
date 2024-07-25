95bf58a95fd5584ca2392870e555f9f72d491b9a
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-07-25 16:44:27 +0300

    upver

--- pkgs/bin/kernel/6/10/ver.sh
+++ pkgs/bin/kernel/6/10/ver.sh
@@ -1,9 +1,9 @@
 {% block kernel_version %}
-6.10
+6.10.1
 {% endblock %}
 
 {% block kernel_sha %}
-774698422ee54c5f1e704456f37c65c06b51b4e9a8b0866f34580d86fef8e226
+70109dfd1cd1c5f8a58eb1cb37122b9bf93f9c6a6280bf91019263c7339cf76b
 {% endblock %}
 
 {% include '//bin/kernel/t/ver.sh' %}
