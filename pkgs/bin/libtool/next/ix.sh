c3e5ffb172f19cbad686e28e5f7d8dba4b8d8789
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-07-27 12:06:02 +0300

    upver

---
+++ pkgs/bin/libtool/next/ix.sh
@@ -0,0 +1,6 @@
+{% extends '//bin/libtool/ix.sh' %}
+
+{% block fetch %}
+https://alpha.gnu.org/gnu/libtool/libtool-2.5.1.tar.xz
+sha:d280e64c8cdbf145d76fb91b3b219edab01bdb43cb99402e4acb97d0100636fd
+{% endblock %}
