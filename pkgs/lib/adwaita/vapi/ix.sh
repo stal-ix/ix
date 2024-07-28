7ab703419576ac71b921ec43af635efd2d5cd577
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-07-28 22:04:07 +0300

    better

--- pkgs/lib/adwaita/vapi/ix.sh
+++ pkgs/lib/adwaita/vapi/ix.sh
@@ -32,6 +32,7 @@ rm -rf ${out}/bin ${out}/lib ${out}/include
 {% endblock %}
 
 {% block env %}
+export GIRPATH="${out}/share/gir-1.0:\${GIRPATH}"
 export VALAFLAGS="--girdir=${out}/share/gir-1.0 --vapidir=${out}/share/vala/vapi \${VALAFLAGS}"
 export GIRSFLAGS="--add-include-path=${out}/share/gir-1.0 \${GIRSFLAGS}"
 export GIRCFLAGS="--includedir=${out}/share/gir-1.0 \${GIRCFLAGS}"
--- pkgs/lib/harfbuzz/gir/ix.sh
+++ pkgs/lib/harfbuzz/gir/ix.sh
@@ -35,6 +35,7 @@ rm -rf ${out}/include ${out}/doc ${out}/bin ${out}/lib
 {% endblock %}
 
 {% block env %}
+export GIRPATH="${out}/share/gir-1.0:\${GIRPATH}"
 export GIRSFLAGS="--add-include-path=${out}/share/gir-1.0 \${GIRSFLAGS}"
 export GIRCFLAGS="--includedir=${out}/share/gir-1.0 \${GIRCFLAGS}"
 {% endblock %}
