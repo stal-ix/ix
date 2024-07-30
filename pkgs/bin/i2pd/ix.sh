586e2395fdd251012f12207499fc3f06e4d7e518
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-07-30 16:01:20 +0300

    upver

--- pkgs/bin/i2pd/ix.sh
+++ pkgs/bin/i2pd/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/make.sh' %}
 
 {% block fetch %}
-https://github.com/PurpleI2P/i2pd/archive/refs/tags/2.53.0.tar.gz
-sha:a5eb7f9faa7a8b66b044841e2967b9dc0367e4e41939ce6a3223f8bd2f347da8
+https://github.com/PurpleI2P/i2pd/archive/refs/tags/2.53.1.tar.gz
+sha:99c3d739530b08b28a796544ead4254394c7226a873bf813db4e7b3c1524d757
 {% endblock %}
 
 {% block bld_libs %}
