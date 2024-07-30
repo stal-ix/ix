274ea09b333342d09550837c47b2e75c34987fdf
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-07-30 18:05:31 +0300

    better

--- pkgs/bin/telegram/desktop/unwrap/ix.sh
+++ pkgs/bin/telegram/desktop/unwrap/ix.sh
@@ -137,10 +137,6 @@ sed -e 's|.*add_cppgir().*||' -i cmake/external/glib/CMakeLists.txt
 
 sed -e 's|DESKTOP_APP_DISABLE_WAYLAND_INTEGRATION|TRUE|' -i Telegram/lib_webview/CMakeLists.txt
 
-find . -type f -name '*.cpp' | while read l; do
-    sed -e 's|GObject::Object|gi::repository::GObject::Object|g' -i ${l}
-done
-
 prepend Telegram/SourceFiles/info/info_content_widget.cpp << EOF
 #include "Telegram/lib_ui/ui/effects/numbers_animation.h"
 EOF
