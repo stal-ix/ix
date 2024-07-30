e0dc368d82e4dada3275af0376354664ffff0e6a
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-07-30 22:30:15 +0300

    split

--- pkgs/lib/handy/unwrap/ix.sh
+++ pkgs/lib/handy/unwrap/ix.sh
@@ -1,26 +1,12 @@
-{% extends '//die/c/meson.sh' %}
-
-{% block fetch %}
-https://gitlab.gnome.org/GNOME/libhandy/-/archive/1.8.2/libhandy-1.8.2.tar.bz2
-sha:67bea0c4afa56f0512a9d872ffc6b8335236f38164b245d1baf85dccde26c05c
-{% endblock %}
-
-{% block lib_deps %}
-lib/c
-lib/glib
-lib/gtk/3
-{% endblock %}
-
-{% block bld_tool %}
-bld/glib
-{% endblock %}
+{% extends '//lib/handy/t/ix.sh' %}
 
 {% block meson_flags %}
+{{super()}}
 introspection=disabled
-glade_catalog=disabled
 {% endblock %}
 
 {% block c_rename_symbol %}
+{{super()}}
 gtk_progress_tracker_advance_frame
 gtk_progress_tracker_finish
 gtk_progress_tracker_get_ease_out_cubic
@@ -34,5 +20,6 @@ gtk_progress_tracker_start
 {% endblock %}
 
 {% block env %}
+{{super()}}
 export CPPFLAGS="-I${out}/include/libhandy-1 \${CPPFLAGS}"
 {% endblock %}
