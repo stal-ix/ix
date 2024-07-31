7621871dc9a5d30b23fa6b328608c35da843d9cc
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-07-31 16:54:18 +0300

    better

--- pkgs/bin/geary/unwrap/ix.sh
+++ pkgs/bin/geary/unwrap/ix.sh
@@ -1,5 +1,7 @@
 {% extends '//die/c/meson.sh' %}
 
+{# WIP #}
+
 {% block fetch %}
 https://gitlab.gnome.org/GNOME/geary/-/archive/46.0/geary-46.0.tar.bz2
 sha:0bac78c2dd2736518dd277edfabba6b40a4eacef6f6e27c9468c09be62e10311
