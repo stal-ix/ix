e83821ff03b616ba8f6bec653a690a7b9ecf274b
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-07-31 11:41:32 +0300

    better

---
+++ pkgs/lib/gtk/layer/shell/t/ix.sh
@@ -0,0 +1,16 @@
+{% extends '//die/c/meson.sh' %}
+
+{% block fetch %}
+https://github.com/wmww/gtk-layer-shell/archive/refs/tags/v0.8.0.tar.gz
+sha:e95a03766302fe330ec3c6ff3e8018642849003ccaf160fb6fd0c039c81fa54c
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+lib/gtk/3
+lib/wayland
+{% endblock %}
+
+{% block bld_tool %}
+bld/wayland
+{% endblock %}
