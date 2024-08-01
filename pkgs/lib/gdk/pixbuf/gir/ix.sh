8c3fd0d8a9e9d6d6eeb77a5e026ade2baf80edf0
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-08-01 13:13:04 +0300

    add

---
+++ pkgs/lib/gdk/pixbuf/gir/ix.sh
@@ -0,0 +1,36 @@
+{% extends '//lib/gdk/pixbuf/t/ix.sh' %}
+
+{% block meson_flags %}
+{{super()}}
+introspection=enabled
+{% endblock %}
+
+{% block host_libs %}
+{{super()}}
+lib/gdk/pixbuf/dl
+{% endblock %}
+
+{% block bld_libs %}
+{{super()}}
+lib/gi/repository
+{% endblock %}
+
+{% block bld_data %}
+lib/gi/files
+{% endblock %}
+
+{% block bld_tool %}
+{{super()}}
+bld/gir
+{% endblock %}
+
+{% block install %}
+{{super()}}
+mv ${out}/lib/gi* ${out}/share/
+{% endblock %}
+
+{% block env %}
+export GIRPATH="${out}/share/gir-1.0:\${GIRPATH}"
+export GIRSFLAGS="--add-include-path=${out}/share/gir-1.0 \${GIRSFLAGS}"
+export GIRCFLAGS="--includedir=${out}/share/gir-1.0 \${GIRCFLAGS}"
+{% endblock %}
