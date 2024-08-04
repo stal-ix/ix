5816abceb68ebab4da47e0d0511ee491127d2664
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-08-04 18:50:34 +0300

    add

---
+++ pkgs/lib/graphene/dl/ix.sh
@@ -0,0 +1,13 @@
+{% extends '//die/dl/lib.sh' %}
+
+{% block lib_deps %}
+lib/graphene
+{% endblock %}
+
+{% block export_libs %}
+libgraphene-1.0.a
+{% endblock %}
+
+{% block export_lib %}
+graphene-1
+{% endblock %}
