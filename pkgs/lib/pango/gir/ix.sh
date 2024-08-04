25392e91d18edc91770e6d250d1763140eb2b22f
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-08-04 16:11:57 +0300

    add

---
+++ pkgs/lib/pango/gir/ix.sh
@@ -0,0 +1,38 @@
+{% extends '//lib/pango/t/ix.sh' %}
+
+{% block meson_flags %}
+{{super()}}
+introspection=enabled
+{% endblock %}
+
+{% block host_libs %}
+{{super()}}
+lib/pango/dl
+{% endblock %}
+
+{% block bld_libs %}
+{{super()}}
+lib/gi/repository
+{% endblock %}
+
+{% block bld_data %}
+lib/gi/files
+lib/harfbuzz/gir
+{% endblock %}
+
+{% block bld_tool %}
+{{super()}}
+bld/gir
+{% endblock %}
+
+{% block install %}
+{{super()}}
+mv ${out}/lib/gi* ${out}/share/
+{% endblock %}
+
+{% block env %}
+export GIRPATH="${out}/share/gir-1.0:\${GIRPATH}"
+export VALAFLAGS="--girdir=${out}/share/gir-1.0 \${VALAFLAGS}"
+export GIRSFLAGS="--add-include-path=${out}/share/gir-1.0 \${GIRSFLAGS}"
+export GIRCFLAGS="--includedir=${out}/share/gir-1.0 \${GIRCFLAGS}"
+{% endblock %}
