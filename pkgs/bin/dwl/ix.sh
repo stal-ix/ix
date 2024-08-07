979274f8157cf31164903ebb789de4d111f5c76a
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-08-07 16:23:31 +0300

    upver

--- pkgs/bin/dwl/ix.sh
+++ pkgs/bin/dwl/ix.sh
@@ -1,14 +1,14 @@
 {% extends '//die/c/make.sh' %}
 
 {% block fetch %}
-https://codeberg.org/dwl/dwl/archive/v0.6.tar.gz
-sha:50b96ae5d11dc269b24e2aa021eb4c2eb6e37c58ccae215bc3e1f0c19013ec0f
+https://codeberg.org/dwl/dwl/archive/v0.7.tar.gz
+sha:401a572462fa89f6adf0d0cfe58d909ef334a2c7cd1357db86ec70f32bd4b4d3
 {% endblock %}
 
 {% block bld_libs %}
 lib/c
 lib/wayland
-lib/wlroots/17
+lib/wlroots/18
 lib/drivers/3d
 {% endblock %}
 
