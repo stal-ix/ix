e4cd9a396a806596fa72c6357782630070157c39
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-08-11 13:02:57 +0300

    better

--- pkgs/bld/rust/linux/t/ix.sh
+++ pkgs/bld/rust/linux/t/ix.sh
@@ -1,10 +1,5 @@
 {% extends '//die/std/ix.sh' %}
 
-{% block fetch %}
-https://static.rust-lang.org/dist/rust-1.80.0-x86_64-unknown-linux-musl.tar.gz
-sha:16f0f4f2e01f613f7ee44787f64708623d8ef8c65ba37a12a682cd0ffea21fe8
-{% endblock %}
-
 {% block bld_tool %}
 bld/musl
 bin/patch/elf
