a0fadae1abe51d8a6b67f14192472cd78d96d6e3
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-08-13 07:40:30 +0300

    clean

--- pkgs/lib/freetype/t/ix.sh
+++ pkgs/lib/freetype/t/ix.sh
@@ -2,7 +2,6 @@
 
 {% block fetch %}
 https://gitlab.com/freetype/freetype/-/archive/VER-2-13-3/freetype-VER-2-13-3.tar.bz2
-#https://gitlab.freedesktop.org/freetype/freetype/-/archive/VER-2-13-2/freetype-VER-2-13-2.tar.bz2
 sha:2f8aa590bbb4deaf77c530b77e4c5c636ae365403b8793b9841f09faf32f3a06
 {% endblock %}
 
