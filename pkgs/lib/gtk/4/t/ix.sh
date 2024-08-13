7bc9c4b5c9474a8ec802888cf544e9766f5c57c2
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-08-13 07:55:46 +0300

    upver

--- pkgs/lib/gtk/4/t/ix.sh
+++ pkgs/lib/gtk/4/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//lib/gtk/t/ix.sh' %}
 
 {% block fetch %}
-https://gitlab.gnome.org/GNOME/gtk/-/archive/4.15.4/gtk-4.15.4.tar.bz2
-sha:d586c2a12cfe0fd8f2e3305aafc61d60dca38a04fb70cc186904e22ab168c7a8
+https://gitlab.gnome.org/GNOME/gtk/-/archive/4.15.5/gtk-4.15.5.tar.bz2
+sha:d15342b95d9211284bc960e679303a29c361d7e4ae53c165d9bd8fd125018866
 {% endblock %}
 
 {% block lib_deps %}
