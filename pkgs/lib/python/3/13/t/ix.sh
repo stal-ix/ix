ec2bc23e8a5d64ceef155ed750c83f6feca463ef
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-08-15 21:03:13 +0300

    add

---
+++ pkgs/lib/python/3/13/t/ix.sh
@@ -0,0 +1,6 @@
+{% extends '//lib/python/3/12/t/ix.sh' %}
+
+{% block fetch %}
+https://www.python.org/ftp/python/3.13.0/Python-3.13.0rc1.tar.xz
+sha:678b884775eec0224d5159fa900879020baca2a36ce942fd95febfa1adb4a6bd
+{% endblock %}
