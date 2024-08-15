61c9bd5b6158a40c2cd29b77ffa5b21bd4cae602
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-08-15 13:45:24 +0300

    revert

--- pkgs/lib/boost/ix.sh
+++ pkgs/lib/boost/ix.sh
@@ -1,8 +1,18 @@
 {% extends '//die/c/ix.sh' %}
 
+{#
+: && /ix/build/T3kvwPusLKM6b72u/bin/tc/clang++ -O3 -DNDEBUG  CMakeFiles/cppgir.dir/tools/cppgir.cpp.o CMakeFiles/cppgir.dir/tools/genbase.cpp.o CMakeFiles/cppgir.dir/tools/genns.cpp.o CMakeFiles/cppgir.dir/tools/genutils.cpp.o CMakeFiles/cppgir.dir/tools/function.cpp.o CMakeFiles/cppgir.dir/tools/repository.cpp.o -o cppgir  /ix/store/F1FHzElAD4TdInUR-lib-fmt-9/lib/libfmt.a && :
+ld.lld: error: duplicate symbol: __cxa_allocate_exception
+>>> defined at from_exception.cpp
+>>>            from_exception.o:(__cxa_allocate_exception) in archive /ix/store/5rV34zMi8lZaI4ic-lib-boost/lib/libboost_stacktrace_from_exception.a
+>>> defined at cxa_exception.cpp
+>>>            cxa_exception.cpp.o:(.text.__cxa_allocate_exception+0x0) in archive /ix/store/0s9LqOSQkwkvgwid-lib-c-plus-plus-18/lib/libc++abi.a
+clang++: error: linker command failed with exit code 1 (use -v to see invocation)
+#}
+
 {% block fetch %}
-https://boostorg.jfrog.io/artifactory/main/release/1.86.0/source/boost_1_86_0.tar.bz2
-sha:1bed88e40401b2cb7a1f76d4bab499e352fa4d0c5f31c0dbae64e24d34d7513b
+https://boostorg.jfrog.io/artifactory/main/release/1.85.0/source/boost_1_85_0.tar.bz2
+sha:7009fe1faa1697476bdc7027703a2badb84e849b7b0baad5086b087b971f8617
 {% endblock %}
 
 {% block bld_tool %}
@@ -33,7 +43,7 @@ b2 link=static
 
 {% block install %}
 b2 link=static install
-#sed -e 's|len + 1}|int(len + 1)}|' -i ${out}/include/boost/process/detail/posix/executor.hpp
+sed -e 's|len + 1}|int(len + 1)}|' -i ${out}/include/boost/process/detail/posix/executor.hpp
 {% endblock %}
 
 {% block build_flags %}
