e6df868d47c79811e459551237ccf7e6127c9443
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-08-20 11:26:24 +0300

    upver

--- pkgs/lib/blake3/c/ix.sh
+++ pkgs/lib/blake3/c/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/BLAKE3-team/BLAKE3/archive/refs/tags/1.5.3.tar.gz
-sha:ec9114480857334858e73b727199c573bfdbed6138a83be573f076d37e671fc1
+https://github.com/BLAKE3-team/BLAKE3/archive/refs/tags/1.5.4.tar.gz
+sha:ddd24f26a31d23373e63d9be2e723263ac46c8b6d49902ab08024b573fd2a416
 {% endblock %}
 
 {% block lib_deps %}
