bb06594be55c86aa26b2cff375d77ca3119e8d34
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-08-24 21:48:48 +0300

    add

---
+++ pkgs/bin/elementary/dock/ix.sh
@@ -0,0 +1,15 @@
+{% extends '//die/vala/meson.sh' %}
+
+{# hard X11 dep #}
+
+{% block fetch %}
+https://github.com/elementary/dock/archive/refs/tags/8.0.0.tar.gz
+sha:9aac9b6e5774adf5bd95605173b2d120036ee5605f28f8e12bc6d4a9bc9e78b8
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/gee
+lib/gtk/4
+lib/adwaita
+{% endblock %}
