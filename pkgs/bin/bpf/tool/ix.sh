c381df9a17522232571ced37d8d7eefb88e58dcb
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-08-25 16:29:18 +0300

    up

--- pkgs/bin/bpf/tool/ix.sh
+++ pkgs/bin/bpf/tool/ix.sh
@@ -19,8 +19,8 @@ lib/cap
 lib/bpf
 lib/bfd
 lib/iberty
-lib/llvm/17
 lib/opcodes
+lib/llvm/18
 {% endblock %}
 
 {% block step_unpack %}
@@ -41,4 +41,8 @@ PACKAGE_VERSION=1
 {% block install %}
 {{super()}}
 mv ${out}/sbin ${out}/bin
+{% endblock %}
+
+{% block bld_tool %}
+bld/llvm/config
 {% endblock %}
