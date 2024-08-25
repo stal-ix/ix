ff677d9537fe338c90ec35e801d26243119c809a
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-08-25 19:01:17 +0300

    upver

--- pkgs/bin/socat/ix.sh
+++ pkgs/bin/socat/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
-http://www.dest-unreach.org/socat/download/socat-1.8.0.0.tar.gz
-sha:6010f4f311e5ebe0e63c77f78613d264253680006ac8979f52b0711a9a231e82
+http://www.dest-unreach.org/socat/download/socat-1.8.0.1.tar.gz
+sha:dc350411e03da657269e529c4d49fe23ba7b4610b0b225c020df4cf9b46e6982
 {% endblock %}
 
 {% block bld_libs %}
