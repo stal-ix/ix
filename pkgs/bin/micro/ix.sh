eacc84ddb0dbd29409913a9ddbb185130d5ba6dc
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-08-28 00:15:42 +0300

    upver

--- pkgs/bin/micro/ix.sh
+++ pkgs/bin/micro/ix.sh
@@ -1,11 +1,11 @@
 {% extends '//die/go/build.sh' %}
 
 {% block go_url %}
-https://github.com/zyedidia/micro/archive/refs/tags/v2.0.13.tar.gz
+https://github.com/zyedidia/micro/archive/refs/tags/v2.0.14.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-661f2ea8aa8ea0060b4a2471441f8c54d046b9c9351fe83f6590ce448ad3ab6d
+b4ea01909023fea11fe66faf4b8a14d170e22dfb2bd3d6f326b66902eb618ddb
 {% endblock %}
 
 {% block unpack %}
