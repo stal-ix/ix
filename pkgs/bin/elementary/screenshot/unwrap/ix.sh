6ff98ee479c398b48dad50f33730bef890cbc22e
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-08-28 23:44:44 +0300

    add

---
+++ pkgs/bin/elementary/screenshot/unwrap/ix.sh
@@ -0,0 +1,33 @@
+{% extends '//die/vala/meson.sh' %}
+
+{% block git_repo %}
+https://github.com/elementary/screenshot
+{% endblock %}
+
+{% block git_commit %}
+cdbd008944c2e29839fb99a60c82c6563ced7582
+{% endblock %}
+
+{% block git_sha %}
+e3884a233b927489e7ec2d0f72c9d5fe23efc9df6150475a7778bf7f9f5664ed
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/gee
+lib/gtk
+lib/glib
+lib/portal
+lib/granite
+{% endblock %}
+
+{% block bld_tool %}
+bld/gettext
+{% endblock %}
+
+{% block bld_data %}
+{{super()}}
+lib/gee/gir
+lib/granite/{{gtk_ver}}/gir
+lib/portal/gir(gtk_ver={{gtk_ver}})
+{% endblock %}
