b9614cf6ca2a29a6fa4c031edd33ac2dca601450
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-08-28 23:05:17 +0300

    upver

---
+++ pkgs/aux/fonts/ms/cascadia/ix.sh
@@ -0,0 +1,6 @@
+{% extends '//die/fonts.sh' %}
+
+{% block fetch %}
+https://github.com/microsoft/cascadia-code/archive/refs/tags/v2404.23.tar.gz
+sha:826aaefb4bb0185f6f9957eca63bfcbc3542d46615481f2646741c39e8f0c59e
+{% endblock %}
