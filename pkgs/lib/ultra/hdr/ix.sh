14393202db407445aa9707085976707234a02016
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-08-29 12:57:28 +0300

    better

--- pkgs/lib/ultra/hdr/ix.sh
+++ pkgs/lib/ultra/hdr/ix.sh
@@ -1,20 +1,12 @@
-{% extends '//die/c/cmake.sh' %}
-
-{% block fetch %}
-https://github.com/google/libultrahdr/archive/refs/tags/v1.1.1.tar.gz
-sha:5c2605bfa8cc56027892e66db8317784949e79bccf197dbcc5c48c8b94f86f32
-{% endblock %}
-
-{% block lib_deps %}
-lib/c
-lib/jpeg
-{% endblock %}
+{% extends 't/ix.sh' %}
 
 {% block cmake_flags %}
+{{super()}}
 UHDR_BUILD_EXAMPLES=OFF
 BUILD_SHARED_LIBS=ON
 {% endblock %}
 
 {% block build_flags %}
+{{super()}}
 wrap_cc
 {% endblock %}
