770f3ebeece29dd82d73b316b3ead36667a90bd1
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-08-29 11:06:25 +0300

    upver

--- pkgs/bin/screen/ix.sh
+++ pkgs/bin/screen/ix.sh
@@ -1,16 +1,23 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-http://ftp.gnu.org/gnu/screen/screen-4.9.1.tar.gz
-sha:26cef3e3c42571c0d484ad6faf110c5c15091fbf872b06fa7aa4766c7405ac69
+#http://ftp.gnu.org/gnu/screen/screen-4.9.1.tar.gz
+#sha:26cef3e3c42571c0d484ad6faf110c5c15091fbf872b06fa7aa4766c7405ac69
+http://ftp.gnu.org/gnu/screen/screen-5.0.0.tar.gz
+sha:f04a39d00a0e5c7c86a55338808903082ad5df4d73df1a2fd3425976aed94971
 {% endblock %}
 
 {% block bld_libs %}
 lib/c
 {% if linux %}
 lib/pam
+lib/bsd/overlay
 {% endif %}
 lib/curses
+{% endblock %}
+
+{% block conf_ver %}
+2/71
 {% endblock %}
 
 {% block setup_target_flags %}
@@ -24,6 +31,5 @@ bld/texinfo
 
 {% block configure_flags %}
 --enable-pam
---disable-socket-dir
 --enable-colors256
 {% endblock %}
