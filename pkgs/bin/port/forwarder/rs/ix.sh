419a7e231941e4bc1131013f5c828b00617159bd
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-02 21:17:11 +0300

    fix

--- pkgs/bin/port/forwarder/rs/ix.sh
+++ pkgs/bin/port/forwarder/rs/ix.sh
@@ -5,7 +5,7 @@ https://github.com/pzmarzly/portforwarder-rs/archive/refs/tags/0.1.0.tar.gz
 {% endblock %}
 
 {% block cargo_sha %}
-11dcdfd92a498765650765c1d19cff783606f9bba2f1c183e14b1889aefce65a
+0ec8fc53234edd8ac6b575d35e66fddffb383af72b61555297019cdc4cd668d1
 {% endblock %}
 
 {% block install %}
