c270ac8a358b8a9604f22bfadcf2034728163daa
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-03 20:59:52 +0300

    upver

--- pkgs/lib/evdev/ix.sh
+++ pkgs/lib/evdev/ix.sh
@@ -1,20 +1,11 @@
-{% extends '//die/c/meson.sh' %}
-
-{% block fetch %}
-https://www.freedesktop.org/software/libevdev/libevdev-1.13.2.tar.xz
-sha:3eca86a6ce55b81d5bce910637fc451c8bbe373b1f9698f375c7f1ad0de3ac48
-{% endblock %}
+{% extends 't/ix.sh' %}
 
 {% block meson_flags %}
-tests=disabled
-documentation=disabled
-{% endblock %}
-
-{% block lib_deps %}
-lib/c
-lib/kernel
+{{super()}}
+tools=disabled
 {% endblock %}
 
 {% block env %}
+{{super()}}
 export CPPFLAGS="-I${out}/include/libevdev-1.0 \${CPPFLAGS}"
 {% endblock %}
