c43e857528108946cba65fa5bc629c2dec17ba77
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-03 09:43:14 +0300

    purge

--- pkgs/bin/kernel/6/7/ver.sh
+++
@@ -1,9 +0,0 @@
-{% block kernel_version %}
-6.7.12
-{% endblock %}
-
-{% block kernel_sha %}
-6c2979e3948806a0dbacba193f8453ea42c179c1eb9f6136e3c35d87e5707984
-{% endblock %}
-
-{% include '//bin/kernel/t/ver.sh' %}
--- pkgs/bin/kernel/6/8/ver.sh
+++
@@ -1,9 +0,0 @@
-{% block kernel_version %}
-6.8.9
-{% endblock %}
-
-{% block kernel_sha %}
-f905f1238ea7a8e85314bacf283302e8097006010d25fcea726d0de0ea5bc9b6
-{% endblock %}
-
-{% include '//bin/kernel/t/ver.sh' %}
