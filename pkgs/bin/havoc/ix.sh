afaa6e15223ad08b0356fed4a297724430aefab3
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-04 10:42:56 +0300

    up

--- pkgs/bin/havoc/ix.sh
+++ pkgs/bin/havoc/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/make.sh' %}
 
 {% block fetch %}
-https://github.com/ii8/havoc/archive/refs/tags/0.5.0.tar.gz
-sha:3b0d37b13e3ca21f5e22f1e62a32fe736119cd5c601307c4000bed5fee179f77
+https://github.com/ii8/havoc/archive/refs/tags/0.6.0.tar.gz
+sha:3f6538eb8a2b5846459f38c283eef2c8152886a4ed079e1038b02d0c31a3247c
 {% endblock %}
 
 {% block bld_libs %}
