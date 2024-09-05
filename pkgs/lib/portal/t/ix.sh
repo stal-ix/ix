c45d5e7cbaa1c5fd7df0ed8184eb5e8d494b000a
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-05 20:19:14 +0300

    upver

--- pkgs/lib/portal/t/ix.sh
+++ pkgs/lib/portal/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://github.com/flatpak/libportal/archive/refs/tags/0.8.0.tar.gz
-sha:ff98d6376980a698273023eda1a93487014d51aaac6a54c6516a6954f2b14188
+https://github.com/flatpak/libportal/archive/refs/tags/0.8.1.tar.gz
+sha:279cd848c41655964ea5109fd50ddf01ba414601d5265d1d16b8409ff54217f8
 {% endblock %}
 
 {% block lib_deps %}
