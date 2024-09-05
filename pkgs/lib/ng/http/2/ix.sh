8c9b0809d79212001d5fa734333abad7140b5bc5
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-05 18:52:15 +0300

    fix

--- pkgs/lib/ng/http/2/ix.sh
+++ pkgs/lib/ng/http/2/ix.sh
@@ -10,6 +10,18 @@
 NGHTTP2_STATICLIB=1
 {% endblock %}
 
+{% block c_rename_symbol %}
+{{super()}}
+sf_base64decode
+sf_parser_dict
+sf_parser_init
+sf_parser_inner_list
+sf_parser_item
+sf_parser_list
+sf_parser_param
+sf_unescape
+{% endblock %}
+
 {% block env %}
 {{super()}}
 export CPPFLAGS="-DNGHTTP2_STATICLIB=1 \${CPPFLAGS}"
--- pkgs/lib/ng/http/3/ix.sh
+++ pkgs/lib/ng/http/3/ix.sh
@@ -5,6 +5,18 @@ ENABLE_LIB_ONLY=ON
 {{super()}}
 {% endblock %}
 
+{% block c_rename_symbol %}
+{{super()}}
+sf_base64decode
+sf_parser_dict
+sf_parser_init
+sf_parser_inner_list
+sf_parser_item
+sf_parser_list
+sf_parser_param
+sf_unescape
+{% endblock %}
+
 {% block env %}
 {{super()}}
 export CPPFLAGS="-DNGHTTP3_STATICLIB=1 \${CPPFLAGS}"
