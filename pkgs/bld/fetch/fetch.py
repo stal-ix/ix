bb87670d58dbad72dbcf7950f11e7b3afd1de8b4
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-06 20:08:18 +0300

    sane defaults

--- pkgs/bld/fetch/fetch.py
+++ pkgs/bld/fetch/fetch.py
@@ -5,18 +5,11 @@ import sys
 import time
 import subprocess
 
-import urllib.error as ue
-import urllib.request as ur
-
-def fetch_url_wget(url, out):
-    return subprocess.check_call(['wget', '--no-check-certificate', '-O', out, url], shell=False)
-
 def fetch_url_curl(url, out):
     return subprocess.check_call(['curl', '-k', '-L', '--output', out, url], shell=False)
 
 def iter_meth():
     while True:
-        yield fetch_url_wget
         yield fetch_url_curl
 
 def fetch_url(url, out):
@@ -32,8 +25,9 @@ def fetch_url(url, out):
                 raise e
 
             print(f'{e}, will sleep for {tout}')
+
             time.sleep(tout)
-            tout = min(60.0, tout * 1.3)
+            tout = min(60.0, tout * 1.5)
 
 def fetch(url):
     return fetch_url(url, os.path.basename(url))
