eea83aba36002580205ffc215c61599bc51d9a49
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-08 08:09:23 +0300

    less deps

--- pkgs/bld/texinfo/ix.sh
+++ pkgs/bld/texinfo/ix.sh
@@ -1,6 +1,7 @@
 {% extends '//die/hub.sh' %}
 
 {% block run_deps %}
-#bld/fake/er(tool_name=texinfo)
-bin/tex/info(std_box=bld/boot/box,intl_ver=no,curses=no)
+bld/fake/er(tool_name=texinfo)
+bld/fake/er(tool_name=makeinfo)
+#bin/tex/info(std_box=bld/boot/box,intl_ver=no,curses=no)
 {% endblock %}
