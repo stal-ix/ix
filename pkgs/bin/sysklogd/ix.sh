53bb3c76236abac7061788930ac654a7cf9f4c36
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-08 10:52:57 +0300

    up

--- pkgs/bin/sysklogd/ix.sh
+++ pkgs/bin/sysklogd/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://github.com/troglobit/sysklogd/archive/refs/tags/v2.6.1.tar.gz
-sha:304c76a11703b525879c8423e3816c14f49e94d6e5b1839225245986400d3399
+https://github.com/troglobit/sysklogd/archive/refs/tags/v2.6.2.tar.gz
+sha:3aa2710f37ac286a83fa646b260742d1ad7d9024419946393392a5a604091b82
 {% endblock %}
 
 {% block bld_libs %}
