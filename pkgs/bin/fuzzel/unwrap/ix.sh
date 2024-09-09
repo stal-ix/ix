f74f0cb4e53d221d91ffa2bd09067bccd726b2e1
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-09 12:59:14 +0300

    upver

--- pkgs/bin/fuzzel/unwrap/ix.sh
+++ pkgs/bin/fuzzel/unwrap/ix.sh
@@ -1,14 +1,15 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://codeberg.org/dnkl/fuzzel/archive/1.10.2.tar.gz
-sha:5362175f301af3f27c23138ac339294ce33dff61720ffc7eb13e78ec56f60a8b
+https://codeberg.org/dnkl/fuzzel/archive/1.11.0.tar.gz
+sha:4071728173dddac0df3d1b0af08ed92c4fdd4e86ff6bcf106d255a0acb67efdb
 {% endblock %}
 
 {% block bld_libs %}
 lib/c
 lib/png
 lib/cairo
+lib/kernel
 lib/tllist
 lib/fcft/3
 lib/pixman
