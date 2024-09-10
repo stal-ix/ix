75ef0748ef1387ecdf996bfd829a0a75f5deec2f
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-10 10:58:45 +0300

    up

--- pkgs/bin/graphviz/ix.sh
+++ pkgs/bin/graphviz/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://gitlab.com/graphviz/graphviz/-/archive/12.1.0/graphviz-12.1.0.tar.bz2
-sha:ad2023c23935397d4b5a34c14682f8098d2f20d2144c63d20d05be372757fdb1
+https://gitlab.com/graphviz/graphviz/-/archive/12.1.1/graphviz-12.1.1.tar.bz2
+sha:c4083e7b746e53f02f6e86de5d5d207a4e2fd0390b7a30476ff0e368d2d8b695
 {% endblock %}
 
 {% block make_no_thrs %}
