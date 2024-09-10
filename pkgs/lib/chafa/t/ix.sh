6e5c271c8c59b667ed42f848eae96c9a42c1150f
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-10 11:05:10 +0300

    upver

--- pkgs/lib/chafa/t/ix.sh
+++ pkgs/lib/chafa/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://github.com/hpjansson/chafa/archive/refs/tags/1.14.3.tar.gz
-sha:bbb5309b3bef5b2107eb7f391ac87dd84c9917dff0331b46538144fb63d1ec87
+https://github.com/hpjansson/chafa/archive/refs/tags/1.14.4.tar.gz
+sha:65b084173eb904c3e0b4eafd561cf0f676a17fe19b0d47b98118808f0646c92e
 {% endblock %}
 
 {% block lib_deps %}
