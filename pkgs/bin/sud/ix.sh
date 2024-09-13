f13cd9d51c0bd309a2a1cf1d12a0f6b4a7e6abf4
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-13 19:38:18 +0300

    fix

--- pkgs/bin/sud/ix.sh
+++ pkgs/bin/sud/ix.sh
@@ -5,5 +5,5 @@ bin/setcwd
 bin/sud/client
 bin/sud/server
 bin/sud/scripts
-etc/services/runit(srv_dir=sud,srv_command=exec sud_server -R -F -E -s -g -j -k -m -p 127.0.0.1:1 -P /proc/self/fd/1)
+etc/services/runit(srv_deps=bin/sud/server,srv_dir=sud,srv_command=exec sud_server -R -F -E -s -g -j -k -m -p 127.0.0.1:1 -P /proc/self/fd/1)
 {% endblock %}
