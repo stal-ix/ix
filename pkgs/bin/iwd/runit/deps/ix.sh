4c75f50b20e2d1878925055aaf406f1da69ca423
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-13 19:33:33 +0300

    better

---
+++ pkgs/bin/iwd/runit/deps/ix.sh
@@ -0,0 +1,6 @@
+{% extends '//die/hub.sh' %}
+
+{% block run_deps %}
+bin/iwd/d
+bin/iwd/runit/conf
+{% endblock %}
--- pkgs/bin/iwd/runit/ix.sh
+++ pkgs/bin/iwd/runit/ix.sh
@@ -1,7 +1,6 @@
 {% extends '//die/hub.sh' %}
 
 {% block run_deps %}
-bin/iwd/d
-bin/iwd/runit/conf
-etc/services/runit(srv_dir=iwd,srv_command=exec iwd)
+bin/iwd/runit/deps
+etc/services/runit(srv_deps=bin/iwd/runit/deps,srv_dir=iwd,srv_command=exec iwd)
 {% endblock %}
