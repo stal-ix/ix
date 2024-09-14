ad94e79c66ffe4c20ec2003fc966b8d113c54074
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-14 07:24:03 +0300

    fix

--- pkgs/bin/dnsmasq/runit/ix.sh
+++ pkgs/bin/dnsmasq/runit/ix.sh
@@ -3,5 +3,5 @@
 {% block run_deps %}
 etc/user/dnsmasq
 bin/dnsmasq/lite
-bin/dnsmasq/runit/script(srv_dir=dnsmasq)
+bin/dnsmasq/runit/script(srv_deps=bin/dnsmasq/lite,srv_dir=dnsmasq)
 {% endblock %}
