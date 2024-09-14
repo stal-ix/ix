f36f913ee1740264880fe6f3b3dad119e6476457
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-14 19:25:49 +0300

    upver

--- pkgs/bin/niri/unwrap/ix.sh
+++ pkgs/bin/niri/unwrap/ix.sh
@@ -1,11 +1,11 @@
 {% extends '//die/rust/cargo.sh' %}
 
 {% block cargo_url %}
-https://github.com/YaLTeR/niri/archive/refs/tags/v0.1.8.tar.gz
+https://github.com/YaLTeR/niri/archive/refs/tags/v0.1.9.tar.gz
 {% endblock %}
 
 {% block cargo_sha %}
-cf0de268fd018cacabbfa6acdf5ba00f41677b531f204e03ef697a4eeda01de9
+46584aa2638daee532809f35f5e63c6d036f54a86c30a97a7ffa7223898f4b03
 {% endblock %}
 
 {% block lib_deps %}
@@ -15,10 +15,12 @@ lib/glib
 lib/pango
 lib/cairo
 lib/input
+lib/bzip/2
 lib/opengl
 lib/drivers/3d
 lib/wayland/dl
 lib/mesa/egl/dl
+lib/display/info
 lib/xkb/common/dl
 lib/wayland/dl/server
 {% endblock %}
