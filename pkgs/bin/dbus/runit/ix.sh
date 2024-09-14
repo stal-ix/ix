11bc1544356affa103ce57955046c3e32b2689fa
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-14 07:27:37 +0300

    fix

--- pkgs/bin/dbus/runit/ix.sh
+++ pkgs/bin/dbus/runit/ix.sh
@@ -1,7 +1,6 @@
 {% extends '//die/hub.sh' %}
 
 {% block run_deps %}
-bin/dbus/sys
-bin/dbus/runit/scripts
-etc/services/runit(srv_dir=dbus,srv_command=exec chrt -f 10 dbus-daemon --config-file=/etc/dbus.conf --nopidfile --syslog --nofork)
+bin/dbus/runit/deps
+etc/services/runit(srv_deps=bin/dbus/runit/deps,srv_dir=dbus,srv_command=exec chrt -f 10 dbus-daemon --config-file=/etc/dbus.conf --nopidfile --syslog --nofork)
 {% endblock %}
