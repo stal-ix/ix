0402a52784626471932360003e7130d9cec86082
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-15 18:26:20 +0300

    better

--- pkgs/lib/vte/3/ix.sh
+++ pkgs/lib/vte/3/ix.sh
@@ -1,18 +1,52 @@
 {% extends '//lib/vte/t/ix.sh' %}
 
 {% block fetch %}
-https://gitlab.gnome.org/GNOME/vte/-/archive/0.76.3/vte-0.76.3.tar.bz2
-sha:fc345b1d493095babf12bf6d812b60447ebd2747a06ef0947c6d7e20c4c8c317
+https://gitlab.gnome.org/GNOME/vte/-/archive/0.78.0/vte-0.78.0.tar.bz2
+sha:f822dbcc1143afb7d542be6af9c1d4b7083322055aa4afc6a10b8eca290e0649
 {% endblock %}
 
 {% block lib_deps %}
 {{super()}}
 lib/lz4
 lib/gtk/3
+lib/fast/float
 {% endblock %}
 
 {% block meson_flags %}
 {{super()}}
 gtk3=true
 gtk4=false
+{% endblock %}
+
+{% block bld_tool %}
+{{super()}}
+bld/prepend
+{% endblock %}
+
+{% block build_flags %}
+{{super()}}
+shut_up
+{% endblock %}
+
+{% block patch %}
+{{super()}}
+echo 'int main() {}'  > src/color-test.cc
+prepend src/icu-glue.hh << EOF
+#include <string_view>
+EOF
+cat - src/termprops.hh << EOF >> _
+#pragma once
+#include <fast_float/fast_float.h>
+#include <charconv>
+namespace std {
+    inline std::from_chars_result from_chars(const char* first, const char* last, double& value, std::chars_format fmt = std::chars_format::general) {
+        auto ret = fast_float::from_chars(first, last, value);
+        return {ret.ptr, ret.ec};
+    }
+}
+EOF
+mv _ src/termprops.hh
+prepend src/minifont.cc << EOF
+#include <algorithm>
+EOF
 {% endblock %}
