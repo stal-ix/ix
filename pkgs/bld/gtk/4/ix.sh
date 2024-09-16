5eb3420a718470f0cbd900784415999d0894d66e
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-16 20:48:37 +0300

    better

--- pkgs/bld/gtk/4/ix.sh
+++ pkgs/bld/gtk/4/ix.sh
@@ -1,10 +1,27 @@
-{% extends '//lib/gtk/4/t/ix.sh' %}
+{% extends '//die/c/ix.sh' %}
 
-{% block ninja_build_targets %}
-tools/gtk4-update-icon-cache
+{% block fetch %}
+{% include '//lib/gtk/4/t/ver.sh' %}
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/gtk/4
+{% endblock %}
+
+{% block patch %}
+sed -e 's|.*GETTEXT_PACKAGE.*||' -i tools/updateiconcache.c
+cat << EOF > tools/config.h
+#pragma once
+#define HAVE_UNISTD_H 1
+EOF
+{% endblock %}
+
+{% block build %}
+cc -o gtk4-update-icon-cache -Igtk tools/updateiconcache.c
 {% endblock %}
 
 {% block install %}
 mkdir ${out}/bin
-cp ${tmp}/obj/tools/gtk4-update-icon-cache ${out}/bin/
+cp gtk4-update-icon-cache ${out}/bin/
 {% endblock %}
