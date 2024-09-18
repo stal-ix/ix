2abfa862b7ab3c89a11e05bbe7838260af7e66d3
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-18 21:56:33 +0300

    up

--- pkgs/bin/gcc/11/unwrap/ix.sh
+++ pkgs/bin/gcc/11/unwrap/ix.sh
@@ -1,6 +1,6 @@
 {% extends '//bin/gcc/10/unwrap/ix.sh' %}
 
 {% block fetch %}
-http://mirror.koddos.net/gcc/releases/gcc-11.4.0/gcc-11.4.0.tar.xz
-sha:3f2db222b007e8a4a23cd5ba56726ef08e8b1f1eb2055ee72c1402cea73a8dd9
+http://mirror.koddos.net/gcc/releases/gcc-11.5.0/gcc-11.5.0.tar.xz
+sha:a6e21868ead545cf87f0c01f84276e4b5281d672098591c1c896241f09363478
 {% endblock %}
