0482915981d661af9deb98aaae962b23c39b4ca0
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-20 19:05:52 +0300

    better

--- pkgs/set/ci/unwrap/ix.sh
+++ pkgs/set/ci/unwrap/ix.sh
@@ -2,10 +2,10 @@
 
 {% block run_deps %}
 #set/ci/unwrap/wasi/32(target=wasi32)
-set/ci/unwrap/mingw/w64(target=mingw-w64-x86_64)
+set/ci/unwrap/mingw/w64(target=mingw-w64-x86_64,clang_ver=18)
 set/ci/unwrap/linux/x86_64(target=linux-x86_64)
 set/ci/unwrap/linux/aarch64(target=linux-aarch64)
-#set/ci/unwrap/linux/riscv64(target=linux-riscv64)
-#set/ci/unwrap/darwin/arm64(target=darwin-arm64)
-#set/ci/unwrap/darwin/x86_64(target=darwin-x86_64)
+set/ci/unwrap/linux/riscv64(target=linux-riscv64,clang_ver=18)
+set/ci/unwrap/darwin/arm64(target=darwin-arm64,clang_ver=18)
+set/ci/unwrap/darwin/x86_64(target=darwin-x86_64,clang_ver=18)
 {% endblock %}
