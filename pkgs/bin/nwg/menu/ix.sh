f2a1f57612b35360f5bfd6c4f4d48b0f04cb6a20
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-21 14:25:06 +0300

    up

--- pkgs/bin/nwg/menu/ix.sh
+++ pkgs/bin/nwg/menu/ix.sh
@@ -1,11 +1,15 @@
 {% extends '//bin/nwg/t/ix.sh' %}
 
 {% block go_url %}
-https://github.com/nwg-piotr/nwg-menu/archive/refs/tags/v0.1.1.tar.gz
+https://github.com/nwg-piotr/nwg-menu/archive/refs/tags/v0.1.5.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-db511009ace82f4bc945541566d71f1cf50298da2b20fcfd2b3cde64370e9ca8
+7e52410806370e56cef2783599e1daaa82a61c7b0421101d1505727ac14e9e06
+{% endblock %}
+
+{% block go_tool %}
+bin/go/lang/23
 {% endblock %}
 
 {% block install %}
