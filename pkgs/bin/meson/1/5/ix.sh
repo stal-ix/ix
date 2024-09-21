26966e27b9e7d56a480693438fd0843391dc13d4
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-21 11:37:45 +0300

    up

--- pkgs/bin/meson/1/5/ix.sh
+++ pkgs/bin/meson/1/5/ix.sh
@@ -1,6 +1,6 @@
 {% extends '//bin/meson/1/3/ix.sh' %}
 
 {% block fetch %}
-https://github.com/mesonbuild/meson/archive/refs/tags/1.5.0.tar.gz
-sha:781913826fb6f478eb7d77e1942ab3df39444e4c90e9a3523737e215171db469
+https://github.com/mesonbuild/meson/archive/refs/tags/1.5.2.tar.gz
+sha:fb41882bef26ffc02647d9978cba502a4accdf2e94c0a6dc9cc498dd7463381e
 {% endblock %}
