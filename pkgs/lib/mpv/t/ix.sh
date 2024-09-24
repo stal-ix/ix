d463b874804e64f8c162c297d4dd352892ef50b6
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-24 10:45:57 +0300

    up

--- pkgs/lib/mpv/t/ix.sh
+++ pkgs/lib/mpv/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://github.com/mpv-player/mpv/archive/refs/tags/v0.38.0.tar.gz
-sha:86d9ef40b6058732f67b46d0bbda24a074fae860b3eaae05bab3145041303066
+https://github.com/mpv-player/mpv/archive/refs/tags/v0.39.0.tar.gz
+sha:2ca92437affb62c2b559b4419ea4785c70d023590500e8a52e95ea3ab4554683
 {% endblock %}
 
 {% block lib_deps %}
