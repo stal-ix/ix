3e665aac214271129bfa9129125b08fb023fb0ae
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-26 11:54:20 +0300

    up

--- pkgs/lib/uv/ix.sh
+++ pkgs/lib/uv/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/libuv/libuv/archive/refs/tags/v1.48.0.tar.gz
-sha:8c253adb0f800926a6cbd1c6576abae0bc8eb86a4f891049b72f9e5b7dc58f33
+https://github.com/libuv/libuv/archive/refs/tags/v1.49.0.tar.gz
+sha:a10656a0865e2cff7a1b523fa47d0f5a9c65be963157301f814d1cc5dbd4dc1d
 {% endblock %}
 
 {% block lib_deps %}
