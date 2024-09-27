8c77269bf5b20ea81f9096efdc3fbfb916730872
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-27 17:51:04 +0300

    add

---
+++ pkgs/bin/postgresql/17/ix.sh
@@ -0,0 +1,6 @@
+{% extends '//bin/postgresql/t/ix.sh' %}
+
+{% block fetch %}
+https://ftp.postgresql.org/pub/source/v17.0/postgresql-17.0.tar.bz2
+sha:7e276131c0fdd6b62588dbad9b3bb24b8c3498d5009328dba59af16e819109de
+{% endblock %}
