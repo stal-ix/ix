76eeaf57ec18b85fe8d264a9a65d58b448897a35
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-27 15:02:55 +0300

    up

--- pkgs/aux/ca/bundle/ix.sh
+++ pkgs/aux/ca/bundle/ix.sh
@@ -1,5 +1,5 @@
 {% extends 'nopredict/ix.sh' %}
 
 {% block predict_outputs %}
-[{"path": "share/ssl/cert.pem", "sum": "99443a9bcba583fd4e4dbbdefde364d583e1ea7c7f41e8dc4cd61d85ab68ca54"}]
+[{"path": "share/ssl/cert.pem", "sum": "d8fcfc0c58ce603d1d2aa9ce034a3fdf26b6eb0606770808818d7552a5a75547"}]
 {% endblock %}
