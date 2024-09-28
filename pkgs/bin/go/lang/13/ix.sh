fef525cee71983bba17e5385d09282ebef2a6b96
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-28 18:36:04 +0300

    add

---
+++ pkgs/bin/go/lang/13/ix.sh
@@ -0,0 +1,6 @@
+{% extends '//bin/go/lang/20/ix.sh' %}
+
+{% block fetch %}
+https://github.com/golang/go/archive/refs/tags/go1.13.14.tar.gz
+sha:5f8fd27d469026b05c46001b9553c8681614c5e8cfbce81e82ce28e7a17567a6
+{% endblock %}
--- pkgs/bin/graphviz/ix.sh
+++ pkgs/bin/graphviz/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://gitlab.com/graphviz/graphviz/-/archive/12.1.1/graphviz-12.1.1.tar.bz2
-sha:c4083e7b746e53f02f6e86de5d5d207a4e2fd0390b7a30476ff0e368d2d8b695
+https://gitlab.com/graphviz/graphviz/-/archive/12.1.2/graphviz-12.1.2.tar.bz2
+sha:2829013236e8ad23bfc96307e0d2928ccfb423bdd289808bca38d228038a9bbe
 {% endblock %}
 
 {% block make_no_thrs %}
