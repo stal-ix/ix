483a6d04cad61f1b18876c4b69a17e88520f99e6
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-30 17:23:06 +0300

    better

--- pkgs/set/pg/user/scripts/ix.sh
+++ pkgs/set/pg/user/scripts/ix.sh
@@ -4,7 +4,7 @@
 mkdir ${out}/bin
 cat << EOF > ${out}/bin/s5
 #!/usr/bin/env sh
-exec wirez run -F 127.0.0.1:10000 -L 10.10.0.1:53:127.0.0.1:53/udp -- "\${@}"
+exec wirez run -F lab3.local:8015 -L 10.10.0.1:53:127.0.0.1:53/udp -- "\${@}"
 EOF
 chmod +x ${out}/bin/*
 {% endblock %}
