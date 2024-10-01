6e60b73252e27b985f834e945c7289a90d265201
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-10-01 23:14:25 +0300

    up

--- pkgs/bin/go/lang/23/ix.sh
+++ pkgs/bin/go/lang/23/ix.sh
@@ -1,6 +1,6 @@
 {% extends '//bin/go/lang/22/ix.sh' %}
 
 {% block fetch %}
-https://github.com/golang/go/archive/refs/tags/go1.23.1.tar.gz
-sha:3bf56ef25470175efaa8a3616737d8b65fdaacf814297de77cba3cffdea39323
+https://github.com/golang/go/archive/refs/tags/go1.23.2.tar.gz
+sha:fc3448a68fca887ceb0e0d4357d0ecd05d54a78e052f8667b283e745c87e7f2e
 {% endblock %}
