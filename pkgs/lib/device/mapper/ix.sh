c1532ec3164c42482eedcee5434b143739c7a0e9
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-10-02 19:49:28 +0300

    refac

--- pkgs/lib/device/mapper/ix.sh
+++ pkgs/lib/device/mapper/ix.sh
@@ -1,32 +1,4 @@
-{% extends '//die/c/autorehell.sh' %}
-
-{% block fetch %}
-http://mirrors.kernel.org/sourceware/lvm2/LVM2.2.03.27.tgz
-sha:3133415905b9b46d152d064865d52f32eee4fcbeb0e8a69e3510caeaae0c56a9
-{% endblock %}
-
-{% block lib_deps %}
-lib/c
-lib/udev
-{% endblock %}
-
-{% block bld_libs %}
-lib/kernel
-{% endblock %}
-
-{% block bld_tool %}
-bld/elfutils
-bld/auto/archive
-{% endblock %}
-
-{% block build_flags %}
-wrap_cc
-{% endblock %}
-
-{% block configure_flags %}
---enable-pkgconfig
---enable-static_link
-{% endblock %}
+{% extends 't/ix.sh' %}
 
 {% block make_target %}
 device-mapper
