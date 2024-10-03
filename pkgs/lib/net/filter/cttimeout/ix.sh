95367f6a493fbe83e811188f79371a0ad76c450c
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-10-03 14:51:56 +0300

    add

---
+++ pkgs/lib/net/filter/cttimeout/ix.sh
@@ -0,0 +1,23 @@
+{% extends '//die/c/autorehell.sh' %}
+
+{% block git_repo %}
+git://git.netfilter.org/libnetfilter_cttimeout
+{% endblock %}
+
+{% block git_branch %}
+libnetfilter_cttimeout-1.0.0
+{% endblock %}
+
+{% block git_sha %}
+fb5940f89ad8d5c415a50a5c9851359ae986cb7dcaa9913eef07ef9fc2498107
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+lib/mnl
+{% endblock %}
+
+{% block bld_libs %}
+lib/kernel
+{% endblock %}
+
