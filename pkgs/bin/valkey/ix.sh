010fbf6dfe3ffc3393feed95e89c19b0f185286c
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-10-03 11:49:46 +0300

    up

--- pkgs/bin/valkey/ix.sh
+++ pkgs/bin/valkey/ix.sh
@@ -1,6 +1,6 @@
 {% extends '//bin/redis/t/ix.sh' %}
 
 {% block fetch %}
-https://github.com/valkey-io/valkey/archive/refs/tags/8.0.0.tar.gz
-sha:f87fef2ba81ae4bce891b874fba58cfde2d19370a3bcac20f0e17498b33c33c0
+https://github.com/valkey-io/valkey/archive/refs/tags/8.0.1.tar.gz
+sha:1e1d6dfbed2f932a87afbc7402be050a73974a9b19a9116897e537a6638e5e1d
 {% endblock %}
