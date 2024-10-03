1f7f8dada33fe98a5d94af6c682903bcc409c488
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-10-03 10:53:43 +0300

    up

--- pkgs/lib/nft/nl/ix.sh
+++ pkgs/lib/nft/nl/ix.sh
@@ -5,11 +5,11 @@ git://git.netfilter.org/libnftnl
 {% endblock %}
 
 {% block git_branch %}
-libnftnl-1.2.7
+libnftnl-1.2.8
 {% endblock %}
 
 {% block git_sha %}
-4eada1ef4d2790dd481e0f8a94a6f2c398bd4eea11c8bf002fb1b0c2add987f6
+4372748fa18d5414552a8e3cd4c306440543627a2c6694d92534126aba27cdf9
 {% endblock %}
 
 {% block lib_deps %}
