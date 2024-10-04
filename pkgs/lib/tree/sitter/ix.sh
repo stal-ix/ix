54afbbbc756d56715339e56e1370110112d65c7a
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-10-04 10:47:59 +0300

    up

--- pkgs/lib/tree/sitter/ix.sh
+++ pkgs/lib/tree/sitter/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/make.sh' %}
 
 {% block fetch %}
-https://github.com/tree-sitter/tree-sitter/archive/refs/tags/v0.23.0.tar.gz
-sha:6403b361b0014999e96f61b9c84d6950d42f0c7d6e806be79382e0232e48a11b
+https://github.com/tree-sitter/tree-sitter/archive/refs/tags/v0.24.0.tar.gz
+sha:1f2d1468a959993f67c8bd08d3cc26ef344a9ba52f8c7063e3fa50167516c53f
 {% endblock %}
 
 {% block lib_deps %}
