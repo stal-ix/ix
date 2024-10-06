6f3bc38003ce0b97827d0ac3bf013b6cb727a807
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-10-06 20:17:45 +0300

    up

--- pkgs/bin/wazero/ix.sh
+++ pkgs/bin/wazero/ix.sh
@@ -1,11 +1,11 @@
 {% extends '//die/go/build.sh' %}
 
 {% block go_url %}
-https://github.com/tetratelabs/wazero/archive/refs/tags/v1.8.0.tar.gz
+https://github.com/tetratelabs/wazero/archive/refs/tags/v1.8.1.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-b1e2404b447f7431ab702a31382efd02cb3b1b4a2734b6fe4d7f775c928fdb97
+c4d6d7a722fc974ea0942f8349d45d392f8e97554b5726a6c53ed65de474907a
 {% endblock %}
 
 {% block unpack %}
