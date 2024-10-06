969aab09acc1b7b2323c222566d4b15a4e6bc803
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-10-06 22:55:18 +0300

    up

--- pkgs/bin/cage/ix.sh
+++ pkgs/bin/cage/ix.sh
@@ -1,13 +1,13 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://github.com/Hjdskes/cage/archive/refs/tags/v0.1.4.tar.gz
-sha:dfe27fb0c7d43db72d6c82f01e2736580a0791a23ba69d7b56285d08af98ad90
+https://github.com/cage-kiosk/cage/archive/refs/tags/v0.2.0.tar.gz
+sha:9d3f659e0f19636a958f9e1bf4d22268d7e2705d7e2989888401ec443c1cb6c3
 {% endblock %}
 
 {% block bld_libs %}
 lib/c
-lib/wlroots/14
+lib/wlroots/18
 {% endblock %}
 
 {% block bld_tool %}
