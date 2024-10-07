a5d0dfe1ae2ddbd743d9ccd4fb75b534ddf19a51
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-10-07 12:56:11 +0300

    up

--- pkgs/lib/ucontext/stock/ix.sh
+++ pkgs/lib/ucontext/stock/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://github.com/kaniini/libucontext/archive/refs/tags/libucontext-1.3.1.tar.gz
-sha:1243ee9f03ad38e624f6844427b7bc1f0a05aa5de70f15f3b03805a364b971d6
+https://github.com/kaniini/libucontext/archive/refs/tags/libucontext-1.3.2.tar.gz
+sha:4faf1838a15d61efe27ddac24fded2c290929eb3a1fefc72f952ae96d5bda006
 {% endblock %}
 
 {% block lib_deps %}
