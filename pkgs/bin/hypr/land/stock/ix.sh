9243f8db747942759c56cf73839189d50781cbb5
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-10-07 14:44:01 +0300

    better

--- pkgs/bin/hypr/land/stock/ix.sh
+++ pkgs/bin/hypr/land/stock/ix.sh
@@ -37,10 +37,6 @@ bld/fakegit
 bin/hypr/wayland/scanner
 {% endblock %}
 
-{% block cpp_defines %}
-__time_t=time_t
-{% endblock %}
-
 {% block patch %}
 sed -e 's|.*define PI .*||' -i src/macros.hpp
 sed -e 's|PI |M_PI |g' -i src/config/ConfigManager.cpp
@@ -48,12 +44,8 @@ sed -e 's|PI |M_PI |g' -i src/render/OpenGL.cpp
 sed -e 's|PI |M_PI |g' -i src/desktop/Window.cpp
 sed -e 's|PI |M_PI |g' -i src/debug/HyprCtl.cpp
 
-sed -e "s|subproject.*wlroots.*|dependency('wlroots')|" \
-    -e 's|have_xwlr = .*|have_xwlr = false|' \
+sed -e 's|have_xwlr = .*|have_xwlr = false|' \
     -i meson.build
-
-sed -e 's|.get_variable.*wlroots.*|,|' \
-    -i src/meson.build
 
 sed -e 's|) {|) const {|' -i src/helpers/WLClasses.hpp
 
