c7649eaeea69a2b727bb0f106c931c502192c355
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-10-07 23:43:48 +0300

    up

--- pkgs/bin/sing/box/ix.sh
+++ pkgs/bin/sing/box/ix.sh
@@ -1,11 +1,11 @@
 {% extends '//die/go/build.sh' %}
 
 {% block go_url %}
-https://github.com/SagerNet/sing-box/archive/refs/tags/v1.9.6.tar.gz
+https://github.com/SagerNet/sing-box/archive/refs/tags/v1.9.7.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-5ac7fa088f25c64cbc01b9802f0c7d9889f20af0f7daa3d94645334a5474199e
+8b6edc409405f4eff079067139c38cff3f20e5d6f8d885c1ac33a3779045a18e
 {% endblock %}
 
 {% block step_unpack %}
