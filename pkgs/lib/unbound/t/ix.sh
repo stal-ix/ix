f77eef040c6f5389312527ea482391d9f15b68f2
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-10-17 20:48:13 +0300

    up

--- pkgs/lib/unbound/t/ix.sh
+++ pkgs/lib/unbound/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://github.com/NLnetLabs/unbound/archive/refs/tags/release-1.21.1.tar.gz
-sha:2b729bcbe77bf8e6f60cd2fbc578f7854837843c993e97615110ec4cdfb0a579
+https://github.com/NLnetLabs/unbound/archive/refs/tags/release-1.22.0.tar.gz
+sha:4e32a36d57cda666b1c8ee02185ba73462330452162d1b9c31a5b91a853ba946
 {% endblock %}
 
 {% block lib_deps %}
