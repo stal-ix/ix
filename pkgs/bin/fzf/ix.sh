7da7dc81ebd7d8be904d91cd3e1974249d019d3f
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-10-27 11:54:06 +0300

    up

--- pkgs/bin/fzf/ix.sh
+++ pkgs/bin/fzf/ix.sh
@@ -1,16 +1,11 @@
 {% extends '//die/go/build.sh' %}
 
 {% block go_url %}
-https://github.com/junegunn/fzf/archive/refs/tags/v0.55.0.tar.gz
+https://github.com/junegunn/fzf/archive/refs/tags/v0.56.0.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-e0450bf924de079d8454553e0c75abd8226ee00d2c85d88c1ab9ae4d95c64172
-{% endblock %}
-
-{% block go_refine %}
-sed -e 's|go 1.17|go 1.21|' -i go.mod
-go mod tidy -compat=1.21
+b9859a78d4b77367a66a31af83fdbd291ba91e7899a9284cd94080d449131694
 {% endblock %}
 
 {% block install %}
