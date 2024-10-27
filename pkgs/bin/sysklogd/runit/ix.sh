df25c7c1b01244c80d28ba2934b157390910ee76
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-10-27 12:40:52 +0300

    better

--- pkgs/bin/sysklogd/runit/ix.sh
+++ pkgs/bin/sysklogd/runit/ix.sh
@@ -2,5 +2,5 @@
 
 {% block run_deps %}
 bin/sysklogd/runit/deps
-etc/services/runit(srv_deps=bin/sysklogd/runit/deps,srv_dir=syslogd,srv_command=exec syslogd -ss -F -f /etc/syslog.conf -P /proc/self/fd/1 -C /dev/null)
+etc/services/runit(srv_deps=bin/sysklogd/runit/deps,srv_dir=syslogd,srv_command=exec syslogd -ss -k -K -F -f /etc/syslog.conf -P /proc/self/fd/1 -C /dev/null)
 {% endblock %}
