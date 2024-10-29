c82a9d0ac02314200b8a9471eee5a035194ecfbd
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-10-29 12:31:35 +0300

    add

--- pkgs/bin/geesefs/ix.sh
+++ pkgs/bin/geesefs/ix.sh
@@ -1,4 +1,19 @@
 {% extends '//die/go/build.sh' %}
 
 {% block go_url %}
+https://github.com/yandex-cloud/geesefs/archive/refs/tags/v0.42.0.tar.gz
+{% endblock %}
 
+{% block go_sha %}
+qw
+{% endblock %}
+
+{% block go_tool %}
+bld/git
+bin/go/lang/23
+{% endblock %}
+
+{% block install %}
+mkdir ${out}/bin
+cp geesefs ${out}/bin/
+{% endblock %}
