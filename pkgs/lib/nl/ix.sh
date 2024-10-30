1b8e74c3bc1fb29b024e113ef4a1dd5d56e8a88d
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-10-30 17:05:38 +0300

    up

--- pkgs/lib/nl/ix.sh
+++ pkgs/lib/nl/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://github.com/thom311/libnl/releases/download/libnl3_9_0/libnl-3.9.0.tar.gz
-sha:aed507004d728a5cf11eab48ca4bf9e6e1874444e33939b9d3dfed25018ee9bb
+https://github.com/thom311/libnl/releases/download/libnl3_11_0/libnl-3.11.0.tar.gz
+sha:2a56e1edefa3e68a7c00879496736fdbf62fc94ed3232c0baba127ecfa76874d
 {% endblock %}
 
 {% block lib_deps %}
