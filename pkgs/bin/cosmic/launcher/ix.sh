e59bcf64f4ca46305414f0422d2ef8e1d6ff753d
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-11-02 14:08:34 +0300

    add

--- pkgs/bin/cosmic/launcher/ix.sh
+++ pkgs/bin/cosmic/launcher/ix.sh
@@ -1,4 +1,4 @@
-{% extends '//die/rust/cargo.sh' %}
+{% extends '//bin/cosmic/t/ix.sh' %}
 
 {% block cargo_url %}
 https://github.com/pop-os/cosmic-launcher/archive/refs/tags/epoch-1.0.0-alpha.2.tar.gz
@@ -6,17 +6,6 @@ https://github.com/pop-os/cosmic-launcher/archive/refs/tags/epoch-1.0.0-alpha.2.
 
 {% block cargo_sha %}
 87ff5d16a1bbd581ed758d5c15cf65cab327e93f4b0c9cd383b12b701ef7d364
-{% endblock %}
-
-{% block bld_tool %}
-bin/just
-bld/fakegit
-{% endblock %}
-
-{% block build %}
-export VERGEN_GIT_COMMIT_DATE="x"
-export VERGEN_GIT_SHA="x"
-{{super()}}
 {% endblock %}
 
 {% block bld_libs %}
@@ -27,8 +16,4 @@ lib/bzip/2
 lib/wayland/dl
 lib/drivers/3d
 lib/xkb/common/dl
-{% endblock %}
-
-{% block install %}
-just --set prefix ${out} install
 {% endblock %}
