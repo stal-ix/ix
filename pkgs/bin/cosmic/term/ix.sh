bb6b4bf576d9c8a820c9ad8c935e0f794033bf59
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-11-02 19:53:11 +0300

    better

--- pkgs/bin/cosmic/term/ix.sh
+++ pkgs/bin/cosmic/term/ix.sh
@@ -1,25 +1,11 @@
-{% extends '//die/rust/cargo.sh' %}
+{% extends '//bin/cosmic/t/ix.sh' %}
 
 {% block cargo_url %}
-https://github.com/pop-os/cosmic-term/archive/90179d5a5f837b60406bcd20315bb8a81cc3bb7d.zip
+https://github.com/pop-os/cosmic-term/archive/refs/tags/epoch-1.0.0-alpha.3.tar.gz
 {% endblock %}
 
 {% block cargo_sha %}
-d68aa5d12eadee32bac71e8fb52fed47ded5602b182cfd147e2d28fa39740000
-{% endblock %}
-
-{% block rustc_ver %}
-75
-{% endblock %}
-
-{% block bld_tool %}
-bld/fakegit
-{% endblock %}
-
-{% block build %}
-export VERGEN_GIT_COMMIT_DATE="x"
-export VERGEN_GIT_SHA="x"
-{{super()}}
+b6b79bcdc654aaf5fb5df1132d8d348505e05d2ba8fd1873d679470c6381dd28
 {% endblock %}
 
 {% block bld_libs %}
@@ -29,9 +15,4 @@ lib/mesa/gl/dl
 lib/drivers/3d
 lib/mesa/egl/dl
 lib/xkb/common/dl
-{% endblock %}
-
-{% block install %}
-mkdir ${out}/bin
-cp ${tmp}/release/cosmic-term ${out}/bin/
 {% endblock %}
