6216548de9845c0df321459715d0fcef40a55903
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-11-03 10:18:16 +0300

    add

---
+++ pkgs/bin/cosmic/settings/ix.sh
@@ -0,0 +1,11 @@
+{% extends '//bin/cosmic/t/ix.sh' %}
+
+{% block cargo_url %}
+https://github.com/pop-os/cosmic-settings/archive/refs/tags/epoch-1.0.0-alpha.3.tar.gz
+{% endblock %}
+
+{% block cargo_sha %}
+83929860d20e1584f910bfd25f0624083fe620a282658ae551cebb3ca2cb925c
+{% endblock %}
+
+{# hard pipewire pulse deps #}
