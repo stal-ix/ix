41af6f4a7f87e645abcaa612e027e4075296bb3c
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-11-04 13:29:24 +0300

    better

--- pkgs/bin/cosmic/idle/ix.sh
+++ pkgs/bin/cosmic/idle/ix.sh
@@ -7,3 +7,8 @@ https://github.com/pop-os/cosmic-idle/archive/refs/tags/epoch-1.0.0-alpha.3.tar.
 {% block cargo_sha %}
 484b3315084bf8ef4b07c86c5bad0287433b8b4388dbe0d0dc5e3a5192495eaf
 {% endblock %}
+
+{% block bld_libs %}
+lib/wayland/dl
+lib/xkb/common/dl
+{% endblock %}
