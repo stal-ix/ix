23c7f783299e4277e3112b2037b4ed6826e11e51
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-11-05 11:26:20 +0300

    refac

--- pkgs/lib/x264/ix.sh
+++ pkgs/lib/x264/ix.sh
@@ -1,41 +1,15 @@
-{% extends '//die/c/make.sh' %}
-
-{% block git_repo %}
-https://code.videolan.org/videolan/x264
-{% endblock %}
-
-{% block git_commit %}
-a8b68ebfaa68621b5ac8907610d3335971839d52
-{% endblock %}
-
-{% block git_sha %}
-654c686533c2a1dcf60611f7245edb90cdbae47653f1ed5b39cdadba33313ea8
-{% endblock %}
-
-{% block lib_deps %}
-lib/c
-{% endblock %}
-
-{% block bld_tool %}
-bld/nasm
-bld/bash
-bld/elfutils
-{% endblock %}
-
-{% block build_flags %}
-wrap_cc
-{% endblock %}
+{% extends 't/ix.sh' %}
 
 {% block make_target %}
-cli
 lib-static
 {% endblock %}
 
 {% block make_install_target %}
-install-cli
 install-lib-static
 {% endblock %}
 
-{% block configure %}
-bash ./configure --prefix=${out}
+{% block configure_all_flags %}
+{{super()}}
+--disable-cli
+--enable-static
 {% endblock %}
---
+++ pkgs/lib/x264/t/ix.sh
@@ -0,0 +1,31 @@
+{% extends '//die/c/configure.sh' %}
+
+{% block git_repo %}
+https://code.videolan.org/videolan/x264
+{% endblock %}
+
+{% block git_commit %}
+a8b68ebfaa68621b5ac8907610d3335971839d52
+{% endblock %}
+
+{% block git_sha %}
+654c686533c2a1dcf60611f7245edb90cdbae47653f1ed5b39cdadba33313ea8
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+{% endblock %}
+
+{% block bld_tool %}
+bld/nasm
+bld/bash
+bld/elfutils
+{% endblock %}
+
+{% block configure_shell %}
+bash
+{% endblock %}
+
+{% block configure_all_flags %}
+--prefix=${out}
+{% endblock %}
