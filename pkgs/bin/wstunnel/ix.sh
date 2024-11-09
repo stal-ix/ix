ddfcfee4d0d91ee2c945b2b1a385080ffd527c9a
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-11-09 13:03:15 +0300

    up

--- pkgs/bin/wstunnel/ix.sh
+++ pkgs/bin/wstunnel/ix.sh
@@ -1,11 +1,11 @@
 {% extends '//die/rust/cargo.sh' %}
 
 {% block cargo_url %}
-https://github.com/erebe/wstunnel/archive/refs/tags/v10.1.5.tar.gz
+https://github.com/erebe/wstunnel/archive/refs/tags/v10.1.6.tar.gz
 {% endblock %}
 
 {% block cargo_sha %}
-6693f550a81826d14c505c445b7f6607bbe580082af494268ab20cf4012471f2
+ecc540ea2f35dcd5113cea04c02a0172a8fea439d97795d838f871fedeb1a592
 {% endblock %}
 
 {% block bld_libs %}
