024e4a796411bf358db415c8efcea69a595f1f94
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-11-12 13:23:25 +0300

    up

--- pkgs/lib/gpg/error/ix.sh
+++ pkgs/lib/gpg/error/ix.sh
@@ -2,8 +2,8 @@
 
 {% block fetch %}
 # 1.50 broke macos
-https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.50.tar.bz2
-sha:69405349e0a633e444a28c5b35ce8f14484684518a508dc48a089992fe93e20a
+https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.51.tar.bz2
+sha:be0f1b2db6b93eed55369cdf79f19f72750c8c7c39fc20b577e724545427e6b2
 {% endblock %}
 
 {% block lib_deps %}
