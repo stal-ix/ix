6cec2ec5deb29985ac49aef150805e226f7877ad
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-11-13 16:04:51 +0300

    add broken

---
+++ pkgs/lib/unistd/ix.sh
@@ -0,0 +1,17 @@
+{% extends '//die/c/cmake.sh' %}
+
+{% block git_repo %}
+https://github.com/robinrowe/libunistd
+{% endblock %}
+
+{% block git_commit %}
+aa70d7b123d31f39ce1eb30331a4145123ae4f47
+{% endblock %}
+
+{% block git_sha %}
+1fd5e3e2cb010be82dbca79694f03ce3ba3c8e077e4a0f4fa4f5ef6461793551
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+{% endblock %}
