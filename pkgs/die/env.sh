2cd047b15c97446720ec8db94170c2ef5bd3eefd
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-11-16 17:23:30 +0300

    better

--- pkgs/die/env.sh
+++ pkgs/die/env.sh
@@ -1,1 +1,18 @@
-{% extends 'py.py' %}
+{% extends 'script.json' %}
+
+{% block script_body %}
+sed -xue
+export PATH=/ix/realm/boot/bin:/bin:/usr/bin:/usr/local/bin
+rm -rf ${out}
+mkdir ${out}
+cat << EOF > ${out}/env
+export PATH=${out}/bin:\${PATH}
+EOF
+base64 -d << EOF >> ${out}/env
+{{self.env() | b64e}}
+EOF
+{% endblock %}
+
+{% block script_exec %}
+["/bin/sh", "-s"]
+{% endblock %}
