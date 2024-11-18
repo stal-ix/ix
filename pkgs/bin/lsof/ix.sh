939666afd7b1cd4de69eb48872a3d1d54d333472
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-11-18 11:58:13 +0300

    up

--- pkgs/bin/lsof/ix.sh
+++ pkgs/bin/lsof/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/make.sh' %}
 
 {% block fetch %}
-https://github.com/lsof-org/lsof/archive/refs/tags/4.99.3.tar.gz
-sha:b9c56468b927d9691ab168c0b1e9f8f1f835694a35ff898c549d383bd8d09bd4
+https://github.com/lsof-org/lsof/archive/refs/tags/4.99.4.tar.gz
+sha:90d02ae35cd14341bfb04ce80e0030767476b0fc414a0acb115d49e79b13d56c
 {% endblock %}
 
 {% block bld_libs %}
