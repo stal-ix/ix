93761c51fd4678e4819620c679d3f3afd11b3713
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-11-19 17:10:29 +0300

    add

---
+++ pkgs/bin/ix/runsvdir/cpp/m.cpp
@@ -0,0 +1,154 @@
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+#include <time.h>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <unistd.h>
+#include <exception>
+#include <filesystem>
+
+namespace {
+    static void log0(const std::string& s) {
+        write(2, s.c_str(), s.length());
+    }
+
+    static void log(const std::string& s) {
+        log0(s + "\n");
+    }
+
+    static inline std::string readf(const std::string& path) {
+        std::ostringstream buf;
+        std::ifstream input(path);
+        buf << input.rdbuf();
+
+        return buf.str();
+    }
+
+    static std::string currentException() {
+        try {
+            throw;
+        } catch (const std::exception& e) {
+            return e.what();
+        }
+
+        return "unknown error";
+    }
+
+    static std::string shash(const std::string& s) {
+        return std::string{};
+    }
+
+    static std::string fhash(const std::string& p) {
+        return shash(p) + "|" + shash(readf(p));
+    }
+
+    using DirList = std::vector<std::string>;
+
+    static DirList listDir(const std::string& where) {
+        DirList res;
+
+        for (const auto& entry : std::filesystem::directory_iterator(where)) {
+            res.push_back(entry.path().string());
+        }
+
+        return res;
+    }
+
+    struct Proc {
+        Proc(const std::string& p) {
+        }
+
+        void terminate() {
+        }
+
+        bool alive() {
+            return true;
+        }
+    };
+
+    struct Context {
+        const std::string where;
+        std::map<std::string, std::shared_ptr<Proc>> running;
+
+        Context(const std::string& where_)
+            : where(where_)
+        {
+        }
+
+        void run() {
+            while (true) {
+                try {
+                    step();
+                } catch (...) {
+                    log("step error " + currentException());
+                }
+
+                sleep(1);
+            }
+        }
+
+        void step() {
+            std::vector<std::string> cur;
+
+            for (auto d : listDir(where)) {
+                auto p = where + d + "/run";
+
+                try {
+                    auto md5 = fhash(p);
+                    auto& proc = running[md5];
+
+                    if (proc.get()) {
+                        log(p + " already running as " + md5);
+                    } else {
+                        log("spawn " + p + " as " + md5);
+                        proc = std::make_shared<Proc>(p);
+                    }
+
+                    cur.push_back(p);
+                } catch (...) {
+                    log("skip " + p + ": " + currentException());
+                }
+            }
+
+            std::set<std::string> scur(cur.begin(), cur.end());
+            std::vector<std::string> dead;
+
+            for (auto& item : running) {
+                auto md5 = item.first;
+                auto proc = item.second;
+
+                if (scur.find(md5) == scur.end()) {
+                    log(md5 + " stale");
+                    proc->terminate();
+                }
+
+                if (proc->alive()) {
+                    log(md5 + " alive");
+                } else {
+                    log(md5 + " dead");
+                    dead.push_back(md5);
+                }
+            }
+
+            for (auto d : dead) {
+                log("erase dead " + d);
+                running.erase(d);
+            }
+        }
+    };
+}
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        abort();
+    }
+
+    if (!argv[1]) {
+        abort();
+    }
+
+    Context(argv[1]).run();
+}
