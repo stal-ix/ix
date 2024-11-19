eee13b79c3f39d99c854c2a772cc85fc6bde168e
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-11-19 23:23:23 +0300

    up

--- pkgs/bin/zellij/ix.sh
+++ pkgs/bin/zellij/ix.sh
@@ -1,11 +1,11 @@
 {% extends '//die/rust/cargo.sh' %}
 
 {% block cargo_url %}
-https://github.com/zellij-org/zellij/archive/refs/tags/v0.41.0.tar.gz
+https://github.com/zellij-org/zellij/archive/refs/tags/v0.41.2.tar.gz
 {% endblock %}
 
 {% block cargo_sha %}
-36e02ada369951d2a506aa1e4dbd666c342b5d67c4056f380324a7eaeccda222
+2d053f573276a92f000308e59456a6bf3f121ef23099b060fb3471344cb559b0
 {% endblock %}
 
 {% block bld_libs %}
