09134e528f0b3f643f147b267d4de7f984591428
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-11-19 15:36:32 +0300

    up

--- pkgs/lib/input/t/ix.sh
+++ pkgs/lib/input/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://gitlab.freedesktop.org/libinput/libinput/-/archive/1.26.2/libinput-1.26.2.tar.bz2
-sha:e2dbbf515905086dc3f8c8536d326e04012f5716b8b047bb3392a17b13ca78ec
+https://gitlab.freedesktop.org/libinput/libinput/-/archive/1.27.0/libinput-1.27.0.tar.bz2
+sha:8e823cc9f44c51b63ae90af0b73676ed82c0402cc1cbd4a59ac91b7bb6cad716
 {% endblock %}
 
 {% block bld_libs %}
