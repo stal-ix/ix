c3ce4e48d712810e4ddf9a8c7dd8c2cbf0ce9b61
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-11-22 12:30:44 +0300

    up

--- pkgs/aux/ca/bundle/ix.sh
+++ pkgs/aux/ca/bundle/ix.sh
@@ -1,5 +1,5 @@
 {% extends 'nopredict/ix.sh' %}
 
 {% block predict_outputs %}
-[{"path": "{{self.pem_path().strip()}}", "sum": "d8fcfc0c58ce603d1d2aa9ce034a3fdf26b6eb0606770808818d7552a5a75547"}]
+[{"path": "{{self.pem_path().strip()}}", "sum": "02b03dcfa423f75b72b47dac373e795d57cde3b341231f5d45922132e3a87f5d"}]
 {% endblock %}
--- pkgs/lib/nss/t/ver.sh
+++ pkgs/lib/nss/t/ver.sh
@@ -1,2 +1,2 @@
-https://ftp.mozilla.org/pub/security/nss/releases/NSS_3_106_RTM/src/nss-3.106.tar.gz
-sha:026b744e1e0784b890c3846ac9506472a92138c1f4d41dec581949574c585c38
+https://ftp.mozilla.org/pub/security/nss/releases/NSS_3_107_RTM/src/nss-3.107.tar.gz
+sha:7f7e96473e38150771a615f5d40e8c41ba3a19385301ae0c525091f2fc9d6729
