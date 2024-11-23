05d692f9c54c95c3472910b8a63e86c7b7d0b930
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-11-23 15:55:53 +0300

    better

--- pkgs/lib/spelling/ix.sh
+++ pkgs/lib/spelling/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://gitlab.gnome.org/GNOME/libspelling/-/archive/0.4.3/libspelling-0.4.3.tar.bz2
-sha:7ae594242b537513ffe333530dad752eeeffb9e52872468d73ceccbda58932b9
+https://gitlab.gnome.org/GNOME/libspelling/-/archive/0.4.5/libspelling-0.4.5.tar.bz2
+sha:d54c0b5e5f176b75bdb57640e13214c92b188995c5333fec798be65603d7cad7
 {% endblock %}
 
 {% block lib_deps %}
