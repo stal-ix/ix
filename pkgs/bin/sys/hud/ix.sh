a414802a92d77b4b698ca94d9e58e7f933ee5cb9
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-11-24 18:51:27 +0300

    add

--- pkgs/bin/sys/hud/ix.sh
+++ pkgs/bin/sys/hud/ix.sh
@@ -11,11 +11,32 @@ lib/c++
 lib/evdev
 lib/gtk/4/mm
 lib/gtk/layer/shell/4
+lib/shim/fake/pkg(pkg_name=wireplumber-0.5,pkg_ver=100500)
 {% endblock %}
 
 {% block patch %}
-sed -e 's|.*define AUDIO.*||' -i src/config.hpp
 sed -e 's|-fno-exceptions||' -i Makefile
+>src/wireplumber.cpp
+cat << EOF > src/wireplumber.hpp
+#pragma once
+#include <glibmm/dispatcher.h>
+class syshud_wireplumber {
+public:
+    syshud_wireplumber(Glib::Dispatcher*, Glib::Dispatcher*) {
+    }
+
+    virtual ~syshud_wireplumber() {
+    }
+
+    int volume = 0;
+    bool muted = true;
+    const char* output_name = "unknown";
+    const char* input_name = "unknown";
+
+    void set_volume(const bool& type, const double& value) {
+    }
+};
+EOF
 {% endblock %}
 
 {% block bld_tool %}
@@ -24,4 +45,8 @@ bld/pkg/config
 
 {% block cpp_missing %}
 unistd.h
+{% endblock %}
+
+{% block build_flags %}
+wrap_cc
 {% endblock %}
