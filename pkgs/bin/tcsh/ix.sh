bbd490a793a393be41e8c3669002f55c86512893
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-11-27 00:16:37 +0300

    up

--- pkgs/bin/tcsh/ix.sh
+++ pkgs/bin/tcsh/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://astron.com/pub/tcsh/tcsh-6.24.13.tar.gz
-sha:1e927d52e9c85d162bf985f24d13c6ccede9beb880d86fec492ed15480a5c71a
+https://astron.com/pub/tcsh/tcsh-6.24.14.tar.gz
+sha:36880f258a63fc11fe72a65098b585ebc4ecdee24388b8ebec97e6ae8e485318
 {% endblock %}
 
 {% block conf_ver %}
