70d70870f31387178e78b9593a1c9f3ec5d4189a
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-11-28 10:49:17 +0300

    up

--- pkgs/bld/boot/7/file/ix.sh
+++ pkgs/bld/boot/7/file/ix.sh
@@ -1,5 +1,10 @@
 {% extends '//bin/file/host/ix.sh' %}
 
+{% block fetch %}
+https://astron.com/pub/file/file-5.45.tar.gz
+sha:fc97f51029bb0e2c9f4e3bffefdaf678f0e039ee872b9de5c002a6d09c784d82
+{% endblock %}
+
 {% block lib_deps %}
 {% endblock %}
 
--- pkgs/lib/magic/t/ix.sh
+++ pkgs/lib/magic/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
-https://astron.com/pub/file/file-5.45.tar.gz
-sha:fc97f51029bb0e2c9f4e3bffefdaf678f0e039ee872b9de5c002a6d09c784d82
+https://astron.com/pub/file/file-5.46.tar.gz
+sha:c9cc77c7c560c543135edc555af609d5619dbef011997e988ce40a3d75d86088
 {% endblock %}
 
 {% block lib_deps %}
