6d1d409d711a4ac2ea1b5d21fb049901b36ed3a6
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-11-29 11:22:05 +0300

    revert

--- pkgs/lib/web/kit/gtk/t/ix.sh
+++ pkgs/lib/web/kit/gtk/t/ix.sh
@@ -1,8 +1,10 @@
 {% extends '//lib/web/kit/t/ix.sh' %}
 
 {% block fetch %}
-https://webkitgtk.org/releases/webkitgtk-2.46.4.tar.xz
-sha:0eff5f0ab0a2872ec87df62bc32e3289c8af625716ac71e94b298d74e0374176
+https://webkitgtk.org/releases/webkitgtk-2.46.3.tar.xz
+sha:85e09fa6ff9fea49678ba9975dbc64ea3242833f8f8a7d6a8937b2f292fcb28d
+#https://webkitgtk.org/releases/webkitgtk-2.46.4.tar.xz
+#sha:0eff5f0ab0a2872ec87df62bc32e3289c8af625716ac71e94b298d74e0374176
 {% endblock %}
 
 {% block lib_deps %}
