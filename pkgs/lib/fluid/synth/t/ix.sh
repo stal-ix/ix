3f5b4afbcbd752b239102958dd3b482b532e9edb
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-12-01 23:59:42 +0300

    up

--- pkgs/lib/fluid/synth/t/ix.sh
+++ pkgs/lib/fluid/synth/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/FluidSynth/fluidsynth/archive/refs/tags/v2.4.0.tar.gz
-sha:fd4d216a3030abc56a339985ab0506328660cd77b2d71dd98c240c58c03d8f7f
+https://github.com/FluidSynth/fluidsynth/archive/refs/tags/v2.4.1.tar.gz
+sha:d1e64155ac902116ed3d4dea512719d8c04ab3877db2e8fb160284379f570a2f
 {% endblock %}
 
 {% block lib_deps %}
