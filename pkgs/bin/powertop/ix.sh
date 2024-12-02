ef9d13254c1ac39958210d784a642e123884efa6
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-12-02 20:47:35 +0300

    revert

--- pkgs/bin/powertop/ix.sh
+++ pkgs/bin/powertop/ix.sh
@@ -18,10 +18,10 @@ bld/gettext
 bld/auto/archive
 {% endblock %}
 
-{% block configure %}
-{{super()}}
-cat << EOF > po/Makefile
-all:
-install:
-EOF
-{% endblock %}
+{#
+*** error: gettext infrastructure mismatch: using a Makefile.in.in from gettext version 0.18 but the autoconf macros are from gettext version 0.23
+make[2]: *** [Makefile:168: check-macro-version] Error 1
+make[1]: *** [Makefile:563: all-recursive] Error 1
+make: *** [Makefile:474: all] Error 2
+?[31mERROR /var/run/ci/mount/ix_root/store/Ek88tN2aA7FfsMmC-bin-aria-2-unwrap?[0m
+#}
