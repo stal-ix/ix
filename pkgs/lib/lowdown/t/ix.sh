953cb540439d68bd8f264cf3576d2565d31ffebd
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-12-06 18:57:08 +0300

    up

--- pkgs/lib/lowdown/t/ix.sh
+++ pkgs/lib/lowdown/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/make.sh' %}
 
 {% block fetch %}
-https://github.com/kristapsdz/lowdown/archive/refs/tags/VERSION_1_3_0.tar.gz
-sha:ef5d2a9322c6350c560fb2299978654177c96e8dde8f878bb5799c6f10216638
+https://github.com/kristapsdz/lowdown/archive/refs/tags/VERSION_1_3_1.tar.gz
+sha:028b77171cfa03d8360aaaaab81ef3ab1e99f9623cd39e62c5dae82ebad7c5db
 {% endblock %}
 
 {% block lib_deps %}
