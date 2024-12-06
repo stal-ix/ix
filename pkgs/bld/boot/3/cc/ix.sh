fd6ba8408772e93b5b1243ea899e21d2de1c65ab
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-12-06 14:38:49 +0300

    better

--- pkgs/bld/boot/3/cc/ix.sh
+++ pkgs/bld/boot/3/cc/ix.sh
@@ -5,13 +5,28 @@ bld/boot/2/env
 {% endblock %}
 
 {% block install %}
+echo "{{ix_boot_path}}"
 mkdir ${out}/bin
-{% for x in ['clang', 'clang++', 'clang-cpp', 'llvm-ar', 'llvm-nm', 'llvm-ranlib', 'gcc', 'g++', 'ar', 'nm', 'ranlib', 'as', 'ld', 'cpp'] %}
-{% if ix_boot_tool(x) %}
-ln -s {{ix_boot_tool(x)}} ${out}/bin/{{x}}
+{% if ix_boot_tool('clang++') %}
+{% for x in ['clang', 'clang++', 'clang-cpp', 'llvm-ar', 'llvm-nm', 'llvm-ranlib'] %}
+{% if not ix_boot_tool(x) %}
+{{error(x + ' not in path')}}
 {% endif %}
+ln -s {{ix_boot_tool(x)}} ${out}/bin/{{x}}
 {% endfor %}
+{% elif ix_boot_tool('g++') %}
+{% for x in ['gcc', 'g++', 'ar', 'nm', 'ranlib', 'as', 'ld', 'cpp'] %}
+{% if not ix_boot_tool(x) %}
+{{error(x + ' not in path')}}
+{% endif %}
+ln -s {{ix_boot_tool(x)}} ${out}/bin/{{x}}
+{% endfor %}
+{% else %}
+error('shit happen')
+{% endif %}
+ls -la ${out}/bin/
 {% endblock%}
+
 
 {% block script_exec %}
 ["/usr/bin/env", "PATH={{ix_boot_path}}", "/bin/sh", "-s"]
