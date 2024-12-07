cdb2a6ebe97ccb7d4335de40aa4873362e95fe62
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-12-07 17:11:25 +0300

    up

--- pkgs/bin/npm/source/ix.sh
+++ pkgs/bin/npm/source/ix.sh
@@ -1,13 +1,13 @@
 {% extends '//die/std/ix.sh' %}
 
 {% block fetch %}
-https://github.com/npm/cli/archive/refs/tags/v10.9.1.tar.gz
-sha:8f8dc61374b66992ad1613185fa66b4059c8826964164c2766dbc75eb609b9fd
+https://github.com/npm/cli/archive/refs/tags/v10.9.2.tar.gz
+sha:6b19afc5ba4bf92108ec366feaf82107fc137274c42158588b2cf9117bc870dc
 {% endblock %}
 
 {% block use_network %}true{% endblock %}
 
-{% set sum %}986eb91733ee4189aa095e61c320f36f16afad83d6bf23d210630f45dc8b24e9{% endset %}
+{% set sum %}13295ed2728bde3a9a1d1e8006f236647580fc0bf89c8dacdcc5c71a592fad5c{% endset %}
 {% set nam %}npm_{{sum}}.pzd{% endset %}
 
 {% block predict_outputs %}
