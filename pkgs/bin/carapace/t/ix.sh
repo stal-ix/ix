307ea16a9e82353da94efcd1c26af4b1609d6b61
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-12-08 22:51:47 +0300

    up

--- pkgs/bin/carapace/t/ix.sh
+++ pkgs/bin/carapace/t/ix.sh
@@ -1,9 +1,13 @@
 {% extends '//die/go/build.sh' %}
 
 {% block go_url %}
-https://github.com/carapace-sh/carapace-bin/archive/refs/tags/v1.0.4.tar.gz
+https://github.com/carapace-sh/carapace-bin/archive/refs/tags/v1.1.0.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-cd81c4f0581559a8ab9ff767789dd6935ab47b0fe6fe9ba0ac308775beb443e3
+9ef056d88d744be9739de5249529df6d3d51d7df8f724f9be7e00ffc5ea1e8a6
+{% endblock %}
+
+{% block go_tool %}
+bin/go/lang/23
 {% endblock %}
