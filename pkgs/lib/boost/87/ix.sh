f07e5f5fb51890ea84adc1f17ac9d9439daa7eb0
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-12-13 12:41:53 +0300

    fix

--- pkgs/lib/boost/87/ix.sh
+++ pkgs/lib/boost/87/ix.sh
@@ -4,3 +4,10 @@
 https://boostorg.jfrog.io/artifactory/main/release/1.87.0/source/boost_1_87_0.tar.bz2
 sha:af57be25cb4c4f4b413ed692fe378affb4352ea50fbe294a11ef548f4d527d89
 {% endblock %}
+
+{% block install %}
+{{super()}}
+llvm-objcopy \
+    --strip-symbol=__cxa_allocate_exception \
+    ${out}/lib/libboost_stacktrace_from_exception.a
+{% endblock %}
