a59bdafab1daa271243627065edbbde933dc00ac
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-12-13 12:32:34 +0300

    add

--- pkgs/bin/b2/t/ix.sh
+++ pkgs/bin/b2/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/ix.sh' %}
 
 {% block fetch %}
-https://github.com/bfgroup/b2/archive/refs/tags/5.1.0.tar.gz
-sha:4d1e5d262f8055e6d639703c5c1ae0aca70c7c453d51a2adc28367fe501d9fa5
+https://github.com/bfgroup/b2/archive/refs/tags/5.2.0.tar.gz
+sha:7e11979235c54b7fc00d3821550b5666816bd3dd61148652620687d690571a3d
 {% endblock %}
 
 {% block bld_libs %}
---
+++ pkgs/lib/boost/87/ix.sh
@@ -0,0 +1,6 @@
+{% extends '//lib/boost/t/ix.sh' %}
+
+{% block fetch %}
+https://boostorg.jfrog.io/artifactory/main/release/1.87.0/source/boost_1_87_0.tar.bz2
+sha:af57be25cb4c4f4b413ed692fe378affb4352ea50fbe294a11ef548f4d527d89
+{% endblock %}
