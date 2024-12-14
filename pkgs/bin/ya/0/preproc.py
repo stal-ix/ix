44d8c3f2a64ff4917d6e815adaec21f00b099d12
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-12-14 09:13:40 +0300

    simplify

--- pkgs/bin/ya/0/preproc.py
+++ pkgs/bin/ya/0/preproc.py
@@ -1,14 +1,12 @@
 import sys
 
-def preproc(l):
-    yield 'cd $(B)'
-    yield 'llvm-ar qL ' + l[65:].replace('$(B)/', '')
-
 def it():
+    yield 'export B = $(B)'
+    yield 'export S = $(S)'
+
     for l in sys.stdin.read().split('\n'):
-        if 'link_lib' in l and 'musl.a' in l:
-            for x in preproc(l):
-                yield '\t' + x
+        if l.startswith('\t') or '=-' in l:
+            yield l.replace('$(B)', '$${B}').replace('$(S)', '$${S}')
         else:
             yield l
 
