5965cd3597a53ea9e3733e7a419354d4bd649224
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-12-16 23:08:31 +0300

    up

--- pkgs/lib/gumbo/ix.sh
+++ pkgs/lib/gumbo/ix.sh
@@ -1,10 +1,14 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://codeberg.org/grisha/gumbo-parser/archive/0.12.1.tar.gz
-sha:c0bb5354e46539680724d638dbea07296b797229a7e965b13305c930ddc10d82
+https://codeberg.org/grisha/gumbo-parser/archive/0.12.3.tar.gz
+sha:a446a2856a5dfcc544f466fdcca4c953096a1895b9a7953eed903bd017c34132
 {% endblock %}
 
 {% block lib_deps %}
 lib/c
+{% endblock %}
+
+{% block conf_ver %}
+2/72
 {% endblock %}
