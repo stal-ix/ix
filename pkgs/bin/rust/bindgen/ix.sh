b7efd4f3eb6bbc527e1e31b3ed93f386bb38297d
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-12-28 09:16:49 +0300

    add

---
+++ pkgs/bin/rust/bindgen/ix.sh
@@ -0,0 +1,39 @@
+{% extends '//die/rust/cargo.sh' %}
+
+{% block cargo_url %}
+https://github.com/rust-lang/rust-bindgen/archive/refs/tags/v0.71.1.tar.gz
+{% endblock %}
+
+{% block cargo_sha %}
+4e58f7ed07ce1aff1a35e2749c42642b8e594196a9ff2ac7b8b8e1e14deddf8c
+{% endblock %}
+
+{% block bld_libs %}
+lib/z
+lib/llvm/19
+lib/shim/fake(lib_name=ffi)
+lib/shim/fake(lib_name=stdc++)
+lib/shim/fake(lib_name=ncursesw)
+{% endblock %}
+
+{% block bld_tool %}
+{{super()}}
+bld/llvm/config
+{% endblock %}
+
+{% block cargo_features %}
+static
+{% endblock %}
+
+{% block patch %}
+{{super()}}
+sed -e 's|"runtime"|"static"|' \
+    -i bindgen/Cargo.toml
+sed -e 's|"runtime"|"static"|' \
+    -i vendored/clang-sys/Cargo.toml
+{% endblock %}
+
+{% block install %}
+mkdir ${out}/bin
+cp ${tmp}/out/bindgen ${out}/bin/
+{% endblock %}
