6f67705d6b00f44f17341ca4bc708ff5af32eb9f
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-01-03 16:37:16 +0300

    fix

--- pkgs/bin/powertop/ix.sh
+++ pkgs/bin/powertop/ix.sh
@@ -25,3 +25,11 @@ make[1]: *** [Makefile:563: all-recursive] Error 1
 make: *** [Makefile:474: all] Error 2
 ?[31mERROR /var/run/ci/mount/ix_root/store/Ek88tN2aA7FfsMmC-bin-aria-2-unwrap?[0m
 #}
+
+{% block configure %}
+{{super()}}
+cat << EOF > po/Makefile
+all:
+install:
+EOF
+{% endblock %}
