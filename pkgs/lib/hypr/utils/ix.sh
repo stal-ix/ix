973af9918aa8ce00f84d25f95d9a5ce1146b9a15
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-01-03 16:57:01 +0300

    up

--- pkgs/lib/hypr/utils/ix.sh
+++ pkgs/lib/hypr/utils/ix.sh
@@ -1,12 +1,22 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/hyprwm/hyprutils/archive/refs/tags/v0.2.5.tar.gz
-sha:19737eca82d9a4c4b4108864960d7f460ad670c1fd95a87a90640153c91dc818
+https://github.com/hyprwm/hyprutils/archive/refs/tags/v0.3.2.tar.gz
+sha:2352acc654027beb5d6a3b18cf58feed5c83fc513b71789b69ef7274bf7c369a
 {% endblock %}
 
 {% block lib_deps %}
 lib/c
 lib/c++
 lib/pixman
+{% endblock %}
+
+{% block bld_tool %}
+bld/prepend
+{% endblock %}
+
+{% block patch %}
+prepend include/hyprutils/animation/AnimationConfig.hpp << EOF
+#include <string>
+EOF
 {% endblock %}
