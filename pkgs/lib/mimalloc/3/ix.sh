140a5aaa868f13f9dfea1592da576e189afc1060
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-01-05 20:52:14 +0300

    up

--- pkgs/lib/mimalloc/3/ix.sh
+++ pkgs/lib/mimalloc/3/ix.sh
@@ -1,33 +1,8 @@
-{% extends '//die/c/cmake.sh' %}
+{% extends '//lib/mimalloc/2/ix.sh' %}
 
 {% block fetch %}
 https://github.com/microsoft/mimalloc/archive/refs/tags/v3.0.1.tar.gz
 sha:6a514ae31254b43e06e2a89fe1cbc9c447fdbf26edc6f794f3eb722f36e28261
-{% endblock %}
-
-{% block bld_libs %}
-lib/c++/headers
-lib/shim/alloc
-{% endblock %}
-
-{% block cmake_flags %}
-MI_USE_CXX=ON
-MI_OVERRIDE=ON
-MI_BUILD_STATIC=ON
-MI_BUILD_SHARED=OFF
-MI_BUILD_TESTS=OFF
-{% endblock %}
-
-{% block cpp_defines %}
-{{super()}}
-MI_STAT=0
-{% endblock %}
-
-{% block install %}
-{{super()}}
-cd ${out}/lib
-mv mimalloc-*/* ./
-rm -rf mimalloc-*
 {% endblock %}
 
 {% block env %}
