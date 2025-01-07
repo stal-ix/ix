39a5f727a0aa718cf1b82edbddd4fcebb992eb69
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-01-08 00:29:36 +0300

    up

--- pkgs/bin/moe/ix.sh
+++ pkgs/bin/moe/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
-https://ftp.gnu.org/gnu/moe/moe-1.14.tar.lz
-sha:f4babd6ce0ae19516f983454fb20d32dff71ad316337ac6bf93a42a5ff209c9d
+https://ftp.gnu.org/gnu/moe/moe-1.15.tar.lz
+sha:41f8c8b099ce3047945ca4e097a60d9243e9c73fbb268c194a12da8b0d9f0a66
 {% endblock %}
 
 {% block bld_libs %}
