e020f6eba48e2e4b63c451529a02da311686c0d6
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-01-08 03:43:00 +0300

    up

---
+++ pkgs/bin/ya/4/ix.sh
@@ -0,0 +1,14 @@
+{% extends '//bin/ya/2/ix.sh' %}
+
+{% block git_commit %}
+2c1b5b75ea9fd913cda4d28c8c18dfe38a320852
+{% endblock %}
+
+{% block git_sha %}
+c0c448b86c0220eb15f5090a26dd14e68015e792281510c1f88c359a0174d5bc
+{% endblock %}
+
+{% block bld_tool %}
+bin/ya/3
+{{super()}}
+{% endblock %}
