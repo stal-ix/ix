b65f5fc704ac4857d2e65d729e7c2638f19f95be
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-01-08 05:37:43 +0300

    up

--- pkgs/bin/ed/stock/ix.sh
+++ pkgs/bin/ed/stock/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
-https://ftp.gnu.org/gnu/ed/ed-1.20.2.tar.lz
-sha:65fec7318f48c2ca17f334ac0f4703defe62037bb13cc23920de077b5fa24523
+https://ftp.gnu.org/gnu/ed/ed-1.21.tar.lz
+sha:60e24998727d453a5cf02c54664b97536de46a2b34cd1f4f67c1c1a61bbbad75
 {% endblock %}
 
 {% block bld_libs %}
