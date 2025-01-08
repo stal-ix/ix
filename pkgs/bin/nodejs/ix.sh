99317bd2371c64db7f90cd0aac05d8b51c0002cc
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-01-08 06:46:29 +0300

    up

--- pkgs/bin/nodejs/ix.sh
+++ pkgs/bin/nodejs/ix.sh
@@ -1,43 +1,5 @@
-{% extends '//die/c/configure.sh' %}
+{% extends '//die/hub.sh' %}
 
-{% block fetch %}
-https://nodejs.org/dist/v23.4.0/node-v23.4.0.tar.gz
-sha:1abc3d7ed398b0fd8606837552732f7bebce0cd2285069477d0692d318442248
-{% endblock %}
-
-{% block bld_libs %}
-lib/c
-lib/z
-lib/uv
-lib/xz
-lib/icu
-lib/c++
-lib/kernel
-lib/c/ares
-lib/brotli
-lib/openssl
-lib/ng/http/2
-lib/shim/fake(lib_name=atomic)
-{% endblock %}
-
-{% block bld_tool %}
-bin/gyp
-bin/procps/ng
-bin/python/huge
-{% endblock %}
-
-{% block configure_all_flags %}
---prefix=${out}
-
---shared-zlib
---shared-libuv
---shared-cares
---shared-brotli
---shared-nghttp2
---shared-openssl
-
---without-npm
---with-intl=system-icu
-
---experimental-http-parser
+{% block run_deps %}
+bin/nodejs/unwrap(libopenssl_ver=3/quic)
 {% endblock %}
---
+++ pkgs/bin/nodejs/unwrap/ix.sh
@@ -0,0 +1,55 @@
+{% extends '//die/c/configure.sh' %}
+
+{% block fetch %}
+https://nodejs.org/dist/v23.6.0/node-v23.6.0.tar.gz
+sha:9cd2aaa44d7d658833bb03ab4ce4c57273ba4c9b716094c931c709e6948fd71d
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/z
+lib/uv
+lib/xz
+#lib/icu
+lib/c++
+lib/kernel
+lib/c/ares
+lib/brotli
+lib/openssl
+lib/simd/json
+lib/ng/http/2
+lib/ng/http/3
+lib/ng/tcp2/openssl
+lib/shim/fake(lib_name=atomic)
+{% endblock %}
+
+{% block bld_tool %}
+bin/gyp
+bin/procps/ng
+bin/python/huge
+{% endblock %}
+
+{% block configure_all_flags %}
+--prefix=${out}
+
+--use_clang
+
+--shared-zlib
+--shared-libuv
+--shared-cares
+--shared-brotli
+--shared-ngtcp2
+--shared-nghttp2
+--shared-nghttp3
+--shared-openssl
+--shared-simdjson
+
+--without-npm
+--with-intl=none
+
+--experimental-http-parser
+{% endblock %}
+
+{% block build_flags %}
+wrap_cc
+{% endblock %}
