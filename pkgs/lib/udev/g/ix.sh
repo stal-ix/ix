aab53c8ab65286fc88e4c3b7909b8a9ab37cbc9e
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-01-10 02:43:07 +0300

    up

--- pkgs/lib/udev/g/ix.sh
+++ pkgs/lib/udev/g/ix.sh
@@ -1,10 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://github.com/GNOME/libgudev/archive/refs/tags/237.tar.gz
-sha:4565d5abce6f71198c7c8ac3cecb30a007df7caf230094e09a6d61763f452a80
-#https://github.com/GNOME/libgudev/archive/refs/tags/238.tar.gz
-#sha:443c702c6339775f413f45d13309b2c641a94530bb27981142a2605eedb0aafd
+https://github.com/GNOME/libgudev/archive/refs/tags/238.tar.gz
+sha:443c702c6339775f413f45d13309b2c641a94530bb27981142a2605eedb0aafd
 {% endblock %}
 
 {% block lib_deps %}
