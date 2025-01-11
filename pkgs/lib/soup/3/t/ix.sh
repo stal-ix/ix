52a326f6eae34ce14bad6c9db0eac3b5b5fc9819
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-01-12 02:23:59 +0300

    up

--- pkgs/lib/soup/3/t/ix.sh
+++ pkgs/lib/soup/3/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://gitlab.gnome.org/GNOME/libsoup/-/archive/3.6.1/libsoup-3.6.1.tar.bz2
-sha:d52815747b4151fb3efc386bcbc5c0a11a1f3aa138ab5db3a9c2f645f403f294
+https://gitlab.gnome.org/GNOME/libsoup/-/archive/3.6.3/libsoup-3.6.3.tar.bz2
+sha:1a5fa841e038aca8a2073e0c1d72db828d4187e8d9ae3ae6bd4e7e7da1617b80
 {% endblock %}
 
 {% block lib_deps %}
