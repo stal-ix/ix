e7c8ffe73c7d2544f847edc0cd55ffc3a009963b
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-01-14 00:34:01 +0300

    up

--- pkgs/bin/ya/gg/ix.sh
+++ pkgs/bin/ya/gg/ix.sh
@@ -1,11 +1,11 @@
 {% extends '//die/go/build.sh' %}
 
 {% block go_url %}
-https://github.com/pg83/gg/archive/a09345e9e07e71e32b0c1585072eb6b9ac594642.zip
+https://github.com/pg83/gg/archive/0e050f34a5fd8ad4430268da051541c7f3692e71.zip
 {% endblock %}
 
 {% block go_sha %}
-fbad9c38edb5f2d9d7fd6b6c1ac86dae61c26b6f434573b6e45a000b4d7cd6c9
+9a02d4762f3dd68dd1c7ca48143056a4e7a1cb273f4e996c5f21a2047e37155f
 {% endblock %}
 
 {% block go_tool %}
