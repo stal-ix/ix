a74fef91067676d5d395a41f63d7ce3deca96fbd
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-01-13 03:48:16 +0300

    better

---
+++ pkgs/bin/esbuild/21/ix.sh
@@ -0,0 +1,9 @@
+{% extends '//bin/esbuild/ix.sh' %}
+
+{% block go_url %}
+https://github.com/evanw/esbuild/archive/refs/tags/v0.21.5.tar.gz
+{% endblock %}
+
+{% block go_sha %}
+f6fabb47144504e31c0c32b750b485ec316eded08e3f7ebf2fc631121509453e
+{% endblock %}
