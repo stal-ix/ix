f6a7958b35a1a840a79f26c6cb23d07f1a2fc686
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-01-17 18:49:01 +0300

    fix

--- pkgs/bin/rtorrent/ix.sh
+++ pkgs/bin/rtorrent/ix.sh
@@ -1,19 +1,5 @@
-{% extends '//die/c/autorehell.sh' %}
+{% extends '//die/hub.sh' %}
 
-{% block fetch %}
-https://github.com/rakshasa/rtorrent/archive/refs/tags/v0.15.0.tar.gz
-sha:9a47f284616c4b3867c787b78f98de25730e7506909824a951399b612d17314a
-{% endblock %}
-
-{% block lib_deps %}
-lib/c
-lib/c++
-lib/curl
-lib/curses
-lib/torrent/rtorrent
-{% endblock %}
-
-{% block autoreconf %}
-{{super()}}
-sed -e 's|.*PKG_CHECK_EXISTS.*||' -i configure
+{% block run_deps %}
+bin/rtorrent/unwrap(libcplpl_ver=18)
 {% endblock %}
