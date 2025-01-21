d73dbad253ed4f0e543982fdd6ecf12f933c4878
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-01-21 17:19:35 +0300

    up

--- pkgs/bin/knot/resolver/ix.sh
+++ pkgs/bin/knot/resolver/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://secure.nic.cz/files/knot-resolver/knot-resolver-5.7.4.tar.xz
-sha:6b6da6ecf06828041afad44dfa227781f0ae34ad183a667008509355d18bd9c8
+https://secure.nic.cz/files/knot-resolver/knot-resolver-6.0.10.tar.xz
+sha:b7cb0b3672836114581747cc5edde4c65b9d07f8d6628a00bceb0b56a14be395
 {% endblock %}
 
 {% block bld_libs %}
@@ -21,12 +21,7 @@ lib/protobuf/c
 {% block bld_tool %}
 bld/bash
 bin/protoc/c
-bld/shebangs
 {% endblock %}
 
 {% block meson_strip_dirs %}
-{% endblock %}
-
-{% block patch %}
-fix_shebangs scripts/get-date.sh
 {% endblock %}
