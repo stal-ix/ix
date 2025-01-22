fd56e6dffbf6bbf9ac3161c9c66bc2ba2e0b28da
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-01-22 19:26:16 +0300

    add

--- pkgs/set/pg/system/ix.sh
+++ pkgs/set/pg/system/ix.sh
@@ -7,8 +7,8 @@ ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDGZ3aEKhxpG9JHG3uso/gUuUKe9NzHOXKAenZIDiDq
 {% block run_deps %}
 etc/tmpfs
 set/stalix
-bin/kernel/6/11
 bin/kernel/6/12
+bin/kernel/6/13
 bin/ix/keyboard
 bin/kernel/gengrub
 set/pg/system/hosts
