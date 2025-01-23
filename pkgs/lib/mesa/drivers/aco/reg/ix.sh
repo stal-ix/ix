43f3d2dccb24588a361e55ccb83a63e730ecfa01
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-01-23 16:51:28 +0300

    refac

--- pkgs/lib/mesa/drivers/aco/reg/ix.sh
+++ pkgs/lib/mesa/drivers/aco/reg/ix.sh
@@ -3,5 +3,6 @@
 {# AMD radv + radeonsi + aco shaders #}
 
 {% block lib_deps %}
-lib/mesa/drivers/valve(vulkan=amd,opengl=radeonsi)
+lib/mesa/drivers/opengl(driver=radeonsi)
+lib/mesa/drivers/vulkan(driver=radeon)
 {% endblock %}
