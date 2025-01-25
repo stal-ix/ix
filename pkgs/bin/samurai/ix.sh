90a0646acfbe881ab68509b16eb51a6a3c0bb9a6
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-01-25 17:08:54 +0300

    fix

--- pkgs/bin/samurai/ix.sh
+++ pkgs/bin/samurai/ix.sh
@@ -1,14 +1,5 @@
-{% extends '//die/c/make.sh' %}
+{% extends '//die/hub.sh' %}
 
-{% block fetch %}
-https://git.sr.ht/~mcf/samurai/refs/download/1.2/samurai-1.2.tar.gz
-sha:3b8cf51548dfc49b7efe035e191ff5e1963ebc4fe8f6064a5eefc5343eaf78a5
-{% endblock %}
-
-{% block c_rename_symbol %}
-warn
-{% endblock %}
-
-{% block bld_libs %}
-lib/c
+{% block run_deps %}
+bin/samurai/unwrap(libc_lite=1)
 {% endblock %}
