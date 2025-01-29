1923995d3aab6fc9b51d28457b944a2cfdc6c815
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-01-29 17:19:13 +0300

    add

---
+++ pkgs/bin/sched/dmesg/scripts/ix.sh
@@ -0,0 +1,17 @@
+{% extends '//die/gen.sh' %}
+
+{% block install %}
+mkdir -p ${out}/etc/sched/{{delay}}
+
+base64 -d << EOF > ${out}/etc/sched/{{delay}}/dmesg.sh
+{% include 'dmesg.sh/base64' %}
+EOF
+
+mkdir -p ${out}/bin
+
+base64 -d << EOF > ${out}/bin/klog
+{% include 'klog.sh/base64' %}
+EOF
+
+chmod +x ${out}/bin/*
+{% endblock %}
