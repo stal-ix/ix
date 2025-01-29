c9252d04cf703c1ae34df478b1fb336a3469561a
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-01-29 17:21:30 +0300

    fix

--- pkgs/bin/sched/purge/ix.sh
+++ pkgs/bin/sched/purge/ix.sh
@@ -1,13 +1,6 @@
-{% extends '//die/gen.sh' %}
+{% extends '//die/hub.sh' %}
 
 {% block run_deps %}
-bin/sched(delay={{delay}})
-{% endblock %}
-
-{% block install %}
-cd ${out}; mkdir -p etc/sched/{{delay}}; cd etc/sched/{{delay}}
-
-cat << EOF > trash_dir_{{trash_dir | b64e}}.sh
-exec rm -rf {{trash_dir}}/*
-EOF
+bin/sched
+bin/sched/purge/scripts
 {% endblock %}
