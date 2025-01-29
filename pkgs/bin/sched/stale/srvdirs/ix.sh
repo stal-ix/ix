afd21c611d6dc438e014680b9b6fb75e21112073
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-01-29 17:27:31 +0300

    fix

--- pkgs/bin/sched/stale/srvdirs/ix.sh
+++ pkgs/bin/sched/stale/srvdirs/ix.sh
@@ -1,14 +1,7 @@
-{% extends '//die/gen.sh' %}
+{% extends '//die/hub.sh' %}
 
 {% block run_deps %}
 bin/purge
-bin/sched(delay={{delay}})
-{% endblock %}
-
-{% block install %}
-cd ${out}; mkdir -p etc/sched/{{delay}}; cd etc/sched/{{delay}}
-
-base64 -d << EOF > srvdirs.sh
-{% include 'srvdirs.sh/base64' %}
-EOF
+bin/sched
+bin/sched/stale/srvdirs/scripts
 {% endblock %}
