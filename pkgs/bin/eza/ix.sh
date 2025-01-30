58c1518e6c1a8f602d38ce3f5d70b2f9f57f1084
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-01-30 20:19:37 +0300

    up

--- pkgs/bin/eza/ix.sh
+++ pkgs/bin/eza/ix.sh
@@ -1,11 +1,11 @@
 {% extends '//die/rust/cargo.sh' %}
 
 {% block cargo_url %}
-https://github.com/eza-community/eza/archive/refs/tags/v0.20.18.tar.gz
+https://github.com/eza-community/eza/archive/refs/tags/v0.20.19.tar.gz
 {% endblock %}
 
 {% block cargo_sha %}
-aefd6954a147a586801627f17b6524d75f3e39777f3fb49d04d1acb383486f45
+484ed2a8984f619c2a357824a04dca05b15cfeb5a6bf733d9f6c47d8fb4f2e11
 {% endblock %}
 
 {% block bld_libs %}
