fa26f0a015a18143e2dccd2d90809cea79b35689
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-02-01 14:26:31 +0300

    add

---
+++ pkgs/lib/icu/chromium/ix.sh
@@ -0,0 +1,6 @@
+{% extends '//lib/icu/static/ix.sh' %}
+
+{% block fetch %}
+https://github.com/unicode-org/icu/archive/refs/tags/release-73-2.tar.gz
+sha:c15f704e83c221c0680640a995d9db641f5b82098fb4b258a94b7d0561493c88
+{% endblock %}
