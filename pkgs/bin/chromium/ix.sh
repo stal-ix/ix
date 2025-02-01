4483a5b77628a4e32457396b25c5d05e93e2c00f
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-02-01 16:07:49 +0300

    fix

--- pkgs/bin/chromium/ix.sh
+++ pkgs/bin/chromium/ix.sh
@@ -1,5 +1,5 @@
 {% extends '//die/hub.sh' %}
 
 {% block run_deps %}
-bin/chromium/unwrap(libc_lite=1,libopenssl_ver=fake,libcplpl_ver=17,clang_ver=17,ffmpeg_ver=6)
+bin/chromium/unwrap(libc_lite=1,libopenssl_ver=fake,libcplpl_ver=17,clang_ver=17,ffmpeg_ver=6,libicu_ver=chromium)
 {% endblock %}
