4c15668234df3efa3a8568167eac3ab2b9706a83
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-02-01 14:00:42 +0300

    fix

--- pkgs/bin/findutils/ix.sh
+++ pkgs/bin/findutils/ix.sh
@@ -4,8 +4,3 @@
 https://ftp.gnu.org/gnu/findutils/findutils-4.10.0.tar.xz
 sha:1387e0b67ff247d2abde998f90dfbf70c1491391a59ddfecb8ae698789f0a4f5
 {% endblock %}
-
-{% block cpp_includes %}
-${PWD}/gl/lib
-{% endblock %}
-
