8ba2a640151d229bc8a124ef5e395de71954a2c4
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-02-02 20:38:11 +0300

    up

--- pkgs/bin/gdb/16/ix.sh
+++ pkgs/bin/gdb/16/ix.sh
@@ -1,6 +1,6 @@
 {% extends '//bin/gdb/14/ix.sh' %}
 
 {% block fetch %}
-https://ftp.gnu.org/gnu/gdb/gdb-16.1.tar.xz
-sha:c2cc5ccca029b7a7c3879ce8a96528fdfd056b4d884f2b0511e8f7bc723355c6
+https://ftp.gnu.org/gnu/gdb/gdb-16.2.tar.xz
+sha:4002cb7f23f45c37c790536a13a720942ce4be0402d929c9085e92f10d480119
 {% endblock %}
