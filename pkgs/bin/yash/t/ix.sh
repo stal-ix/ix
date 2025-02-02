0a13de022dc6b5d2fe46a5f56825b49320100990
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-02-02 20:28:17 +0300

    up

--- pkgs/bin/yash/t/ix.sh
+++ pkgs/bin/yash/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
-https://github.com/magicant/yash/releases/download/2.57/yash-2.57.tar.xz
-sha:f5ff3334dcfa0fdde3882f5df002623f46a0a4f2b2335e7d91715520d8fb1dab
+https://github.com/magicant/yash/releases/download/2.58/yash-2.58.tar.xz
+sha:1a027496a6b8d2aa946d0b13407fdc3d5030f1d17f09b27768967c50f09e61f0
 {% endblock %}
 
 {% block c_rename_symbol %}
