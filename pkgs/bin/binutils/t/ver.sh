19361a0f1947470c3d69a792ce1fcc55becad753
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-02-03 22:52:16 +0300

    up

--- pkgs/bin/binutils/t/ver.sh
+++ pkgs/bin/binutils/t/ver.sh
@@ -1,2 +1,2 @@
-https://ftp.gnu.org/gnu/binutils/binutils-2.43.1.tar.bz2
-sha:becaac5d295e037587b63a42fad57fe3d9d7b83f478eb24b67f9eec5d0f1872f
+https://ftp.gnu.org/gnu/binutils/binutils-2.44.tar.bz2
+sha:f66390a661faa117d00fab2e79cf2dc9d097b42cc296bf3f8677d1e7b452dc3a
