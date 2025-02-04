506ac88a141356fec9086c711104020123c95291
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-02-04 22:20:31 +0300

    up

--- pkgs/bin/bcache/fs/tools/ver.sh
+++ pkgs/bin/bcache/fs/tools/ver.sh
@@ -1,11 +1,11 @@
 {% block http_sha %}
-sha:cc7196505eba3a71e2ab8b9d0d19c6b16959b15aeb6e57e696dc14bd735ee0d5
+sha:e616fea8bbd26581fb614fef9e50432a55a30248d7c42823cc06fee9e0d38747
 {% endblock %}
 
 {% block cargo_url %}
-https://github.com/koverstreet/bcachefs-tools/archive/refs/tags/v1.13.0.tar.gz
+https://github.com/koverstreet/bcachefs-tools/archive/refs/tags/v1.20.0.tar.gz
 {% endblock %}
 
 {% block cargo_sha %}
-ef400d4a868d791e0aa8455adff22aec68500906578e93b85661cbb5d82c6d72
+d23b51e6820dd5d3b66faefba3a635284eee8c83ee03697b0bc82b384efd397b
 {% endblock %}
