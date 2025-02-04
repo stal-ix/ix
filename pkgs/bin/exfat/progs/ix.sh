e7c04295886aa38b8e605cf4f3a084f6706469dd
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-02-04 12:47:35 +0300

    up

--- pkgs/bin/exfat/progs/ix.sh
+++ pkgs/bin/exfat/progs/ix.sh
@@ -1,11 +1,19 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://github.com/exfatprogs/exfatprogs/archive/refs/tags/1.2.5.tar.gz
-sha:ce638f06b15b3553307d244f13c64bdbd52a73460e383a5aa5d60d84a58d51b0
+https://github.com/exfatprogs/exfatprogs/archive/refs/tags/1.2.7.tar.gz
+sha:dadf6c58ecc58572d6a482c66a5c60fea2ca71a7df293be6b6008d7cdcebe99a
+{% endblock %}
+
+{% block conf_ver %}
+2/71
 {% endblock %}
 
 {% block bld_libs %}
 lib/c
 lib/kernel
+{% endblock %}
+
+{% block cpp_defines %}
+loff_t=off_t
 {% endblock %}
