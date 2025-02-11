30740a0a3179688c8f7fe05f8ad93729b2b6d5e6
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-02-11 18:09:23 +0300

    fix

--- pkgs/lib/nss/init/ix.sh
+++ pkgs/lib/nss/init/ix.sh
@@ -8,9 +8,14 @@ lib/nss/unwrap
 #include <stdlib.h>
 
 extern int RNG_RNGInit();
+extern int pk11_setGlobalOptions(int, int, int);
 
 __attribute__ ((__constructor__))
 void init_nss(void) {
+    if (pk11_setGlobalOptions(0, 1, 0)) {
+        abort();
+    }
+
     if (RNG_RNGInit()) {
         abort();
     }
--- pkgs/lib/nss/test/main.cpp
+++ pkgs/lib/nss/test/main.cpp
@@ -7,6 +7,21 @@
 #include <secmod.h>
 #include <iostream>
 
+SECMODModule* LoadNSSModule(const char* name, const char* library_path, const char* params) {
+  std::string modparams = "name=\"" + std::string(name) + "\" library=\"" + std::string(library_path) + "\" " + std::string(params) + "";
+
+  SECMODModule* module = SECMOD_LoadUserModule(const_cast<char*>(modparams.c_str()), nullptr, PR_FALSE);
+  if (!module) {
+    return nullptr;
+  }
+  if (!module->loaded) {
+    std::cerr << "not loaded" << std::endl;
+    SECMOD_DestroyModule(module);
+    return nullptr;
+  }
+  return module;
+}
+
 int main() {
     PR_Init(PR_USER_THREAD, PR_PRIORITY_NORMAL, 0);
     std::cerr << PR_GetError() << std::endl;
@@ -15,4 +30,5 @@ int main() {
     std::cerr << PR_GetError() << std::endl;
     std::cerr << PR_GetOSError() << std::endl;
     std::cerr << SECSuccess << std::endl;
+    LoadNSSModule("Root Certs", "libnssckbi.so", "");
 }
