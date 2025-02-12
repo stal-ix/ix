94d646275778f2b24a2b720bdefa787040e3ea28
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-02-12 17:25:47 +0300

    up

--- pkgs/lib/openssl/3/ix.sh
+++ pkgs/lib/openssl/3/ix.sh
@@ -1,8 +1,8 @@
 {% extends 't/ix.sh' %}
 
 {% block fetch %}
-https://github.com/openssl/openssl/archive/refs/tags/openssl-3.4.0.tar.gz
-sha:1ca043a26fbea74cdf7faf623a6f14032a01117d141c4a5208ccac819ccc896b
+https://github.com/openssl/openssl/archive/refs/tags/openssl-3.4.1.tar.gz
+sha:d4b22527a645acf76b53e44487a8db687c6eed621d7246891d025e38ba8c9651
 {% endblock %}
 
 {% block env %}
