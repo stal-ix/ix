2e8b9143ed61b9967823e0075a90dce8230d7fc3
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-02-13 19:55:41 +0300

    up

--- pkgs/bin/gnupg/ix.sh
+++ pkgs/bin/gnupg/ix.sh
@@ -1,6 +1,6 @@
 {% extends 't/ix.sh' %}
 
 {% block fetch %}
-https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-2.5.3.tar.bz2
-sha:23128b136aed4e5121e793d1b6c60ee50c8007a9d926c1313e524d05386b54ac
+https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-2.5.4.tar.bz2
+sha:f8cb2ac6c9f17f81e3541c62158764910fad5c3a0765e00b398c41a32bc4e0ea
 {% endblock %}
