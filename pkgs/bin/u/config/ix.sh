7cf3cb3d4ddc50579014e3ab2baade4226d97f51
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-02-13 20:46:34 +0300

    up

--- pkgs/bin/u/config/ix.sh
+++ pkgs/bin/u/config/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/ix.sh' %}
 
 {% block fetch %}
-https://github.com/skeeto/u-config/archive/refs/tags/v0.33.0.tar.gz
-sha:bed099bf49625b0ec616446f037b76cef85243ef5fcd7ad07ef8fcb82f20f00d
+https://github.com/skeeto/u-config/archive/refs/tags/v0.33.2.tar.gz
+sha:58779127559e511e85a85f703c4ab82d65655908da060d1e3e5e1e6d49daf63f
 {% endblock %}
 
 {% block bld_libs %}
