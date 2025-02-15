0378e27696495598620135a7f82536ac22041f92
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-02-15 18:00:43 +0300

    up

--- pkgs/bin/iperf/ix.sh
+++ pkgs/bin/iperf/ix.sh
@@ -1,11 +1,19 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://downloads.sourceforge.net/iperf2/iperf-2.1.9.tar.gz
-sha:5c0771aab00ef14520013aef01675977816e23bb8f5d9fde016f90eb2f1be788
+https://downloads.sourceforge.net/iperf2/iperf-2.2.1.tar.gz
+sha:754ab0a7e28033dbea81308ef424bc7df4d6e2fe31b60cc536b61b51fefbd8fb
+{% endblock %}
+
+{% block conf_ver %}
+2/71
 {% endblock %}
 
 {% block bld_libs %}
 lib/c
 lib/kernel
+{% endblock %}
+
+{% block cpp_defines %}
+_GNU_SOURCE
 {% endblock %}
