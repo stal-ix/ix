79e3e02448f59bcafd0236d614a7fc5315d33ead
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-02-15 17:51:23 +0300

    up

--- pkgs/bin/kernel/6/12/ver.sh
+++ pkgs/bin/kernel/6/12/ver.sh
@@ -1,9 +1,9 @@
 {% block kernel_version %}
-6.12.9
+6.12.13
 {% endblock %}
 
 {% block kernel_sha %}
-87be0360df0931b340d2bac35161a548070fbc3a8c352c49e21e96666c26aeb4
+f3ebdeea9e555b4cface44e29670056f4024541e6bd222fbcf776c818974fbba
 {% endblock %}
 
 {% include '//bin/kernel/t/ver.sh' %}
