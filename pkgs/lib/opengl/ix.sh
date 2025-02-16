9244a8d128ea206d8bb25454ff89f1ab14f91cf8
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-02-16 16:53:00 +0300

    better

--- pkgs/lib/opengl/ix.sh
+++ pkgs/lib/opengl/ix.sh
@@ -1,11 +1,10 @@
 {% extends '//die/hub.sh' %}
 
 {% block lib_deps %}
-{% if linux %}
-lib/mesa/gl
-lib/mesa/egl
-{% endif %}
-{% if darwin %}
-lib/darwin/framework/Carbon
-{% endif %}
+  {% if linux %}
+    lib/mesa/gl
+    lib/mesa/egl
+  {% elif darwin %}
+    lib/darwin/framework/Carbon
+  {% endif %}
 {% endblock %}
