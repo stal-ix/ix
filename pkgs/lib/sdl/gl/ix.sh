7a5da6311a5c08ba2fc48f5e0f39032873ea931a
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-02-16 16:50:33 +0300

    fix

--- pkgs/lib/sdl/gl/ix.sh
+++ pkgs/lib/sdl/gl/ix.sh
@@ -3,8 +3,10 @@
 {% block lib_deps %}
 {% if mesa_driver %}
 lib/opengl
-{% else %}
+{% elif linux %}
 lib/shim/fake(lib_name=EGL)
 lib/shim/fake/pkg(pkg_name=egl,pkg_ver=100500)
+{% else %}
+lib/opengl
 {% endif %}
 {% endblock %}
