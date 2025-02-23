7d71fd977624d2c003462b7217e018a7b08bd870
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-02-23 18:51:49 +0300

    add

--- pkgs/lib/opengl/headers/ix.sh
+++ pkgs/lib/opengl/headers/ix.sh
@@ -3,6 +3,14 @@
 {% block fetch %}
 https://github.com/NVIDIA/libglvnd/archive/refs/tags/v1.7.0.tar.gz
 sha:073e7292788d4d3eeb45ea6c7bdcce9bfdb3b3eef8d7dbd47f2f30dce046ef98
+https://gitlab.freedesktop.org/mesa/mesa/-/raw/06391759f0c705f24d0763a2e3691f55ce5f4f22/src/gbm/main/gbm.h
+sha:95f3b4a6ee5175c7cc5d47368d4efb100063fe49e5a6f5b19030ac2ceed73b81
+{% endblock %}
+
+{% block step_unpack %}
+mkdir src
+cd src
+extract 1 ${src}/v*
 {% endblock %}
 
 {% block install %}
@@ -12,4 +20,5 @@ rm \
     ${out}/include/meson.build \
     ${out}/include/Makefile.am
 rm -rf ${out}/include/glvnd
+cp ${src}/gbm.h ${out}/include/
 {% endblock %}
