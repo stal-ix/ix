3a78e417d536fc7af6a26103028f4867607282c8
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-02-25 19:36:07 +0300

    up

--- pkgs/lib/dex/ix.sh
+++ pkgs/lib/dex/ix.sh
@@ -1,13 +1,18 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://gitlab.gnome.org/GNOME/libdex/-/archive/0.9.0/libdex-0.9.0.tar.bz2
-sha:cf7f923f3fc48d7e9efadc72cb22219b189776ffc896ac86fa0b60dc6e0b459a
+https://gitlab.gnome.org/GNOME/libdex/-/archive/0.9.1/libdex-0.9.1.tar.bz2
+sha:e4cc4beb08acc62474c437d4f698067f0176afbc5863f362a646b9714f9b21d2
 {% endblock %}
 
 {% block lib_deps %}
 lib/c
 lib/glib
+lib/uring
+{% endblock %}
+
+{% block bld_libs %}
+lib/shim/fake(lib_name=atomic)
 {% endblock %}
 
 {% block build_flags %}
@@ -19,4 +24,9 @@ examples=false
 vapi=false
 introspection=disabled
 tests=false
+{% endblock %}
+
+{% block install %}
+{{super()}}
+sed -e 's|-latomic||g' -i ${out}/lib/pkgconfig/libdex-1.pc
 {% endblock %}
