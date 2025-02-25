b11d531b8e034a16b1ca4ca2ab0f94e02cd018cb
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-02-25 21:47:49 +0300

    up

--- pkgs/lib/gtk/4/t/ver.sh
+++ pkgs/lib/gtk/4/t/ver.sh
@@ -1,2 +1,2 @@
-https://gitlab.gnome.org/GNOME/gtk/-/archive/4.16.12/gtk-4.16.12.tar.bz2
-sha:c98fe4a4fa4ad4f578c21a96f31930c53e7f7d8a45efee433d144c99f449865d
+https://gitlab.gnome.org/GNOME/gtk/-/archive/4.17.5/gtk-4.17.5.tar.bz2
+sha:b0b23d29502c5745bb1ccd7cdcb78403ccb91da1b3fe13065a7de88a1733fbc2
--- pkgs/lib/gtk/source/view/5/t/ix.sh
+++ pkgs/lib/gtk/source/view/5/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/gnome.sh' %}
 
 {% block fetch %}
-https://gitlab.gnome.org/GNOME/gtksourceview/-/archive/5.14.2/gtksourceview-5.14.2.tar.bz2
-sha:ed95c15fd1cb4c017883b154f412a19fe6db248c869a94da91ee08d6877aef25
+https://gitlab.gnome.org/GNOME/gtksourceview/-/archive/5.15.1/gtksourceview-5.15.1.tar.bz2
+sha:6e0d84900cfbce55964ef0936a2c61009140d33c21b92d319724567d0a0a7fab
 {% endblock %}
 
 {% block lib_deps %}
