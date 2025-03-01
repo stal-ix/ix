4d42670f7704897abfd3a7743cd8339ec09fc3c1
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-03-01 21:10:47 +0300

    fix

--- pkgs/aux/ca/bundle/ix.sh
+++ pkgs/aux/ca/bundle/ix.sh
@@ -1,5 +1,5 @@
 {% extends 'nopredict/ix.sh' %}
 
 {% block predict_outputs %}
-[{"path": "{{self.pem_path().strip()}}", "sum": "02b03dcfa423f75b72b47dac373e795d57cde3b341231f5d45922132e3a87f5d"}]
+[{"path": "{{self.pem_path().strip()}}", "sum": "b6800e7c18d6f7d1cdb4644836cc4564b759cf04e6d2bba8c059e888b30b98b5"}]
 {% endblock %}
