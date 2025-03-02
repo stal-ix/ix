3a84c85385a100d4c920f20266a0f66278fae7c2
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-03-02 19:56:42 +0300

    add

---
+++ pkgs/lib/utempter/ix.sh
@@ -0,0 +1,26 @@
+{% extends '//die/c/make.sh' %}
+
+{% block fetch %}
+https://github.com/altlinux/libutempter/archive/refs/tags/1.2.3-alt1.tar.gz
+sha:52809c75af9b0e13249521177def85787f457888cbcee5511bf96fe06e146711
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+{% endblock %}
+
+{% block unpack %}
+{{super()}}
+cd libutempter
+{% endblock %}
+
+{% block build_flags %}
+wrap_cc
+{% endblock %}
+
+{% block make_flags %}
+libdir=${out}/lib
+libexecdir=${out}/bin
+includedir=${out}/include
+mandir=${out}/man
+{% endblock %}
