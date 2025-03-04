1acf882bcd9a5ea102bfabbd889b2f53b6b0d21e
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-03-05 00:33:58 +0300

    better

--- pkgs/bld/jail/scripts/jail0.sh
+++ pkgs/bld/jail/scripts/jail0.sh
@@ -7,6 +7,7 @@ shift
 mount -t tmpfs tmpfs ${where}
 cd ${where}
 mkdir -p ./${store}
+mount --bind ${store} ./${store}
 mkdir dev
 >dev/null
 >dev/zero
@@ -25,6 +26,8 @@ mkdir bin
 cp $(command -v sh) bin/
 mkdir -p usr/bin
 cp $(command -v env) usr/bin/
-mount --bind ${store} ./${store}
-ls -la ./${store}
+mkdir etc
+cat << EOF > etc/passwd
+root:x:0:0:none:/home/root:/bin/sh
+EOF
 exec ${@}
