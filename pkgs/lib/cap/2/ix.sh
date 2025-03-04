0094c6204697ec8666035f4648879aa5ad5cf0fe
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-03-04 22:28:11 +0300

    up

--- pkgs/lib/cap/2/ix.sh
+++ pkgs/lib/cap/2/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//lib/cap/t/ix.sh' %}
 
 {% block fetch %}
-https://www.kernel.org/pub/linux/libs/security/linux-privs/libcap2/libcap-2.73.tar.gz
-sha:16d8e4e625d2a2ed1601172704c560de3745bac64ab1384a8074379c03165948
+https://www.kernel.org/pub/linux/libs/security/linux-privs/libcap2/libcap-2.74.tar.gz
+sha:10250c32cd3060bb6ca2b8470ae929fdb5459ba0cdec91c223a0271ab7f419fc
 {% endblock %}
 
 {% block host_libs %}
