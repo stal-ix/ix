13599f4665721ebca3ebc63aa0153ecd7adba2f7
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-03-06 17:16:12 +0300

    better

--- pkgs/bin/ruff/ix.sh
+++ pkgs/bin/ruff/ix.sh
@@ -1,5 +1,31 @@
-{% extends '//die/hub.sh' %}
+{% extends '//die/rust/cargo.sh' %}
 
-{% block run_deps %}
-bin/ruff/unwrap(allocator=jemalloc)
+{% block cargo_url %}
+https://github.com/astral-sh/ruff/archive/refs/tags/0.9.6.tar.gz
+{% endblock %}
+
+{% block cargo_sha %}
+26897ac3da13a0f43532323db923f4094f53f3955683fc3b003332c3ac8a4c81
+{% endblock %}
+
+{% block install %}
+mkdir ${out}/bin
+cp ${tmp}/out/ruff ${out}/bin/
+{% endblock %}
+
+{% block cargo_ver %}
+v4
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/zstd
+lib/shim/je
+{{super()}}
+{% endblock %}
+
+{% block host_libs %}
+lib/c
+lib/zstd
+{{super()}}
 {% endblock %}
