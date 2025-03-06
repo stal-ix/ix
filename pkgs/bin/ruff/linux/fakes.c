7e7c90e5eecaa57ffd5bd4ca57675ad73960a209
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-03-07 02:23:47 +0300

    fix

--- pkgs/bin/ruff/linux/fakes.c
+++ pkgs/bin/ruff/linux/fakes.c
@@ -1,5 +1,1 @@
-void rallocx() {
-}
 
-void mallocx() {
-}
---
+++ pkgs/bin/ruff/unwrap/ix.sh
@@ -0,0 +1,36 @@
+{% extends '//die/rust/cargo.sh' %}
+
+{% block cargo_url %}
+https://github.com/astral-sh/ruff/archive/refs/tags/0.9.6.tar.gz
+{% endblock %}
+
+{% block cargo_sha %}
+26897ac3da13a0f43532323db923f4094f53f3955683fc3b003332c3ac8a4c81
+{% endblock %}
+
+{% block install %}
+mkdir ${out}/bin
+cp ${tmp}/out/ruff{{target.exe_suffix}} ${out}/bin/
+{% endblock %}
+
+{% block cargo_ver %}
+v4
+{% endblock %}
+
+{% block bld_libs %}
+lib/c
+lib/zstd
+bin/ruff/{{target.os}}
+{{super()}}
+{% endblock %}
+
+{% block host_libs %}
+lib/c
+lib/zstd
+{{super()}}
+{% endblock %}
+
+{% block cargo_flags %}
+{{super()}}
+--config 'target.x86_64-pc-windows-gnullvm.linker = "clang"'
+{% endblock %}
