d27c3ce8bd61a3823802ccd4c7dac70d8eebda74
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-03-06 17:51:21 +0300

    add

--- pkgs/bld/rust/linux/83/ix.sh
+++ pkgs/bld/rust/linux/83/ix.sh
@@ -9,6 +9,8 @@ https://static.rust-lang.org/dist/rust-std-1.83.0-aarch64-apple-darwin.tar.gz
 sha:7b9fa15213cf3fae3a4ae80bb2a0ebabdce29599aa381bbb7b12bb5043fe9a04
 https://static.rust-lang.org/dist/rust-std-1.83.0-x86_64-apple-darwin.tar.gz
 sha:f66146cde62697a6ff2587b3282b4a00f54c35f17109a7a86f5c56b1d0917261
+https://static.rust-lang.org/dist/rust-std-1.83.0-x86_64-pc-windows-gnullvm.tar.gz
+sha:29ba0e4821bd0c27b8d515a5fab070cc8581c72d2c3c53fecc9dbd4bcd755026
 {% endblock %}
 
 {% block bld_tool %}
