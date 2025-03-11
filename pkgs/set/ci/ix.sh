b01ee487348c087b0d1568efa00929a6b85f26ea
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-03-11 19:21:36 +0300

    better

--- pkgs/set/ci/ix.sh
+++ pkgs/set/ci/ix.sh
@@ -3,4 +3,5 @@
 {% block run_deps %}
 set/ci/tier/0
 set/ci/tier/1
+set/ci/tier/2
 {% endblock %}
--- pkgs/set/ci/tier/2/ix.sh
+++ pkgs/set/ci/tier/2/ix.sh
@@ -1,5 +1,5 @@
 {% extends '//die/hub.sh' %}
 
 {% block run_deps %}
-set/ci/unwrap(slot=1)
+bin/chromium
 {% endblock %}
