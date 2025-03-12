0e352443261e34bbcfbb7d7daa9720c95b069ea5
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-03-12 13:23:47 +0300

    up

--- pkgs/lib/pango/t/ix.sh
+++ pkgs/lib/pango/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://gitlab.gnome.org/GNOME/pango/-/archive/1.56.1/pango-1.56.1.tar.bz2
-sha:ccca809519ef46a5c9503e600ef5a1f03412a35ce2a55937144b8fcd9300d580
+https://gitlab.gnome.org/GNOME/pango/-/archive/1.56.2/pango-1.56.2.tar.bz2
+sha:dd45737946ffec9ccf589209caed19dee437d36d61ac92dfb42f7e99be8c46f3
 {% endblock %}
 
 {% block lib_deps %}
