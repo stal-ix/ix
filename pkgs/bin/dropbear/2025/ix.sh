e0920164af7129aabc9d9838a52ae09981814604
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-03-13 13:49:06 +0300

    add

---
+++ pkgs/bin/dropbear/2025/ix.sh
@@ -0,0 +1,10 @@
+{% extends '//bin/dropbear/stock/ix.sh' %}
+
+{% block fetch %}
+https://github.com/mkj/dropbear/archive/refs/tags/DROPBEAR_2025.87.tar.gz
+sha:ca32b32280546dfafe474d3976fc80f337cc91f529f05772ea27eafe43a74051
+{% endblock %}
+
+{% block conf_ver %}
+2/71
+{% endblock %}
