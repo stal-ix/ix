2f65a1d6e59dd6fc01f28115a51e6b6d750d84ad
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-03-13 14:47:30 +0300

    better

--- pkgs/bin/u/utils/find/ix.sh
+++ pkgs/bin/u/utils/find/ix.sh
@@ -12,8 +12,7 @@ d387e3a3e0677362bde5a4c1bc15cc10c85942af66b12c57456cf91750f4a721
 lib/oniguruma
 {% endblock %}
 
-{% block install %}
-mkdir ${out}/bin
-cp ${tmp}/out/find ${out}/bin/
-cp ${tmp}/out/xargs ${out}/bin/
+{% block cargo_bins %}
+find
+xargs
 {% endblock %}
