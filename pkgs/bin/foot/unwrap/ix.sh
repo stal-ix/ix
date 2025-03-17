da6117b5ef8f6d9c5aebd726232539e75188d03e
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-03-17 23:27:37 +0300

    up

--- pkgs/bin/foot/unwrap/ix.sh
+++ pkgs/bin/foot/unwrap/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block fetch %}
-https://codeberg.org/dnkl/foot/archive/1.20.2.tar.gz
-sha:9a8832e2485923c8a79e77958c8b0357abd159dfa8958609402362da8e56c073
+https://codeberg.org/dnkl/foot/archive/1.21.0.tar.gz
+sha:b93b196a3fbab86678c54be627557bdc7b1fc8042d99b14c4a74b149f60bcd52
 {% endblock %}
 
 {% block bld_libs %}
