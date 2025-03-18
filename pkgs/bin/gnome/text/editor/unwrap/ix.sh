48c7f7c54eb3d8178603601eeb6ae4f0dae9bbe2
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-03-18 12:44:03 +0300

    up

--- pkgs/bin/gnome/text/editor/unwrap/ix.sh
+++ pkgs/bin/gnome/text/editor/unwrap/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/gnome.sh' %}
 
 {% block fetch %}
-https://gitlab.gnome.org/GNOME/gnome-text-editor/-/archive/47.3/gnome-text-editor-47.3.tar.bz2
-sha:06e46659350ba52fb433e8d2c18124b1cd198ce10e2a5817495e37591b60d146
+https://gitlab.gnome.org/GNOME/gnome-text-editor/-/archive/48.0/gnome-text-editor-48.0.tar.bz2
+sha:2c8113d6b3e146fe1145ec16db7645a616b23e3ba31b68ef9182ea7046c270c5
 {% endblock %}
 
 {% block bld_libs %}
