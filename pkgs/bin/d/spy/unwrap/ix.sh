b77d872810cf661270722eed4ccd7eaccddbd5b9
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-03-18 12:47:29 +0300

    up

--- pkgs/bin/d/spy/unwrap/ix.sh
+++ pkgs/bin/d/spy/unwrap/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/gnome.sh' %}
 
 {% block fetch %}
-https://gitlab.gnome.org/GNOME/d-spy/-/archive/47.0/d-spy-47.0.tar.bz2
-sha:6dae353c0f515a898352f38429ca5376d8c5b47f258d2abda25df3248d435033
+https://gitlab.gnome.org/GNOME/d-spy/-/archive/48.0/d-spy-48.0.tar.bz2
+sha:ea985ac78980b0664464ce94f01ef2375e2829f0d0c3018ceca1dfa53de371d8
 {% endblock %}
 
 {% block bld_libs %}
