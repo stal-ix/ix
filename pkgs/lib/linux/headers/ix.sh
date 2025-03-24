65f574750e3cd4899f2c1c8560354e11a2794ce2
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-03-25 00:27:43 +0300

    up

--- pkgs/lib/linux/headers/ix.sh
+++ pkgs/lib/linux/headers/ix.sh
@@ -1,8 +1,10 @@
 {% extends 't/ix.sh' %}
 
+{% include '//bin/kernel/6/12/ver.sh' %}
+
 {% block fetch %}
-https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.10.3.tar.xz
-sha:fa5f22fd67dd05812d39dca579320c493048e26c4a556048a12385e7ae6fc698
+{{self.kernel_url().strip()}}
+{{self.kernel_sha().strip()}}
 {% endblock %}
 
 {% block host_libs %}
