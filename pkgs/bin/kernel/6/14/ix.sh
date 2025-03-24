13e84446757a3e7a6db9304509a91c5790feb4ba
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-03-24 23:23:46 +0300

    add

---
+++ pkgs/bin/kernel/6/14/ix.sh
@@ -0,0 +1,7 @@
+{% extends '//bin/kernel/t/4/ix.sh' %}
+
+{% include 'ver.sh' %}
+
+{% block kernel_flags %}
+{% include '//bin/kernel/configs/cfg_6_12' %}
+{% endblock %}
---
+++ pkgs/bin/kernel/6/14/ver.sh
@@ -0,0 +1,9 @@
+{% block kernel_version %}
+6.14
+{% endblock %}
+
+{% block kernel_sha %}
+a294b683e7b161bb0517bb32ec7ed1d2ea7603dfbabad135170ed12d00c47670
+{% endblock %}
+
+{% include '//bin/kernel/t/ver.sh' %}
