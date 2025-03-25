407621d02ded2af77abebed0ab3450591d7eeb8a
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-03-25 18:54:22 +0300

    better

--- pkgs/lib/svt/av1/t/ix.sh
+++ pkgs/lib/svt/av1/t/ix.sh
@@ -3,20 +3,11 @@
 {% block fetch %}
 https://gitlab.com/AOMediaCodec/SVT-AV1/-/archive/v3.0.2/SVT-AV1-v3.0.2.tar.bz2
 sha:7548a380cd58a46998ab4f1a02901ef72c37a7c6317c930cde5df2e6349e437b
-https://github.com/1480c1/cpuinfo/archive/e649baaa95efeb61517c06cc783287d4942ffe0e.zip
-sha:d2cfc3b4120ad2f17a7c6cc43d0547ecfb9f375b7918d723d234100822aa490e
 {% endblock %}
 
 {% block lib_deps %}
 lib/c
-{% endblock %}
-
-{% block step_unpack %}
-mkdir src
-cd src
-extract 1 ${src}/SVT-AV1-v3.0.2.tar.bz2
-mkdir -p ${tmp}/obj/_deps/cpuinfo-subbuild/cpuinfo-populate-prefix/src
-cp ${src}/e649baaa95efeb61517c06cc783287d4942ffe0e.zip ${tmp}/obj/_deps/cpuinfo-subbuild/cpuinfo-populate-prefix/src
+lib/cpu/info
 {% endblock %}
 
 {% block bld_tool %}
@@ -29,6 +20,7 @@ bld/cctools
 {% endblock %}
 
 {% block cmake_flags %}
+USE_CPUINFO=SYSTEM
 {% if x86_64 %}
 ENABLE_NASM=ON
 {% endif %}
