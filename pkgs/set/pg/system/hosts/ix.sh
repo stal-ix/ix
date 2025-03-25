98c9114e2b2c3ab7e510fa01947b6b03555d1cd7
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-03-26 00:32:12 +0300

    add

--- pkgs/set/pg/system/hosts/ix.sh
+++ pkgs/set/pg/system/hosts/ix.sh
@@ -7,6 +7,7 @@ cat << EOF > ${out}/etc/hosts.d/01-nebula.conf
 192.168.100.16 lab1.nebula
 192.168.100.17 lab2.nebula
 192.168.100.18 lab3.nebula
+192.168.100.19 lab4.nebula
 10.0.0.64      lab1.local
 10.0.0.68      lab2.local
 10.0.0.72      lab3.local
