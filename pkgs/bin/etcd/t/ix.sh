e7c14e51a7bec5dbe93029da225bd96d79566264
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-03-28 13:56:37 +0300

    up

--- pkgs/bin/etcd/t/ix.sh
+++ pkgs/bin/etcd/t/ix.sh
@@ -1,11 +1,11 @@
 {% extends '//die/go/build.sh' %}
 
 {% block go_url %}
-https://github.com/etcd-io/etcd/archive/refs/tags/v3.5.20.tar.gz
+https://github.com/etcd-io/etcd/archive/refs/tags/v3.5.21.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-72e76d730e43ad726e3031dd099ebdaef411bd7a5e53a1a0b15ea61f35fec85a
+7fe8b7346312b96bb787c4b83bd370a66edc333608dca888f87c84cc2d3c9f3f
 {% endblock %}
 
 {% block bld_libs %}
