def8a7d51aaf23474fc54c5db2ca6f7c76ae3726
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-03-28 13:57:55 +0300

    up

--- pkgs/lib/ell/ix.sh
+++ pkgs/lib/ell/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://mirrors.edge.kernel.org/pub/linux/libs/ell/ell-0.74.tar.xz
-sha:f1690913024f80fafff19ecf010accb58a56784d78937bae536c016b8276bb75
+https://mirrors.edge.kernel.org/pub/linux/libs/ell/ell-0.75.tar.xz
+sha:10cee84ee075d9e0fc1c65fa1acdf7f6ffc2b3ed7e09622be9fc8f8979b02bd5
 {% endblock %}
 
 {% block lib_deps %}
