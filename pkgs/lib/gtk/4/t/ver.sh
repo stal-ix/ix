57180bf5a63e67008785e8c52c3a70fbebf88347
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-03-29 23:36:50 +0300

    up

--- pkgs/lib/gtk/4/t/ver.sh
+++ pkgs/lib/gtk/4/t/ver.sh
@@ -1,2 +1,2 @@
-https://gitlab.gnome.org/GNOME/gtk/-/archive/4.18.2/gtk-4.18.2.tar.bz2
-sha:5b743401689a4f7f8a4ad0959bf18850f21373912eb982a7d168d3005bf903ad
+https://gitlab.gnome.org/GNOME/gtk/-/archive/4.18.3/gtk-4.18.3.tar.bz2
+sha:9964e62ef27e3e5e03d5840664568590bec6575b036d85da4fe1e079db98b62f
