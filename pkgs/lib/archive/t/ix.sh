75e9654ec86dd050ccd1de61147af4c1bb06d780
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-03-31 13:53:41 +0300

    up

--- pkgs/lib/archive/t/ix.sh
+++ pkgs/lib/archive/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/libarchive/libarchive/archive/refs/tags/v3.7.8.tar.gz
-sha:a1d557d7d8b7fb61102d20006fead5a916624051b7c6ded444e6c6b5af01b226
+https://github.com/libarchive/libarchive/archive/refs/tags/v3.7.9.tar.gz
+sha:6191ea9756a7a0b116d701f3bce30ee3dc8c1f0a16b1e6221d273c85b57ce8c6
 {% endblock %}
 
 {% block cmake_flags %}
