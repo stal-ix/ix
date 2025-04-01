a00a503f7e0daeee60365148f2bf506897ed5c03
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-01 14:57:58 +0300

    up

--- pkgs/bin/hypr/idle/ix.sh
+++ pkgs/bin/hypr/idle/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/cmake.sh' %}
 
 {% block fetch %}
-https://github.com/hyprwm/hypridle/archive/refs/tags/v0.1.5.tar.gz
-sha:174d4b4706ec5c79e6d80a7cc3b297b5564c923f60f408c1638b55424a998a4b
+https://github.com/hyprwm/hypridle/archive/refs/tags/v0.1.6.tar.gz
+sha:ea4faf92e7ef303a538551e2b0ea67a557b2d711574993a5a3fea3b27667dc63
 {% endblock %}
 
 {% block bld_libs %}
@@ -15,4 +15,6 @@ lib/sd/bus/c++/2
 
 {% block bld_tool %}
 bld/wayland
+bin/hypr/wayland/scanner
+bin/hypr/wayland/protocols
 {% endblock %}
