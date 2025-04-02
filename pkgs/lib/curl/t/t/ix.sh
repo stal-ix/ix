ca3d8b52bbfef0218cb5fd3c43495a7426ef2c07
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-02 11:19:39 +0300

    up

--- pkgs/lib/curl/t/t/ix.sh
+++ pkgs/lib/curl/t/t/ix.sh
@@ -1,8 +1,15 @@
 {% extends '//die/c/autorehell.sh' %}
 
+{#
+/ix/build/MDjqMeM8IRlmXue54dm0E6/src/Source/cmCurl.cxx:178:26: error: assigning to 'CURL_NETRC_OPTION' from incompatible type 'long'
+  178 |       curl_netrc_level = CURL_NETRC_OPTIONAL;
+#}
+
 {% block fetch %}
 https://github.com/curl/curl/archive/refs/tags/curl-8_12_1.tar.gz
 sha:6edc063d1ebaf9cf3b3b46e9fef2f3cd8932694989ecd43d005d6e828426d09f
+#https://github.com/curl/curl/archive/refs/tags/curl-8_13_0.tar.gz
+#sha:ccc5ba45d9f5320c70ffb24e5411b66ba55ea1f333bf78be0963ed90a9328699
 {% endblock %}
 
 {% block bld_tool %}
