d05b5673a5219876eeeaa83732f0ac406a0c427e
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-07 14:01:02 +0300

    up

--- pkgs/lib/protobuf/c/t/ix.sh
+++ pkgs/lib/protobuf/c/t/ix.sh
@@ -1,6 +1,6 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://github.com/protobuf-c/protobuf-c/archive/refs/tags/v1.5.1.tar.gz
-sha:f3046f94ef6f85cf4311f6c4c51e2095c381ce7bb7fd4f90ea8d9cd99fca3c04
+https://github.com/protobuf-c/protobuf-c/archive/refs/tags/v1.5.2.tar.gz
+sha:cea46eeaa19c52924938b582c5d128a6ed3b6fb5b3f4677476a1781cc06e03f3
 {% endblock %}
