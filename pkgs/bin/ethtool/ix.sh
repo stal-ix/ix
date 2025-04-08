995938849b7e5367e54cdf2efd7da1c2541aee1d
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-09 00:24:48 +0300

    up

--- pkgs/bin/ethtool/ix.sh
+++ pkgs/bin/ethtool/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://mirrors.edge.kernel.org/pub/software/network/ethtool/ethtool-6.11.tar.xz
-sha:8d91f5c72ae3f25b7e88d4781279dcb320f71e30058914370b1c574c96b31202
+https://mirrors.edge.kernel.org/pub/software/network/ethtool/ethtool-6.14.tar.xz
+sha:9338bb00e492878d3bbe3cd2894e60db35813634c208db0b20f5c7ee84da69b1
 {% endblock %}
 
 {% block bld_libs %}
