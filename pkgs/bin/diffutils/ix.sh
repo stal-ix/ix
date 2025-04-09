982646ad215c5dbe2de529867da529d9e17c6fd1
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-09 16:07:36 +0300

    up

--- pkgs/bin/diffutils/ix.sh
+++ pkgs/bin/diffutils/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
-https://ftp.gnu.org/gnu/diffutils/diffutils-3.11.tar.xz
-sha:a73ef05fe37dd585f7d87068e4a0639760419f810138bd75c61ddaa1f9e2131e
+https://ftp.gnu.org/gnu/diffutils/diffutils-3.12.tar.xz
+sha:7c8b7f9fc8609141fdea9cece85249d308624391ff61dedaf528fcb337727dfd
 {% endblock %}
 
 {% block bld_libs %}
