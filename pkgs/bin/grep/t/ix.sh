0771d58c02444b81236f04cafe7f95cd32d54d3c
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-11 22:12:32 +0300

    up

--- pkgs/bin/grep/t/ix.sh
+++ pkgs/bin/grep/t/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
-https://ftp.gnu.org/gnu/grep/grep-3.11.tar.xz
-sha:1db2aedde89d0dea42b16d9528f894c8d15dae4e190b59aecc78f5a951276eab
+https://ftp.gnu.org/gnu/grep/grep-3.12.tar.xz
+sha:2649b27c0e90e632eadcd757be06c6e9a4f48d941de51e7c0f83ff76408a07b9
 {% endblock %}
 
 {% block bld_libs %}
