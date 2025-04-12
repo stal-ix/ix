321743f290a083312a8ef13b96b68c0126fcdb97
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-12 19:59:31 +0300

    up

--- pkgs/bin/hypr/picker/ix.sh
+++ pkgs/bin/hypr/picker/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//bin/hypr/t/ix.sh' %}
 
 {% block fetch %}
-https://github.com/hyprwm/hyprpicker/archive/refs/tags/v0.4.3.tar.gz
-sha:bd6a4a0bb264905570a08cef2c4a37170df89bc64fdd0a359440b55f97e84a5b
+https://github.com/hyprwm/hyprpicker/archive/refs/tags/v0.4.4.tar.gz
+sha:90e9dce28176d5998bf3afeeb30382317e206a166729d980499974c0d756ec31
 {% endblock %}
 
 {% block bld_libs %}
