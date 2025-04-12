5fe44485e143bd66144ef1e75969b88b67eb70c7
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-12 20:12:02 +0300

    up

--- pkgs/bin/hugo/ix.sh
+++ pkgs/bin/hugo/ix.sh
@@ -1,11 +1,11 @@
 {% extends '//die/go/build.sh' %}
 
 {% block go_url %}
-https://github.com/gohugoio/hugo/archive/refs/tags/v0.145.0.tar.gz
+https://github.com/gohugoio/hugo/archive/refs/tags/v0.146.2.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-e27b3e243580537e2f6b0644d6785ab8d25c74634f2e01ac7a807f0eb92d97bf
+8d6fd93efe167d2d2df04e44fdbd0345b47b8dfecc0e13f3db58a568c84b80fb
 {% endblock %}
 
 {% block go_tool %}
