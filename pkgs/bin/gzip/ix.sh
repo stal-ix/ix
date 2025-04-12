65aa3770ee44af4fb877171cfcf04c46bfe1c821
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-12 11:15:39 +0300

    up

--- pkgs/bin/gzip/ix.sh
+++ pkgs/bin/gzip/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/c/autorehell.sh' %}
 
 {% block fetch %}
-https://ftp.gnu.org/gnu/gzip/gzip-1.13.tar.xz
-sha:7454eb6935db17c6655576c2e1b0fabefd38b4d0936e0f87f48cd062ce91a057
+https://ftp.gnu.org/gnu/gzip/gzip-1.14.tar.xz
+sha:01a7b881bd220bfdf615f97b8718f80bdfd3f6add385b993dcf6efd14e8c0ac6
 {% endblock %}
 
 {% block bld_libs %}
