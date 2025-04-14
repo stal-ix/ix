9050ec51b0464729e11c18e997791e53649290cb
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-15 01:46:16 +0300

    up

---
+++ pkgs/lib/cairo/mm/18/ix.sh
@@ -0,0 +1,21 @@
+{% extends '//die/c/meson.sh' %}
+
+{% block pkg_name %}
+cairomm
+{% endblock %}
+
+{% block version %}
+1.18.0
+{% endblock %}
+
+{% block fetch %}
+https://www.cairographics.org/releases/cairomm-{{self.version().strip()}}.tar.xz
+sha:b81255394e3ea8e8aa887276d22afa8985fc8daef60692eb2407d23049f03cfb
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+lib/c++
+lib/cairo
+lib/sigc++/3
+{% endblock %}
