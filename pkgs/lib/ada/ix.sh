f8fd8a5d1b3b0eac45d000e0aba2fe64698d1457
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-14 23:39:39 +0300

    up

--- pkgs/lib/ada/ix.sh
+++ pkgs/lib/ada/ix.sh
@@ -5,12 +5,12 @@ ada
 {% endblock %}
 
 {% block version %}
-2.9.0
+3.2.2
 {% endblock %}
 
 {% block fetch %}
 https://github.com/ada-url/ada/archive/refs/tags/v{{self.version().strip()}}.tar.gz
-sha:8b992f0ce9134cb4eafb74b164d2ce2cb3af1900902162713b0e0c5ab0b6acd8
+sha:2eb3d3d7bd2e0c74785f873fc98cf56556294ac76532ef69a01605329b629162
 {% endblock %}
 
 {% block lib_deps %}
