9158ef00dbb3b2aeb9cb9b6ccd7f1ed4eebeafc4
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-15 21:04:02 +0300

    up

--- pkgs/bin/elementary/files/unwrap/ix.sh
+++ pkgs/bin/elementary/files/unwrap/ix.sh
@@ -5,12 +5,12 @@ files
 {% endblock %}
 
 {% block version %}
-7.1.0
+7.1.1
 {% endblock %}
 
 {% block fetch %}
 https://github.com/elementary/files/archive/refs/tags/{{self.version().strip()}}.tar.gz
-sha:d26ca8e95a61052ab3d5ac65abd355209458dba4b94b09108718ce1bbc58d701
+sha:7d93a4551b9110e9a29a85d1aa40fadd7a104762f6aad8419c95d1e65e2f7b7a
 {% endblock %}
 
 {% block bld_libs %}
