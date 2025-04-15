9c1bba6abe0e48ff7fa926bcbd751869b158a5df
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-15 17:57:13 +0300

    up

--- pkgs/bin/s3/fs/fuse/unwrap/ix.sh
+++ pkgs/bin/s3/fs/fuse/unwrap/ix.sh
@@ -5,12 +5,12 @@ s3fs-fuse
 {% endblock %}
 
 {% block version %}
-1.94
+1.95
 {% endblock %}
 
 {% block fetch %}
 https://github.com/s3fs-fuse/s3fs-fuse/archive/refs/tags/v{{self.version().strip()}}.tar.gz
-sha:46eb3bcc16eff63008ae2c3177765264b88627482bdb978fc3d10e34e9d52284
+sha:0c97b8922f005500d36f72aee29a1345c94191f61d795e2a7b79fb7e3e6f5517
 {% endblock %}
 
 {% block bld_libs %}
