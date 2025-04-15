b6e4fe3465e1c0c7cd6b6364cfce27b362008556
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-15 16:21:54 +0300

    up

--- pkgs/bin/dialog/ix.sh
+++ pkgs/bin/dialog/ix.sh
@@ -5,12 +5,12 @@ dialog
 {% endblock %}
 
 {% block version %}
-1.3-20240101
+1.3-20250116
 {% endblock %}
 
 {% block fetch %}
 https://invisible-island.net/archives/dialog/dialog-{{self.version().strip()}}.tgz
-sha:9419eb52b95837312a76ccb26002c5d624fab53abde0859f1c7364179dc0ebad
+sha:68406329827b783d0a8959cc20a94c6e1791ac861a27f854e06e9020541816dd
 {% endblock %}
 
 {% block bld_libs %}
