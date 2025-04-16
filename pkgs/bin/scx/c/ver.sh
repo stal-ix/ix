26d7d072a29b26d91a8ea17cca7af2799b1821ec
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-16 21:14:49 +0300

    ver

--- pkgs/bin/scx/c/ver.sh
+++ pkgs/bin/scx/c/ver.sh
@@ -1,1 +1,11 @@
-https://github.com/sched-ext/scx/archive/refs/tags/v1.0.8.tar.gz
+{% block version %}
+1.0.8
+{% endblock %}
+
+{% block pkg_name %}
+sched-ext
+{% endblock %}
+
+{% block source_url %}
+https://github.com/sched-ext/scx/archive/refs/tags/v{{self.version().strip()}}.tar.gz
+{% endblock %}
