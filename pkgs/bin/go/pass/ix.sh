fcc26021a83f8a14ff91b97eb6106410ae4d2453
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-16 16:52:36 +0300

    up

--- pkgs/bin/go/pass/ix.sh
+++ pkgs/bin/go/pass/ix.sh
@@ -5,7 +5,7 @@ gopass
 {% endblock %}
 
 {% block version %}
-1.14.3
+1.15.15
 {% endblock %}
 
 {% block go_url %}
@@ -13,10 +13,14 @@ https://github.com/gopasspw/gopass/archive/refs/tags/v{{self.version().strip()}}
 {% endblock %}
 
 {% block go_sha %}
-e82a085324332eac34e51c9e1eb115c4f8728b3144f137900935f5f72991c73a
+2ca97752e31699ef374c86cff1efdf4584a1b00ed837f8b342524cd42ecdd834
 {% endblock %}
 
 {% block install %}
 mkdir ${out}/bin
 cp gopass ${out}/bin/
+{% endblock %}
+
+{% block go_tool %}
+bin/go/lang/24
 {% endblock %}
