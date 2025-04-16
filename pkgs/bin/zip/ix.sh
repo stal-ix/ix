9104c788e504025cd45ba2b49ec1949c148c8256
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-16 21:09:28 +0300

    ver

--- pkgs/bin/zip/ix.sh
+++ pkgs/bin/zip/ix.sh
@@ -1,5 +1,13 @@
 {% extends '//die/c/make.sh' %}
 
+{% block version %}
+3.0
+{% endblock %}
+
+{% block pkg_name %}
+zip
+{% endblock %}
+
 {% block fetch %}
 https://src.fedoraproject.org/repo/pkgs/zip/zip30.tar.gz/7b74551e63f8ee6aab6fbc86676c0d37/zip30.tar.gz
 sha:f0e8bb1f9b7eb0b01285495a2699df3a4b766784c1765a8f1aeedf63c0806369
