2d3c9734ef15c7e31e36ab9108adc7a2cea1b92e
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-16 16:40:58 +0300

    up

--- pkgs/bin/xdg/file/utils/ix.sh
+++ pkgs/bin/xdg/file/utils/ix.sh
@@ -5,12 +5,12 @@ desktop-file-utils
 {% endblock %}
 
 {% block version %}
-0.27
+0.28
 {% endblock %}
 
 {% block fetch %}
 https://gitlab.freedesktop.org/xdg/desktop-file-utils/-/archive/{{self.version().strip()}}/desktop-file-utils-{{self.version().strip()}}.tar.bz2
-sha:55cb1da944b519ad7eae221097ff4e85edd10ffcc35990f0e88d709e034483e8
+sha:cf81ab80b672a54b3858cb1b28d6ac02b84a3085151cbed92c6a2457643304fd
 {% endblock %}
 
 {% block bld_libs %}
