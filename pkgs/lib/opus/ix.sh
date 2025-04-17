8c146f6a61f12adc9578e2ef5b0854ece87799f6
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-17 18:52:43 +0300

    up

--- pkgs/lib/opus/ix.sh
+++ pkgs/lib/opus/ix.sh
@@ -5,12 +5,12 @@ opus
 {% endblock %}
 
 {% block version %}
-1.5.1
+1.5.2
 {% endblock %}
 
 {% block fetch %}
 https://github.com/xiph/opus/archive/refs/tags/v{{self.version().strip()}}.tar.gz
-sha:7ce44ef3d335a3268f26be7d53bb3bed7205b34eaf80bf92a99e69d490afe9d9
+sha:9480e329e989f70d69886ded470c7f8cfe6c0667cc4196d4837ac9e668fb7404
 {% endblock %}
 
 {% block lib_deps %}
