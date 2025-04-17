7053dcafe60f4ee1544f758d4df55296dd10c497
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-17 18:19:55 +0300

    up

--- pkgs/lib/ng/http/3/t/ix.sh
+++ pkgs/lib/ng/http/3/t/ix.sh
@@ -5,12 +5,12 @@ nghttp3
 {% endblock %}
 
 {% block version %}
-1.5.0
+1.9.0
 {% endblock %}
 
 {% block fetch %}
 https://github.com/ngtcp2/nghttp3/releases/download/v{{self.version().strip()}}/nghttp3-{{self.version().strip()}}.tar.xz
-sha:8c00e3910ea2ad1218dafebcf8dd2ffdf030c992d9ceb65834d29e5e5278dd0d
+sha:b1b531cf6ae5c9366abea2d3319e4ac932fd64c072c8aafd69cf6295733ce12a
 {% endblock %}
 
 {% block lib_deps %}
