9a9ade3b2e8d8c570833426d96484d1bc6deeac3
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-17 18:29:59 +0300

    fixes

--- pkgs/lib/gdbm/full/t/ix.sh
+++ pkgs/lib/gdbm/full/t/ix.sh
@@ -17,8 +17,6 @@ gdbm
 {% block fetch %}
 https://ftp.gnu.org/gnu/gdbm/gdbm-{{self.version().strip()}}.tar.gz
 sha:695e9827fdf763513f133910bc7e6cfdb9187943a4fec943e57449723d2b8dbf
-#https://ftp.gnu.org/gnu/gdbm/gdbm-1.25.tar.gz
-#sha:d02db3c5926ed877f8817b81cd1f92f53ef74ca8c6db543fbba0271b34f393ec
 {% endblock %}
 
 {% block lib_deps %}
