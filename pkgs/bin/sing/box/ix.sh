f99a1aa22e908de209282d8dc0a31c939438511d
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-18 13:59:20 +0300

    up

--- pkgs/bin/sing/box/ix.sh
+++ pkgs/bin/sing/box/ix.sh
@@ -5,7 +5,7 @@ sing-box
 {% endblock %}
 
 {% block version %}
-1.10.2
+1.11.7
 {% endblock %}
 
 {% block go_url %}
@@ -13,7 +13,7 @@ https://github.com/SagerNet/sing-box/archive/refs/tags/v{{self.version().strip()
 {% endblock %}
 
 {% block go_sha %}
-7d24d35d8c2f5ad3567d520d259a830592598ee270c5272d70332187ab323218
+598cc3b579cbc8cdecfc961205fa93043264d6f7a87bbfac703aaed9a3386a1f
 {% endblock %}
 
 {% block step_unpack %}
@@ -24,4 +24,9 @@ cd cmd/sing-box
 {% block install %}
 mkdir ${out}/bin
 cp sing-box ${out}/bin/
+{% endblock %}
+
+
+{% block go_tool %}
+bin/go/lang/24
 {% endblock %}
