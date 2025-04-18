066cbbff29fcae36edbd52517aa881325fae149d
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-18 22:27:23 +0300

    up

--- pkgs/lib/spelling/ix.sh
+++ pkgs/lib/spelling/ix.sh
@@ -5,12 +5,12 @@ libspelling
 {% endblock %}
 
 {% block version %}
-0.4.7
+0.4.8
 {% endblock %}
 
 {% block fetch %}
 https://gitlab.gnome.org/GNOME/libspelling/-/archive/{{self.version().strip()}}/libspelling-{{self.version().strip()}}.tar.bz2
-sha:96fc2b1ae447536e00a6231541bb177a08fda6f30fdfeae68a0622040d82d827
+sha:dadd7bfc58511b85f966570b6f241ae1c695986746daa634de4a2d856886ae18
 {% endblock %}
 
 {% block lib_deps %}
