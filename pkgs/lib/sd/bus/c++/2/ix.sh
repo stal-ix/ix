c8d9512a8f5ef565cd5a0017441e66c91db84e7d
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-18 22:16:36 +0300

    better

--- pkgs/lib/sd/bus/c++/2/ix.sh
+++ pkgs/lib/sd/bus/c++/2/ix.sh
@@ -1,14 +1,10 @@
-{% extends '//lib/sd/bus/c++/ix.sh' %}
-
-{% block pkg_name %}
-sdbus-cpp
-{% endblock %}
+{% extends '//lib/sd/bus/c++/t/t/ix.sh' %}
 
 {% block version %}
 2.0.0
 {% endblock %}
 
 {% block fetch %}
-https://github.com/Kistler-Group/sdbus-cpp/archive/refs/tags/v{{self.version().strip()}}.tar.gz
+{{super()}}
 sha:88af4569161a0d0192f0f4a94582a1af4e75722499d06984fb7f91f638f5afb3
 {% endblock %}
