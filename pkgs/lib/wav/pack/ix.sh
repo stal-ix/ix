9d891be1604d30f2a17aac0da770a630636b7c73
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-19 10:12:42 +0300

    up

--- pkgs/lib/wav/pack/ix.sh
+++ pkgs/lib/wav/pack/ix.sh
@@ -5,13 +5,13 @@ WavPack
 {% endblock %}
 
 {% block version %}
-5.6.0
+5.8.1
 {% endblock %}
 
 {% block fetch %}
 https://github.com/dbry/WavPack/releases/download/{{self.version().strip()}}/wavpack-{{self.version().strip()}}.tar.xz
-sha:af8035f457509c3d338b895875228a9b81de276c88c79bb2d3e31d9b605da9a9
-#https://github.com/dbry/WavPack/archive/refs/tags/5.6.0.tar.gz
+sha:7322775498602c8850afcfc1ae38f99df4cbcd51386e873d6b0f8047e55c0c26
+#https://github.com/dbry/WavPack/archive/refs/tags/5.8.1.tar.gz
 #sha:44043e8ffe415548d5723e9f4fc6bda5e1f429189491c5fb3df08b8dcf28df72
 {% endblock %}
 
