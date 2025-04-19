1a6492bd12c31881e38acfd4bf3e5297c58c015b
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-19 10:57:10 +0300

    up

--- pkgs/lib/z/ng/t/ix.sh
+++ pkgs/lib/z/ng/t/ix.sh
@@ -5,12 +5,12 @@ zlib-ng
 {% endblock %}
 
 {% block version %}
-2.2.2
+2.2.4
 {% endblock %}
 
 {% block fetch %}
 https://github.com/zlib-ng/zlib-ng/archive/refs/tags/{{self.version().strip()}}.tar.gz
-sha:fcb41dd59a3f17002aeb1bb21f04696c9b721404890bb945c5ab39d2cb69654c
+sha:a73343c3093e5cdc50d9377997c3815b878fd110bf6511c2c7759f2afb90f5a3
 {% endblock %}
 
 {% block lib_deps %}
