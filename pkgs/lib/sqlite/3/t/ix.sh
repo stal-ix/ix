0cb733d261ab420c10919ef6fb43d540551c0f8a
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-19 08:07:03 +0300

    up

--- pkgs/lib/sqlite/3/t/ix.sh
+++ pkgs/lib/sqlite/3/t/ix.sh
@@ -1,24 +1,28 @@
-{% extends '//die/c/autorehell.sh' %}
+{% extends '//die/c/configure.sh' %}
 
 {% block pkg_name %}
 sqlite
 {% endblock %}
 
 {% block version %}
-3470200
+3.49.1
+{% endblock %}
+
+{% block sqlite_ver %}
+{{self.version() | strip | field(0)}}{{self.version() | strip | field(1) | pad(3)}}{{self.version() | strip | field(2) | pad(3)}}
 {% endblock %}
 
 {% block fetch %}
-https://www.sqlite.org/2024/sqlite-autoconf-{{self.version().strip()}}.tar.gz
-sha:f1b2ee412c28d7472bc95ba996368d6f0cdcf00362affdadb27ed286c179540b
+https://www.sqlite.org/2025/sqlite-autoconf-{{self.sqlite_ver().strip()}}.tar.gz
+sha:106642d8ccb36c5f7323b64e4152e9b719f7c0215acf5bfeac3d5e7f97b59254
 {% endblock %}
 
 {% block lib_deps %}
 lib/c
 {% endblock %}
 
-{% block configure_flags %}
---enable-dynamic-extensions=no
+{% block build_flags %}
+wrap_cc
 {% endblock %}
 
 {% block cpp_defines %}
