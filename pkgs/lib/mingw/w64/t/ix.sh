0a4c4aeb9a616dfc3281816c43a813816a7ebca3
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-21 17:34:42 +0300

    up

--- pkgs/lib/mingw/w64/t/ix.sh
+++ pkgs/lib/mingw/w64/t/ix.sh
@@ -5,10 +5,10 @@ mingw-w64
 {% endblock %}
 
 {% block version %}
-11.0.1
+12.0.0
 {% endblock %}
 
 {% block fetch %}
 https://downloads.sourceforge.net/project/mingw-w64/mingw-w64/mingw-w64-release/mingw-w64-v{{self.version().strip()}}.tar.bz2
-sha:3f66bce069ee8bed7439a1a13da7cb91a5e67ea6170f21317ac7f5794625ee10
+sha:cc41898aac4b6e8dd5cffd7331b9d9515b912df4420a3a612b5ea2955bbeed2f
 {% endblock %}
