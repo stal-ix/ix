955127ce95b5d5aa34345983544a671ab34a4ecd
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-21 17:25:39 +0300

    up

--- pkgs/bin/mawk/t/ix.sh
+++ pkgs/bin/mawk/t/ix.sh
@@ -5,10 +5,10 @@ mawk
 {% endblock %}
 
 {% block version %}
-1.3.4-20230203
+1.3.4.20250131
 {% endblock %}
 
 {% block fetch %}
-https://invisible-mirror.net/archives/mawk/mawk-{{self.version().strip()}}.tgz
-sha:6db7a32ac79c51107ad31a407d4f92c6b842dde2f68a7533b4e7b7b03e8900be
+https://invisible-mirror.net/archives/mawk/mawk-{{self.version().strip()[:5]}}-{{self.version() | strip | field(3)}}.tgz
+sha:51bcb82d577b141d896d9d9c3077d7aaa209490132e9f2b9573ba8511b3835be
 {% endblock %}
