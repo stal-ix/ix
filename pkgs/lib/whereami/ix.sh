dca49e9db55e2b366c80b0bd6845aff4784fa743
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-21 21:11:40 +0300

    ver

--- pkgs/lib/whereami/ix.sh
+++ pkgs/lib/whereami/ix.sh
@@ -1,7 +1,15 @@
 {% extends '//die/c/ix.sh' %}
 
+{% block pkg_name %}
+whereami
+{% endblock %}
+
+{% block version %}
+ba364cd54fd431c76c045393b6522b4bff547f50
+{% endblock %}
+
 {% block fetch %}
-https://github.com/gpakosz/whereami/archive/ba364cd54fd431c76c045393b6522b4bff547f50.tar.gz
+https://github.com/gpakosz/whereami/archive/{{self.version().strip()}}.tar.gz
 sha:1d8744177f37e8386ec2f6c5992592399040cb93535ed4fd253e1976f889a744
 {% endblock %}
 
