a9320157b0d20969ba0b8d02b07cf313fb2d3e2c
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-21 22:25:48 +0300

    ver

--- pkgs/lib/natpmp/ix.sh
+++ pkgs/lib/natpmp/ix.sh
@@ -1,7 +1,15 @@
 {% extends '//die/c/make.sh' %}
 
+{% block pkg_name %}
+libnatpmp
+{% endblock %}
+
+{% block version %}
+20150609
+{% endblock %}
+
 {% block fetch %}
-https://launchpad.net/ubuntu/+archive/primary/+sourcefiles/libnatpmp/20150609-7.1build2/libnatpmp_20150609.orig.tar.gz
+https://launchpad.net/ubuntu/+archive/primary/+sourcefiles/libnatpmp/{{self.version().strip()}}-7.1build2/libnatpmp_{{self.version().strip()}}.orig.tar.gz
 sha:e1aa9c4c4219bc06943d6b2130f664daee213fb262fcb94dd355815b8f4536b0
 {% endblock %}
 
