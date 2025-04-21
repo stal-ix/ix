f78a99f8084381e5f56d7b55e9e4bb78c6c95596
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-21 04:04:32 +0300

    up

--- pkgs/bin/gperf/ix.sh
+++ pkgs/bin/gperf/ix.sh
@@ -5,12 +5,12 @@ gperf
 {% endblock %}
 
 {% block version %}
-3.2.1
+3.3
 {% endblock %}
 
 {% block fetch %}
 http://ftp.gnu.org/pub/gnu/gperf/gperf-{{self.version().strip()}}.tar.gz
-sha:ed5ad317858e0a9badbbada70df40194002e16e8834ac24491307c88f96f9702
+sha:fd87e0aba7e43ae054837afd6cd4db03a3f2693deb3619085e6ed9d8d9604ad8
 {% endblock %}
 
 {% block bld_libs %}
