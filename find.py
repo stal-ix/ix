1ad19f5a52ce7df59227cac2d9979ea828ffb6a8
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-21 21:10:46 +0300

    ver

---
+++ find.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import os
+
+for a, b, c in os.walk('.'):
+    for x in c:
+        pp = os.path.join(a, x)
+
+        if '.sh' in pp:
+            with open(pp) as f:
+                data = f.read()
+
+            if 'http' in data and 'version' not in data and 'git_commit' not in data:
+                print(pp)
