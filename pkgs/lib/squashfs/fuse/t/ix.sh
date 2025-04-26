b84e8d571fee6d32fcac809f20aa26d84a41daa3
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-26 19:13:12 +0300

    up 0.6.0 0.6.1 bin/squashfs/fuse lib/squashfs/fuse/patched

--- pkgs/lib/squashfs/fuse/t/ix.sh
+++ pkgs/lib/squashfs/fuse/t/ix.sh
@@ -5,12 +5,12 @@ squashfuse
 {% endblock %}
 
 {% block version %}
-0.6.0
+0.6.1
 {% endblock %}
 
 {% block fetch %}
 https://github.com/vasi/squashfuse/archive/refs/tags/{{self.version().strip()}}.tar.gz
-sha:fee364242fb4eea72e082ec674b6e6661856408a06719e1e88ce8673a22dc78a
+sha:7730066d1e9baf0084c71674d168331296921e0d7ae0f34de7307744be4ed568
 {% endblock %}
 
 {% block lib_deps %}
