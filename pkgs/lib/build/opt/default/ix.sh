3ca554ecafdc16848bf9bc811a3c61ef26df33c8
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-29 23:23:46 +0300

    better

--- pkgs/lib/build/opt/default/ix.sh
+++ pkgs/lib/build/opt/default/ix.sh
@@ -1,9 +1,13 @@
 {% extends '//die/hub.sh' %}
 
 {% block lib_deps %}
-lib/build/opt/gc
+{% if boot %}
 lib/build/opt/O2
+{% else %}
+lib/build/opt/O2
+lib/build/opt/gc
 lib/build/opt/ICF
 lib/build/opt/safe
 lib/build/opt/sections
+{% endif %}
 {% endblock %}
