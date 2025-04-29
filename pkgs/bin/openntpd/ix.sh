39821e0f085108e04c73d6fc97e6581d4f97b3f1
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-29 09:39:11 +0300

    ver

--- pkgs/bin/openntpd/ix.sh
+++ pkgs/bin/openntpd/ix.sh
@@ -1,7 +1,15 @@
 {% extends '//die/c/autohell.sh' %}
 
+{% block pkg_name %}
+OpenNTPD
+{% endblock %}
+
+{% block version %}
+6.8p1
+{% endblock %}
+
 {% block fetch %}
-https://cdn.openbsd.org/pub/OpenBSD/OpenNTPD/openntpd-6.8p1.tar.gz
+https://cdn.openbsd.org/pub/OpenBSD/OpenNTPD/openntpd-{{self.version().strip()}}.tar.gz
 sha:8582db838a399153d4a17f2a76518b638cc3020f58028575bf54127518f55a46
 {% endblock %}
 
