cf9e808a8b565cda062f1fd9570a645d7dd709e6
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-29 09:27:03 +0300

    up

--- pkgs/bin/tail/spin/ix.sh
+++ pkgs/bin/tail/spin/ix.sh
@@ -5,7 +5,7 @@ tailspin
 {% endblock %}
 
 {% block version %}
-3.0.0
+5.4.2
 {% endblock %}
 
 {% block cargo_url %}
@@ -13,10 +13,17 @@ https://github.com/bensadeh/tailspin/archive/refs/tags/{{self.version().strip()}
 {% endblock %}
 
 {% block cargo_sha %}
-89581650da222c69bb0746f94c820c696402f7680f09b812e9c2007504108212
+fa6327a86e652ff6d8d1019c6fe7f03f12f9c30f005f46e2039ffc102298a045
 {% endblock %}
 
-{% block install %}
-mkdir ${out}/bin
-cp ${tmp}/out/tspin ${out}/bin/
+{% block cargo_bins %}
+tspin
+{% endblock %}
+
+{% block cargoc_ver %}
+bld/cargo/86
+{% endblock %}
+
+{% block rustc_ver %}
+86
 {% endblock %}
