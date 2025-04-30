6eaca53674eed4d32579772940139525abe8e0f4
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-04-30 16:14:22 +0300

    fix

--- pkgs/lib/build/opt/default/ix.sh
+++ pkgs/lib/build/opt/default/ix.sh
@@ -3,14 +3,15 @@
 {% block lib_deps %}
 {% if boot %}
 lib/build/opt/O2
-{% else %}
+{% elif linux %}
 lib/build/opt/O2
 lib/build/opt/gc
 lib/build/opt/ICF
 lib/build/opt/safe
 lib/build/opt/sections
-{% if linux %}
 lib/build/noexec
-{% endif %}
+{% else %}
+lib/build/opt/O2
+lib/build/opt/safe
 {% endif %}
 {% endblock %}
