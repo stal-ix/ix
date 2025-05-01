5c15120e3715112b257e134f8fd0d41eb533b722
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-01 18:03:59 +0300

    up 7.6 7.7 bin/loksh

--- pkgs/bin/loksh/ix.sh
+++ pkgs/bin/loksh/ix.sh
@@ -5,7 +5,7 @@ loksh
 {% endblock %}
 
 {% block version %}
-7.6
+7.7
 {% endblock %}
 
 {% block git_repo %}
@@ -17,7 +17,7 @@ https://github.com/dimkr/loksh
 {% endblock %}
 
 {% block git_sha %}
-b002d6c7dea0d0547b36bc5bd91c2b804438531d2c35ba222b2e2ce226927ac5
+9eb736a33947d829b8ae67868063bfac54e3527643cde430f6020c610545249d
 {% endblock %}
 
 {% block lib_deps %}
