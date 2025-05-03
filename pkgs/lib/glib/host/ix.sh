cabb99d747aecebcb222e50f98ff8be88a653eca
Andrew Grigorev <andrew@ei-grad.ru>
2025-05-03 23:41:59 +0300

    fix

--- pkgs/lib/glib/host/ix.sh
+++ pkgs/lib/glib/host/ix.sh
@@ -1,4 +1,11 @@
 {% extends '//lib/glib/t/ix.sh' %}
 
+{% block meson_flags %}
+libelf=disabled
+sysprof=disabled
+default_library=static
+libmount=disabled
+{% endblock %}
+
 {% block strip_pc %}
 {% endblock %}
