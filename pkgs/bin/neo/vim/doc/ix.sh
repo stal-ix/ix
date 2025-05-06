ad80f9f5bae1189c518ca9851d0f21fd3c547a98
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-06 19:38:24 +0300

    up

--- pkgs/bin/neo/vim/doc/ix.sh
+++ pkgs/bin/neo/vim/doc/ix.sh
@@ -1,16 +1,18 @@
 {% extends '//die/c/make.sh' %}
 
+# check bin/neo/vim
+
 {% block pkg_name %}
 tree-sitter-vimdoc
 {% endblock %}
 
 {% block version %}
-3.0.0
+3.0.1
 {% endblock %}
 
 {% block fetch %}
 https://github.com/neovim/tree-sitter-vimdoc/archive/refs/tags/v{{self.version().strip()}}.tar.gz
-sha:a639bf92bf57bfa1cdc90ca16af27bfaf26a9779064776dd4be34c1ef1453f6c
+sha:76b65e5bee9ff78eb21256619b1995aac4d80f252c19e1c710a4839481ded09e
 {% endblock %}
 
 {% block bld_libs %}
