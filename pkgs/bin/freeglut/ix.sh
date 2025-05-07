44ff272fe321ac4dac3b5660ae39df4b4cd9c6df
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-08 02:20:23 +0300

    up

--- pkgs/bin/freeglut/ix.sh
+++ pkgs/bin/freeglut/ix.sh
@@ -2,11 +2,18 @@
 
 {% block bld_libs %}
 lib/c++
+lib/glu
 lib/drivers/3d
+lib/build/muldefs
 {{super()}}
 {% endblock %}
 
 {% block cmake_flags %}
 FREEGLUT_BUILD_DEMOS=ON
 {{super()}}
+{% endblock %}
+
+{% block cpp_missing %}
+{{super()}}
+GL/glu.h
 {% endblock %}
