7c03ced1f1082aebbba41866d0c3aa5e474c62a8
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-08 00:27:51 +0300

    up 1.24.2 1.24.3 bin/go/lang/24

--- pkgs/bin/go/lang/24/ix.sh
+++ pkgs/bin/go/lang/24/ix.sh
@@ -1,12 +1,12 @@
 {% extends '//bin/go/lang/22/ix.sh' %}
 
 {% block version %}
-1.24.2
+1.24.3
 {% endblock %}
 
 {% block fetch %}
 https://github.com/golang/go/archive/refs/tags/go{{self.version().strip()}}.tar.gz
-sha:aed36493f20f6701b2832ce12b8d531ab36785da2ffb68553badbcdf8212b116
+sha:4358c465e80e2124bee1969dd5e8f84d4f445a2c5a58b4b33f17bfe9e9deefed
 {% endblock %}
 
 {% block go_bootstrap %}
