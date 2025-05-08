f9fa6236038a6a4d7d25376243df78205267a72b
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-09 00:30:42 +0300

    better

--- pkgs/bin/ix/tools/regen/scripts/repo_export.sh
+++ pkgs/bin/ix/tools/regen/scripts/repo_export.sh
@@ -2,4 +2,13 @@
 
 set -xue
 
-IX_EXEC_KIND=fake IX_DUMP_REPO=1 ./ix build set/ci --seed=1 | ix_repo | sort | uniq | ix_flt | sort | uniq | grep -v nlua0 | grep -v "bin/xxd" > pkgs/die/scripts/dump.json
+IX_EXEC_KIND=fake IX_DUMP_REPO=1 ./ix build set/ci --seed=1 \
+    | ix_repo \
+    | sort \
+    | uniq \
+    | ix_flt \
+    | sort \
+    | uniq \
+    | grep -v nlua0 \
+    | grep -v "bin/xxd" \
+    > pkgs/die/scripts/dump.json
