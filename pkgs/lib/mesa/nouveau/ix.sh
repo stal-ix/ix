e0b5c0133a8362ead6ecb7ceca752e2ea603ad54
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-09 16:32:39 +0300

    better

--- pkgs/lib/mesa/nouveau/ix.sh
+++
@@ -1,7 +0,0 @@
-{% extends '//die/hub.sh' %}
-
-{% block lib_deps %}
-lib/mesa/valve(opengl=nouveau)
-lib/mesa/opengl(driver=nouveau)
-#lib/mesa/vulkan(driver=lvp)
-{% endblock %}
---
+++ pkgs/lib/mesa/nvk/ix.sh
@@ -0,0 +1,10 @@
+{% extends '//die/hub.sh' %}
+
+{# NVIDIA nvk + zink opengl #}
+
+{% block lib_deps %}
+lib/vulkan/loader/dl
+lib/mesa/vulkan(driver=nvk)
+lib/mesa/opengl(driver=zink)
+lib/mesa/valve(vulkan=nvk,opengl=zink)
+{% endblock %}
