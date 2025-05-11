42959224bd593e43b85caaf8653f4c45bbe4b24e
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-11 18:54:13 +0300

    up 2.5.4 2.5.6 bin/gnupg

--- pkgs/bin/gnupg/ix.sh
+++ pkgs/bin/gnupg/ix.sh
@@ -5,10 +5,10 @@ gnupg
 {% endblock %}
 
 {% block version %}
-2.5.4
+2.5.6
 {% endblock %}
 
 {% block fetch %}
 https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-{{self.version().strip()}}.tar.bz2
-sha:f8cb2ac6c9f17f81e3541c62158764910fad5c3a0765e00b398c41a32bc4e0ea
+sha:377f9d79af0ce494c0946dbe7c92197425bb522d7edd6f54acbc9869695131a8
 {% endblock %}
