0e6fdb357ac3280267f2deb8a6d5aaf5ff2fbaf6
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-13 01:50:53 +0300

    up

--- pkgs/bin/sfwbar/unwrap/ix.sh
+++ pkgs/bin/sfwbar/unwrap/ix.sh
@@ -1,7 +1,7 @@
 {% extends '//die/c/meson.sh' %}
 
 {% block version %}
-1.0_beta13
+1.0_beta16
 {% endblock %}
 
 {% block pkg_name %}
@@ -10,7 +10,7 @@ sfwbar
 
 {% block fetch %}
 https://github.com/LBCrion/sfwbar/archive/refs/tags/v{{self.version().strip()}}.tar.gz
-sha:06abcfbdf1c0d1e547cdf5110a8b58f2997bbc243d350c2a3e02f70120ee0c5a
+sha:238d6394650dfdfc2f36374ec25702bbef57e2cff1d9a149ecb9a52f887c3cdf
 {% endblock %}
 
 {% block bld_libs %}
