db1779d83a20753f4ae9425ee583bc771e6e55cc
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-12 23:29:49 +0300

    up 2.12.0 2.12.1 lib/hwloc

--- pkgs/lib/hwloc/ix.sh
+++ pkgs/lib/hwloc/ix.sh
@@ -5,12 +5,12 @@ hwloc
 {% endblock %}
 
 {% block version %}
-2.12.0
+2.12.1
 {% endblock %}
 
 {% block fetch %}
 https://download.open-mpi.org/release/hwloc/v{{self.version().strip()[:4]}}/hwloc-{{self.version().strip()}}.tar.bz2
-sha:06a0a2bdc0a5714e839164683846a0e936a896213758e9d37e49e232b89c58d4
+sha:38a90328bb86259f9bb2fe1dc57fd841e111d1e6358012bef23dfd95d21dc66b
 {% endblock %}
 
 {% block lib_deps %}
