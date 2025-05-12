020c0022c7eee6c735327537159d296ec612ef2d
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-12 23:53:44 +0300

    add

---
+++ pkgs/lib/soft/float/ix.sh
@@ -0,0 +1,17 @@
+{% extends '//die/c/meson.sh' %}
+
+{% block git_repo %}
+https://github.com/rizinorg/softfloat
+{% endblock %}
+
+{% block git_commit %}
+537d18e71a51aea70f6b54334854f7014c6458c7
+{% endblock %}
+
+{% block git_sha %}
+d7c7d46fb5925235b8e0d04f7b1d3f413ea786aa873a88032d1732140431eaee
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+{% endblock %}
