feceddffaffc16a7994b6a569d5d0ba21f249add
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-14 00:42:07 +0300

    ver

--- pkgs/aux/fonts/nerd/ix.sh
+++ pkgs/aux/fonts/nerd/ix.sh
@@ -1,7 +1,15 @@
 {% extends '//die/fonts.sh' %}
 
+{% block pkg_name %}
+nerd-fonts
+{% endblock %}
+
+{% block version %}
+3.3.0
+{% endblock %}
+
 {% block fetch %}
-https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/NerdFontsSymbolsOnly.zip
+https://github.com/ryanoasis/nerd-fonts/releases/download/v{{self.version().strip()}}/NerdFontsSymbolsOnly.zip
 sha:2078603c1e7a2fc2fa9e625ba1c30264d5d7c39907813d89beaa373f73a3a340
 {% endblock %}
 
