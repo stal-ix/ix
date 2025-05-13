b112163941abe6a286605cba87bc3397b7d83e03
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-13 09:58:28 +0300

    up

--- pkgs/lib/stemmer/ix.sh
+++ pkgs/lib/stemmer/ix.sh
@@ -5,12 +5,12 @@ libstemmer_c
 {% endblock %}
 
 {% block version %}
-2.2.0
+3.0.1
 {% endblock %}
 
 {% block fetch %}
 https://snowballstem.org/dist/libstemmer_c-{{self.version().strip()}}.tar.gz
-sha:b941d9fe9cf36b4e2f8d3873cd4d8b8775bd94867a1df8d8c001bb8b688377c3
+sha:419db89961cf2e30e6417265a4f3c903632d47d6917e7f8c6ae0e4d998743aad
 {% endblock %}
 
 {% block lib_deps %}
