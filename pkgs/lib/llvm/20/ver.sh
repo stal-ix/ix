a9f8ae4ec8a0a290d1c5b6775d8f880e44a0c8bc
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-13 22:04:19 +0300

    up

--- pkgs/lib/llvm/20/ver.sh
+++ pkgs/lib/llvm/20/ver.sh
@@ -1,2 +1,2 @@
-https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-20.1.2.tar.gz
-sha:9ee597456405ddf4809bcf66a4765137a68a85361347ca2a4bb13d9176e932ab
+https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-20.1.4.tar.gz
+sha:65e3a582c4c684fa707a56ff643427bce3633eceaceae3295d81c0e830f44b89
