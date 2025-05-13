5046a57bafbd4449d6024b933c60c3fd6d04db37
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-13 22:35:43 +0300

    ver

--- pkgs/bin/dwl/ix.sh
+++ pkgs/bin/dwl/ix.sh
@@ -4,6 +4,10 @@
 0.7
 {% endblock %}
 
+{% block pkg_name %}
+dwl
+{% endblock %}
+
 {% block fetch %}
 https://codeberg.org/dwl/dwl/archive/v{{self.version().strip()}}.tar.gz
 sha:401a572462fa89f6adf0d0cfe58d909ef334a2c7cd1357db86ec70f32bd4b4d3
