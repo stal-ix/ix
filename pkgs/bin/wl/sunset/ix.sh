38e15c05317747a8d7eb191ca08b297f3bd55860
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-13 22:46:17 +0300

    ver

--- pkgs/bin/wl/sunset/ix.sh
+++ pkgs/bin/wl/sunset/ix.sh
@@ -4,6 +4,10 @@
 0.2.0
 {% endblock %}
 
+{% block pkg_name %}
+wlsunset
+{% endblock %}
+
 {% block fetch %}
 https://git.sr.ht/~kennylevinsen/wlsunset/archive/{{self.version().strip()}}.tar.gz
 sha:88bce695722a09d79545f14d404cc0659f7176bb08d5db5f4f23228b6ba8bb03
