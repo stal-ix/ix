705da4a659c2d22fa54c8434df5f8dfa7a0b33c9
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-13 22:50:19 +0300

    ver

--- pkgs/bin/wmenu/ix.sh
+++ pkgs/bin/wmenu/ix.sh
@@ -4,6 +4,10 @@
 0.1.8
 {% endblock %}
 
+{% block pkg_name %}
+wmenu
+{% endblock %}
+
 {% block fetch %}
 https://git.sr.ht/~adnano/wmenu/archive/{{self.version().strip()}}.tar.gz
 sha:0079d3bbe82e0b02035450b4b31609dd5b822116ea4c14979d2e26d91db3c461
