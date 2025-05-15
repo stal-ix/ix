2a7520c77495d72c2611caac6a3036099c83341f
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-15 16:36:18 +0300

    up 5.0.0 5.0.1 bin/screen

--- pkgs/bin/screen/ix.sh
+++ pkgs/bin/screen/ix.sh
@@ -5,14 +5,14 @@ screen
 {% endblock %}
 
 {% block version %}
-5.0.0
+5.0.1
 {% endblock %}
 
 {% block fetch %}
 #http://ftp.gnu.org/gnu/screen/screen-4.9.1.tar.gz
 #sha:26cef3e3c42571c0d484ad6faf110c5c15091fbf872b06fa7aa4766c7405ac69
 http://ftp.gnu.org/gnu/screen/screen-{{self.version().strip()}}.tar.gz
-sha:f04a39d00a0e5c7c86a55338808903082ad5df4d73df1a2fd3425976aed94971
+sha:bca9b5b9022ca7b8c1a61b503e53ace7dd7cb61eac14e39e7ccbc0b139495d49
 {% endblock %}
 
 {% block bld_libs %}
