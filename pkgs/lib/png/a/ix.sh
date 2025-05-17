6e74552682709c4ecf3564f7694200953accebfb
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-17 12:59:04 +0300

    split packages

--- pkgs/lib/png/a/ix.sh
+++ pkgs/lib/png/a/ix.sh
@@ -1,10 +1,17 @@
 {% extends '//lib/png/stock/ix.sh' %}
 
-# noauto
+{% block pkg_name %}
+libpng-apng
+{% endblock %}
+
+{% block version %}
+1.6.43
+{% endblock %}
 
 {% block fetch %}
-{{super()}}
-https://downloads.sourceforge.net/project/libpng-apng/libpng16/1.6.43/libpng-1.6.43-apng.patch.gz
+https://github.com/glennrp/libpng/archive/refs/tags/v{{self.version().strip()}}.tar.gz
+sha:fecc95b46cf05e8e3fc8a414750e0ba5aad00d89e9fdf175e94ff041caf1a03a
+https://downloads.sourceforge.net/project/libpng-apng/libpng16/{{self.version().strip()}}/libpng-{{self.version().strip()}}-apng.patch.gz
 sha:d107579e90d55386d00e6086ea750942f22a04b9ab476bba0c660770cefafe22
 {% endblock %}
 
