b85badea44f39289f8e77048872c0059211bf394
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-17 12:03:39 +0300

    WIP NVK support

--- pkgs/lib/mesa/nouveau/ix.sh
+++ pkgs/lib/mesa/nouveau/ix.sh
@@ -1,8 +1,23 @@
-{% extends '//lib/mesa/valve/ix.sh' %}
+{% extends '//lib/mesa/llvm/ix.sh' %}
+
+{% block bld_libs %}
+{{super()}}
+lib/clc
+lib/vulkan/spirv/llvm/translator
+{% endblock %}
 
 {% block bld_tool %}
 {{super()}}
 bin/cbindgen
+bin/llvm/spirv
 bin/rust/bindgen
 bld/rust(rustc_ver=86)
+{% endblock %}
+
+{% block step_setup %}
+{{super()}}
+cat << EOF >> ${tmp}/cross.ini
+[binaries]
+rust = 'rustc'
+EOF
 {% endblock %}
