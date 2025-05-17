163dfe9f3170212797dc9f676a4b88fd72696911
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-17 09:25:45 +0300

    better

--- pkgs/lib/firmware/ix.sh
+++ pkgs/lib/firmware/ix.sh
@@ -1,15 +1,7 @@
 {% extends '//die/std/ix.sh' %}
 
-{% block pkg_name %}
-linux-firmware
-{% endblock %}
-
-{% block version %}
-20220209
-{% endblock %}
-
 {% block fetch %}
-https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/snapshot/linux-firmware-{{self.version().strip()}}.tar.gz
+https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/snapshot/linux-firmware-20220209.tar.gz
 sha:1ff936dc3946affa2695f31024b22e117b3faf9aa7b8857aba1d0473cd277897
 https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/snapshot/linux-firmware-20240312.tar.gz
 sha:89cbac35d1bd21ebf64936d764ccd01d4e0b6cde973e3b940f8ad2bac9086ec8
