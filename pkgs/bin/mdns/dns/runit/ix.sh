2fea3bed1532e605be838cd075d707d33820ff14
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-20 05:19:48 +0300

    proper service deps

--- pkgs/bin/mdns/dns/runit/ix.sh
+++ pkgs/bin/mdns/dns/runit/ix.sh
@@ -3,5 +3,5 @@
 {% block run_deps %}
 bin/mdns/dns
 bin/mdns/responder/d
-etc/services/runit(srv_dir=mdnsd_dns,srv_user=nobody,srv_command=exec env GOMAXPROCS=2 mdns2dns 5354)
+etc/services/runit(srv_deps=bin/mdns/dns,srv_dir=mdnsd_dns,srv_user=nobody,srv_command=exec env GOMAXPROCS=2 mdns2dns 5354)
 {% endblock %}
