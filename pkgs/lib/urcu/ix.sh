ecc7022b3995577dfd0103f49d516acc1e6ef27e
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-21 17:45:06 +0300

    up 0.15.2 0.15.3 lib/urcu

--- pkgs/lib/urcu/ix.sh
+++ pkgs/lib/urcu/ix.sh
@@ -5,12 +5,12 @@ userspace-rcu
 {% endblock %}
 
 {% block version %}
-0.15.2
+0.15.3
 {% endblock %}
 
 {% block fetch %}
 https://lttng.org/files/urcu/userspace-rcu-{{self.version().strip()}}.tar.bz2
-sha:59f36f2b8bda1b7620a7eced2634f26c549444818a8313025a3bb09c0766a61d
+sha:26687ec84e3e114759454c884a08abeaf79dec09b041895ddf4c45ec150acb6d
 {% endblock %}
 
 {% block lib_deps %}
