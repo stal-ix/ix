8befbb801098f5519a58ef74188d5168ee6ef5e1
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-21 17:01:10 +0300

    up 2.0.1 3.0.2 pip/markupsafe

--- pkgs/pip/markupsafe/ix.sh
+++ pkgs/pip/markupsafe/ix.sh
@@ -5,12 +5,12 @@ python:MarkupSafe
 {% endblock %}
 
 {% block version %}
-2.0.1
+3.0.2
 {% endblock %}
 
 {% block fetch %}
 https://files.pythonhosted.org/packages/bf/10/ff66fea6d1788c458663a84d88787bae15d45daa16f6b3ef33322a51fc7e/MarkupSafe-{{self.version().strip()}}.tar.gz
-sha:594c67807fb16238b30c44bdf74f36c02cdf22d1c8cda91ef8a0ed8dabf5620a
+sha:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
 {% endblock %}
 
 {% block build %}
