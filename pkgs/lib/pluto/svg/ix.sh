daf06866162917045ca32334b2881b0a64a022ff
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-21 16:56:10 +0300

    up 0.0.6 0.0.7 lib/pluto/svg

--- pkgs/lib/pluto/svg/ix.sh
+++ pkgs/lib/pluto/svg/ix.sh
@@ -5,12 +5,12 @@ plutosvg
 {% endblock %}
 
 {% block version %}
-0.0.6
+0.0.7
 {% endblock %}
 
 {% block fetch %}
 https://github.com/sammycage/plutosvg/archive/refs/tags/v{{self.version().strip()}}.tar.gz
-sha:01f8aee511bd587a602a166642a96522cc9522efd1e38c2d00e4fbc0aa22d7a0
+sha:78561b571ac224030cdc450ca2986b4de915c2ba7616004a6d71a379bffd15f3
 {% endblock %}
 
 {% block lib_deps %}
