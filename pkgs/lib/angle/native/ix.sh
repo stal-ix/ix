65d6315698059a0baacf709756a93c4cc8fcffa9
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-21 12:46:52 +0300

    build angle native

--- pkgs/lib/angle/native/ix.sh
+++ pkgs/lib/angle/native/ix.sh
@@ -17,20 +17,105 @@ git rm third_party/gles1_conform
 git rm third_party/dawn
 {% endblock %}
 
+{% block bld_tool %}
+bld/fake/binutils
+{% endblock %}
+
 {% block bld_libs %}
 lib/c
 lib/c++
+lib/glib
+lib/kernel
+lib/wayland
+lib/pci/utils
 {% endblock %}
 
 {% block build_flags %}
 wrap_cc
+no_werror
 {% endblock %}
 
 {% block gn_args %}
+angle_enable_swiftshader=false
 angle_enable_vulkan=true
+angle_enable_wgpu=false
 angle_use_wayland=true
+angle_use_x11=false
+angle_enable_d3d9=false
+angle_enable_d3d11=false
+angle_enable_gl=false
+angle_enable_metal=false
+angle_enable_null=false
+angle_enable_vulkan=true
+angle_enable_essl=false
+angle_enable_glsl=false
+angle_shared_libvulkan=false
+angle_build_tests=false
+angle_enable_commit_id=false
+clang_base_path=""
+clang_use_chrome_plugins=false
+clang_version=19
+is_clang=true
+custom_toolchain="//build/toolchain/linux/unbundle:default"
+host_toolchain="//build/toolchain/linux/unbundle:default"
+libcxx_is_shared=false
+symbol_level=0
+sysroot="/"
+treat_warnings_as_errors=false
+is_component_build=false
+use_sysroot=false
+use_static_angle=true
+use_x11=false
+ozone_platform_x11=false
+vulkan_use_x11=false
 {% endblock %}
 
 {% block ninja_build_targets %}
 libEGL.so
+{% endblock %}
+
+{% block patch %}
+cat << EOF > build/config/gclient_args.gni
+build_with_chromium = false
+checkout_android = false
+checkout_android_prebuilts_build_tools = false
+checkout_android_native_support = false
+checkout_ios_webkit = false
+checkout_nacl = true
+checkout_openxr = false
+checkout_rts_model = false
+checkout_src_internal = false
+cros_boards = ""
+cros_boards_with_qemu_images = ""
+generate_location_tags = false
+EOF
+mkdir -p build/linux/debian_bullseye_amd64-sysroot
+{% endblock %}
+
+{% block configure %}
+{{super()}}
+find ${tmp}/obj -type f -name '*.ninja' | while read l; do
+    sed -e 's|../../../../lib/clang/19/lib/x86_64-unknown-linux-gnu/libclang_rt.builtins.a||' \
+        -e 's|/bin/clang++|clang++|g' \
+        -e 's|/bin/clang|clang|g' \
+        -e 's|/bin/llvm-readelf|llvm-readelf|g' \
+        -e 's|/bin/llvm-nm|llvm-nm|g' \
+        -e 's|-Wno-nontrivial-memcall||g' \
+        -i ${l}
+done
+find third_party/libc++abi -type f | while read l; do
+    echo > ${l}
+done
+find third_party/libc++abi -type f -name '*.h' -delete
+rm -rf third_party/libc++abi/src/include
+find third_party/libc++ -type f | while read l; do
+    echo > ${l}
+done
+find third_party/libc++ -type f -name '*.h' -delete
+rm -rf third_party/libc++/src/include
+{% endblock %}
+
+{% block install %}
+mkdir ${out}/lib
+llvm-ar qL ${out}/lib/libangle.a ${tmp}/lib/libGLESv2.a ${tmp}/lib/libEGL.a
 {% endblock %}
