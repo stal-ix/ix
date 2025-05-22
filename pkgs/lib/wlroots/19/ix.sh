3a3a844474b2df9b6436bb2ad72124d5f1ac8ec2
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-22 20:32:44 +0300

    add libliftoff

--- pkgs/lib/wlroots/19/ix.sh
+++ pkgs/lib/wlroots/19/ix.sh
@@ -12,3 +12,8 @@ wlroots
 https://gitlab.freedesktop.org/wlroots/wlroots/-/archive/{{self.version().strip()}}/wlroots-{{self.version().strip()}}.tar.bz2
 sha:2f31ba9d4ae1ff3f5ac62f21b046fe31bb408c7f887c4dfafb2e09e98c991980
 {% endblock %}
+
+{% block lib_deps %}
+{{super()}}
+lib/lift/off
+{% endblock %}
