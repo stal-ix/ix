c739219386a75a55ed524902f3143c4e83785353
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-22 09:31:57 +0300

    revert

--- pkgs/lib/wlroots/18/ix.sh
+++ pkgs/lib/wlroots/18/ix.sh
@@ -5,10 +5,10 @@ wlroots
 {% endblock %}
 
 {% block version %}
-0.19.0
+0.18.2
 {% endblock %}
 
 {% block fetch %}
 https://gitlab.freedesktop.org/wlroots/wlroots/-/archive/{{self.version().strip()}}/wlroots-{{self.version().strip()}}.tar.bz2
-sha:2f31ba9d4ae1ff3f5ac62f21b046fe31bb408c7f887c4dfafb2e09e98c991980
+sha:a28061f7778f28f7be377fd4d6f0ebd58cb2a63b52460e9fde28e2ab43e80cab
 {% endblock %}
