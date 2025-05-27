3804e9ab7008864acf7bad7e1ab3f15ca0c1e757
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-27 20:21:00 +0300

    one more mirror

---
+++ pkgs/bin/git/lfs/ix.sh
@@ -0,0 +1,22 @@
+{% extends '//die/go/build.sh' %}
+
+{% block pkg_name %}
+git-lfs
+{% endblock %}
+
+{% block version %}
+3.6.1
+{% endblock %}
+
+{% block go_url %}
+https://github.com/git-lfs/git-lfs/archive/refs/tags/v{{self.version().strip()}}.tar.gz
+{% endblock %}
+
+{% block go_sha %}
+2a1f031b45960621119c571c4e82b2418567e7ebdd45514f6dded55e615312b3
+{% endblock %}
+
+{% block install %}
+mkdir ${out}/bin
+cp git-lfs ${out}/bin/
+{% endblock %}
