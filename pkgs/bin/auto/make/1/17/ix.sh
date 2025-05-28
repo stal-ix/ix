d1d55cc645540089e5b75de333414c9d3a60142d
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-28 22:22:39 +0300

    revert

--- pkgs/bin/auto/make/1/17/ix.sh
+++ pkgs/bin/auto/make/1/17/ix.sh
@@ -1,10 +1,10 @@
 {% extends '//bin/auto/make/1/16/3/ix.sh' %}
 
 {% block version %}
-1.18
+1.17
 {% endblock %}
 
 {% block fetch %}
 https://ftp.gnu.org/gnu/automake/automake-{{self.version().strip()}}.tar.xz
-sha:5bdccca96b007a7e344c24204b9b9ac12ecd17f5971931a9063bdee4887f4aaf
+sha:8920c1fc411e13b90bf704ef9db6f29d540e76d232cb3b2c9f4dc4cc599bd990
 {% endblock %}
