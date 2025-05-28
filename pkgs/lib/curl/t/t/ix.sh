24e6e991ac9c5507fdb0befb2a492a4677d90407
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-28 18:00:02 +0300

    enable autoupdate

--- pkgs/lib/curl/t/t/ix.sh
+++ pkgs/lib/curl/t/t/ix.sh
@@ -1,7 +1,5 @@
 {% extends '//die/c/autorehell.sh' %}
 
-# noauto
-
 {% block pkg_name %}
 curl
 {% endblock %}
@@ -14,12 +12,6 @@ curl
 https://github.com/curl/curl/archive/refs/tags/curl-{{self.version().strip().replace('.', '_')}}.tar.gz
 sha:93fc06e62db6c0ae8841d967179e04cc4e8b1837fa38d4db3d570eadd82be112
 {% endblock %}
-
-{#
-https://github.com/curl/curl/archive/refs/tags/curl-8_13_0.tar.gz
-/ix/build/MDjqMeM8IRlmXue54dm0E6/src/Source/cmCurl.cxx:178:26: error: assigning to 'CURL_NETRC_OPTION' from incompatible type 'long'
-  178 |       curl_netrc_level = CURL_NETRC_OPTIONAL;
-#}
 
 {% block bld_tool %}
 bld/auto
