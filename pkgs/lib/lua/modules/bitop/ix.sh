aeae410b4962ee549fbc29e488eabfa809889d9a
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-30 16:30:37 +0300

    up

--- pkgs/lib/lua/modules/bitop/ix.sh
+++ pkgs/lib/lua/modules/bitop/ix.sh
@@ -1,16 +1,18 @@
 {% extends '//lib/lua/modules/t.sh' %}
 
+# check bin/neo/vim
+
 {% block pkg_name %}
 LuaBitOp
 {% endblock %}
 
 {% block version %}
-1.0.2
+1.0.3
 {% endblock %}
 
 {% block fetch %}
 http://bitop.luajit.org/download/LuaBitOp-{{self.version().strip()}}.tar.gz
-md5:d0c1080fe0c844e8477279668e2d0d06
+sha:d514a3d2cefa76c8d11c1b9ec740d5fae316a9c9764e1e12ddea21e4982fab4b
 {% endblock %}
 
 {% block cpp_defines %}
