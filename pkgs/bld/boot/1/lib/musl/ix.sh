6a1a59d387718c6c36e5bbf489988cd2600b4f59
Anton Samokhvalov <pg@yandex-team.ru>
2025-06-04 17:35:18 +0300

    do not use custom musl build sequence

--- pkgs/bld/boot/1/lib/musl/ix.sh
+++ pkgs/bld/boot/1/lib/musl/ix.sh
@@ -1,5 +1,14 @@
-{% extends '//die/hub.sh' %}
+{% extends '//lib/musl/boot/ix.sh' %}
 
-{% block lib_deps %}
-bld/boot/1/lib/musl/{{target.gnu_arch}}
+{% block bld_deps %}
+bld/boot/1/sh
+bld/boot/0/env
+{% endblock %}
+
+{% block script_parts %}
+/usr/bin/env
+PATH={{ix_boot_path}}
+env
+sh
+-s
 {% endblock %}
