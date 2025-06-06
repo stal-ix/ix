1abd3548f69575754a841ff4c4d911bfdba364f8
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-06-06 10:04:54 +0300

    up 0.395 0.396 aux/hwdata

--- pkgs/aux/hwdata/ix.sh
+++ pkgs/aux/hwdata/ix.sh
@@ -5,12 +5,12 @@ hwdata
 {% endblock %}
 
 {% block version %}
-0.395
+0.396
 {% endblock %}
 
 {% block fetch %}
 https://github.com/vcrhonek/hwdata/archive/refs/tags/v{{self.version().strip()}}.tar.gz
-sha:1166f733c57afa82cfdad56e62ef044835fbc8c99ef65f033e6a5802716b5c66
+sha:6ed6ff6eb9d137b9669af6966974643a015cf302a39237ef84dd2efa5e20bae8
 {% endblock %}
 
 {% block configure %}
