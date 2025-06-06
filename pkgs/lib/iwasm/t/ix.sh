96bfb131b8837fdec9ba29035933762cfc455a1b
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-06-06 18:16:08 +0300

    up

--- pkgs/lib/iwasm/t/ix.sh
+++ pkgs/lib/iwasm/t/ix.sh
@@ -5,12 +5,12 @@ wasm-micro-runtime
 {% endblock %}
 
 {% block version %}
-2.3.0
+2.3.1
 {% endblock %}
 
 {% block fetch %}
 https://github.com/bytecodealliance/wasm-micro-runtime/archive/refs/tags/WAMR-{{self.version().strip()}}.tar.gz
-sha:542bdc941fd88b125057d5337e14e3abb8690223069d9503110ecbd2dbdc1ea6
+sha:542d93386f032101635e7f7cf67bdd172adfe2d49dd9eb92c0bbea5cfafd1f8e
 {% endblock %}
 
 {% block bld_libs %}
