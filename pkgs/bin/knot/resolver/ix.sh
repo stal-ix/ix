cb42a86ce731cfb2c1577b74de706727870624e0
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-06-06 10:25:22 +0300

    up 6.0.13 6.0.14 bin/knot/resolver

--- pkgs/bin/knot/resolver/ix.sh
+++ pkgs/bin/knot/resolver/ix.sh
@@ -5,12 +5,12 @@ knot-resolver
 {% endblock %}
 
 {% block version %}
-6.0.13
+6.0.14
 {% endblock %}
 
 {% block fetch %}
 https://secure.nic.cz/files/knot-resolver/knot-resolver-{{self.version().strip()}}.tar.xz
-sha:5e00f10948cc6c8b3eb22ff58fbd6f101f4b92a44e9e760d98519ada1076259f
+sha:c0521401d27435f601f7abba6cc5a0a1b98a7ac6cdca5b13ebfc81e9a13b6019
 {% endblock %}
 
 {% block bld_libs %}
