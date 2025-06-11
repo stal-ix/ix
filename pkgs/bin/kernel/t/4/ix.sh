c1f1db19b114313857d92cef1b21a6ba5db6a552
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-06-11 20:08:33 +0300

    reegen

--- pkgs/bin/kernel/t/4/ix.sh
+++ pkgs/bin/kernel/t/4/ix.sh
@@ -1,5 +1,13 @@
 {% extends '//bin/kernel/t/3/ix.sh' %}
 
+{% block pkg_name %}
+linux-kernel
+{% endblock %}
+
+{% block version %}
+{{self.kernel_version().strip()}}
+{% endblock %}
+
 {% block fetch %}
 {{self.kernel_url().strip()}}
 {{self.kernel_sha().strip()}}
