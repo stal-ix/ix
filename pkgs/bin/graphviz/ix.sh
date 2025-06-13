10392d325909e098919463a2d5c1f62e91da844e
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-06-13 13:45:39 +0300

    up 12.2.1 13.0.0 bin/graphviz

--- pkgs/bin/graphviz/ix.sh
+++ pkgs/bin/graphviz/ix.sh
@@ -5,12 +5,12 @@ graphviz
 {% endblock %}
 
 {% block version %}
-12.2.1
+13.0.0
 {% endblock %}
 
 {% block fetch %}
 https://gitlab.com/graphviz/graphviz/-/archive/{{self.version().strip()}}/graphviz-{{self.version().strip()}}.tar.bz2
-sha:a990b38c3ea807a06597ce8d46d87878e59bb3fb12609fd98c02a861a4ca81b8
+sha:d3926ff2c626b0825dd3e6172a21381000735a61586fc8bab3908abe3f635252
 {% endblock %}
 
 {% block make_no_thrs %}
