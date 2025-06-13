0fbe3a0d2c817f1a7aa12b33d4064aec3cf3fd6c
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-06-13 14:06:20 +0300

    up 2.6.1 2.6.2 bin/hysteria

--- pkgs/bin/hysteria/ix.sh
+++ pkgs/bin/hysteria/ix.sh
@@ -5,7 +5,7 @@ hysteria
 {% endblock %}
 
 {% block version %}
-2.6.1
+2.6.2
 {% endblock %}
 
 {% block go_url %}
@@ -13,11 +13,11 @@ https://github.com/apernet/hysteria/archive/refs/tags/app/v{{self.version().stri
 {% endblock %}
 
 {% block go_sha %}
-056941e5f0e003fa0a8450e845404ad85398d8503abdaef1c29b942e8064db5c
+95c86e242f1fba38bd10ef5f3f7ad9f947a1296707a7a03c6c0100a8e235c23b
 {% endblock %}
 
 {% block go_tool %}
-bin/go/lang/23
+bin/go/lang/24
 {% endblock %}
 
 {% block unpack %}
