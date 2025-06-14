91bfc7771fed53dfa8bd1bfe7247b33f515040dd
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-06-14 12:11:17 +0300

    fix url

--- pkgs/bld/sbcl/ix.sh
+++ pkgs/bld/sbcl/ix.sh
@@ -1,7 +1,7 @@
 {% extends '//bin/sbcl/t/ix.sh' %}
 
 {% block fetch %}
-https://nav.dl.sourceforge.net/project/sbcl/sbcl/2.2.1/sbcl-2.2.1-source.tar.bz2
+https://downloads.sourceforge.net/project/sbcl/sbcl/2.2.1/sbcl-2.2.1-source.tar.bz2
 5dd6e6e3f08b7c6edf262a0e844a9f8b5e562cca08155034c1f2c014fc9087da
 {% endblock %}
 
