c8d6c18787e978b1c366ecaf9ed70d40709340e8
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-06-15 00:53:00 +0300

    up 0.77 0.78 lib/ell

--- pkgs/lib/ell/ix.sh
+++ pkgs/lib/ell/ix.sh
@@ -5,12 +5,12 @@ ell
 {% endblock %}
 
 {% block version %}
-0.77
+0.78
 {% endblock %}
 
 {% block fetch %}
 https://mirrors.edge.kernel.org/pub/linux/libs/ell/ell-{{self.version().strip()}}.tar.xz
-85884a8da59de4e88db065da41b21ebee7074754a52ef69bad53fe2acb64d5d3
+28ee1a586b4891c96e68667a424684e64ad35120f345e89d6705bd4023d2d40a
 {% endblock %}
 
 {% block lib_deps %}
