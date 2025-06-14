dfeed29e0f64e9c2981ca2141e100c916ca09b16
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-06-15 01:59:22 +0300

    up 4.0.48 4.0.49 bin/mtools

--- pkgs/bin/mtools/ix.sh
+++ pkgs/bin/mtools/ix.sh
@@ -5,12 +5,12 @@ mtools
 {% endblock %}
 
 {% block version %}
-4.0.48
+4.0.49
 {% endblock %}
 
 {% block fetch %}
 http://ftp.gnu.org/gnu/mtools/mtools-{{self.version().strip()}}.tar.lz
-7d57f14a2482dc4658e4b386180793ef50f0de89ee3a9a28f0a371630baac7c4
+76dfea98d923dfc9806ce34bd1786aa9b5a39d70f56f26c0670a348c664f1d2a
 {% endblock %}
 
 {% block bld_libs %}
