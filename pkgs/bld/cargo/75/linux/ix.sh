5c663b1f6210698e84a67992427c6d09650a28aa
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-06-18 00:11:32 +0300

    simplify

--- pkgs/bld/cargo/75/linux/ix.sh
+++
@@ -1,6 +0,0 @@
-{% extends '//bld/rust/t/ix.sh' %}
-
-{% block fetch %}
-https://static.rust-lang.org/dist/rust-1.75.0-x86_64-unknown-linux-musl.tar.gz
-cc6ef41aa811ab34f946fe2b4338d1107daf08642125fd566386bf45563597de
-{% endblock %}
