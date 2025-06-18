b08097f507e074d50a8cb228f2b777ef5521336b
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-06-19 00:35:06 +0300

    use custom cargo

--- pkgs/bld/cargo/86/ix.sh
+++ pkgs/bld/cargo/86/ix.sh
@@ -1,5 +1,9 @@
-{% extends '//die/hub.sh' %}
+{% extends '//bld/cargo/t/ix.sh' %}
 
-{% block run_deps %}
-bld/rust/86
+{% block version %}
+0.86.0
+{% endblock %}
+
+{% block cargo_sha %}
+656a05e30c33725b71f644cc8d772bbac3ed74d227826a0d1fcd510dfce336f3
 {% endblock %}
--- pkgs/bld/cargo/87/ix.sh
+++ pkgs/bld/cargo/87/ix.sh
@@ -1,30 +1,9 @@
-{% extends '//die/rust/cargo.sh' %}
-
-{% block pkg_name %}
-cargo
-{% endblock %}
+{% extends '//bld/cargo/t/ix.sh' %}
 
 {% block version %}
 0.87.0
 {% endblock %}
 
-{% block cargo_url %}
-https://github.com/rust-lang/cargo/archive/refs/tags/{{self.version().strip()}}.tar.gz
-{% endblock %}
-
 {% block cargo_sha %}
 31982949e65e6c16f921a329697a6e63c0a7cd6a0fb7369730dfab7da6c0e02f
-{% endblock %}
-
-{% block bld_libs %}
-lib/curl
-lib/git/2
-{% endblock %}
-
-{% block cargo_bins %}
-cargo
-{% endblock %}
-
-{% block cargo_tool %}
-bld/rust/86
 {% endblock %}
