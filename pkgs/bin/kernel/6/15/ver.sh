c3bdbfd17c2d204695ac82d3953c1706690b20ee
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-06-19 23:05:06 +0300

    up kernel 6.15.3

--- pkgs/bin/kernel/6/15/ver.sh
+++ pkgs/bin/kernel/6/15/ver.sh
@@ -1,9 +1,9 @@
 {% block kernel_version %}
-6.15.2
+6.15.3
 {% endblock %}
 
 {% block kernel_sha %}
-3458cd6a6c508e161dbc5406e72b99d5dbdf929faf704a67db9ba46d07514858
+12b50c89925438d9cd7385a0cafc9c433e6562ac5df00a21889fce9f548d65b0
 {% endblock %}
 
 {% block kernel_url %}
