3fb2eee6d8a4666c80eac68bf8c6c95764119589
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-06-22 11:39:08 +0300

    optimize binary size

--- pkgs/bin/ix/init/ix.sh
+++ pkgs/bin/ix/init/ix.sh
@@ -4,8 +4,5 @@
 bin/ix/init/rc
 bin/ix/init/pid1
 bin/runit/sys/etc
-bin/ix/init/ewontfix
-bin/ix/init/reboot(prog_name=reboot,prog_action=RB_AUTOBOOT)
-bin/ix/init/reboot(prog_name=halt,prog_action=RB_HALT_SYSTEM)
-bin/ix/init/reboot(prog_name=poweroff,prog_action=RB_POWER_OFF)
+bin/ix/init/binaries(bumpalloc_buf=10000)
 {% endblock %}
