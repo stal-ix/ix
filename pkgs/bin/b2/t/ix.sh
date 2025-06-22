b8d0ff5c0d60c3d1cd8cab73eed830bd7355980c
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-06-22 13:17:20 +0300

    up 5.3.2 5.3.3 bin/b2

--- pkgs/bin/b2/t/ix.sh
+++ pkgs/bin/b2/t/ix.sh
@@ -5,12 +5,12 @@ b2
 {% endblock %}
 
 {% block version %}
-5.3.2
+5.3.3
 {% endblock %}
 
 {% block fetch %}
 https://github.com/bfgroup/b2/archive/refs/tags/{{self.version().strip()}}.tar.gz
-a59c4e6093daad38f5f70fa7d83f93afeeca9c56a51e009009b7e3088735cc3e
+400a4671a58531f53e2b2d9526e6bd0b92535391ff8e906f2e6bd201f7b8c327
 {% endblock %}
 
 {% block bld_libs %}
