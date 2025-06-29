a3b2285ac4a37ac45b859eaf1ffd19627ea68ceb
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-06-29 16:05:02 +0300

    up 2.56.2 2.56.3 lib/atk

--- pkgs/lib/atk/ix.sh
+++ pkgs/lib/atk/ix.sh
@@ -5,12 +5,12 @@ at-spi2-core
 {% endblock %}
 
 {% block version %}
-2.56.2
+2.56.3
 {% endblock %}
 
 {% block fetch %}
 https://download.gnome.org/sources/at-spi2-core/2.56/at-spi2-core-{{self.version().strip()}}.tar.xz
-e1b1c9836a8947852f7440c32e23179234c76bd98cd9cc4001f376405f8b783b
+0e41e1fc6a1961b38b4f9c0bea64bad30efff75949b7cdb988d2f2fdab72267a
 {% endblock %}
 
 {% block lib_deps %}
