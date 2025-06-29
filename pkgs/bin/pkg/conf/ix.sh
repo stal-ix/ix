a342adb23bfaa2dad39fbfbe3a827d5da8e28580
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-06-29 17:34:48 +0300

    up 2.5.0 2.5.1 bin/pkg/conf

--- pkgs/bin/pkg/conf/ix.sh
+++ pkgs/bin/pkg/conf/ix.sh
@@ -5,12 +5,12 @@ pkgconf
 {% endblock %}
 
 {% block version %}
-2.5.0
+2.5.1
 {% endblock %}
 
 {% block fetch %}
 https://github.com/pkgconf/pkgconf/archive/refs/tags/pkgconf-{{self.version().strip()}}.tar.gz
-89060c3b3e5e5c7d7101eefd6fb8df8d9bcdc6c14217390b925557b6c0064671
+79721badcad1987dead9c3609eb4877ab9b58821c06bdacb824f2c8897c11f2a
 {% endblock %}
 
 {% block bld_libs %}
