5416f4ad1172847aee80cf135a612fed95e8170d
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-06-29 22:29:15 +0300

    add remark

--- pkgs/lib/mu/js/ix.sh
+++ pkgs/lib/mu/js/ix.sh
@@ -1,13 +1,25 @@
 {% extends '//die/c/make.sh' %}
 
+{#
+/ix/store/9wSuGHsxpXbosjt7KKWhG1-bin-dash-sh/bin/sh: 1: curl: not found
+make: *** [Makefile:76: UnicodeData.txt] Error 127
+make: *** Waiting for unfinished jobs....
+/ix/store/9wSuGHsxpXbosjt7KKWhG1-bin-dash-sh/bin/sh: 1: curl: not found
+make: *** [Makefile:78: SpecialCasing.txt] Error 127
+/ix/store/9wSuGHsxpXbosjt7KKWhG1-bin-dash-sh/bin/sh: 1: curl: not found
+make: *** [Makefile:76: UnicodeData.txt] Error 127
+#}
+
 {% block pkg_name %}
 mujs
 {% endblock %}
 
-{% block version %}1.3.6{% endblock %}
+{% block version %}
+1.3.6
+{% endblock %}
 
 {% block fetch %}
-https://github.com/ccxvii/mujs/archive/refs/tags/{{self.version()}}.tar.gz
+https://github.com/ccxvii/mujs/archive/refs/tags/{{self.version().strip()}}.tar.gz
 7cf3a5e622cff41903efff0334518fc94af063256752c38ba4618a5191e44f18
 {% endblock %}
 
@@ -25,5 +37,6 @@ wrap_cc
 
 {% block install %}
 {{super()}}
-sed -e 's|Version:.*|Version: {{self.version()}}|' -i ${out}/lib/pkgconfig/mujs.pc
+sed -e 's|Version:.*|Version: {{self.version().strip()}}|' \
+    -i ${out}/lib/pkgconfig/mujs.pc
 {% endblock %}
