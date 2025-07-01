8c2935d812aa663a8930a717673530bc6c7b32e7
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-07-01 08:36:28 +0300

    add wget to ci

--- pkgs/lib/freebsd/c/env/ix.sh
+++ pkgs/lib/freebsd/c/env/ix.sh
@@ -1,6 +1,8 @@
 {% extends '//die/env.sh' %}
 
 {% block env %}
-export LDFLAGS="-lc -lm ${LDFLAGS}"
+export LDFLAGS="-lc -lm -lpthread ${LDFLAGS}"
 export gl_cv_func_strcasecmp_works=y
+export ac_cv_func_malloc_0_nonnull=yes
+export ac_cv_func_realloc_0_nonnull=yes
 {% endblock %}
--- pkgs/set/ci/unwrap/freebsd/x86_64/ix.sh
+++ pkgs/set/ci/unwrap/freebsd/x86_64/ix.sh
@@ -1,6 +1,7 @@
 {% extends '//die/hub.sh' %}
 
 {% block run_deps %}
+bin/wget
 bin/unbound
 set/compress
 sert/box/gnu
