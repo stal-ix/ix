f48fc03d4d4337fa2da585bd6e1b4fa64eab22a6
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-07-02 20:44:51 +0300

    explicit target for freebsd

--- pkgs/die/dl/implib.sh
+++ pkgs/die/dl/implib.sh
@@ -20,6 +20,16 @@ bld/implib
 bld/fake/binutils
 {% endblock %}
 
+{% block implib_target %}
+{% if linux %}
+{{target.gnu_arch}}-linux-gnu
+{% elif freebsd %}
+{{target.gnu_arch}}-linux-gnu
+{% else %}
+{{error('unsupported target for implib')}}
+{% endif %}
+{% endblock %}
+
 {% block build %}
 cat << EOF > {{self.implib_name().strip()}}.def
 LIBRARY lib{{self.implib_name().strip()}}.so
@@ -29,7 +39,7 @@ EXPORTS
 {% endfor %}
 EOF
 cat {{self.implib_name().strip()}}.def
-implib-gen.py --target {{target.gnu_arch}}-linux-gnu {{self.implib_name().strip()}}.def
+implib-gen.py --target {{self.implib_target().strip()}} {{self.implib_name().strip()}}.def
 cc -c -o tramp.o {{self.implib_name().strip()}}.tramp.S
 cc -c -o init.o {{self.implib_name().strip()}}.init.c
 ar q libimplib.a tramp.o init.o
