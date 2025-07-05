b5838c1b3554bd2af69efbf900b431d4f89c7a68
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-07-05 20:03:46 +0300

    up 3.5.0 3.5.1 bin/openssl lib/openssl/3

--- pkgs/lib/openssl/3/ix.sh
+++ pkgs/lib/openssl/3/ix.sh
@@ -5,12 +5,12 @@ openssl
 {% endblock %}
 
 {% block version %}
-3.5.0
+3.5.1
 {% endblock %}
 
 {% block fetch %}
 https://github.com/openssl/openssl/archive/refs/tags/openssl-{{self.version().strip()}}.tar.gz
-576740bfe93209efda2be7cecb00dfeabe77fe9c5c908c1790cbe8fa50b223d0
+24e3a8a1e832e29255b55d06fcc473b9f56e60555b504bf4422ca64a0d0c1426
 {% endblock %}
 
 {% block env %}
