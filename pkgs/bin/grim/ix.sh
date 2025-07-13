2e4affade7cf5e47d321943d9c9e2f2ac8abe152
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-07-13 23:31:59 +0300

    up gim

--- pkgs/bin/grim/ix.sh
+++ pkgs/bin/grim/ix.sh
@@ -5,12 +5,12 @@ grim
 {% endblock %}
 
 {% block version %}
-1.4.1
+1.5.0
 {% endblock %}
 
 {% block fetch %}
-https://git.sr.ht/~emersion/grim/archive/v{{self.version().strip()}}.tar.gz
-5ed8e70fcd83a7e203e92d34dbb82a1342d3f13ad98a6b0310cc97e1a9342ded
+https://gitlab.freedesktop.org/emersion/grim/-/archive/v{{self.version().strip()}}/grim-v{{self.version().strip()}}.tar.bz2
+f27cbbc87c191a10b4643cb1f600a3b6cce1613cfb46e4b199b098fb3b83a5a9
 {% endblock %}
 
 {% block bld_libs %}
