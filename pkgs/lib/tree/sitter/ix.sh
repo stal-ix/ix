67dd588711193cbaee43c697fee8dffc05631eec
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-07-13 23:07:53 +0300

    up 0.25.6 0.25.7 lib/tree/sitter

--- pkgs/lib/tree/sitter/ix.sh
+++ pkgs/lib/tree/sitter/ix.sh
@@ -5,12 +5,12 @@ tree-sitter
 {% endblock %}
 
 {% block version %}
-0.25.6
+0.25.7
 {% endblock %}
 
 {% block fetch %}
 https://github.com/tree-sitter/tree-sitter/archive/refs/tags/v{{self.version().strip()}}.tar.gz
-ac6ed919c6d849e8553e246d5cd3fa22661f6c7b6497299264af433f3629957c
+ef9d1afe8e81a508c28e529101f28ad38b785daf3acc0a2f707d00e8c4a498a8
 {% endblock %}
 
 {% block lib_deps %}
