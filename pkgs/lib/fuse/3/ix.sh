e249816ee91ca72e7d673f8d490c3c54d9ff2f8d
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-07-20 17:42:48 +0300

    up 3.17.2 3.17.3 bin/fusermount/3 lib/fuse/3

--- pkgs/lib/fuse/3/ix.sh
+++ pkgs/lib/fuse/3/ix.sh
@@ -5,12 +5,12 @@ libfuse
 {% endblock %}
 
 {% block version %}
-3.17.2
+3.17.3
 {% endblock %}
 
 {% block fetch %}
 https://github.com/libfuse/libfuse/archive/refs/tags/fuse-{{self.version().strip()}}.tar.gz
-9d34adf5bd979cd62479340d9854e6a424a7ead7ee632e4a6da104ec0796f923
+99dcab8a4be0e5d3c44f882ab9d4a711533f8cb4a4298e41175ff9bb63770fe5
 {% endblock %}
 
 {% block lib_deps %}
