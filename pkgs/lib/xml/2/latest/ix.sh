cdda3751f50f6854b40f76197129b4dac309f162
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-07-20 17:45:56 +0300

    up 2.14.4 2.14.5 bin/xml/lint lib/xml/2/latest

--- pkgs/lib/xml/2/latest/ix.sh
+++ pkgs/lib/xml/2/latest/ix.sh
@@ -1,10 +1,10 @@
 {% extends '//lib/xml/2/t/ix.sh' %}
 
 {% block version %}
-2.14.4
+2.14.5
 {% endblock %}
 
 {% block fetch %}
 {{super()}}
-8e20c2ef2fc7965d7851e3ab3a756c3fc8b449ba47526269a9b4d7f0480550db
+f5eb7a794759770debada3a1e3feba130af1a5735615974509224b5b97289c1a
 {% endblock %}
