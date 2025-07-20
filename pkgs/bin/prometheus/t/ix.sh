6c0af7382767b35bff5b2047eb9eeb1903963360
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-07-20 18:15:42 +0300

    up 3.4.2 3.5.0 bin/prometheus/d bin/prometheus/tool

--- pkgs/bin/prometheus/t/ix.sh
+++ pkgs/bin/prometheus/t/ix.sh
@@ -5,7 +5,7 @@ prometheus
 {% endblock %}
 
 {% block version %}
-3.4.2
+3.5.0
 {% endblock %}
 
 {% block go_url %}
@@ -13,7 +13,7 @@ https://github.com/prometheus/prometheus/archive/refs/tags/v{{self.version().str
 {% endblock %}
 
 {% block go_sha %}
-7756d1877e0f60c1b0600d9475ded4876da333a8281c29fad77e5f5dc8e1576e
+91387d4238d36a45a22b9a0a8935912cdf7ca040d10f7ed98fa3e4d83dafeed3
 {% endblock %}
 
 {% block go_tool %}
