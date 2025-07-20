47b136f5135f17f0260e512992137a2d994c5f50
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-07-20 17:04:38 +0300

    up 2.5.4 2.6.0 bin/go/crypt/fs

--- pkgs/bin/go/crypt/fs/ix.sh
+++ pkgs/bin/go/crypt/fs/ix.sh
@@ -5,7 +5,7 @@ gocryptfs
 {% endblock %}
 
 {% block version %}
-2.5.4
+2.6.0
 {% endblock %}
 
 {% block go_url %}
@@ -13,7 +13,7 @@ https://github.com/rfjakob/gocryptfs/archive/refs/tags/v{{self.version().strip()
 {% endblock %}
 
 {% block go_sha %}
-cfbc2b4c664e4ea6fefb35c58b26bba227c593475c6e8d0de2dcc4be91d74b43
+dfb8bc3f052cf7aa944c60138890fefdbe5f0ef5640847beebbe308d5dfb96fa
 {% endblock %}
 
 {% block bld_libs %}
@@ -27,4 +27,8 @@ cd gocryptfs-xray
 
 {% block go_bins %}
 gocryptfs-xray
+{% endblock %}
+
+{% block go_tool %}
+bin/go/lang/24
 {% endblock %}
