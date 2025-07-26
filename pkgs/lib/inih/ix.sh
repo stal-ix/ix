fe02e45c3e46d7eed3f14bd817afe603ed64ed2c
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-07-27 02:32:50 +0300

    up 60 61 lib/inih

--- pkgs/lib/inih/ix.sh
+++ pkgs/lib/inih/ix.sh
@@ -5,12 +5,12 @@ inih
 {% endblock %}
 
 {% block version %}
-60
+61
 {% endblock %}
 
 {% block fetch %}
 https://github.com/benhoyt/inih/archive/refs/tags/r{{self.version().strip()}}.tar.gz
-706aa05c888b53bd170e5d8aa8f8a9d9ccf5449dfed262d5103d1f292af26774
+7caf26a2202a4ca689df3fe4175dfa74e0faa18fcca07331bba934fd0ecb8f12
 {% endblock %}
 
 {% block lib_deps %}
