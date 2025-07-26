7f2078c6b906c404caa8d192b4ab3ebb164c0399
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-07-27 02:32:31 +0300

    up 0.6.3 0.6.4 lib/hypr/lang

--- pkgs/lib/hypr/lang/ix.sh
+++ pkgs/lib/hypr/lang/ix.sh
@@ -5,12 +5,12 @@ hyprlang
 {% endblock %}
 
 {% block version %}
-0.6.3
+0.6.4
 {% endblock %}
 
 {% block fetch %}
 https://github.com/hyprwm/hyprlang/archive/refs/tags/v{{self.version().strip()}}.tar.gz
-f5effe017edc7a0036c20c7ecbea4edc2bfdacbc0f791b283bd21ec202384251
+cd7395eaeb1eee90ebeba9c1330a7635709dd0900ebbe349a7aa5702ba4d613f
 {% endblock %}
 
 {% block lib_deps %}
