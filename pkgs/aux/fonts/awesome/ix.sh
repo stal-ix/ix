8ddbb716169bcba0ec9e5ef1beff084db7f24644
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-07-27 02:17:01 +0300

    up 6.7.2 7.0.0 aux/fonts/awesome

--- pkgs/aux/fonts/awesome/ix.sh
+++ pkgs/aux/fonts/awesome/ix.sh
@@ -5,12 +5,12 @@ Font-Awesome
 {% endblock %}
 
 {% block version %}
-6.7.2
+7.0.0
 {% endblock %}
 
 {% block fetch %}
 https://github.com/FortAwesome/Font-Awesome/archive/refs/tags/{{self.version().strip()}}.tar.gz
-fdebdf3f1b8641a4b665c61f1f48e482b140a817ce619113559201b8a1fcdd51
+ac6013cd8c7c49f3f388506bf2ef4c280edea38cc0267d83b445cf1f8a3c1e4b
 {% endblock %}
 
 {% block unpack %}
