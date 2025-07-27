354479463198b6125fd207d74aa7a5de95e408c6
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-07-27 03:01:29 +0300

    up 6.15.7 6.15.8 bin/kernel/6/15 lib/linux/headers/next

--- pkgs/bin/kernel/6/15/ver.sh
+++ pkgs/bin/kernel/6/15/ver.sh
@@ -1,9 +1,9 @@
 {% block kernel_version %}
-6.15.7
+6.15.8
 {% endblock %}
 
 {% block kernel_sha %}
-3507dd105b0a0e1101bd43d294472fccf853429a259a5fa7c67467bba318f8e9
+d37e92bc16b962a3025df156647bdad90b2db4ff36c7a6137817fe81ef3f28a6
 {% endblock %}
 
 {% block kernel_url %}
