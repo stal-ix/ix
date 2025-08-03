ddb964f95e32a9855fcf8be0e14abf0e1b9d5d77
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-08-04 00:37:02 +0300

    up 3.50.3 3.50.4 bin/sqlite3 lib/sqlite/3

--- pkgs/lib/sqlite/3/t/t/ix.sh
+++ pkgs/lib/sqlite/3/t/t/ix.sh
@@ -5,7 +5,7 @@ sqlite
 {% endblock %}
 
 {% block version %}
-3.50.3
+3.50.4
 {% endblock %}
 
 {% block sqlite_ver %}
@@ -14,7 +14,7 @@ sqlite
 
 {% block fetch %}
 https://www.sqlite.org/2025/sqlite-autoconf-{{self.sqlite_ver().strip()}}.tar.gz
-ec5496cdffbc2a4adb59317fd2bf0e582bf0e6acd8f4aae7e97bc723ddba7233
+a3db587a1b92ee5ddac2f66b3edb41b26f9c867275782d46c3a088977d6a5b18
 {% endblock %}
 
 {% block lib_deps %}
