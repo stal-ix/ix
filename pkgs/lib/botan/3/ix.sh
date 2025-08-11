5cb02f7369e049791680245afb6300860f0662bb
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-08-11 22:57:40 +0300

    up 3.8.1 3.9.0 lib/botan/3

--- pkgs/lib/botan/3/ix.sh
+++ pkgs/lib/botan/3/ix.sh
@@ -5,10 +5,10 @@ botan
 {% endblock %}
 
 {% block version %}
-3.8.1
+3.9.0
 {% endblock %}
 
 {% block fetch %}
 https://github.com/randombit/botan/archive/refs/tags/{{self.version().strip()}}.tar.gz
-8eb79a49c1a3f7e5e7563c13752a37557de935cdac48d9221ea4b580158e8965
+89dabf2e9bdb889242f73e579ac7e25e095e28e549bf83a00357602bc47f2618
 {% endblock %}
