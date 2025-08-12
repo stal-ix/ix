77713b441ceced191858805b0d9f0b75160bca4e
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-08-12 05:27:40 +0300

    up 4.2.7 4.2.8 bin/jasper lib/jasper

--- pkgs/lib/jasper/t/ix.sh
+++ pkgs/lib/jasper/t/ix.sh
@@ -5,12 +5,12 @@ jasper
 {% endblock %}
 
 {% block version %}
-4.2.7
+4.2.8
 {% endblock %}
 
 {% block fetch %}
 https://github.com/jasper-software/jasper/archive/refs/tags/version-{{self.version().strip()}}.tar.gz
-93d3472ae50b2cc6547d3d8ea53de7e820588a0e5be6557a7fe0b1d94eb1e5ca
+987e8c8b4afcff87553833b6f0fa255b5556a0ecc617b45ee1882e10c1b5ec14
 {% endblock %}
 
 {% block lib_deps %}
