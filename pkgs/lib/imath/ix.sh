53fd58f9daaff079a74226c6ae3a1752eec46a3a
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-08-25 15:21:56 +0300

    up 3.2.0 3.2.1 lib/imath

--- pkgs/lib/imath/ix.sh
+++ pkgs/lib/imath/ix.sh
@@ -5,12 +5,12 @@ Imath
 {% endblock %}
 
 {% block version %}
-3.2.0
+3.2.1
 {% endblock %}
 
 {% block fetch %}
 https://github.com/AcademySoftwareFoundation/Imath/archive/refs/tags/v{{self.version().strip()}}.tar.gz
-f3c0c4210b5e6fe17d90cd7ebbe9638eea9fb458421d00eddb4d1a9d5ad47b36
+b2c8a44c3e4695b74e9644c76f5f5480767355c6f98cde58ba0e82b4ad8c63ce
 {% endblock %}
 
 {% block lib_deps %}
