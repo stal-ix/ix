ad61af2ce0fd6d8c6c1317795be382fae8ec97cb
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-08-25 14:55:14 +0300

    up 2.49.0 2.50.2 bin/fastfetch

--- pkgs/bin/fastfetch/ix.sh
+++ pkgs/bin/fastfetch/ix.sh
@@ -5,12 +5,12 @@ fastfetch
 {% endblock %}
 
 {% block version %}
-2.49.0
+2.50.2
 {% endblock %}
 
 {% block fetch %}
 https://github.com/fastfetch-cli/fastfetch/archive/refs/tags/{{self.version().strip()}}.tar.gz
-5c656581c6cb3061cf8648e2cd0cdf07abcf5f680fdc8bda935deece90b086a0
+e59f2a2bdd5834ef40adb9fb6680820c268ff60ca0534469c5ea4b86775c83db
 {% endblock %}
 
 {% block bld_libs %}
