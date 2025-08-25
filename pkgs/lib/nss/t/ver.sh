04bf758aa736afce3f364d4b7298e17be9f93421
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-08-25 15:50:51 +0300

    up 3.114 3.115.1 aux/ca/bundle bin/nsstools lib/nss/unwrap

--- pkgs/lib/nss/t/ver.sh
+++ pkgs/lib/nss/t/ver.sh
@@ -3,10 +3,10 @@ nss
 {% endblock %}
 
 {% block version %}
-3.114
+3.115.1
 {% endblock %}
 
 {% block fetch_impl %}
 https://ftp.mozilla.org/pub/security/nss/releases/NSS_{{self.version().strip().replace('.', '_')}}_RTM/src/nss-{{self.version().strip()}}.tar.gz
-cac3c0d67028804fb316e9695f81749fa4dc118e731d674b4c4c347bd849c2f1
+b8189c030b528e57dc5290023c07eea429ce242912a51a0388c184c75a257bcf
 {% endblock %}
