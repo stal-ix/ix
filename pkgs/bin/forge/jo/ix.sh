b83bdfadca26317c5a4b321e462912f4762f0438
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-09-08 00:06:08 +0300

    up 12.0.1 12.0.3 bin/forge/jo

--- pkgs/bin/forge/jo/ix.sh
+++ pkgs/bin/forge/jo/ix.sh
@@ -5,7 +5,7 @@ forgejo
 {% endblock %}
 
 {% block version %}
-12.0.1
+12.0.3
 {% endblock %}
 
 {% block go_url %}
@@ -13,7 +13,7 @@ https://codeberg.org/forgejo/forgejo/archive/v{{self.version().strip()}}.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-81c2257a8a9dc2e32611844cff899140989aa0439d335835ca99ad4efb55149c
+b1deff9ef69e82f992940a0fee698360baf6cd49f2aac5891393cbc0d61ecc88
 {% endblock %}
 
 {% block go_tool %}
