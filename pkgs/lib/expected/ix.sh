eff10ff38577c0433af459cde66518a974eef9f6
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-09-08 03:28:22 +0300

    up 1.2.0 1.3.1 lib/expected

--- pkgs/lib/expected/ix.sh
+++ pkgs/lib/expected/ix.sh
@@ -5,12 +5,12 @@ expected
 {% endblock %}
 
 {% block version %}
-1.2.0
+1.3.1
 {% endblock %}
 
 {% block fetch %}
 https://github.com/TartanLlama/expected/archive/refs/tags/v{{self.version().strip()}}.tar.gz
-f5424f5fc74e79157b9981ba2578a28e0285ac6ec2a8f075e86c41226fe33386
+9a04f4f472fbb5c30bf60402f1ca626c4a76987f867978d0b8a35d7ab3fb8fe7
 {% endblock %}
 
 {% block lib_deps %}
