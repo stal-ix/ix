36e2e685adcd5020b052046931aef43df0172963
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-09-13 11:54:04 +0300

    move

--- pkgs/bin/ix/enclave/enclave.sh
+++ pkgs/bin/ix/enclave/enclave.sh
@@ -2,11 +2,11 @@
 
 mount -t tmpfs tmpfs /sys
 cd /sys
-mkdir -p var/mnt/root var/tmp ix home/pg
-ln -s ix/realm/pg/bin bin
-ln -s ix/realm/pg/etc etc
+mkdir -p var/mnt/root var/tmp ix home/${USER}
+ln -s ix/realm/${USER}/bin bin
+ln -s ix/realm/${USER}/etc etc
 mount /ix ix
-mount /home/pg home/pg
+mount /home/${USER} home/${USER}
 mount ${TMPDIR} var/tmp
 ln -s / usr
 pivot_root /sys /sys/var/mnt/root
--- pkgs/bin/vt/runit/ix.sh
+++ pkgs/bin/vt/runit/ix.sh
@@ -1,10 +1,10 @@
 {% extends '//die/hub.sh' %}
 
 {% block run_deps %}
-bin/pidns
 bin/fixtty
 bin/runsrv
 bin/session
+bin/ix/pidns
 bin/subreaper
 bin/vt/runit/scripts(slot={{vt_slot}})
 {% endblock %}
