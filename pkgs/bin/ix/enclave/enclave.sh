d24cd70e2e04dce0be7ff28ee8b8cc08958e606c
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-09-13 14:14:44 +0300

    better

--- pkgs/bin/ix/enclave/enclave.sh
+++ pkgs/bin/ix/enclave/enclave.sh
@@ -1,26 +1,3 @@
 #!/bin/sh
 
-mount -t tmpfs tmpfs /sys
-cd /sys
-mkdir -p var/mnt/root var/tmp ix home/${USER}
-ln -s ix/realm/${USER}/bin bin
-ln -s ix/realm/${USER}/etc etc
-mount /ix ix
-mount /home/${USER} home/${USER}
-mount ${TMPDIR} var/tmp
-ln -s / usr
-pivot_root /sys /sys/var/mnt/root
-cd /
-mkdir -p dev sys proc var/run
-export TMPDIR=/var/tmp
-export XDG_RUNTIME_DIR=${TMPDIR}
-mount --rbind /var/mnt/root/dev /dev
-mount --rbind /var/mnt/root/sys /sys
-mount --rbind /var/mnt/root/proc /proc
-mkdir -p /var/run/seatd
-mount --rbind /var/mnt/root/var/run/seatd /var/run/seatd
-umount -l /var/mnt/root
-rmdir /var/mnt/root
-rmdir /var/mnt
-
-exec "${@}"
+exec unshare -U -r -m enclave_slave "${@}"
--- pkgs/bin/ix/enclave/ix.sh
+++ pkgs/bin/ix/enclave/ix.sh
@@ -5,5 +5,8 @@ mkdir ${out}/bin
 base64 -d << EOF > ${out}/bin/enclave
 {% include 'enclave.sh/base64' %}
 EOF
+base64 -d << EOF > ${out}/bin/enclave_slave
+{% include 'enclave_slave.sh/base64' %}
+EOF
 chmod +x ${out}/bin/*
 {% endblock %}
