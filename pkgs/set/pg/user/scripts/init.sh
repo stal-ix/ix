5b988683fe6d6d491d0844311094886195165d8a
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-09-13 19:04:56 +0300

    better

--- pkgs/set/pg/user/scripts/init.sh
+++ pkgs/set/pg/user/scripts/init.sh
@@ -5,5 +5,12 @@ export PATH=/bin
 export XDG_SESSION_ID=$$
 export XDG_DATA_DIRS="/ix/realm/${USER}/share"
 export XDG_RUNTIME_DIR="${TMPDIR}"
+export PS1="pg:home# "
+export EDITOR=ted
+export TZ=Europe/Moscow
+export XCURSOR_SIZE=48
+export QT_FONT_DPI=192
+export GTK_A11Y=none
+export FONTCONFIG_FILE=/ix/realm/pg/etc/fonts/fonts.conf
 
 exec niri-session
