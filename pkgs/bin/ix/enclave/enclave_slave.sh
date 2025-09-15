b83f092875d8b00163b5b5e4062a3d964fbc334f
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-09-15 19:53:07 +0300

    fix

--- pkgs/bin/ix/enclave/enclave_slave.sh
+++ pkgs/bin/ix/enclave/enclave_slave.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
+set -ue
+
 mount -t tmpfs tmpfs /sys
 
 cd /sys
 
 mkdir -p var/mnt/root tmp ix home/${USER}
-ln -s ${USER} home/root
 ln -s ix/realm/${USER}/bin bin
 ln -s ix/realm/${USER}/etc etc
 mount /ix ix
