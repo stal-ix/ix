6000452ca3f1d008e60fef5800dad2d094709a38
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-09-20 21:08:04 +0300

    better

--- pkgs/bin/kernel/initrd/fix.sh
+++ pkgs/bin/kernel/initrd/fix.sh
@@ -5,7 +5,10 @@ test -f bin/initrd
 mkdir -p tmp/bin
 copybin bin tmp/bin
 cd tmp
-ln -s bin/initrd init
+cp bin/initrd init
+ln -s . usr
+ln -s bin sbin
+cp init linuxrc
 find . | cpio -o -H newc | gzip -9 -n > ../bin/initrd.gz
 cd ..
 rm -rf tmp
--- pkgs/bin/kernel/initrd/gen/scripts/copybin.py
+++ pkgs/bin/kernel/initrd/gen/scripts/copybin.py
@@ -7,12 +7,23 @@ import shutil
 fr = sys.argv[1]
 to = sys.argv[2]
 
+vis = set()
+
 for x in os.listdir(fr):
+    if 'kernel-' in x:
+        continue
+
     p = os.path.join(fr, x)
 
     if os.path.isdir(p):
         continue
 
     r = os.path.normpath(os.path.realpath(p))
+    br = os.path.basename(r)
 
-    print(r)
+    if br == x:
+        if r not in vis:
+            vis.add(r)
+            shutil.copyfile(r, to + '/' + br)
+    else:
+        os.symlink(br, to + '/' + x)
