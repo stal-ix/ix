f560e35eb5c1e5b5200f46b5c8804959d6157521
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-09-22 01:47:44 +0300

    up 2.14.0 2.14.1 lib/freetype/bootstrap lib/freetype/unwrap/ct

--- pkgs/lib/freetype/t/ix.sh
+++ pkgs/lib/freetype/t/ix.sh
@@ -5,12 +5,12 @@ freetype
 {% endblock %}
 
 {% block version %}
-2.14.0
+2.14.1
 {% endblock %}
 
 {% block fetch %}
 https://gitlab.com/freetype/freetype/-/archive/VER-{{self.version().strip().replace('.', '-')}}/freetype-VER-{{self.version().strip().replace('.', '-')}}.tar.bz2
-a84e167dcc3858418e8022c742007da13aeed7d6c457a739ee0568583a1e612d
+7fea7e6fd000b6c9387374d9487d66a0822d45721f43f47e00201f62977e2b0f
 {% endblock %}
 
 {% block lib_deps %}
