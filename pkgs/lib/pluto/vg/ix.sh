a1d41ce4bc33160dc74f5b3724472ed23238b36a
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-09-22 03:52:26 +0300

    up 1.3.0 1.3.1 lib/pluto/vg

--- pkgs/lib/pluto/vg/ix.sh
+++ pkgs/lib/pluto/vg/ix.sh
@@ -5,12 +5,12 @@ plutovg
 {% endblock %}
 
 {% block version %}
-1.3.0
+1.3.1
 {% endblock %}
 
 {% block fetch %}
 https://github.com/sammycage/plutovg/archive/refs/tags/v{{self.version().strip()}}.tar.gz
-4b08587d782f6858e6cb815b455fd7238f45190a57094857a3123883ecb595eb
+bea672eb96ee36c2cbeb911b9bac66dfe989b3ad9a9943101e00aeb2df2aefdb
 {% endblock %}
 
 {% block lib_deps %}
