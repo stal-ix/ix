9a0a7fff8bdfa41d97b3a4510c78524e2e7c2042
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-09-26 21:12:21 +0300

    up gperftools

--- pkgs/lib/reallocarray/ix.sh
+++ pkgs/lib/reallocarray/ix.sh
@@ -2,18 +2,7 @@
 
 {% block lib_deps %}
 lib/c/naked
+lib/reallocarray/impl
 lib/reallocarray/headers
 lib/build/w/include/next
-{% endblock %}
-
-{% block setup_target_flags %}
-export CFLAGS="-fno-builtin ${CFLAGS}"
-{% endblock %}
-
-{% block sources %}
-reallocarray.cpp
-{% endblock %}
-
-{% block env %}
-export ac_cv_func_reallocarray=yes
 {% endblock %}
