3073cf82be4b5901e35a12f759b178ad75ae7ecf
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-09-30 00:28:02 +0300

    up 15.4.0 16.0.0 bin/glslang lib/glslang

--- pkgs/lib/glslang/t/ix.sh
+++ pkgs/lib/glslang/t/ix.sh
@@ -5,12 +5,12 @@ glslang
 {% endblock %}
 
 {% block version %}
-15.4.0
+16.0.0
 {% endblock %}
 
 {% block fetch %}
 https://github.com/KhronosGroup/glslang/archive/refs/tags/{{self.version().strip()}}.tar.gz
-b16c78e7604b9be9f546ee35ad8b6db6f39bbbbfb19e8d038b6fe2ea5bba4ff4
+172385478520335147d3b03a1587424af0935398184095f24beab128a254ecc7
 {% endblock %}
 
 {% block bld_tool %}
