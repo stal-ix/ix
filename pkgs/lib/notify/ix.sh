9a05962974e1404f211fa5c8bf6c7b590795cbf2
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-09-30 01:13:30 +0300

    up 0.8.6 0.8.7 bin/notify/send lib/notify

--- pkgs/lib/notify/ix.sh
+++ pkgs/lib/notify/ix.sh
@@ -5,12 +5,12 @@ libnotify
 {% endblock %}
 
 {% block version %}
-0.8.6
+0.8.7
 {% endblock %}
 
 {% block fetch %}
 https://download.gnome.org/sources/libnotify/0.8/libnotify-{{self.version().strip()}}.tar.xz
-c5540aaefb60e1d63b1c587c05f2284ebe72ece7d0c0e5e4a778cfd5844b6b58
+4be15202ec4184fce1ac15997ece5530d2be32fe9573875aeb10e3b573858748
 {% endblock %}
 
 {% block lib_deps %}
