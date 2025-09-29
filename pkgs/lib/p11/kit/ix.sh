18a90ea27b11900f1da8d5c75d26614d1ea7591c
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-09-30 01:44:31 +0300

    up 0.25.9 0.25.10 lib/p11/kit

--- pkgs/lib/p11/kit/ix.sh
+++ pkgs/lib/p11/kit/ix.sh
@@ -5,12 +5,12 @@ p11-kit
 {% endblock %}
 
 {% block version %}
-0.25.9
+0.25.10
 {% endblock %}
 
 {% block fetch %}
 https://github.com/p11-glue/p11-kit/releases/download/{{self.version().strip()}}/p11-kit-{{self.version().strip()}}.tar.xz
-98a96f6602a70206f8073deb5e894b1c8efd76ef53c629ab88815d58273f2561
+a62a137a966fb3a9bbfa670b4422161e369ddea216be51425e3be0ab2096e408
 {% endblock %}
 
 {% block lib_deps %}
