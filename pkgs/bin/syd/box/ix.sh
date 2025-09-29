79e70ab4068acdd6aad689e39492f1b2616727e7
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-09-30 02:42:03 +0300

    up 3.38.3 3.39.0 bin/syd/box

--- pkgs/bin/syd/box/ix.sh
+++ pkgs/bin/syd/box/ix.sh
@@ -5,7 +5,7 @@ sydbox
 {% endblock %}
 
 {% block version %}
-3.38.3
+3.39.0
 {% endblock %}
 
 {% block cargo_url %}
@@ -13,7 +13,7 @@ https://gitlab.exherbo.org/sydbox/sydbox/-/archive/v{{self.version().strip()}}/s
 {% endblock %}
 
 {% block cargo_sha %}
-8307d30ebeaca603b641582b08fe2280df951d5d37e637da839c24a23fbe2de6
+caf1ccb3f0f6094c3fa776a6b4273e000cd7f77edfce1315528564f778381790
 {% endblock %}
 
 {% block bld_libs %}
