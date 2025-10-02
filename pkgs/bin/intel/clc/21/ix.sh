a5c86ec3c385d4d2b3f28e96706bce95fa35bedc
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-10-02 18:31:41 +0300

    fix build

--- pkgs/bin/intel/clc/21/ix.sh
+++ pkgs/bin/intel/clc/21/ix.sh
@@ -1,7 +1,54 @@
-{% extends '//bin/intel/clc/t/ix.sh' %}
+{% extends '//die/c/meson.sh' %}
+
+{% block fetch %}
+{% include '//lib/mesa/t/ver.sh' %}
+{% endblock %}
 
 {% block bld_libs %}
-{{super()}}
+lib/c
+lib/z
+lib/clc
+lib/kernel
 lib/llvm/21
+lib/spirv/tools
 lib/spirv/llvm/translator/21
+{% endblock %}
+
+{% block bld_tool %}
+pip/Mako
+bld/flex
+bld/bison
+pip/PyYAML
+bld/llvm/config
+bld/spirv/tools
+{% endblock %}
+
+{% block ninja_build_targets %}
+src/compiler/clc/mesa_clc
+src/compiler/spirv/vtn_bindgen2
+{% endblock %}
+
+{% block meson_flags %}
+gallium-drivers=
+vulkan-drivers=
+platforms=
+tools=
+install-mesa-clc=true
+mesa-clc=enabled
+video-codecs=
+xmlconfig=disabled
+llvm=enabled
+shared-llvm=disabled
+cpp_rtti=false
+shader-cache=disabled
+intel-rt=disabled
+{% endblock %}
+
+{% block meson_cross %}
+{% endblock %}
+
+{% block install %}
+mkdir ${out}/bin
+cp ${tmp}/obj/src/compiler/clc/mesa_clc ${out}/bin/
+cp ${tmp}/obj/src/compiler/spirv/vtn_bindgen2 ${out}/bin/
 {% endblock %}
