432ef86a326ceef3f45958371d5681d2af48a63a
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-10-05 00:10:44 +0300

    up 2.79.0 2.81.0 bin/gh

--- pkgs/bin/gh/ix.sh
+++ pkgs/bin/gh/ix.sh
@@ -5,7 +5,7 @@ cli
 {% endblock %}
 
 {% block version %}
-2.79.0
+2.81.0
 {% endblock %}
 
 {% block go_url %}
@@ -13,7 +13,7 @@ https://github.com/cli/cli/archive/refs/tags/v{{self.version().strip()}}.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-2c633895d0adef6c964d45ba6115002f78fe686073c828de751032b54dac8b14
+e2acc805e74675df31fa8bb99feac9df173efa5b35a18bc53fe787451316aefb
 {% endblock %}
 
 {% block unpack %}
