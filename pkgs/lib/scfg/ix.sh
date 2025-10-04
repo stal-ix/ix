84d00e2d65875ab35caf989405698f1e08125d74
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-10-05 00:38:34 +0300

    up 0.1.1 0.2.0 lib/scfg

--- pkgs/lib/scfg/ix.sh
+++ pkgs/lib/scfg/ix.sh
@@ -5,12 +5,12 @@ libscfg
 {% endblock %}
 
 {% block version %}
-0.1.1
+0.2.0
 {% endblock %}
 
 {% block fetch %}
 https://codeberg.org/emersion/libscfg/archive/v{{self.version().strip()}}.tar.gz
-851f889985e89637db430409431e63db5e0a150f1a9e18a4f4ccf1e6612c5915
+cf37ef00ac8efb28821dac1ad49e2c6b23b242d9d961fab6fcda72fc73a7291b
 {% endblock %}
 
 {% block lib_deps %}
