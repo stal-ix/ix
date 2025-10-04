c350fad51d218277521d9344f0ee9ddb64ae8a00
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-10-05 01:19:21 +0300

    up 6.16 6.17 bin/strace

--- pkgs/bin/strace/ix.sh
+++ pkgs/bin/strace/ix.sh
@@ -5,12 +5,12 @@ strace
 {% endblock %}
 
 {% block version %}
-6.16
+6.17
 {% endblock %}
 
 {% block fetch %}
 https://strace.io/files/{{self.version().strip()}}/strace-{{self.version().strip()}}.tar.xz
-3d7aee7e4f044b2f67f3d51a8a76eda18076e9fb2774de54ac351d777d4ebffa
+0a7c7bedc7efc076f3242a0310af2ae63c292a36dd4236f079e88a93e98cb9c0
 {% endblock %}
 
 {% block bld_libs %}
