a6458481a360bb7b867c6e1e2e94b89acde07074
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-10-05 16:38:48 +0300

    remove unused patch

--- pkgs/lib/mesa/valve/ix.sh
+++ pkgs/lib/mesa/valve/ix.sh
@@ -56,29 +56,3 @@ mv ${out}/lib/libGLESv2.a ${out}/lib/libmesa_GLESv2.a
 patchns ${out}/lib/libgbm.a mesa_gbm_
 mv ${out}/lib/libgbm.a ${out}/lib/libmesa_gbm.a
 {% endblock %}
-
-{% block patch %}
-(
-{{super()}}
-)
-
-(
-cd src/gallium/frontends/dri
-
-for l in *.c *.h; do
-    for x in dri2_lookup_egl_image dri2_lookup_egl_image_validated dri2_validate_egl_image; do
-        sed -e "s|${x}|${x}_xxx|g" -i ${l}
-    done
-done
-)
-
-(
-cd src/gallium/drivers/radeonsi
-
-for l in *.c *.h *.cpp; do
-    for x in vi_alpha_is_on_msb si_emit_cache_flush si_cp_dma_prefetch si_cp_dma_clear_buffer si_cp_dma_wait_for_idle; do
-        sed -e "s|${x}|${x}_xxx|g" -i ${l}
-    done
-done
-)
-{% endblock %}
