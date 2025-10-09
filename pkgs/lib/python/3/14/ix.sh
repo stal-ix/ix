e86ae31c46dfbc6feaba1c2066956f141319e09f
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-10-09 22:08:36 +0300

    add python 3.14

---
+++ pkgs/lib/python/3/14/ix.sh
@@ -0,0 +1,56 @@
+{% extends 't/ix.sh' %}
+
+{% block lib_deps %}
+lib/dlfcn
+{{super()}}
+{% endblock %}
+
+{% block bld_tool %}
+bld/python/14
+bld/pip/scripts
+{{super()}}
+{% endblock %}
+
+{% block configure_flags %}
+--disable-test-modules
+--with-build-python=${NATIVE_PYTHON}
+{{super()}}
+{% endblock %}
+
+{% block patch %}
+sed -e 's|MACHDEP=.*unknown.*|:|' \
+    -e 's|.*ERROR.*cross build not.*||' \
+    -i configure.ac
+{{super()}}
+{% endblock %}
+
+{% block configure %}
+export READELF=llvm-readelf
+export MACHDEP={{target.os}}
+export ac_sys_system={{target.cmake_system_name}}
+export ac_sys_release=
+export ac_md_system=${ac_sys_system}
+export ac_md_release=${ac_sys_release}
+export ac_cv_file__dev_ptc=no
+export ac_cv_file__dev_ptmx=yes
+{{super()}}
+{% endblock %}
+
+{% block install %}
+{{super()}}
+{% if lib %}
+cp Modules/_hacl/libH*.a ${out}/lib/
+find ${out}/lib/python3.14/ -type f | while read l; do
+    sed -e 's|Modules/_hacl/libHacl_Hash_SHA2.a||g' -i ${l}
+done
+cd ${out}/lib/python3.14
+>__init__.py
+py_exports > exports
+cat exports
+{% endif %}
+{% endblock %}
+
+{% block env %}
+{{super()}}
+export PYTHONPATH="${out}/lib/python3.14:\${PYTHONPATH}"
+{% endblock %}
