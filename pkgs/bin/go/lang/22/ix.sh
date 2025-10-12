b24231a5f1100557035fb43fc7a4dd4a0224efbe
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-10-12 23:28:32 +0300

    update golang 1.22

--- pkgs/bin/go/lang/22/ix.sh
+++ pkgs/bin/go/lang/22/ix.sh
@@ -1,12 +1,12 @@
 {% extends '//bin/go/lang/20/ix.sh' %}
 
 {% block version %}
-1.22.9
+1.22.12
 {% endblock %}
 
 {% block fetch %}
 https://github.com/golang/go/archive/refs/tags/go{{self.version().strip()}}.tar.gz
-5b77fa05205714383f35491150e8cc9dc849ad73aef634f7b7ac4854e44ecde1
+36abf4c3c80af07b7ca0543b359384ef92b06fc4102efb7f5c02b0db5d9ec651
 {% endblock %}
 
 {% block go_bootstrap %}
