2d02209c6312e38cfa2aac5b266780fe58e0f70c
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-10-12 20:40:24 +0300

    up 1.11.0 1.12.0 bin/xkbcli lib/xkb/common

--- pkgs/lib/xkb/common/t/ix.sh
+++ pkgs/lib/xkb/common/t/ix.sh
@@ -5,12 +5,12 @@ libxkbcommon
 {% endblock %}
 
 {% block version %}
-1.11.0
+1.12.0
 {% endblock %}
 
 {% block fetch %}
 https://github.com/xkbcommon/libxkbcommon/archive/refs/tags/xkbcommon-{{self.version().strip()}}.tar.gz
-78a6b14f16e9a55025978c252e53ce9e16a02bfdb929550b9a0db5af87db7e02
+bf9d9e0283ef4211f1f3f228787d38ba095fe9f0e84c5f798b6a8661033a9b51
 {% endblock %}
 
 {% block bld_tool %}
