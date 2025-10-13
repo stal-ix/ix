2b98464bdc6bb7715fb4a52a34240b445b003eec
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-10-13 13:51:22 +0300

    fix

--- pkgs/lib/pty/tty/ix.sh
+++ pkgs/lib/pty/tty/ix.sh
@@ -1,7 +1,15 @@
 {% extends '//die/c/cmake.sh' %}
 
+{% block pkg_name %}
+libptytty
+{% endblock %}
+
+{% block version %}
+2.0
+{% endblock %}
+
 {% block fetch %}
-https://dist.schmorp.de/libptytty/libptytty-2.0.tar.gz
+https://dist.schmorp.de/libptytty/libptytty-{{self.version().strip()}}.tar.gz
 8033ed3aadf28759660d4f11f2d7b030acf2a6890cb0f7926fb0cfa6739d31f7
 {% endblock %}
 
