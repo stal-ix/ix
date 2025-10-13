0b9ae2ad4694d9db6282bc884926da1835178fae
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-10-13 13:22:55 +0300

    update gtkmm

--- pkgs/lib/gtk/4/mm/ix.sh
+++ pkgs/lib/gtk/4/mm/ix.sh
@@ -5,12 +5,12 @@ gtkmm
 {% endblock %}
 
 {% block version %}
-4.18.0
+4.20.0
 {% endblock %}
 
 {% block fetch %}
-https://download.gnome.org/sources/gtkmm/4.18/gtkmm-{{self.version().strip()}}.tar.xz
-2ee31c15479fc4d8e958b03c8b5fbbc8e17bc122c2a2f544497b4e05619e33ec
+https://download.gnome.org/sources/gtkmm/{{self.version().strip()[:4]}}/gtkmm-{{self.version().strip()}}.tar.xz
+daad9bf9b70f90975f91781fc7a656c923a91374261f576c883cd3aebd59c833
 {% endblock %}
 
 {% block lib_deps %}
