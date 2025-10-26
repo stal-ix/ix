22b915a11ac89bb40a8f9e29bd2a92babdfe75e7
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-10-26 19:52:21 +0300

    up 48.3 49.0 bin/gnome/text/editor/unwrap

--- pkgs/bin/gnome/text/editor/unwrap/ix.sh
+++ pkgs/bin/gnome/text/editor/unwrap/ix.sh
@@ -5,12 +5,12 @@ gnome-text-editor
 {% endblock %}
 
 {% block version %}
-48.3
+49.0
 {% endblock %}
 
 {% block fetch %}
 https://gitlab.gnome.org/GNOME/gnome-text-editor/-/archive/{{self.version().strip()}}/gnome-text-editor-{{self.version().strip()}}.tar.bz2
-cfd9453b11887fbcfd157a8f57e9eb7d8aca95768902b3e6c2a807d5ad826dad
+e9509a1cc5a6a10b616af285ee4eb29e045ff0439ca8bb0ee884645e74cb4da3
 {% endblock %}
 
 {% block bld_libs %}
