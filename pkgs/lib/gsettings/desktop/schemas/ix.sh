237eb4e82813c9cce320f56bcd0aff405203a135
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-10-26 22:08:39 +0300

    up 49.0 49.1 lib/gsettings/desktop/schemas

--- pkgs/lib/gsettings/desktop/schemas/ix.sh
+++ pkgs/lib/gsettings/desktop/schemas/ix.sh
@@ -5,12 +5,12 @@ gsettings-desktop-schemas
 {% endblock %}
 
 {% block version %}
-49.0
+49.1
 {% endblock %}
 
 {% block fetch %}
 https://github.com/GNOME/gsettings-desktop-schemas/archive/refs/tags/{{self.version().strip()}}.tar.gz
-eba76a4965f3be005b7c413b71bc8933e04c5a81dd37c936416281dd44bcff30
+c6baabe8c2c8b56825d0cfcc2ef859e5878a9de100be2894342640c094334517
 {% endblock %}
 
 {% block bld_tool %}
