596d4d897c5ad0ec76a57050628f6a6b8dcb0055
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-11-10 19:29:05 +0300

    up 1.10.2 1.10.3 lib/panel

--- pkgs/lib/panel/ix.sh
+++ pkgs/lib/panel/ix.sh
@@ -5,12 +5,12 @@ libpanel
 {% endblock %}
 
 {% block version %}
-1.10.2
+1.10.3
 {% endblock %}
 
 {% block fetch %}
 https://gitlab.gnome.org/GNOME/libpanel/-/archive/{{self.version().strip()}}/libpanel-{{self.version().strip()}}.tar.bz2
-cbc69348edc3385d36c9af2770c12f6214f6700b52957aa1e1665aee9662233d
+4445c8367e97a82b37e943db71ddd3f0204c073c423b94f980c95ca1bfa5d789
 {% endblock %}
 
 {% block bld_libs %}
