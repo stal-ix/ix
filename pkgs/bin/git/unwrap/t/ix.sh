61daf25589f9fd6106a5ccc299092767212b9e27
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-11-10 18:30:38 +0300

    up 2.51.1 2.51.2 bin/git/cred bin/git/unwrap

--- pkgs/bin/git/unwrap/t/ix.sh
+++ pkgs/bin/git/unwrap/t/ix.sh
@@ -5,10 +5,10 @@ git
 {% endblock %}
 
 {% block version %}
-2.51.1
+2.51.2
 {% endblock %}
 
 {% block fetch %}
 https://github.com/git/git/archive/refs/tags/v{{self.version().strip()}}.tar.gz
-586fc2c8ba466c1f2122d44047e5fb575a5c0a7620840d3ab5f6943fc281c8c4
+024e03605148682cf36fc7eade98405f4448e44532e1e65bd653c2ad2a59e0f7
 {% endblock %}
