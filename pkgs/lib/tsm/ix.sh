adfe01a23ac5e93ba58ade343f55417ebd7f427a
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-11-13 22:12:21 +0300

    upver libtsm

--- pkgs/lib/tsm/ix.sh
+++ pkgs/lib/tsm/ix.sh
@@ -1,16 +1,16 @@
-{% extends '//die/c/cmake.sh' %}
+{% extends '//die/c/meson.sh' %}
 
 {% block pkg_name %}
 libtsm
 {% endblock %}
 
 {% block version %}
-4.1.0
+4.2.0
 {% endblock %}
 
 {% block fetch %}
 https://github.com/Aetf/libtsm/archive/refs/tags/v{{self.version().strip()}}.tar.gz
-879a166a07aed63146ffe3e2afe803c0c3e22f264db0f1fa6fdb44687adeeef8
+1fa400d669a7b31df0e1953a193d648d2da7ebc674e606f10ae36db90c7f5337
 {% endblock %}
 
 {% block lib_deps %}
