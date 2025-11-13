c9548de3eebc34c63fed55565cd6de091ba7f7bf
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-11-13 17:35:02 +0300

    up 8.6 8.7 bin/nano

--- pkgs/bin/nano/ix.sh
+++ pkgs/bin/nano/ix.sh
@@ -5,12 +5,12 @@ nano
 {% endblock %}
 
 {% block version %}
-8.6
+8.7
 {% endblock %}
 
 {% block fetch %}
 https://www.nano-editor.org/dist/v8/nano-{{self.version().strip()}}.tar.xz
-f7abfbf0eed5f573ab51bd77a458f32d82f9859c55e9689f819d96fe1437a619
+afd287aa672c48b8e1a93fdb6c6588453d527510d966822b687f2835f0d986e9
 {% endblock %}
 
 {% block bld_libs %}
