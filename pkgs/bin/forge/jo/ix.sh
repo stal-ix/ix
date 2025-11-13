2e96677691169d8ff4bd35e3d390637a5301ac32
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-11-13 16:07:51 +0300

    upver forgejo

--- pkgs/bin/forge/jo/ix.sh
+++ pkgs/bin/forge/jo/ix.sh
@@ -5,7 +5,7 @@ forgejo
 {% endblock %}
 
 {% block version %}
-12.0.4
+13.0.2
 {% endblock %}
 
 {% block go_url %}
@@ -13,11 +13,11 @@ https://codeberg.org/forgejo/forgejo/archive/v{{self.version().strip()}}.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-45e9724c431a9a1ab7b19d78c9f47fd8351e56dacc89bab3824681e167aa673e
+33a805e09198ea5f918f5ba7b69941d70ec5703f62ad1dcee4525711d746824d
 {% endblock %}
 
 {% block go_tool %}
-bin/go/lang/24
+bin/go/lang/25
 {% endblock %}
 
 {% block go_bins %}
