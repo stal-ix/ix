834030524a5f7f63ef153b8555fd57806e3fe1df
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-11-15 21:23:20 +0300

    up 10.2.4 10.3.0 bin/dhcpcd bin/dhcpcd/sys

--- pkgs/bin/dhcpcd/ix.sh
+++ pkgs/bin/dhcpcd/ix.sh
@@ -5,12 +5,12 @@ dhcpcd
 {% endblock %}
 
 {% block version %}
-10.2.4
+10.3.0
 {% endblock %}
 
 {% block fetch %}
 https://github.com/NetworkConfiguration/dhcpcd/archive/refs/tags/v{{self.version().strip()}}.tar.gz
-85c8b2535ddf52091b30b31f29b0f9cafd9cd94dc6b78f1bd92db2afce4c1943
+1ed36ac4016a6b8a971ef9fb2b7167bc1af6d3b24ddfceee14ad0066e30dfae1
 {% endblock %}
 
 {% block bld_libs %}
