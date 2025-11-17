6fd8853f8313d7d98acc54c00b5106fd377eb1c0
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-11-17 04:03:55 +0300

    add ollama

---
+++ pkgs/bin/ollama/vulkan/driver/ix.sh
@@ -0,0 +1,26 @@
+{% extends '//die/c/cmake.sh' %}
+
+{% block fetch %}
+https://github.com/ollama/ollama/archive/refs/tags/v0.12.11.tar.gz
+710df5e218a29a2500ed973c81879514b5618df01687373a3467a05f63fe8002
+{% endblock %}
+
+{% block lib_deps %}
+lib/c
+lib/c++
+lib/vulkan/loader
+lib/vulkan/headers
+{% endblock %}
+
+{% block build_flags %}
+wrap_cc
+{% endblock %}
+
+{% block bld_tool %}
+bin/glslc
+{% endblock %}
+
+{% block install %}
+mkdir ${out}/lib
+cp ${tmp}/obj/lib/ollama/libggml-vulkan.so ${out}/lib/libggml-vulkan.a
+{% endblock %}
