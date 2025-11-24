93962067daa9dec5bf260fb7e8bd2f6807317377
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-11-24 03:31:47 +0300

    up 7.5.0 7.7.0 lib/simd/utf

--- pkgs/lib/simd/utf/ix.sh
+++ pkgs/lib/simd/utf/ix.sh
@@ -5,12 +5,12 @@ simdutf
 {% endblock %}
 
 {% block version %}
-7.5.0
+7.7.0
 {% endblock %}
 
 {% block fetch %}
 https://github.com/simdutf/simdutf/archive/refs/tags/v{{self.version().strip()}}.tar.gz
-3cad2f554912ecd77222272e5d1a7c1e5e33b4011bee823269cdc9095d2fdce2
+0180de81a1dd48a87b8c0442ffa81734f3db91a7350914107a449935124e3c6f
 {% endblock %}
 
 {% block lib_deps %}
