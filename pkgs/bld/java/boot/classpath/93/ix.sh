2ecc27e36b5a698ef4da361dd3a0685eb7949cd0
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-11-30 01:00:24 +0300

    refactor

--- pkgs/bld/java/boot/classpath/93/ix.sh
+++ pkgs/bld/java/boot/classpath/93/ix.sh
@@ -1,51 +1,25 @@
-{% extends '//die/c/autohell.sh' %}
+{% extends '//bld/java/boot/classpath/t/ix.sh' %}
 
 {% block fetch %}
 https://ftp.gnu.org/pub/gnu/classpath/classpath-0.93.tar.gz
 df2d093612abd23fe67e9409d89bb2a8e79b1664fe2b2da40e1c8ed693e32945
 {% endblock %}
 
-{% block bld_libs %}
-lib/c
-{% endblock %}
-
 {% block bld_tool %}
-bin/fastjar
+{{super()}}
 bld/java/boot/jikes
 {% endblock %}
 
-{% block configure_flags %}
---disable-Werror
---disable-gmp
---disable-gtk-peer
---disable-gconf-peer
---disable-plugin
---disable-dssi
---disable-alsa
---disable-gjdoc
-{% endblock %}
-
 {% block cpp_missing %}
+{{super()}}
 stdlib.h
 {% endblock %}
 
 {% block patch %}
+{{super()}}
 (base64 -d | patch -p1) << EOF
 {% include 'classpath-miscompilation.patch/base64' %}
 EOF
 sed -e 's|/tmp/|'${TMPDIR}'|' \
     -i scripts/check_jni_methods.sh
-{% endblock %}
-
-{% block postinstall %}
-mv ${out}/lib/classpath/*.a ${out}/lib/
-rm -rf ${out}/lib/classpath
-{% endblock %}
-
-{% block env %}
-export GNU_CLASSPATH=${out}
-{% endblock %}
-
-{% block c_rename_symbol %}
-fabsf
 {% endblock %}
--- pkgs/bld/java/boot/classpath/99/ix.sh
+++ pkgs/bld/java/boot/classpath/99/ix.sh
@@ -1,46 +1,23 @@
-{% extends '//die/c/autohell.sh' %}
+{% extends '//bld/java/boot/classpath/t/ix.sh' %}
 
 {% block fetch %}
 https://ftp.gnu.org/pub/gnu/classpath/classpath-0.99.tar.gz
 f929297f8ae9b613a1a167e231566861893260651d913ad9b6c11933895fecc8
 {% endblock %}
 
-{% block bld_libs %}
-lib/c
-{% endblock %}
-
 {% block bld_tool %}
 bld/perl
-bin/fastjar
+{{super()}}
 bld/java/boot/ecj/3/javac
 {% endblock %}
 
 {% block configure_flags %}
 --with-ecj-jar=${ECJ_JAR}
---disable-Werror
---disable-gmp
---disable-gtk-peer
---disable-gconf-peer
---disable-plugin
---disable-dssi
---disable-alsa
---disable-gjdoc
+{{super()}}
 {% endblock %}
 
 {% block patch %}
+{{super()}}
 sed -e 's|/tmp/|'${TMPDIR}'|' \
     -i scripts/check_jni_methods.sh.in
-{% endblock %}
-
-{% block postinstall %}
-mv ${out}/lib/classpath/*.a ${out}/lib/
-rm -rf ${out}/lib/classpath
-{% endblock %}
-
-{% block env %}
-export GNU_CLASSPATH=${out}
-{% endblock %}
-
-{% block c_rename_symbol %}
-fabsf
 {% endblock %}
