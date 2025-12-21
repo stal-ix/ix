4bb1c1e3915e674452c64da937f0af3a24335514
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-12-22 00:10:11 +0300

    fix build

--- pkgs/die/c/ix0.sh
+++ pkgs/die/c/ix0.sh
@@ -3,6 +3,9 @@
 {% block std_box %}
   {% if 'wrap_cc' in build_flags %}
     bld/wrap/cc
+    {% if 'wrap_rdynamic' in build_flags %}
+      bld/wrap/cc/plugins/rdynamic
+    {% endif %}
   {% endif %}
   {{super()}}
 {% endblock %}
