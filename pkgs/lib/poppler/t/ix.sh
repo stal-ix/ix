d0c82fef0ffa762e937ed9a3339d47a92f11d98a
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-12-28 02:17:14 +0300

    up 25.11.0 25.12.0 bin/poppler lib/poppler

--- pkgs/lib/poppler/t/ix.sh
+++ pkgs/lib/poppler/t/ix.sh
@@ -5,12 +5,12 @@ poppler
 {% endblock %}
 
 {% block version %}
-25.11.0
+25.12.0
 {% endblock %}
 
 {% block fetch %}
 https://poppler.freedesktop.org/poppler-{{self.version().strip()}}.tar.xz
-63f155142b77349e2bccaef148e754e7506ab1641e713b83af4f54a8f8b15369
+c18b40eb36b1a0c5b86e29ca054bf0770304583da4f2cdd42fe86eca6a20de48
 {% endblock %}
 
 {% block lib_deps %}
