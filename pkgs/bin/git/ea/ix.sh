aef5c82b4a24c1ffe2f3506f3ae40dc7e250f170
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-12-27 21:35:25 +0300

    up 1.25.1 1.25.3 bin/git/ea

--- pkgs/bin/git/ea/ix.sh
+++ pkgs/bin/git/ea/ix.sh
@@ -5,7 +5,7 @@ gitea
 {% endblock %}
 
 {% block version %}
-1.25.1
+1.25.3
 {% endblock %}
 
 {% block go_url %}
@@ -13,7 +13,7 @@ https://github.com/go-gitea/gitea/archive/refs/tags/v{{self.version().strip()}}.
 {% endblock %}
 
 {% block go_sha %}
-71443f70c4b6e3bb7153f5953945543453f039b1d61e60718f99d002d10cb99f
+f2f0affed50433f2f749675affbe17ad6645c0a831611801d39d86500decb356
 {% endblock %}
 
 {% block go_tool %}
