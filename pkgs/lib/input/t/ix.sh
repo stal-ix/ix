4b57e9bcad0ddcbace75c6a676757139f3299cf4
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-12-28 00:28:35 +0300

    up 1.29.2 1.30.1 bin/input/tools lib/input/data lib/input

--- pkgs/lib/input/t/ix.sh
+++ pkgs/lib/input/t/ix.sh
@@ -5,12 +5,12 @@ libinput
 {% endblock %}
 
 {% block version %}
-1.29.2
+1.30.1
 {% endblock %}
 
 {% block fetch %}
 https://gitlab.freedesktop.org/libinput/libinput/-/archive/{{self.version().strip()}}/libinput-{{self.version().strip()}}.tar.bz2
-0429db1121ea514eda80038ca74d9ac866c32596e9eff14b2d9453a696e947d1
+c5169568f337c4a3bda927c42afef3a9d4c891e48a405cba39d57cb4631b9fb0
 {% endblock %}
 
 {% block bld_libs %}
