cad3806869533cf86d4180ec04fbc15eeb2d5059
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-12-27 23:55:19 +0300

    up 2.5.0 2.6.0 bin/keyd

--- pkgs/bin/keyd/ix.sh
+++ pkgs/bin/keyd/ix.sh
@@ -5,12 +5,12 @@ keyd
 {% endblock %}
 
 {% block version %}
-2.5.0
+2.6.0
 {% endblock %}
 
 {% block fetch %}
 https://github.com/rvaiya/keyd/archive/refs/tags/v{{self.version().strip()}}.tar.gz
-93ec6c153ef673a7a8b4d8b686494dee11d182513f4531c71dce15a8db7f6c1c
+697089681915b89d9e98caf93d870dbd4abce768af8a647d54650a6a90744e26
 {% endblock %}
 
 {% block bld_libs %}
