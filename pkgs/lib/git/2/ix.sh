b475e0b2f9e1bf1c660d6dfb2824f70f1d42dfe6
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-12-28 00:27:11 +0300

    up 1.9.1 1.9.2 bin/git/2 lib/git/2

--- pkgs/lib/git/2/ix.sh
+++ pkgs/lib/git/2/ix.sh
@@ -5,12 +5,12 @@ libgit2
 {% endblock %}
 
 {% block version %}
-1.9.1
+1.9.2
 {% endblock %}
 
 {% block fetch %}
 https://github.com/libgit2/libgit2/archive/refs/tags/v{{self.version().strip()}}.tar.gz
-14cab3014b2b7ad75970ff4548e83615f74d719afe00aa479b4a889c1e13fc00
+6f097c82fc06ece4f40539fb17e9d41baf1a5a2fc26b1b8562d21b89bc355fe6
 {% endblock %}
 
 {% block lib_deps %}
