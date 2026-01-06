3040b43713efe345570aa452210dd42c29557ace
Anton Samokhvalov <anton@samokhvalov.xyz>
2026-01-07 02:07:54 +0300

    fix

--- pkgs/bld/wrap/cc/plugins/logcmd/logcmd.py
+++ pkgs/bld/wrap/cc/plugins/logcmd/logcmd.py
@@ -2,12 +2,16 @@
 
 import os
 import sys
+import json
 import hashlib
+
+if os.environ.get('IX_STEP', '') == 'configure':
+    sys.exit(0)
 
 data = sys.stdin.buffer.read()
 
-if b'output.exe' in data:
-    sys.exit(0)
+cmd = json.loads(data)['cmd']
 
-with open(os.environ['tmp'] + '/linkcmd_' + hashlib.md5(data).hexdigest(), 'wb') as f:
-    f.write(data)
+if '-o' in cmd:
+    with open(os.environ['tmp'] + '/linkcmd_' + hashlib.md5(data).hexdigest(), 'wb') as f:
+        f.write(data)
