4ed60bafb74961d8bae64ea5158f5e9f57ec4231
Anton Samokhvalov <anton@samokhvalov.xyz>
2026-04-18 21:13:23 +0300

    fix

--- pkgs/bin/gorn/ix.sh
+++ pkgs/bin/gorn/ix.sh
@@ -5,11 +5,11 @@ bin/go/lang/22
 {% endblock %}
 
 {% block go_url %}
-https://github.com/pg83/gorn/archive/refs/tags/1.tar.gz
+https://github.com/pg83/gorn/archive/refs/tags/2.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-088c4d3643fb994a31b2fef981ed6a1dde41139a026fec42e1d75890fb145316
+c5c361db38edd531e1e9bc460f2e4c386fc27e1bb3f5cb6e946c48f3a05ee1f2
 {% endblock %}
 
 {% block go_bins %}
