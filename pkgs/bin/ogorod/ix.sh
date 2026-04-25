428750e82730b5490a5a3c11122e8eabcb4deadd
Anton Samokhvalov <anton@samokhvalov.xyz>
2026-04-25 21:26:10 +0300

    bin/ogorod: bump to e18fb12 (GIT_EXEC_PATH multi-entry)

--- pkgs/bin/ogorod/ix.sh
+++ pkgs/bin/ogorod/ix.sh
@@ -5,11 +5,11 @@ bin/go/lang/25
 {% endblock %}
 
 {% block go_url %}
-https://github.com/pg83/ogorod/archive/ed0838cf0caeb048782c9a81ad4b998992cf5039.tar.gz
+https://github.com/pg83/ogorod/archive/e18fb1264baf893845545bdbdf047960e578d134.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-b982f29785a79d71ae3add5abe52de5b8c5386c61276088cbf56f7ca5249b10a
+37f1cef2d182e6abd32db85f11f40e4a7fadef11ca6feaee7d529f8fcc07d175
 {% endblock %}
 
 {% block go_bins %}
