259e16f3de3b75e5c4c1608473f75b3f02d43d00
Anton Samokhvalov <anton@samokhvalov.xyz>
2026-04-25 21:10:40 +0300

    bin/ogorod: bump to ed0838c (thin debug logs)

--- pkgs/bin/ogorod/ix.sh
+++ pkgs/bin/ogorod/ix.sh
@@ -5,11 +5,11 @@ bin/go/lang/25
 {% endblock %}
 
 {% block go_url %}
-https://github.com/pg83/ogorod/archive/9d715cb9be81d0c8825de5ad120bd9848b5c003d.tar.gz
+https://github.com/pg83/ogorod/archive/ed0838cf0caeb048782c9a81ad4b998992cf5039.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-6008a631dbde6e7976b1078ae10517e6ff82ba8f202f1c65a30ac2e7af53ca79
+b982f29785a79d71ae3add5abe52de5b8c5386c61276088cbf56f7ca5249b10a
 {% endblock %}
 
 {% block go_bins %}
