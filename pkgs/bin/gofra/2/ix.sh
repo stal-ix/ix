9296870934db25098ee5eae71509e242cd541573
Anton Samokhvalov <anton@samokhvalov.xyz>
2026-04-29 01:48:50 +0300

    bin/gofra/2: bump to 0470065 (UDP_GRO on RX)

--- pkgs/bin/gofra/2/ix.sh
+++ pkgs/bin/gofra/2/ix.sh
@@ -5,8 +5,8 @@ gofra
 {% endblock %}
 
 {% block fetch %}
-https://github.com/pg83/gofra/archive/9ead179d8ae30bfcc1045e78d6b65a8a137de4b3.tar.gz
-fd2679dec942aaa01d1a88b103a0f9541f367772bbe93071709ee2395cef3b99
+https://github.com/pg83/gofra/archive/0470065f10995bd8e9d6a002a105f061eaff1e3f.tar.gz
+769264a9b7d02cce105875afe67c4c4c8bd81a45c1842ab0a88420c94dc4b8d0
 {% endblock %}
 
 {% block bld_libs %}
