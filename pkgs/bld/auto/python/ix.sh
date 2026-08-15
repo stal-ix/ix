e3858e0db3b29ced11c84a1225c613e2322965a1
Anton Samokhvalov <anton@samokhvalov.xyz>
2026-08-15 03:14:41 +0000

    build Python autoconf set with system tools

--- pkgs/bld/auto/python/ix.sh
+++ pkgs/bld/auto/python/ix.sh
@@ -1,7 +1,9 @@
 {% extends '//die/hub.sh' %}
 
 {% block run_deps %}
-{% if all_system or system_auto_python %}
+{% if all_system %}
+bld/auto(conf_ver=2/71,std_box=bld/system/box,libc_lite=1)
+{% elif system_auto_python %}
 bld/system
 {% elif native %}
 bld/auto(conf_ver=2/71,std_box=bld/boot/box)
