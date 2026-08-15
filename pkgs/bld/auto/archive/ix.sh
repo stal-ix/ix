1c4b431531ddf9853a4674bc3db170cac4e58f2e
Anton Samokhvalov <anton@samokhvalov.xyz>
2026-08-15 01:10:51 +0000

    build autoconf archive with system tools

--- pkgs/bld/auto/archive/ix.sh
+++ pkgs/bld/auto/archive/ix.sh
@@ -1,7 +1,9 @@
 {% extends '//die/hub.sh' %}
 
 {% block run_deps %}
-{% if all_system or system_auto_archive %}
+{% if all_system %}
+bin/auto/conf/archive(std_box=bld/system/box,libc_lite=1)
+{% elif system_auto_archive %}
 bld/system
 {% elif native %}
 bin/auto/conf/archive(std_box=bld/boot/box)
