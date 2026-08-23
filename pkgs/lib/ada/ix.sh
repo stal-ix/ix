e25169ad4c76dc4b4c11814878de3b393ceee692
ix updater <ix-updater@users.noreply.github.com>
2026-08-23 12:48:15 +0000

    up 3.4.2 4.0.0 lib/ada

--- pkgs/lib/ada/ix.sh
+++ pkgs/lib/ada/ix.sh
@@ -5,12 +5,12 @@ ada
 {% endblock %}
 
 {% block version %}
-3.4.2
+4.0.0
 {% endblock %}
 
 {% block fetch %}
 https://github.com/ada-url/ada/archive/refs/tags/v{{self.version().strip()}}.tar.gz
-3aceb6028eb0787ea77c8f3035a5aaa15108ab11d0fe24f23fe850cf94816523
+6d6c7ef7dd2e329320d34eb2ab29ccdc879ee3935af9dfb894a6640e58dc381d
 {% endblock %}
 
 {% block lib_deps %}
