c6ad6e9174103b22548e73a705b548827af55820
ix updater <ix-updater@users.noreply.github.com>
2026-08-23 22:17:11 +0000

    up 0.68.0 0.74.3 bin/fzf

--- pkgs/bin/fzf/ix.sh
+++ pkgs/bin/fzf/ix.sh
@@ -5,7 +5,7 @@ fzf
 {% endblock %}
 
 {% block version %}
-0.68.0
+0.74.3
 {% endblock %}
 
 {% block go_url %}
@@ -13,7 +13,7 @@ https://github.com/junegunn/fzf/archive/refs/tags/v{{self.version().strip()}}.ta
 {% endblock %}
 
 {% block go_sha %}
-cc052a63e774bac1362becd42f0cfc14b8c93d6fc5785a52033cec3555f1ff1f
+b110bfe7b8d9da89bcf085b68251c44d50201ff9f169842ef6d110f6ab24d201
 {% endblock %}
 
 {% block go_bins %}
