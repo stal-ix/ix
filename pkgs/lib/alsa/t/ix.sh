944482b4efba7b2b4f0d3e1a6f667590fe38be6e
ix fixer <ix-fixer@users.noreply.github.com>
2026-08-23 16:15:18 +0000

    fix CI after c03d50dec474

--- pkgs/lib/alsa/t/ix.sh
+++ pkgs/lib/alsa/t/ix.sh
@@ -8,6 +8,8 @@ alsa-lib
 1.2.16.1
 {% endblock %}
 
+{% block conf_ver %}2/72{% endblock %}
+
 {% block fetch %}
 https://www.alsa-project.org/files/pub/lib/alsa-lib-{{self.version().strip()}}.tar.bz2
 f740db7f488255944ffd4428416ee3390a96742856916433df468c281436480e
