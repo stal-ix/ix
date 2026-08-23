8f7d0b8ae79fd9ce8e32e7e678a648fa19ebf654
ix fixer <ix-fixer@users.noreply.github.com>
2026-08-23 15:51:36 +0000

    fix CI after 4ce61aa24b6f

--- pkgs/lib/abseil/cpp/ix.sh
+++ pkgs/lib/abseil/cpp/ix.sh
@@ -1,5 +1,7 @@
 {% extends '//die/c/cmake.sh' %}
 
+# noauto
+
 # check bin/telegram/desktop
 
 {% block pkg_name %}
@@ -7,12 +9,12 @@ abseil-cpp
 {% endblock %}
 
 {% block version %}
-20260817.0
+20250512.1
 {% endblock %}
 
 {% block fetch %}
 https://github.com/abseil/abseil-cpp/archive/refs/tags/{{self.version().strip()}}.tar.gz
-f7e05179df39c45434cad433f5783840bb3788ef322976f9138bc6b72b3a107d
+9b7a064305e9fd94d124ffa6cc358592eb42b5da588fb4e07d09254aa40086db
 {% endblock %}
 
 {% block lib_deps %}
