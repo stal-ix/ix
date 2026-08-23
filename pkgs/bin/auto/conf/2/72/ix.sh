d056f3fddfe3d015de7cb8025050e41ddb1e1462
ix updater <ix-updater@users.noreply.github.com>
2026-08-23 13:16:43 +0000

    up 2.72 2.73 bin/auto/conf/2/72

--- pkgs/bin/auto/conf/2/72/ix.sh
+++ pkgs/bin/auto/conf/2/72/ix.sh
@@ -5,10 +5,10 @@ autoconf
 {% endblock %}
 
 {% block version %}
-2.72
+2.73
 {% endblock %}
 
 {% block fetch %}
 https://ftp.gnu.org/gnu/autoconf/autoconf-{{self.version().strip()}}.tar.xz
-ba885c1319578d6c94d46e9b0dceb4014caafe2490e437a0dbca3f270a223f5a
+9fd672b1c8425fac2fa67fa0477b990987268b90ff36d5f016dae57be0d6b52e
 {% endblock %}
