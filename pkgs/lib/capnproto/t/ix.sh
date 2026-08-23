9944982468f761014a25fb993865024200eb5ce7
ix updater <ix-updater@users.noreply.github.com>
2026-08-23 20:51:57 +0000

    up 1.3.0 1.5.0 bin/capnc

--- pkgs/lib/capnproto/t/ix.sh
+++ pkgs/lib/capnproto/t/ix.sh
@@ -5,12 +5,12 @@ capnproto
 {% endblock %}
 
 {% block version %}
-1.3.0
+1.5.0
 {% endblock %}
 
 {% block fetch %}
 https://github.com/capnproto/capnproto/archive/refs/tags/v{{self.version().strip()}}.tar.gz
-01ab2ba7f52fcc3c51a10e22935aae56f3bc5e99b726b7e507fe6700cb12147d
+d5ebdf858e9885c33d4b3f765006d68bd66e9b002bf4d607ff4317ef9c1aac6a
 {% endblock %}
 
 {% block lib_deps %}
