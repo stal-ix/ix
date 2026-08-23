cf1c7c0a6f59110bce6124a6aa7d8b9d84772c85
ix updater <ix-updater@users.noreply.github.com>
2026-08-23 21:48:24 +0000

    up 14.0.2 16.0.3 bin/forge/jo

--- pkgs/bin/forge/jo/ix.sh
+++ pkgs/bin/forge/jo/ix.sh
@@ -5,7 +5,7 @@ forgejo
 {% endblock %}
 
 {% block version %}
-14.0.2
+16.0.3
 {% endblock %}
 
 {% block go_url %}
@@ -13,7 +13,7 @@ https://codeberg.org/forgejo/forgejo/archive/v{{self.version().strip()}}.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-4018e4f1a8b067696f742adda6080aa8a0b8c2d78ab7149bc6dc48b66093d6ac
+b110bfe7b8d9da89bcf085b68251c44d50201ff9f169842ef6d110f6ab24d201
 {% endblock %}
 
 {% block go_tool %}
