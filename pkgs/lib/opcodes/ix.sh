934fb4d5222c362ec7807cbcf37dfb25a934deea
ix fixer <ix-fixer@users.noreply.github.com>
2026-08-23 19:11:16 +0000

    fix CI after dab9f2bc1674

--- pkgs/lib/opcodes/ix.sh
+++ pkgs/lib/opcodes/ix.sh
@@ -24,4 +24,5 @@ lib/c
 {% block patch %}
 echo 0 > ../bfd/libtool-soversion
 sed -i '/assert (opcode->bin_opcode == bin);/a\      (void) bin;' avr-dis.c
+sed -i '/unsigned int bin = 0;/a\          (void) bin;' xgate-dis.c
 {% endblock %}
