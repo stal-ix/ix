07abdc2fdc65ae24f1a2797b1a9c84f5087a6b00
ix updater <ix-updater@users.noreply.github.com>
2026-08-23 13:14:57 +0000

    up 2.58.3 2.60.6 lib/atk

--- pkgs/lib/atk/ix.sh
+++ pkgs/lib/atk/ix.sh
@@ -5,12 +5,12 @@ at-spi2-core
 {% endblock %}
 
 {% block version %}
-2.58.3
+2.60.6
 {% endblock %}
 
 {% block fetch %}
 https://github.com/GNOME/at-spi2-core/archive/refs/tags/{{self.version().strip()}}.tar.gz
-9103162c4d3367c202585a907b072e4b08c6f5c3d3eec75504392c7dc5a5206b
+3ad6bcc25b884ac154aa8c12e2d907d04b514d47d06e8a07ec18ffcf1c98dc26
 {% endblock %}
 
 {% block lib_deps %}
