b6267f367053302ecf96ce48d32c2a81c5fff22c
ix updater <ix-updater@users.noreply.github.com>
2026-08-24 07:43:21 +0000

    up 0.27.3 0.28.2 bin/esbuild

--- pkgs/bin/esbuild/ix.sh
+++ pkgs/bin/esbuild/ix.sh
@@ -5,7 +5,7 @@ esbuild
 {% endblock %}
 
 {% block version %}
-0.27.3
+0.28.2
 {% endblock %}
 
 {% block go_url %}
@@ -13,7 +13,7 @@ https://github.com/evanw/esbuild/archive/refs/tags/v{{self.version().strip()}}.t
 {% endblock %}
 
 {% block go_sha %}
-66f3a7b811b8b6568547b98bc37218224aa2c14ad5b2e2999edda9a9fef40740
+b110bfe7b8d9da89bcf085b68251c44d50201ff9f169842ef6d110f6ab24d201
 {% endblock %}
 
 {% block unpack %}
