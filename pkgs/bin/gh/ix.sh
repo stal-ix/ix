c91fbec2f37fbf9d04f4f9b7cc5bd44f0ed209af
ix updater <ix-updater@users.noreply.github.com>
2026-08-24 03:04:05 +0000

    up 2.83.2 2.98.0 bin/gh

--- pkgs/bin/gh/ix.sh
+++ pkgs/bin/gh/ix.sh
@@ -5,7 +5,7 @@ cli
 {% endblock %}
 
 {% block version %}
-2.83.2
+2.98.0
 {% endblock %}
 
 {% block go_url %}
@@ -13,7 +13,7 @@ https://github.com/cli/cli/archive/refs/tags/v{{self.version().strip()}}.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-2a584a8d82aa65bff0976b69c4cfa337079e2d8048660ecd2d12ccf47a51c7c1
+b110bfe7b8d9da89bcf085b68251c44d50201ff9f169842ef6d110f6ab24d201
 {% endblock %}
 
 {% block unpack %}
