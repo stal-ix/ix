eb3b2298d585633efd3a7b174327ba58c25a6340
ix updater <ix-updater@users.noreply.github.com>
2026-08-24 12:40:09 +0000

    up 2.18 2.18.1 lib/gperftools/17

--- pkgs/lib/gperftools/17/ix.sh
+++ pkgs/lib/gperftools/17/ix.sh
@@ -1,12 +1,12 @@
 {% extends '//lib/gperftools/16/ix.sh' %}
 
 {% block version %}
-2.18
+2.18.1
 {% endblock %}
 
 {% block fetch %}
 https://github.com/gperftools/gperftools/releases/download/gperftools-{{self.version().strip()}}/gperftools-{{self.version().strip()}}.tar.gz
-a64c8873b63ebf631a5fc05af7f81f3ddf550c3bde37245e10311c7ae7a0c718
+d18d919175f9e4d740ace6b52f0f4f91284160c454e91b36ffd6456282a02206
 {% endblock %}
 
 {% block lib_deps %}
