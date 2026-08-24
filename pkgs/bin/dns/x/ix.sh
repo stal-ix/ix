a71063ad40f11e0b3efc4e99e8a7b3da9a6c0cf7
ix updater <ix-updater@users.noreply.github.com>
2026-08-24 07:21:03 +0000

    up 1.2.3 1.3.0 bin/dns/x

--- pkgs/bin/dns/x/ix.sh
+++ pkgs/bin/dns/x/ix.sh
@@ -5,7 +5,7 @@ dnsx
 {% endblock %}
 
 {% block version %}
-1.2.3
+1.3.0
 {% endblock %}
 
 {% block go_url %}
@@ -13,7 +13,7 @@ https://github.com/projectdiscovery/dnsx/archive/refs/tags/v{{self.version().str
 {% endblock %}
 
 {% block go_sha %}
-51a49a0578367be0a93f287836674d87b655969a619b5e7af43143f87652f138
+b110bfe7b8d9da89bcf085b68251c44d50201ff9f169842ef6d110f6ab24d201
 {% endblock %}
 
 {% block unpack %}
