0b0fdff6a41559993912fff1e67e492ca4e6239a
ix updater <ix-updater@users.noreply.github.com>
2026-08-24 00:54:58 +0000

    up 2.3.0 2.3.1 lib/gflags

--- pkgs/lib/gflags/ix.sh
+++ pkgs/lib/gflags/ix.sh
@@ -5,12 +5,12 @@ gflags
 {% endblock %}
 
 {% block version %}
-2.3.0
+2.3.1
 {% endblock %}
 
 {% block fetch %}
 https://github.com/gflags/gflags/archive/refs/tags/v{{self.version().strip()}}.tar.gz
-f619a51371f41c0ad6837b2a98af9d4643b3371015d873887f7e8d3237320b2f
+1b5e0648d7b94021895086e65479b4eaca7935eecfffd7dd9512eb576181c53d
 {% endblock %}
 
 {% block lib_deps %}
