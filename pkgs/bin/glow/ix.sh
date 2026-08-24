1af43c45b2ac6e18ff759cbcb6398df67ce2d2bf
ix updater <ix-updater@users.noreply.github.com>
2026-08-24 04:52:14 +0000

    up 2.1.1 3.0.0 bin/glow

--- pkgs/bin/glow/ix.sh
+++ pkgs/bin/glow/ix.sh
@@ -5,7 +5,7 @@ glow
 {% endblock %}
 
 {% block version %}
-2.1.1
+3.0.0
 {% endblock %}
 
 {% block go_url %}
@@ -13,7 +13,7 @@ https://github.com/charmbracelet/glow/archive/refs/tags/v{{self.version().strip(
 {% endblock %}
 
 {% block go_sha %}
-1c95500e7f1313f684d0af1ddb53a7616e12b264cdcf5b498c53b5c1cd1d991d
+b110bfe7b8d9da89bcf085b68251c44d50201ff9f169842ef6d110f6ab24d201
 {% endblock %}
 
 {% block go_bins %}
