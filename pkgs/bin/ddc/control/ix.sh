7caa051e8bc14240691933c9ee592fce6d5ee3ee
ix updater <ix-updater@users.noreply.github.com>
2026-08-24 07:14:58 +0000

    up 1.0.3 3.2.0 bin/ddc/control

--- pkgs/bin/ddc/control/ix.sh
+++ pkgs/bin/ddc/control/ix.sh
@@ -5,12 +5,12 @@ ddccontrol
 {% endblock %}
 
 {% block version %}
-1.0.3
+3.2.0
 {% endblock %}
 
 {% block fetch %}
 https://github.com/ddccontrol/ddccontrol/archive/refs/tags/{{self.version().strip()}}.tar.gz
-4f3a3d9a00e09b07423d2aed308b21dccfe57642f5d9bbf79802a0656dd11d1e
+2565aa907cc891554bf01878444326a97cd53a7b91dc7207b1dbddc87bca1331
 {% endblock %}
 
 {% block bld_libs %}
