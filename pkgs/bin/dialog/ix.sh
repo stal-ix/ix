dee0653b736a27d08ef14b91cdbfc07d3a108fc0
ix updater <ix-updater@users.noreply.github.com>
2026-08-24 07:17:33 +0000

    up 1.3.20260107 1.3_20260721 bin/dialog

--- pkgs/bin/dialog/ix.sh
+++ pkgs/bin/dialog/ix.sh
@@ -5,12 +5,12 @@ dialog
 {% endblock %}
 
 {% block version %}
-1.3.20260107
+1.3_20260721
 {% endblock %}
 
 {% block fetch %}
 https://invisible-island.net/archives/dialog/dialog-{{self.version().strip()[:3]}}-{{self.version().strip()[4:]}}.tgz
-78b3dd18d95e50f0be8f9b9c1e7cffe28c9bf1cdf20d5b3ef17279c4da35c5b5
+62bdf59057d4f760a1cc2217827f07887b4a3eebf694c25eacd4803d2171cdc6
 {% endblock %}
 
 {% block bld_libs %}
