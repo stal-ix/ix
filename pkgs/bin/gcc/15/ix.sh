f50ab5419cfadda146ded0a11a8047ee33e19fe9
ix updater <ix-updater@users.noreply.github.com>
2026-08-25 19:21:34 +0000

    up 15.2.0 16.2.0 bin/gcc/15

--- pkgs/bin/gcc/15/ix.sh
+++ pkgs/bin/gcc/15/ix.sh
@@ -5,10 +5,10 @@ gcc
 {% endblock %}
 
 {% block version %}
-15.2.0
+16.2.0
 {% endblock %}
 
 {% block fetch %}
 https://ftp.fu-berlin.de/unix/languages/gcc/releases/gcc-{{self.version().strip()}}/gcc-{{self.version().strip()}}.tar.xz
-438fd996826b0c82485a29da03a72d71d6e3541a83ec702df4271f6fe025d24e
+e6738e29597f733270731aa90600f37ffdc045079dfc27ec7e8192cc81085c3e
 {% endblock %}
