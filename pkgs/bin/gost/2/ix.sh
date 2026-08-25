32014107efbdad1ee0cd3e2bd48b04109adceeba
ix fixer <ix-fixer@users.noreply.github.com>
2026-08-25 14:56:08 +0000

    fix CI after e3fc0f3c9728

--- pkgs/bin/gost/2/ix.sh
+++ pkgs/bin/gost/2/ix.sh
@@ -1,11 +1,13 @@
 {% extends '//die/go/build.sh' %}
 
+# noauto
+
 {% block pkg_name %}
 gost
 {% endblock %}
 
 {% block version %}
-3.2.6
+2.12.0
 {% endblock %}
 
 {% block go_url %}
@@ -13,11 +15,11 @@ https://github.com/ginuerzh/gost/archive/refs/tags/v{{self.version().strip()}}.t
 {% endblock %}
 
 {% block go_sha %}
-b110bfe7b8d9da89bcf085b68251c44d50201ff9f169842ef6d110f6ab24d201
+2307cf2ea61eccf2c90f1201d9fa68048915d27c15963802e8fd304fce6dcea8
 {% endblock %}
 
 {% block go_tool %}
-bin/go/lang/25
+bin/go/lang/22
 {% endblock %}
 
 {% block step_unpack %}
