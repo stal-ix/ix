e3fc0f3c972875a373e09441be278a098fa56e00
ix fixer <ix-fixer@users.noreply.github.com>
2026-08-25 14:52:28 +0000

    fix CI after 7e56f5d2921f

--- pkgs/bin/dns/proxy/ix.sh
+++ pkgs/bin/dns/proxy/ix.sh
@@ -17,7 +17,12 @@ b110bfe7b8d9da89bcf085b68251c44d50201ff9f169842ef6d110f6ab24d201
 {% endblock %}
 
 {% block go_tool %}
-bin/go/lang/25
+bin/go/lang/26
+{% endblock %}
+
+{% block go_build_flags %}
+{{super()}}
+-o dnsproxy
 {% endblock %}
 
 {% block go_bins %}
