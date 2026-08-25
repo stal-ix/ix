bc3308bb509bdd899e30897555413641eeea6d85
ix fixer <ix-fixer@users.noreply.github.com>
2026-08-25 21:20:16 +0000

    bin/geesefs: correct vendored source checksum

--- pkgs/bin/geesefs/ix.sh
+++ pkgs/bin/geesefs/ix.sh
@@ -13,7 +13,7 @@ https://github.com/yandex-cloud/geesefs/archive/refs/tags/v{{self.version().stri
 {% endblock %}
 
 {% block go_sha %}
-66383e8a6162e389037135482e93ebe6d04fb0451f98e081d87b089c94fb7ec0
+4d948a5983fedd84d6d4303d375c459d870f9919f794bbd8402b57a3e6be33d7
 {% endblock %}
 
 {% block go_bins %}
