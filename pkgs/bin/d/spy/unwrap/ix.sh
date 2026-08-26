9b25c01093d0d17a1773831fb2c10c70e16f7b84
ix updater <ix-updater@users.noreply.github.com>
2026-08-26 14:52:56 +0000

    up 49.2 50.0 bin/d/spy/unwrap

--- pkgs/bin/d/spy/unwrap/ix.sh
+++ pkgs/bin/d/spy/unwrap/ix.sh
@@ -5,12 +5,12 @@ d-spy
 {% endblock %}
 
 {% block version %}
-49.2
+50.0
 {% endblock %}
 
 {% block fetch %}
 https://gitlab.gnome.org/GNOME/d-spy/-/archive/{{self.version().strip()}}/d-spy-{{self.version().strip()}}.tar.bz2
-ddff71ff69bc5f5751fd596d75a89cf84932a5f220d2f0ef52f176e92849c5ac
+eaab4c62ee48f83c1779f9e55a47285e0599020e58a0361c615e27513171219e
 {% endblock %}
 
 {% block bld_libs %}
