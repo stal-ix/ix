c3233c90acf008839b1402ab00eddc90f426b1e9
ix fixer <ix-fixer@users.noreply.github.com>
2026-08-27 23:39:43 +0000

    fix hyprpicker build with libc++ 19

--- pkgs/bin/hypr/picker/ix.sh
+++ pkgs/bin/hypr/picker/ix.sh
@@ -11,6 +11,12 @@ hyprpicker
 {% block fetch %}
 https://github.com/hyprwm/hyprpicker/archive/refs/tags/v{{self.version().strip()}}.tar.gz
 cde02b92abe372187c9d59a7cb2ca07ede5c3e1a8c81ed8b209dc0d3ac5f0339
+https://github.com/hyprwm/hyprpicker/commit/d6115cad31aab3d537039a817b0590d83833d2a2.diff
+ab9afa0317bbeacc6b4ff49f5a3ebf05d06b632ef5e9e72fa7823bb442578083
+{% endblock %}
+
+{% block patch %}
+patch -p1 < ${src}/d6115cad31aab3d537039a817b0590d83833d2a2.diff
 {% endblock %}
 
 {% block bld_libs %}
