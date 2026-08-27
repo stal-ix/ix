692905c099a7958e0493e9d0c8c43e97a513620e
ix updater <ix-updater@users.noreply.github.com>
2026-08-27 18:57:40 +0000

    up 49.1 50.1 lib/gsettings/desktop/schemas

--- pkgs/lib/gsettings/desktop/schemas/ix.sh
+++ pkgs/lib/gsettings/desktop/schemas/ix.sh
@@ -5,12 +5,12 @@ gsettings-desktop-schemas
 {% endblock %}
 
 {% block version %}
-49.1
+50.1
 {% endblock %}
 
 {% block fetch %}
 https://github.com/GNOME/gsettings-desktop-schemas/archive/refs/tags/{{self.version().strip()}}.tar.gz
-c6baabe8c2c8b56825d0cfcc2ef859e5878a9de100be2894342640c094334517
+ac9328c00ca18e5ac921bb2c13048b1ce5b43302b0c8d89d7fec22b05e22ec7a
 {% endblock %}
 
 {% block bld_tool %}
