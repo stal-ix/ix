934897b163fed84dc866154bf977f7e3563c873f
ix updater <ix-updater@users.noreply.github.com>
2026-08-27 21:20:17 +0000

    up 1.7.2 1.7.3 bin/hunspell lib/hunspell

--- pkgs/lib/hunspell/ix.sh
+++ pkgs/lib/hunspell/ix.sh
@@ -5,12 +5,12 @@ hunspell
 {% endblock %}
 
 {% block version %}
-1.7.2
+1.7.3
 {% endblock %}
 
 {% block fetch %}
 https://github.com/hunspell/hunspell/archive/refs/tags/v{{self.version().strip()}}.tar.gz
-69fa312d3586c988789266eaf7ffc9861d9f6396c31fc930a014d551b59bbd6e
+933be3dac6fd55f6e752331a170efb7e33800e40fae1156d8434cc8c85379a1b
 {% endblock %}
 
 {% block lib_deps %}
