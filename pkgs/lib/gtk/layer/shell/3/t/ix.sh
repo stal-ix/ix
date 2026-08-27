198618b319ceefa5e8384465e0cd7db52cf9d50f
ix updater <ix-updater@users.noreply.github.com>
2026-08-27 19:58:09 +0000

    up 0.10.0 0.10.1 lib/gtk/layer/shell/3/gir lib/gtk/layer/shell/3

--- pkgs/lib/gtk/layer/shell/3/t/ix.sh
+++ pkgs/lib/gtk/layer/shell/3/t/ix.sh
@@ -5,12 +5,12 @@ gtk-layer-shell
 {% endblock %}
 
 {% block version %}
-0.10.0
+0.10.1
 {% endblock %}
 
 {% block fetch %}
 https://github.com/wmww/gtk-layer-shell/archive/refs/tags/v{{self.version().strip()}}.tar.gz
-ed9bb801d6d9252defba41104820ace595dac824dc8972a758ee2ad134e10505
+88c3a3e0a5300532f3d368d5df64838a87f1fb85273f22d41df0a6b8d0ec59c6
 {% endblock %}
 
 {% block lib_deps %}
