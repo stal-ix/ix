80eaa1551d7aeefafffda1f74982257eecdd484e
ix fixer <ix-fixer@users.noreply.github.com>
2026-08-28 21:46:15 +0000

    fix lowdown 3.1.1 musl build

--- pkgs/lib/lowdown/t/ix.sh
+++ pkgs/lib/lowdown/t/ix.sh
@@ -22,6 +22,50 @@ https://github.com/kristapsdz/lowdown/archive/refs/tags/VERSION_{{self.version()
 6676ff3c37f4958615906dae6c03ecd24c40509fd85c06acc98f55fa77e2d61c
 {% endblock %}
 
+{% block patch %}
+patch -p1 <<'EOF'
+--- a/configure
++++ b/configure
+@@ -2628,19 +2628,17 @@ cat << __HEREDOC__
+-#if HAVE_SYS_CDEFS_H
+-# include <sys/cdefs.h>
+-#endif
+-
+-#ifndef __dead
+-# define __dead __attribute__((__noreturn__))
+-#endif /* !__dead */
+-
+-#ifndef __pure
+-# define __pure __attribute__((__pure__))
+-#endif /* !__pure */
+-
+-#ifndef __unused
+-# define __unused __attribute__((__unused__))
+-#endif /* !__unused */
+-
+-#ifndef __used
+-# define __used __attribute__((__used__))
+-#endif /* !__used */
++#if ENABLE_BSD_CDEFS
++# if HAVE_SYS_CDEFS_H
++#  include <sys/cdefs.h>
++# endif
++# ifndef __dead
++#  define __dead __attribute__((__noreturn__))
++# endif /* !__dead */
++# ifndef __pure
++#  define __pure __attribute__((__pure__))
++# endif /* !__pure */
++# ifndef __unused
++#  define __unused __attribute__((__unused__))
++# endif /* !__unused */
++# ifndef __used
++#  define __used __attribute__((__used__))
++# endif /* !__used */
++#endif /* ENABLE_BSD_CDEFS */
+EOF
+{% endblock %}
+
 {% block lib_deps %}
 lib/c
 {% endblock %}
