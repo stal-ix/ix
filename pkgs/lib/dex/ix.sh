39428e9b5a51f7b1d8a288b07cdb81c65d3092d6
ix updater <ix-updater@users.noreply.github.com>
2026-08-28 17:28:52 +0000

    up 1.0.0 1.1.0 lib/dex

--- pkgs/lib/dex/ix.sh
+++ pkgs/lib/dex/ix.sh
@@ -5,12 +5,12 @@ libdex
 {% endblock %}
 
 {% block version %}
-1.0.0
+1.1.0
 {% endblock %}
 
 {% block fetch %}
 https://gitlab.gnome.org/GNOME/libdex/-/archive/{{self.version().strip()}}/libdex-{{self.version().strip()}}.tar.bz2
-5cdef6ae0a19040f82d1d9c21c1b5a9cfe1cc0e5d58bfdd2ce71dcad31a2e965
+06a58786f825d5fcfe84ffac9458efc40c5019176960437b1730e2038d3ae01d
 {% endblock %}
 
 {% block lib_deps %}
