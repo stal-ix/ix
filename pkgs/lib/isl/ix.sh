9227202f6d01e3e4123dd53052055b4bc96a861d
ix updater <ix-updater@users.noreply.github.com>
2026-08-28 13:50:43 +0000

    up 0.27 0.28 lib/isl

--- pkgs/lib/isl/ix.sh
+++ pkgs/lib/isl/ix.sh
@@ -5,12 +5,12 @@ isl
 {% endblock %}
 
 {% block version %}
-0.27
+0.28
 {% endblock %}
 
 {% block fetch %}
 https://libisl.sourceforge.io/isl-{{self.version().strip()}}.tar.xz
-6d8babb59e7b672e8cb7870e874f3f7b813b6e00e6af3f8b04f7579965643d5c
+3dc31b8e1b18329e42d5dfbf84dd55e15c59b61569a2ab246f61497d9592f727
 {% endblock %}
 
 {% block lib_deps %}
