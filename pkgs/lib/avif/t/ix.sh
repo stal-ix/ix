386d8eab11c43c33c5b1c6b379369a157c092ec5
ix updater <ix-updater@users.noreply.github.com>
2026-08-28 17:01:28 +0000

    up 1.3.0 1.4.2 bin/avif lib/avif

--- pkgs/lib/avif/t/ix.sh
+++ pkgs/lib/avif/t/ix.sh
@@ -5,12 +5,12 @@ libavif
 {% endblock %}
 
 {% block version %}
-1.3.0
+1.4.2
 {% endblock %}
 
 {% block fetch %}
 https://github.com/AOMediaCodec/libavif/archive/refs/tags/v{{self.version().strip()}}.tar.gz
-0a545e953cc049bf5bcf4ee467306a2f113a75110edf59e61248873101cd26c1
+2b645287340ba5a631d268b551dc2d72bd73ac33335962dd36dcdb6d8366921d
 {% endblock %}
 
 {% block lib_deps %}
