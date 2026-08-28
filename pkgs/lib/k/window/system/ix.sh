34bc7674ca3d552997367930b0082b9bf8176edc
ix updater <ix-updater@users.noreply.github.com>
2026-08-28 15:50:42 +0000

    up 6.19.0 6.29.0 lib/k/window/system

--- pkgs/lib/k/window/system/ix.sh
+++ pkgs/lib/k/window/system/ix.sh
@@ -5,12 +5,12 @@ kwindowsystem
 {% endblock %}
 
 {% block version %}
-6.19.0
+6.29.0
 {% endblock %}
 
 {% block fetch %}
 https://invent.kde.org/frameworks/kwindowsystem/-/archive/v{{self.version().strip()}}/kwindowsystem-v{{self.version().strip()}}.tar.bz2
-48384ff2e2f3b28595c079ab11b5097991a3af3ca59096ef12819ed1ad21b624
+5bfc959cff452d40493110921c10fe039abe4f9b8c5ac54c0ce26cb71188229c
 {% endblock %}
 
 {% block lib_deps %}
