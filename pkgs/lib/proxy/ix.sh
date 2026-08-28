3a7539cc22b8a0052c15589997e7ab6520c02870
ix updater <ix-updater@users.noreply.github.com>
2026-08-28 19:38:42 +0000

    up 0.5.11 0.5.12 lib/proxy

--- pkgs/lib/proxy/ix.sh
+++ pkgs/lib/proxy/ix.sh
@@ -5,12 +5,12 @@ libproxy
 {% endblock %}
 
 {% block version %}
-0.5.11
+0.5.12
 {% endblock %}
 
 {% block fetch %}
 https://github.com/libproxy/libproxy/archive/refs/tags/{{self.version().strip()}}.tar.gz
-b364f4dbbffc5bdf196330cb76b48abcb489f38b1543e67595ca6cb7ec45d265
+a1fa55991998b80a567450a9e84382421a7176a84446c95caaa8b72cf09fa86f
 {% endblock %}
 
 {% block lib_deps %}
