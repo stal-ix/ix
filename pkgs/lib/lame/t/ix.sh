41ce42afb793962dfe7bb5cdab5cf3a2c3e11d78
ix fixer <ix-fixer@users.noreply.github.com>
2026-08-28 17:43:40 +0000

    fix lame 4.0 build

--- pkgs/lib/lame/t/ix.sh
+++ pkgs/lib/lame/t/ix.sh
@@ -8,6 +8,10 @@ lame
 4.0
 {% endblock %}
 
+{% block conf_ver %}
+2/72
+{% endblock %}
+
 {% block fetch %}
 https://downloads.sourceforge.net/project/lame/lame/{{self.version().strip()}}/lame-{{self.version().strip()}}.tar.gz
 3df5124d5ad3a98312ffd7ba6a9b36230e4f8a3e66d3ce0f425e336c32d216eb
@@ -15,11 +19,18 @@ https://downloads.sourceforge.net/project/lame/lame/{{self.version().strip()}}/l
 
 {% block lib_deps %}
 lib/c
+lib/mpg123
 {% endblock %}
 
 {% block bld_tool %}
 bld/nasm
 bld/gettext
+{% endblock %}
+
+{% block patch %}
+patch -p1 <<'EOF'
+{% include 'id3v2-utf8.patch' %}
+EOF
 {% endblock %}
 
 {% block configure_flags %}
