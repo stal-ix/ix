30c95af80d0bb0a270441d85b1acb5367ef2dfcd
ix updater <ix-updater@users.noreply.github.com>
2026-08-28 15:21:16 +0000

    up 2025.06.03 2026.05.21 bin/kakoune/unwrap

--- pkgs/bin/kakoune/unwrap/ix.sh
+++ pkgs/bin/kakoune/unwrap/ix.sh
@@ -5,12 +5,12 @@ kakoune
 {% endblock %}
 
 {% block version %}
-2025.06.03
+2026.05.21
 {% endblock %}
 
 {% block fetch %}
 https://github.com/mawww/kakoune/archive/refs/tags/v{{self.version().strip()}}.tar.gz
-e065dbb715bfe6b0abdaeee775243115748bd540a346d0212fdca6c20c13d340
+7ef778bd199e05977841e6f69aad2bee6cd58fb23b0e4bc265d012d42de17580
 {% endblock %}
 
 {% block bld_libs %}
