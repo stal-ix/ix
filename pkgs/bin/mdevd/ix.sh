770c049bd58ad412fdda8cc36b362438fe3d1b87
ix updater <ix-updater@users.noreply.github.com>
2026-08-30 04:25:38 +0000

    up 0.1.7.0 0.1.8.2 bin/mdevd

--- pkgs/bin/mdevd/ix.sh
+++ pkgs/bin/mdevd/ix.sh
@@ -5,12 +5,12 @@ mdevd
 {% endblock %}
 
 {% block version %}
-0.1.7.0
+0.1.8.2
 {% endblock %}
 
 {% block fetch %}
 https://github.com/skarnet/mdevd/archive/refs/tags/v{{self.version().strip()}}.tar.gz
-55596f1702207093dd59a3aee4ba68c5e53e08335c27ec6f18444aaf5231679a
+23e242d70492b5381cab2227dd5b5407f748e52fe7a5df919a352c8dae26919d
 {% endblock %}
 
 {% block bld_libs %}
