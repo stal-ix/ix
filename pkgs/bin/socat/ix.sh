32216bb0657e3be04beb39028789f4ad8bcacef9
ix updater <ix-updater@users.noreply.github.com>
2026-08-30 18:35:36 +0000

    up 1.8.0.3 1.8.1.3 bin/socat

--- pkgs/bin/socat/ix.sh
+++ pkgs/bin/socat/ix.sh
@@ -5,12 +5,12 @@ socat
 {% endblock %}
 
 {% block version %}
-1.8.0.3
+1.8.1.3
 {% endblock %}
 
 {% block fetch %}
 http://www.dest-unreach.org/socat/download/socat-{{self.version().strip()}}.tar.gz
-a9f9eb6cfb9aa6b1b4b8fe260edbac3f2c743f294db1e362b932eb3feca37ba4
+06602ffd591e98c75b3dc1d66f0f19136cc666b0b2d95caad987d6ab2cb28097
 {% endblock %}
 
 {% block bld_libs %}
