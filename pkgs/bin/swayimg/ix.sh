511a3411d03f583fb230de0f527106bad8843f1c
ix updater <ix-updater@users.noreply.github.com>
2026-08-30 20:14:42 +0000

    up 4.6 5.5 bin/swayimg

--- pkgs/bin/swayimg/ix.sh
+++ pkgs/bin/swayimg/ix.sh
@@ -5,12 +5,12 @@ swayimg
 {% endblock %}
 
 {% block version %}
-4.6
+5.5
 {% endblock %}
 
 {% block fetch %}
 https://github.com/artemsen/swayimg/archive/refs/tags/v{{self.version().strip()}}.tar.gz
-030ac867501f4d10a2d85bb47d65d51f31c75282f3805079e0aaf9ac05bfb1df
+f84fec722cd0b4cfd86d0a8a820b15a95bbdb1d2cfcded7f67446a7a0c1d1c09
 {% endblock %}
 
 {% block bld_libs %}
