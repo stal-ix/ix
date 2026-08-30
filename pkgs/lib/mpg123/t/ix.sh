57b3952706cbdeb2e4415cc3c0e1d9c307f82fc6
ix updater <ix-updater@users.noreply.github.com>
2026-08-30 05:25:17 +0000

    up 1.33.4 1.33.7 bin/mpg123 lib/mpg123

--- pkgs/lib/mpg123/t/ix.sh
+++ pkgs/lib/mpg123/t/ix.sh
@@ -5,12 +5,12 @@ mpg123
 {% endblock %}
 
 {% block version %}
-1.33.4
+1.33.7
 {% endblock %}
 
 {% block fetch %}
 https://downloads.sourceforge.net/project/mpg123/mpg123/{{self.version().strip()}}/mpg123-{{self.version().strip()}}.tar.bz2
-3ae8c9ff80a97bfc0e22e89fbcd74687eca4fc1db315b12607f27f01cb5a47d9
+31d0e35a4ca567ec9b5ebda6c3062bb4435d6d3eacd6ef0d95cadd7854dc03ee
 {% endblock %}
 
 {% block lib_deps %}
