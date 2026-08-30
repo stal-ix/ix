fd97eae8543d4486b3a29028e4fb0741b29a9640
ix updater <ix-updater@users.noreply.github.com>
2026-08-30 20:49:34 +0000

    up 4.7.0 4.7.2 bin/tiff lib/tiff

--- pkgs/lib/tiff/t/ix.sh
+++ pkgs/lib/tiff/t/ix.sh
@@ -7,12 +7,12 @@ libtiff
 {% endblock %}
 
 {% block version %}
-4.7.0
+4.7.2
 {% endblock %}
 
 {% block fetch %}
 https://gitlab.com/libtiff/libtiff/-/archive/v{{self.version().strip()}}/libtiff-v{{self.version().strip()}}.tar.bz2
-2b8b5c4e79cb2a4651796602e45d6179b7f18bc283feab417b8e881ef7e81862
+c5086d8f7c5ba51ca98241f24a8bd1cb66218c399077aeccbf6a236cf3152acc
 {% endblock %}
 
 {% block lib_deps %}
