6c4050d9a01323edc7227f462c61057eac4a7a34
ix updater <ix-updater@users.noreply.github.com>
2026-08-30 05:28:31 +0000

    up 0.40.0 0.41.0 bin/mpv/unwrap lib/mpv

--- pkgs/lib/mpv/t/ix.sh
+++ pkgs/lib/mpv/t/ix.sh
@@ -5,12 +5,12 @@ mpv
 {% endblock %}
 
 {% block version %}
-0.40.0
+0.41.0
 {% endblock %}
 
 {% block fetch %}
 https://github.com/mpv-player/mpv/archive/refs/tags/v{{self.version().strip()}}.tar.gz
-10a0f4654f62140a6dd4d380dcf0bbdbdcf6e697556863dc499c296182f081a3
+ee21092a5ee427353392360929dc64645c54479aefdb5babc5cfbb5fad626209
 {% endblock %}
 
 {% block lib_deps %}
