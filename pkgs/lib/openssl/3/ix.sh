be5a60bfa9eaffd4d80e02e7d3675f079f3f58e0
ix fixer <ix-fixer@users.noreply.github.com>
2026-08-30 08:52:24 +0000

    keep lib/openssl/3 on OpenSSL 3

--- pkgs/lib/openssl/3/ix.sh
+++ pkgs/lib/openssl/3/ix.sh
@@ -1,16 +1,18 @@
 {% extends 't/ix.sh' %}
 
+# noauto
+
 {% block pkg_name %}
 openssl
 {% endblock %}
 
 {% block version %}
-4.0.2
+3.6.0
 {% endblock %}
 
 {% block fetch %}
 https://github.com/openssl/openssl/archive/refs/tags/openssl-{{self.version().strip()}}.tar.gz
-0c79e20fe326f50eb2ef58ee45306db2258d55bd16f6dbf6061658a1cad48c35
+1535d184cbd04ea6b20485b98719fa8d609e45c3657f7c65b8299ae606f78159
 {% endblock %}
 
 {% block env %}
