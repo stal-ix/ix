de19c1c436108a5311fd60e07b4ab5284d3aedd7
ix updater <ix-updater@users.noreply.github.com>
2026-08-30 05:20:04 +0000

    up 1.15 1.16 bin/moe

--- pkgs/bin/moe/ix.sh
+++ pkgs/bin/moe/ix.sh
@@ -5,12 +5,12 @@ moe
 {% endblock %}
 
 {% block version %}
-1.15
+1.16
 {% endblock %}
 
 {% block fetch %}
 https://ftp.gnu.org/gnu/moe/moe-{{self.version().strip()}}.tar.lz
-41f8c8b099ce3047945ca4e097a60d9243e9c73fbb268c194a12da8b0d9f0a66
+4c25cd78919272aebec0a7f8c126011bb5a4b5d87422807a3423216f0a17a868
 {% endblock %}
 
 {% block bld_libs %}
