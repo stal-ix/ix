53afd2fec546917813e3577ba20cd42abcbeb815
ix updater <ix-updater@users.noreply.github.com>
2026-08-30 04:14:05 +0000

    up 3.3 3.4 lib/cerf

--- pkgs/lib/cerf/ix.sh
+++ pkgs/lib/cerf/ix.sh
@@ -5,12 +5,12 @@ libcerf
 {% endblock %}
 
 {% block version %}
-3.3
+3.4
 {% endblock %}
 
 {% block fetch %}
 https://jugit.fz-juelich.de/mlz/libcerf/-/archive/v{{self.version().strip()}}/libcerf-v{{self.version().strip()}}.tar.bz2
-6177829f3ebbe52970aa784de2de68d0e2a450e6d20eb78f4d14f6f2b6119572
+d70ab4d0d82fa7e456cbeac00f1b1ff494a6720b8d001cbe4c95cbead50bddc0
 {% endblock %}
 
 {% block lib_deps %}
