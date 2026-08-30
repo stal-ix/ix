1e8198853183840e70d286b5afcc60b7eee173f3
ix updater <ix-updater@users.noreply.github.com>
2026-08-30 20:32:22 +0000

    up 5.5.0 7.0.0 bin/tail/spin

--- pkgs/bin/tail/spin/ix.sh
+++ pkgs/bin/tail/spin/ix.sh
@@ -5,7 +5,7 @@ tailspin
 {% endblock %}
 
 {% block version %}
-5.5.0
+7.0.0
 {% endblock %}
 
 {% block cargo_url %}
@@ -13,7 +13,7 @@ https://github.com/bensadeh/tailspin/archive/refs/tags/{{self.version().strip()}
 {% endblock %}
 
 {% block cargo_sha %}
-538e6b8faa5d5e76e06f17fb7c92df4e2e1a873f2c410eecd7e67adfc66b79cf
+737ce20389977de2002ee95c0ef486f657b8da68b796e625284f6b34eb9977d7
 {% endblock %}
 
 {% block cargo_bins %}
@@ -21,5 +21,5 @@ tspin
 {% endblock %}
 
 {% block cargo_tool %}
-bld/cargo/86
+bld/cargo/96
 {% endblock %}
