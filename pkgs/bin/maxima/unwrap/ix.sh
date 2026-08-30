28432b83807df0e4f9222910b1c3732e5286f142
ix updater <ix-updater@users.noreply.github.com>
2026-08-30 04:23:55 +0000

    up 5.49.0 5.50.0 bin/maxima/unwrap

--- pkgs/bin/maxima/unwrap/ix.sh
+++ pkgs/bin/maxima/unwrap/ix.sh
@@ -5,12 +5,12 @@ maxima
 {% endblock %}
 
 {% block version %}
-5.49.0
+5.50.0
 {% endblock %}
 
 {% block fetch %}
 https://downloads.sourceforge.net/project/maxima/Maxima-source/{{self.version().strip()}}-source/maxima-{{self.version().strip()}}.tar.gz
-6d401a4aa307cd3a5a9cadca4fa96c4ef0e24ff95a18bb6a8f803e3d2114adee
+0bc4b5e11fe153ef20b24a3a816b668ece5378cc738fa24ca426b62fd6d8fc37
 {% endblock %}
 
 {% block bld_tool %}
