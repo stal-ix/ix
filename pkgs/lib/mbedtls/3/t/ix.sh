09b3e81ce7964beaaed98bb61e84fb824e0d27ab
ix fixer <ix-fixer@users.noreply.github.com>
2026-08-30 05:56:25 +0000

    pin mbedtls 3 to compatible release

--- pkgs/lib/mbedtls/3/t/ix.sh
+++ pkgs/lib/mbedtls/3/t/ix.sh
@@ -1,11 +1,13 @@
 {% extends '//lib/mbedtls/t/ix.sh' %}
 
+# noauto
+
 {% block pkg_name %}
 mbedtls
 {% endblock %}
 
 {% block version %}
-4.2.0
+3.6.4
 {% endblock %}
 
 {% block git_repo %}
@@ -17,7 +19,7 @@ v{{self.version().strip()}}
 {% endblock %}
 
 {% block git_sha %}
-d3c93b5764ddb1864593c589200c833e641b516f6f0686c479722894fd74591a
+aeb291a29bc3094f528e9e354d575e023c58b4f22abb6fabfe4b274c91bb4f59
 {% endblock %}
 
 {% block lib_deps %}
