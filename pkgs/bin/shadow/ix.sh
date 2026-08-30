e9e97e5392633fb3c33e1b6ab61b755380fa65ff
ix updater <ix-updater@users.noreply.github.com>
2026-08-30 18:01:04 +0000

    up 4.18.0 4.20.2 bin/shadow

--- pkgs/bin/shadow/ix.sh
+++ pkgs/bin/shadow/ix.sh
@@ -5,12 +5,12 @@ shadow
 {% endblock %}
 
 {% block version %}
-4.18.0
+4.20.2
 {% endblock %}
 
 {% block fetch %}
 https://github.com/shadow-maint/shadow/archive/refs/tags/{{self.version().strip()}}.tar.gz
-3f431df0caa13cd351edfa851d127afa4146f6a31129915ef9b79d40eccd4aea
+61a7c9052a3b0ddc3d449e4e833c9ef90069ee944adab223080443adc1a91637
 {% endblock %}
 
 {% block bld_libs %}
