ca381bc1cd4c0d6dc5a1c2b5f618a4442c6db15a
ix updater <ix-updater@users.noreply.github.com>
2026-08-30 19:57:06 +0000

    up 2.3.4 2.4.1 bin/stp

--- pkgs/bin/stp/ix.sh
+++ pkgs/bin/stp/ix.sh
@@ -5,12 +5,12 @@ stp
 {% endblock %}
 
 {% block version %}
-2.3.4
+2.4.1
 {% endblock %}
 
 {% block fetch %}
 https://github.com/stp/stp/archive/refs/tags/{{self.version().strip()}}.tar.gz
-dc197e337c058dc048451b712169a610f7040b31d0078b6602b831fbdcbec990
+6f8bca3612e3d61868450dbf7771897b2a909f446e8de460bdf31f13a6cd0318
 {% endblock %}
 
 {% block bld_libs %}
