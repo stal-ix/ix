d008ba64e625bcc4b76fcc8c378d3b4be5e147fc
ix updater <ix-updater@users.noreply.github.com>
2026-08-30 20:46:03 +0000

    up 1.3.1 2.0.0 bin/think/fan

--- pkgs/bin/think/fan/ix.sh
+++ pkgs/bin/think/fan/ix.sh
@@ -5,12 +5,12 @@ thinkfan
 {% endblock %}
 
 {% block version %}
-1.3.1
+2.0.0
 {% endblock %}
 
 {% block fetch %}
 https://github.com/vmatare/thinkfan/archive/refs/tags/{{self.version().strip()}}.tar.gz
-9466c8c82b7c4333b280fa66445ab26185ffbb4aada6bcb4a164eed742f8d78c
+0fc94eb378dcba8c889e91f41dab3a8d6eebc7324a59a0704cc39aa66551987e
 {% endblock %}
 
 {% block bld_libs %}
