839f85956bba869446cd1eee7adf8394921faf7f
ix updater <ix-updater@users.noreply.github.com>
2026-08-30 22:47:38 +0000

    up 0.82.0 0.83.0 bin/uncrustify

--- pkgs/bin/uncrustify/ix.sh
+++ pkgs/bin/uncrustify/ix.sh
@@ -5,12 +5,12 @@ uncrustify
 {% endblock %}
 
 {% block version %}
-0.82.0
+0.83.0
 {% endblock %}
 
 {% block fetch %}
 https://github.com/uncrustify/uncrustify/archive/refs/tags/uncrustify-{{self.version().strip()}}.tar.gz
-e05f8d5ee36aaa1acfa032fe97546b7be46b1f4620e7c38037f8a42e25fe676f
+1d4d3ab3a991c66eb24cb0a3e690573b4882fd98bc6ec64a01c283998c20dc87
 {% endblock %}
 
 {% block bld_libs %}
