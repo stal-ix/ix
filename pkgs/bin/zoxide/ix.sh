e482ed400af6ac275771c4da5ee4f0452b0233ce
ix updater <ix-updater@users.noreply.github.com>
2026-08-31 06:55:24 +0000

    up 0.9.9 0.10.0 bin/zoxide

--- pkgs/bin/zoxide/ix.sh
+++ pkgs/bin/zoxide/ix.sh
@@ -5,7 +5,7 @@ zoxide
 {% endblock %}
 
 {% block version %}
-0.9.9
+0.10.0
 {% endblock %}
 
 {% block cargo_url %}
@@ -13,7 +13,7 @@ https://github.com/ajeetdsouza/zoxide/archive/refs/tags/v{{self.version().strip(
 {% endblock %}
 
 {% block cargo_sha %}
-d550a59f7a84023641b969194f3b32534382493428cfc97bc517bc741010afdc
+377753db6d70fdb0b51cfa76e4880c8b8db36fc2ef395a23f22cb8cc33bc7eca
 {% endblock %}
 
 {% block cargo_bins %}
@@ -21,5 +21,5 @@ zoxide
 {% endblock %}
 
 {% block cargo_tool %}
-bld/cargo/91
+bld/cargo/96
 {% endblock %}
