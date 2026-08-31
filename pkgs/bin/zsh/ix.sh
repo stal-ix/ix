002e3a8353c2f99056c61ad73e38977e3bb45131
ix updater <ix-updater@users.noreply.github.com>
2026-08-31 06:57:07 +0000

    up 5.9 5.9.2 bin/zsh

--- pkgs/bin/zsh/ix.sh
+++ pkgs/bin/zsh/ix.sh
@@ -5,12 +5,12 @@ zsh
 {% endblock %}
 
 {% block version %}
-5.9
+5.9.2
 {% endblock %}
 
 {% block fetch %}
 https://www.zsh.org/pub/zsh-{{self.version().strip()}}.tar.xz
-9b8d1ecedd5b5e81fbf1918e876752a7dd948e05c1a0dba10ab863842d45acd5
+36fa734374b44783582cec09bcd67822e2f992c779ec1624ab5596df078d2f81
 {% endblock %}
 
 {% block bld_libs %}
