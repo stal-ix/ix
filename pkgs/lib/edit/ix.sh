b791d2872f9b907a7231312b0be704128b964e51
ix updater <ix-updater@users.noreply.github.com>
2026-08-31 17:03:51 +0000

    up 20250104-3.1 20260512-3.1 lib/edit

--- pkgs/lib/edit/ix.sh
+++ pkgs/lib/edit/ix.sh
@@ -5,12 +5,12 @@ libedit
 {% endblock %}
 
 {% block version %}
-20250104-3.1
+20260512-3.1
 {% endblock %}
 
 {% block fetch %}
 https://www.thrysoee.dk/editline/libedit-{{self.version().strip()}}.tar.gz
-23792701694550a53720630cd1cd6167101b5773adddcb4104f7345b73a568ac
+432d5e7ea8b0116dd39f2eca7bc11d0eed77faa6b77ea526ace89907c23ea4a0
 {% endblock %}
 
 {% block lib_deps %}
