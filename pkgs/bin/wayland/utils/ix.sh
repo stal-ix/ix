59a422683c19a59f1013264d3f68320fc03ddb04
ix updater <ix-updater@users.noreply.github.com>
2026-08-31 00:58:29 +0000

    up 1.2.0 1.3.0 bin/wayland/utils

--- pkgs/bin/wayland/utils/ix.sh
+++ pkgs/bin/wayland/utils/ix.sh
@@ -5,12 +5,12 @@ wayland-utils
 {% endblock %}
 
 {% block version %}
-1.2.0
+1.3.0
 {% endblock %}
 
 {% block fetch %}
 https://gitlab.freedesktop.org/wayland/wayland-utils/-/archive/{{self.version().strip()}}/wayland-utils-{{self.version().strip()}}.tar.bz2
-f38c6a4ca2113cf716ca687a4cd8e24a11cbeeb04759678b7bb2da7d16335d18
+898ceff3007b11aaec5b13844ac673b99ee186b2706b9b2ab41ba6be8c29ad06
 {% endblock %}
 
 {% block bld_libs %}
