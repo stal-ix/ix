010821952eb9463fcb3ba644ed9a15373c8cf0a5
ix fixer <ix-fixer@users.noreply.github.com>
2026-08-31 02:01:31 +0000

    fix wl-clipboard 2.3 build

--- pkgs/bin/wl/clipboard/ix/ix.sh
+++ pkgs/bin/wl/clipboard/ix/ix.sh
@@ -1,18 +1,1 @@
 {% extends '//bin/wl/clipboard/stock/ix.sh' %}
-
-{% block bld_libs %}
-{{super()}}
-lib/shim/ix
-{% endblock %}
-
-{% block cpp_missing %}
-{{super()}}
-ix.h
-{% endblock %}
-
-{% block patch %}
-{{super()}}
-sed -e 's|char.*tmp.*|char* dirpath = ix_mkstemp_template();|' \
-    -e 's|sizeof(dirpath)|(strlen(dirpath) + 1)|g' \
-    -i src/util/files.c
-{% endblock %}
