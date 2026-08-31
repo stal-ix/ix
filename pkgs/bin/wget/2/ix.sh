a9f5668d4bc68be7a09ff0a72b9876b9c998b84c
ix updater <ix-updater@users.noreply.github.com>
2026-08-31 01:09:30 +0000

    up 2.2.0 2.2.1 bin/wget/2

--- pkgs/bin/wget/2/ix.sh
+++ pkgs/bin/wget/2/ix.sh
@@ -5,12 +5,12 @@ wget
 {% endblock %}
 
 {% block version %}
-2.2.0
+2.2.1
 {% endblock %}
 
 {% block fetch %}
 https://ftp.gnu.org/gnu/wget/wget2-{{self.version().strip()}}.tar.gz
-2b3b9c85b7fb26d33ca5f41f1f8daca71838d869a19b406063aa5c655294d357
+d7544b13e37f18e601244fce5f5f40688ac1d6ab9541e0fbb01a32ee1fb447b4
 {% endblock %}
 
 {% block bld_libs %}
